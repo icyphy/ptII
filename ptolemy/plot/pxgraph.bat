62a3776e39f14c11f9bcfe5118188c4d116e1673
cxh <cxh>
1997-10-02 13:09:29 +0000

    plot -> ptplot

--- ptolemy/plot/pxgraph.bat
+++ ptolemy/plot/pxgraph.bat
@@ -10,12 +10,12 @@ rem Check the TYCHO variable.
 if not "%tycho%" == "" set TYCHO=c:\tycho0.2.1devel
 
 rem Make sure that we can find the parts of Tycho we need
-if exist %tycho%\java\plot\pxgraph.bat goto tychoexists
-echo %tycho\java\plot\pxgraph.bat does not exist! exiting.
+if exist %tycho%\java\ptplot\pxgraph.bat goto tychoexists
+echo %tycho\java\ptplot\pxgraph.bat does not exist! exiting.
 exit
 :tychoexists
 
 rem echo Starting Plotter with $TYCHO = "%tycho%"
 
-java -classpath %classpath%;%java_home%\lib\classes.zip;%tycho%\java plot.Pxgraph %1 %2 %3 %4 %5 %6 %7 %8 %9
+java -classpath %classpath%;%java_home%\lib\classes.zip;%tycho%\java ptplot.Pxgraph %1 %2 %3 %4 %5 %6 %7 %8 %9
 
