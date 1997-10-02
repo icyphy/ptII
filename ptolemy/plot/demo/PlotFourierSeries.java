159a61a2fd6eb3ef44fb0dac8c0c32eb763258dc
eal <eal>
1997-10-02 10:18:25 +0000

    Initial revision

---
+++ ptolemy/plot/demo/PlotFourierSeries.java
@@ -0,0 +1,81 @@
+/* Demo for a signal plotter.
+
+@Copyright (c) 1997 The Regents of the University of California.
+All rights reserved.
+
+Permission is hereby granted, without written agreement and without
+license or royalty fees, to use, copy, modify, and distribute this
+software and its documentation for any purpose, provided that the
+above copyright notice and the following two paragraphs appear in all
+copies of this software.
+
+IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+SUCH DAMAGE.
+
+THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ENHANCEMENTS, OR MODIFICATIONS.
+
+                                                PT_COPYRIGHT_VERSION_2
+                                                COPYRIGHTENDKEY
+*/
+package plot;
+
+import java.awt.*;
+import java.applet.Applet;
+
+//////////////////////////////////////////////////////////////////////////
+//// PlotFourierSeries
+/** 
+ * Plot a Fourier series approximation to a square wave.
+ * This is a demonstration of the use of the Plot class.
+ *
+ * @author: Edward A. Lee
+ * @version: $Id$
+ */
+public class PlotFourierSeries extends Plot {
+
+    //////////////////////////////////////////////////////////////////////////
+    ////                         public methods                           ////
+        
+    /**
+     * Return a string describing this applet.
+     */
+    public String getAppletInfo() {
+        return "PlotFourierSeries 1.0: Demo of Plot.\n" +
+            "By: Edward A. Lee, eal@eecs.berkeley.edu\n " +
+            "$Id$";
+    }
+
+    /**
+     * Initialize the applet.
+     */
+    public void init () {
+        super.init();
+        
+        setTitle("Fourier Series Approximation");
+        setXRange(0,400);
+        setXLabel("x");
+        setYLabel("y");
+        setNumSets(11);
+        setMarksStyle("none");
+        
+        boolean first = true;
+        for (int i=0; i <= 400; i++) {
+            double approximation = 0.0;
+            for (int j=0; j < 11; j++) {
+                double sig = 4.0*Math.sin(i*2.0*Math.PI*(2*j+1)/400.0)/
+                       (Math.PI*(2*j+1));
+                approximation += sig;
+                addPoint(j,(double)i, approximation, !first);
+            }
+            first = false;
+        }
+    }
+}
