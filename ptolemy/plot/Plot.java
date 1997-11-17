837b9c63c97df17ea3deef11da32aa69f8dc35ab
cxh <cxh>
1997-11-17 20:01:14 +0000

    Removed bogus break; added from last change

--- ptolemy/plot/Plot.java
+++ ptolemy/plot/Plot.java
@@ -1034,7 +1034,7 @@ public class Plot extends PlotBox {
                 }
                 break;
             default:
-                break;
+                // none
             }
         }
     }
