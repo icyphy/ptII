ff7402299d29cad65a1dd9c28906f2ebd1006b17
cxh <cxh>
1997-12-17 14:00:17 +0000

    Reformatted

--- ptolemy/plot/demo/Ptplot.js
+++ ptolemy/plot/demo/Ptplot.js
@@ -1,4 +1,8 @@
-// $Id$
+// JavaScript interface to PlotApplet
+//
+// @Author: Christopher Hylands
+// @Version: $Id$
+//
 // @Copyright (c) 1997 The Regents of the University of California.
 // All rights reserved.
 // 
@@ -25,96 +29,97 @@
 //                                                COPYRIGHTENDKEY
 
 
-// 
-function checkBoxValue(flag, onstring, offstring) {
-	if (flag == "on") 
-		return onstring
-	else
-		return offstring
-}
-function computeArgs(theform) {
-	var retval = "";
-	retval += ( theform.ticks.value == "on" ) ? "-tk ": "";
-	retval += ( theform.binary.value == "on" ) ? "-binary ": "";
-	return retval;
-}
-
-function computeArgs(ticks, binary) {
-	var retval = "";
-	retval += ( ticks == "on" ) ? "-tk ": "";
-	retval += ( binary == "on" ) ? "-binary ": "";
-	return retval;
-}
-
-// Create an applet with the following parameters
+//////////////////////////////////////////////////////////////////////////
+// ptplot
+// Generate html with parameters from theform.
+//
 function ptplot(theform) {
-var width = theform.width.value;
-var height = theform.height.value; 
-var background = theform.background.value;
-var foreground = theform.foreground.value;
-var urlbase = theform.urlbase.value;
-fileObj = theform.fileSelection;
-var file = fileObj.options[fileObj.selectedIndex].text;
-var pxgraphargs = theform.pxgraphargs.value; 
-var displayargs = theform.displayargs.checked;
-var otherargs = "";
+    var width = theform.width.value;
+    var height = theform.height.value; 
+    var background = theform.background.value;
+    var foreground = theform.foreground.value;
+    var urlbase = theform.urlbase.value;
+    fileObj = theform.fileSelection;
+    var files = fileObj.options[fileObj.selectedIndex].text;
+    var pxgraphargs = theform.pxgraphargs.value; 
+    var displayargs = theform.displayargs.checked;
+    var otherargs = "";
 
-otherargs += ( theform.bar.checked ) ? "-bar ": "";
-otherargs += ( theform.binary.checked ) ? "-binary ": "";
-otherargs += ( theform.nolines.checked ) ? "-nl ": "";
-otherargs += ( theform.smallpoints.checked ) ? "-p ": "";
-otherargs += ( theform.largepoints.checked ) ? "-P ": "";
-otherargs += ( theform.reversevideo.checked ) ? "-rv ": "";
-otherargs += ( theform.ticks.checked ) ? "-tk ": "";
-otherargs += ( theform.title.value != "" ) ? 
+    otherargs += ( theform.bar.checked ) ? "-bar ": "";
+    otherargs += ( theform.binary.checked ) ? "-binary ": "";
+    otherargs += ( theform.nolines.checked ) ? "-nl ": "";
+    otherargs += ( theform.smallpoints.checked ) ? "-p ": "";
+    otherargs += ( theform.largepoints.checked ) ? "-P ": "";
+    otherargs += ( theform.reversevideo.checked ) ? "-rv ": "";
+    otherargs += ( theform.ticks.checked ) ? "-tk ": "";
+    otherargs += ( theform.title.value != "" ) ? 
 	"-t '" + theform.title.value + "' " : "";
-//otherargs += ( theform.titlefont.value != "" ) ? 
-//	"-tf '" + theform.titlefont.value + "' " : "";
+    otherargs += "-tf " +
+        theform.titleFontFamily.options[
+                theform.titleFontFamily.selectedIndex].text +
+        "-" +
+        theform.titleFontStyle.options[
+                theform.titleFontStyle.selectedIndex].text +
+        "-" +
+        theform.titleFontSize.options[
+                theform.titleFontSize.selectedIndex].text +
+        " ";
 
-otherargs += "-tf " +
- theform.titleFontFamily.options[theform.titleFontFamily.selectedIndex].text +
- "-" +
- theform.titleFontStyle.options[theform.titleFontStyle.selectedIndex].text +
- "-" +
- theform.titleFontSize.options[theform.titleFontSize.selectedIndex].text +
- " ";
+    otherargs += "-lf " +
+        theform.labelFontFamily.options[
+                theform.labelFontFamily.selectedIndex].text +
+        "-" +
+        theform.labelFontStyle.options[
+                theform.labelFontStyle.selectedIndex].text +
+        "-" +
+        theform.labelFontSize.options[
+                theform.labelFontSize.selectedIndex].text +
+        " ";
 
-otherargs += ( theform.xaxis.value != "" ) ? 
+    otherargs += ( theform.xaxis.value != "" ) ? 
 	"-x '" + theform.xaxis.value + "' " : "";
-otherargs += ( theform.yaxis.value != "" ) ? 
+    otherargs += ( theform.yaxis.value != "" ) ? 
 	"-y '" + theform.yaxis.value + "' " : "";
 
-if (urlbase != "" && file != "(NONE)") {
-	pxgraphargs = urlbase + file + " " + pxgraphargs;
-}
+    if (urlbase != "" && files != "(NONE)") {
+        // Split filenames by spaces.  Note that this means we
+        // cannot support filenames with spaces in the file selector
+        filearray = files.split(" ");
+        var tmpfiles = "";
+        for (var i=0; i < filearray.length; i++) {
+            tmpfiles+= urlbase + filearray[i] + " ";
+        }
+	pxgraphargs = tmpfiles + pxgraphargs;
+    }
 
-pxgraphargs = otherargs + pxgraphargs;
+    pxgraphargs = otherargs + pxgraphargs;
 
 
-document.write("<html>");
-document.write("<head>");
-document.write("<title>Plot</title>");
-document.write("<body bgcolor=\"#faf0e6\">");
-document.write("<H1>Plot</H1>");
-//document.write("<pre>");
-document.write("<applet name=\"JSPlot\" code=\"ptplot.PlotApplet\"");
-document.write("   width=" + parseInt(width));
-document.write("   height=" + parseInt(height));
-document.write("   codebase=\"../..\"\n");
-document.write("   alt=\"If you had a java-enabled browser you would see an applet here.\">\n");
-if ( background != "" )
- document.write("<param name=\"background\" value=\"" + background +"\">\n");
-if ( foreground.value != "" )
- document.write("<param name=\"foreground\" value=\"" + foreground +"\">\n");
-if ( pxgraphargs != "" )
- document.write("<param name=\"pxgraphargs\" value=\"" + pxgraphargs +"\">\n");
-document.write("</applet>");
-document.write("<form name=\"aboutplot\">")
-document.write("<input type=\"button\" Value=\"About\"");
-document.write("onClick=\"alert(document.JSPlot.getAppletInfo())\">");
-document.write("</form>");
-//document.write("</pre>");
-if ( displayargs ) {
+    document.write("<html>");
+    document.write("<head>");
+    document.write("<title>Plot</title>");
+    document.write("<body bgcolor=\"#faf0e6\">");
+    document.write("<H1>Plot</H1>");
+    document.write("<applet name=\"JSPlot\" code=\"ptplot.PlotApplet\"");
+    document.write("   width=" + parseInt(width));
+    document.write("   height=" + parseInt(height));
+    document.write("   codebase=\"../..\"\n");
+    document.write("   alt=\"If you had a java-enabled browser you would see an applet here.\">\n");
+    if ( background != "" )
+        document.write("<param name=\"background\" value=\"" +
+                background +"\">\n");
+    if ( foreground.value != "" )
+        document.write("<param name=\"foreground\" value=\"" +
+                foreground +"\">\n");
+    if ( pxgraphargs != "" )
+        document.write("<param name=\"pxgraphargs\" value=\"" +
+                pxgraphargs +"\">\n");
+    document.write("</applet>");
+    document.write("<form name=\"aboutplot\">")
+        document.write("<input type=\"button\" Value=\"About\"");
+    document.write("onClick=\"alert(document.JSPlot.getAppletInfo())\">");
+    document.write("</form>");
+    if ( displayargs ) {
 	document.write("<table border> <tr><th>Variable<th>Value");
 	document.write("<tr><td>width<td>" + width);
 	document.write("<tr><td>height<td>" + height);
@@ -122,9 +127,7 @@ if ( displayargs ) {
 	document.write("<tr><td>foreground<td>" + foreground);
 	document.write("<tr><td>pxgraphargs<td>" + pxgraphargs);
 	document.write("</table>");
-}
-
-document.write("</body>");
-document.write("</html>");
-
+    }
+    document.write("</body>");
+    document.write("</html>");
 }
