26c23ca2662140860418bc6a58fee198d04e71a3
cxh <cxh>
1998-01-02 16:30:01 +0000

    Fixed todo list at top

--- ptolemy/plot/Plot.java
+++ ptolemy/plot/Plot.java
@@ -31,20 +31,8 @@ package ptplot;
 //   - support for oscilloscope-like plots (where x axis wraps around).
 //   - steps between points rather than connected lines.
 //   - cubic spline interpolation
-//   - support error bars (ydelta, or ylow yhigh)
-//     (adjust yrange to fit error bars)
 //   - log scale axes
-//   - bar graph output does not handle having the y value be 0 or
-//     less than 0.  With the dataset below, there is no bar for the 2nd val.
-//     1 1
-//     3 -0.2
-//   - Support raw 4 byte float data files.  Here, we could check to 
-//     see if the first character was d, e, n, or m.  If it was, we
-//     would assume pxgraph format.  If it was not, we would read in the
-//     data as 4 byte float x/y pairs.  We could also have a command
-//     line arg called -raw that would do the same.  It would be nice if
-//     we could handle other formats, such as raw ints and doubles, but
-//     that would be real work.
+//   - get rid of all of the deprecated lines and require JDK1.1
 //     
 // NOTE: The XOR drawing mode is needed in order to be able to erase
 // plotted points and restore the grid line, tick marks, and boundary
