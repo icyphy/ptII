ba346cd2155e01f3faf30d3cb05fb24017464bdd
eal <eal>
1998-01-04 17:03:18 +0000

    Initial revision

---
+++ ptolemy/actor/util/FIFOQueue.java
@@ -0,0 +1,270 @@
+/* A queue with optional capacity and history.
+
+ Copyright (c) 1997 The Regents of the University of California.
+ All rights reserved.
+ Permission is hereby granted, without written agreement and without
+ license or royalty fees, to use, copy, modify, and distribute this
+ software and its documentation for any purpose, provided that the above
+ copyright notice and the following two paragraphs appear in all copies
+ of this software.
+
+ IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+ FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+ ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+ THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+ SUCH DAMAGE.
+
+ THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+ INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+ PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+ CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ ENHANCEMENTS, OR MODIFICATIONS.
+ 
+                                        PT_COPYRIGHT_VERSION_2
+                                        COPYRIGHTENDKEY
+
+@ProposedRating Red (eal@eecs.berkeley.edu)
+
+*/
+
+// FIXME: This should be moved to another package.
+package pt.kernel;
+
+import collections.LinkedList;
+import collections.CollectionEnumeration;
+import java.util.NoSuchElementException;
+
+//////////////////////////////////////////////////////////////////////////
+//// FIFOQueue
+/** 
+A first-in, first-out (FIFO) queue with optional capacity and
+history. Objects are appended to the queue with the put() method,
+and removed from the queue with the take() method. The object
+removed is the oldest one in the queue. By default, the capacity is
+unbounded, but it can be set to any nonnegative size. If the history
+capacity is greater than zero (or infinite, indicated by a capacity
+of -1), then objects removed from the queue are transfered to a
+second queue rather than simply deleted. By default, the history
+capacity is zero.
+
+@author Edward A. Lee
+@version $Id$
+*/
+public class FIFOQueue implements Cloneable {
+
+    /** Construct an empty queue with no container.
+     */	
+    public FIFOQueue() {
+        _queuelist = new LinkedList();
+        _historylist = new LinkedList();
+    }
+
+    /** Construct an empty queue with the specified container.
+     */	
+    public FIFOQueue(Nameable container) {
+        this();
+        _container = container;
+    }
+
+    /** Copy constructor.  Create a copy of the specified queue, but
+     *  with no container.  This is useful to permit enumerations over
+     *  a queue while the queue continues to be modified.
+     */	
+    public FIFOQueue(FIFOQueue model) {
+        this();
+        _queuelist.appendElements(model.elements());
+    }
+
+    /** Copy constructor.  Create a copy of the specified queue, but
+     *  with the specified container.
+     */	
+    public FIFOQueue(FIFOQueue model, Nameable container) {
+        this(model);
+        _container = container;
+    }
+
+    //////////////////////////////////////////////////////////////////////////
+    ////                         public methods                           ////
+
+    /** Return the capacity, or -1 if it is unbounded.
+     */	
+    public int capacity() {
+        return _queuecapacity;
+    }
+
+    /** Enumerate the items on the queue, beginning with the oldest.
+     *  @return An enumeration of objects.
+     *  @see collections.LinkedList#elements()
+     */	
+    public CollectionEnumeration elements() {
+        return _queuelist.elements();
+    }
+
+    /** Return true if the number of objects in the queue equals the
+     *  capacity.
+     */	
+    public boolean full() {
+        return _queuelist.size() == _queuecapacity;
+    }
+
+    /** Return an element on the queue.  If the offset argument is
+     *  zero, return the most recent object that was put on the queue.
+     *  If the offset is 1, return second most recent the object, etc.
+     *  If there is no such element on the queue (the offset is greater
+     *  than or equal to the size, or is negative), throw an exception.
+     *  @exception NoSuchElementException The offset is out of range.
+     */	
+    public Object get(int offset) 
+            throws NoSuchElementException {
+        return _queuelist.at(size()-offset-1);
+    }
+
+    /** Return the container of the queue, or null if there is none.
+     */	
+    public Nameable getContainer() {
+        return _container;
+    }
+
+    /** Enumerate the items stored in the history queue, which are
+     *  the N most recent items taken from the queue, beginning with
+     *  the oldest, where N is less
+     *  than or equal to the history capacity.  If the history capacity
+     *  is -1, then the enumeration includes all items previously taken
+     *  from the queue.  If the history capacity is zero, then return an
+     *  empty enumeration.
+     *  @return An enumeration of objects.
+     *  @see collections.LinkedList#elements()
+     */	
+    public CollectionEnumeration history() {
+        return _historylist.elements();
+    }
+
+    /** Return the capacity of the history queue.
+     *  This will be zero if the history mechanism is disabled
+     *  and -1 if the history capacity is unbounded.
+     */	
+    public int historyCapacity() {
+        return _historycapacity;
+    }
+
+    /** Return the number of objects in the history.
+     */	
+    public int historySize() {
+        return _historylist.size();
+    }
+
+    /** Return an element from the history.  If the offset argument is
+     *  zero, return the most recent object in the history, which is
+     *  object most recently taken from the queue.
+     *  If the offset is 1, return second most recent the object, etc.
+     *  If there is no such element in the history (the offset is greater
+     *  than or equal to the number of objects in the history, or is
+     *  negative), throw an exception.
+     *  @exception NoSuchElementException The offset is out of range.
+     */	
+    public Object previous(int offset) 
+            throws NoSuchElementException {
+        return _historylist.at(historySize()-offset-1);
+    }
+
+    /** Put an object on the queue and return true if this will not
+     *  cause the capacity to be exceeded.  Otherwise, do not put
+     *  the object on the queue and return false.
+     *  @param element An object to put on the queue.
+     *  @return A boolean indicating success.
+     */	
+    public boolean put(Object element) {
+        if (_queuecapacity == -1 || _queuecapacity > _queuelist.size()) {
+            _queuelist.insertLast(element);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /** Set the capacity.  Use -1 to indicate unbounded capacity
+     *  (which is the default).  If the size of the queue exceeds the
+     *  desired capacity, throw an exception.
+     *  @exception IllegalActionException Queue contains more elements
+     *   than the proposed capacity.
+     */	
+    public void setCapacity(int capacity) 
+           throws IllegalActionException {
+        if (size() > capacity) {
+            throw new IllegalActionException(_container,
+                   "Queue contains more elements than the proposed capacity.");
+        }
+        _queuecapacity = capacity;
+    }
+
+    /** Set the capacity of the history queue.
+     *  Use 0 to disable the history mechanism
+     *  and -1 to make the history capacity unbounded.
+     *  If the size of the history list exceeds the
+     *  desired capacity, then remove the oldest items from
+     *  the history list until its size equals the proposed capacity.
+     *  Note that this can be used to clear the history list.
+     */	
+    public void setHistoryCapacity(int capacity) {
+        if (capacity > 0) {
+            while (_historylist.size() > capacity) {
+                _historylist.take();
+            }
+        }
+        _historycapacity = capacity;
+    }
+
+    /** Return the number of objects in the queue.
+     */	
+    public int size() {
+        return _queuelist.size();
+    }
+
+    /** Take the oldest object off the queue and return it.
+     *  If there is no such object on the queue (the queue is empty),
+     *  then return null.  If the history mechanism is enabled,
+     *  then put the taken object on the history queue.  If the capacity
+     *  of the history queue would be exceeded by this, then first remove
+     *  the oldest object on that queue.
+     *  @return An object from the queue.
+     */	
+    public Object take() {
+        if (_queuelist.size() > 0) {
+            // Ignore the exception since we ensure it can't occur.
+            try {
+                Object obj = _queuelist.take();
+                if (_historycapacity != 0) {
+                    if (_historycapacity == _historylist.size()) {
+                        _historylist.take();
+                    }
+                    _historylist.insertLast(obj);
+                }
+                return obj;
+            } catch (NoSuchElementException ex) {
+                // Does not happen...
+                return null;
+            }
+        } else {
+            return null;
+        }
+    }
+
+    //////////////////////////////////////////////////////////////////////////
+    ////                         private variables                        ////
+
+    // The container, if there is one.
+    private Nameable _container = null;
+
+    // The capacity of the queue, defaulting to infinite.
+    private int _queuecapacity = -1;
+
+    // The list of objects currently in the queue.
+    private LinkedList _queuelist;
+
+    // The capacity of the history queue, defaulting to zero.
+    private int _historycapacity = 0;
+
+    // The list of objects recently removed from the queue.
+    private LinkedList _historylist = null;
+}
---
+++ ptolemy/actor/util/test/FIFOQueue.tcl
@@ -0,0 +1,215 @@
+# Tests for the FIFOQueue class
+#
+# @Author: Edward A. Lee
+#
+# @Version: $Id$
+#
+# @Copyright (c) 1988 The Regents of the University of California.
+# All rights reserved.
+# 
+# Permission is hereby granted, without written agreement and without
+# license or royalty fees, to use, copy, modify, and distribute this
+# software and its documentation for any purpose, provided that the
+# above copyright notice and the following two paragraphs appear in all
+# copies of this software.
+# 
+# IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+# FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+# ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+# THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+# 
+# THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+# INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+# PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+# CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+# ENHANCEMENTS, OR MODIFICATIONS.
+# 
+# 						PT_COPYRIGHT_VERSION_2
+# 						COPYRIGHTENDKEY
+#######################################################################
+
+# Tycho test bed, see $TYCHO/doc/coding/testing.html for more information.
+
+# Load up the test definitions.
+if {[string compare test [info procs test]] == 1} then { 
+    source testDefs.tcl
+} {}
+
+# Load up Tcl procs to print out enums
+if {[info procs _testEnums] == "" } then { 
+    source testEnums.tcl
+}
+
+# Uncomment this to get a full report, or set in your Tcl shell window.
+# set VERBOSE 1
+
+# If a file contains non-graphical tests, then it should be named .tcl
+# If a file contains graphical tests, then it should be called .itcl
+# 
+# It would be nice if the tests would work in a vanilla itkwish binary.
+# Check for necessary classes and adjust the auto_path accordingly.
+#
+
+######################################################################
+####
+# 
+# test FIFOQueue-1.1 {Get class information} {
+#     # If anything changes, we want to know about it so we can write tests.
+#     set n [java::new pt.kernel.FIFOQueue]
+#     list [getJavaInfo $n]
+# } {{
+#   class:         pt.kernel.FIFOQueue
+#   fields:        
+#   methods:       getClass hashCode {equals java.lang.Object} toString notify notifyAll {wait long} {wait long int} wait capacity elements full {get int} getContainer history historyCapacity historySize {previous int} {put java.lang.Object} {setCapacity int} {setHistoryCapacity int} size take
+#   constructors:  pt.kernel.FIFOQueue {pt.kernel.FIFOQueue pt.kernel.Nameable} {pt.kernel.FIFOQueue pt.kernel.FIFOQueue}
+#   properties:    class historyCapacity capacity container {{}}
+#   superclass:    java.lang.Object
+# }}
+# 
+######################################################################
+####
+# 
+test FIFOQueue-2.1 {Construct an empty queue and check defaults} {
+    set queue [java::new pt.kernel.FIFOQueue]
+    list [$queue capacity] \
+            [_testEnums elements $queue] \
+            [$queue full] \
+            [_testEnums history $queue] \
+            [$queue historyCapacity] \
+            [$queue historySize] \
+            [$queue size]
+} {-1 {{}} 0 {{}} 0 0 0}
+
+######################################################################
+####
+# 
+test FIFOQueue-2.2 {Construct an empty queue and attempt a get, previous, take} {
+    set queue [java::new pt.kernel.FIFOQueue]
+    catch { [$queue get 0] } msg1
+    catch { [$queue previous 0] } msg2
+    list $msg1 $msg2 [expr { [$queue take] == [java::null] }]
+} {{java.util.NoSuchElementException: Element access on empty collection} {java.util.NoSuchElementException: Element access on empty collection} 1}
+
+######################################################################
+####
+# 
+test FIFOQueue-2.3 {Construct an empty queue with a container} {
+    set container [java::new pt.kernel.NamedObj "parent"]
+    set queue [java::new {pt.kernel.FIFOQueue pt.kernel.Nameable} $container]
+    [$queue getContainer] getName
+} {parent}
+
+
+    ######################################################################
+    ######################################################################
+    # The following named objects are used throughout the rest of the tests.
+    set n1 [java::new pt.kernel.NamedObj "n1"]
+    set n2 [java::new pt.kernel.NamedObj "n2"]
+    set n3 [java::new pt.kernel.NamedObj "n3"]
+    set n4 [java::new pt.kernel.NamedObj "n4"]
+    set n5 [java::new pt.kernel.NamedObj "n5"]
+
+######################################################################
+####
+# 
+test FIFOQueue-3.1 {Put data on a queue} {
+    set queue [java::new pt.kernel.FIFOQueue]
+    $queue put $n1
+    $queue put $n2
+    $queue put $n3
+    $queue put $n4
+    $queue put $n5
+    _testEnums elements $queue
+} {{n1 n2 n3 n4 n5}}
+
+######################################################################
+####
+# 
+test FIFOQueue-3.2 {Get individual items} {
+    list [[$queue get 1] getName] [$queue size] [$queue full]
+} {n4 5 0}
+
+######################################################################
+####
+# 
+test FIFOQueue-3.3 {Take items} {
+    list [[$queue take] getName] [_testEnums elements $queue]
+} {n1 {{n2 n3 n4 n5}}}
+
+######################################################################
+####
+# 
+test FIFOQueue-4.1 {Put data on a queue with bounded capacity} {
+    set queue [java::new pt.kernel.FIFOQueue]
+    $queue setCapacity 3
+    list \
+            [$queue put $n1] \
+            [$queue put $n2] \
+            [$queue put $n3] \
+            [$queue put $n4] \
+            [_testEnums elements $queue]
+} {1 1 1 0 {{n1 n2 n3}}}
+
+######################################################################
+####
+# 
+test FIFOQueue-4.2 {Take data off a queue with bounded capacity} {
+    $queue take
+    list \
+            [$queue put $n5] \
+            [_testEnums elements $queue]
+} {1 {{n2 n3 n5}}}
+
+######################################################################
+####
+# 
+test FIFOQueue-5.1 {Test history} {
+    set queue [java::new pt.kernel.FIFOQueue]
+    $queue setHistoryCapacity -1
+    $queue put $n1
+    $queue put $n2
+    $queue put $n3
+    $queue put $n4
+    $queue put $n5
+    $queue take
+    $queue take
+    $queue take
+    list [_testEnums elements $queue] [_testEnums history $queue]
+} {{{n4 n5}} {{n1 n2 n3}}}
+
+######################################################################
+####
+# 
+test FIFOQueue-5.2 {Test previous} {
+    list \
+            [[$queue previous 0] getName] \
+            [[$queue previous 1] getName] \
+            [[$queue previous 2] getName]
+} {n3 n2 n1}
+
+######################################################################
+####
+# 
+test FIFOQueue-5.3 {Test previous with error} {
+    catch {[$queue previous 3]} msg
+    list $msg
+} {{java.util.NoSuchElementException: Index -1 out of range for collection of size3}}
+
+######################################################################
+####
+# 
+test FIFOQueue-6.1 {Test history with bounded capacity} {
+    set queue [java::new pt.kernel.FIFOQueue]
+    $queue setHistoryCapacity 2
+    $queue put $n1
+    $queue put $n2
+    $queue put $n3
+    $queue put $n4
+    $queue put $n5
+    $queue take
+    $queue take
+    $queue take
+    list [_testEnums elements $queue] [_testEnums history $queue]
+} {{{n4 n5}} {{n2 n3}}}
