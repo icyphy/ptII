57e1a0018c1cb72b7aa5a411a578edf9ad9eaa61
mudit <mudit>
1998-02-11 11:12:21 +0000

    Initial revision

---
+++ ptolemy/data/ScalarToken.java
@@ -0,0 +1,50 @@
+/* A base class for tokens that contain a reference to a scalar object.
+
+ Copyright (c) 1997 The Regents of the University of California.
+ All rights reserved.
+ Permission is hereby granted, without written agreement and without
+ license or royalty fees, to use, copy, modify, and distribute this
+ software and its documentation for any purpose, provided that the above
+ copyright notice and the following two paragraphs appear in all copies
+ of this software.
+
+ IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+ FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+ ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+ THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+ SUCH DAMAGE.
+
+ THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+ INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+ PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+ CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ ENHANCEMENTS, OR MODIFICATIONS.
+ 
+                                        PT_COPYRIGHT_VERSION_2
+                                        COPYRIGHTENDKEY
+*/
+
+package pt.kernel;
+
+//////////////////////////////////////////////////////////////////////////
+//// ScalarToken
+/** 
+A token that contains a reference to an arbitrary scalar object.
+
+@author Mudit Goel
+@version $Id$
+*/
+public abstract class ScalarToken extends Token {
+
+    //////////////////////////////////////////////////////////////////////////
+    ////                         public methods                           ////
+
+    public abstract byte byteValue();
+    public abstract Object complexValue();
+    public abstract double doubleValue();
+    public abstract Object fixValue();
+    public abstract int intValue();
+    public abstract long longValue();
+
+}
