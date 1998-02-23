49bab53e47fc024be9e227483773de0a228e9e9f
mudit <mudit>
1998-02-23 20:01:18 +0000

    Created and runs??

--- ptolemy/domains/pn/demo/PNInterleavingExample.java
+++ ptolemy/domains/pn/demo/PNInterleavingExample.java
@@ -29,7 +29,7 @@ package pt.domains.pn.kernel;
 import pt.kernel.*;
 
 //////////////////////////////////////////////////////////////////////////
-//// PNExample
+//// PNInterleavingExample
 /** 
 An example to test the PN domain. This example tests the PN INterleaving 
 example.
@@ -37,7 +37,7 @@ example.
 @version $Id$
 */
 
-class PNInterExample {
+class PNInterleavingExample {
 
     public static void main(String args[]) throws 
 	    IllegalStateException, IllegalActionException, 
@@ -45,48 +45,48 @@ class PNInterExample {
 	PNUniverse myUniverse = new PNUniverse();
         myUniverse.setMode(Integer.parseInt(args[0]));
         myUniverse.setNoCycles(Integer.parseInt(args[1]));
-        _interleave = new PNInterleave(myUniverse, "interleave");
+        PNInterleave _interleave = new PNInterleave(myUniverse, "interleave");
         _interleave.initialize();
-        _interleave.setCycles(_count);
-        _alternate = new PNAlternate(myUniverse, "alternate");
+        _interleave.setCycles(Integer.parseInt(args[1]));
+        PNAlternate _alternate = new PNAlternate(myUniverse, "alternate");
         _alternate.initialize();
-        _redirect0 = new PNRedirect(myUniverse, "redirect0");
+        PNRedirect _redirect0 = new PNRedirect(myUniverse, "redirect0");
         _redirect0.initialize(0);
-        _redirect1 = new PNRedirect(myUniverse, "redirect1");
+        PNRedirect _redirect1 = new PNRedirect(myUniverse, "redirect1");
         _redirect1.initialize(1);
 
         //FIXME: Find a neat way of specifying the queue length of input port!
         //FIXME: Need a nice way of doing the following.
         //Maybe a nice method that set all star parameters and links all ports
-        _queueX = new IORelation(myUniverse, "QX");
+        IORelation _queueX = new IORelation(myUniverse, "QX");
         PNPort port = (PNPort)_interleave.getPort("output");
         port.link(_queueX);
         port = (PNPort)_alternate.getPort("input");
         port.getQueue().setCapacity(1);
         port.link(_queueX);
  
-        _queueY = new IORelation(myUniverse, "QY");
+        IORelation _queueY = new IORelation(myUniverse, "QY");
         port = (PNPort)_redirect0.getPort("output");
         port.link(_queueY);
         port = (PNPort)_interleave.getPort("input1");
         port.getQueue().setCapacity(1);
         port.link(_queueY);        
  
-        _queueZ = new IORelation(PNUniverse, "QZ");
+        IORelation _queueZ = new IORelation(PNUniverse, "QZ");
         port = (PNPort)_redirect1.getPort("output");
         port.link(_queueZ);
         port = (PNPort)_interleave.getPort("input2");
         port.getQueue().setCapacity(1);
         port.link(_queueZ);
  
-        _queueT1 = new IORelation(myUniverse, "QT1");
+        IORelation _queueT1 = new IORelation(myUniverse, "QT1");
         port = (PNPort)_alternate.getPort("output1");
         port.link(_queueT1);
         port = (PNPort)_redirect0.getPort("input");
         port.getQueue().setCapacity(1);
         port.link(_queueT1);
  
-        _queueT2 = new IORelation(myUniverse, "QT2");
+        IORelation _queueT2 = new IORelation(myUniverse, "QT2");
         port = (PNPort)_alternate.getPort("output2");
         port.link(_queueT2);
         port = (PNPort)_redirect1.getPort("input");
@@ -98,19 +98,6 @@ class PNInterExample {
         System.out.println("Bye World\n");
 	return;
     }
-
-    //////////////////////////////////////////////////////////////////////////
-    ////                         private variables                        ////
- 
-    private PNAlternate _alternate;
-    private PNInterleave _interleave;
-    private PNRedirect _redirect0;
-    private PNRedirect _redirect1;
-    private IORelation _queueX;
-    private IORelation _queueY;
-    private IORelation _queueZ;
-    private IORelation _queueT1;
-    private IORelation _queueT2;
 
 }
    
