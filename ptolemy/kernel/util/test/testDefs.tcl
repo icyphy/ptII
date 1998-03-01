8d3c4c0b1a88eb747cb0011a07d3aac3cf0201e7
cxh <cxh>
1998-03-01 22:29:39 +0000

    sccs keys

--- ptolemy/kernel/util/test/testDefs.tcl
+++ ptolemy/kernel/util/test/testDefs.tcl
@@ -2,9 +2,9 @@
 #
 # @Author: Christopher Hylands
 #
-# @Version: @(#)testDefs.tcl	1.9 01/07/98
+# @Version: $Id$
 #
-# @Copyright (c) 1997 The Regents of the University of California.
+# @Copyright (c) 1997- The Regents of the University of California.
 # All rights reserved.
 # 
 # Permission is hereby granted, without written agreement and without
