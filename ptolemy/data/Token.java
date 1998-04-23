a74533708ba20f99e5a97a74849a2ea25257766e
nsmyth <nsmyth>
1998-04-23 16:36:48 +0000

    added funtionality to allow parsing of inputs in tokens

--- ptolemy/data/Token.java
+++ ptolemy/data/Token.java
@@ -28,6 +28,7 @@
 package pt.data;
 
 import pt.kernel.*;
+import pt.data.parser.*;
 
 //////////////////////////////////////////////////////////////////////////
 //// Token
@@ -40,9 +41,10 @@ a string and to return a description of the token as a string.
 Not all derived classes are required to implement these methods,
 so the default implementation here triggers an exception.
 
-@author Edward A. Lee
+@author Edward A. Lee, Neil Smyth
 @version $Id$
 @see java.lang.Object
+@see pt.parser.PtSimpleParser
 */
 public abstract class Token implements Cloneable {
 
@@ -72,10 +74,76 @@ public abstract class Token implements Cloneable {
                 + myclass.getName() + " cannot be initialized from a string.");
     }
 
+    /** Return the Publisher object associated with this Token. 
+     */
+    public TokenPublisher getPublisher() {
+        if (_publisher == null) {
+            _publisher = new TokenPublisher(this);
+        }
+        return _publisher;
+    }
+
+    /** This method should be overridden where appropriate in subclasses
+     */
+    public boolean isArray() {
+        return false;
+    }
+
+    /** Attach a new TokenPublisher to this token. This method is 
+     *  only intended for use when placing a new Token in a Param.
+     *  This method should: be called by a param, be synchronized
+     *  and call update on the observers of the publisher
+     *  @return The previous Publisher
+     * FIXME: setting of tokenpublisher with token needs to be done
+     * in a contract manner
+     */
+     public TokenPublisher setPublisher(TokenPublisher publ) {
+         TokenPublisher old = _publisher;
+         _publisher = publ;
+         _publisher.setToken(this);
+         return old;
+     }
+
+     /* This method is used to set the value of the token from a 
+      * String. It relies on each derived class having an appropriate 
+      * definition of fromString(). 
+      * FIXME: This method is not final, but perhaps should be?
+      * FIXME: this currently only implements a simple version of
+      * parsing, more functionality will be added later
+      */
+
+      public void setValue(String value) throws IllegalArgumentException {
+          if (_parser == null) {
+              _parser = new PtSimpleParser();
+          }
+          try {
+              double result = _parser.parse(value);
+              this.fromString(String.valueOf(result));
+          } catch (Exception ex) {
+              throw new IllegalArgumentException("Cannot parse argument "+value);
+          }               
+      }
+
+              
+          
+     /** This method should be overridden where appropriate in subclasses
+     */
+    public int size() {
+        return 1;
+    }
+
     /** Return a description of the token as a string.
      *  In this base class, we return the fully qualified class name.
      */	
     public String toString() {
         return getClass().getName();
     }
+
+    ////////////////////////////////////////////////////////////////////////
+    ////                         private variables                      ////
+
+    // the associated TokenPublisher
+    private TokenPublisher _publisher;
+    // the associated Parser
+    private PtSimpleParser _parser;
 }
