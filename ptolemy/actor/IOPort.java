34e73e600b9cc20894d554e2fb8e65c2d1fc2c92
liuj <liuj>
1998-05-03 14:57:40 +0000

    change IOPort from interface to abstract class

--- ptolemy/actor/IOPort.java
+++ ptolemy/actor/IOPort.java
@@ -29,7 +29,7 @@
 */
 
 package pt.actors;
-import pt.kernel.InvalidStateException;
+import pt.kernel.*;
 
 //////////////////////////////////////////////////////////////////////////
 //// IOPort
@@ -44,10 +44,50 @@ propagate these arrays throw Entities is an important part of the data
 transfer mechanism. For more information about the data transfer, see
 the Design Document.
 
-@author Jie Liu
+@author Edward A. Lee
 @version $Id$
 */
-public interface IOPort {
+public abstract class IOPort extends ComponentPort {
+
+    /** Construct an IOPort with no container, no name, that is
+     *  neither an input nor an output. By defalt the port is not a 
+     *  multiport, and has width 1.
+     */	
+    public IOPort() {
+        super();
+    }
+
+    /** Construct an IOPort with a containing entity and a name 
+     *  that is neither an input nor an output. By defalt the port 
+     *  is not a multiport, and has width 1.
+     *  @param container
+     *  @param name
+     *  @exception NameDuplicationException Name coincides with
+     *   an element already on the port list of the parent.
+     */	
+    public IOPort(ComponentEntity container, String name) 
+             throws NameDuplicationException {
+	super(container,name);
+    }
+
+    /** Construct an IOPort with a container and a name, that is
+     *  either an input or an output or both, depending on the third
+     *  and fourth arguments. By defalt the port is not a 
+     *  multiport, and has width 1.
+     *  @param container
+     *  @param name
+     *  @param isinput
+     *  @param isoutput
+     *  @exception NameDuplicationException Name coincides with
+     *   an element already on the port list of the parent.
+     */	
+    public IOPort(ComponentEntity container, String name,
+             boolean isinput, boolean isoutput) 
+             throws NameDuplicationException {
+	this(container,name);
+        makeInput(isinput);
+        makeOutput(isoutput);
+    }
 
     //////////////////////////////////////////////////////////////////////////
     ////                         public methods                           ////
@@ -66,37 +106,78 @@ public interface IOPort {
      *  @exception If the Port/Relation connection is inconsistent, eg. 
      *             unmatched width.
      */
-    public Receptionist[][] deepReceptionists(IORelation relation)
+    public abstract Receptionist[][] deepReceptionists(IORelation relation)
            throws InvalidStateException;
 
- 
-    /** Return true if the port is an input. 
-     */	
-    public boolean isInput(); 
-
-    /** Return true if the port is an output. 
-     */	
-    public boolean isOutput() ;
-
-    /** If the argument is true, make the port an input port.
-     *  If the argument is false,
-     *  make the port not an input port.
-     */	
-    public void makeInput(boolean isinput); 
-
-    /** If the argument is true, make the port an output port.
-     *  If the argument is false,
-     *  make the port not an output port.
-     */	
-    public void makeOutput(boolean isoutput);
-    
     /** Return the width of the port. It will check the version of the 
      *  workspace. If the version is obsoleted, use updateWidth to find
      *  the new width.
      * @return The width of the port.
      */
-    public int getWidth(); 
+    public int getWidth() {
+        return _width;
+    }
 
+    /** Return true if the port is an input. 
+     */	
+    public boolean isInput() {
+        return _isinput;
+    }
+
+    /** Return true if the port is an output. 
+     */	
+    public boolean isOutput() {
+        return _isoutput;
+    }
+
+    /** Return true if the port is a multiport.
+     */
+    public boolean isMultiplex() {
+        return _isMultiplex;
+    }
+
+    /** If the argument is true, make the port an input port.
+     *  If the argument is false,
+     *  make the port not an input port.
+     */	
+    public void makeInput(boolean isinput) {
+        _isinput = isinput;
+    }
+
+    /** If the argument is true, make the port an output port.
+     *  If the argument is false,
+     *  make the port not an output port.
+     */	
+    public void makeOutput(boolean isoutput) {
+        _isoutput = isoutput;
+    }
+    
+    /** If the argument is true, make the port an multi-port.
+     *  That is, make it capable of linking with multiple IORelations, 
+     *  or with a IORelation that has width 2 or more. 
+     *  If the argument is false,
+     *  make the port not multi-port.
+     */	
+    public void makeMultiplex(boolean isMultiplex) {
+        _isMultiplex = isMultiplex;
+        if(isMultiplex) {
+            _width = -1;
+        } else {
+            _width = 1;
+        }
+    }
+
+    
+    ////////////////////////////////////////////////////////////////////////
+    ////                         private variables                      ////
+
+    // Indicate whether the port is an input, an output, or both.
+    private boolean _isinput, _isoutput;
+    // Indicate whether the port is an multiport. Default false.
+    private boolean _isMultiplex = false;
+    // The width of the port, Default 1. It should equal to the sum of the
+    // width of the linked Relations. It can not be set directly.
+    private int _width = 1;
 }
 
 
