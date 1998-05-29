801dc48228cf2f9548e2416f83b1cd0bc6c053f8
eal <eal>
1998-05-29 22:25:42 +0000

    parent from RuntimeException

--- ptolemy/kernel/util/InvalidStateException.java
+++ ptolemy/kernel/util/InvalidStateException.java
@@ -36,48 +36,141 @@ inconsistent or contradictory information in it, e.g. an entity
 contains a port that has a different entity as it container. Our
 design should make it impossible for this exception to ever occur,
 so occurrence is a bug. This exception supports all the constructor
-forms of KernelException.
+forms of KernelException, but is implemented as a RuntimeException
+so that it does not have to be declared.
 
 @author Edward A. Lee
 @version $Id$
 */
-public class InvalidStateException extends KernelException {
-    /** Constructs an Exception with no detail message */  
+public class InvalidStateException extends RuntimeException {
+
+    // NOTE: This class has much duplicated code with KernelException,
+    // but because it needs to be a RuntimeException, there seemed to
+    // be no way to avoid this.  Should there be an interface defined
+    // for the commonality?
+
+    /** Constructs an Exception with no names or message. */  
     public InvalidStateException() {
-        super("Serious internal error!");
+        this(null, null, "Serious internal error!");
     }
 
-    /** Constructs an Exception with a detail message */  
+    /** Constructs an Exception with only a detail message. */  
     public InvalidStateException(String detail) {
-        super(detail);
+        this(null, null, detail);
     }
 
-    /** Constructs an Exception with a detail message that is only the
+    /** Constructs an Exception with a message that is only the
      * name of the argument.
      */  
     public InvalidStateException(Nameable obj) {
-        super(obj, "Serious internal error!");
+        this(obj, null, "Serious internal error!");
     }
 
     /** Constructs an Exception with a detail message that includes the
-     * name of the argument.
+     * name of the first argument and the second argument string.
      */  
     public InvalidStateException(Nameable obj, String detail) {
-        super(obj, detail);
+        this(obj, null, detail);
     }
 
     /** Constructs an Exception with a detail message that consists of
-     * only the names of the obj1 and obj2 arguments.
+     * only the names of the two arguments.
      */  
     public InvalidStateException(Nameable obj1, Nameable obj2)  {
-        super(obj1, obj2, "Serious internal error!");
+        this(obj1, obj2, "Serious internal error!");
     }
 
     /** Constructs an Exception with a detail message that includes the
-     * names of the obj1 and obj2 arguments.
+     * names of the first two arguments plus the third argument string.
      */  
     public InvalidStateException(Nameable obj1, Nameable obj2,
             String detail) {
-        super(obj1, obj2, detail);
+        String obj1string = getFullName(obj1);
+        String obj2string = getFullName(obj2);
+        String prefix;
+        if (obj1string != "") {
+            if (obj2string != "") {
+                prefix = new String(obj1string + " and " + obj2string);
+            } else {
+                prefix = obj1string;
+            }
+        } else {
+            prefix = obj2string;
+        }
+        setMessage(prefix);
+        if (detail != null) {
+            if (!detail.equals("")) {
+                if (!prefix.equals("")) {
+                    setMessage(new String(prefix + ": " + detail));
+                } else {
+                    setMessage(detail);
+                }
+            }
+        }
     }
+    
+    //////////////////////////////////////////////////////////////////////////
+    ////                         public methods                           ////
+
+    /** Get a localized version of the detail message.  Note that
+     *  in this implementation, we merely return the same result as
+     *  getMessage().
+     */  
+    public String getLocalizedMessage() {
+        return getMessage();
+    }
+
+    /** Get the detail message. */
+    public String getMessage() {
+        return _message;
+    }
+
+    //////////////////////////////////////////////////////////////////////////
+    ////                         protected methods                        ////
+
+    /** Get the name of a Nameable object.
+     *  If the argument is a null reference, return an empty string.
+     */
+    protected String getName(Nameable obj) {
+        String name;
+        if (obj == null) {
+            return "";
+        } else {
+            name = obj.getName();
+            if (name.equals("")) {
+                name = new String("<Unnamed Object>");
+            }
+        }
+        return name;
+    }
+
+    /** Get the name of a Nameable object.  This method attempts to use
+     *  getFullName(), if it is defined, and resorts to getName() if it is
+     *  not.  If the argument is a null reference, return an empty string.
+     */
+    protected String getFullName(Nameable obj) {
+        String name;
+        if (obj == null) {
+            return "";
+        } else {
+            try {
+                name = obj.getFullName();
+            } catch (InvalidStateException ex) {
+                name = obj.getName();
+            }
+        }
+        return name;
+    }
+
+    /** Sets the error message to the specified string.
+     */  
+    protected void setMessage(String msg) {
+        _message = msg;
+    }
+    
+    //////////////////////////////////////////////////////////////////////////
+    ////                         private variables                        ////
+
+    // The detail message.
+    private String _message ;
 }
