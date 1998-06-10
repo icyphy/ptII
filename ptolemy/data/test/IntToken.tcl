6704bb47653b96f8cbf1cb8d67e47d2eda96e9b9
nsmyth <nsmyth>
1998-06-10 15:43:48 +0000

    *** empty log message ***

--- ptolemy/data/test/IntToken.tcl
+++ ptolemy/data/test/IntToken.tcl
@@ -2,9 +2,9 @@
 #
 # @Author: Mudit Goel, Neil Smyth
 #
-# @Version $Id$
+# @Version @(#)IntToken.tcl	1.6    06/02/98
 #
-# @Copyright (c) 1997- The Regents of the University of California.
+# @Copyright (c) 1997-1998 The Regents of the University of California.
 # All rights reserved.
 # 
 # Permission is hereby granted, without written agreement and without
