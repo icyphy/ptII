2e8b1f8ee5011bf4030d4fd99569bef85d493102
yuhong <yuhong>
1998-06-14 21:13:10 +0000

    support finding both least and greatest solutions

--- ptolemy/graph/InequalitySolver.java
+++ ptolemy/graph/InequalitySolver.java
@@ -39,27 +39,28 @@ An algorithm to solve a set of inequalities.
 This algorithm is based on
 <a href=http://www.diku.dk/topps/personal/rehof/publications.html>
 <i>Tractable Constraints in Finite Semilattices</i></a> by Jakob Rehof
-and Torben Mogensen.
+and Torben Mogensen.<p>
 
 The algorithm in Rehof works for definite inequalities, which are
 inequalities that the greater term is a constant or a variable.  This
-solver doesn't enforce this requirement.  However, if the greater term
-of some inequalities is a function, this solver may not be able to find
-the solution even when the set of inequalities is satisfiable.  See
-the paper for details.
+solver doesn't enforce this requirement.  However, if the inequalities
+are not definite, this solver may not be able to find the solution even
+when the set of inequalities is satisfiable.  See the paper for
+details.<p>
 
-This solver assumes that the CPO passed to the constructor is a lattice,
-but it does not verify it.  If the least upper bound (LUB) of some
-elements doesn't exist during the execution of the algorithm, an exception
-is thrown.
+This solver supports finding both the least and greatest solutions (if
+they exist).  It assumes that the CPO passed to the constructor is a
+lattice, but it does not verify it.  If the LUB or GLB of some
+elements doesn't exist during the execution of the algorithm, an
+exception is thrown.
 
 @author Yuhong Xiong
 $Id$
 */
 
-// To make it easier to reference the above paper, this class uses the
-// same names for methods and variables as in the paper, which may
-// voilate the naming convention in some cases.
+// Note: To make it easier to reference the above paper, this class
+// uses the same names for methods and variables as in the paper, which
+// may voilate the naming convention in some cases.
 
 public class InequalitySolver {
 
@@ -68,7 +69,6 @@ public class InequalitySolver {
      */
     public InequalitySolver(CPO cpo) {
         _cpo = cpo;
-        _bottom = _cpo.bottom();
     }
     
     ////////////////////////////////////////////////////////////////////////
@@ -76,68 +76,95 @@ public class InequalitySolver {
 
     /** Adds an inequality to the set of constraints.
      *  @param ineq an <code>Inequality</code>.
-     *  @param varList an array of <code>InequalityTerm</code>s corresponding
+     *  @param variables an array of inequality terms corresponding
      *   to variables in the specified inequality.
      */
-    public void addInequality(Inequality ineq, InequalityTerm[] varList) {
-        // init all variables to bottom
-        for (int i = 0; i < varList.length; i++) {
-	    try {
-                varList[i].set(_bottom);
-	    } catch (IllegalActionException e) {
-		System.out.println("IllegalActionException: can't " +
-			"initialize variable.");
+    public void addInequality(Inequality ineq, InequalityTerm[] variables) {
+
+	// put ineq. to _Ilist
+	Integer indexWrap = new Integer(_Ilist.size());
+	Info info = new Info(ineq);
+	_Ilist.addElement(info);
+
+        // add var->ineq to Hashtable
+        for (int i = 0; i < variables.length; i++) {
+            Vector entry = (Vector)(_Clist.get(variables[i]));
+            if (entry == null) {	// variable not in Hashtable
+                entry = new Vector();
+                _Clist.put(variables[i], entry);
 	    }
-        }
-        
-        // put ineq to _Cvar or _Ccnst
-        if (ineq.greaterTerm().settable()) {
-            Integer indexObj = new Integer(_Cvar.size());
-            CvarRecord rec = new CvarRecord(ineq, !ineq.satisfied());
-            _Cvar.addElement(rec);
-
-            // update _NS
-            if (rec._inserted) {
-
-		// FIXME: restore this line for jdk1.2
-//                _NS.addLast(indexObj);
-
-		// FIXME: delete this line for jdk1.2
-                _NS.insertLast(indexObj);
-
-            }
-            
-            // add var->ineq to Hashtable
-            for (int i = 0; i < varList.length; i++) {
-                Object clisti = _Clist.get(varList[i]);
-                if (clisti == null) {
-                    Vector ineqVec = new Vector();
-                    ineqVec.addElement(indexObj);
-                    _Clist.put(varList[i], ineqVec);
-                } else {
-                    ((Vector)clisti).addElement(indexObj);
-                }
-            }
-        } else {                // ineq is not in Cvar
-            _Ccnst.addElement(ineq);
+            entry.addElement(indexWrap);
         }
     }
-    
+
     /** Solves the set of inequalities and updates the variables.
      *  If the set of inequalities is satisfiable, this method returns
-     *  <code>true</code>; if the set of inequalities is definite,
-     *  ie., the greater term of each inequality is a constant or a
-     *  variable, and the set of inequalities is not satisfiable, this
-     *  method returns <code>false</code>; if the set of inequalities
-     *  is not definite, a <code>false</code> return value doesn't
-     *  guarantee the set of inequalities is not satisfiable.
+     *  <code>true</code> and the variables are set to the least or
+     *  greatest solution depending on the <code>least</code> parameter;
+     *  if the set of inequalities is definite (see class document), and
+     *  the set of inequalities is not satisfiable, this method returns
+     *  <code>false</code>; if the set of inequalities is not definite,
+     *  a <code>false</code> return value doesn't guarantee the set of
+     *  inequalities is not satisfiable.  In the case this method
+     *  returns <code>false</code>, the variable values are random.
+     *  @param least if <code>true</code>, this method will try to
+     *   find the least solution; otherwise, this method will try to find
+     *   the greatest solution.
      *  @return <code>true</code> if the set of inequalities is
      *   satisfiable; <code>false</code> if not satisfiable, or
      *   the set of inequalities is not definite.
      *  @exception InvalidStateException the CPO over which the
      *   inequalities are defined is not a lattice.
      */
-    public boolean solve() {
+    public boolean solve(boolean least) {
+ 
+        // initialize all variables
+	Object init = least ? _cpo.bottom() : _cpo.top();
+	if (init == null) {
+	    throw new InvalidStateException("The underlining CPO is not" +
+				" a lattice.");
+	}
+
+	for (Enumeration e = _Clist.keys(); e.hasMoreElements() ;) {
+	    InequalityTerm variable = (InequalityTerm)e.nextElement();
+	    try {
+	        variable.set(init);
+	    } catch (IllegalActionException ex) {
+		System.out.println("IllegalActionException: should be " +
+			"able to initialize variable.");
+	    }
+	}
+
+	// initialize _NS(not satisfied) list; set _inCvar and _inserted flags. 
+
+        // Not Satisfied list.  Each entry is an Integer storing index to
+	// _Ilist.
+    	// Note: removal in jdk1.2 LinkedList is not an O(1) operation, but
+    	// an O(n) operation, where n is the number of elements in list.
+    	// If the size of _NS is large, writing our own linked list class
+    	// with a Cell class might be better.
+    	LinkedList _NS = new LinkedList();
+
+	for (int i = 0; i < _Ilist.size(); i++) {
+	    Info info = (Info)_Ilist.elementAt(i);
+	    info._inCvar = least ? info._ineq.greaterTerm().settable()
+				 : info._ineq.lesserTerm().settable();
+	    if (info._inCvar) {
+	    	if (info._ineq.satisfied()) {
+		    info._inserted = false;
+		} else { 	// insert to _NS
+		    // FIXME: restore this line for jdk1.2
+//                  _NS.addLast(new Integer(i));
+
+		    // FIXME: delete this line for jdk1.2
+                    _NS.insertLast(new Integer(i));
+
+		    info._inserted = true;
+		}
+	    }
+	}
+
+	// solve the inequalities
         while (_NS.size() > 0) {
 
 	    // FIXME: restore this line for jdk1.2
@@ -149,46 +176,57 @@ public class InequalitySolver {
 	    // end last FIXME
 
 
-            CvarRecord rec = (CvarRecord)(_Cvar.elementAt(index));
-            rec._inserted = false;
-            Object value = _cpo.lub(rec._ineq.lesserTerm().value(),
-                                    rec._ineq.greaterTerm().value());
+            Info info = (Info)(_Ilist.elementAt(index));
+            info._inserted = false;
+            Object value = null;
+	    InequalityTerm updateTerm = null;
+	    if (least) {
+		updateTerm = info._ineq.greaterTerm();
+	        value = _cpo.lub(info._ineq.lesserTerm().value(),
+                                 updateTerm.value());
+	    } else {
+		updateTerm = info._ineq.lesserTerm();
+	        value = _cpo.glb(updateTerm.value(),
+                                 info._ineq.greaterTerm().value());
+	    }
+
             if (value == null) {
                 throw new InvalidStateException("The CPO over which " +
                           "the inequalities are defined is not a lattice.");
             }
+
 	    try {
-                rec._ineq.greaterTerm().set(value);
+		updateTerm.set(value);
 	    } catch (IllegalActionException e) {
 		System.out.println("IllegalActionException: Can't " +
 			"set value to variable.");
 	    }
             
-            // insert or drop the inequalities in Clist[rec._ineq.greaterTerm()]
-            Vector affected = (Vector)(_Clist.get(rec._ineq.greaterTerm()));
+            // insert or drop the inequalities affected
+            Vector affected = (Vector)_Clist.get(updateTerm);
             for (int i = 0; i < affected.size(); i++) {
-                Integer index1Obj = (Integer)(affected.elementAt(i));
-                int index1 = index1Obj.intValue();
-                if (index1 != index) {
-                    rec = (CvarRecord)(_Cvar.elementAt(index1));
-                    if (rec._ineq.satisfied()) {    // drop
-                        if (rec._inserted) {
+                Integer index1Wrap = (Integer)(affected.elementAt(i));
+                int index1 = index1Wrap.intValue();
+		Info affectedInfo = (Info)_Ilist.elementAt(index1);
+                if (index1 != index && affectedInfo._inCvar) {
+                    if (info._ineq.satisfied()) {    // drop
+                        if (info._inserted) {
 
 			    // FIXME: restore this line for jdk1.2
-//                            _NS.remove(index1Obj);
+//                            _NS.remove(index1Wrap);
 
 			    // FIXME: delete this line for jdk1.2
-                            _NS.removeOneOf(index1Obj);
+                            _NS.removeOneOf(index1Wrap);
 
                         }
                     } else {                        // insert
-                        if ( !rec._inserted) {
+                        if ( !info._inserted) {
 
 			    // FIXME: restore this line for jdk1.2
-//                            _NS.addFirst(index1Obj);
+//                            _NS.addFirst(index1Wrap);
 
 			    // FIXME: delete this line for jdk1.2
-                            _NS.insertFirst(index1Obj);
+                            _NS.insertFirst(index1Wrap);
 
                         }
                     }
@@ -197,10 +235,12 @@ public class InequalitySolver {
         }
         
         // check if the inequalities in Ccnst are satisfied
-        for (int i = 0; i < _Ccnst.size(); i++) {
-            Inequality ineq = (Inequality)(_Ccnst.elementAt(i));
-            if ( !ineq.satisfied()) {
-                return false;
+        for (int i = 0; i < _Ilist.size(); i++) {
+	    Info info = (Info)_Ilist.elementAt(i);
+	    if ( !info._inCvar) {
+                if ( !info._ineq.satisfied()) {
+                    return false;
+		}
             }
         }
         return true;
@@ -209,48 +249,35 @@ public class InequalitySolver {
     ////////////////////////////////////////////////////////////////////////
     ////                          inner class                           ////
 
-    // This class contains a reference to an inequality and the inserted
-    // flag.  Each instance is an entry in _Cvar.
-    private class CvarRecord {
-        private CvarRecord(Inequality ineq, boolean inserted) {
+    // Each instance of this class is an entry in _Ilist.
+    private class Info {
+        private Info(Inequality ineq) {
             _ineq = ineq;
-            _inserted = inserted;
         }
-        
+ 
         private Inequality _ineq;
-        private boolean _inserted;
+
+	// If this ineq. is in Cvar, i.e., if looking for least solution
+	// and greaterTerm is settable, or looking for greatest solution
+	// and lesserTerm is settable.
+	private boolean _inCvar = false;
+
+	// If this ineq. is in _NS
+        private boolean _inserted = false;
     }
     
     ////////////////////////////////////////////////////////////////////////
     ////                       private variables                        ////
 
     private CPO _cpo = null;
-    private Object _bottom = null;    // local cache
+ 
+    // Vector representation of Ilist. Each entry is an instance of the
+    // inner class Info. This vector effectively gives each inequality an
+    // index, _Clist and _NS use that index.
+    private Vector _Ilist = new Vector();
     
-    // Vector representation of Cvar.  Each entry is an instance of the
-    // inner class CvarRecord which contains an inequality and the inserted
-    // flag.
-    // These vectors effectively gives each inequality in Cvar an index,
-    // and _ClistCvar and NS (below) use that index.
-    private Vector _Cvar = new Vector();
-    
-    // inequalities in Ccnst.  This vector and _CvarIneq holds all the
-    // inequalities in the constraint set, which makes up Ilist in Rehof.
-    private Vector _Ccnst = new Vector();
-    
-    // In Rehof, each entry of Clist holds pointers to inequalities in
-    // Ilist, but only pointers to inequalities in Cvar is used during
-    // the iteration, so the Clist here only contains references to
-    // inequalities in Cvar.
     // Each entry in _Clist is a vector of Integers containing the
-    // index of inequalities in _Cvar.
+    // index of inequalities in _Ilist.
     private Hashtable _Clist = new Hashtable();
-    
-    // Not Satisfied list.  Each entry is an Integer.
-    // Note remove in jdk1.2 LinkedList is not an O(1) operation, but
-    // an O(n) operation, where n is the number of elements in list.
-    // If the size of _NS is large, writing our own linked list class
-    // with a Cell class can be considered.
-    private LinkedList _NS = new LinkedList();
 }
 
