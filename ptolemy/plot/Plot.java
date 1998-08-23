ad479332610644996d341d6efe06279de8b906ee
cxh <cxh>
1998-08-23 11:16:12 +0000

    pt. -> ptolemy.

--- ptolemy/plot/Plot.java
+++ ptolemy/plot/Plot.java
@@ -25,7 +25,7 @@ ENHANCEMENTS, OR MODIFICATIONS.
                                                 PT_COPYRIGHT_VERSION_2
                                                 COPYRIGHTENDKEY
 */
-package pt.plot;
+package ptolemy.plot;
 
 // TO DO:
 //   - support for oscilloscope-like plots (where x axis wraps around).
