47b7acb17e578f01de79b2d47ed0d90448592683
nsmyth <nsmyth>
1998-08-28 09:20:52 +0000

    Initial revision

---
+++ ptolemy/domains/csp/demo/BasicDemo.java
@@ -0,0 +1,90 @@
+/* A  two actor simulation executing a simple get and put rendezvous.
+
+ Copyright (c) 1998 The Regents of the University of California.
+ All rights reserved.
+ Permission is hereby granted, without written agreement and without
+ license or royalty fees, to use, copy, modify, and distribute this
+ software and its documentation for any purpose, provided that the above
+ copyright notice and the following two paragraphs appear in all copies
+ of this software.
+
+ IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+ FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+ ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+ THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+ SUCH DAMAGE.
+
+ THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+ INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+ PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+ CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ ENHANCEMENTS, OR MODIFICATIONS.
+ 
+                                        PT_COPYRIGHT_VERSION_2
+                                        COPYRIGHTENDKEY
+*/
+
+
+import ptolemy.kernel.util.*;
+import ptolemy.actor.*;
+import ptolemy.domains.csp.kernel.*;
+import ptolemy.domains.csp.lib.*;
+
+
+//////////////////////////////////////////////////////////////////////////
+//// BasicDemo
+/** 
+Source - Sink
+@author Neil Smyth
+@version $Id$
+@see classname
+@see full-classname
+*/
+public class BasicDemo {
+    /** Constructor
+     * @see full-classname#method-name()
+     * @param parameter-name description
+     * @param parameter-name description
+     * @return description
+     * @exception full-classname description
+     */	
+    public BasicDemo() {}
+
+    ///////////////////////////////////////////////////////////////////
+    ////                         public methods                    ////
+
+    /** Description
+     * @see full-classname#method-name()
+     * @param parameter-name description
+     * @param parameter-name description
+     * @return description
+     * @exception full-classname description
+     */	
+    public static void main(String[] args) {
+        try {
+            CSPCompositeActor univ = new CSPCompositeActor();
+            univ.setName( "Universe");
+            CSPDirector execdir = new CSPDirector();
+            CSPDirector localdir = new CSPDirector();
+            univ.setExecutiveDirector(execdir);
+            univ.setDirector(localdir);
+
+	    CSPReceiver rec = new CSPReceiver();
+
+            CSPSource source = new CSPSource(univ, "Source", rec);
+            CSPSink sink = new CSPSink(univ, "Sink", rec);
+            
+            IOPort out = source.output;
+            IOPort in = sink.input;
+
+            //IORelation rel = (IORelation)univ.connect(out, in, "R1");
+            System.out.println(univ.description(1023));
+            System.out.println(univ.getFullName() + " starting!");
+            univ.getExecutiveDirector().run();
+        } catch (Exception e) {
+            System.out.println(e.getMessage() + ": " + e.getClass().getName());
+            throw new InvalidStateException(e.getMessage());
+        }
+    }
+}
