54d9f40bf27eeb20c4cd0f4e6a4125bdbf9b0d50
ptuser <ptuser>
1998-08-31 01:11:15 +0000

    Initial revision

---
+++ ptolemy/filter/view/PlotPoint.java
@@ -0,0 +1,58 @@
+/* A point in a plot.
+
+@Author: Edward A. Lee
+@Version: @(#)PlotPoint.java	1.10    02/08/98
+
+@Copyright (c) 1997-1998 The Regents of the University of California.
+All rights reserved.
+
+Permission is hereby granted, without written agreement and without
+license or royalty fees, to use, copy, modify, and distribute this
+software and its documentation for any purpose, provided that the
+above copyright notice and the following two paragraphs appear in all
+copies of this software.
+
+IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+SUCH DAMAGE.
+
+THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ENHANCEMENTS, OR MODIFICATIONS.
+
+                                                PT_COPYRIGHT_VERSION_2
+                                                COPYRIGHTENDKEY
+*/
+package ptolemy.filter.view;
+
+//////////////////////////////////////////////////////////////////////////
+//// PlotPoint
+/** 
+ * A simple structure for storing a plot point.
+ * @author Edward A. Lee
+ * @version @(#)PlotPoint.java	1.10    02/08/98
+ */
+public class PlotPoint {
+
+    /////////////////////////////////////////////////////////////////////////
+    ////                         public variables                        ////
+   
+    public double x,y;
+
+    /** Error bar Y low value. */
+    public double yLowEB;
+
+    /** Error bar Y low value. */
+    public double yHighEB;
+
+    /** True if this point is connected to the previous point by a line. */
+    public boolean connected = false;
+
+    /** True if the yLowEB and yHighEB fields are valid. */
+    public boolean errorBar = false;
+}
