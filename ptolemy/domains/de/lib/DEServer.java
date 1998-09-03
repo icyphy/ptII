84adede97a64776a8ef6a25cc9ca3f556153562c
cxh <cxh>
1998-09-03 10:12:44 +0000

    Removed trailing spaces

--- ptolemy/domains/de/lib/DEServer.java
+++ ptolemy/domains/de/lib/DEServer.java
@@ -1,139 +1,83 @@
-/* A DE star that emulates a server
+#Makefile for ptolemy.filter.view package 
+#
+# @Authors: Christopher Hylands
+#
+# @Version: @(#)makefile        1.52 03/02/98
+#
+# @Copyright (c) 1997-1998 The Regents of the University of California.
+# All rights reserved.
+# 
+# Permission is hereby granted, without written agreement and without
+# license or royalty fees, to use, copy, modify, and distribute this
+# software and its documentation for any purpose, provided that the
+# above copyright notice and the following two paragraphs appear in all
+# copies of this software.
+# 
+# IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+# FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+# ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+# THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+# 
+# THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+# INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+# PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+# CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+# ENHANCEMENTS, OR MODIFICATIONS.
+# 
+#                                               PT_COPYRIGHT_VERSION_2
+#                                               COPYRIGHTENDKEY
+# $Id$ %S%
 
- Copyright (c) 1997- The Regents of the University of California.
- All rights reserved.
- Permission is hereby granted, without written agreement and without
- license or royalty fees, to use, copy, modify, and distribute this
- software and its documentation for any purpose, provided that the above
- copyright notice and the following two paragraphs appear in all copies
- of this software.
+# Root of the Java directory
+ROOT =          ../../..
 
- IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
- FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
- ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
- THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
- SUCH DAMAGE.
 
- THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
- INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
- MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
- PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
- CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
- ENHANCEMENTS, OR MODIFICATIONS.
+# Get configuration info
+CONFIG =        $(ROOT)/mk/tycho.mk
+include $(CONFIG)
  
-                                        PT_COPYRIGHT_VERSION_2
-                                        COPYRIGHTENDKEY
-*/
+JPACKAGE =      ptfilter.filtgui
+JVERSION =      1.0
+JDIST =         $(JPACKAGE)$(JVERSION)
+JZIP =          filtgui/$(JPACKAGE).zip
+ 
+DIRS =  demo #test
+ 
+ CLASSPATH =     ../../..
+# CLASSPATH =     ../
+# Order matters here.
+ 
+# Sources that compile under JDK1.0.2 and later
+JSRCS = \
+       InteractComponent.java \
+       PlotPoint.java \
+       PlotBox.java \
+       Plot.java \
+       InteractPlot.java \
+       PoleZeroPlot.java \
+       View.java \
+       DataView.java \
+       IIRFiltSetView.java \
+       PoleZeroView.java \
+       FreqView.java \
+       ImpulsView.java \
+       DataModel.java \
+       HistoModel.java 
+ 
+JCLASS = $(JSRCS:%.java=%.class)
+ 
+all: sources jclass $(JZIP)
+	chmod a+x pxgraph testpxgraph
+install: sources jclass javadocs $(JZIP) 
+	chmod a+x pxgraph testpxgraph
+docs: javadocs
+ 
+depend:
+	@echo "no dependencies in this directory"
 
-package ptolemy.domains.de.lib;
-
-import ptolemy.actor.*;
-import ptolemy.domains.de.kernel.*;
-import ptolemy.kernel.*;
-import ptolemy.kernel.util.*;
-import ptolemy.data.*;
-import java.util.Enumeration;
-
-//////////////////////////////////////////////////////////////////////////
-//// DEServer
-/** 
-Emulate a server. If input events arrive when it is not busy, it delays
-them by the service time (a constant parameter). If they arrive when it is
-not busy, it delays them the service time plus however long it takes to
-become free from the previous tasks.
-
-@author Lukito Muliadi
-@version $Id$
-@see Actor
-*/
-public class DEServer extends AtomicActor {
-    /** Construct a DEServer star.
-     *  
-     * @param serviceTime The service time
-     * @param container The composite actor that this actor belongs too.
-     * @param name The name of this actor.
-     *
-     * @exception NameDuplicationException Other star already had this name
-     * @exception IllegalActionException internal problem
-     */	
-    public DEServer(double serviceTime, 
-            CompositeActor container, 
-            String name) 
-            throws NameDuplicationException, IllegalActionException  {
-        super(container, name);
-        // create an output port
-        output = new DEIOPort(this, "output", false, true);
-        // create an input port
-        input = new DEIOPort(this, "input", true, false);
-        // set the service time.
-        _serviceTime = serviceTime;
-    }
-
-
-    ///////////////////////////////////////////////////////////////////
-    ////                         public methods                    ////
-
-
-    /** Produce the output event according to whether the server is busy or
-     *  not.
-     * 
-     * @exception CloneNotSupportedException Error when cloning event.
-     * @exception IllegalActionException Not thrown in this class.
-     */	
-    public void fire() throws CloneNotSupportedException, IllegalActionException{
-        System.out.println("Firing DERamp");
-        // get the input token from the input port.
-        DoubleToken inputToken;
-        try {
-            inputToken = (DoubleToken)(input.get(0));
-        } catch (NoSuchItemException e) {
-            // this can't happen
-            throw new InvalidStateException("Bug in DEServer.fire()");
-        }
-        
-        // produce the output token.
-        double inputTime = ((DECQDirector)getDirector()).currentTime();
-	double outputTime;
-	if (_firstInput) {
-	    _firstInput = false;
-	    // always not busy at the first input
-	    outputTime = inputTime + _serviceTime;
-	    _doneTime = outputTime;
-	} else {
-	    if (_doneTime < inputTime) {
-		// not busy
-		outputTime = inputTime + _serviceTime;
-		_doneTime = outputTime;
-	    } else {
-		// busy
-		outputTime = _doneTime + _serviceTime;
-		_doneTime = outputTime;
-	    }
-	}
-
-	
-        // send the output token via output DEIOPort.
-        output.broadcast(inputToken, outputTime);
-    }
-
-    ///////////////////////////////////////////////////////////////////
-    ////                         private variables                 ////
-
-    // Private variables should not have doc comments, they should
-    // have regular C++ comments.
-
-    // the intial value and increment
-    private double _serviceTime;
-    private boolean _firstInput = true;
-    private double _doneTime = 0.0;
-    
-    // the ports.
-    public DEIOPort output;
-    public DEIOPort input;
-}
-
-
-
-
+include $(ROOT)/mk/tycommon.mk
+ 
 
 
