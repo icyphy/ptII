951ac143dffec4d60ed8becdb39d8ac5993ed9dd
cxh <cxh>
1998-09-27 23:47:34 +0000

    whitespace

--- ptolemy/graph/DirectedGraph.java
+++ ptolemy/graph/DirectedGraph.java
@@ -201,9 +201,9 @@ public class DirectedGraph extends Graph {
         }
 
         // Warshall's algorithm
-        for (int k=0; k<size; k++) {
-            for (int i=0; i<size; i++) {
-                for (int j=0; j<size; j++) {
+        for (int k = 0; k < size; k++) {
+            for (int i = 0; i < size; i++) {
+                for (int j = 0; j < size; j++) {
                     _transitiveClosure[i][j] |= _transitiveClosure[i][k] &
                         _transitiveClosure[k][j];
                 }
