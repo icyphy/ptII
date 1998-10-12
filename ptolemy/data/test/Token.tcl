7c79c510ab2dd75f9322710f0df014bc795d45a1
yuhong <yuhong>
1998-10-12 13:05:23 +0000

    Initial revision

---
+++ ptolemy/data/test/Token.tcl
@@ -0,0 +1,203 @@
+# Tests for the Token class
+#
+# @Author: Yuhong Xiong
+#
+# @Version $Id$
+#
+# @Copyright (c) 1997- The Regents of the University of California.
+# All rights reserved.
+# 
+# Permission is hereby granted, without written agreement and without
+# license or royalty fees, to use, copy, modify, and distribute this
+# software and its documentation for any purpose, provided that the
+# above copyright notice and the following two paragraphs appear in all
+# copies of this software.
+# 
+# IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+# FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+# ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+# THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+# 
+# THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+# INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+# PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+# CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+# ENHANCEMENTS, OR MODIFICATIONS.
+# 
+# 						PT_COPYRIGHT_VERSION_2
+# 						COPYRIGHTENDKEY
+#######################################################################
+
+# Tycho test bed, see $TYCHO/doc/coding/testing.html for more information.
+
+# Load up the test definitions.
+if {[string compare test [info procs test]] == 1} then { 
+    source testDefs.tcl
+} {}
+
+# Uncomment this to get a full report, or set in your Tcl shell window.
+# set VERBOSE 1
+
+# If a file contains non-graphical tests, then it should be named .tcl
+# If a file contains graphical tests, then it should be called .itcl
+# 
+# It would be nice if the tests would work in a vanilla itkwish binary.
+# Check for necessary classes and adjust the auto_path accordingly.
+#
+
+######################################################################
+####
+# 
+test Token-1.0 {Create an instance} {
+    set p [java::new ptolemy.data.Token]
+    $p toString
+} {ptolemy.data.Token}
+
+######################################################################
+####
+# 
+test Token-2.0 {Test add} {
+    set p1 [java::new ptolemy.data.Token]
+    set p2 [java::new ptolemy.data.Token]
+    catch {$p1 add $p2} msg
+    list $msg
+} {{ptolemy.kernel.util.IllegalActionException: add method not supported on ptolemy.data.Token objects.}}
+
+######################################################################
+####
+# 
+test Token-2.1 {Test addR} {
+    set p1 [java::new ptolemy.data.Token]
+    set p2 [java::new ptolemy.data.Token]
+    catch {$p1 addR $p2} msg
+    list $msg
+} {{ptolemy.kernel.util.IllegalActionException: addR method not supported on ptolemy.data.Token objects.}}
+
+######################################################################
+####
+# 
+test Token-2.2 {Test divide} {
+    set p1 [java::new ptolemy.data.Token]
+    set p2 [java::new ptolemy.data.Token]
+    catch {$p1 divide $p2} msg
+    list $msg
+} {{ptolemy.kernel.util.IllegalActionException: divide method not supported on ptolemy.data.Token objects.}}
+
+######################################################################
+####
+# 
+test Token-2.3 {Test divideR} {
+    set p1 [java::new ptolemy.data.Token]
+    set p2 [java::new ptolemy.data.Token]
+    catch {$p1 divideR $p2} msg
+    list $msg
+} {{ptolemy.kernel.util.IllegalActionException: divideR method not supported on ptolemy.data.Token objects.}}
+
+######################################################################
+####
+# 
+test Token-2.4 {Test equals} {
+    set p1 [java::new ptolemy.data.Token]
+    set p2 [java::new ptolemy.data.Token]
+    catch {$p1 {equals ptolemy.data.Token} $p2} msg
+    list $msg
+} {{ptolemy.kernel.util.IllegalActionException: equals method not supported on ptolemy.data.Token objects.}}
+
+######################################################################
+####
+# 
+test Token-2.5 {Test modulo} {
+    set p1 [java::new ptolemy.data.Token]
+    set p2 [java::new ptolemy.data.Token]
+    catch {$p1 modulo $p2} msg
+    list $msg
+} {{ptolemy.kernel.util.IllegalActionException: modulo method not supported on ptolemy.data.Token objects.}}
+
+######################################################################
+####
+# 
+test Token-2.6 {Test moduloR} {
+    set p1 [java::new ptolemy.data.Token]
+    set p2 [java::new ptolemy.data.Token]
+    catch {$p1 moduloR $p2} msg
+    list $msg
+} {{ptolemy.kernel.util.IllegalActionException: moduloR method not supported on ptolemy.data.Token objects.}}
+
+######################################################################
+####
+# 
+test Token-2.7 {Test multiply} {
+    set p1 [java::new ptolemy.data.Token]
+    set p2 [java::new ptolemy.data.Token]
+    catch {$p1 multiply $p2} msg
+    list $msg
+} {{ptolemy.kernel.util.IllegalActionException: multiply method not supported on ptolemy.data.Token objects.}}
+
+######################################################################
+####
+# 
+test Token-2.8 {Test multiplyR} {
+    set p1 [java::new ptolemy.data.Token]
+    set p2 [java::new ptolemy.data.Token]
+    catch {$p1 multiplyR $p2} msg
+    list $msg
+} {{ptolemy.kernel.util.IllegalActionException: multiplyR method not supported on ptolemy.data.Token objects.}}
+
+######################################################################
+####
+# 
+test Token-2.9 {Test subtract} {
+    set p1 [java::new ptolemy.data.Token]
+    set p2 [java::new ptolemy.data.Token]
+    catch {$p1 subtract $p2} msg
+    list $msg
+} {{ptolemy.kernel.util.IllegalActionException: subtract method not supported on ptolemy.data.Token objects.}}
+
+######################################################################
+####
+# 
+test Token-2.10 {Test subtractR} {
+    set p1 [java::new ptolemy.data.Token]
+    set p2 [java::new ptolemy.data.Token]
+    catch {$p1 subtractR $p2} msg
+    list $msg
+} {{ptolemy.kernel.util.IllegalActionException: subtractR method not supported on ptolemy.data.Token objects.}}
+
+######################################################################
+####
+# 
+test Token-3.0 {Test one} {
+    set p [java::new ptolemy.data.Token]
+    catch {$p1 one} msg
+    list $msg
+} {{ptolemy.kernel.util.IllegalActionException: Multiplicative identity not supported on ptolemy.data.Token objects.}}
+
+######################################################################
+####
+# 
+test Token-3.1 {Test zero} {
+    set p [java::new ptolemy.data.Token]
+    catch {$p1 zero} msg
+    list $msg
+} {{ptolemy.kernel.util.IllegalActionException: Additive identity not supported on ptolemy.data.Token objects.}}
+
+######################################################################
+####
+# 
+test Token-4.0 {Test stringValue} {
+    set p [java::new ptolemy.data.Token]
+    list [$p stringValue]
+} {present}
+
+######################################################################
+####
+# 
+test Token-5.0 {Test convert} {
+    set p1 [java::new ptolemy.data.Token]
+    set p2 [java::new ptolemy.data.Token]
+    set p3 [$p1 convert $p2]
+    $p3 toString
+} {ptolemy.data.Token}
+
