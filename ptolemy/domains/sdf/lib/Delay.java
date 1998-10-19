f3fbbbe5cdb788cd8faefe1b2d447491d5cb8362
neuendor <neuendor>
1998-10-19 14:40:24 +0000

    Initial revision

---
+++ ptolemy/domains/sdf/lib/Delay.java
@@ -0,0 +1,59 @@
+package ptolemy.domains.sdf.lib;
+
+import ptolemy.kernel.*;
+import ptolemy.kernel.util.*;
+import ptolemy.data.*;
+import ptolemy.actor.*;
+import java.util.Enumeration;
+import ptolemy.domains.sdf.kernel.*;
+
+
+/**
+ This class serves as a delay for Dataflow Domains which allows schedulers
+ like SDF to break a cycle within the topology
+ * @version $Id$
+ */
+public class Delay extends SDFAtomicActor {
+    public IOPort inputport;
+    public IOPort outputport;
+
+    public Delay(CompositeActor container, String name) 
+        throws IllegalActionException, NameDuplicationException {
+        super(container,name);
+        try{
+            inputport=(IOPort)newPort("input");
+            inputport.makeInput(true);
+            setTokenConsumptionRate(inputport, 1);
+            outputport=(IOPort)newPort("output");
+            outputport.makeOutput(true);
+            setTokenProductionRate(outputport, 1);
+            setTokenInitProduction(outputport, 1);
+        }
+        catch (IllegalActionException e1) {
+            System.out.println("SDFDelay: constructor error");
+            e1.printStackTrace();
+        }
+    }
+
+    public void initialize() throws IllegalActionException {
+        IntToken token = new IntToken();
+
+        System.out.println("Delay, initial token" + token.toString());
+        outputport.send(0,token);
+    }
+    
+    public void fire() throws IllegalActionException {
+        IntToken message;
+        
+        message=(IntToken)inputport.get(0);
+        System.out.print("Delay - ");
+        System.out.println(message.getValue());
+        outputport.send(0,message);
+    }
+}
+
+
+
+
+
+
