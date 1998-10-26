205ae50a8b2f755374af639c2d7446b6e32614c8
liuj <liuj>
1998-10-26 12:50:37 +0000

    Initial revision

---
+++ ptolemy/domains/ct/kernel/CTDynamicActor.java
@@ -0,0 +1,48 @@
+/* Interface of dynamic actors in the CT domain.
+
+ Copyright (c) 1998 The Regents of the University of California.
+ All rights reserved.
+ Permission is hereby granted, without written agreement and without
+ license or royalty fees, to use, copy, modify, and distribute this
+ software and its documentation for any purpose, provided that the above
+ copyright notice and the following two paragraphs appear in all copies
+ of this software.
+
+ IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+ FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+ ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+ THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+ SUCH DAMAGE.
+
+ THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+ INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+ PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+ CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ ENHANCEMENTS, OR MODIFICATIONS.
+ 
+                                        PT_COPYRIGHT_VERSION_2
+                                        COPYRIGHTENDKEY
+*/
+
+package ptolemy.domains.ct.kernel;
+import ptolemy.kernel.util.*;
+
+//////////////////////////////////////////////////////////////////////////
+//// CTDynamicActor
+/** 
+Interface of dynamic actors in the CT domain. Dynamic actors are actors
+that have state and will emit their state at the begining of a execution.
+Typically, integrators and analog filters are dynamic actors.
+@author Jie Liu
+@version $Id$
+@see classname
+@see full-classname
+*/
+public interface CTDynamicActor {
+    /** Emit the potential state.
+     *  @exception IllegalActionException If the data transfer is not 
+     *       completed.
+     */
+    public void emitPotentialStates() throws IllegalActionException;
+}
