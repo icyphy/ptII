3407bb24814c0058dfa3bbe56863a4b02a429de4
johnr <johnr>
1998-10-27 16:30:58 +0000

    Method skeletons

--- ptolemy/schematic/Icon.java
+++ ptolemy/schematic/Icon.java
@@ -1,4 +1,4 @@
-/* An Icon is the graphical representation of an schematic entity.
+/* An Icon is the graphical representation of a schematic entity.
 
  Copyright (c) 1998 The Regents of the University of California.
  All rights reserved.
@@ -37,9 +37,62 @@ import collections.LinkedList;
 //// Icon 
 /**
 
+An icon is the graphical representation of a schematic entity.
+Icons are created by an IconLibrary in response to a request
+for an icon. Each icon is represented in an icon library
+XML files by the <icon> element.
+
 @author Steve Neuendorffer, John Reekie
 @version $Id$
 */
-public class Icon {
+public class Icon extends XMLElement {
+
+    /**
+     * Create a new Icon. By default, the icon contains no graphic
+     * representation.
+     */
+    public Icon () {
+        ;
+    }
+
+    /**
+     * Get an enumeration over the names of the graphics formats
+     * supported by this icon. Each element of the enumeration
+     * is a string.
+     */
+    public Enumeration graphicsFormats() {
+        return null;
+    }
+
+    /** Add a new graphics element to the icon. The format
+     * specifies the graphics format. Throw an exception if
+     * a graphics element in this format already exists.
+     */
+    public void addGraphicsElement (String format, XMLElement element) {
+        ;
+    }
+
+     /** Test if this icon contains a graphics element in the
+     * given graphics format.
+     */
+    public boolean containsGraphicsElement (String format) {
+        return false;
+    }
+
+   /** Given the graphics format attribute, return the graphics
+     * element that has that format. If there isn't one, throw
+     * an exception.
+     */
+    public XMLElement getGraphicsElement (String format) {
+        return null;
+    }
+
+    /** Remove a graphics element from the icon. Throw an exception if
+     * a graphics element in this format does not exists.
+     */
+    public void removeGraphicsElement (String format) {
+        ;
+    }
+
 }
 
