e1afc506b19e15ca4b7db9080a8eab8d3427dcf9
liuj <liuj>
1998-10-28 14:04:59 +0000

    Initial revision

---
+++ ptolemy/domains/ct/kernel/solver/FixedStepSolver.java
@@ -0,0 +1,107 @@
+/* Base class for fixed step size (no error control) ODE solvers.
+
+ Copyright (c) 1998 The Regents of the University of California.
+ All rights reserved.
+ Permission is hereby granted, without written agreement and without
+ license or royalty fees, to use, copy, modify, and distribute this
+ software and its documentation for any purpose, provided that the above
+ copyright notice and the following two paragraphs appear in all copies
+ of this software.
+
+ IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+ FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+ ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+ THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+ SUCH DAMAGE.
+
+ THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+ INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+ PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+ CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ ENHANCEMENTS, OR MODIFICATIONS.
+ 
+                                        PT_COPYRIGHT_VERSION_2
+                                        COPYRIGHTENDKEY
+@ProposedRating red (liuj@eecs.berkeley.edu)
+
+*/
+
+package ptolemy.domains.ct.kernel.solver;
+import ptolemy.kernel.util.*;
+import ptolemy.actor.*;
+import ptolemy.domains.ct.kernel.*;
+
+//////////////////////////////////////////////////////////////////////////
+//// FixedStepSolver
+/** 
+Base class for fixed step size (no error control) ODE solvers.
+@version $Id$
+@see classname
+@see full-classname
+*/
+public abstract class FixedStepSolver extends ODESolver{
+    /** Construct a solver in the default workspace with an empty
+     *  string as name. The solver is added to the list of objects in
+     *  the workspace. Increment the version number of the workspace.
+     */	
+    public FixedStepSolver() {
+        super();
+    }
+
+    /** Construct a solver in the default workspace with the given name.
+     *  If the name argument is null, then the name is set to the empty
+     *  string. The director is added to the list of objects in the workspace.
+     *  Increment the version number of the workspace.
+     *  @param name Name of this solver.
+     */
+    public FixedStepSolver(String name) {
+        super(name);
+    }
+
+    /** Construct a solver in the given workspace with the given name.
+     *  If the workspace argument is null, use the default workspace.
+     *  The director is added to the list of objects in the workspace.
+     *  If the name argument is null, then the name is set to the
+     *  empty string. Increment the version number of the workspace.
+     *
+     *  @param workspace Object for synchronization and version tracking
+     *  @param name Name of this solver.
+     */
+    public FixedStepSolver(Workspace workspace, String name) {
+        super(workspace, name);
+    }
+
+    ///////////////////////////////////////////////////////////////////
+    ////                         public methods                    ////
+
+    /** Hook method for isSuccess() method of integrators.
+     *  Always returns true, since not error control.
+     *  @return True always.
+     */
+    public final boolean integratorIsSuccess(CTBaseIntegrator integrator) {
+        return true;
+    }
+
+    /** Hook method for suggestedNextStepSize() method of integrators.
+     *  Always returns 0 since no error control functionality.
+     *  @return 0 always.
+     */
+    public final double integratorSuggestedNextStepSize( 
+         CTBaseIntegrator integrator){
+        return 0;
+    }
+
+    /** Override the startOverLastStep() for the base class. Do nothing.
+     */
+    public final void startOverLastStep(){
+    }
+
+    /** Return true always, since this is a fixed step size method.
+     *  @return true always.
+     */
+    public final boolean errorTolerable(){
+        return true;
+    }
+
+}
---
+++ ptolemy/domains/ct/kernel/solver/ForwardEulerSolver.java
@@ -0,0 +1,180 @@
+/* The Forward Euler ODE solver.
+
+ Copyright (c) 1998 The Regents of the University of California.
+ All rights reserved.
+ Permission is hereby granted, without written agreement and without
+ license or royalty fees, to use, copy, modify, and distribute this
+ software and its documentation for any purpose, provided that the above
+ copyright notice and the following two paragraphs appear in all copies
+ of this software.
+
+ IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+ FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+ ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+ THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+ SUCH DAMAGE.
+
+ THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+ INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+ PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+ CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ ENHANCEMENTS, OR MODIFICATIONS.
+ 
+                                        PT_COPYRIGHT_VERSION_2
+                                        COPYRIGHTENDKEY
+@ProposedRating Green (yourname@eecs.berkeley.edu)
+@AcceptedRating Green (reviewmoderator@eecs.berkeley.edu)
+
+*/
+
+package ptolemy.domains.ct.kernel.solver;
+
+import ptolemy.kernel.util.*;
+import ptolemy.actor.*;
+import ptolemy.domains.ct.kernel.*;
+import java.util.Enumeration;
+import ptolemy.data.*;
+
+//////////////////////////////////////////////////////////////////////////
+//// ForwardEulerSolver
+/** 
+The Forward Euler(FE) ODE solver. For ODE
+    dx/dt = f(x, u, t), x(0) = x0;
+The FE method approximate the x(t+h) as:
+    x(t+h) =  x(t) + h * f(x(t), u(t), t)
+No error control is performed.
+@author  Jie Liu
+@version $Id$
+@see classname
+@see full-classname
+*/
+public class ForwardEulerSolver extends FixedStepSolver{
+
+    public static final boolean VERBOSE = true;
+    public static final boolean DEBUG = true;
+
+    /** Construct a solver in the default workspace with an empty
+     *  string as name. The solver is added to the list of objects in
+     *  the workspace. Increment the version number of the workspace.
+     */	
+    public ForwardEulerSolver() {
+        super();
+        try {
+            setName(_name);
+        } catch (NameDuplicationException ex) {
+            throw new InternalErrorException( "Scheduler name duplication.");
+        }
+    }
+
+    /** Construct a solver in the default workspace with the given name.
+     *  If the name argument is null, then the name is set to the empty
+     *  string. The director is added to the list of objects in the workspace.
+     *  Increment the version number of the workspace.
+     *  @param name Name of this solver.
+     */
+    public ForwardEulerSolver(String name) {
+        super(name);
+        try {
+            setName(_name);
+        } catch (NameDuplicationException ex) {
+            throw new InternalErrorException( "Scheduler name duplication.");
+        }
+    }
+
+    /** Construct a solver in the given workspace with the given name.
+     *  If the workspace argument is null, use the default workspace.
+     *  The director is added to the list of objects in the workspace.
+     *  If the name argument is null, then the name is set to the
+     *  empty string. Increment the version number of the workspace.
+     *
+     *  @param workspace Object for synchronization and version tracking
+     *  @param name Name of this solver.
+     */
+    public ForwardEulerSolver(Workspace workspace, String name) {
+        super(workspace, name);
+        try {
+            setName(_name);
+        } catch (NameDuplicationException ex) {
+            throw new InternalErrorException( "Scheduler name duplication.");
+        }
+    }
+
+    ///////////////////////////////////////////////////////////////////
+    ////                         public methods                    ////
+    
+    /** Resolve the state of the integrators at time 
+     *  CurrentTime+CurrentStepSize. It gets the state transition
+     *  schedule from the scheduler and fire for one iteration.
+     * 
+     * @exception IllegalActionException Not thrown in this base
+     *  class. May be needed by the derived class.
+     */
+    public void resolveStates() throws IllegalActionException {
+        if(VERBOSE) {
+            System.out.println("FE: resolveState().");
+        }
+        CTDirector dir = (CTDirector)getContainer();
+        if (dir == null) {
+            throw new IllegalActionException( this,
+                    " must have a CT director.");
+        }
+        CTScheduler sch = (CTScheduler)dir.getScheduler();
+        if (sch == null) {
+            throw new IllegalActionException( dir,
+                    " must have a director to fire.");
+        }
+        resetRound();
+        Enumeration actors = sch.stateTransitionSchedule();
+        while(actors.hasMoreElements()) {
+            Actor next = (Actor)actors.nextElement();
+            if(DEBUG) {
+                System.out.println("Firing..."+((Nameable)next).getName());
+            }
+            next.fire();
+        }
+        actors = sch.dynamicActorSchedule();
+        while(actors.hasMoreElements()) {
+            Actor next = (Actor)actors.nextElement();
+            if(DEBUG) {
+                System.out.println("Firing..."+((Nameable)next).getName());
+            }
+            next.fire();
+        }
+        dir.setCurrentTime(dir.getCurrentTime()+dir.getCurrentStepSize());
+    }
+
+    /** Abstract fire() method for integrators.
+     *
+     *  @param integrator The integrator of that calls this method.
+     * @exception IllegalActionException Not thrown in this base
+     *  class. May be needed by the derived class.
+     */
+    public void integratorFire(CTBaseIntegrator integrator)
+            throws IllegalActionException {
+        CTDirector dir = (CTDirector)getContainer();
+        if (dir == null) {
+            throw new IllegalActionException( this,
+                    " must have a CT director.");
+        }
+        double f = ((DoubleToken)integrator.input.get(0)).doubleValue();
+        double pstate = integrator.getState() + f*(dir.getCurrentStepSize());
+        integrator.setPotentialState(pstate);
+        
+        integrator.output.broadcast(new DoubleToken(pstate));
+    }
+
+    /** Integrator's aux variable number needed when solving the ODE.
+     *  @return The number of auxilary variables for the solver in each
+     *       integrator.
+     */
+    public final int integratorAuxVariableNumber() {
+        return 1;
+    }
+
+    ///////////////////////////////////////////////////////////////////
+    ////                         private variables                 ////
+    
+    // static name.
+    private static final String _name="CT_Forward_Euler_Solver" ;
+}
