ef89adc0e74a79e8b7ba2e7a434cca441b6a598c
liuj <liuj>
1998-10-29 17:42:56 +0000

    Initial revision

---
+++ ptolemy/domains/ct/kernel/NumericalNonconvergeException.java
@@ -0,0 +1,78 @@
+/* Exception to indicate numerically not converge.
+
+ Copyright (c) 1998 The Regents of the University of California.
+ All rights reserved.
+ Permission is hereby granted, without written agreement and without
+ license or royalty fees, to use, copy, modify, and distribute this
+ software and its documentation for any purpose, provided that the above
+ copyright notice and the following two paragraphs appear in all copies
+ of this software.
+
+ IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+ FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+ ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+ THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+ SUCH DAMAGE.
+
+ THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+ INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+ PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+ CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ ENHANCEMENTS, OR MODIFICATIONS.
+ 
+                                        PT_COPYRIGHT_VERSION_2
+                                        COPYRIGHTENDKEY
+@ProposedRating red (liuj@eecs.berkeley.edu)
+*/
+
+package ptolemy.domains.ct.kernel;
+import ptolemy.kernel.util.*;
+import java.util.Enumeration;
+
+//////////////////////////////////////////////////////////////////////////
+//// NumericalNonconvergeException
+/** 
+This exception is used to indicate a numerical calculation failiar,
+typically in the process of finding the fixed point solution of an 
+algebriac equation. This is a RuntimeException.
+@author  Jie Liu
+@version $Id$
+*/
+public class NumericalNonconvergeException extends InvalidStateException{
+    /** Constructs an Exception with only a detail message.
+     *  @param detail The message.
+     */
+    public NumericalNonconvergeException(String detail) {
+        super(detail);
+    }
+
+    /** Constructs an Exception with a detail message that includes the
+     *  name of the first argument and the second argument string.
+     *  @param obj The object.
+     *  @param detail The message.
+     */
+    public NumericalNonconvergeException(Nameable obj, String detail) {
+        super(obj, detail);
+    }
+
+    /** Constructs an Exception with a detail message that includes the
+     *  names of the first two arguments plus the third argument string.
+     *  @param obj1 The first object.
+     *  @param obj2 The second object.
+     *  @param detail The message.
+     */
+    public NumericalNonconvergeException(Nameable obj1, Nameable obj2,
+            String detail) {
+        super(obj1, obj2, detail);
+    }
+
+    /** Constructs an Exception with a detail message that includes the
+     *  names of an enumeration of nameable plus the an argument string.
+     *  @param objects The enumeration of Nameable objects
+     *  @param detail The message.
+     */
+    public NumericalNonconvergeException( Enumeration objects, String detail) {
+        super(objects, detail);
+    } 
+}
