afede9c0ec4191b02019493071317b7cfdaab5d9
neuendor <neuendor>
1998-11-08 13:47:12 +0000

    Initial revision

---
+++ com/microstar/xml/demo/ReaderDemo.java
@@ -0,0 +1,41 @@
+// ReaderDemo.java: demonstration application showing Ælfred's reader stream.
+// NO WARRANTY! See README, and copyright below.
+// $Id$
+
+import com.microstar.xml.XmlParser;
+import java.io.StringReader;
+import java.io.Reader;
+
+
+/**
+  * Demonstration application showing Ælfred's event stream from a stream.
+  * <p>Usage: <code>java StreamDemo</code>
+  * @author Copyright (c) 1998 by Microstar Software Ltd.;
+  * @author written by David Megginson &lt;dmeggins@microstar.com&gt;
+  * @version 1.1
+  * @see com.microstar.xml.XmlParser
+  * @see com.microstar.xml.XmlHandler
+  * @see XmlApp
+  * @see EventDemo
+  */
+public class ReaderDemo extends EventDemo {
+
+  public static void main (String args[]) 
+    throws Exception
+  {
+    ReaderDemo handler = new ReaderDemo();
+    Reader reader;
+
+    if (args.length != 0) {
+      System.err.println("Usage: java ReaderDemo");
+      System.exit(1);
+    }
+
+    reader = new StringReader("<doc>\n<title>Sample</title>\n<p n=\"1\">Sample document</p>\n</doc>\n");
+
+    XmlParser parser = new XmlParser();
+    parser.setHandler(handler);
+    parser.parse(null, null, reader);
+  }
+
+}
