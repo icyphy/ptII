f0941ab282fa42dd03be725fa5071c4dcf2e9d33
eal <eal>
1998-11-08 22:24:26 +0000

    made immutable

--- ptolemy/kernel/event/TopologyEvent.java
+++ ptolemy/kernel/event/TopologyEvent.java
@@ -1,5 +1,4 @@
-/* One or more TopologyEvents are generated whenever a TopologyChangeRequest
-   is processed.
+/* Class representing one atomic change to the topology.
 
  Copyright (c) 1998 The Regents of the University of California.
  All rights reserved.
@@ -36,68 +35,50 @@ import ptolemy.kernel.ComponentEntity;
 import ptolemy.kernel.ComponentRelation;
 import ptolemy.kernel.Relation;
 import ptolemy.kernel.Port;
+import ptolemy.kernel.util.IllegalActionException;
 
 
 //////////////////////////////////////////////////////////////////////////
 //// TopologyEvent
 /**
-A TopologyEvent is an event object that is broadcast to TopologyListener
-objects by a topology change request. TopologyEvents are queued in
-a TopologyChangeRequest object until a client object (such as a
-Director) decides that it is safe to perform the topology change.
-At the time, the event has its action performed by the request
-object that contains. After performing the request, the client
-will cause the events to be broadcast to any interested
-TopologyListener objects.
-
-<p> The <i>source</i> is the object that created the topology change
-request and the events is contains, and is typically an object such as
-an Actor or a Director.  In addition, each event object contains
+A TopologyEvent represents an atomic change to a topology, such as the
+addition of an entity or link.  A sequence of instances of this class
+are aggregated in an instance of TopologyChangeRequest to be performed
+together by a server that is capable of safely performing mutations to
+the topology. A server may also maintain a list of listeners (instances
+of TopologyListener), in which case instances of this class are used
+to inform the listeners what mutations have been performed.
+Thus, instances of this class represent both topology mutations to be
+performed, and notifications of topology events that have been performed.
+<p>
+The <i>client</i> or <i>source</i>
+is the object that creates a topology change
+request, which is an aggregation of topology events (instances of this
+class). The <i>server</i> is the object that implements the topology
+changes (by invoking doTopologyChange()).
+<p>
+This class contains
 several fields that indicate the objects involved in the mutation,
 such as <b>compositeEntity</b>, <b>entity</b>, <b>componentEntity</b>.
 <b>port</b>, <b>relation</b>. and <b>componentRelation</b>.
-
-<p> The <i>id</id> field in the event signifies the type of this
-event. For example, if its value is the value of the field
+In a typical usage, a client constructs an instance of this classes,
+using a constructor that sets the appropriate fields.
+<p>
+The <i>id</id> field in the event signifies the type of this
+event. It should be one of the public static integer constants
+provided in the class.
+For example, if the <i>id</i> is
 ENTITY_ADDED, then the doTopologyChange() method will cause
 <i>componentEntity</i> to be added to <i>compositeEntity</i>. In
-addition, when the request object notifies the listeners, the
+addition, when notifyListener() is called, the
 entityAdded() method of the TopologyListener interface will be called.
 
-@author John Reekie
+@author John Reekie, Edward A. Lee
 @version $Id$
 @see TopologyListener
+@see TopologyChangeRequest
 */
 public class TopologyEvent extends java.util.EventObject {
-
-    ///////////////////////////////////////////////////////////////////
-    ////                         public fields                    ////
-
-    /** The composite entity that had something added or removed.
-     */
-    public CompositeEntity compositeEntity;
-
-    /** The component entity that was added to or removed from
-     * a composite entity
-     */
-    public ComponentEntity componentEntity;
-
-    /** The entity that had a port added or removed
-     */
-    public Entity entity;
-
-    /** The port that was added, removed, linked, or unlinked
-     */
-    public Port port;
-
-    /** The relation that was linked or unlinked
-     */
-    public Relation relation;
-
-    /** The component relation that was added to or removed
-     * from a composite entity
-     */
-    public ComponentRelation componentRelation;
 
     ///////////////////////////////////////////////////////////////////
     ////                     public static fields                  ////
@@ -147,52 +128,52 @@ public class TopologyEvent extends java.util.EventObject {
     ////                         constructors                      ////
 
     /** Create a new mutation event, with the specified ID and with
-     * the given source (i.e. director or similar), composite entity,
+     * the given client (i.e. actor or similar), composite entity,
      * and component entity.
      */
-    public TopologyEvent (int id, Object source,
+    public TopologyEvent (int id, Object client,
             CompositeEntity compositeEntity,
             ComponentEntity componentEntity) {
-        super(source);
+        super(client);
         this.id = id;
         this.compositeEntity = compositeEntity;
         this.componentEntity = componentEntity;
     }
 
     /** Create a new mutation event, with the specified ID and with
-     * the given source (i.e. director or similar), entity,
+     * the given client (i.e. actor or similar), entity,
      * and port.
      */
-    public TopologyEvent (int id, Object source,
+    public TopologyEvent (int id, Object client,
             Entity entity,
             Port port) {
-        super(source);
+        super(client);
         this.id = id;
         this.entity = entity;
         this.port = port;
     }
 
     /** Create a new mutation event, with the specified ID and with
-     * the given source (i.e. director or similar), composite entity,
+     * the given client (i.e. actor or similar), composite entity,
      * and component relation.
      */
-    public TopologyEvent (int id, Object source,
+    public TopologyEvent (int id, Object client,
             CompositeEntity compositeEntity,
             ComponentRelation componentRelation) {
-        super(source);
+        super(client);
         this.id = id;
         this.compositeEntity = compositeEntity;
         this.componentRelation = componentRelation;
     }
 
     /** Create a new mutation event, with the specified ID and with
-     * the given source (i.e. director or similar), relation,
+     * the given client (i.e. actor or similar), relation,
      * and port.
      */
-    public TopologyEvent (int id, Object source,
+    public TopologyEvent (int id, Object client,
             Relation relation,
             Port port) {
-        super(source);
+        super(client);
         this.id = id;
         this.relation = relation;
         this.port = port;
@@ -266,8 +247,19 @@ public class TopologyEvent extends java.util.EventObject {
     }
 
     /** Make the topology change represented by this event.
+     *
+     *  @exception IllegalActionException If the change has already
+     *   been implemented.
+     *  @exception Exception If the topology change implementation
+     *   throws it.
      */
     public void doTopologyChange() throws Exception {
+        if(_mutationDone) {
+            throw new IllegalActionException("TopologyEvent.doTopologyChange()"
+            + ": attempt to implement topology change that has already been"
+            + "implemented.");
+        }
+        _mutationDone = true;
 	switch (getID()) {
 	case ENTITY_ADDED:
             componentEntity.setContainer(compositeEntity);
@@ -301,6 +293,62 @@ public class TopologyEvent extends java.util.EventObject {
             componentRelation.setContainer(null);
             break;
 	}
+    }
+
+    /** Send this mutation event to one or more mutation listeners.
+     *  This method must be called only after doTopologyChange()
+     *  has successfully completed.  Typically is is called by
+     *  the notifyListeners() method of a TopologyChangeRequest object.
+     *  Also, typically the caller will
+     *  pass an instance of TopologyMulticaster, so that a number
+     *  mutation listeners will be notified with the events.
+     *
+     *  @param listener The mutation listener to pass the
+     *   sequence of events to
+     *  @exception IllegalActionException If the topology change has not
+     *   yet been implemented.
+     */
+    public void notifyListeners (TopologyListener listener)
+            throws IllegalActionException {
+        if(!_mutationDone) {
+            throw new IllegalActionException("TopologyEvent.notifyListeners()"
+            + ": attempt to notify listeners of a topology change that has not"
+            + " been implemented.");
+        }
+
+        switch (getID()) {
+        case TopologyEvent.ENTITY_ADDED:
+            listener.entityAdded(this);
+            break;
+
+        case TopologyEvent.ENTITY_REMOVED:
+            listener.entityRemoved(this);
+            break;
+
+        case TopologyEvent.PORT_ADDED:
+            listener.portAdded(this);
+            break;
+
+        case TopologyEvent.PORT_REMOVED:
+            listener.portRemoved(this);
+            break;
+
+        case TopologyEvent.PORT_LINKED:
+            listener.portLinked(this);
+            break;
+
+        case TopologyEvent.PORT_UNLINKED:
+            listener.portUnlinked(this);
+            break;
+
+        case TopologyEvent.RELATION_ADDED:
+            listener.relationAdded(this);
+            break;
+
+        case TopologyEvent.RELATION_REMOVED:
+            listener.relationRemoved(this);
+            break;
+        }
     }
 
     /** Undo the topology change represented by this event.
@@ -344,8 +392,30 @@ public class TopologyEvent extends java.util.EventObject {
     ///////////////////////////////////////////////////////////////////
     ////                       private fields                    ////
 
-    /** The id of the event. Note: this field name does not have
-     * a preceding underscore for consistency with AWT.
-     */
+    // Flag that the mutation has been performed.
+    private boolean _mutationDone = false;
+
+    // The id of the event. Note: this field name does not have
+    // a preceding underscore for consistency with AWT.
     private int id;
+
+    // The component entity that was added to or removed from
+    //  a composite entity
+    private ComponentEntity componentEntity;
+
+    // The composite entity that had something added or removed.
+    private CompositeEntity compositeEntity;
+
+    // The entity that had a port added or removed
+    private Entity entity;
+
+    // The port that was added, removed, linked, or unlinked
+    private Port port;
+
+    // The relation that was linked or unlinked
+    private Relation relation;
+
+    // The component relation that was added to or removed
+    // from a composite entity
+    private ComponentRelation componentRelation;
 }
