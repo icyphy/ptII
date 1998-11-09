9f48bc3e5f583f802b5ace453aeae229f1baf548
lmuliadi <lmuliadi>
1998-11-09 15:01:21 +0000

    Initial revision

---
+++ ptolemy/domains/de/demo/ABROApplet.java
@@ -0,0 +1,293 @@
+/* An applet that uses Ptolemy II DE domain.
+
+ Copyright (c) 1998 The Regents of the University of California.
+ All rights reserved.
+ Permission is hereby granted, without written agreement and without
+ license or royalty fees, to use, copy, modify, and distribute this
+ software and its documentation for any purpose, provided that the above
+ copyright notice and the following two paragraphs appear in all copies
+ of this software.
+
+ IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+ FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+ ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+ THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+ SUCH DAMAGE.
+
+ THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+ INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+ PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+ CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ ENHANCEMENTS, OR MODIFICATIONS.
+ 
+                                        PT_COPYRIGHT_VERSION_2
+                                        COPYRIGHTENDKEY
+*/
+
+package ptolemy.domains.de.demo;
+
+import java.applet.Applet;
+import java.awt.*;
+import java.awt.event.*;
+import ptolemy.domains.de.kernel.*;
+import ptolemy.domains.de.lib.*;
+import ptolemy.actor.*;
+import ptolemy.kernel.*;
+import ptolemy.kernel.util.*;
+import ptolemy.plot.*;
+import ptolemy.data.*;
+import ptolemy.data.expr.Parameter;
+import collections.LinkedList;
+
+//////////////////////////////////////////////////////////////////////////
+//// QueueApplet
+/** 
+An applet that uses Ptolemy II DE domain.
+
+@author Lukito
+@version $Id$
+*/
+public class ABROApplet extends Applet {
+
+    public static final boolean DEBUG = true;
+
+    ////////////////////////////////////////////////////////////////////////
+    ////                         public methods                         ////
+
+    /** Initialize the applet.
+     */	
+    public void init() {
+
+        // Process the background parameter.
+        Color background = Color.white;
+        try {
+            String colorspec = getParameter("background");
+            if (colorspec != null) {
+                background = Color.decode(colorspec);
+            }
+        } catch (Exception ex) {}
+        setBackground(background);
+
+        // Initialization
+
+        _stopTimeBox = new TextField("10.0", 10);
+        _currentTimeLabel = new Label("Current time = 0.0      ");
+        _goButton = new Button("Go");
+        _pauseButton = new Button(" Pause ");
+        _finishButton = new Button("Finish");
+        _terminateButton = new Button("Terminate");
+
+        // The applet has two panels, stacked vertically
+        setLayout(new BorderLayout());
+        Plot panel = new Plot();
+        add(panel, "Center");
+        
+        // Adding a control panel in the main panel.
+        Panel controlPanel = new Panel();
+        add(controlPanel, "South");
+        // Done adding a control panel.
+
+
+        // Adding simulation parameter panel in the control panel.
+        Panel simulationParam = new Panel();
+        simulationParam.setLayout(new GridLayout(2,1));
+        controlPanel.add(simulationParam);
+        // Done adding simulation parameter panel.
+
+        // Adding Stop time in the simulation panel.
+        Panel subSimul = new Panel();
+        simulationParam.add(subSimul);
+        subSimul.add(new Label("Stop time:"));
+        subSimul.add(_stopTimeBox);
+        // Done adding stop time.
+
+        // Adding current time in the sub panel.
+        simulationParam.add(_currentTimeLabel);
+        // Done adding average wait time.
+        
+        // Adding go button in the control panel.
+        controlPanel.add(_goButton);
+        controlPanel.add(_pauseButton);
+        controlPanel.add(_finishButton);
+        controlPanel.add(_terminateButton);
+            
+        _goButton.addActionListener(new GoButtonListener());        
+        _pauseButton.addActionListener(new PauseButtonListener());
+        _finishButton.addActionListener(new FinishButtonListener());
+        _terminateButton.addActionListener(new TerminateButtonListener());
+        // Done adding go button
+        
+
+        // Creating the topology.
+        try {
+            TypedCompositeActor sys = new TypedCompositeActor();
+            sys.setName("Demo");
+        
+            // Set up the top level composite actor, director and manager
+            _localDirector = new DECQDirector("DEDir");
+            sys.setDirector(_localDirector);
+            _manager = new Manager("Manager");
+            _manager.addExecutionListener(new MyExecutionListener());
+            sys.setManager(_manager);            
+
+            // ---------------------------------
+            // Create the actors.
+            // ---------------------------------
+            DEABRO abro = new DEABRO(sys, "ABRO");
+
+            DEPoisson a = new DEPoisson(sys, "A", 1.2, 1.0);
+            DEPoisson b = new DEPoisson(sys, "B", 1.3, 1.0);
+            DEPoisson r = new DEPoisson(sys, "R", 1.4, 3.0);
+
+            DEPlot plot = new DEPlot(sys, "Plot", panel);
+
+            // -----------------------
+            // Creating connections
+            // -----------------------
+
+            Relation r1 = sys.connect(a.output, abro.A);
+            Relation r2 = sys.connect(b.output, abro.B);
+            Relation r3 = sys.connect(r.output, abro.R);
+
+            
+            Relation r4 = sys.connect(abro.O, plot.input);
+            plot.input.link(r1);
+            plot.input.link(r2);
+            plot.input.link(r3);
+
+            String[] legends = {"O", "A", "B", "R"}; 
+            plot.setLegend(legends);
+
+                        
+        } catch (Exception ex) {
+            System.err.println("Setup failed: " + ex.getMessage());
+            ex.printStackTrace();
+        }
+    }
+
+    ////////////////////////////////////////////////////////////////////////
+    ////                         private variables                      ////
+    
+    // The thread that runs the simulation.
+    private boolean _isSimulationRunning;
+
+    // FIXME: Under jdk 1.2, the following can (and should) be private
+    private DECQDirector _localDirector;
+    private Manager _manager;
+
+    private TextField _stopTimeBox;
+    private double _stopTime = 100.0;
+    private Button _goButton;
+    private Button _pauseButton;
+    private Button _finishButton;
+    private Button _terminateButton;
+
+
+    private Label _currentTimeLabel;
+    private boolean _isSimulationPaused = false;
+    
+    ////////////////////////////////////////////////////////////////////////
+    ////                         private methods                        ////
+    
+
+    //////////////////////////////////////////////////////////////////////////
+    ////                       inner classes                              ////
+
+    // Show simulation progress.
+    private class CurrentTimeThread extends Thread {
+        public void run() {
+            while (_isSimulationRunning) {
+                // get the current time from director.
+                double currenttime = _localDirector.getCurrentTime();
+                _currentTimeLabel.setText("Current time = "+currenttime);
+                try {
+                    sleep(500);
+                } catch (InterruptedException e) {}
+            }
+        }
+    }
+
+    private class MyExecutionListener extends DefaultExecutionListener {
+        public void executionFinished(ExecutionEvent e) {
+            super.executionFinished(e);
+            _isSimulationRunning = false;
+        }
+        
+    }
+    
+    private class GoButtonListener implements ActionListener {
+        public void actionPerformed(ActionEvent evt) {
+
+            if (_isSimulationRunning) {
+                System.out.println("Simulation still running.. hold on..");
+                return;
+            }
+
+            try {
+                
+                // The simulation is started non-paused (of course :-) )
+                _isSimulationPaused = false;
+                _pauseButton.setLabel(" Pause ");
+                
+                // Set the stop time.
+                String timespec = _stopTimeBox.getText();
+                try {
+                    Double spec = Double.valueOf(timespec);
+                    _stopTime = spec.doubleValue();
+                } catch (NumberFormatException ex) {
+                    System.err.println("Invalid stop time: " + ex.getMessage());
+                    return;
+                }
+                
+                _localDirector.setStopTime(_stopTime);
+                
+                // Start the CurrentTimeThread.
+                Thread ctt = new CurrentTimeThread();
+                _isSimulationRunning = true;
+                ctt.start();
+                
+                _manager.startRun();
+                                
+            } catch (Exception e) {
+                System.out.println("Error: " + e.getMessage());
+                e.printStackTrace();
+            }
+                
+        }
+    }
+    
+    private class PauseButtonListener implements ActionListener {
+        public void actionPerformed(ActionEvent evt) {
+            
+            if (_isSimulationPaused) {
+                _isSimulationPaused = false;
+                _manager.resume();
+                _pauseButton.setLabel(" Pause ");
+                    
+            } else {
+                _isSimulationPaused = true;
+                _manager.pause();
+                _pauseButton.setLabel("Resume");
+                
+            }
+            
+        }
+    }
+
+    private class FinishButtonListener implements ActionListener {
+        public void actionPerformed(ActionEvent evt) {
+            _manager.finish();
+        }
+    }
+
+    private class TerminateButtonListener implements ActionListener {
+        public void actionPerformed(ActionEvent evt) {
+            _manager.terminate();
+        }
+    }
+
+}
+
+
+
---
+++ ptolemy/domains/de/lib/DEABRO.java
@@ -0,0 +1,179 @@
+/* ABRO
+
+ Copyright (c) 1998 The Regents of the University of California.
+ All rights reserved.
+ Permission is hereby granted, without written agreement and without
+ license or royalty fees, to use, copy, modify, and distribute this
+ software and its documentation for any purpose, provided that the above
+ copyright notice and the following two paragraphs appear in all copies
+ of this software.
+
+ IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+ FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+ ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+ THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+ SUCH DAMAGE.
+
+ THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+ INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+ PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+ CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ ENHANCEMENTS, OR MODIFICATIONS.
+
+                                        PT_COPYRIGHT_VERSION_2
+                                        COPYRIGHTENDKEY
+*/
+
+package ptolemy.domains.de.lib;
+
+import ptolemy.actor.*;
+import ptolemy.domains.de.kernel.*;
+import ptolemy.kernel.*;
+import ptolemy.kernel.util.*;
+import ptolemy.data.*;
+import ptolemy.data.expr.Parameter;
+import java.util.Enumeration;
+
+//////////////////////////////////////////////////////////////////////////
+//// DEABRO
+/**
+This actor implements the classical ABRO specification.
+<br>
+Emit an output O as soon as two inputs A and B have occured. Reset this
+behavior each time the input R occurs.
+
+@author Lukito Muliadi
+@version $Id$
+*/
+public class DEABRO extends DEThreadActor {
+
+    private static boolean DEBUG = true;
+
+    /** Construct a DEANDGate actor with the specified delay.
+     *  @param container The composite actor that this actor belongs too.
+     *  @param name The name of this actor.
+     *  @param tlh The amount of propagation delay for the output to goes
+     *      from low to high.
+     *  @param thl The amount of propagation delay for the output to goes
+     *      from high to low.
+     *
+     *  @exception IllegalActionException If the entity cannot be contained
+     *   by the proposed container, or if the delay is less than zero.
+     *  @exception NameDuplicationException If the container already has an
+     *   actor with this name.
+     */
+    public DEABRO(TypedCompositeActor container,
+            String name)
+            throws NameDuplicationException, IllegalActionException  {
+        super(container, name);
+        
+        // create the ports
+        A = new DEIOPort(this, "A", true, false);
+        B = new DEIOPort(this, "B", true, false);
+        R = new DEIOPort(this, "R", true, false);
+        O = new DEIOPort(this, "O", false, true);
+        A.setDeclaredType(Token.class);
+        B.setDeclaredType(Token.class);
+        R.setDeclaredType(Token.class);
+        O.setDeclaredType(DoubleToken.class);
+    }
+
+    ///////////////////////////////////////////////////////////////////
+    ////                         public methods                    ////
+
+    public void run() {
+        try {
+        outer:
+            while (true) {
+                if (DEBUG) {
+                    System.out.println("In initial state..");
+                }
+                waitForInputs();
+                if (R.hasToken(0)) {
+                    if (DEBUG) {
+                        System.out.println("Resetting..");
+                    }
+                    R.get(0);
+                    continue outer;
+                }
+                if (A.hasToken(0)) {
+                    if (DEBUG) {
+                        System.out.println("Seen A..");
+                    }
+                    A.get(0);
+
+                    while (true) {
+
+                        waitForInputs();
+                        if (R.hasToken(0)) {
+                            if (DEBUG) {
+                                System.out.println("Resetting..");
+                            }
+                            R.get(0);
+                            continue outer;
+                        } else if (B.hasToken(0)) {
+                            if (DEBUG) {
+                                System.out.println("Seen A then B..");
+                            }
+                            B.get(0);
+                            break;
+                        } else {
+                            continue;
+                        }
+                    }
+                    
+                } else if (B.hasToken(0)) {
+                    if (DEBUG) {
+                        System.out.println("Seen B..");
+                    }
+                    B.get(0);
+
+                    while (true) {
+
+                        waitForInputs();
+                        if (R.hasToken(0)) {
+                            if (DEBUG) {
+                                System.out.println("Resetting..");
+                            }
+                            R.get(0);
+                            continue outer;
+                        } else if (A.hasToken(0)) {
+                            if (DEBUG) {
+                                System.out.println("Seen B then A..");
+                            }
+                            A.get(0);
+                            break;
+                        } else {
+                            continue;
+                        }
+                    }
+                } else {
+                    throw new InternalErrorException("This can't happen!");
+                }
+                O.broadcast(new DoubleToken(1.0));
+            }
+        } catch (IllegalActionException e) {
+            e.printStackTrace();
+            throw new InternalErrorException(e.getMessage());
+            
+        }
+    }
+
+    ///////////////////////////////////////////////////////////////////
+    ////                         public variables                  ////
+
+    // the ports.
+    public DEIOPort A;
+    public DEIOPort B;
+    public DEIOPort R;
+    public DEIOPort O;
+
+    ///////////////////////////////////////////////////////////////////
+    ////                         private variables                 ////
+
+}
+
+
+
+
