f5186cd26f5bbd49f5312e8d030fc871b66335ce
lmuliadi <lmuliadi>
1998-11-10 12:11:44 +0000

    Changed blockingGo -> run

--- ptolemy/actor/test/Director.tcl
+++ ptolemy/actor/test/Director.tcl
@@ -91,7 +91,7 @@ test Director-5.1 {Test action methods} {
     set a1 [java::new ptolemy.actor.test.TestActor $e0 A1]
     set a2 [java::new ptolemy.actor.test.TestActor $e0 A2]
     $a1 clear
-    $manager blockingGo
+    $manager run
     $a1 getRecord
 } {W.E0.A1.initialize
 W.E0.A2.initialize
@@ -114,7 +114,7 @@ test Director-6.1 {Test wormhole activation} {
     $e1 setDirector $d5
     $a2 setContainer $e1
     $a1 clear
-    $manager blockingGo
+    $manager run
     $a1 getRecord
 } {W.E0.A1.initialize
 W.E0.E1.A2.initialize
