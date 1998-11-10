f086c2eae162377346b386b990c5b8b07be72394
neuendor <neuendor>
1998-11-10 12:27:14 +0000

    Fixed

--- ptolemy/schematic/test/SchematicElement.tcl
+++ ptolemy/schematic/test/SchematicElement.tcl
@@ -57,10 +57,8 @@ test SchematicElement-2.1 {Constructor tests} {
     $attributes putAt name2 value2
     set e1 [java::new ptolemy.schematic.SchematicElement element $attributes]
     list [$e0 toString] [$e1 toString]
-} {{<element>
-</element>
-} {<element name1="value1" name2="value2">
-</element>
+} {{<element name=""></element>
+} {<element name1="value1" name2="value2" name=""></element>
 }}
 
 ######################################################################
@@ -74,12 +72,10 @@ test SchematicElement-3.1 {addChildElement tests} {
     set e1 [java::new ptolemy.schematic.SchematicElement element1 $attributes]
     $e0 addChildElement $e1
     list [$e0 toString] [$e1 toString]
-} {{<element0>
-<element1 name1="value1" name2="value2">
-</element1>
+} {{<element0 name="">
+<element1 name1="value1" name2="value2" name=""></element1>
 </element0>
-} {<element1 name1="value1" name2="value2">
-</element1>
+} {<element1 name1="value1" name2="value2" name=""></element1>
 }}
 
 
@@ -90,10 +86,8 @@ test SchematicElement-3.2 {removeChildElement tests} {
     # NOTE: Uses the setup above
     $e0 removeChildElement $e1
     list [$e0 toString] [$e1 toString]
-} {{<element0>
-</element0>
-} {<element1 name1="value1" name2="value2">
-</element1>
+} {{<element0 name=""></element0>
+} {<element1 name1="value1" name2="value2" name=""></element1>
 }}
 
 ######################################################################
@@ -115,11 +109,9 @@ test SchematicElement-4.1 {childElements tests} {
     set e0child2 [$e0children nextElement] 
     list [$e0child1 toString] [$e0child2 toString] \
 [$e0children hasMoreElements]
-} {{<element1 name1="value1" name2="value2">
-</element1>
-} {<element2>
-<element3>
-</element3>
+} {{<element1 name1="value1" name2="value2" name=""></element1>
+} {<element2 name="">
+<element3 name=""></element3>
 </element2>
 } 0}
 
@@ -150,8 +142,7 @@ test SchematicElement-5.1 {setAttribute tests} {
     $e0 setAttribute name1 value1
     $e0 setAttribute name2 value2
     $e0 toString
-} {<element0 name1="value1" name2="value2">
-</element0>
+} {<element0 name1="value1" name2="value2" name=""></element0>
 }
 
 
@@ -162,8 +153,7 @@ test SchematicElement-6.2 {removeAttribute tests} {
     # NOTE: Uses the setup above
     $e0 removeAttribute name1
     $e0 toString
-} {<element0 name2="value2">
-</element0>
+} {<element0 name2="value2" name=""></element0>
 }
 
 ######################################################################
@@ -177,9 +167,10 @@ test SchematicElement-7.1 {attributes tests} {
     set e0attributes [$e0 attributeNames]
     set e0attrib1 [$e0attributes nextElement] 
     set e0attrib2 [$e0attributes nextElement] 
-    list $e0attrib1 $e0attrib2 \
+    set e0attrib3 [$e0attributes nextElement] 
+    list $e0attrib1 $e0attrib2 $e0attrib3\
 [$e0attributes hasMoreElements]
-} {name1 name2 0}
+} {name1 name2 name 0}
 
 ######################################################################
 ####
@@ -197,8 +188,7 @@ test SchematicElement-8.1 {setPCData tests} {
     set e0 [java::new ptolemy.schematic.SchematicElement element0]
     $e0 setPCData "hello this is a test\n"
     $e0 toString
-} {<element0>
-hello this is a test
+} {<element0 name="">hello this is a test
 </element0>
 }
 
@@ -210,8 +200,7 @@ test SchematicElement-8.2 {setPCData tests} {
     $e0 setPCData "hello this is a test"
     $e0 appendPCData " of appending\n"
     $e0 toString
-} {<element0>
-hello this is a test of appending
+} {<element0 name="">hello this is a test of appending
 </element0>
 }
 
@@ -224,9 +213,54 @@ test SchematicElement-8.3 {setPCData tests} {
     $e0 appendPCData " of appending\n"
     $e0 setPCData "and resetting PCData\n"    
     $e0 toString
-} {<element0>
-and resetting PCData
+} {<element0 name="">and resetting PCData
 </element0>
 }
 
- 
+######################################################################
+####
+#
+test SchematicElement-9.1 {set/getName tests} {
+    set e0 [java::new ptolemy.schematic.SchematicElement "schematicelement"]
+    $e0 setName "SchematicElement Name"
+    list [$e0 toString] [$e0 getName]
+} {{<schematicelement name="SchematicElement Name"></schematicelement>
+} {SchematicElement Name}}
+
+######################################################################
+####
+#
+test SchematicElement-10.1 {Parameter tests} {
+    set e0 [java::new ptolemy.schematic.SchematicElement "schematicelement"]
+    $e0 addParameter testparameter testtype testvalue
+    list [$e0 toString] [$e0 containsParameter testlibrary]
+} {{<schematicelement name="">
+<parameter value="testvalue" name="testparameter" type="testtype"></parameter>
+</schematicelement>
+} 0}
+
+######################################################################
+####
+#
+test SchematicElement-10.2 {parameters tests} {
+    # uses configuration above
+    set enumlib [$e0 parameters]
+    set onelib [$enumlib hasMoreElements]
+    $enumlib nextElement
+    set zerolib [$enumlib hasMoreElements]
+    list $onelib $zerolib [$e0 getParameterType testparameter]\
+[$e0 getParameterValue testparameter]
+} {1 0 testtype testvalue}
+
+######################################################################
+####
+#
+test SchematicElement-10.3 {remove Parameter tests} {
+    # uses configuration above
+    $e0 removeParameter testparameter
+    set enumlib [$e0 parameters]
+    list [$e0 toString] [$e0 containsParameter testparameter]\
+[$enumlib hasMoreElements]
+} {{<schematicelement name=""></schematicelement>
+} 0 0}
+
