8b56039ded7a09a569f495567f4bdeda4b9b3b66
cxh <cxh>
1998-11-12 18:06:14 +0000

    Initial revision

---
+++ ptolemy/data/expr/test/UtilityFunctions.tcl
@@ -0,0 +1,79 @@
+# Tests for the functionality of functions in UtilityFunctions class.
+#
+# @author: Neil Smyth
+#
+# @Version $Id$
+#
+# @Copyright (c) 1997- The Regents of the University of California.
+# All rights reserved.
+# 
+# Permission is hereby granted, without written agreement and without
+# license or royalty fees, to use, copy, modify, and distribute this
+# software and its documentation for any purpose, provided that the
+# above copyright notice and the following two paragraphs appear in all
+# copies of this software.
+# 
+# IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+# FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+# ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+# THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+# 
+# THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+# INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+# PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+# CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+# ENHANCEMENTS, OR MODIFICATIONS.
+# 
+# 						PT_COPYRIGHT_VERSION_2
+# 						COPYRIGHTENDKEY
+#######################################################################
+
+# Tycho test bed, see $TYCHO/doc/coding/testing.html for more information.
+
+# Load up the test definitions.
+if {[string compare test [info procs test]] == 1} then { 
+    source testDefs.tcl
+} {}
+
+# Uncomment this to get a full report, or set in your Tcl shell window.
+# set VERBOSE 1
+
+# If a file contains non-graphical tests, then it should be named .tcl
+# If a file contains graphical tests, then it should be called .itcl
+# 
+# It would be nice if the tests would work in a vanilla itkwish binary.
+# Check for necessary classes and adjust the auto_path accordingly.
+#
+
+
+######################################################################
+####
+# 
+test UtilityFunctions-1.0 {Check readFile method} {
+    set parser [java::new ptolemy.data.expr.PtParser]
+    
+    set tree [$parser generateParseTree "readFile(\"message.txt\")"]
+
+    #$tree displayParseTree " "
+    set res [$tree evaluateParseTree]
+
+    set value [$res toString]
+
+    list $value
+} {ptolemy.data.StringToken(Greetings...)}
+######################################################################
+####
+# result is 50 as the string for the re-invoked parser is 3+43+4 !
+test UtilityFunctions-1.0 {Check recurive calls to the parser with eval method} {
+    set parser [java::new ptolemy.data.expr.PtParser]
+    
+    set tree [$parser generateParseTree "eval(\"3 + 4\" + \"3 + 4\")"]
+
+    set res [$tree evaluateParseTree]
+
+    set value [$res toString]
+
+    list $value
+} {ptolemy.data.IntToken(50)}
