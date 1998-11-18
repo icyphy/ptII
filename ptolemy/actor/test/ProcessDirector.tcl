482c2b0e7f164540013b5b0f295a14c34af24ef9
mudit <mudit>
1998-11-18 03:30:10 +0000

    Added methods that test the basic functionality

--- ptolemy/actor/test/ProcessDirector.tcl
+++ ptolemy/actor/test/ProcessDirector.tcl
@@ -71,84 +71,32 @@ test ProcessDirector-3.1 {Test clone} {
     enumToFullNames [$w directory]
 } {W.D3}
 
+
 ######################################################################
 ####
 #
-test ProcessDirector-4.1 {Test action methods} {
+test Director-4.1 {Test _makeDirectorOf} {
     # NOTE: Uses the setup above
-    set a1 [java::new ptolemy.actor.test.TestActor $e0 A1]
-    set a2 [java::new ptolemy.actor.test.TestActor $e0 A2]
-    $a1 clear
-    $manager run
-    $a1 getRecord
-} {W.E0.A1.initialize
-W.E0.A2.initialize
-W.E0.A1.prefire
-W.E0.A1.fire
-W.E0.A1.postfire
-W.E0.A2.prefire
-W.E0.A2.fire
-W.E0.A2.postfire
-W.E0.A1.wrapup
-W.E0.A2.wrapup
-}
+    set e0 [java::new ptolemy.actor.CompositeActor $w]
+    $e0 setName E0
+    $e0 setManager $manager
+    $e0 setDirector $d3
+    list [$d3 getFullName] [$d4 getFullName] [enumToFullNames [$w directory]]
+} {W.E0.D3 W.D4 W.E0}
 
 ######################################################################
 ####
 #
-test Director-6.1 {Test wormhole activation} {
-    set e1 [java::new ptolemy.actor.CompositeActor $e0 E1]
-    set d5 [java::new ptolemy.actor.Director $w D5]
-    $e1 setDirector $d5
-    $a2 setContainer $e1
+test ProcessDirector-5.1 {Test action methods} {
+    # NOTE: Uses the setup above
+    set a1 [java::new ptolemy.actor.test.TestProcessActor $e0 A1]
+    set a2 [java::new ptolemy.actor.test.TestProcessActor $e0 A2]
     $a1 clear
     $manager run
-    $a1 getRecord
-} {W.E0.A1.initialize
-W.E0.E1.A2.initialize
-W.E0.A1.prefire
-W.E0.A1.fire
-W.E0.A1.postfire
-W.E0.E1.A2.prefire
-W.E0.E1.A2.fire
-W.E0.E1.A2.postfire
-W.E0.A1.wrapup
-W.E0.E1.A2.wrapup
-}
+    lsort [$a1 getRecord]
+    
+} {W.E0.A1.fire W.E0.A1.initialize W.E0.A1.postfire W.E0.A1.prefire W.E0.A1.wrapup W.E0.A2.fire W.E0.A2.initialize W.E0.A2.postfire W.E0.A2.prefire W.E0.A2.wrapup}
 
-######################################################################
-####
-#
-test Director-7.1 {Test mutations (adding an actor} {
-    $a1 clear
-    $d4 initialize
-    $d4 iterate
-    $a2 addActor A3
-    $d4 iterate
-    $d4 wrapup
-    $a1 getRecord
-} {W.E0.A1.initialize
-W.E0.E1.A2.initialize
-W.E0.A1.prefire
-W.E0.E1.A2.prefire
-W.E0.A1.fire
-W.E0.E1.A2.fire
-W.E0.A1.postfire
-W.E0.E1.A2.postfire
-W.E0.A1.prefire
-W.E0.E1.A3.initialize
-W.E0.E1.A2.prefire
-W.E0.E1.A3.prefire
-W.E0.A1.fire
-W.E0.E1.A2.fire
-W.E0.E1.A3.fire
-W.E0.A1.postfire
-W.E0.E1.A2.postfire
-W.E0.E1.A3.postfire
-W.E0.A1.wrapup
-W.E0.E1.A2.wrapup
-W.E0.E1.A3.wrapup
-} {KNOWN_FAILED}
 
 
 
