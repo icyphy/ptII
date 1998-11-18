54a34f7f36d2122e3d06300f060204144ab28bfe
cxh <cxh>
1998-11-18 06:41:01 +0000

    Removed trailing whitespace

--- ptolemy/actor/ProcessThread.java
+++ ptolemy/actor/ProcessThread.java
@@ -95,7 +95,7 @@ public class ProcessThread extends PtolemyThread {
                 iterate = false;
                 // container is checked for null to detect the
                 // termination of the actor
-                if (((Entity)_actor).getContainer() != null 
+                if (((Entity)_actor).getContainer() != null
                         && _actor.prefire()){
                     _actor.fire();
                     iterate =  _actor.postfire();
