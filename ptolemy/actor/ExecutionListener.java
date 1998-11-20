80f39eba6e455f349ed01ac85d05b64c557d35e5
eal <eal>
1998-11-20 00:28:01 +0000

    comments only.  fine tuning.

--- ptolemy/actor/ExecutionListener.java
+++ ptolemy/actor/ExecutionListener.java
@@ -34,15 +34,16 @@ package ptolemy.actor;
 //// ExecutionListener
 /**
 An ExecutionListener is able to receive ExecutionEvents that are issued
-during the execution of a simulation by a Manager.   In general, an
-object that implements this interface will probably be a frontend for
-the Ptolemy II system, or an object that is communicating with a frontend.
+during the execution of a model by a Manager.   In general, an
+object that implements this interface will probably be a front end such
+as a GUI for
+the Ptolemy II system, or an object that is communicating with a front end.
 The events are issued only when the event actually occurs, not when it is
 requested.   For example: A Manager receives a call to pause(), corresponding
-to a request to pause execution after the current toplevel iteration ends.
+to a request to pause execution after the current top-level iteration ends.
 The executionPaused() method will not be called until the iteration actually
-completes and the toplevel thread is suspended.   These calls are synchronous
-to the ptolemy system and will block further execution within the topology
+completes and the top-level thread is suspended.   These calls are synchronous
+to the model and will block further execution within the topology
 until they return.
 
 @author Steve Neuendorffer
@@ -52,27 +53,28 @@ until they return.
 public interface ExecutionListener {
 
     /** Called to report an execution failure.   This method will be called
-     *  when an Exeception is caught at the toplevel of execution.  Instead
-     *  of allowing the exception to propagate out of ptolemy, it is caught
-     *  here and encapsulated within an event.   It will also report the
+     *  when an Exeception is caught at the top level of execution.  Instead
+     *  of allowing the exception to propagate out the user interface,
+     *  it is caught
+     *  here and encapsulated within an event.   The event will also report the
      *  iteration during which the exception was caught, if possible.
      *
-     *  @param event an ExecutionEvent that contains a valid exception.
+     *  @param event An ExecutionEvent that contains a valid exception.
      */
     public void executionError(ExecutionEvent event);
 
-    /** Called to report that the current iteration finished and
+    /** Called to report that the current execution finished and
      *  the wrapup sequence completed normally.   The execution event will
      *  contain the number of the last successfully completed iteration.
      *
-     *  @param event an ExecutionEvent with valid iteration field.
+     *  @param event An ExecutionEvent with a valid iteration field.
      */
     public void executionFinished(ExecutionEvent event);
 
-    /** Called to report that a toplevel iteration has begun.   The execution
+    /** Called to report that a top-level iteration has begun.   The execution
      *  event will contain the number of the iteration that is starting.
      *
-     *  @param event an ExecutionEvent with valid iteration field.
+     *  @param event An ExecutionEvent with valid iteration field.
      */
     public void executionIterationStarted(ExecutionEvent event);
 
@@ -80,7 +82,7 @@ public interface ExecutionListener {
      *  event will contain the number of the last successfully completed
      *  iteration.
      *
-     *  @param event an ExecutionEvent with a valid iterations field.
+     *  @param event An ExecutionEvent with a valid iteration field.
      */
     public void executionPaused(ExecutionEvent event);
 
@@ -88,7 +90,7 @@ public interface ExecutionListener {
      *  event will contain the number of the last successfully completed
      *  iteration.
      *
-     *  @param event an ExecutionEvent with a valid iterations field.
+     *  @param event An ExecutionEvent with a valid iteration field.
      */
     public void executionResumed(ExecutionEvent event);
 
@@ -102,11 +104,10 @@ public interface ExecutionListener {
     public void executionStarted(ExecutionEvent event);
 
     /** Called to report termination of execution.   This method is called
-     *  when the Manager has done everything it can to forcibly stop a
+     *  when the Manager has done everything it can to stop a
      *  running execution.
      *
      *  @param event an ExecutionEvent.
      */
     public void executionTerminated(ExecutionEvent event);
-
 }
