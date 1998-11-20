9f8fd32016a2dcbc482dd1e7eeaccfdc81189efc
davisj <davisj>
1998-11-20 21:01:30 +0000

    Single test (3.1) successfully passes. Older tests for this class must be added back in.

--- ptolemy/domains/od/kernel/test/ODIOPort.tcl
+++ ptolemy/domains/od/kernel/test/ODIOPort.tcl
@@ -47,49 +47,11 @@ if {[string compare test [info procs test]] == 1} then {
 # Check for necessary classes and adjust the auto_path accordingly.
 #
 
-######################################################################
-####
-#
-test ODIOPort-2.1 {Send and receive multiple Tokens across one channel} {
-    set wspc [java::new ptolemy.kernel.util.Workspace]
-    set topLevel [java::new ptolemy.actor.CompositeActor $wspc]
-    # Note: Director not really needed since blocking won't occur in this test
-    set dir [java::new ptolemy.domains.od.kernel.ODDirector $wspc "director"]
-    $topLevel setDirector $dir
-    set actorA [java::new ptolemy.domains.od.kernel.ODActor $topLevel "actorA"] 
-    set actorB [java::new ptolemy.domains.od.kernel.ODActor $topLevel "actorB"] 
-    
-    set portA [java::new ptolemy.domains.od.kernel.ODIOPort $actorA "portA"]
-    $portA setOutput true
-    
-    set portB [java::new ptolemy.domains.od.kernel.ODIOPort $actorB "portB"]
-    $portB setInput true
-    
-    set rel [$topLevel connect $portA $portB "rel"]
-    
-    $dir initialize
-    
-    $actorB setPriorities
-    
-    set t1 [java::new ptolemy.data.Token]
-    set t2 [java::new ptolemy.data.Token]
-    set t3 [java::new ptolemy.data.Token]
-    
-    $portA send 0 $t1 
-    $portA send 0 $t2 
-    $portA send 0 $t3 
-    
-    set t4 [$actorB getNextToken]
-    set t5 [$actorB getNextToken]
-    set t6 [$actorB getNextToken]
-
-    list [expr {$t1 == $t4} ] [expr {$t2 == $t5} ] [expr {$t3 == $t6} ] 
-} {1 1 1}
 
 ######################################################################
 ####
 #
-test ODIOPort-3.1 {Receive tokens along two channels} {
+test ODIOPort-3.1 {Receive tokens at different times along two channels} {
     set wspc [java::new ptolemy.kernel.util.Workspace]
     set topLevel [java::new ptolemy.actor.CompositeActor $wspc]
     # Note: Director not really needed since blocking won't occur in this test
@@ -105,12 +67,14 @@ test ODIOPort-3.1 {Receive tokens along two channels} {
     set portB [java::new ptolemy.domains.od.kernel.ODIOPort $actorB "portB"]
     $portB setOutput true
     
-    set portC [java::new ptolemy.domains.od.kernel.ODIOPort $actorC "portC"]
-    $portC setInput true
-    $portC setMultiport true
+    set portC1 [java::new ptolemy.domains.od.kernel.ODIOPort $actorC "portC1"]
+    $portC1 setInput true
     
-    set rel1 [$topLevel connect $portA $portC "rel1"]
-    set rel2 [$topLevel connect $portB $portC "rel2"]
+    set portC2 [java::new ptolemy.domains.od.kernel.ODIOPort $actorC "portC2"]
+    $portC2 setInput true
+    
+    set rel1 [$topLevel connect $portA $portC1 "rel1"]
+    set rel2 [$topLevel connect $portB $portC2 "rel2"]
     
     $dir initialize
     
@@ -118,14 +82,17 @@ test ODIOPort-3.1 {Receive tokens along two channels} {
     
     set t1 [java::new ptolemy.data.Token]
     set t2 [java::new ptolemy.data.Token]
+    set endToken [java::new ptolemy.data.Token]
     
-    $portA send 0 $t1 
-    $portB send 0 $t2 
+    $portA send 0 $t1 10.0
+    $portB send 0 $t2 5.0
+    $portA send 0 $endToken 1000.0
+    $portB send 0 $endToken 1000.0
     
     set t3 [$actorC getNextToken]
     set t4 [$actorC getNextToken]
 
-    list [expr {$t1 == $t3} ] [expr {$t2 == $t4} ]
+    list [expr {$t3 == $t2} ] [expr {$t4 == $t1} ] 
 } {1 1}
 
 
