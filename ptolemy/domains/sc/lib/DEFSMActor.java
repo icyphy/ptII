0d60bc69c99395409e60ec6f70bb0154fcb77357
cxh <cxh>
1998-12-01 01:47:53 +0000

    Added copyright etc.

--- ptolemy/domains/sc/lib/DEFSMActor.java
+++ ptolemy/domains/sc/lib/DEFSMActor.java
@@ -1,3 +1,32 @@
+/* A DE FSM Actor
+
+ Copyright (c) 1998 The Regents of the University of California.
+ All rights reserved.
+ Permission is hereby granted, without written agreement and without
+ license or royalty fees, to use, copy, modify, and distribute this
+ software and its documentation for any purpose, provided that the above
+ copyright notice and the following two paragraphs appear in all copies
+ of this software.
+
+ IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+ FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+ ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+ THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+ SUCH DAMAGE.
+
+ THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+ INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+ PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+ CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ ENHANCEMENTS, OR MODIFICATIONS.
+
+                                        PT_COPYRIGHT_VERSION_2
+                                        COPYRIGHTENDKEY
+
+@ProposedRating Red (liuxj@eecs.berkeley.edu)
+@AcceptedRating Red
+*/
 
 package ptolemy.domains.sc.lib;
 
@@ -12,6 +41,14 @@ import ptolemy.graph.*;
 import java.util.Enumeration;
 import collections.LinkedList;
 
+//////////////////////////////////////////////////////////////////////////
+//// DEFSMActor
+/**
+An SCController is an FSM controller.
+
+@authors Xiaojun Liu
+@version $Id$
+*/
 public class DEFSMActor extends SCController implements TypedActor {
 
     public DEFSMActor(CompositeActor container, String name)
@@ -29,7 +66,7 @@ public class DEFSMActor extends SCController implements TypedActor {
 
     public double getStartTime() throws IllegalActionException {
 	DEDirector dir = (DEDirector)getDirector();
-	if (dir==null) {
+	if (dir == null) {
 	    throw new IllegalActionException("No director available");
 	}
 	return dir.getStartTime();
@@ -37,7 +74,7 @@ public class DEFSMActor extends SCController implements TypedActor {
 
     public double getStopTime() throws IllegalActionException {
 	DEDirector dir = (DEDirector)getDirector();
-	if (dir==null) {
+	if (dir == null) {
 	    throw new IllegalActionException("No director available");
 	}
 	return dir.getStopTime();
