8924bd757046b9cacb27ea0344941783ebb1479b
davisj <davisj>
1998-12-01 20:30:20 +0000

    Organized synchronization hierarchy and added notes to class comments. The od.demo.ODStringDemo mostly works. I need to fix deadlock detection methods.

--- ptolemy/domains/od/kernel/ODDirector.java
+++ ptolemy/domains/od/kernel/ODDirector.java
@@ -39,6 +39,43 @@ import ptolemy.actor.process.*;
 //// ODDirector 
 /** 
 
+Synchronization Notes:
+This domain observes a hierarchy of synchronization locks. When multiple
+synchronization locks are required, they must be obtained in an order that
+is consistent with this hierarchy. Adherence to this hierarchical ordering
+ensures that deadlock can not occur due to circular lock dependencies.
+ 
+The following synchronization hierarchy is utilized:
+ 
+        1. read/write access on the workspace
+        2. synchronization on the receiver
+        3. synchronization on the director
+        4. synchronization on the actor
+        5. (other) synchronization on the workspace
+ 
+We say that lock #1 is at the highest level in the hierarchy and lock #5
+is at the lowest level.
+
+As an example, a method that synchronizes on a receiver can not contain
+read/write access on the workspace; such accesses must occur outside of
+the receiver synchronization. Similarly, a method which synchronizes on a
+director must not synchronize on the receiver or contain read/write
+accesses on the workspace; it can contain synchronizations on actors or
+the workspace.
+ 
+The justification of the chosen ordering of this hierarchy is based on
+the access a method has to the fields of its object versus the fields of
+other objects. The more (less) a method focuses on the internal state of
+its object and non-synchronized methods of external objects, the lower
+(higher) the method is placed in the synchronization hierarchy. In the
+case of read/write access on the workspace, the corresponding methods,
+i.e, getReadAccess() and getWriteAccess(), access the current thread
+running in the JVM. This external access deems these methods as being at
+the top of the hierarchy. All other synchronizations on the workspace only
+focus on the internal state of the workspace and hence are at the bottom
+of the synchronization hierarchy.
+
+
 @author John S. Davis II
 @version @(#)ODDirector.java	1.3	11/16/98
 */
@@ -81,6 +118,7 @@ public class ODDirector extends ProcessDirector {
     /** 
      */
     public void initialize() throws IllegalActionException {
+        // System.out.println("ODDirector.initialize()");
         super.initialize();
     }
     
@@ -88,12 +126,14 @@ public class ODDirector extends ProcessDirector {
      */
     public synchronized void addReadBlock() {
         _readBlocks++;
+        System.out.println(_readBlocks + " actors are blocked on reads.");
     }
     
     /** 
      */
     public synchronized void addWriteBlock() {
         _writeBlocks++;
+        System.out.println(_writeBlocks + " actors are blocked on writes.");
     }
 
     /** Execute all deeply contained actors of the container of this
@@ -103,7 +143,25 @@ public class ODDirector extends ProcessDirector {
      *  container or one of the deeply contained actors throws it.
      */
     public void fire() throws IllegalActionException {
+        // System.out.println("ODDirector.fire()");
+        
+        boolean continueIteration = true;
+        while( continueIteration ) {
+            System.out.println("Director will wait until deadlock");
+            workspace().wait(this);
+            System.out.println("Awakened - now checking for deadlock");
+            if( isDeadlocked() ) {
+                resolveDeadlock();
+                if( isDeadlocked() ) {
+                    return;
+                }
+            }
+        }
+        
+        
+        /*
         while( !isDeadlocked() && !hasMutation() ) {
+            System.out.println("Entered fire loop.");
             workspace().wait(this);
             if( isDeadlocked() ) {
                 resolveDeadlock();
@@ -112,19 +170,13 @@ public class ODDirector extends ProcessDirector {
                 }
             }
             if( hasMutation() ) {
-                // try {
-                    _performMutations();
-                    if( hasMutation() ) {
-                        return;
-                    }
-                    /* 
-                } catch( NameDuplicationException e ) {
-                    System.err.println("NameDuplicationException occurring "
-                    + "because of mutation.");
+                _performMutations();
+                if( hasMutation() ) {
+                    return;
                 }
-                */
             }
         }
+        */
     }
     
     /** FIXME
@@ -156,6 +208,7 @@ public class ODDirector extends ProcessDirector {
         if( _readBlocks > 0 ) {
             _readBlocks--;
         }
+        System.out.println(_readBlocks + " actors are blocked on reads.");
     }
     
     /** 
@@ -164,12 +217,13 @@ public class ODDirector extends ProcessDirector {
         if( _writeBlocks > 0 ) {
             _writeBlocks--;
         }
+        System.out.println(_writeBlocks + " actors are blocked on writes.");
     }
     
     /** FIXME
      */
     public void resolveDeadlock() {
-        ;
+        System.out.println("*** Deadlock Needs To Be Resolved!!!");
     }
     
     ///////////////////////////////////////////////////////////////////
