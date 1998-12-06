bf79b550ae5359ea55e0644fd6b90681bdc74c84
liuj <liuj>
1998-12-06 09:03:39 +0000

    add ImpulseBESolver.java TrapezoidalRuleSolver.java

---
+++ ptolemy/domains/ct/kernel/solver/ImpulseBESolver.java
@@ -0,0 +1,123 @@
+/* The Impluse Backward Euler Solver
+
+ Copyright (c) 1998 The Regents of the University of California.
+ All rights reserved.
+ Permission is hereby granted, without written agreement and without
+ license or royalty fees, to use, copy, modify, and distribute this
+ software and its documentation for any purpose, provided that the above
+ copyright notice and the following two paragraphs appear in all copies
+ of this software.
+
+ IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+ FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+ ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+ THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+ SUCH DAMAGE.
+
+ THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+ INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+ PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+ CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ ENHANCEMENTS, OR MODIFICATIONS.
+ 
+                                        PT_COPYRIGHT_VERSION_2
+                                        COPYRIGHTENDKEY
+@ProposedRating Green (yourname@eecs.berkeley.edu)
+@AcceptedRating Green (reviewmoderator@eecs.berkeley.edu)
+
+*/
+
+package ptolemy.domains.ct.kernel.solver;
+
+import ptolemy.kernel.util.*;
+import ptolemy.actor.*;
+import ptolemy.domains.ct.kernel.*;
+import java.util.Enumeration;
+import ptolemy.data.*;
+
+//////////////////////////////////////////////////////////////////////////
+//// ImpulseBESolver
+/** 
+Description of the class
+@author  youname
+@version %W%	%G%
+@see classname
+@see full-classname
+*/
+public class ImpulseBESolver extends BackwardEulerSolver {
+    /** Construct a solver in the default workspace with an empty
+     *  string as name. The solver is added to the list of objects in
+     *  the workspace. Increment the version number of the workspace.
+     */
+    public ImpulseBESolver() {
+        super();
+        try {
+            setName(_name);
+        } catch (NameDuplicationException e) {
+            // this should never happen.
+            throw new InternalErrorException(
+                "internal error when set name to an ImpulseBESolver.");
+        }
+    }
+
+    /** Construct a solver in the given workspace with the given name.
+     *  If the workspace argument is null, use the default workspace.
+     *  The director is added to the list of objects in the workspace.
+     *  If the name argument is null, then the name is set to the
+     *  empty string. Increment the version number of the workspace.
+     *
+     *  @param workspace Object for synchronization and version tracking
+     *  @param name Name of this solver.
+     */
+    public ImpulseBESolver(Workspace workspace) {
+        super(workspace);
+        try {
+            setName(_name);
+        } catch (NameDuplicationException e) {
+            // this should never happen.
+            throw new InternalErrorException(
+                "internal error when set name to an ImpulseBESolver.");
+        }
+    }
+
+    ///////////////////////////////////////////////////////////////////
+    ////                         public methods                    ////
+
+    /** Description
+     * @see full-classname#method-name()
+     * @param parameter-name description
+     * @param parameter-name description
+     * @return description
+     * @exception full-classname description
+     */	
+    public void resolveStates() throws IllegalActionException {
+        
+        super.resolveStates();
+        CTDirector dir = (CTDirector)getContainer();
+        Enumeration actors = ((CTScheduler)dir.getScheduler()
+            ).dynamicActorSchedule();
+        while(actors.hasMoreElements()) {
+            Actor next = (Actor)actors.nextElement();
+            if(DEBUG) {
+                System.out.println("update..."+((Nameable)next).getName());
+            }
+            next.postfire();
+        }
+        dir.setCurrentStepSize(-dir.getCurrentStepSize());
+        super.resolveStates();
+        dir.setCurrentStepSize(-dir.getCurrentStepSize());
+    }
+
+
+    ////////////////////////////////////////////////////////////////////////
+    ////                         protected methods                      ////
+
+    ///////////////////////////////////////////////////////////////////
+    ////                         private variables                 ////
+
+    // Private variables should not have doc comments, they should
+    // have regular C++ comments.
+    private static final String _name="CT_ImpulseBE_Solver" ;
+
+}
---
+++ ptolemy/domains/ct/kernel/solver/TrapezoidalRuleSolver.java
@@ -0,0 +1,280 @@
+/* The Variable Step Trapezoidal Rule Solver
+
+ Copyright (c) 1998 The Regents of the University of California.
+ All rights reserved.
+ Permission is hereby granted, without written agreement and without
+ license or royalty fees, to use, copy, modify, and distribute this
+ software and its documentation for any purpose, provided that the above
+ copyright notice and the following two paragraphs appear in all copies
+ of this software.
+
+ IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+ FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+ ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+ THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+ SUCH DAMAGE.
+
+ THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+ INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+ PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+ CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ ENHANCEMENTS, OR MODIFICATIONS.
+ 
+                                        PT_COPYRIGHT_VERSION_2
+                                        COPYRIGHTENDKEY
+@ProposedRating Red (liuj@eecs.berkeley.edu)
+
+*/
+package ptolemy.domains.ct.kernel.solver;
+
+import ptolemy.kernel.util.*;
+import ptolemy.actor.*;
+import ptolemy.domains.ct.kernel.*;
+import java.util.Enumeration;
+import ptolemy.data.*;
+
+//////////////////////////////////////////////////////////////////////////
+//// TrapezoidalRuleSolver
+/** 
+Description of the class
+@author  youname
+@version %W%	%G%
+@see classname
+@see full-classname
+*/
+public class TrapezoidalRuleSolver extends VariableStepSolver{
+    
+
+    /** Construct a solver in the default workspace with an empty
+     *  string as name. The solver is added to the list of objects in
+     *  the workspace. Increment the version number of the workspace.
+     */
+    public TrapezoidalRuleSolver() {
+        super(_name);
+    }
+
+    /** Construct a solver in the given workspace with the given name.
+     *  If the workspace argument is null, use the default workspace.
+     *  The director is added to the list of objects in the workspace.
+     *  If the name argument is null, then the name is set to the
+     *  empty string. Increment the version number of the workspace.
+     *
+     *  @param workspace Object for synchronization and version tracking
+     *  @param name Name of this solver.
+     */
+    public TrapezoidalRuleSolver(Workspace workspace) {
+        super(workspace, _name);
+    }
+
+    ///////////////////////////////////////////////////////////////////
+    ////                         public methods                    ////
+
+    /** Vote if a fixed point has reached. The final result is the
+     *  <i>and</i> of all votes.
+     *  @param converge True if vote for converge.
+     */
+    public void voteForConverge(boolean converge) {
+        _converge = _converge && converge;
+    }
+
+    /** Return true if the vote result is true.
+     *  @return True if all the votes are true.
+     */
+    public boolean isConverged() {
+        return _converge;
+    }
+
+    /** Resolve the state of the integrators at time
+     *  CurrentTime+CurrentStepSize. It gets the state transition
+     *  schedule from the scheduler and fire until the fixed point.
+     *
+     * @exception IllegalActionException Not thrown in this base
+     *  class. May be needed by the derived class.
+     */
+    public void resolveStates() throws IllegalActionException {
+        if(VERBOSE) {
+            System.out.println("BE: resolveState().");
+        }
+        CTDirector dir = (CTDirector)getContainer();
+        if (dir == null) {
+            throw new IllegalActionException( this,
+                    " must have a CT director.");
+        }
+        CTScheduler sch = (CTScheduler)dir.getScheduler();
+        if (sch == null) {
+            throw new IllegalActionException( dir,
+                    " must have a director to fire.");
+        }
+        resetRound();
+        // prediction
+        Enumeration actors = sch.dynamicActorSchedule();
+        while(actors.hasMoreElements()) {
+            Actor next = (Actor)actors.nextElement();
+            if(DEBUG) {
+                System.out.println("Guessing..."+((Nameable)next).getName());
+            }
+            next.fire();
+        }
+        dir.setCurrentTime(dir.getCurrentTime()+dir.getCurrentStepSize());
+        _setConverge(false);
+        int iterations = 0;
+        while(!isConverged()) {
+            if(dir.STAT) {
+                dir.NFUNC ++;
+            }
+            incrRound();
+            _setConverge(true);
+            actors = sch.stateTransitionSchedule();
+            while(actors.hasMoreElements()) {
+                Actor next = (Actor)actors.nextElement();
+                if(DEBUG) {
+                    System.out.println("Firing..."+((Nameable)next).getName());
+                }
+                next.fire();
+            }
+            actors = sch.dynamicActorSchedule();
+            while(actors.hasMoreElements()) {
+                Actor next = (Actor)actors.nextElement();
+                if(DEBUG) {
+                    System.out.println("Refiring..."+((Nameable)next).getName());
+                }
+                next.fire();
+            }
+            if(iterations++ > dir.getMaxIterations()) {
+                //reduce step size and start over.
+                startOverLastStep();
+                resetRound();
+                // prediction
+                actors = sch.dynamicActorSchedule();
+                while(actors.hasMoreElements()) {
+                    Actor next = (Actor)actors.nextElement();
+                    if(DEBUG) {
+                        System.out.println("Guessing..."+
+                            ((Nameable)next).getName());
+                    }
+                    next.fire();
+                }
+                dir.setCurrentTime(dir.getCurrentTime()+
+                    dir.getCurrentStepSize());
+                _setConverge(false);
+                iterations = 0;
+            }
+        }
+    }
+
+    /**  fire() method for integrators.
+     *
+     *  @param integrator The integrator of that calls this method.
+     * @exception IllegalActionException Not thrown in this base
+     *  class. May be needed by the derived class.
+     */
+    public void integratorFire(CTBaseIntegrator integrator)
+            throws IllegalActionException {
+        CTDirector dir = (CTDirector)getContainer();
+        if (dir == null) {
+            throw new IllegalActionException( this,
+                    " must have a CT director.");
+        }
+        double f1 = (integrator.getHistory(0))[1];;
+        double h = dir.getCurrentStepSize();
+        double pstate;
+        if (getRound() == 0) {
+            // prediction
+            pstate = integrator.getState() + f1*h;
+            // for error control
+            integrator.setAuxVariables(0, pstate);
+        } else {
+            //correction
+            double f2 = ((DoubleToken)integrator.input.get(0)).doubleValue();
+            pstate = integrator.getState() + (h*(f1+f2))/(double)2.0;
+            double cerror = Math.abs(pstate-integrator.getPotentialState());
+            if( !(cerror < dir.getValueAccuracy())) {
+                voteForConverge(false);
+            }
+            integrator.setPotentialDerivative(f2);
+        }
+        integrator.setPotentialState(pstate);
+        integrator.output.broadcast(new DoubleToken(pstate));
+    }
+
+    /** Integrator calculate potential state and test for local
+     *  truncation error.
+     *  @param integrator The integrator of that calls this method.
+     *  @return True if the intergrator report a success on the last step.
+     */
+    public boolean integratorIsSuccess(CTBaseIntegrator integrator){
+        try {
+            CTDirector dir = (CTDirector)getContainer();
+            double errtol = dir.getLTETolerant();
+            double[] k = integrator.getAuxVariables();
+            double lte = 0.5*Math.abs(integrator.getPotentialState() - k[0]);
+            integrator.setAuxVariables(1, lte);
+            if(DEBUG) {
+                System.out.println("Integrator: "+ integrator.getName() +
+                " local truncation error = " + lte);
+            }
+            if(lte<errtol) {
+                if(DEBUG) {
+                    System.out.println("Integrator: " + integrator.getName() +
+                    " report a success.");
+                }
+                return true;
+            } else {
+                if(DEBUG) {
+                    System.out.println("Integrator: " + integrator.getName() +
+                    " reports a failiar.");
+                }
+                return false;
+            }
+        } catch (IllegalActionException e) {
+            //should never happen.
+            throw new InternalErrorException(integrator.getName() +
+            " can't read input." + e.getMessage());
+        }
+    }
+
+    /** Hook method for suggestedNextStepSize() method of
+     *  integrators.
+     *  @param integrator The integrator of that calls this method.
+     *  @return The suggested next step by the given integrator.
+     */
+    public double integratorSuggestedNextStepSize(CTBaseIntegrator integrator) {
+        CTDirector dir = (CTDirector)getContainer();
+        double lte = (integrator.getAuxVariables())[1];
+        double h = dir.getCurrentStepSize();
+        double errtol = dir.getLTETolerant();
+        double newh = 5.0*h;
+        if(lte>dir.getValueAccuracy()) {
+            newh = h* Math.max(0.5, Math.pow((3.0*errtol/lte), 1.0/3.0));
+        }
+        if(DEBUG) {
+            System.out.println("integrator: " + integrator.getName() +
+            " suggests next step size = " + newh);
+        }
+        return newh;
+    }
+
+    /** Integrator's aux variable number needed when solving the ODE.
+     *  @return The number of auxilary variables for the solver in each
+     *       integrator.
+     */
+    public final int integratorAuxVariableNumber() {
+        return 2;
+    }
+
+    ////////////////////////////////////////////////////////////////////////
+    ////                       protected methods                        ////
+    /** Set the convergence flag.
+     *  @param converge The flag setting.
+     */
+    protected void _setConverge(boolean converge) {
+        _converge = converge;
+    }
+    ////////////////////////////////////////////////////////////////////////
+    ////                         private variables                      ////
+
+    // static name.
+    private static final String _name="CT_Trapezoidal_Rule_Solver" ;
+    private boolean _converge;
+}
