7f5dd09e06ea543b5619a4be43fbdc0f7fe0f61d
ptII <ptII>
1998-12-18 16:11:38 +0000

    Reindented with jindent

--- ptolemy/filter/controller/FilterApplication.java
+++ ptolemy/filter/controller/FilterApplication.java
@@ -63,214 +63,214 @@ public class FilterApplication extends Frame implements ActionListener {
    
     public FilterApplication(String title){
  
-      super(title);
+        super(title);
  
-      MenuBar menubar = new MenuBar();
-      this.setMenuBar(menubar);
-      Menu file = new Menu("File");
+        MenuBar menubar = new MenuBar();
+        this.setMenuBar(menubar);
+        Menu file = new Menu("File");
 
-      MenuItem newf = new MenuItem("New");
-      file.add(newf);
-      MenuItem dele = new MenuItem("Delete");
-      file.add(dele);
-      MenuItem quit = new MenuItem("Quit");
-      file.add(quit);
+        MenuItem newf = new MenuItem("New");
+        file.add(newf);
+        MenuItem dele = new MenuItem("Delete");
+        file.add(dele);
+        MenuItem quit = new MenuItem("Quit");
+        file.add(quit);
 
-      file.addActionListener(this);
-      newf.setActionCommand("New");
-      dele.setActionCommand("Delete");
-      quit.setActionCommand("Quit");
+        file.addActionListener(this);
+        newf.setActionCommand("New");
+        dele.setActionCommand("Delete");
+        quit.setActionCommand("Quit");
 
-      Menu edit = new Menu("View");
-      MenuItem viewcontroller = new MenuItem("Show/Hide View Controller");
-      edit.add(viewcontroller);
+        Menu edit = new Menu("View");
+        MenuItem viewcontroller = new MenuItem("Show/Hide View Controller");
+        edit.add(viewcontroller);
 
-      edit.addActionListener(this);
-      viewcontroller.setActionCommand("Show/Hide view controller");
+        edit.addActionListener(this);
+        viewcontroller.setActionCommand("Show/Hide view controller");
 
-      menubar.add(file);
-      menubar.add(edit);
-      this.setLayout(new BorderLayout(15, 15));
-      _message = new TextArea(30, 20);
-      this.add("Center", _message);  
+        menubar.add(file);
+        menubar.add(edit);
+        this.setLayout(new BorderLayout(15, 15));
+        _message = new TextArea(30, 20);
+        this.add("Center", _message);  
 
-      // run Manager in Frame mode
-      _man = new Manager(FilterView.FRAMEMODE);
-      _newline = System.getProperty("line.separator");
-      this.setSize(new Dimension(200,200)); 
-      this.setLocation(300, 300);
-      this.setVisible(true);
-   }
+        // run Manager in Frame mode
+        _man = new Manager(FilterView.FRAMEMODE);
+        _newline = System.getProperty("line.separator");
+        this.setSize(new Dimension(200,200)); 
+        this.setLocation(300, 300);
+        this.setVisible(true);
+    }
 
     //////////////////////////////////////////////////////////////////////////
     ////                         public methods                           ////
-   /**
-    * Handle different selection on the menu items.
-    * @param evt action event created by user. 
-    */
-   public void actionPerformed(ActionEvent evt){ 
+    /**
+     * Handle different selection on the menu items.
+     * @param evt action event created by user. 
+     */
+    public void actionPerformed(ActionEvent evt){ 
 
-       String command = evt.getActionCommand(); 
-       if (command.equals("New")) {
+        String command = evt.getActionCommand(); 
+        if (command.equals("New")) {
 
-           // delete the filter first
-           _man.deletefilter();
-           // _filtset = new Filtset(this, this, "Setup New Filter"); 
-           if (_filttypeset == null) _filttypeset = new FiltTypeSet(this, "Set Filter Type");
-           _filttypeset.setVisible(true);
+            // delete the filter first
+            _man.deletefilter();
+            // _filtset = new Filtset(this, this, "Setup New Filter"); 
+            if (_filttypeset == null) _filttypeset = new FiltTypeSet(this, "Set Filter Type");
+            _filttypeset.setVisible(true);
 
-       } else if (command.equals("Delete")) {
+        } else if (command.equals("Delete")) {
 
-           _man.deletefilter();
+            _man.deletefilter();
 
-       } else if (command.equals("Quit")) {
+        } else if (command.equals("Quit")) {
 
-           System.exit(0);
+            System.exit(0);
 
-       } else if (command.equals("Show/Hide view controller")) {
+        } else if (command.equals("Show/Hide view controller")) {
 
-           _man.toggleViewControllerVisibility();
+            _man.toggleViewControllerVisibility();
 
-       }
-
-   }
-
-   /**
-    * Process the kill window event.
-    * @param event kill window event.
-    */
-   public void processEvent(AWTEvent event) {
-        if (event.getID() == Event.WINDOW_DESTROY) {
-             System.exit(0);
-        }   
-        super.processEvent(event);
-   }
-
-
-   /**
-    * Call manager to create a new filter.  Called by filter type setup 
-    * dialog.
-    *
-    * @param type type of filter (IIR, FIR, etc)
-    * @param name name of filter 
-    */ 
-   public void newFilter(int type, String name){
-        _man.newFilter(type, name);
-   }
-
-   /**
-    * Main function.  
-    */
-   public static void main(String[] args){
-        FilterApplication tm = new FilterApplication("J-Filter");
-   }
-
-
-   //////////////////////////////////////////////////////////////////////////
-   ////                         private variables                        ////
-
-   private FiltTypeSet _filttypeset;
-   private TextArea _message;
-   private Manager _man;
-   private String _newline;
-
-
-   //////////////////////////////////////////////////////////////////////////
-   ////                         inner class                              ////
-
-   // Dialog ask user to enter the type and name of the filter when
-   // the filter is created.
-   class FiltTypeSet extends Dialog implements ActionListener {
-
-        public FiltTypeSet(Frame parent, String title){
-             super(parent, title, false);
-             this.setLayout(new BorderLayout(5, 5));
-
-             // filter type 
-             _ftype = new CheckboxGroup();
-             _iir = new Checkbox("IIR", _ftype, true);
-             _firWin = new Checkbox("FIR Windowed", _ftype, false);
-             _firMP = new Checkbox("FIR M & P", _ftype, false);
-             _firFS = new Checkbox("FIR Frequency Sampling", _ftype, false);
-             _blank = new Checkbox("Blank", _ftype, false);
-   
-             // type and name 
-   
-             _p0 = new Panel();
-             _p0.setLayout(new FlowLayout(5,5,5)); 
-             _p0.add(new Label("Enter the name"));
-             _name = new TextField(15); 
-             _p0.add(_name);
-
-             _p1 = new Panel();
-             _p1.setLayout(new BorderLayout(5, 5));
-             _p1.add("North", new Label("Select Filter Type"));
-             _p1.add("Center", _blank);
-             _p1.add("South", _iir);
-
-             _p2 = new Panel();
-             _p2.setLayout(new BorderLayout(5, 5));
-             _p1.add("North", _firWin);
-             _p2.add("Center", _firMP);
-             _p2.add("South", _firFS);
-
-             _p3 = new Panel();
-             _p3.setLayout(new BorderLayout(5, 5));
-             _p3.add("North", _p0);
-             _p3.add("Center",_p1);
-             _p3.add("South", _p2);
-       
-             _ok = new Button("OK");
-
-             _ok.setActionCommand("OK"); 
-             _ok.addActionListener(this);
-             this.setLayout(new BorderLayout(10, 10));
-             this.add("Center", _p3);
-             this.add("South", _ok);
-             this.pack();
-             this.setLocation(400, 350);
         }
 
-       /**
-        * Handle the event from the ok button.  Obtain all the information
-        * from the widgets and send them to TMain.
-        */ 
-       public void actionPerformed(ActionEvent evt){
+    }
 
-           if (evt.getActionCommand().equals("OK")){
-               int ft;
-               String name;
+    /**
+     * Process the kill window event.
+     * @param event kill window event.
+     */
+    public void processEvent(AWTEvent event) {
+        if (event.getID() == Event.WINDOW_DESTROY) {
+            System.exit(0);
+        }   
+        super.processEvent(event);
+    }
 
-               // only IIR filter is supported now
-               if (_iir.getState() == true){
-                   ft = Filter.IIR;
-               } else if (_firWin.getState() == true){
-                   ft = Filter.IIR;
-                  // ft = Filter.FIRWIN;
-               } else if (_firMP.getState() == true){
-                   ft = Filter.IIR;
-                   // ft = Filter.FIROPT;
-               } else if (_firFS.getState() == true){
-                   ft = Filter.IIR;;
-                   // ft = DigitalFilter.FIRFS;
-               } else if (_blank.getState() == true){
-                   ft = Filter.IIR;
-               } else ft = 0;
 
-               name = _name.getText();
-               setVisible(false);
-               newFilter(ft, name);
-           }  
+    /**
+     * Call manager to create a new filter.  Called by filter type setup 
+     * dialog.
+     *
+     * @param type type of filter (IIR, FIR, etc)
+     * @param name name of filter 
+     */ 
+    public void newFilter(int type, String name){
+        _man.newFilter(type, name);
+    }
+
+    /**
+     * Main function.  
+     */
+    public static void main(String[] args){
+        FilterApplication tm = new FilterApplication("J-Filter");
+    }
+
+
+    //////////////////////////////////////////////////////////////////////////
+    ////                         private variables                        ////
+
+    private FiltTypeSet _filttypeset;
+    private TextArea _message;
+    private Manager _man;
+    private String _newline;
+
+
+    //////////////////////////////////////////////////////////////////////////
+    ////                         inner class                              ////
+
+    // Dialog ask user to enter the type and name of the filter when
+    // the filter is created.
+    class FiltTypeSet extends Dialog implements ActionListener {
+
+        public FiltTypeSet(Frame parent, String title){
+            super(parent, title, false);
+            this.setLayout(new BorderLayout(5, 5));
+
+            // filter type 
+            _ftype = new CheckboxGroup();
+            _iir = new Checkbox("IIR", _ftype, true);
+            _firWin = new Checkbox("FIR Windowed", _ftype, false);
+            _firMP = new Checkbox("FIR M & P", _ftype, false);
+            _firFS = new Checkbox("FIR Frequency Sampling", _ftype, false);
+            _blank = new Checkbox("Blank", _ftype, false);
+   
+            // type and name 
+   
+            _p0 = new Panel();
+            _p0.setLayout(new FlowLayout(5,5,5)); 
+            _p0.add(new Label("Enter the name"));
+            _name = new TextField(15); 
+            _p0.add(_name);
+
+            _p1 = new Panel();
+            _p1.setLayout(new BorderLayout(5, 5));
+            _p1.add("North", new Label("Select Filter Type"));
+            _p1.add("Center", _blank);
+            _p1.add("South", _iir);
+
+            _p2 = new Panel();
+            _p2.setLayout(new BorderLayout(5, 5));
+            _p1.add("North", _firWin);
+            _p2.add("Center", _firMP);
+            _p2.add("South", _firFS);
+
+            _p3 = new Panel();
+            _p3.setLayout(new BorderLayout(5, 5));
+            _p3.add("North", _p0);
+            _p3.add("Center",_p1);
+            _p3.add("South", _p2);
+       
+            _ok = new Button("OK");
+
+            _ok.setActionCommand("OK"); 
+            _ok.addActionListener(this);
+            this.setLayout(new BorderLayout(10, 10));
+            this.add("Center", _p3);
+            this.add("South", _ok);
+            this.pack();
+            this.setLocation(400, 350);
+        }
+
+        /**
+         * Handle the event from the ok button.  Obtain all the information
+         * from the widgets and send them to TMain.
+         */ 
+        public void actionPerformed(ActionEvent evt){
+
+            if (evt.getActionCommand().equals("OK")){
+                int ft;
+                String name;
+
+                // only IIR filter is supported now
+                if (_iir.getState() == true){
+                    ft = Filter.IIR;
+                } else if (_firWin.getState() == true){
+                    ft = Filter.IIR;
+                    // ft = Filter.FIRWIN;
+                } else if (_firMP.getState() == true){
+                    ft = Filter.IIR;
+                    // ft = Filter.FIROPT;
+                } else if (_firFS.getState() == true){
+                    ft = Filter.IIR;;
+                    // ft = DigitalFilter.FIRFS;
+                } else if (_blank.getState() == true){
+                    ft = Filter.IIR;
+                } else ft = 0;
+
+                name = _name.getText();
+                setVisible(false);
+                newFilter(ft, name);
+            }  
          
-       }
+        }
 
-      // private variables  
-      private Panel _p0, _p1, _p2, _p3;
+        // private variables  
+        private Panel _p0, _p1, _p2, _p3;
 
-      private TextField _name;
-      private CheckboxGroup _ftype;
-      private Checkbox _iir, _firWin, _firMP, _firFS, _blank;
-      private Button _ok;
-   }
+        private TextField _name;
+        private CheckboxGroup _ftype;
+        private Checkbox _iir, _firWin, _firMP, _firFS, _blank;
+        private Button _ok;
+    }
 
 }
--- ptolemy/filter/controller/Manager.java
+++ ptolemy/filter/controller/Manager.java
@@ -54,7 +54,7 @@ public class Manager {
      * @param mode mode of operation, either Frame mode, or Applet mode.
      */
     public Manager(int mode){
-       _opMode = mode;
+        _opMode = mode;
     }
 
     //////////////////////////////////////////////////////////////////////////
@@ -71,42 +71,42 @@ public class Manager {
      */
     public void newFilter(int type, String name){
 
-       if (_fobj!= null){
-          deletefilter();
-       }
-       _fobj = new FilterObj();
-       _fobj.init(name, type);
-       if (type == Filter.IIR){ // IIR
-          if (_opMode == FilterView.FRAMEMODE){ // frame mode, it is ok to start
-                                     // filter design now since all the
-                                     // graphics is taken care internally
-              _fobj.setIIRParameter(Filter.BUTTERWORTH, Filter.BILINEAR,
-                                    Filter.LOWPASS, 1.0);
-          }
-          _addView("IIRFilterParameterView");
-       }
+        if (_fobj!= null){
+            deletefilter();
+        }
+        _fobj = new FilterObj();
+        _fobj.init(name, type);
+        if (type == Filter.IIR){ // IIR
+            if (_opMode == FilterView.FRAMEMODE){ // frame mode, it is ok to start
+                // filter design now since all the
+                // graphics is taken care internally
+                _fobj.setIIRParameter(Filter.BUTTERWORTH, Filter.BILINEAR,
+                        Filter.LOWPASS, 1.0);
+            }
+            _addView("IIRFilterParameterView");
+        }
 
-       _addView("PoleZeroView");
-       _addView("FreqView");
-       _addView("ImpulseView");
-       _addView("TransferFunctionView");
+        _addView("PoleZeroView");
+        _addView("FreqView");
+        _addView("ImpulseView");
+        _addView("TransferFunctionView");
 
-       // setup view controller
-       Enumeration viewkey = _views.keys();
-       String [] viewnames = new String[_views.size()];
-       int i=0;
-       while (viewkey.hasMoreElements()){
+        // setup view controller
+        Enumeration viewkey = _views.keys();
+        String [] viewnames = new String[_views.size()];
+        int i=0;
+        while (viewkey.hasMoreElements()){
             viewnames[i++] = new String((String) viewkey.nextElement());
-       }
-       _viewcontroller = new ViewController(name, viewnames, this);
+        }
+        _viewcontroller = new ViewController(name, viewnames, this);
 
-       // send the view controller reference to the view
-       viewkey = _views.keys();
-       i = 0;
-       while (viewkey.hasMoreElements()){
+        // send the view controller reference to the view
+        viewkey = _views.keys();
+        i = 0;
+        while (viewkey.hasMoreElements()){
             FilterView view = (FilterView) _views.get(viewkey.nextElement());
             view.setViewController(_viewcontroller);
-       }
+        }
     }
 
 
@@ -117,28 +117,28 @@ public class Manager {
      * @param name name of the view to obtain.
      */
     public FilterView getView(String name){
-       Enumeration viewkeys = _views.keys();
-       FilterView view = null;
-       while (viewkeys.hasMoreElements()){
-           String viewname = (String) viewkeys.nextElement();
-           if (viewname.equals(name)){
+        Enumeration viewkeys = _views.keys();
+        FilterView view = null;
+        while (viewkeys.hasMoreElements()){
+            String viewname = (String) viewkeys.nextElement();
+            if (viewname.equals(name)){
                 view = (FilterView) _views.get(viewname);
                 break;
-           }
-       }
-       return view;
+            }
+        }
+        return view;
     }
 
     public FilterObj getFilterObject(){
-       return _fobj;
+        return _fobj;
     }
 
 
-   /**
-    * Delete the filter object.  It check if the view objects
-    * exists or not, delete them if they do exist.
-    */
-   public void deletefilter() {
+    /**
+     * Delete the filter object.  It check if the view objects
+     * exists or not, delete them if they do exist.
+     */
+    public void deletefilter() {
         if (_fobj != null){
             Enumeration viewkey = _views.keys();
             while (viewkey.hasMoreElements()){
@@ -152,75 +152,75 @@ public class Manager {
             _viewcontroller.setVisible(false);
             _viewcontroller = null;
         }
-   }
+    }
 
-   public void toggleViewControllerVisibility(){
+    public void toggleViewControllerVisibility(){
         if (_viewcontroller != null){
-             if (_viewcontroller.isVisible()){
-                   _viewcontroller.setVisible(false);
-             } else {
-                   _viewcontroller.setVisible(true);
-             }
+            if (_viewcontroller.isVisible()){
+                _viewcontroller.setVisible(false);
+            } else {
+                _viewcontroller.setVisible(true);
+            }
         }
-   }
+    }
 
-   public void toggleView(String toggleviewname, boolean show){
-       Enumeration viewkeys = _views.keys();
-       FilterView view = null;
-       while (viewkeys.hasMoreElements()){
-           String viewname = (String) viewkeys.nextElement();
-           if (viewname.equals(toggleviewname)){
+    public void toggleView(String toggleviewname, boolean show){
+        Enumeration viewkeys = _views.keys();
+        FilterView view = null;
+        while (viewkeys.hasMoreElements()){
+            String viewname = (String) viewkeys.nextElement();
+            if (viewname.equals(toggleviewname)){
                 view = (FilterView) _views.get(viewname);
                 view.setVisible(show);
                 break;
-           }
-       }
-   }
+            }
+        }
+    }
 
-   //////////////////////////////////////////////////////////////////////////
-   ////                         public variables                        ////
+    //////////////////////////////////////////////////////////////////////////
+    ////                         public variables                        ////
 
-   //////////////////////////////////////////////////////////////////////////
-   ////                         private methods                         ////
+    //////////////////////////////////////////////////////////////////////////
+    ////                         private methods                         ////
 
     //
     // Add a view according to the given name.
     // It calls filter object's <code> addObserver() </code>
     // to add the observer to the observer list.
     //
-   private void _addView(String name){
+    private void _addView(String name){
         if (name.equals("PoleZeroView")){
-             PoleZeroView pzv = new PoleZeroView(_fobj, _opMode,
-                                new String("PoleZeroView"));
-             _fobj.addObserver(pzv);
-             _views.put(new String(name), pzv);
+            PoleZeroView pzv = new PoleZeroView(_fobj, _opMode,
+                    new String("PoleZeroView"));
+            _fobj.addObserver(pzv);
+            _views.put(new String(name), pzv);
         } else if (name.equals("FreqView")){
-             FreqView fv = new FreqView(_fobj, _opMode, new String("FreqView"));
-             _fobj.addObserver(fv);
-             _views.put(new String(name), fv);
+            FreqView fv = new FreqView(_fobj, _opMode, new String("FreqView"));
+            _fobj.addObserver(fv);
+            _views.put(new String(name), fv);
         } else if (name.equals("ImpulseView")){
-             ImpulseView iv = new ImpulseView(_fobj, _opMode,
-                              new String("ImpulseView"));
-             _fobj.addObserver(iv);
-             _views.put(new String(name), iv);
+            ImpulseView iv = new ImpulseView(_fobj, _opMode,
+                    new String("ImpulseView"));
+            _fobj.addObserver(iv);
+            _views.put(new String(name), iv);
         } else if (name.equals("IIRFilterParameterView")){
-             IIRFiltSetView iirv = new IIRFiltSetView(_fobj, _opMode,
-                                   new String("IIRFilterParameterView"));
-             _fobj.addObserver(iirv);
-             _views.put(new String(name), iirv);
+            IIRFiltSetView iirv = new IIRFiltSetView(_fobj, _opMode,
+                    new String("IIRFilterParameterView"));
+            _fobj.addObserver(iirv);
+            _views.put(new String(name), iirv);
         } else if (name.equals("TransferFunctionView")){
-             TransFunctView tfv = new TransFunctView(_fobj, _opMode,
-                                  new String("TransferFunctionView"));
-             _fobj.addObserver(tfv);
-             _views.put(new String(name), tfv);
+            TransFunctView tfv = new TransFunctView(_fobj, _opMode,
+                    new String("TransferFunctionView"));
+            _fobj.addObserver(tfv);
+            _views.put(new String(name), tfv);
         }
-   }
+    }
 
-   //////////////////////////////////////////////////////////////////////////
-   ////                         private variables                        ////
+    //////////////////////////////////////////////////////////////////////////
+    ////                         private variables                        ////
 
-   private int _opMode;
-   private Hashtable _views = new Hashtable();
-   private FilterObj _fobj;
-   private ViewController _viewcontroller;
+    private int _opMode;
+    private Hashtable _views = new Hashtable();
+    private FilterObj _fobj;
+    private ViewController _viewcontroller;
 }
