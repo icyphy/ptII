88dd8dfa754c12d41273bad55b7883efc19e9032
cxh <cxh>
1999-01-07 23:36:30 +0000

    Print out the java version without a newline.  The newline was causing problems under Cygwin32

--- config/confTest.java
+++ config/confTest.java
@@ -33,6 +33,6 @@ public class confTest {
      *  compiler is working.
      */  
     public static void main(String args[]) {
-        System.out.println(System.getProperty("java.version"));
+        System.out.print(System.getProperty("java.version"));
     }
 }
