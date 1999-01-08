717f251014510a8f4d74c32aaf97daecb9d7d94b
liuj <liuj>
1999-01-08 19:34:34 +0000

    New actor interfaces

---
+++ ptolemy/domains/ct/kernel/CTEventInterpreter.java
@@ -0,0 +1,50 @@
+/* Interface for actors that generates discrete events.
+
+ Copyright (c) 1998 The Regents of the University of California.
+ All rights reserved.
+ Permission is hereby granted, without written agreement and without
+ license or royalty fees, to use, copy, modify, and distribute this
+ software and its documentation for any purpose, provided that the above
+ copyright notice and the following two paragraphs appear in all copies
+ of this software.
+
+ IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+ FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+ ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+ THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+ SUCH DAMAGE.
+
+ THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+ INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+ PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+ CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ ENHANCEMENTS, OR MODIFICATIONS.
+
+                                        PT_COPYRIGHT_VERSION_2
+                                        COPYRIGHTENDKEY
+*/
+
+package ptolemy.domains.ct.kernel;
+
+//////////////////////////////////////////////////////////////////////////
+//// CTEventInterpreter
+/**
+Interface for CT actors that generates continuous signals (waveforms) from 
+discrete events. Typical implementations of event interpreters are 
+zero-order holds and impulses.
+
+@author Jie Liu
+@version $Id$
+*/
+public interface CTEventInterpreter {
+
+    ////////////////////////////////////////////////////////////////////////
+    ////                         public methods                         ////
+
+    /** Consume the discrete event that happens at the current time. If there
+     *  is nosuch events, do nothing.
+     */
+    public void consumeCurrentEvents();
+
+}
---
+++ ptolemy/domains/ct/kernel/CTStepSizeControlActor.java
@@ -0,0 +1,87 @@
+/* Interface for actors that controls integration step sizes.
+
+ Copyright (c) 1998 The Regents of the University of California.
+ All rights reserved.
+ Permission is hereby granted, without written agreement and without
+ license or royalty fees, to use, copy, modify, and distribute this
+ software and its documentation for any purpose, provided that the above
+ copyright notice and the following two paragraphs appear in all copies
+ of this software.
+
+ IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+ FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+ ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+ THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+ SUCH DAMAGE.
+
+ THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+ INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+ PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+ CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ ENHANCEMENTS, OR MODIFICATIONS.
+ 
+                                        PT_COPYRIGHT_VERSION_2
+                                        COPYRIGHTENDKEY
+@ProposedRating Red (liuj@eecs.berkeley.edu)
+@AcceptedRating Green (reviewmoderator@eecs.berkeley.edu)
+
+*/
+
+package ptolemy.domains.ct.kernel;
+
+import ptolemy.kernel.util.*;
+
+//////////////////////////////////////////////////////////////////////////
+//// CTStepSizeControlActor
+/** 
+Interface for actors that controls integration step sizes. Typically, 
+the step size of an integration algorithm is determined by the initial
+step size parameter, the local truncation error at the integrators, 
+the discontinuiyy of actor bahaviours, etc. All actors that want to 
+effect the integration step size should implement this interface.
+<P>
+Three methods are defined in this interface, isThisStepAcceptable(),
+refinedStepSize(), and predictedStepSize(). At the end of each integration
+step, the CTStepSizeControlActors will be asked whether this step is
+acceptable. If one of the actor is not satisfied, then it will be asked
+for a refined step size. The integration step will be restarted with 
+this refined step size. If all the step-size-control actors are satisfied,
+they will be asked for the (predicted) next step size.
+@author  Jie Liu
+@version $ID$
+*/
+public interface CTStepSizeControlActor {
+    
+    ///////////////////////////////////////////////////////////////////
+    ////                         public methods                    ////
+    
+    /** Return true if the current integration step is acceptable. 
+     *  Actors that implement this interface will interprete "acceptable"
+     *  themselves. For example, for integrators, "acceptable" could 
+     *  mean that the local truncation error is small enough; for 
+     *  event detectors, "acceptable" could mean that there is not event
+     *  missed during the current integration step.
+     *  @return True if the current integration step is acceptable.
+     */
+    public boolean isThisStepAcceptable();
+
+    /** Return the predicted next step size. If the current integration 
+     *  step is acceptable, the actor will be asked for the prediction 
+     *  of the next step size. If the actor that implement this interface
+     *  does not know how to predict the next step size, it should 
+     *  return java.lang.Double.MAX_VALUE.
+     *  @return The predicted next step size.
+     */
+    public double predictedNextStepSize();
+    
+    /** Return the refined step size for restarting current step. If the 
+     *  current integration step size is not acceptable, the actor will
+     *  be asked for a refined step size. The current integation step 
+     *  will be restarted with the minimum of all returned values.
+     *  If the actor does not want to restart the current integration 
+     *  step, this method should return the current step size.
+     *  @return The refined step size.
+     */
+    public double refinedStepSize();
+}
