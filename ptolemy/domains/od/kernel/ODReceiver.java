208d8385a335a85c96f58e3967047c1202680018
davisj <davisj>
1999-01-22 23:38:51 +0000

    Removed most FIXME's.

--- ptolemy/domains/od/kernel/ODReceiver.java
+++ ptolemy/domains/od/kernel/ODReceiver.java
@@ -45,8 +45,6 @@ import java.util.Enumeration;
 /**
 A receiver which stores time stamped tokens using blocking reads/writes.
 A "time stamped token" is a token that has a time stamp associated with it.
-Time stamps are explained further in 
-         ptolemy/domains/od/kernel/TimedQueueReceiver.java.
 An ODReceiver stores time stamped tokens by enforcing a blocking read and
 blocking write format. Time stamped tokens are appended to the queue with 
 either of the put() methods, both of which block on a write if the queue 
@@ -54,12 +52,12 @@ is full. Time stamped tokens are removed from the queue with the get()
 method which blocks on a read if the queue is empty. If a process blocks on 
 a read or a write, the director is informed. Blocks are removed (and the 
 director is informed) if the conditions which led to them no longer apply. 
-
+<P>
 Since ODReceiver is derived from TimedQueueReceiver, it inherits both the
 lastTime and rcvrTime flags. ODReceiver sets these flags in a manner 
 similar to TimedQueueReceiver. The key difference between ODReceiver and
 TimedQueueReceiver is that get() and put() block as described above.
-
+<P>
 An additional flag in an ODReceiver is the "simultaneousIgnore" flag, the
 use of which is described in the following. In general, an actor containing 
 an ODReceiver will get a token from the receiver if the receiver has a 
@@ -67,8 +65,8 @@ uniquely minimum rcvrTime flag value with respect to the other receivers of
 the containing actor. If a receiver has a minimum rcvrTime w.r.t. all 
 receivers of the containing actor, then get() will return a non-null token 
 only if simultaneousIgnore is true.
-
-NOTE:
+<P>
+NOTE:<BR>
 This class (and indeed the OD MoC) does not place emphasis on blocking 
 writes and generally is designed for systems in which memory will be 
 abundant. Hence, queue capacities are assumed to be infinite. In particular, 
@@ -78,47 +76,9 @@ infinite queue capacities) allows us to avoid certain difficult deadlock
 situations.
 
 
-*** 
-Synchronization Notes:
-*** 
-This domain observes a hierarchy of synchronization locks. When multiple 
-synchronization locks are required, they must be obtained in an order that 
-is consistent with this hierarchy. Adherence to this hierarchical ordering
-ensures that deadlock can not occur due to circular lock dependencies.
-
-The following synchronization hierarchy is utilized: 
-
-	1. read/write access on the workspace 
-        2. synchronization on the receiver 
-        3. synchronization on the director 
-        4. synchronization on the actor
-        5. (other) synchronization on the workspace
-        
-We say that lock #1 is at the highest level in the hierarchy and lock #5
-is at the lowest level.
-
-As an example, a method that synchronizes on a receiver can not contain 
-read/write access on the workspace; such accesses must occur outside of 
-the receiver synchronization. Similarly, a method which synchronizes on a 
-director must not synchronize on the receiver or contain read/write 
-accesses on the workspace; it can contain synchronizations on actors or
-the workspace. 
-
-The justification of the chosen ordering of this hierarchy is based on
-the access a method has to the fields of its object versus the fields of
-other objects. The more (less) a method focuses on the internal state of 
-its object and non-synchronized methods of external objects, the lower 
-(higher) the method is placed in the synchronization hierarchy. In the 
-case of read/write access on the workspace, the corresponding methods, 
-i.e, getReadAccess() and getWriteAccess(), access the current thread 
-running in the JVM. This external access deems these methods as being at 
-the top of the hierarchy. All other synchronizations on the workspace only 
-focus on the internal state of the workspace and hence are at the bottom 
-of the synchronization hierarchy.
-
-
 @author John S. Davis II
 @version @(#)ODReceiver.java	1.16	11/18/98
+@see ptolemy.domains.od.kernel.TimedQueueReceiver
 */
 public class ODReceiver extends TimedQueueReceiver 
         implements ProcessReceiver {
@@ -153,10 +113,6 @@ public class ODReceiver extends TimedQueueReceiver
      * @return Token The oldest token on this queue.
      */
     public Token get() {
-        // System.out.println("\nCall to ODReceiver.get()");
-        // System.out.println("Previous rcvrTime = " + getRcvrTime() );
-       
-       
         // Cache values so that the synchronization hierarchy 
         // will not be violated
         Workspace workspace = getContainer().workspace();
@@ -168,27 +124,12 @@ public class ODReceiver extends TimedQueueReceiver
         
         synchronized(this) {
             if( _terminate ) {
-	        /*
-	        System.out.println("Get called during terminate");
-                director.addReadBlock(); 
-		notifyAll(); 
-		workspace.wait( this );
-	        */
                 throw new TerminateProcessException( getContainer(), "This "
                         + "receiver has been terminated during get().");
             }
             if( super.hasToken() && !_terminate ) {
-                /*
-                if( name.equals("printer") ) {
-                    System.out.println("\t"+name+" entered hasToken() block");
-                    System.out.println("\trcvrTime = "+getRcvrTime());
-                    System.out.println("\tactor time = " 
-                            +actor.getCurrentTime());
-                }
-                */
 	        // FIXME: What if current time = -1.0??
 	        if( getRcvrTime() <= actor.getNextTime() ) {
-		// if( getRcvrTime() <= actor.getCurrentTime() ) {
                     if( actor.hasMinRcvrTime() ) {
                         token = super.get();
                         notifyAll(); 
@@ -199,11 +140,6 @@ public class ODReceiver extends TimedQueueReceiver
                         notifyAll(); 
                         return token;
                     }
-                    /*
-                    if( name.equals("printer") ) {
-                        System.out.println(name+" reached first null");
-                    }
-                    */
                     return null;
 		}
 		// Return null in case rcvrTime is no longer the lowest.
@@ -211,21 +147,8 @@ public class ODReceiver extends TimedQueueReceiver
             } else if( !_terminate ) {
                 director.addReadBlock();
                 while( !_terminate && !super.hasToken() ) {
-                    /*
-                    System.out.println("Actor(" 
-                            + ((ComponentEntity)actor).getName() + 
-                            ") about to block in Port("
-                            + myPort.getName() 
-                            + ") ODReceiver.get()");
-                    */
-                    
                     notifyAll();
                     workspace.wait( this );
-                    /*
-                    System.out.println("Actor: " 
-                            + ((ComponentEntity)actor).getName() + 
-                            " awakened after blocking in ODReceiver.get()");
-                    */
                 }
             } 
             if( _terminate ) {
@@ -234,7 +157,6 @@ public class ODReceiver extends TimedQueueReceiver
                         + "receiver has been terminated during get().");
             } else {
 	        if( getRcvrTime() <= actor.getNextTime() ) {
-		// if( getRcvrTime() <= actor.getCurrentTime() ) {
                     if( actor.hasMinRcvrTime() ) {
                         token = super.get();
                         notifyAll(); 
@@ -246,32 +168,10 @@ public class ODReceiver extends TimedQueueReceiver
 		}
                 director.removeReadBlock(); 
             }
-            // FIXME: Will this point ever be reached?
         }
         
         return token;
     }
-
-    /** FIXME: Is this necessary?
-     *  Throw an IllegalActionException because polling of queues 
-     *  is not allowed.
-    public boolean hasRoom() {
-        return true;
-        // throw new IllegalActionException( getContainer(), "This domain"
-          //      + " does not allow polling of input queues.");
-    }
-     */
-
-    /** FIXME: Is this necessary?
-     *  Throw an IllegalActionException because polling of queues 
-     *  is not allowed.
-    public boolean hasToken() {
-        // FIXME: This needs to be changed back to "return true;" after testing
-        return super.hasToken();
-        // throw new IllegalActionException( getContainer(), "This domain"
-              //  + " does not allow polling of output queues.");
-    }
-     */
 
     /** Do a blocking write on the queue. If at any point during this 
      *  method this receiver is scheduled for termination, then throw 
@@ -293,7 +193,6 @@ public class ODReceiver extends TimedQueueReceiver
 	        ((ODActor)getContainer().getContainer()).getCurrentTime();
         put( token, currentTime );
         */ 
-
         put( token, getLastTime() );
     }
 
