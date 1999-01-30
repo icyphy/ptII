93b20ef38da6c9ac103e58d612cfc684c212526f
mudit <mudit>
1999-01-30 21:14:10 +0000

    Copy of kernelalljtests.tcl

--- ptolemy/domains/pn/kernel/test/alljtests.tcl
+++ ptolemy/domains/pn/kernel/test/alljtests.tcl
@@ -16,7 +16,7 @@ if {[info command update] == ""} then {
 #Do an update so that we are sure tycho is done displaying
 update
 set savedir "[pwd]"
-if {"BasePNDirector.tcl" != ""} {foreach i [list BasePNDirector.tcl] {puts $i; cd "$savedir"; if [ file exists $i ] {source $i}}}
+if {"BasePNDirector.tcl PNDirector.tcl" != ""} {foreach i [list BasePNDirector.tcl PNDirector.tcl] {puts $i; cd "$savedir"; if [ file exists $i ] {source $i}}}
 puts stderr dummy.tcl
 cd "$savedir"
 if [ file exists dummy.tcl ] {source dummy.tcl}
