a8a7b7d08cc48a7c9895af7f2cdb9f3093687c89
neuendor <neuendor>
1999-02-02 03:21:47 +0000

    added horribly verbose comments, for use as a template.

--- ptolemy/domains/sdf/lib/SDFDelay.java
+++ ptolemy/domains/sdf/lib/SDFDelay.java
@@ -33,38 +33,214 @@ import ptolemy.actor.*;
 import java.util.Enumeration;
 import ptolemy.domains.sdf.kernel.*;
 
-
+/* Just below this comment is a javadoc class comment.   Javadoc is a 
+ * program that automagically creates nicely formatted HTML documentation
+ * from comments.  @version is a tag that tells javadoc what the version of
+ * this file is.  The funky string between the dollar signs are automatically
+ * added to the file by CVS.  CVS is a versioning control system which allows
+ * multiple people in the group to edit copies of this file, and merge the
+ * changes back into a central master file.  This class extends the 
+ * SDFAtomicActor class (in other words, it includes everything that
+ * SDFAtomicActor does, along with whatever methods and data are derived here).
+ */ 
 /**
+ * This actor copies one token from its input to its output when fired. 
+ *
  * @version $Id$
  */
 public class SDFDelay extends SDFAtomicActor {
-    public IOPort inputport;
-    public IOPort outputport;
-
+    /*
+     * Object constructor. This creates an object of class SDFDelay.  
+     * The object will have a name given by the 'name' argument and will
+     * be contained within the composite actor given by the 'container' 
+     * argument.   This method throws two exceptions, which are error 
+     * conditions that the method (or one of the methods it calls) has 
+     * detected (notice the @exception tag).
+     */
+    /** 
+     * Create an SDFDelay actor in the given container with the given name.
+     * This actor copies one token from the input port named "input" to the
+     * output port named "output" when fired.
+     * @exception IllegalActionException If the contained methods throw it
+     * @exception NameDuplicationException If the contained methods throw it
+     */
     public SDFDelay(CompositeActor container, String name)
         throws IllegalActionException, NameDuplicationException {
+        /* This starts out by calling SDFAtomicActor's constructor.  
+         * allows us to add some extra code specific to this actor, in 
+         * addition to the regular initializer in SDFAtomicActor.
+         */
         super(container,name);
+        /* This construct is called a "try-catch block".  It is used to 
+         * detect exceptions that occured during the execution of some
+         * other code.  In this case, we detect IllegalActionException and
+         * print a reasonable error message.  This is convenient under 
+         * some environments which can destroy the debugging information from
+         * java
+         */
         try{
-            inputport=(IOPort)newPort("input");
+            /* Create a new port */
+            IOPort inputport = (IOPort)newPort("input");
+            /* Make it an input port */
             inputport.setInput(true);
-            setTokenConsumptionRate(inputport,1);
-            outputport=(IOPort)newPort("output");
+            /* Set the Consumption rate on the port.  A good way to think of
+             * this is that you are making a contract with the Ptolemy system.
+             * If you always consume one token from this port when fired, 
+             * then Ptolemy will always guarauntee that there will be a token
+             * ready when you are fired.
+             */
+            setTokenConsumptionRate(inputport, 1);
+            /* Similarly for the output port */
+            IOPort outputport = (IOPort)newPort("output");
             outputport.setOutput(true);
-            setTokenProductionRate(outputport,1);
+            setTokenProductionRate(outputport, 1);
         }
         catch (IllegalActionException e1) {
             System.out.println("SDFDelay: constructor error");
             e1.printStackTrace();
+            throw e1;
         }
     }
 
+    /* Notice that constuctors start the file.  Public methods should follow,
+     * in alphabetical order, followed by protected and private methods.
+     * Instance variables come at the end of the class.
+     */
+
+    /* The fire method is where the bulk fo the action takes place.   In
+     * SDF, fire will be called exactly once between prefire and postfire.
+     * In this class, we get an input port, print its value, and put the
+     * same token back on the output port.
+     */
+    /** This fires an actor and may be invoked several times between
+     *  invocations of prefire() and postfire(). It may produce output
+     *  data. Typically, the fire() method performs the computation associated
+     *  with an actor.
+     *
+     *  @exception IllegalActionException If firing is not permitted.
+     */
     public void fire() throws IllegalActionException {
+        /* First we declare a variable that is a Token that can carry 
+         * integer data (ptolemy.data.IntToken).
+         */
         IntToken message;
 
-        message=(IntToken)inputport.get(0);
+        /* The getPort method (in ptolemy.kernel.Entity) finds a port by
+         * name.  It returns a Port object, but all the ports in the
+         * actor package are of type IOPort, which extends Port.   So, we
+         * have to upcast the return value to the appropriate type.
+         */
+        IOPort inputport = (IOPort)getPort("input");
+        IOPort outputport = (IOPort)getPort("output");
+
+        /* now that we have the port, we can get a token from it.
+         * The argument to get is known as the channel.  The channel must be 
+         * zero, since there can only be one IORelation connected to this 
+         * inputport.  Multiple channels are useful with Multiports, which
+         * allow more than one relation to be connected to a port.
+         */
+        message = (IntToken)inputport.get(0);
+
+        /* System.out.print and System.out.println are calls to the operating
+         * system to print characters (and possibly a linefeed) to the console.
+         */
         System.out.print("Delay - ");
+
+        /* IntToken.intValue returns the value stored in an IntToken as an 
+         * integer.
+         */
         System.out.println(message.intValue());
+        
+        /* After looking at the token, pass it along to the next actor
+         */
         outputport.send(0,message);
+    }
+
+    /* Initialization is the first step in executing a model.   This happens 
+     * prior to any actual execution.   This actor doesn't need to do any
+     * initialization, other than what has already occurred in the constructor
+     */
+    /** This method should be invoked exactly once per execution
+     *  of an application, before any of these other methods are invoked.
+     *  It may produce output data.  This method typically initializes
+     *  internal members of an actor and produces initial output data.
+     *
+     *  @exception IllegalActionException If initializing is not permitted.
+     */
+    public void initialize() throws IllegalActionException {
+        return;
+    }
+
+    /* Postfire is the last step in an iteration.   This actor returns 
+     * true, since it places no limit on how execution may proceed.   In 
+     * practice this method can be used to define a stopping condition, 
+     * where execution will stop once some condition has been met.
+     */
+    /** This method should be invoked once per iteration, after the last
+     *  invocation of fire() in that iteration. It may produce output data.
+     *  It returns true if the execution can proceed into the next iteration.
+     *  This method typically wraps up an iteration, which may involve
+     *  updating local state. In an opaque, non-atomic entity, it may also
+     *  transfer output data.
+     *
+     *  @return True if the execution can continue.
+     *  @exception IllegalActionException If postfiring is not permitted.
+     */
+    public boolean postfire() throws IllegalActionException {
+        return true;
+    }
+
+    /* Prefire is the first step in every iteration.  This method returns 
+     * true if the actor can perform useful work by being fired.   For
+     * example, an actor could return false if it was waiting for a certain
+     * amount of data for a calculation, but that amount of data was not 
+     * yet present.  Most actors under SDF will just return true, since they
+     * have made a contract with the SDF domain to be fired when enough data
+     * is present.
+     */
+    /** This method should be invoked once per iteration, before the first
+     *  invocation of fire() in that iteration.  It returns true if the
+     *  iteration can proceed (the fire() method can be invoked). Thus
+     *  this method will typically check preconditions for an iteration, if
+     *  there are any. In an opaque, non-atomic entity,
+     *  it may move data into an inner subsystem.
+     *
+     *  @return True if the iteration can proceed.
+     *  @exception IllegalActionException If prefiring is not permitted.
+     */
+    public boolean prefire() throws IllegalActionException {
+        return true;
+    }
+
+    /* When this method is called, the actor must do everything in its power
+     * to immediately stop whatever it is doing.   This is primarily 
+     * called under 
+     * two circumstances.  The first is a user initiated break, such as 
+     * via CTRL-C.  The second is if an error occurs during wrapup() that 
+     * cannot be recovered from.
+     */ 
+    /** This method is invoked to immediately terminate any execution
+     *  within an actor.
+     */
+    public void terminate() {
+        return;
+    }
+
+    /* This method is called at the end of an execution, to allow the
+     * actor to cleanup after itself.  Often this will involve destroying
+     * graphical windows,  flushing buffers, etc.   This method will also get
+     * called under most abnormal termination conditions.
+     * This actor has nothing to do here.
+     */
+    /** This method should be invoked exactly once per execution
+     *  of an application.  None of the other action methods should be
+     *  be invoked after it.  It finalizes an execution, typically closing
+     *  files, displaying final results, etc.
+     *
+     *  @exception IllegalActionException If wrapup is not permitted.
+     */
+    public void wrapup() throws IllegalActionException {
+        return;
     }
 }
 
