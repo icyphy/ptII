0a3aafa0727e87f3d2306ce9beffe220e379b18f
neuendor <neuendor>
1999-02-03 03:56:35 +0000

    *** empty log message ***

--- ptolemy/domains/sdf/demo/SDFDemo.java
+++ ptolemy/domains/sdf/demo/SDFDemo.java
@@ -46,20 +46,20 @@ public class SDFdemo {
     private SDFDelay delay;
     private SDFPrint print;
     private Manager m = new Manager();
-    private CompositeActor c= new CompositeActor();
-    private SDFDirector d= new SDFDirector();
-    private SDFScheduler s=new SDFScheduler();
+    private CompositeActor c = new CompositeActor();
+    private SDFDirector d = new SDFDirector();
+    private SDFScheduler s = new SDFScheduler();
     private IORelation r;
 
     public static void main(String args[])
             throws IllegalActionException, NameDuplicationException
-         {
-             DebugListener debugger = new DebugListener();
-
-        Debug.register(debugger);
-        SDFdemo demo=new SDFdemo();
-        demo.execute();
-    }
+        {
+            DebugListener debugger = new DebugListener();
+            
+            Debug.register(debugger);
+            SDFdemo demo = new SDFdemo();
+            demo.execute();
+        }
     public void execute()
             throws IllegalActionException, NameDuplicationException
         {
@@ -69,12 +69,15 @@ public class SDFdemo {
                 d.setScheduler(s);
                 d.setScheduleValid(false);
 
-                print=new SDFPrint(c,"print");
-                ramp=new SDFRamp(c,"ramp");
-                delay=new SDFDelay(c,"delay");
+                print = new SDFPrint(c, "print");
+                ramp = new SDFRamp(c, "ramp");
+                delay = new SDFDelay(c, "delay");
 
-                r=(IORelation) c.connect(ramp.outputport,delay.inputport,"R1");
-                r=(IORelation) c.connect(delay.outputport,print.inputport,"R2");
+                IOPort delayinput = (IOPort) delay.getPort("input");
+                IOPort delayoutput = (IOPort) delay.getPort("output");
+
+                r = (IORelation) c.connect(ramp.outputport,delayinput, "R1");
+                r = (IORelation) c.connect(delayoutput,print.inputport, "R2");
 
 
                 Parameter p = (Parameter) d.getAttribute("Iterations");
