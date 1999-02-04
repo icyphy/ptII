716ff07d4ba47dbd5b41861cae93c854da022e94
cxh <cxh>
1999-02-04 18:49:00 +0000

    Added bin_install_dir rule

--- mk/ptcommon.mk
+++ mk/ptcommon.mk
@@ -131,6 +131,13 @@ subinstall:
 		done ; \
 	fi
 
+# Create the directory that contains the scripts
+bin_install_dir:
+	if [ ! -d "$(BIN_INSTALL_DIR}" ]; then
+		echo "Creating '$(BIN_INSTALL_DIR)' directory"; \
+		mkdir -p $(BIN_INSTALL_DIR); \
+	fi
+
 # Install links for scripts in $PTOLEMY/bin and $TYCHO/bin
 tybin_install:
 	-if [ "x$(EXTRA_SRCS)" != "x" ]; then \
