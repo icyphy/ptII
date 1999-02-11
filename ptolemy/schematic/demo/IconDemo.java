f645f91f760fb57008838366b575320265d7d68b
johnr <johnr>
1999-02-11 04:38:56 +0000

    Can display icon!

--- ptolemy/schematic/demo/IconDemo.java
+++ ptolemy/schematic/demo/IconDemo.java
@@ -33,6 +33,7 @@ import java.awt.*;
 import java.awt.geom.*;
 
 import java.util.HashMap;
+import java.util.Enumeration;
 import javax.swing.SwingConstants;
 
 /**
@@ -95,7 +96,7 @@ public class IconDemo {
 
         // Create the icon
         BasicRectangle background = new BasicRectangle(0,0,100,100,
-                Color.green.brighter().brighter());
+                Color.green.brighter().brighter().brighter().brighter());
         IconFigure icon = new IconFigure(background, graphic);
         layer.add(icon);
         icon.setInteractor(defaultInteractor);
@@ -104,25 +105,54 @@ public class IconDemo {
     /** Create an icon from an XML library
      */
     public void createIconFromLibrary () {
+        FigureLayer layer = graphicsPane.getForegroundLayer();
         PTMLParser parser = new PTMLParser();
         
         // FIXME Un-hardwire this
-        String url = "file:c:/java/ptII/ptolemy/schematic/lib/pticons.ptml";
+        // String url = "file:c:/java/ptII/ptolemy/schematic/lib/pticons.ptml";
+        String url = "file:/users/johnr/java/ptII/ptolemy/schematic/lib/pticons.ptml";
             
+        // Parse the icon libraries
         XMLElement root = null;
+        IconLibrary iconLibrary = null;
         try {
             root = parser.parse(url);
+            System.out.println("Parsed:\n" + root);
+
+            iconLibrary = PTMLObjectFactory.createIconLibrary(root);
+            System.out.println("Icon library: " + iconLibrary);
         }
         catch (Exception e) {
             System.out.println(e);
         }
-        System.out.println("Parsed:\n" + root);
+ 
+        // Get the "sources" library and the "Const" icon
+        IconLibrary sources = iconLibrary.getSubLibrary("Sources");
+        Icon constIcon = sources.getIcon("Clock");
 
-        //IconLibrary iconLibrary = PTMLObjectFactory.createIconLibrary(root);
-        //System.out.println("Icon library: " + iconLibrary);
+        // Create a new painted object for the graphic
+        PaintedList graphic = new PaintedList();
+        for (Enumeration i = constIcon.graphicElements(); i.hasMoreElements(); ) {
+            GraphicElement ge = (GraphicElement) i.nextElement();
+            String type = ge.getType();
+            HashMap map = new HashMap();
+            for (Enumeration j = ge.attributeNames(); j.hasMoreElements(); ) {
+                String key = (String) j.nextElement();
+                String val = (String) ge.getAttribute(key);
+                map.put(key,val);
+            }
+            PaintedObject po = GraphicsParser.createPaintedObject(type, map);
+            graphic.add(po);
+        }
 
-        
-    }
+        // Create an icon for it
+        BasicRectangle background = new BasicRectangle(0,0,100,100,
+                Color.yellow.brighter().brighter().brighter().brighter().brighter());
+        IconFigure icon = new IconFigure(background, graphic);
+        layer.add(icon);
+        icon.setInteractor(defaultInteractor);
+        icon.translate(200,100);
+     }
     
     /** Main function
      */
