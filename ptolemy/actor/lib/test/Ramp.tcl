9ddf5595485381fb3e61f54be1c68a726455b800
yuhong <yuhong>
1999-02-11 00:33:51 +0000

    initial check in.

---
+++ ptolemy/actor/lib/test/Ramp.tcl
@@ -0,0 +1,73 @@
+# Test Ramp..
+#
+# @Author: Yuhong Xiong
+#
+# @Version: $Id$
+#
+# @Copyright (c) 1997-1999 The Regents of the University of California.
+# All rights reserved.
+#
+# Permission is hereby granted, without written agreement and without
+# license or royalty fees, to use, copy, modify, and distribute this
+# software and its documentation for any purpose, provided that the
+# above copyright notice and the following two paragraphs appear in all
+# copies of this software.
+#
+# IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+# FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+# ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+# THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+#
+# THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+# INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+# PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+# CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+# ENHANCEMENTS, OR MODIFICATIONS.
+#
+# 						PT_COPYRIGHT_VERSION_2
+# 						COPYRIGHTENDKEY
+#######################################################################
+
+# Tycho test bed, see $TYCHO/doc/coding/testing.html for more information.
+
+# Load up the test definitions.
+if {[string compare test [info procs test]] == 1} then {
+    source testDefs.tcl
+} {}
+
+# Uncomment this to get a full report, or set in your Tcl shell window.
+# set VERBOSE 1
+
+# If a file contains non-graphical tests, then it should be named .tcl
+# If a file contains graphical tests, then it should be called .itcl
+#
+# It would be nice if the tests would work in a vanilla itkwish binary.
+# Check for necessary classes and adjust the auto_path accordingly.
+#
+
+######################################################################
+####
+#
+test Ramp-1.1 {test clone} {
+    set e0 [java::new ptolemy.actor.TypedCompositeActor]
+    set ramp [java::new ptolemy.actor.lib.Ramp $e0 Ramp]
+    $ramp initialize
+
+    set newobj [$ramp clone]
+    set initVal [[[$newobj getAttribute init] getToken] doubleValue]
+    set stepVal [[[$newobj getAttribute step] getToken] doubleValue]
+    list $initVal $stepVal
+} {0.0 1.0}
+
+test Ramp-1.2 {test clone} {
+    set orginit [$ramp getAttribute init]
+    set dToken [java::new {ptolemy.data.DoubleToken double} 3.0]
+    $orginit setToken $dToken
+
+    set orgInitVal [[[$ramp getAttribute init] getToken] doubleValue]
+    set initVal [[[$newobj getAttribute init] getToken] doubleValue]
+    list $orgInitVal $initVal
+} {3.0 0.0}
+
