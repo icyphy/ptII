d2111ffa036b4bd24150981fe889c8067e79ecbd
eal <eal>
1999-02-15 08:10:41 +0000

    major work

--- ptolemy/actor/util/Query.java
+++ ptolemy/actor/util/Query.java
@@ -20,7 +20,7 @@
  PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
  CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
  ENHANCEMENTS, OR MODIFICATIONS.
-
+ 
                                         PT_COPYRIGHT_VERSION_2
                                         COPYRIGHTENDKEY
 @ProposedRating Red (eal@eecs.berkeley.edu)
@@ -38,7 +38,7 @@ import java.util.NoSuchElementException;
 
 //////////////////////////////////////////////////////////////////////////
 //// Query
-/**
+/** 
 Create a query with various types of entry boxes and controls.
 
 @author  Edward A. Lee
@@ -47,46 +47,141 @@ Create a query with various types of entry boxes and controls.
 public class Query extends Panel {
 
     /** Construct a panel with no queries in it.
-     */
-    public Query() {
-        // FIXME: Setting the number of rows to a large number.
-        setLayout(new GridLayout(1,2));
+     */	
+    public Query () {
+        _grid = new GridBagLayout();
+        setLayout(_grid);
     }
 
     ///////////////////////////////////////////////////////////////////
     ////                         public methods                    ////
 
-    /** Create a single-line entry box with the specified name, label, and
-     *  default value.
+    /** Get the current value in the entry with the given name
+     *  and return as a boolean.  If the value of the entry is not
+     *  a boolean, then throw an exception.  A boolean entry is created
+     *  with the onoff() method.
+     *  @return The value currently in the entry as a boolean.
+     *  @exception NoSuchElementException If there is no item with the
+     *   specified name.  Note that this is a runtime exception, so it
+     *   need not be declared explicitly.
+     *  @exception NumberFormatException If the value of the entry cannot
+     *   be converted to a boolean.  This is a runtime exception, so it
+     *   need not be declared explicitly.
      */
+    public boolean booleanValue(String name)
+             throws NoSuchElementException, NumberFormatException
+    {
+        Checkbox result = (Checkbox)(_entries.get(name));
+        if(result == null) {
+            throw new NoSuchElementException("No item named \"" +
+                    name + " \" in the query box.");
+        }
+        return (new Boolean(result.getState())).booleanValue();
+    }    
+
+    /** Get the current value in the entry with the given name
+     *  and return as an integer.  If the value of the entry is not
+     *  an integer, then throw an exception.
+     *  @return The value currently in the entry as an integer.
+     *  @exception NoSuchElementException If there is no item with the
+     *   specified name.  Note that this is a runtime exception, so it
+     *   need not be declared explicitly.
+     *  @exception NumberFormatException If the value of the entry cannot
+     *   be converted to an integer.  This is a runtime exception, so it
+     *   need not be declared explicitly.
+     */
+    public int intValue(String name)
+             throws NoSuchElementException, NumberFormatException
+    {
+        TextField result = (TextField)(_entries.get(name));
+        if(result == null) {
+            throw new NoSuchElementException("No item named \"" +
+                    name + " \" in the query box.");
+        }
+        return (new Integer(result.getText())).intValue();
+    }    
+
+    /** Create a single-line entry box with the specified name, label, and
+     *  default value.  The width will be the default width.
+     *  @param name The name used to identify the entry (when calling get).
+     *  @param label The label to attach to the entry.
+     *  @param defvalue Default value to appear in the entry box.
+     */	
     public void line(String name, String label, String defvalue) {
-        add(new Label(label));
-        // FIXME: Fixed width.
-        TextField entrybox = new TextField(defvalue, 20);
-        add(entrybox);
-        _lines.put(name, entrybox);
+        line(name, label, defvalue, DEFAULT_ENTRY_WIDTH);
     }
 
-    /** Get the current value in the entry with the given name.
-     *  @return The value currently in the dialog box.
+    /** Create a single-line entry box with the specified name, label,
+     *  default value, and width.
+     *  @param name The name used to identify the entry (when calling get).
+     *  @param label The label to attach to the entry.
+     *  @param defvalue Default value to appear in the entry box.
+     *  @param width The width of the entry box in characters.
+     */	
+    public void line(String name, String label, String defvalue, int width) {
+        Label lbl = new Label(label);
+        TextField entrybox = new TextField(defvalue, width);
+        _addPair(lbl, entrybox);
+        _entries.put(name, entrybox);
+    }
+
+    /** Create an on-off check box.
+     *  @param name The name used to identify the entry (when calling get).
+     *  @param label The label to attach to the entry.
+     *  @param defvalue Default value (true for on).
+     */	
+    public void onoff(String name, String label, boolean defvalue) {
+        Label lbl = new Label(label);
+        Checkbox checkbox = new Checkbox();
+        checkbox.setState(defvalue);
+        _addPair(lbl, checkbox);
+        _entries.put(name, checkbox);
+    }
+
+    /** Get the current value in the entry with the given name,
+     *  and return as a String.  All entry types support this.
+     *  @return The value currently in the entry as a String.
      *  @exception NoSuchElementException If there is no item with the
      *   specified name.  Note that this is a runtime exception, so it
      *   need not be declared explicitly.
      */
-    public String get(String name) throws NoSuchElementException {
-        TextField result = (TextField)(_lines.get(name));
+    public String stringValue(String name) throws NoSuchElementException {
+        TextField result = (TextField)(_entries.get(name));
         if(result == null) {
             throw new NoSuchElementException("No item named \"" +
                     name + " \" in the query box.");
         }
         return result.getText();
-    }
+    }    
+
+    ///////////////////////////////////////////////////////////////////
+    ////                         public variables                  ////
+
+    public static final int DEFAULT_ENTRY_WIDTH = 12;
 
     ///////////////////////////////////////////////////////////////////
     ////                         protected methods                 ////
 
+    /** Add a label and a widget to the panel.
+     *  @param label The label.
+     *  @param widget The interactive entry to the right of the label.
+     */	
+    protected void _addPair(Label label, Component widget) {
+        GridBagConstraints constraints = new GridBagConstraints();
+        constraints.fill = GridBagConstraints.HORIZONTAL;
+        constraints.weightx = 1.0;
+        constraints.weighty = 1.0;
+        _grid.setConstraints(label, constraints);
+        add(label);
+        constraints.gridwidth = GridBagConstraints.REMAINDER;
+        _grid.setConstraints(widget, constraints);
+        add(widget);
+    }
+
     ///////////////////////////////////////////////////////////////////
     ////                         protected variables               ////
+
+    protected GridBagLayout _grid;
 
     ///////////////////////////////////////////////////////////////////
     ////                         private methods                   ////
@@ -94,5 +189,5 @@ public class Query extends Panel {
     ///////////////////////////////////////////////////////////////////
     ////                         private variables                 ////
 
-    private Hashtable _lines = new Hashtable();
+    private Hashtable _entries = new Hashtable();
 }
