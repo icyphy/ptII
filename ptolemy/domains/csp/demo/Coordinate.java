83ecf8e59e7f5cac17875979bfc153f69c790692
cxh <cxh>
1999-02-16 09:27:08 +0000

    Cleaned up formatting

--- ptolemy/domains/csp/demo/Coordinate.java
+++ ptolemy/domains/csp/demo/Coordinate.java
@@ -6,36 +6,29 @@
 package ptolemy.domains.csp.demo;
 
 public class Coordinate {
-    /**
-     * The X coordinate.
-	 */
+    /** The X coordinate. */
     public int X = 0;
-    /**
-	 * The Y coordinate.
-	 */
+    /** The Y coordinate. */
     public int Y = 0;
 
-    /**
-	 * Constructs a new Coordinate initialised as the origin (0, 0).
-	 */
+    /** Constructs a new Coordinate initialised as the origin (0, 0). */
     public Coordinate() {}
 
-    /**
-	 * Constructs a new Coordinate with the specified values.
-	 *
-	 * @param  x the X coordinate.
-	 * @param  y the Y coordinate.
-	 */
+    /** Constructs a new Coordinate with the specified values.
+     *
+     * @param  x the X coordinate.
+     * @param  y the Y coordinate.
+     */
     public Coordinate(int x, int y) {
         X = x;
         Y = y;
     }
 
     /**
-	 * Tests another object for equality with this instance.
-	 *
-	 * @return whether it is equal or not.
-	 */
+     * Tests another object for equality with this instance.
+     *
+     * @return whether it is equal or not.
+     */
     public boolean equals(Object o) {
         Coordinate c;
 
@@ -54,10 +47,10 @@ public class Coordinate {
     }
 
     /**
-	 * Returns a string representation of the Coordinate in the form "(X, Y)".
-	 *
-	 * @returns a string representation of the Coordinates.
-	 */
+     * Returns a string representation of the Coordinate in the form "(X, Y)".
+     *
+     * @returns a string representation of the Coordinates.
+     */
     public String toString() {
         return "(" + X + ", " + Y + ")";
     }
