8496930f20eceb9ce01b34b9cd9d97cd2f42ff4a
liuxj <liuxj>
1999-02-17 19:16:13 +0000

    Removed several debugging println.

--- ptolemy/domains/sc/kernel/SCDirector.java
+++ ptolemy/domains/sc/kernel/SCDirector.java
@@ -297,7 +297,7 @@ System.out.println("SCDirector: get controller's current refinement.");
             result = refine.prefire();
 
 /* REMOVE! */
-System.out.println("Result of prefire " + ((ComponentEntity)refine).getFullName()
+//System.out.println("Result of prefire " + ((ComponentEntity)refine).getFullName()
 	+ " is " + result);
         }
 
@@ -355,7 +355,7 @@ System.out.println("Result of prefire " + ((ComponentEntity)refine).getFullName(
                 Token t = port.get(0);
                 p = (IOPort)_controller.getPort(port.getName());
 
-System.out.println("Try get a port from " + ((ComponentEntity)_controller).getFullName());
+//System.out.println("Try get a port from " + ((ComponentEntity)_controller).getFullName());
 
                 if (p != null) {
                     rec = (p.getReceivers())[0][0];
@@ -391,8 +391,6 @@ System.out.println("Try get a port from " + ((ComponentEntity)_controller).getFu
      */
     public void transferOutputs(IOPort port) throws IllegalActionException {
 
-//System.out.println("SCD Transferout: port is " + port.getFullName());
-
         if (!port.isOutput() || !port.isOpaque()) {
             throw new IllegalActionException(this, port,
                     "transferOutputs: port argument is not an opaque output port.");
@@ -400,45 +398,15 @@ System.out.println("Try get a port from " + ((ComponentEntity)_controller).getFu
         // do not handle multiple tokens, multiple channels now
         Receiver insiderec = (port.getInsideReceivers())[0][0];
 
-//System.out.println("Check 1");
-
         CompositeActor cont = (CompositeActor)getContainer();
-
-//System.out.println("Check 2");
-
         IOPort p = (IOPort)cont.getPort(port.getName());
-
-//System.out.println("Check 3");
-
         if (insiderec.hasToken()) {
-
-//System.out.println("Check 4");
-
             try {
                 Token t = insiderec.get();
-
-//System.out.println("Check 5" + t.stringValue());
-
                 _controller.currentState().setLocalInputVar(port.getName(), t);
-
-//System.out.println("Check 6 " + p.getFullName() + " is null? " + (p == null));
-
                 if (p != null) {
-
-//if (p.getInsideReceivers() == null) {
-//System.out.println("Inside receiver for " + p.getFullName() + " is null!");
-//}
-
-//System.out.println("Check 6.5");
-
                     Receiver rec = (p.getInsideReceivers())[0][0];
-
-//System.out.println("Check 7");
-
                     rec.put(t);
-
-//System.out.println("Check 8");
-
                 }
             } catch (NoTokenException ex) {
                 throw new InternalErrorException(
