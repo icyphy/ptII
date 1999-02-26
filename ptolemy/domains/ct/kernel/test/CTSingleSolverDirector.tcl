cb1523c8b8dc17194154e7108e2c692fa80c3db9
liuj <liuj>
1999-02-26 07:18:07 +0000

    add new tests

--- ptolemy/domains/ct/kernel/test/CTSingleSolverDirector.tcl
+++ ptolemy/domains/ct/kernel/test/CTSingleSolverDirector.tcl
@@ -70,10 +70,125 @@ test CTScheduler-1.3 {Construct a director with a name in a workspace} {
     list [$d2 getFullName]
 } {W.DIR2}
 
+
+######################################################################
+####  Test methods in (abstract) CTDirector
+#
+test CTSingleSolverDirector-2.1 {Get default values} {
+    set sys [java::new ptolemy.actor.TypedCompositeActor]
+    $sys setName System
+    set dir [java::new ptolemy.domains.ct.kernel.CTSingleSolverDirector DIR]
+    $sys setDirector $dir
+    $dir initialize
+    list [[$dir getCurrentODESolver] getFullName] \
+	    [$dir getCurrentStepSize] \
+	    [$dir getCurrentTime] \
+	    [$dir getFireBeginTime] \
+	    [$dir getInitialStepSize] \
+	    [$dir getErrorTolerance] \
+	    [$dir getMaxIterations] \
+	    [$dir getMaxStepSize] \
+	    [$dir getMinStepSize] \
+	    [$dir getNextIterationTime] \
+	    [$dir getStartTime] \
+	    [$dir getStopTime] \
+	    [$dir getSuggestedNextStepSize] \
+	    [$dir getTimeResolution] \
+	    [$dir getValueResolution]
+} {.System.DIR.CT_Forward_Euler_Solver 0.1 0.0 0.0 0.1 0.0001 20 1.0 1e-05 0.1 0.0 1.0 0.1 1e-10 1e-06}
+
+test CTSingleSolverDirector-2.2 {set Parameters} {
+    #Note: Use above set up.
+    set param [$dir getAttribute ODESolver]
+    set token [java::new ptolemy.data.StringToken ptolemy.domains.ct.kernel.solver.BackwardEulerSolver]
+    $param setToken $token
+    $param parameterChanged [java::null]
+    set param [$dir getAttribute InitialStepSize]
+    $param setExpression 0.5
+    $param parameterChanged [java::null]
+    set param [$dir getAttribute LocalTrancationErrorTolerance]
+    $param setExpression 0.4
+    $param parameterChanged [java::null]
+    set param [$dir getAttribute MaximumIterationsPerStep]
+    $param setExpression 10
+    $param parameterChanged [java::null]
+    set param [$dir getAttribute MaximumStepSize]
+    $param setExpression 0.3
+    $param parameterChanged [java::null]    
+    set param [$dir getAttribute MinimumStepSize]
+    $param setExpression 0.2
+    $param parameterChanged [java::null]
+    set param [$dir getAttribute StartTime]
+    $param setExpression 10.0
+    $param parameterChanged [java::null]
+    set param [$dir getAttribute StopTime]
+    $param setExpression 100.0
+    $param parameterChanged [java::null]
+    set param [$dir getAttribute TimeResolution]
+    $param setExpression 1e-11
+    $param parameterChanged [java::null]
+    set param [$dir getAttribute ConvergeValueResolution]
+    $param setExpression 0.1
+    $param parameterChanged [java::null]
+    $dir prefire
+    list [[$dir getCurrentODESolver] getFullName] \
+	    [$dir getCurrentStepSize] \
+	    [$dir getCurrentTime] \
+	    [$dir getFireBeginTime] \
+	    [$dir getInitialStepSize] \
+	    [$dir getErrorTolerance] \
+	    [$dir getMaxIterations] \
+	    [$dir getMaxStepSize] \
+	    [$dir getMinStepSize] \
+	    [$dir getNextIterationTime] \
+	    [$dir getStartTime] \
+	    [$dir getStopTime] \
+	    [$dir getSuggestedNextStepSize] \
+	    [$dir getTimeResolution] \
+	    [$dir getValueResolution]
+} {.System.DIR.CT_Backward_Euler_Solver 0.1 0.0 0.0 0.5 0.4 10 0.3 0.2 0.1 10.0 100.0 0.1 1e-11 0.1}
+
+test CTSingleSolverDirector-2.3 {sets and gets} {
+    #Note: Use above set up.
+    $dir setCurrentTime 0.1
+    $dir setCurrentStepSize 0.2
+    $dir setStartTime 0.3
+    $dir setStopTime 0.4
+    list [$dir getCurrentTime] \
+	    [$dir getCurrentStepSize] \
+	    [$dir getStartTime] \
+	    [$dir getStopTime] \
+} {0.1 0.2 0.3 0.4}
+
+test CTSingleSolverDirector-2.4 {suggested next step greater than max step} {
+    #Note: Use above set up.
+    # Max step size is 0.3
+    $dir setSuggestedNextStepSize 0.5
+    list [$dir getSuggestedNextStepSize]
+} {0.3}
+
+
+test CTSingleSolverDirector-2.5 {suggested next step less than max step} {
+    #Note: Use above set up.
+    # Max step size is 0.3
+    $dir setSuggestedNextStepSize 0.1
+    list [$dir getSuggestedNextStepSize]
+} {0.1}
+
+######################################################################
+####  Test Breakpoints
+#  
+#test CTSingleSolverDirector-3.1 {register a breakpoint} {     
+#    #Note: Use above set up.
+#    $dir fireAt [java:null] 0.1
+    
+
+
 ######################################################################
 ####  Test Director by execute a demo system
 #  
-test CTSingleSolverDirector-2.1 {Ramp with ForwardEulerSolver} {
+# Note: Not depends on above set up. Can be moved to anywhere.
+test CTSingleSolverDirector-4.1 {Ramp with ForwardEulerSolver} {
     set sys [java::new ptolemy.actor.TypedCompositeActor]
     $sys setName System
     set man [java::new ptolemy.actor.Manager]
@@ -86,17 +201,16 @@ test CTSingleSolverDirector-2.1 {Ramp with ForwardEulerSolver} {
     set integral [java::new ptolemy.domains.ct.lib.CTIntegrator $sys Integ]
     set print [java::new ptolemy.domains.ct.kernel.test.CTTestValueSink\
 	    $sys Sink]
-    set sink [java::new ptolemy.domains.ct.lib.CTPrintln\
-	    $sys pl]
+    #set sink [java::new ptolemy.domains.ct.lib.CTPrintln $sys pl]
     set constout [$const getPort output]
     set intglin [$integral getPort input]
     set intglout [$integral getPort output]
     set printin [$print getPort input]
-    set sinkin [$sink getPort input]
+    #set sinkin [$sink getPort input]
 
     set r1 [$sys connect $constout $intglin R1]
     set r2 [$sys connect $intglout $printin R2]
-    $sinkin link $r2
+    #$sinkin link $r2
     
     set initstate [$integral getAttribute InitialState]
     $initstate setExpression 0.0
@@ -122,7 +236,7 @@ test CTSingleSolverDirector-2.1 {Ramp with ForwardEulerSolver} {
     list [$print isSuccessful]
 } {1}
 
-test CTSingleSolverDirector-2.2 {Ramp with BackwardEulerSolver} {
+test CTSingleSolverDirector-4.2 {Ramp with BackwardEulerSolver} {
     #Note: use above setup. reset parameters.
     set solver [$dir getAttribute ODESolver]
     set token [java::new ptolemy.data.StringToken\
@@ -154,7 +268,7 @@ test CTSingleSolverDirector-2.2 {Ramp with BackwardEulerSolver} {
     list [$print isSuccessful]
 } {1}
 
-test CTSingleSolverDirector-2.3 {Ramp with ExplicitRK23Solver} {
+test CTSingleSolverDirector-4.3 {Ramp with ExplicitRK23Solver} {
     #Note: use above setup. reset parameters.
     set solver [$dir getAttribute ODESolver]
     set token [java::new ptolemy.data.StringToken\
