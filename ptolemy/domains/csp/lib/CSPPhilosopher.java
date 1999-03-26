d514e99513065bf42aa3ec3fb843682a548b4f0a
cxh <cxh>
1999-03-26 23:02:56 +0000

    typo

--- ptolemy/domains/csp/lib/CSPPhilosopher.java
+++ ptolemy/domains/csp/lib/CSPPhilosopher.java
@@ -66,7 +66,7 @@ which controls the distribution of the thinking times. Both these
 rates characterize a uniform distribution between 0 and the rate.
 <p>
 @author Neil Smyth
-@version %Id$
+@version $Id$
  */
 public class CSPPhilosopher extends CSPActor {
 
