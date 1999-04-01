ce6e09614b3dfa749e4d1b682a00989f0f22cf3f
davisj <davisj>
1999-04-01 19:35:16 +0000

    Update rating to yellow based on 12/8/99 code review.

--- ptolemy/domains/csp/kernel/ConditionalReceive.java
+++ ptolemy/domains/csp/kernel/ConditionalReceive.java
@@ -24,8 +24,8 @@
                                         PT_COPYRIGHT_VERSION_2
                                         COPYRIGHTENDKEY
 
-@ProposedRating Red (nsmyth@eecs.berkeley.edu)
-@AcceptedRating Red (cxh@eecs.berkeley.edu)
+@ProposedRating Yellow (nsmyth@eecs.berkeley.edu)
+@AcceptedRating Yellow (davisj@eecs.berkeley.edu)
 */
 
 package ptolemy.domains.csp.kernel;
