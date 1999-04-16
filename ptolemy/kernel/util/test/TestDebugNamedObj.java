68c52715348503b479012b1e477458a004c56aaf
cxh <cxh>
1999-04-16 21:27:36 +0000

    Added StreamListener.tcl and TestDebugNamedObj.java

---
+++ ptolemy/kernel/util/test/TestDebugNamedObj.java
@@ -0,0 +1,66 @@
+/* For Testing _debug in NamedObj
+
+ Copyright (c) 1999 The Regents of the University of California.
+ All rights reserved.
+ Permission is hereby granted, without written agreement and without
+ license or royalty fees, to use, copy, modify, and distribute this
+ software and its documentation for any purpose, provided that the above
+ copyright notice and the following two paragraphs appear in all copies
+ of this software.
+
+ IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+ FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+ ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+ THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+ SUCH DAMAGE.
+
+ THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+ INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+ PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+ CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ ENHANCEMENTS, OR MODIFICATIONS.
+
+                                        PT_COPYRIGHT_VERSION_2
+                                        COPYRIGHTENDKEY
+
+@ProposedRating Green (cxh@eecs.berkeley.edu)
+@AcceptedRating Red (cxh@eecs.berkeley.edu)
+*/
+
+package ptolemy.kernel.util.test;
+
+import ptolemy.kernel.util.*;
+
+//////////////////////////////////////////////////////////////////////////
+//// TestDebugNamedObj
+
+/**
+TestDebugNamedObj is like NamedObj, except it has a debug() method that
+calls _debug().
+@author Christopher Hylands
+@version $Id$
+*/
+public class TestDebugNamedObj extends NamedObj {
+
+    /** Construct an object in the default workspace with an empty string
+     *  as its name. The object is added to the list of objects in
+     *  the workspace. Increment the version number of the workspace.
+     */
+    public TestDebugNamedObj() {
+        super(_defaultWorkspace, "");
+    }
+
+    /** Construct an object in the default workspace with an empty string
+     *  as its name. The object is added to the list of objects in
+     *  the workspace. Increment the version number of the workspace.
+     */
+    public TestDebugNamedObj(String name) {
+        super(_defaultWorkspace, name);
+    }
+
+    /** Pass a message string to the debugListeners */
+    public debug(String message) {
+        _debug(message);
+    }
+}
