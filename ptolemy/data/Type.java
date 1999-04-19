c6beb59017e1578dd78086b05d3f330c661706b7
neuendor <neuendor>
1999-04-19 17:21:12 +0000

    Created

---
+++ ptolemy/data/Type.java
@@ -0,0 +1,67 @@
+/** An Interface representing the Type of an object.
+
+ Copyright (c) 1997-1999 The Regents of the University of California.
+ All rights reserved.
+ Permission is hereby granted, without written agreement and without
+ license or royalty fees, to use, copy, modify, and distribute this
+ software and its documentation for any purpose, provided that the above
+ copyright notice and the following two paragraphs appear in all copies
+ of this software.
+
+ IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+ FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+ ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+ THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+ SUCH DAMAGE.
+
+ THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+ INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+ PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+ CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ ENHANCEMENTS, OR MODIFICATIONS.
+
+                                        PT_COPYRIGHT_VERSION_2
+                                        COPYRIGHTENDKEY
+
+@ProposedRating Red (neuendor@eecs.berkeley.edu)
+@AcceptedRating Red (cxh@eecs.berkeley.edu)
+
+*/
+
+package ptolemy.data;
+
+import ptolemy.graph.InequalityTerm;
+import ptolemy.graph.Inequality;	/* Needed for javadoc */ 
+import ptolemy.kernel.util.IllegalActionException;
+import java.util.Enumeration;
+
+//////////////////////////////////////////////////////////////////////////
+//// Type
+/**
+An interface representing the type of an object.  
+
+@author Steve Neuendorffer
+$Id$
+
+*/
+
+public interface Type
+{
+    /** Return true if the given type is equal to this type.   In other words, 
+     *  an object with this type can be expressed as an object of Type t with 
+     *  no conversion.
+     */
+    public boolean isEqualTo(Type t);
+
+    /** Return true if this type can be converted into an object of the given
+     *  type.
+     */
+    public boolean isConvertibleTo(Type t);
+
+    /** Return true if the given type can be converted into an object of this
+     *  type.
+     */
+    public boolean isConvertibleFrom(Type t);
+}
+
