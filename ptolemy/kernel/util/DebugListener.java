3b386048b8e3d4acc2d29940102e4b9991fa067f
cxh <cxh>
1999-04-29 23:01:51 +0000

    Updated to Green

--- ptolemy/kernel/util/DebugListener.java
+++ ptolemy/kernel/util/DebugListener.java
@@ -24,8 +24,8 @@
                                         PT_COPYRIGHT_VERSION_2
                                         COPYRIGHTENDKEY
 
-@ProposedRating Yellow (eal@eecs.berkeley.edu)
-@AcceptedRating Red (cxh@eecs.berkeley.edu)
+@ProposedRating Green (eal@eecs.berkeley.edu)
+@AcceptedRating Green (cxh@eecs.berkeley.edu)
 */
 
 package ptolemy.kernel.util;
