4c6534d3438e961d9924e3af25f957a6fa438647
cxh <cxh>
1999-05-06 17:57:55 +0000

    Removed tybin_install

--- mk/ptcommon.mk
+++ mk/ptcommon.mk
@@ -168,30 +168,6 @@ fast:
 		CLASSPATH="$(CLASSPATH)$(AUXCLASSPATH)" $(JAVAC) $(JFLAGS) *.java; \
 	fi
 
-# Install links for scripts in $PTOLEMY/bin and $TYCHO/bin
-tybin_install:
-	-if [ "x$(EXTRA_SRCS)" != "x" ]; then \
-		set $(EXTRA_SRCS); \
-		for f do \
-			if [ -w "$(PTOLEMY)/bin" ]; then \
-				echo "Installing $$f in $(PTOLEMY)/bin";\
-				rm -f $(PTOLEMY)/bin/$$f; \
-				ln $$f $(PTOLEMY)/bin; \
-			fi; \
-			if [ -w "$(PTOLEMY)/tycho/bin" ]; then \
-				echo "Installing $$f in $(PTOLEMY)/tycho/bin";\
-				rm -f $(PTOLEMY)/tycho/bin/$$f; \
-				ln $$f $(PTOLEMY)/tycho/bin; \
-			else \
-				if [ -w "$(TYCHO)/bin" ]; then \
-					echo "Installing $$f in $(TYCHO)/bin";\
-					rm -f $(TYCHO)/bin/$$f; \
-					ln $$f $(TYCHO)/bin; \
-				fi; \
-			fi; \
-		done ; \
-	fi
-
 # "make sources" will do SCCS get on anything where SCCS file is newer.
 sources::	$(SRCS) $(EXTRA_SRCS) $(HDRS) $(MISC_FILES) makefile
 	@if [ "x$(DIRS)" != "x" ]; then \
