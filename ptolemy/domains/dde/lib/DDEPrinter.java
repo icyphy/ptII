838393cf71a75b9d5732ac37be15fc7c38532a74
davisj <davisj>
1999-05-07 22:44:44 +0000

    Initial file.

---
+++ ptolemy/domains/dde/lib/DDEPrinter.java
@@ -0,0 +1,108 @@
+/* 
+
+ Copyright (c) 1997-1999 The Regents of the University of California.
+ All rights reserved.
+ Permission is hereby granted, without written agreement and without
+ license or royalty fees, to use, copy, modify, and distribute this
+ software and its documentation for any purpose, provided that the above
+ copyright notice and the following two paragraphs appear in all copies
+ of this software.
+
+ IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+ FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+ ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+ THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+ SUCH DAMAGE.
+
+ THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+ INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+ PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+ CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ ENHANCEMENTS, OR MODIFICATIONS.
+
+                                        PT_COPYRIGHT_VERSION_2
+                                        COPYRIGHTENDKEY
+
+@ProposedRating Red (davisj@eecs.berkeley.edu)
+
+*/
+
+package ptolemy.domains.dde.lib;
+
+import ptolemy.domains.dde.kernel.*;
+import ptolemy.kernel.util.*;
+import ptolemy.actor.*;
+import ptolemy.data.*;
+import collections.LinkedList;
+import java.util.Enumeration;
+
+//////////////////////////////////////////////////////////////////////////
+//// DDEPrinter
+/** 
+
+
+@author John S. Davis II
+@version $Id$
+*/
+public class DDEPrinter extends DDEActor {
+
+    /** 
+     */
+    public DDEPrinter(TypedCompositeActor container, String name)
+            throws IllegalActionException, NameDuplicationException {
+        super(container, name);
+        
+        input = new TypedIOPort( this, "input", true, false );
+	input.setTypeEquals(Token.class);
+        input.setMultiport(true);
+    }
+ 
+    ///////////////////////////////////////////////////////////////////
+    ////                        public variables                   ////
+   
+    public TypedIOPort input;
+
+    ///////////////////////////////////////////////////////////////////
+    ////                         public methods                    ////
+
+    /** 
+     */
+    public void fire() throws IllegalActionException {
+	getNextToken();
+        double time = getCurrentTime();
+	System.out.println("\n***DDEPrinter at time = " 
+                + time + " ***");
+    }
+    
+    /** 
+     */
+    public void wrapup() throws IllegalActionException {
+        System.out.println("\nIt is finished.\n");
+        super.wrapup();
+    }
+
+    ///////////////////////////////////////////////////////////////////
+    ////                        private variables                  ////
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
