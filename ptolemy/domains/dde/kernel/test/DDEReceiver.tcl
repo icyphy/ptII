2c802608315fbd93268c005888c9b1a3cc341d37
davisj <davisj>
1999-05-10 04:04:54 +0000

    Have one passing test in this file (2.1). All 22 of 22 tests in the directory pass.

--- ptolemy/domains/dde/kernel/test/DDEReceiver.tcl
+++ ptolemy/domains/dde/kernel/test/DDEReceiver.tcl
@@ -49,245 +49,78 @@ if {[string compare test [info procs test]] == 1} then {
 
 ######################################################################
 ####
+# Global Variables 
+set globalEndTimeRcvr [java::new ptolemy.domains.dde.kernel.TimedQueueReceiver]
+set globalEndTime [java::field $globalEndTimeRcvr INACTIVE]
+set globalIgnoreTimeRcvr [java::new ptolemy.domains.dde.kernel.TimedQueueReceiver]
+set globalIgnoreTime [java::field $globalIgnoreTimeRcvr IGNORE]
+
+######################################################################
+####
 #
-test DDEReceiver-2.1 {get(), single arg put(), check _rcvrTime and _lastTime} {
+test DDEReceiver-2.1 {Send three tokens between two actors} {
     set wspc [java::new ptolemy.kernel.util.Workspace]
     set toplevel [java::new ptolemy.actor.TypedCompositeActor $wspc]
-    set dir [java::new ptolemy.domains.dde.kernel.DDEDirector $topLevel "director"]
+    set dir [java::new ptolemy.domains.dde.kernel.DDEDirector $toplevel "director"]
     set mgr [java::new ptolemy.actor.Manager $wspc "manager"]
+    $toplevel setDirector $dir
     $toplevel setManager $mgr
     
-    set actorRcvr [java::new ptolemy.actor.TypedAtomicActor $toplevel "actorRcvr"]
-    set actorSend [java::new ptolemy.actor.TypedAtomicActor $toplevel "actorSend"]
-    set ioprcvr [java::new ptolemy.actor.TypedIOPort $actorRcvr  "port"]
-    set iopsend [java::new ptolemy.actor.TypedIOPort $actorSend "port"]
-    $ioprcvr setInput true
-    $iopsend setOutput true
+    set actorRcvr [java::new ptolemy.domains.dde.kernel.test.DDEGetNToken $toplevel "actorRcvr" 3]
+    set actorSend [java::new ptolemy.domains.dde.kernel.test.DDEPutToken $toplevel "actorSend" 3]
+
+    set tok1 [java::new ptolemy.data.Token]
+    $actorSend setToken $tok1 5.0 0 
+    $actorSend setToken $tok1 15.0 1
+    $actorSend setToken $tok1 25.0 2
+
+    set ioprcvr [$actorRcvr getPort "input"]
+    set iopsend [$actorSend getPort "output"]
     set rel [$toplevel connect $ioprcvr $iopsend "rel"]
-    $actorRcvr createReceivers
-    $actorSend createReceivers
-    set rcvrkeeper [java::new ptolemy.domains.dde.kernel.TimeKeeper $actorRcvr]
-    set sendkeeper [java::new ptolemy.domains.dde.kernel.TimeKeeper $actorSend]
 
-    set rcvrs [$ioprcvr getReceivers]
-    set odr [java::cast ptolemy.domains.dde.kernel.DDEReceiver [$rcvrs get {0 0}]]
+    $mgr run
 
-    $odr setReceivingTimeKeeper $rcvrkeeper
-    $odr setSendingTimeKeeper $sendkeeper
+    set time0 [$actorRcvr getThreadTime 0]
+    set time1 [$actorRcvr getThreadTime 1]
+    set time2 [$actorRcvr getThreadTime 2]
 
-    set token0 [java::new ptolemy.data.Token]
-    set token1 [java::new ptolemy.data.Token]
-    set token2 [java::new ptolemy.data.Token]
+    list $time0 $time1 $time2
 
-    $sendkeeper setCurrentTime 12.5
-    $odr put $token0
-    set rcvrTime0 [$odr getRcvrTime]
-    set lastTime0 [$odr getLastTime]
-
-    $sendkeeper setDelayTime 12.5
-    $odr put $token1
-    set rcvrTime1 [$odr getRcvrTime]
-    set lastTime1 [$odr getLastTime]
-
-    set outToken0 [$odr get] 
-    set rcvrTime2 [$odr getRcvrTime]
-
-    list $rcvrTime0 $lastTime0 $rcvrTime1 $lastTime1 $rcvrTime2
-} {12.5 12.5 12.5 25.0 25.0}
+} {5.0 15.0 25.0}
 
 ######################################################################
 ####
 #
-test DDEReceiver-2.2 {Put delayed event into non-empty queue; \
-	check rcvrTime and lastTime} {
-    set actorRcvr [java::new ptolemy.actor.TypedAtomicActor]
-    set actorSend [java::new ptolemy.actor.TypedAtomicActor]
-    set iop [java::new ptolemy.actor.TypedIOPort $actorRcvr \
-	    "port"]
-    set odr [java::new ptolemy.domains.dde.kernel.DDEReceiver $iop]
-    set rcvrkeeper [java::new ptolemy.domains.dde.kernel.TimeKeeper \
-	    $actorRcvr]
-    set sendkeeper [java::new ptolemy.domains.dde.kernel.TimeKeeper \
-	    $actorSend]
-    $odr setReceivingTimeKeeper $rcvrkeeper
-    $odr setSendingTimeKeeper $sendkeeper
-    set t1 [java::new ptolemy.data.Token]
-    set t2 [java::new ptolemy.data.Token]
-    $odr put $t1 
-    $odr put $t2 5.0
-    list [$odr getRcvrTime] [$odr getLastTime]
-} {0.0 5.0}
+test DDEReceiver-2.2 {Send a real token, an ignore token and a real token.} {
+    set wspc [java::new ptolemy.kernel.util.Workspace]
+    set toplevel [java::new ptolemy.actor.TypedCompositeActor $wspc]
+    set dir [java::new ptolemy.domains.dde.kernel.DDEDirector $toplevel "director"]
+    set mgr [java::new ptolemy.actor.Manager $wspc "manager"]
+    $toplevel setDirector $dir
+    $toplevel setManager $mgr
+    
+    set actorRcvr [java::new ptolemy.domains.dde.kernel.test.DDEGetNToken $toplevel "actorRcvr" 2]
+    set actorSend [java::new ptolemy.domains.dde.kernel.test.DDEPutToken $toplevel "actorSend" 3]
 
-######################################################################
-####
-#
-test DDEReceiver-3.1 {Set event time = -1.0  and place into \
-	empty queue; check rcvrTime and lastTime} {
-    set actorRcvr [java::new ptolemy.actor.TypedAtomicActor]
-    set actorSend [java::new ptolemy.actor.TypedAtomicActor]
-    set iop [java::new ptolemy.actor.TypedIOPort $actorRcvr \
-	    "port"]
-    set odr [java::new ptolemy.domains.dde.kernel.DDEReceiver $iop]
-    set rcvrkeeper [java::new ptolemy.domains.dde.kernel.TimeKeeper \
-	    $actorRcvr]
-    set sendkeeper [java::new ptolemy.domains.dde.kernel.TimeKeeper \
-	    $actorSend]
-    $odr setReceivingTimeKeeper $rcvrkeeper
-    $odr setSendingTimeKeeper $sendkeeper
+    set tok1 [java::new ptolemy.data.Token]
+    $actorSend setToken $tok1 5.0 0 
+    $actorSend setToken $tok1 $globalIgnoreTime 1
+    $actorSend setToken $tok1 25.0 2
 
-    $sendkeeper setCurrentTime -1.0
-    $sendkeeper setDelayTime 5.0
+    set ioprcvr [$actorRcvr getPort "input"]
+    set iopsend [$actorSend getPort "output"]
+    set rel [$toplevel connect $ioprcvr $iopsend "rel"]
 
-    $odr put $t1 
-    list [$odr getRcvrTime] [$odr getLastTime]
-} {-1.0 -1.0}
+    $mgr run
 
-######################################################################
-####
-#
-test DDEReceiver-3.2 {Set negative delay time} {
-    set wkspc [java::new ptolemy.kernel.util.Workspace]
-    set comp [java::new ptolemy.actor.TypedCompositeActor $wkspc]
-    set actorRcvr [java::new ptolemy.actor.TypedAtomicActor \
-	    $comp "Receiver"]
-    set actorSend [java::new ptolemy.actor.TypedAtomicActor \
-	    $comp "Sender"]
-    set iop [java::new ptolemy.actor.TypedIOPort $actorRcvr \
-	    "port"]
-    set odr [java::new ptolemy.domains.dde.kernel.DDEReceiver $iop]
-    set rcvrkeeper [java::new ptolemy.domains.dde.kernel.TimeKeeper \
-	    $actorRcvr]
-    set sendkeeper [java::new ptolemy.domains.dde.kernel.TimeKeeper \
-	    $actorSend]
-    $odr setReceivingTimeKeeper $rcvrkeeper
-    $odr setSendingTimeKeeper $sendkeeper
+    set time0 [$actorRcvr getThreadTime 0]
 
-    catch {$sendkeeper setDelayTime -1.0} msg
-    list $msg 
+    list $time0
 
-} {{ptolemy.kernel.util.IllegalActionException: Sender - Attempt to set negative delay time.}}
+} {5.0}
 
-######################################################################
-####
-#
-test DDEReceiver-3.3 {Set negative current time} {
-    set wkspc [java::new ptolemy.kernel.util.Workspace]
-    set comp [java::new ptolemy.actor.TypedCompositeActor $wkspc]
-    set actorRcvr [java::new ptolemy.actor.TypedAtomicActor \
-	    $comp "Receiver"]
-    set actorSend [java::new ptolemy.actor.TypedAtomicActor \
-	    $comp "Sender"]
-    set iop [java::new ptolemy.actor.TypedIOPort $actorRcvr \
-	    "port"]
-    set odr [java::new ptolemy.domains.dde.kernel.DDEReceiver $iop]
-    set rcvrkeeper [java::new ptolemy.domains.dde.kernel.TimeKeeper \
-	    $actorRcvr]
-    set sendkeeper [java::new ptolemy.domains.dde.kernel.TimeKeeper \
-	    $actorSend]
-    $odr setReceivingTimeKeeper $rcvrkeeper
-    $odr setSendingTimeKeeper $sendkeeper
 
-    catch {$sendkeeper setCurrentTime -1.0} msg1
-    catch {$sendkeeper setCurrentTime -3.0} msg2
-    list $msg1 $msg2
 
-} {{} {java.lang.IllegalArgumentException: Sender - Attempt to set current time in the past.}}
-
-######################################################################
-####
-#
-test DDEReceiver-4.1 {Three gets followed by three puts} {
-    set actorRcvr [java::new ptolemy.actor.TypedAtomicActor]
-    set actorSend [java::new ptolemy.actor.TypedAtomicActor]
-    set iop [java::new ptolemy.actor.TypedIOPort $actorRcvr \
-	    "port"]
-    set odr [java::new ptolemy.domains.dde.kernel.DDEReceiver $iop]
-    set rcvrkeeper [java::new ptolemy.domains.dde.kernel.TimeKeeper \
-	    $actorRcvr]
-    set sendkeeper [java::new ptolemy.domains.dde.kernel.TimeKeeper \
-	    $actorSend]
-    $odr setReceivingTimeKeeper $rcvrkeeper
-    $odr setSendingTimeKeeper $sendkeeper
-
-    set t1 [java::new ptolemy.data.Token]
-    set t2 [java::new ptolemy.data.Token]
-    set t3 [java::new ptolemy.data.Token]
-    $odr put $t1 0.5
-    $odr put $t2 5.0
-    $odr put $t3 12.5
-    set t4 [$odr get]
-    set t5 [$odr get]
-    set t6 [$odr get]
-    list [expr {$t1 == $t4} ] [expr {$t2 == $t5} ] [expr {$t3 == $t6} ] \
-	    [$odr getLastTime] [$odr getRcvrTime]
-} {1 1 1 12.5 12.5}
-
-######################################################################
-####
-#
-test DDEReceiver-4.2 {Insert events in wrong order} {
-    set actorRcvr [java::new ptolemy.actor.TypedAtomicActor]
-    set actorSend [java::new ptolemy.actor.TypedAtomicActor]
-    set iop [java::new ptolemy.actor.TypedIOPort $actorRcvr \
-	    "port"]
-    set odr [java::new ptolemy.domains.dde.kernel.DDEReceiver $iop]
-    set rcvrkeeper [java::new ptolemy.domains.dde.kernel.TimeKeeper \
-	    $actorRcvr]
-    set sendkeeper [java::new ptolemy.domains.dde.kernel.TimeKeeper \
-	    $actorSend]
-    $odr setReceivingTimeKeeper $rcvrkeeper
-    $odr setSendingTimeKeeper $sendkeeper
-
-    $odr put $t1 5.0
-    catch {$odr put $t2 0.5} msg
-    list $msg
-} {{java.lang.IllegalArgumentException:  - Attempt to set current time in the past.}}
-
-######################################################################
-####
-#
-test DDEReceiver-5.1 {hasToken() - tokens available} {
-    set actorRcvr [java::new ptolemy.actor.TypedAtomicActor]
-    set actorSend [java::new ptolemy.actor.TypedAtomicActor]
-    set iop [java::new ptolemy.actor.TypedIOPort $actorRcvr \
-	    "port"]
-    set odr [java::new ptolemy.domains.dde.kernel.DDEReceiver $iop]
-    set rcvrkeeper [java::new ptolemy.domains.dde.kernel.TimeKeeper \
-	    $actorRcvr]
-    set sendkeeper [java::new ptolemy.domains.dde.kernel.TimeKeeper \
-	    $actorSend]
-    $odr setReceivingTimeKeeper $rcvrkeeper
-    $odr setSendingTimeKeeper $sendkeeper
-
-    set token [java::new ptolemy.data.Token]
-    $odr put $token 5.0
-    set hastoken [$odr hasToken]
-
-    list $hastoken
-} {1}
-
-######################################################################
-####
-# FIXME
-test DDEReceiver-5.1 {hasToken() - tokens available} {
-    set actorRcvr [java::new ptolemy.actor.TypedAtomicActor]
-    set actorSend [java::new ptolemy.actor.TypedAtomicActor]
-    set iop [java::new ptolemy.actor.TypedIOPort $actorRcvr \
-	    "port"]
-    set odr [java::new ptolemy.domains.dde.kernel.DDEReceiver $iop]
-    set rcvrkeeper [java::new ptolemy.domains.dde.kernel.TimeKeeper \
-	    $actorRcvr]
-    set sendkeeper [java::new ptolemy.domains.dde.kernel.TimeKeeper \
-	    $actorSend]
-    $odr setReceivingTimeKeeper $rcvrkeeper
-    $odr setSendingTimeKeeper $sendkeeper
-
-    set token [java::new ptolemy.data.Token]
-    $odr put $token 5.0
-    set hastoken [$odr hasToken]
-
-    list $hastoken
-} {1}
 
 
 
