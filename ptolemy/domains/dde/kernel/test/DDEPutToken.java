8c147d48c19178fbdcbf1979e8d87860fc7c9a3d
davisj <davisj>
1999-05-12 23:26:03 +0000

    Minor changes to comments.

--- ptolemy/domains/dde/kernel/test/DDEPutToken.java
+++ ptolemy/domains/dde/kernel/test/DDEPutToken.java
@@ -79,8 +79,10 @@ public class DDEPutToken extends DDEPut {
 	    for( int i = 0; i < rcvrs.length; i++ ) {
 		for( int j = 0; j < rcvrs[i].length; j++ ) {
 		    DDEReceiver rcvr = (DDEReceiver)rcvrs[i][j];
-		    // System.out.println("DDEPutToken receiver["+i+"]["+j+
-		    // "]; cnt = "+cnt);
+		    /*
+		    System.out.println("DDEPutToken receiver["+i+"]["+j+
+		    "]; cnt = "+cnt);
+		    */
                     if( _oneArg ) {
                         rcvr.put( _tokens[cnt] );
                     } else {
