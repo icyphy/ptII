2c7970ac237b37633ab8a2145dcb0cad7a7b7a7e
cxh <cxh>
1999-05-14 03:11:05 +0000

    Removed demos

--- mk/ptdir.mk
+++ mk/ptdir.mk
@@ -32,7 +32,7 @@
 
 all makefiles install fast clean sources depend distclean checkjunk sccsinfo \
 sccsclean itcldocs javadocs extraclean tests weblint idocs dists jsall \
-jsrestore jars demos demo:
+jsrestore jars demo:
 	@for x in $(DIRS); do \
 	    if [ -w $$x ] ; then \
 		( cd $$x ; \
