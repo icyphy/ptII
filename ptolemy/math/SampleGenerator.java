b88af0af0db8085e683e50618a5c70ffb4e7b17e
cxh <cxh>
1999-05-25 02:16:27 +0000

    Reindented with jindent

--- ptolemy/math/SampleGenerator.java
+++ ptolemy/math/SampleGenerator.java
@@ -34,9 +34,9 @@ package ptolemy.math;
 
 public interface SampleGenerator {
 
-  /** Generate a sample of the waveform at a specified time.
-   *  @param time The time at which to sample the waveform.
-   *  @return A sample of the waveform.
-   */
-  public double sampleAt(double time);
+    /** Generate a sample of the waveform at a specified time.
+     *  @param time The time at which to sample the waveform.
+     *  @return A sample of the waveform.
+     */
+    public double sampleAt(double time);
 }
