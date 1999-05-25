c1b7704a703735dafd18e6d4e5e682500bd0059b
cxh <cxh>
1999-05-25 23:44:34 +0000

    Split long line, added space around =

--- ptolemy/domains/pn/demo/Interleave/PNInterleave.java
+++ ptolemy/domains/pn/demo/Interleave/PNInterleave.java
@@ -72,10 +72,11 @@ public class PNInterleave extends AtomicActor{
         Token data;
 	while (true) {
 	    int width = _input.getWidth();
-	    for (int i=0; i<width; i++) {
+	    for (int i = 0; i<width; i++) {
 		data = _input.get(i);
 		_output.broadcast(data);
-                System.out.println("Interleave writes "+((IntToken)data).intValue());
+                System.out.println("Interleave writes " +
+                        ((IntToken)data).intValue());
 	    }
 	}
     }
