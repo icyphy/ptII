55cddae50922882f5faa7cee2974a88c1eec3fed
cxh <cxh>
1999-05-25 01:14:23 +0000

    Spelling: finshed

--- ptolemy/domains/pn/demo/RunLength/ThreadDemo.java
+++ ptolemy/domains/pn/demo/RunLength/ThreadDemo.java
@@ -474,7 +474,7 @@ public class ThreadDemo {
             _currentElement[id] = element;
         }
 
-        /** Respond to a process finshed event.
+        /** Respond to a process finished event.
          */
         public void processFinished(PNProcessEvent event) {
             processStateChanged(event);
