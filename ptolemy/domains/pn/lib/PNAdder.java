614f7574be65b42804a5b2c6475c35af5f232772
cxh <cxh>
1999-05-27 01:27:36 +0000

    Split long lines, added space around =

--- ptolemy/domains/pn/lib/PNAdder.java
+++ ptolemy/domains/pn/lib/PNAdder.java
@@ -68,10 +68,13 @@ public class PNAdder extends AtomicActor {
         double[][] sum = ((DoubleMatrixToken)_input.get(0)).doubleMatrix();
 	for (int i = 1; i < _input.getWidth(); i++) {
 	    //FIXME: This is a hack. Generalize it.
-	    double[][] data =((DoubleMatrixToken)_input.get(i)).doubleMatrix();
+	    double[][] data =
+                ((DoubleMatrixToken)_input.get(i)).doubleMatrix();
 	    //FIXME: Take care. Perhaps throw an exception.
 	    if (sum.length != data.length && sum[0].length != data[0].length) {
-		System.out.println("Error: Lengths in adder not equal: length 0 ="+data[0].length+"length 1 = "+sum[0].length);
+		System.out.println("Error: Lengths in adder not equal: " +
+                        "length 0 = " + data[0].length + "length 1 = " + 
+                        sum[0].length);
 	    }
 	    for (int j = 0; j < sum.length; j++) {
 		for (int k = 0; k<sum[j].length; k++) {
@@ -83,7 +86,7 @@ public class PNAdder extends AtomicActor {
 	_output.broadcast(result);
 	//debug
 	//double[][] debug = result.doubleMatrix();
-	//for (int j =0; j < debug[0].length; j++) {
+	//for (int j = 0; j < debug[0].length; j++) {
 	//System.out.println(getName()+": debug[0]["+j+"] = "+debug[0][j]);
 	//writeTo(_output, result);
     }
