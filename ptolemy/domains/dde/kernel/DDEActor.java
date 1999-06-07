38ae0a7a16d3ad08d9aa56bb2d3b2dc71890904a
davisj <davisj>
1999-06-07 21:10:44 +0000

    Cleaned up code.

--- ptolemy/domains/dde/kernel/DDEActor.java
+++ ptolemy/domains/dde/kernel/DDEActor.java
@@ -1,4 +1,4 @@
-/* The base class for DDE actors.
+/* An optional base class for DDE actors.
 
  Copyright (c) 1997-1999 The Regents of the University of California.
  All rights reserved.
@@ -40,23 +40,24 @@ import collections.LinkedList;
 //////////////////////////////////////////////////////////////////////////
 //// DDEActor
 /**
-The base class for DDE actors. DDEActors are intended to execute as
-autonomous processes that maintain a distributed notion of time. In
-an DDE model, each actor is controlled by a unique DDEThread. Each
-DDEThread maintains its actor's local notion of time. Local time
-information is dependent on the time stamps associated with tokens
-that are consumed by an actor. More precisely, an actor's local
-notion of time is equivalent to the maximum time stamp of all tokens
-that the actor has consumed. Constraints on the consumption of tokens
-are described in the documentation for DDEThread. Note that consumed
-tokens may include NullTokens. A NullToken is a subclass of Token
-that is communicated solely for the purpose of advancing the local
-notion of time of the actor that receives the NullToken.
+An optional base class for DDE actors. DDEActors are intended to 
+execute as autonomous processes that maintain a distributed notion 
+of time. In an DDE model, each actor is controlled by a unique 
+DDEThread. Each DDEThread maintains its actor's local notion of 
+time. Local time information is dependent on the time stamps 
+associated with tokens that are consumed by an actor. More 
+precisely, an actor's local notion of time is equivalent to the 
+maximum time stamp of all tokens that the actor has consumed. 
+Constraints on the consumption of tokens are described in the 
+documentation for DDEThread. Note that consumed tokens may include 
+NullTokens. A NullToken is a subclass of Token that is communicated 
+solely for the purpose of advancing the local notion of time of the 
+actor that consumes the NullToken.
 <P>
 The DDE model of computation supports typed, polymorphic actors and
 does not require this base class for implementation; this class is
-purely optional. Nevertheless, this class provides useful syntactic
-conveniences for developing DDE models.
+purely optional. This class provides convenient syntactic shortcuts
+for developing actors that operate according to DDE semantics.
 
 @author John S. Davis II
 @version $Id$
@@ -65,21 +66,24 @@ conveniences for developing DDE models.
 */
 public class DDEActor extends TypedAtomicActor {
 
-    /** Construct an DDEActor with no container and a name that
-     *  is an empty.
+    /** Construct a DDEActor with no container and a name that
+     *  is an empty string.
      */
     public DDEActor() {
         super();
     }
 
-    /** Construct an DDEActor with the specified workspace and no name.
+    /** Construct a DDEActor with the specified workspace and a
+     *  name that is an empty string.
      * @param workspace The workspace for this DDEActor.
      */
     public DDEActor(Workspace workspace) {
 	super(workspace);
     }
 
-    /** Construct an DDEActor with the specified container and name.
+    /** Construct a DDEActor with the specified container and name.
+     *  The name must be unique with respect to the container or an
+     *  exception is thrown.
      * @param container The container of this DDEActor.
      * @param name The name of this DDEActor.
      * @exception IllegalActionException If the constructor of the
@@ -95,11 +99,12 @@ public class DDEActor extends TypedAtomicActor {
     ///////////////////////////////////////////////////////////////////
     ////                         public methods                    ////
 
-    /** Return the current time of this actor. The current time is
-     *  equal to the time stamp associated with the token most recently
-     *  consumed by one of the receivers contained by this DDEActor.
-     *  If the current thread accessing this method is not an instance
-     *  of DDEThread, then return the cached current time valu.
+    /** Return the current time of this actor. The current time 
+     *  is equal to the time stamp associated with the token most 
+     *  recently consumed by any of the receivers contained by 
+     *  this DDEActor. If the current thread accessing this method 
+     *  is not an instance of DDEThread, then return the cached 
+     *  current time value.
      * @return The current time of this DDEActor.
      */
     public double getCurrentTime() {
@@ -115,10 +120,10 @@ public class DDEActor extends TypedAtomicActor {
     /** Return the last port through which a token was consumed by
      *  this actor. If no tokens have ever been consumed through any
      *  ports of this actr, then return null.
-     * @return IOPort The last port through which a token was
+     * @return TypedIOPort The last port through which a token was
      *  consumed by this actor.
      */
-    public IOPort getLastPort() {
+    public TypedIOPort getLastPort() {
 	return _lastPort;
     }
 
@@ -135,36 +140,26 @@ public class DDEActor extends TypedAtomicActor {
     public Token getNextToken() throws IllegalActionException {
         Token token = _getNextInput();
         if( token instanceof NullToken ) {
-	    /*
-              System.out.println(getName()+": got a NullToken "
-              + "from _getNextInput()");
-	    */
             return getNextToken();
 	}
         return token;
     }
 
-    /** Prepare to cease iterations of this actor. Notify actors which
-     *  are connected downstream of this actor's cessation. Return false
-     *  to indicate that future execution can not occur.
-     * @return False to indicate that future execution can not occur.
-     * @exception IllegalActionException Not thrown in this class. May be
-     *  thrown in derived classes.
-     public boolean postfire() throws IllegalActionException {
-     return false;
-     }
-    */
-
     ///////////////////////////////////////////////////////////////////
     ////                   package friendly methods		   ////
 
-    /** Return a token from the receiver that has the minimum receiver
-     *  time of all receivers contained by this actor. The returned token
-     *  will have the lowest time stamp of all pending tokens for this
-     *  actor. If there exists a set of multiple receivers that share
-     *  a common minimum receiver time, then return the token contained
-     *  by the highest priority receiver within this set. If this actor
+    /** Return a token from the receiver that has the minimum 
+     *  receiver time of all receivers contained by this actor. 
+     *  The returned token will have the lowest time stamp of 
+     *  all pending tokens for this actor. If there exists a 
+     *  set of multiple receivers that share a common minimum 
+     *  receiver time, then return the token contained by the 
+     *  highest priority receiver within this set. If this actor
      *  contains no receivers then return null.
+     *  <P>
+     *  The primary difference between this method and getNextToken() 
+     *  is that this method returns all types of tokens including 
+     *  NullTokens while getNextToken only returns real tokens.
      * @return The token with the smallest time stamp of all tokens
      *  contained by this actor. If multiple tokens share the smallest
      *  time stamp this token will come from the highest priority
@@ -176,7 +171,6 @@ public class DDEActor extends TypedAtomicActor {
      */
     Token _getNextInput() throws IllegalActionException {
 	String calleeName = getName();
-	new FooBar( calleeName, "DDEActor._getNextInput()", "START" );
 	Thread thread = Thread.currentThread();
 	if( thread instanceof DDEThread ) {
 	    TimeKeeper timeKeeper = ((DDEThread)thread).getTimeKeeper();
