70d549a43f92cb0097e2820bcee8332c28473ad0
cxh <cxh>
1999-06-24 18:17:44 +0000

    Changes from eal

--- ptolemy/data/test/BooleanToken.tcl
+++ ptolemy/data/test/BooleanToken.tcl
@@ -94,3 +94,102 @@ test BooleanToken-3.0 {Create an non-empty instance and add it to Strings} {
 
     list [$token6 toString]
 } {{ptolemy.data.StringToken(value is truetrue.....)}}
+
+######################################################################
+####
+# 
+test BooleanToken-4.0 {Test addition of booleans} {
+    set trueToken [java::field ptolemy.data.BooleanToken TRUE]
+    set falseToken [java::field ptolemy.data.BooleanToken FALSE]
+    set r1 [$falseToken add $falseToken]
+    set r2 [$trueToken add $falseToken]
+    set r3 [$falseToken add $trueToken]
+    set r4 [$trueToken add $trueToken]
+    list [$r1 stringValue] [$r2 stringValue] [$r3 stringValue] [$r4 stringValue]
+} {false true true false}
+
+test BooleanToken-4.1 {Test reverse addition of booleans} {
+    set r1 [$falseToken addReverse $falseToken]
+    set r2 [$trueToken addReverse $falseToken]
+    set r3 [$falseToken addReverse $trueToken]
+    set r4 [$trueToken addReverse $trueToken]
+    list [$r1 stringValue] [$r2 stringValue] [$r3 stringValue] [$r4 stringValue]
+} {false true true false}
+
+test BooleanToken-5.0 {Test division of booleans} {
+    set r1 [$falseToken divide $trueToken]
+    set r2 [$trueToken divide $trueToken]
+    list [$r1 stringValue] [$r2 stringValue]
+} {false true}
+
+test BooleanToken-5.1 {Test division by zero} {
+    catch {[$falseToken divide $falseToken]} msg1
+    catch {[$trueToken divide $falseToken]} msg2
+    list $msg1 $msg2
+} {{java.lang.IllegalArgumentException: BooleanToken: division by false-valued token (analogous to division by zero).} {java.lang.IllegalArgumentException: BooleanToken: division by false-valued token (analogous to division by zero).}}
+
+test BooleanToken-5.2 {Test reverse division of booleans} {
+    set r3 [$trueToken divideReverse $falseToken]
+    set r4 [$trueToken divideReverse $trueToken]
+    list [$r1 stringValue] [$r2 stringValue]
+} {false true}
+
+test BooleanToken-5.3 {Test division by zero} {
+    catch {[$falseToken divideReverse $falseToken]} msg1
+    catch {[$falseToken divideReverse $trueToken]} msg2
+    list $msg1 $msg2
+} {{java.lang.IllegalArgumentException: BooleanToken: division by false-valued token (analogous to division by zero).} {java.lang.IllegalArgumentException: BooleanToken: division by false-valued token (analogous to division by zero).}}
+
+
+test BooleanToken-6.0 {Test equality test} {
+    set r1 [$falseToken isEqualTo $falseToken]
+    set r2 [$trueToken isEqualTo $falseToken]
+    set r3 [$falseToken isEqualTo $trueToken]
+    set r4 [$trueToken isEqualTo $trueToken]
+    list [$r1 stringValue] [$r2 stringValue] [$r3 stringValue] [$r4 stringValue]
+} {true false false true}
+
+
+test BooleanToken-7.0 {Test multiplication} {
+    set r1 [$falseToken multiply $falseToken]
+    set r2 [$trueToken multiply $falseToken]
+    set r3 [$falseToken multiply $trueToken]
+    set r4 [$trueToken multiply $trueToken]
+    list [$r1 stringValue] [$r2 stringValue] [$r3 stringValue] [$r4 stringValue]
+} {false false false true}
+
+test BooleanToken-7.1 {Test reverse multiplication} {
+    set r1 [$falseToken multiplyReverse $falseToken]
+    set r2 [$trueToken multiplyReverse $falseToken]
+    set r3 [$falseToken multiplyReverse $trueToken]
+    set r4 [$trueToken multiplyReverse $trueToken]
+    list [$r1 stringValue] [$r2 stringValue] [$r3 stringValue] [$r4 stringValue]
+} {false false false true}
+
+test BooleanToken-8.0 {Test not} {
+    set r1 [$falseToken not]
+    set r2 [$trueToken not]
+    list [$r1 stringValue] [$r2 stringValue]
+} {true false}
+
+test BooleanToken-9.0 {Test identities} {
+    set r1 [$falseToken one]
+    set r2 [$trueToken zero]
+    list [$r1 stringValue] [$r2 stringValue]
+} {true false}
+
+test BooleanToken-10.0 {Test subtraction of booleans} {
+    set r1 [$falseToken subtract $falseToken]
+    set r2 [$trueToken subtract $falseToken]
+    set r3 [$falseToken subtract $trueToken]
+    set r4 [$trueToken subtract $trueToken]
+    list [$r1 stringValue] [$r2 stringValue] [$r3 stringValue] [$r4 stringValue]
+} {false true true false}
+
+test BooleanToken-4.1 {Test reverse subtraction of booleans} {
+    set r1 [$falseToken subtractReverse $falseToken]
+    set r2 [$trueToken subtractReverse $falseToken]
+    set r3 [$falseToken subtractReverse $trueToken]
+    set r4 [$trueToken subtractReverse $trueToken]
+    list [$r1 stringValue] [$r2 stringValue] [$r3 stringValue] [$r4 stringValue]
+} {false true true false}
