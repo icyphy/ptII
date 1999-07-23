8814ca395f19be7e79ff73a323c20e98d6dbf455
neuendor <neuendor>
1999-07-23 19:56:42 +0000

    created

---
+++ ptolemy/schematic/editor/GraphDocument.java
@@ -0,0 +1,154 @@
+/*
+ * $Id$
+ *
+ * Copyright (c) 1998 The Regents of the University of California.
+ * All rights reserved.  See the file COPYRIGHT for details.
+ */
+package ptolemy.schematic.editor;
+
+import ptolemy.schematic.util.*;
+
+import diva.graph.model.GraphModel;
+import diva.graph.toolbox.GraphParser;
+import diva.graph.toolbox.GraphWriter;
+
+import diva.gui.AbstractDocument;
+import diva.gui.Application;
+import diva.gui.BasicSheet;
+import diva.gui.Document;
+import diva.gui.DocumentFactory;
+import diva.gui.Sheet;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.DataOutputStream;
+import java.net.URL;
+
+/**
+ * A class representing graph-structured documents.
+ * This class saves and loads graphs to a schematic ptml file.
+ *
+ * @author John Reekie (johnr@eecs.berkeley.edu)
+ * @version $Revision$
+ */
+public class GraphDocument extends AbstractDocument {
+    //DEBUG
+    static int _globalCount = 0;
+    int _localID = _globalCount++;
+    // What the hell... public String getTitle() { return "Graph" + _localID; }
+    
+    /** Construct a graph document that is owned by the given
+     *  application
+     */
+    public GraphDocument(Application a) {
+        super(a);
+    }
+
+    /** Close the document. This method doesn't do anything, as
+     * graph data doesn't change.
+     */
+    public void close () throws Exception {
+        // Do nothing
+    }
+
+    /** Open the document from its current file.  If successful, add a
+     * new Sheet to the document containing the model parsed from the
+     * current file.
+     *
+     * @throws Exception  If there is no file, or if the I/O operation failed.
+     */
+    public void open () throws Exception {
+        if (getFile() == null) {
+            throw new IllegalStateException(
+                    "GraphDocument " + getTitle() + " has no current file");
+        }
+        String filename = getFile().getCanonicalPath();
+	URL urlbase = new URL("file:");// + System.getProperty("PTII"));
+	URL iconlibURL = new URL(urlbase,  filename);
+	    
+        System.out.println("Parsing " + iconlibURL);
+        Schematic schematic = 
+	    PTMLObjectFactory.parseSchematic(iconlibURL,
+		((GraphEditor)getApplication()).getEntityLibrary());
+
+        Sheet s = new BasicSheet(this, "main", schematic);
+        addSheet(s);
+    }
+
+    /** Save the document to the current file.
+     * 
+     * @throws Exception  If there is no file, or if the I/O operation failed.
+     */
+    public void save () throws Exception {
+        if (getFile() == null) {
+            throw new IllegalStateException(
+                    "GraphDocument " + getTitle() + " has no current file");
+        }
+        saveAs(getFile());
+    }
+
+    /** Save the document to the given file. Do not change the file
+     * attribute to the new File object.
+     *
+     * @throws Exception  If the I/O operation failed.
+     */
+    public void saveAs (File file) throws Exception {
+        String filename = file.getName();
+        FileOutputStream fout = new FileOutputStream(filename);
+        DataOutputStream out = new DataOutputStream(fout);
+        GraphModel model = (GraphModel) getSheet(0).getModel();
+        new GraphWriter().write(model, out);
+    }
+
+    /** Throw an exception, as save to URLs is not supported.
+     *
+     * @throws Exception Always
+     */
+    public void saveAs (URL url) {
+        throw new UnsupportedOperationException(
+                "GraphDocument " + getTitle() + ": save to URL not supported");
+    }
+
+    /** Print information about the graph document
+     */
+    public String toString () {
+        return
+            getClass().getName() + "["
+            + "title = " + getTitle()
+            + ", file = " + getFile()
+            + ", url = " + getURL()
+            + "]";
+    }
+ 
+    /** GraphDocument.Factory is a factory for graph documents.  We
+     * put this in an inner class of GraphDocument because this
+     * factory can only produce one kind of document.
+     */
+    public static class Factory implements DocumentFactory {
+        /** Create an empty graph document
+         */
+        public Document createDocument (Application app) {
+            GraphDocument d = new GraphDocument(app);
+            d.addSheet(new BasicSheet(d, "New graph", new GraphModel()));
+            return d;
+        }
+
+        /** Throw an exception, as URLs are not supported.
+         */
+        public Document createDocument (Application app, URL url) {
+            throw new UnsupportedOperationException(
+                    "Graph documents cannot yet be loaded from a URL");
+        }
+
+        /** Create a new graph that contains the given file path.
+         */
+        public Document createDocument (Application app, File file) {
+            GraphDocument d = new GraphDocument(app);
+            d.setFile(file);
+            return d;
+        }
+    }
+}
