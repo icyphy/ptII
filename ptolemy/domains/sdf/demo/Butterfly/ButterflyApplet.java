760500cc744db27e1315d077fe50eede9ed91110
cxh <cxh>
1999-09-07 16:45:54 +0000

    set x and y ranges for plot

--- ptolemy/domains/sdf/demo/Butterfly/ButterflyApplet.java
+++ ptolemy/domains/sdf/demo/Butterfly/ButterflyApplet.java
@@ -109,6 +109,9 @@ public class ButterflyApplet extends SDFApplet {
             xyPlotter.setPanel(this);
             Dimension size = getSize();
             xyPlotter.plot.setSize(size.width, size.height - 50);
+	    xyPlot.plot.clear(false);
+	    xyPlot.plot.setXRange(-3, 4);
+	    xyPlot.plot.setYRange(-4, 4);
 
 	    _toplevel.connect(scale2.output, sin1Input);
 	    _toplevel.connect(scale1.output, cos1Input);
