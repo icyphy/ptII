2d5eceeea51385ab4ffb80446599f0483a10c75a
eal <eal>
1999-09-10 16:08:13 +0000

    added toString() method

--- ptolemy/actor/TypedIOPort.java
+++ ptolemy/actor/TypedIOPort.java
@@ -643,6 +643,14 @@ public class TypedIOPort extends IOPort implements Typeable {
                     + "the value of a type constant.");
         }
 
+        /** Override the base class to give a description of the port
+         *  and its type.
+         *  @return A description of the port and its type.
+         */
+        public String toString() {
+            return "(" + _port.toString() + ", " + getType() + ")";
+        }
+
         ///////////////////////////////////////////////////////////////
         ////                       private inner variable          ////
 
