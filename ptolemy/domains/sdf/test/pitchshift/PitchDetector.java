f954cd089b21110fcbd97b58c5213ac639a5493b
vogel <vogel>
1999-09-17 18:56:12 +0000

    Adeed Java-based real-time pitch shifting application.

---
+++ ptolemy/domains/sdf/test/pitchshift/PitchDetector.java
@@ -0,0 +1,236 @@
+/* Perform pitch detection using Cepstrum Analysis.
+
+ Copyright (c) 1998-1999 The Regents of the University of California.
+ All rights reserved.
+ Permission is hereby granted, without written agreement and without
+ license or royalty fees, to use, copy, modify, and distribute this
+ software and its documentation for any purpose, provided that the above
+ copyright notice and the following two paragraphs appear in all copies
+ of this software.
+
+ IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+ FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+ ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+ THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+ SUCH DAMAGE.
+
+ THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+ INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+ PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+ CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ ENHANCEMENTS, OR MODIFICATIONS.
+
+                                        PT_COPYRIGHT_VERSION_2
+                                        COPYRIGHTENDKEY
+
+@ProposedRating Red (vogel@eecs.berkeley.edu)
+@AcceptedRating 
+*/
+
+import ptolemy.math.*;
+import ptolemy.math.SignalProcessing;
+
+//////////////////////////////////////////////////////////////////////////
+//// PitchDetector
+/** Perform pitch detection on an input signal using cepstrum analysis.
+    The real cepstrum is computed as the IDFT(log(magnitude(DFT(input_signal)))).
+    The maxumim value of the high-time part of the cepstrum is converted to
+    the corresponding pitch value. The current implementation searches for
+    pitches between 70 and 300 Hz. The latency of the pitch detector is 2048
+    samples.
+@author Brian K. Vogel
+@version 1.0
+*/
+    public class PitchDetector {
+
+	// Array to hold recent input. Note that the size of this
+	// array is independent from the size of the array read in
+	// by performPitchDetect().
+	// 2048 samples (for 44100 sampling rate) is enough to
+	// resolve pitches down to about 60 Hz, provide that
+	// the input signal has several harmonics.
+	private final int recentInputArraySize = 2048;
+
+	// Cutoff value in cepstrum for voiced/unvoiced. If largest peak
+	// in the high-time part is greater than cepstralCutoff, then
+	// call it voiced, else call it unvoiced.
+	private final double cepstralCutoff = 0.03; 
+
+	// The lowest pitch to search for.
+	private final double minAllowablePitch = 70;
+	// The highest pitch to search for
+	private final double maxAllowablePitch = 300;
+
+	private double[] outPitchArray;
+	
+	private double[] windowedInput;
+	private Complex[] dftInput;
+	private Complex[] outCmplxArray;
+
+	private double[] recentInputArray;
+	private double[] logMagDFTWinInputArray;
+	private double[] cepstrumArray;
+	private int voiced; // == 1 for voiced, == 0, for unvoiced.
+
+	 // Initialize input pos
+	private int recentInputArrayPos  = 0;
+
+	private int vectorLoopPos;
+
+	// Most recent pitch estimate (in Hz).
+	// Set to -1 for unvoiced/don't know.
+	private double currentPitch = -1;
+	private double lastPitch = -1;
+	private double tempPitch;
+
+	// A hamming window.
+	private double[] hammingWindow;
+
+	/** Constructor
+	    Initialize the pitch detector.
+	    Parameter <i>vectorSize</i> sets the vector size to be used by <i>performPitchDetect()</i>. <i>vectorSize</i> may be any length.
+	*/
+	public PitchDetector(int vectorSize) {
+	     this.outPitchArray = new double[vectorSize];
+	     this.windowedInput = new double[recentInputArraySize];
+	     this.logMagDFTWinInputArray = new double[recentInputArraySize];
+	     this.recentInputArray = new double[recentInputArraySize];
+	     // Initialize the hamming window.
+	     this.hammingWindow = makeHamming(recentInputArraySize);
+	}
+
+	/**
+	   Perform pitch detection. The input signal should uniformly partitioned
+	   into <i>vectorSize</i> length chunks of data. <i>performPitchDetect()</i>
+	   should be called on each consecutive vector of input data. A
+	   <i>vectorSize</i> length array of doubles containing the data is
+	   passed to <i>performPitchDetect()</i> via <i>inputArray[]</i>. Each
+	   element of <i>inputArray[]</i> is assumes to contain a single data
+	   sample. The <i>sampleRate</i> parameter contains that sample rate,
+	   in Hz.  <i>performPitchDetect()</i> returns a <i>vectorSize</i> length
+	   array of doubles containing the pitch estimates.
+	   <p>
+	   Note that there is a 2048 sample delay between an element of 
+	   <i>inputArray[]</i> and the corresponding element of the returned
+	   vector of pitch estimates.
+	*/
+	public double[] performPitchDetect(double [] inputArray,int sampleRate) {
+	   
+	    //System.out.println("in");
+	    //for (int i=0; i < outArray.length; i++) {
+	    //outArray[i] = inputArray[i];
+	    //}
+	    //tempCmplxArray = FFTComplexOut(inputArray); // ???
+
+	    // Main loop.
+	    for (vectorLoopPos = 0; vectorLoopPos < inputArray.length; vectorLoopPos++) {
+		//System.out.println("vectorLoopPos " + vectorLoopPos);
+		if (recentInputArrayPos < recentInputArraySize) {
+		    // Ok to read in another samples, array not full yet.
+		     
+		    // Read in an input sample.
+		    recentInputArray[recentInputArrayPos] = inputArray[vectorLoopPos];
+		   
+		    recentInputArrayPos++;
+		} else {
+		    // Array is full now. Time to update pitch estimate.
+		    
+		    // Perforam pitch detection using Cepstral Analysis.
+		    
+		    // Step 1. Window recent input with a hamming window.
+		    for (int ind1 = 0; ind1 < recentInputArray.length; ind1++) {
+			
+			windowedInput[ind1] = recentInputArray[ind1]*hammingWindow[ind1];
+		    }	
+		    
+		    // Step 2. Take DFT of recent input.
+		    dftInput = SignalProcessing.FFTComplexOut(windowedInput);
+		    // Step 3. Take log of magnitude of dftInput.
+		    for (int ind2 = 0; ind2 < recentInputArray.length; ind2++) {
+			logMagDFTWinInputArray[ind2] = Math.log(dftInput[ind2].magnitude());
+		    }
+		    // Step 4. Take IDFT of logMagDFTWinInputArray.
+		    // FIXME: Is real idft correct? or complex?
+		    cepstrumArray = SignalProcessing.IFFTRealOut(logMagDFTWinInputArray);
+		    // Step 5. Find the peak in the high time part. This is
+		    // the pitch. FIXME: clean this up.
+		    voiced = 0; 
+		    double largest = 0; // Initialize largest element found so far to 0.
+
+		    // Ignore the low-time part of cepstrum. FIXME: 12-16 is somwhat
+		    // arbitrary. Should be higher than 16 for vocal signals (like 50-100).
+		    int startCepstralIndex = (int)(Math.ceil((double)sampleRate/maxAllowablePitch));
+		    int stopCepstralIndex = (int)Math.min((double)sampleRate/minAllowablePitch, (double)recentInputArray.length/3);
+		    
+		    int pitchIndex = 0; // Index corresp to the pitch.
+		    int foundAPeak = 0;
+		    double linearLength2 = cepstrumArray.length/4 - startCepstralIndex;
+		     double fadeFactor2;
+		     double octaveLargest = 0;
+		     double thirdOctaveLargest = 0;
+		     int octaveInd = 0;
+		     int thirdOctaveInd = 0;
+		     int initialPitchInd = 0;
+		     int secondEstPitchInd = 0;
+		     int thirdEstPitchInd = 0;
+		     int searchRange;
+		    // IMPORTANT NOTE: Increasing index number corresponds to
+		    // decreasing pitch!
+		    for (pitchIndex = startCepstralIndex; pitchIndex < stopCepstralIndex; pitchIndex++) {
+			
+			// Now find the largest element in the high-time
+			// part of the cepstrum.
+			if ((cepstrumArray[pitchIndex]) > largest) {
+			  
+			    largest = cepstrumArray[pitchIndex];
+			    initialPitchInd = pitchIndex;
+			    //System.out.println("largest = " + largest);
+			    //System.out.println("pitchIndex = " + pitchIndex);
+			}
+		    }
+		    // If the cepstral index of largest is more than 15%
+		    // of the length of the cepstrum, I do not check that
+		    // largest > cepstralCutoff. This is because peaks
+		    // higher in the high-time are more significant.
+		    //if ((initialPitchInd > .125*cepstrumArray.length) && (cepstrumArray[initialPitchInd] > cepstralCutoff)) {
+		    if ((initialPitchInd > startCepstralIndex) && (cepstrumArray[initialPitchInd] > cepstralCutoff)) {
+			voiced = 1; // Current windowed signal is voiced (pitched).
+			currentPitch = (double)sampleRate/(double)initialPitchInd;
+		    // Now check that largest is greater than cepstralCutoff.
+		    }
+		    
+		    if (voiced == 0) {
+			// Found a peak in the ceptsrum
+			
+			currentPitch = -1;
+			//System.out.println("UNVOICED" );
+		    } else {
+			//System.out.println(" " );
+			//System.out.println("i1 i2 i3 = " + thirdEstPitchInd + " " + secondEstPitchInd + " " + initialPitchInd);
+			//System.out.println("n1 n2 n3 = " + cepstrumArray[thirdEstPitchInd] + " " + cepstrumArray[secondEstPitchInd] + " " + cepstrumArray[initialPitchInd]); 
+			//System.out.println("p1 p2 p3 = " + (double)sampleRate/(double)thirdEstPitchInd + " " + (double)sampleRate/(double)secondEstPitchInd + " " + (double)sampleRate/(double)initialPitchInd);
+			//System.out.println("currentPitch = " + currentPitch);
+			//System.out.println(" " );
+		    }
+	
+		    recentInputArrayPos = 0;
+		}
+		outPitchArray[vectorLoopPos] = currentPitch;
+	    }
+	    //System.out.println("out");
+	    return outPitchArray;
+	}
+
+	// Create a hamming window.
+	private double[] makeHamming(int size) {
+
+	    double[] hammingWindow = new double[size];
+
+	    for (int i = 0; i < size; i++) {
+		hammingWindow[i] = 0.54 - 0.46*Math.cos(2*Math.PI*i/size);
+	    }
+	    return hammingWindow;
+	}
+
+    }
---
+++ ptolemy/domains/sdf/test/pitchshift/PitchShift.java
@@ -0,0 +1,346 @@
+/* Perform pitch shifting.
+
+ Copyright (c) 1998-1999 The Regents of the University of California.
+ All rights reserved.
+ Permission is hereby granted, without written agreement and without
+ license or royalty fees, to use, copy, modify, and distribute this
+ software and its documentation for any purpose, provided that the above
+ copyright notice and the following two paragraphs appear in all copies
+ of this software.
+
+ IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+ FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+ ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+ THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+ SUCH DAMAGE.
+
+ THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+ INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+ PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+ CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ ENHANCEMENTS, OR MODIFICATIONS.
+
+                                        PT_COPYRIGHT_VERSION_2
+                                        COPYRIGHTENDKEY
+
+@ProposedRating Red (vogel@eecs.berkeley.edu)
+@AcceptedRating 
+*/
+
+
+//////////////////////////////////////////////////////////////////////////
+//// PitchShift
+/**
+Perform pitch scaling of an input signal.
+@author Brian K. Vogel
+@version
+*/
+public class PitchShift {
+
+    private static final int RING_BUFFER_SIZE = 6000;
+    private static final int OUTPUT_BUFFER_DELAY = 1100;
+    private static final double MINIMUM_PITCH = 50.0;
+    private static final double DEFAULT_PITCH = MINIMUM_PITCH;
+    
+    // Delay of the pitch detector, in samples.
+    // FIXME: this should be public, and set in constructor.
+    private static final int pitchDetectorDelay = 2048;
+
+    // The input ring buffer:
+    private double[] inputRingBuf;
+    // The output ring buffer:
+    private double[] outputRingBuf;
+
+    private int inputRingBufWritePos;
+  
+
+
+    private double minimumPitch = MINIMUM_PITCH;
+
+    /* This contains the element in the output ring buffer corresponding to
+     * to the current synthesis pitch marker, about which OLA (OverLap Add)
+     * is being performed. This variable gets incremented by the current
+     * synthesis period when necessary. This parameter is called <outptr> in
+     * Keith Lent's paper.
+     */
+    private int outputRingBufPitchMarkerPos;
+
+    /* The ring buffer size (number of elements). Both the input and output
+     * ring buffers are the same size. The choice of size is somewhat arbitrary
+     * but must be chosen large enough that a few periods of the waveform
+     * can fit in it.
+     */
+    private int ringBufSize;
+
+    // Output buffer delay, in samples
+    private int outputDelay;
+
+    /* Current element to read from in output ring buffer.
+     *  This element is obtained by subtracting OUTPUT_BUFFER_DELAY from
+     *  inputRingBufWritePos.
+     */
+    private int readPos;
+
+    /* Current period length (in samples) of input signal. This gets updated once
+     * per input signal period.
+     */
+    private int inputPeriodLength; 
+
+    /* The number of remaining samples to process before upding <inputPeriodLength>.
+     * <inputPeriodLength> is updated when <samplesLeftInPeriod> is 0.
+     */
+    private int samplesLeftInPeriod;
+
+  
+    /* Output signal pitch = input signal pitch * pitchScaleFactor.
+     */
+    private double pitchScaleFactor;  
+
+    private int isUnvoiced;  // 1 if unvoice (no pitch), else set to 0.
+
+
+  
+    private int minimumPitchSamps; // This is minimumPitch converted to samples.
+  
+    private int sampleRate; // sampling rate.
+
+    /* Initialize the current output ring buffer read postition.
+     * Maybe need to add ringBuffSize because % does not like negative numbers?
+     */
+	
+    // Constructor
+    public PitchShift() {
+	this.outputDelay = OUTPUT_BUFFER_DELAY;
+	this.ringBufSize = RING_BUFFER_SIZE;
+	///////////////////////////////////////////////
+	// Debug
+	//this.inputRingBufWritePos = 0;
+	////////////////////////////////////////////
+	// This is delay from the pitch detector.
+	this.inputRingBufWritePos = 0;
+	//////////////////////////////////////
+	
+	this.readPos = (inputRingBufWritePos - outputDelay + ringBufSize) % ringBufSize;
+
+	// The input ring buffer:
+	this.inputRingBuf = new double[RING_BUFFER_SIZE];
+
+	// The output ring buffer:
+	this.outputRingBuf = new double[RING_BUFFER_SIZE];
+
+	this.outputRingBufPitchMarkerPos = 0;
+	// Starting period. Initialize to default (unvoiced) pitch.
+	this.inputPeriodLength = 100;
+	this.samplesLeftInPeriod = 0;
+	// Initialize to unvoiced, since don't know the pitch yet.
+	this.isUnvoiced = 1;
+    }
+
+    /**
+       Perform pitch shifting on the input signal. Read in an array of doubles
+       in <i>in[]</i>, sampled at <i>sampleRate</i>, with corresponding pitch
+       values given by <i>pitchArray</i>. The desired pitch scale factor is
+       passed via <i>pitchScaleIn</i>. The delayed and pitch shifted signal is
+       returned as a vector of the same length as <i>in[]</i>. The delay
+       (latency) of this pitch shifter is given by <i>pitchDetectorDelay</i>
+       + OUTPUT_BUFFER_DELAY + <i>sampleRate</i>/<i>MINIMUM_PITCH</i>. The
+       delay of the external pitch detector (delay between a sample of <i>in</i>
+       and the sample of <i>pitchArray</i> containing the corresponding pitch)
+       is set by <i>pitchDetectorDelay</i>.
+    */
+    public double[] performPitchShift(double[] in, float sampleRate, double[] pitchArray, double pitchScaleIn) {
+	minimumPitchSamps =(int)((1/minimumPitch)*sampleRate);
+
+	int inputPitchInPtr = 0;
+	int curOutSamp = 0;
+	// Initialize input pos
+	int curInSamp = 0;
+	double[] out = new double[in.length];
+	// Temporary variables
+	int size = in.length;
+	
+	double periodRatio;
+	
+	double correctedPitchScale;
+	double correctedPitchIn;
+	
+	double windowVal;  // Current value of window function.
+	
+	
+	
+	/* Current position in OLA process relative to current synthesis pitch marker.
+	 */
+	int olaIndex;
+	
+	int outLag; // Set to 1 if x->outputRingBufPitchMarkerPos lags x->inputRingBufWritePos, else set to zero.
+	
+	/* This is a marker for the element half way around the cirucular buffer w.r.t. x->inputRingBufWritePos */
+	int inHalfAway;
+	
+	//t_pitcher *x = (t_pitcher *)(w[6]);
+	
+	/*The main loop. This will iterate <MSP vector size> times for each call to this perform
+	 * method. */
+	while (size > 0) {
+	    
+	    
+	    
+	    // Write the current input sample into the input circular array.
+	    //inputRingBuf[inputRingBufWritePos] = in[curInSamp];
+	    // Add some delay, to compensate for the pitch detector.
+	    inputRingBuf[(inputRingBufWritePos+pitchDetectorDelay) % ringBufSize] = in[curInSamp];
+	    
+	    //////////////////////////////////////////////////////
+	    //////////////////////////////////////////////////////
+	    // Do all interesting processing here.
+	    
+	    /* Check if have reached the end of the current period in the
+	       input signal.
+	    */
+	    if (samplesLeftInPeriod == 0)
+		{
+		    /* Check if ok to do an OLA in the output buffer. */
+		    /* That is, check if the the outputRingBufPitchMarkerPos lags nputRingBufWritePos. */
+		    //lag = (x->inputRingBufWritePos - x->outputRingBufPitchMarkerPos) % x->ringBufSize;
+		    
+		    outLag = 1;
+		    inHalfAway = (inputRingBufWritePos + ringBufSize/2) % ringBufSize;
+		    if (inHalfAway < (ringBufSize/2))
+			{
+	           	   	/* The zero element of the input buffer lies in (inptr, inHalfAway] */
+			    if ((outputRingBufPitchMarkerPos < inHalfAway) || (outputRingBufPitchMarkerPos > inputRingBufWritePos))
+	           	   	// The current input element lags current synthesis pitch marker.
+				outLag = 0;
+			}
+		    else
+			{
+	           	    	/* The zero element of the input buffer lies in (inHalfAway, inptr] */
+			    if ((outputRingBufPitchMarkerPos > inputRingBufWritePos) && (outputRingBufPitchMarkerPos < inHalfAway))
+				{
+				// The current input element lags current synthesis pitch marker.
+				    outLag = 0;
+				}
+			}
+		    
+		    while (outLag == 1)	   
+			{
+	           	    	// Do an OLA
+			    
+	           	    	/* Update the synthesis pitch marker posistion (in the output buffer)/
+				 */
+	           	    	// Do error checking
+			    if ((pitchScaleIn <= 0.1) || (pitchScaleIn > 6.0) || (isUnvoiced == 1))
+				{
+				// UhOh, out of range. Fix that.
+				    correctedPitchScale = 1.0;
+				}
+			    else
+				{
+				    correctedPitchScale = pitchScaleIn;
+				}  
+			    
+	           	    	// FOR DEBUG ONLY  
+	           	    	//correctedPitchScale = 1.5f;
+			    
+			    periodRatio = 1.0/(correctedPitchScale); // Period scale factor.
+			    outputRingBufPitchMarkerPos = (int)(outputRingBufPitchMarkerPos + (int)(inputPeriodLength*periodRatio)) % ringBufSize;
+	           	    	//x->outputRingBufPitchMarkerPos = (x->outputRingBufPitchMarkerPos + 100) % x->ringBufSize;
+			    
+			    
+	           	    	/* Do an OLA (in the output buffer) about the synthesis pitch marker. Note that
+				 * this implementation differs slightly from Lent's algorithm, in that 1 input signal
+				 * is subtracted from the current input pointer position. This is done in order to
+				 * reduce latency and should not have an audible impact, I think.
+				 */
+			    for (olaIndex = -inputPeriodLength; olaIndex <= inputPeriodLength; ++olaIndex)
+				{
+				    
+				    windowVal = (1 + Math.cos(Math.PI*olaIndex/(float)inputPeriodLength))*0.5;
+				// windowVal = 1;  // For debuging only (no window!).
+				    outputRingBuf[(olaIndex + outputRingBufPitchMarkerPos + ringBufSize) % ringBufSize] += windowVal*inputRingBuf[(olaIndex + inputRingBufWritePos - minimumPitchSamps + ringBufSize) % ringBufSize];
+				//x->outputRingBuf[(olaIndex + x->outputRingBufPitchMarkerPos + x->ringBufSize + 50000) % x->ringBufSize] += windowVal*x->inputRingBuf[(olaIndex + x->inputRingBufWritePos - x->inputPeriodLength + x->ringBufSize) % x->ringBufSize];
+				}  
+	           	    	// Update loop condition variable.    	
+			    outLag = 1;
+			    inHalfAway = (inputRingBufWritePos + ringBufSize/2) % ringBufSize;
+			    if (inHalfAway < (ringBufSize/2))
+				{
+				/* The zero element of the input buffer lies in (inptr, inHalfAway] */
+				    if ((outputRingBufPitchMarkerPos < inHalfAway) || (outputRingBufPitchMarkerPos > inputRingBufWritePos))
+					// The current input element lags current synthesis pitch marker.
+					outLag = 0;
+				}
+			    else
+				{
+				/* The zero element of the input buffer lies in (inHalfAway, inptr] */
+				    if ((outputRingBufPitchMarkerPos > inputRingBufWritePos) && (outputRingBufPitchMarkerPos <= inHalfAway))
+					{
+					    // The current input element lags current synthesis pitch marker.
+					    outLag = 0;
+					}
+				}
+			}
+		    
+		    /* Update input period value */
+		    
+		    
+		    // Do error checking on input pitch signal value.
+		    if (pitchArray[inputPitchInPtr] <= minimumPitch)
+			{
+			    // UhOh, pitch below range. Fix that.
+			    correctedPitchIn = DEFAULT_PITCH;
+			    isUnvoiced = 1;
+			}
+		    else
+			{
+			    
+			    correctedPitchIn = pitchArray[inputPitchInPtr];
+			    isUnvoiced = 0;
+			}  
+		    // correctedPitchIn = 441.0;  // FOR DEBUG
+		    inputPeriodLength = (int)((1.0/correctedPitchIn)*(float)sampleRate);
+		    // inputPeriodLength = 100;  // FOR DEBUG
+		    samplesLeftInPeriod = inputPeriodLength;
+		}
+	    
+	    --samplesLeftInPeriod; 
+	    
+	    // For debugging, simply copy from input buffer to output buffer.
+	    //outputRingBuf[inputRingBufWritePos] = inputRingBuf[inputRingBufWritePos];
+	    
+	    
+	    // End of all interesting processing.
+	    ////////////////////////////////////////////////////////
+	    ////////////////////////////////////////////////////////
+	    
+	    // Read an output sample from the output Ring buffer.
+	    out[curOutSamp] = outputRingBuf[readPos];
+	    //*out = inputRingBuf[readPos];
+	    
+	    // Now set the element just read from to zero, since it is no longer needed.
+	    outputRingBuf[readPos] = 0;	
+	    
+	    // Update the pointers.
+	    inputRingBufWritePos++;
+	    // Make the write postition pointer wrap back to the begining after it
+	    // reaches the end of the buffer.
+	    inputRingBufWritePos %= ringBufSize;
+	    
+	    readPos++;
+	    // Make the write postition pointer wrap back to the begining after it
+	    // reaches the end of the buffer.
+	    readPos %= ringBufSize;
+	    
+	    
+	    curInSamp++;
+	    curOutSamp++;
+	    inputPitchInPtr++;
+	    size--;
+	}
+	
+	return out;
+    }
+    
+
+}
