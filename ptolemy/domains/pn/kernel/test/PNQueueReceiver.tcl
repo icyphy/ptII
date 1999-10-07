e0b96c1fd2196c86e24b401e522e9186ba1960a1
davisj <davisj>
1999-10-07 01:12:57 +0000

    Tested isInsideBoundary(), isOutsideBoundary() and isConnectedToBoundary(). Now 29 of 29 tests pass!

--- ptolemy/domains/pn/kernel/test/PNQueueReceiver.tcl
+++ ptolemy/domains/pn/kernel/test/PNQueueReceiver.tcl
@@ -1,6 +1,6 @@
 # Tests for the Director class
 #
-# @Author: Mudit Goel
+# @Author: Mudit Goel, John S. Davis II
 #
 # @Version: $Id$
 #
@@ -171,4 +171,261 @@ test PNQueueReceiver-4.4 {Test for initialize} {
     set ans1 [$elem hasMoreElements]
     list $ans1
 } {0}
+
+
+######################################################################
+####
+#
+test PNQueueReceiver-5.1 {Check is...Boundary() methods for single layer boundary} {
+    set wspc [java::new ptolemy.kernel.util.Workspace]
+    set mgr [java::new ptolemy.actor.Manager $wspc "manager"]
+    set toplevel [java::new ptolemy.actor.TypedCompositeActor $wspc]
+    set wormhole [java::new ptolemy.actor.TypedCompositeActor $toplevel "wormhole"]
+    set topdir [java::new ptolemy.domains.pn.kernel.BasePNDirector $toplevel "topdirector"]
+    set wormdir [java::new ptolemy.domains.pn.kernel.BasePNDirector $wormhole "wormdirector"]
+    
+    
+    # Assign Directors/Managers
+    $toplevel setManager $mgr
+    $toplevel setDirector $topdir
+    $wormhole setDirector $wormdir
+    
+    
+    # Instantiate Actors
+    set a1 [java::new ptolemy.actor.process.test.TypedTestProcessActor $toplevel "a1"]
+    set a2 [java::new ptolemy.actor.process.test.TypedTestProcessActor $toplevel "a2"]
+    
+    set b1 [java::new ptolemy.actor.process.test.TypedTestProcessActor $wormhole "b1"]
+    set b2 [java::new ptolemy.actor.process.test.TypedTestProcessActor $wormhole "b2"]
+    
+    
+    # Add Ports to the Wormhole
+    set wormInPort [java::new ptolemy.actor.TypedIOPort $wormhole "input" true false]
+    set wormOutPort [java::new ptolemy.actor.TypedIOPort $wormhole "output" false true]
+    
+    
+    # Add Ports to the other Actors
+    set a1OutPort [java::new ptolemy.actor.TypedIOPort $a1 "output" false true]
+    set a2InPort [java::new ptolemy.actor.TypedIOPort $a2 "input" true false]
+    
+    set b1InPort [java::new ptolemy.actor.TypedIOPort $b1 "input" true false]
+    set b1OutPort [java::new ptolemy.actor.TypedIOPort $b1 "output" false true]
+    set b2InPort [java::new ptolemy.actor.TypedIOPort $b2 "input" true false]
+    
+    $b1OutPort setMultiport true
+    
+    
+    # Connect Inside Wormhole Ports
+    $wormhole connect $wormInPort $b1InPort
+    $wormhole connect $b1OutPort $b2InPort
+    $wormhole connect $b1OutPort $wormOutPort 
+    
+
+    # Connect Outer Ports
+    $toplevel connect $a1OutPort $wormInPort
+    $toplevel connect $wormOutPort $a2InPort
+    
+    
+    # Create Receivers
+    $toplevel preinitialize
+    
+    set b1InIOPort [java::cast ptolemy.actor.TypedIOPort $b1InPort]
+    set rcvrs [$b1InIOPort getReceivers]
+    set rcvr [java::cast ptolemy.domains.pn.kernel.PNQueueReceiver [$rcvrs get {0 0}]]
+    set vala [$rcvr isInsideBoundary]
+    set valb [$rcvr isConnectedToBoundary]
+    set valc [$rcvr isOutsideBoundary]
+    if { $vala == 0  && $valb == 1 && $valc == 0 } {
+        set val1 1
+    } else {
+        set val1 0
+    }
+    
+    set b2InIOPort [java::cast ptolemy.actor.TypedIOPort $b2InPort]
+    set rcvrs [$b2InIOPort getReceivers]
+    set rcvr [java::cast ptolemy.domains.pn.kernel.PNQueueReceiver [$rcvrs get {0 0}]]
+    set vala [$rcvr isInsideBoundary]
+    set valb [$rcvr isConnectedToBoundary]
+    set valc [$rcvr isOutsideBoundary]
+    if { $vala == 0  && $valb == 0 && $valc == 0 } {
+        set val2 1
+    } else {
+        set val2 0
+    }
+    
+    set a2InIOPort [java::cast ptolemy.actor.TypedIOPort $a2InPort]
+    set rcvrs [$a2InIOPort  getReceivers]
+    set rcvr [java::cast ptolemy.domains.pn.kernel.PNQueueReceiver [$rcvrs get {0 0}]]
+    set vala [$rcvr isInsideBoundary]
+    set valb [$rcvr isConnectedToBoundary]
+    set valc [$rcvr isOutsideBoundary]
+    if { $vala == 0  && $valb == 0 && $valc == 0 } {
+        set val3 1
+    } else {
+        set val3 0
+    }
+    
+    set wormOutIOPort [java::cast ptolemy.actor.TypedIOPort $wormOutPort]
+    set rcvrs [$wormOutIOPort getInsideReceivers]
+    set rcvr [java::cast ptolemy.domains.pn.kernel.PNQueueReceiver [$rcvrs get {0 0}]]
+    set vala [$rcvr isInsideBoundary]
+    set valb [$rcvr isConnectedToBoundary]
+    set valc [$rcvr isOutsideBoundary]
+    if { $vala == 1  && $valb == 0 && $valc == 0 } {
+        set val4 1
+    } else {
+        set val4 0
+    }
+    
+    set wormInIOPort [java::cast ptolemy.actor.TypedIOPort $wormInPort]
+    set rcvrs [$wormInIOPort getReceivers]
+    set rcvr [java::cast ptolemy.domains.pn.kernel.PNQueueReceiver [$rcvrs get {0 0}]]
+    set vala [$rcvr isInsideBoundary]
+    set valb [$rcvr isConnectedToBoundary]
+    set valc [$rcvr isOutsideBoundary]
+    if { $vala == 0  && $valb == 0 && $valc == 1 } {
+        set val5 1
+    } else {
+        set val5 0
+    }
+    
+    list $val1 $val2 $val3 $val4 $val5
+    
+} {1 1 1 1 1}
+
+######################################################################
+####
+#
+test PNQueueReceiver-5.2 {Check is...Boundary() for multilayered boundaries} {
+    set wspc [java::new ptolemy.kernel.util.Workspace]
+    set mgr [java::new ptolemy.actor.Manager $wspc "manager"]
+    set toplevel [java::new ptolemy.actor.TypedCompositeActor $wspc]
+    set outerworm [java::new ptolemy.actor.TypedCompositeActor $toplevel "outerworm"]
+    set innerworm [java::new ptolemy.actor.TypedCompositeActor $outerworm "innerworm"]
+    set topdir [java::new ptolemy.domains.pn.kernel.BasePNDirector $toplevel "topdirector"]
+    set outerwormdir [java::new ptolemy.domains.pn.kernel.BasePNDirector $outerworm "outerwormdirector"]
+    set innerwormdir [java::new ptolemy.domains.pn.kernel.BasePNDirector $innerworm "innerwormdirector"]
+    
+    # Assign Directors/Managers
+    $toplevel setManager $mgr
+    $toplevel setDirector $topdir
+    $outerworm setDirector $outerwormdir
+    $innerworm setDirector $innerwormdir
+    
+    # Instantiate Atomic Actors
+    set a1 [java::new ptolemy.actor.process.test.TypedTestProcessActor $toplevel "a1"]
+    set a2 [java::new ptolemy.actor.process.test.TypedTestProcessActor $toplevel "a2"]
+    
+    set b1 [java::new ptolemy.actor.process.test.TypedTestProcessActor $innerworm "b1"]
+    set b2 [java::new ptolemy.actor.process.test.TypedTestProcessActor $innerworm "b2"]
+    
+    
+    # Add Ports to the Inner Wormhole
+    set innerwormInPort [java::new ptolemy.actor.TypedIOPort $innerworm "input" true false]
+    set innerwormOutPort [java::new ptolemy.actor.TypedIOPort $innerworm "output" false true]
+    
+    # Add Ports to the Outer Wormhole
+    set outerwormInPort [java::new ptolemy.actor.TypedIOPort $outerworm "input" true false]
+    set outerwormOutPort [java::new ptolemy.actor.TypedIOPort $outerworm "output" false true]
+    
+    # Add Ports to the Atomic Actors
+    set a1OutPort [java::new ptolemy.actor.TypedIOPort $a1 "output" false true]
+    set a2InPort [java::new ptolemy.actor.TypedIOPort $a2 "input" true false]
+    
+    set b1InPort [java::new ptolemy.actor.TypedIOPort $b1 "input" true false]
+    set b1OutPort [java::new ptolemy.actor.TypedIOPort $b1 "output" false true]
+    set b2InPort [java::new ptolemy.actor.TypedIOPort $b2 "input" true false]
+    
+    $b1OutPort setMultiport true
+    
+    
+    # Connect Interior Inner Wormhole Ports
+    $innerworm connect $innerwormInPort $b1InPort
+    $innerworm connect $b1OutPort $b2InPort 
+    $innerworm connect $b1OutPort $innerwormOutPort
+    
+    
+    # Connect Interior Outer Wormhole Ports
+    $outerworm connect $outerwormInPort $innerwormInPort 
+    $outerworm connect $innerwormOutPort $outerwormOutPort 
+    
+    
+    # Connect Interior Top Level Ports
+    $toplevel connect $a1OutPort $outerwormInPort
+    $toplevel connect $outerwormOutPort $a2InPort
+    
+
+    # Create Receivers
+    $toplevel preinitialize
+    
+    set innerwormOutIOPort [java::cast ptolemy.actor.TypedIOPort $innerwormOutPort]
+    set rcvrs [$innerwormOutIOPort getInsideReceivers]
+    set rcvr [java::cast ptolemy.domains.pn.kernel.PNQueueReceiver [$rcvrs get {0 0}]]
+    set vala [$rcvr isInsideBoundary]
+    set valb [$rcvr isConnectedToBoundary]
+    set valc [$rcvr isOutsideBoundary]
+    if { $vala == 1  && $valb == 0 && $valc == 0 } {
+        set val1 1
+    } else {
+        set val1 0
+    }
+    
+    set innerwormInIOPort [java::cast ptolemy.actor.TypedIOPort $innerwormInPort]
+    set rcvrs [$innerwormInIOPort getReceivers]
+    set rcvr [java::cast ptolemy.domains.pn.kernel.PNQueueReceiver [$rcvrs get {0 0}]]
+    set vala [$rcvr isInsideBoundary]
+    set valb [$rcvr isConnectedToBoundary]
+    set valc [$rcvr isOutsideBoundary]
+    if { $vala == 0  && $valb == 1 && $valc == 1 } {
+        set val2 1
+    } else {
+        set val2 0
+    }
+    
+    set outerwormOutIOPort [java::cast ptolemy.actor.TypedIOPort $outerwormOutPort]
+    set rcvrs [$outerwormOutIOPort getInsideReceivers]
+    set rcvr [java::cast ptolemy.domains.pn.kernel.PNQueueReceiver [$rcvrs get {0 0}]]
+    set vala [$rcvr isInsideBoundary]
+    set valb [$rcvr isConnectedToBoundary]
+    set valc [$rcvr isOutsideBoundary]
+    if { $vala == 1  && $valb == 0 && $valc == 0 } {
+        set val3 1
+    } else {
+        set val3 0
+    }
+    
+    set outerwormInIOPort [java::cast ptolemy.actor.TypedIOPort $outerwormInPort]
+    set rcvrs [$outerwormInIOPort getReceivers]
+    set rcvr [java::cast ptolemy.domains.pn.kernel.PNQueueReceiver [$rcvrs get {0 0}]]
+    set vala [$rcvr isInsideBoundary]
+    set valb [$rcvr isConnectedToBoundary]
+    set valc [$rcvr isOutsideBoundary]
+    if { $vala == 0  && $valb == 0 && $valc == 1 } {
+        set val4 1
+    } else {
+        set val4 0
+    }
+    
+    list $val1 $val2 $val3 $val4
+    
+} {1 1 1 1}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
