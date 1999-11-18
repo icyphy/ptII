696968f440e2cfd3cbd4bb3d77eebc0ede523148
vogel <vogel>
1999-11-18 05:27:58 +0000

    Added HDFFSMTransition.java. Moved it here from ../lib.

---
+++ ptolemy/domains/fsm/kernel/HDFFSMTransition.java
@@ -0,0 +1,189 @@
+/* A FSMTransition connects two FSMStates.
+
+ Copyright (c) 1997-1999 The Regents of the University of California.
+ All rights reserved.
+ Permission is hereby granted, without written agreement and without
+ license or royalty fees, to use, copy, modify, and distribute this
+ software and its documentation for any purpose, provided that the above
+ copyright notice and the following two paragraphs appear in all copies
+ of this software.
+
+ IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+ FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+ ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+ THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+ SUCH DAMAGE.
+
+ THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+ INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+ PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+ CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ ENHANCEMENTS, OR MODIFICATIONS.
+
+                                        PT_COPYRIGHT_VERSION_2
+                                        COPYRIGHTENDKEY
+
+@ProposedRating Red (liuxj@eecs.berkeley.edu)
+@AcceptedRating Red (cxh@eecs.berkeley.edu)
+
+*/
+
+package ptolemy.domains.fsm.kernel;
+
+import ptolemy.kernel.*;
+import ptolemy.kernel.util.*;
+import ptolemy.data.*;
+import ptolemy.data.type.BaseType;
+import ptolemy.actor.*;
+import ptolemy.data.expr.Variable;
+import ptolemy.domains.fsm.kernel.util.VariableList;
+import java.util.Enumeration;
+import collections.LinkedList;
+import ptolemy.domains.fsm.kernel.*;
+import ptolemy.domains.fsm.lib.*;
+
+//////////////////////////////////////////////////////////////////////////
+//// HDFFSMTransition
+/**
+A FSMTransition connects two FSMStates. It has a trigger event, a trigger
+condition, and a set of trigger actions.
+
+@author Brian K. Vogel
+@version $Id$
+*/
+public class HDFFSMTransition extends FSMTransition {
+
+    /** Construct a transition in the default workspace with an empty string
+     *  as its name. Add the transition to the directory of the workspace.
+     */
+    public HDFFSMTransition() {
+        super();
+    }
+
+    /** Construct a transition in the specified workspace with an empty
+     *  string as a name. You can then change the name with setName().
+     *  If the workspace argument is null, then use the default workspace.
+     *  Add the transition to the workspace directory.
+     *  @param workspace The workspace that will list the transition.
+     */
+    public HDFFSMTransition(Workspace workspace) {
+	super(workspace);
+    }
+
+    /** Construct a transition with the given name contained by the specified
+     *  entity. The container argument must not be null, or a
+     *  NullPointerException will be thrown. This transition will use the
+     *  workspace of the container for synchronization and version counts.
+     *  If the name argument is null, then the name is set to the empty string.
+     *  This constructor write-synchronizes on the workspace.
+     *  @param container The container.
+     *  @param name The name of the transition.
+     *  @exception IllegalActionException If the container is incompatible
+     *   with this transition.
+     *  @exception NameDuplicationException If the name coincides with
+     *   any relation already in the container.
+     */
+    public HDFFSMTransition(CompositeEntity container, String name)
+            throws IllegalActionException, NameDuplicationException {
+        super(container, name);
+    }
+
+    /** Clone the object into the specified workspace. The new object is
+     *  <i>not</i> added to the directory of that workspace (you must do this
+     *  yourself if you want it there).
+     *  The result is a new relation with no links and no container.
+     *  @param ws The workspace for the cloned object.
+     *  @exception CloneNotSupportedException If one or more of the attributes
+     *   cannot be cloned.
+     *  @return A new ComponentRelation.
+     */
+    public Object clone(Workspace ws) throws CloneNotSupportedException {
+        HDFFSMTransition newobj = (HDFFSMTransition)super.clone(ws);
+        newobj._stateVersion = -1;
+        if (_trigger != null) {
+            newobj._trigger = (VariableList)newobj.getAttribute("Trigger");
+            newobj._te = (Variable)newobj._trigger.getAttribute("TriggerEvent");
+            newobj._tc = (Variable)newobj._trigger.getAttribute("TriggerCondition");
+            newobj._actions = (VariableList)newobj.getAttribute("Actions");
+            newobj._localVarUpdates = (VariableList)newobj.getAttribute("LocalVarUpdates");
+        }
+        return newobj;
+    }
+
+
+    /** Return true if this transition is enabled.
+     */
+    public boolean isEnabled() throws IllegalActionException {
+
+        if (_teSet) {
+
+            System.out.println("HDFFSMTransition: isEnabled(): Testing trigger event of " + this.getFullName());
+
+            _te.getToken();
+	    /*
+            if (((BooleanToken)_te.getToken()).booleanValue() == false) {
+                return false;
+            }
+	    */
+        }
+        if (_tcSet) {
+
+            System.out.println("HDFFSMTransition: isEnabled(): Testing condition of " + this.getFullName());
+
+            _tc.getToken();
+            if (((BooleanToken)_tc.getToken()).booleanValue() == false) {
+                return false;
+            }
+        }
+	System.out.println("HDFFSMTransition: isEnabled(): returning TRUE");
+        return true;
+    }
+
+
+    /** Set the trigger event of this transition.
+     */
+    public void setTriggerEvent(String te) {
+	// _te is a Variable.
+	// Create a VariableList and place _te in it.
+        if (_te == null) {
+            _createVarLists();
+        }
+        _te.setExpression(te);
+        _teSet = true;
+    }
+
+    /** Set the trigger condition of this transition.
+     */
+    public void setTriggerCondition(String tc) {
+        if (_tc == null) {
+            _createVarLists();
+        }
+        _tc.setExpression(tc);
+        _tcSet = true;
+    }
+
+    /** Setup the scope of the guard (trigger condition). The scope
+     *  consists of ????
+     */
+    public void setupScope() throws NameDuplicationException, IllegalActionException {
+	HDFFSMController ctrl = (HDFFSMController)getContainer();
+	HDFFSMDirector direct = ((HDFFSMDirector)ctrl.getDirector());
+	directorGuard = direct.guardVar;
+	if (direct == null) {
+	    System.out.println("PPPPPPPPPPPPPPPPPPP");
+	}
+	if (directorGuard != null) {
+            _te.addToScope(directorGuard);
+            _tc.addToScope(directorGuard);
+
+        } else {
+	    //throw new IllegalActionException((HDFFSMController)getContainer(), this,
+	    //      "The guard variable is null");
+	    System.out.println("The guard variable is null");
+	}
+    }
+
+    ///////////////////////////////////////////////////////////////
+    Variable directorGuard;
+}
