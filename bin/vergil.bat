6c9754616ff248e2afa74ab8fbc31a32f03d68d3
cxh <cxh>
1999-12-01 20:56:35 +0000

    Added .bat files, makefile and ptinvoke.in

---
+++ bin/vergil.bat
@@ -0,0 +1,10 @@
+@echo off
+rem MSDOS batch script to start ptolemy
+
+rem @author Edward A. Lee
+rem @version $Id$
+rem @copyright: Copyright (c) 1997-1999
+rem The Regents of the University of California.
+
+java -classpath %PTII% ptolemy.vergil.Vergil %1 %2 %3 %4 %5 %6 %7 %8 %9
+
