725d5045ed938228b20f6860a27170b63a5faed4
kienhuis <kienhuis>
1999-12-08 02:32:38 +0000

    Changed the behavior of doubleValue. Instead use convertToDouble

--- ptolemy/data/FixToken.java
+++ ptolemy/data/FixToken.java
@@ -231,6 +231,16 @@ public class FixToken extends ScalarToken {
         return _value;
     }
 
+    /** Return the fix point value of this token as a double. The
+     *  conversion from a fix point to a double is not lossless, and
+     *  the doubleValue() cannot be used. Therefore an explicit lossy
+     *  conversion method is provided, 
+     *  @return A double.
+     */
+    public double convertToDouble() {
+        return _value.doubleValue();
+    }
+
     /** Return the type of this token.
      *  @return BaseType.FIX
      */
@@ -453,7 +463,7 @@ public class FixToken extends ScalarToken {
     }
 
     /** Set the Rounding mode of the FixPoint number. */
-    // fixm: Currently it is a string, should be come a
+    // FIXME: Currently it is a string, should be come a
     // type safe enumerated type
     public void setRoundingMode(String x) {
 	// _value.setRounding( (String) x );
@@ -464,74 +474,80 @@ public class FixToken extends ScalarToken {
         _value.printFix();
     }
 
-    ///////////////////////////////////////////////////////////////////
-    ////                         private variables                 ////
-    private FixPoint _value;
+    /** 
 
+     Create a Token with a fix point value of a given precision. This
+     function is used by the expression parser to create a FixToken. A
+     FixToken is created as 
 
-    ///////////////////////////////////////////////////////////////////
-    ////                         private variables                 ////
-    
-    public static FixToken fix( double value, int numberOfBits, 
-            int integerBits) {
-        System.out.println(" Expression Language: received value,value,value");
+     <p><pre>fix( 5.34, 10, 2)</pre><p> 
+
+     This generates a FixToken with the value 5.34 using 10 bits of
+     which 2 are used to represent the integer part and 8 for the
+     fractional part.
+
+     @param value The value of the FixToken
+     @param numberOfBits The total number of bits.  
+     @param integerBits The number of bits used for the integer part.
+     */
+    public static Token fix( double value, 
+            int numberOfBits, int integerBits) {
         return new FixToken( value, numberOfBits, integerBits );
     }
 
+    /** Create a matrix with fix point values of a given
+     precision. This method is used by the expression parser to create
+     a matrix of fix point tokens. It is created as
+     
+     <p><pre>fix( [ -.040609, -.001628, .17853, .37665, .37665, .17853, -.001628, -.040609 ], 10,  2)</pre><p> 
 
-   public static Token fix(  DoubleMatrixToken values, int numberOfBits, 
-           int integerBits) {
-       System.out.println(" Expression Language: received array of values");
-       System.out.println(" Expression Language: return a Fix matrix ");
-       System.out.println(" Precision: (" + numberOfBits + "/" + 
-               integerBits + ")" );
-       FixPoint [][] fxa = new FixPoint[1][values.getColumnCount()];
+     This generates a 1x8 FixMatrixToken. The precision of each fix
+     point value is 10 bits of which 2 are used for the integer part
+     and 8 for the fraction part.
+     @param values The matrix with double values.
+     @param numberOfBits The total number of bits.
+     @param integerBits The number of bits used for the integer part.
+
+     */
+   public static Token fix(  DoubleMatrixToken values, 
+           int numberOfBits, int integerBits) {
+       FixPoint [][] fxa = new FixPoint[1][values.getColumnCount()];       
        Precision precision = new Precision( numberOfBits, integerBits );
        for( int i=0; i<values.getColumnCount(); i++) {
            fxa[0][i] = Quantizer.round( values.getElementAt(0,i), precision);
-           System.out.println(" Result["+i+"] = " + fxa[0][i].toString() );
        }
-       return new FixMatrixToken( fxa );
+       return new FixMatrixToken( fxa );        
    }
-    
 
-   public static Token quantize(  DoubleMatrixToken values, int numberOfBits, 
+    /** Create a matrix of double values that are quantized to a given
+     precision. This function is used by the expression parser to
+     create a matrix of quantized doubles. The matrix of quantized
+     doubles is created as
+     
+     <p><pre>quantize( [ -.040609, -.001628, .17853, .37665, .37665, 
+     .17853, -.001628, -.040609 ], 10,  2)</pre><p> 
+
+     This generates a 1x8 matrix of double values, of which each value
+     is quantized using a given precision. In this case 10 bits are
+     used of which 2 for the integer part and 8 for the fraction part.
+     @param values The matrix with quantized double values.
+     @param numberOfBits The total number of bits.
+     @param integerBits The number of bits used for the integer part.
+     */
+   public static Token quantize( DoubleMatrixToken values, int numberOfBits, 
            int integerBits) {
-       System.out.println(" Expression Language: received array of values");
-       System.out.println(" Expression Language: return a double matrix ");
-       System.out.println(" Precision: (" + numberOfBits + "/" + 
-               integerBits + ")" );
        double [][] fxa = new double[1][values.getColumnCount()];
        for( int i=0; i<values.getColumnCount(); i++) {
-           // fxa[0][i] = 1.0;
            Precision precision = new Precision( numberOfBits, integerBits );
-           fxa[0][i] = (Quantizer.round(values.getElementAt(0,i), precision)).doubleValue();;
-           System.out.println(" Result["+i+"] = " + fxa[0][i] );
+           fxa[0][i] = 
+               (Quantizer.round(values.getElementAt(0,i), precision)).doubleValue();;
        }
        return new DoubleMatrixToken( fxa );
    }
-
-
-    /**
-   public static FixToken fix( DoubleMatrixToken values, int numberOfBits, 
-           int integerBits) {
-       System.out.println(" Expression Language: received Matrix of values");
-       System.out.println(" Precision: (" + numberOfBits + "/" + 
-               integerBits + ")" );
-       FixToken[] fxa = new FixToken[values.getColumnCount()];
-       
-       for( int i=0; i<values.getColumnCount(); i++) {
-           fxa[i] = new FixToken( values.getElementAt(0,i), numberOfBits, integerBits );
-           System.out.println(" Result["+i+"] = " + fxa[i].toString() );
-       }      
-       return new FixToken(10.0, 16,2);
-       }
-       */
-
-    public static FixToken fix( double value, String precision ) {
-        System.out.println(" Expression Language: received value,precision");
-        return new FixToken(value, precision);
-    }
+    
+    ///////////////////////////////////////////////////////////////////
+    ////                         private variables                 ////
+    private FixPoint _value;
 
 }
 
