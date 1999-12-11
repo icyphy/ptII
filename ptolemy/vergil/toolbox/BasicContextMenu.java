37ba98adcc4725afc650f279ebd0b2638aa39f6f
eal <eal>
1999-12-11 00:15:13 +0000

    improved edit params

--- ptolemy/vergil/toolbox/BasicContextMenu.java
+++ ptolemy/vergil/toolbox/BasicContextMenu.java
@@ -30,12 +30,16 @@
 
 package ptolemy.vergil.toolbox;
 
+// Ptolemy imports.
 import ptolemy.actor.*;
 import ptolemy.actor.gui.*;
+import ptolemy.data.expr.Parameter;
 import ptolemy.kernel.*;
 import ptolemy.kernel.util.*;
 import ptolemy.gui.*;
 import ptolemy.moml.*;
+
+// Diva imports.
 import diva.gui.*;
 import diva.gui.toolbox.*;
 import diva.graph.*; 
@@ -45,14 +49,17 @@ import diva.canvas.connector.*;
 import diva.canvas.event.*;
 import diva.canvas.interactor.*;
 import diva.canvas.toolbox.*;
-import java.awt.geom.Rectangle2D;
 import diva.util.Filter;
+
+// Java imports.
+import java.awt.geom.Rectangle2D;
 import java.awt.*;
 import java.awt.event.InputEvent;
 import java.awt.event.ActionEvent;
 import java.util.HashMap;
 import java.util.Enumeration;
 import java.util.Iterator;
+import java.util.List;
 import java.net.URL;
 import javax.swing.*;
 import javax.swing.event.*;
@@ -63,7 +70,7 @@ import javax.swing.event.*;
 This is a base class for context menus for most objects in the vergil
 editor.  It contains a single action, for editing the parameters of 
 a named object.  This action creates configuration widgets using
-the createEditor() method of the Configurer class.
+the Configurer class.
 
 This class also contains methods that make it simple to add new actions to
 this menu.
@@ -83,15 +90,17 @@ public class BasicContextMenu extends JPopupMenu {
 
 		// Create a dialog for configuring the object.
                 try {
-                    JPanel panel = Configurer.createEditor(_target);
-                    String[] buttons = {"OK"};
+                    Configurer panel = new Configurer(_target);
                     // FIXME: First argument below should a a parent window
                     // (a JFrame).
                     ComponentDialog dialog = new ComponentDialog(
                             null,
                             "Edit parameters for " + _target.getName(),
-                            panel,
-                            buttons);
+                            panel);
+                    if (!(dialog.buttonPressed().equals("OK"))) {
+                        // Restore original parameter values.
+                        panel.restore();
+                    }
                 } catch (IllegalActionException ex) {
                     // FIXME: How are errors reported in Vergil?
                     System.out.println(ex.toString());
@@ -99,6 +108,66 @@ public class BasicContextMenu extends JPopupMenu {
 	    }
 	};
 	add(action, "Edit Parameters");
+
+	action = new AbstractAction ("Configure Parameter") {
+	    public void actionPerformed(ActionEvent e) {
+
+		// Create a dialog for configuring a parameter.
+// FIXME                try {
+                    List parameterList = _target.attributeList(Parameter.class);
+                    String[] parameterNames = new String[parameterList.size()];
+                    Iterator parameters = parameterList.iterator();
+                    int i = 0;
+                    while (parameters.hasNext()) {
+                        parameterNames[i++] =
+                                ((NamedObj)parameters.next()).getName();
+                    }
+                    Query query = new Query();
+                    query.addChoice("Parameter", "Parameter", parameterNames,
+                             parameterNames[0]);
+                    // FIXME: First argument below should a a parent window
+                    // (a JFrame).
+                    ComponentDialog dialog = new ComponentDialog(
+                            null,
+                            "Select a parameter of " + _target.getName(),
+                            query);
+                    if (dialog.buttonPressed().equals("OK")) {
+                        String parameterName = query.stringValue("Parameter");
+                        query = new Query();
+                        query.setTextWidth(25);
+                        String[] choices = {"none", "check box",
+                                "choice", "radio button", "slider"};
+                        query.addChoice("Style", "Interaction style",
+                                choices, choices[0]);
+                        // FIXME: First argument below should a a parent window
+                        // (a JFrame).
+                        dialog = new ComponentDialog(
+                                null,
+                                "Configure parameter " + parameterName +
+                                " of " + _target.getName(),
+                                query);
+                        if (dialog.buttonPressed().equals("OK")) {
+                            String choiceResponse = query.stringValue("Style");
+                            Attribute attribute
+                                    = _target.getAttribute(parameterName);
+                            if (choiceResponse.equals("check box")) {
+                                CheckBoxStyle style
+                                        = new CheckBoxStyle(attribute,
+                                        attribute.uniqueName("style"));
+                            }
+                            // FIXME...  need to deal with other styles.
+                        }
+                    }
+/* FIXME
+                } catch (IllegalActionException ex) {
+                    // FIXME: How are errors reported in Vergil?
+                    System.out.println(ex.toString());
+                }
+*/
+	    }
+	};
+	add(action, "Configure Parameters");
+
     }
 
     /** Add an action to this menu and return the menu item created.  If
