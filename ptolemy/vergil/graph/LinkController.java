5591e1055b1e439354965fafdb34935be8b8f339
cxh <cxh>
2000-01-18 19:28:43 +0000

    Changed copyright to 2000

--- ptolemy/vergil/graph/LinkController.java
+++ ptolemy/vergil/graph/LinkController.java
@@ -1,6 +1,6 @@
 /* The edge controller for links between ports and relations.
 
- Copyright (c) 1998-1999 The Regents of the University of California.
+ Copyright (c) 1998-2000 The Regents of the University of California.
  All rights reserved.
  Permission is hereby granted, without written agreement and without
  license or royalty fees, to use, copy, modify, and distribute this
