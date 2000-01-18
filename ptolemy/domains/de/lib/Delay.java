9b538b5ea30325c6d829f86b01f9e9cf6fdcd5b5
cxh <cxh>
2000-01-18 22:57:42 +0000

    Added @AcceptedRating

--- ptolemy/domains/de/lib/Delay.java
+++ ptolemy/domains/de/lib/Delay.java
@@ -23,6 +23,9 @@
 
                                         PT_COPYRIGHT_VERSION_2
                                         COPYRIGHTENDKEY
+
+@ProposedRating Red (cxh@eecs.berkeley.edu)
+@AcceptedRating Red (cxh@eecs.berkeley.edu)
 */
 
 package ptolemy.domains.de.lib;
