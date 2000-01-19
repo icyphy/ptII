89206bc3418502cd5240ce3bc557e9822db49982
cxh <cxh>
2000-01-19 19:20:55 +0000

    Fixed header

--- ptolemy/domains/de/lib/test/SimpleDelay.java
+++ ptolemy/domains/de/lib/test/SimpleDelay.java
@@ -1,5 +1,6 @@
-// Example actor described in the DE chapter of the design doc.
- Copyright (c) 1997-2000 The Regents of the University of California.
+/* Example actor described in the DE chapter of the design doc.
+
+ Copyright (c) 1999-2000 The Regents of the University of California.
  All rights reserved.
  Permission is hereby granted, without written agreement and without
  license or royalty fees, to use, copy, modify, and distribute this
