46479f6856666fa75a6f9e1f5dbab1b653c1d03f
cxh <cxh>
2000-01-19 18:33:44 +0000

    Changed copyright to 2000

--- bin/histogram.bat
+++ bin/histogram.bat
@@ -3,7 +3,7 @@ rem MSDOS batch script to start ptplot
 
 rem @author Edward A. Lee
 rem @version $Id$
-rem @copyright: Copyright (c) 1997-1999
+rem @copyright: Copyright (c) 1997-2000
 rem The Regents of the University of California.
 
 java -classpath %PTII% ptolemy.plot.plotml.HistogramMLApplication %1 %2 %3 %4 %5 %6 %7 %8 %9
