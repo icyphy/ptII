21531f85da9b831c2d3bbef5716842481cd9605f
cxh <cxh>
2000-01-20 01:18:15 +0000

    Spelling: trsndpsrent

--- ptolemy/domains/ct/kernel/CTTransparentDirector.java
+++ ptolemy/domains/ct/kernel/CTTransparentDirector.java
@@ -32,7 +32,7 @@ package ptolemy.domains.ct.kernel;
 //////////////////////////////////////////////////////////////////////////
 //// CTTransparentDirector
 /**
-Interface for CT trsndpsrent directors. It defines methods to support the
+Interface for CT transparent directors. It defines methods to support the
 outside CTDirector. The methods are about step size control. After the internal
 CT subsystem finishes one integration step, its step size control information
 should be accessible from the outside CT director.
