559c028a74f019e7be5e5a33d8f751b8abd5808b
cxh <cxh>
2000-01-20 01:03:19 +0000

    Changed copyright to 2000

--- ptolemy/domains/csp/lib/CSPSieve.java
+++ ptolemy/domains/csp/lib/CSPSieve.java
@@ -1,7 +1,7 @@
 /* Sieve filtering out all multiples of a given number for
 the Sieve of Eratosthenes.
 
- Copyright (c) 1997-1999 The Regents of the University of California.
+ Copyright (c) 1997-2000 The Regents of the University of California.
  All rights reserved.
  Permission is hereby granted, without written agreement and without
  license or royalty fees, to use, copy, modify, and distribute this
