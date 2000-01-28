3d8c6d24baa7bf8ec1b7e148def516e2986e2b57
cxh <cxh>
2000-01-28 08:53:44 +0000

    Copyright date

--- ptolemy/plot/EditablePlot.java
+++ ptolemy/plot/EditablePlot.java
@@ -1,6 +1,6 @@
 /* Extension of plot that allows interactive modification of plot data.
 
- Copyright (c) 1998 The Regents of the University of California.
+ Copyright (c) 1998-2000 The Regents of the University of California.
  All rights reserved.
  Permission is hereby granted, without written agreement and without
  license or royalty fees, to use, copy, modify, and distribute this
