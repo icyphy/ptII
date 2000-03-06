7581c792a49b3f547223fc2e96bb7f018b2448e0
vogel <vogel>
2000-03-06 17:36:39 +0000

    Improved audio demos.

--- ptolemy/domains/sdf/lib/javasound/demo/FMSynthDemo/FMSynthDemo.java
+++ ptolemy/domains/sdf/lib/javasound/demo/FMSynthDemo/FMSynthDemo.java
@@ -66,82 +66,64 @@ import ptolemy.plot.*;
 //////////////////////////////////////////////////////////////////////////
 //// FMSynthDemo
 /**
-FIXME
+This applet consists of a simple FM instrument implemented in the SDF domain. The Chowning FM synthesis technique is used. In Chowning FM synthesis, a carrier oscillator has its frequency modulated by a modulating oscillator. The slider labled "Carrier Frequency" sets the fundamental frequency (pitch). The slider labled "Harmonicity Ratio" sets the modulating oscillator's frequency, which is the product of the harmonicity ratio and the carrier frequency. The slider labled "Modulation" scales the output of the modulator oscillator before it enters the carrier oscillator. 
 <p>
 Note: Requires Java 2 v1.3.0 RC1 or later.
 
 @author Brian K. Vogel
 @version $Id$
 */
-public class FMSynthDemo extends TypedCompositeActor {   
+public class FMSynthDemo extends SDFApplet {   
     
 
-    public FMSynthDemo() {
-        super();
-	create();
-    }
 
-    public FMSynthDemo(Workspace workspace) {
-	super(workspace);
-	create();
-    }
-
-    public FMSynthDemo(TypedCompositeActor container, String name)
-            throws IllegalActionException, NameDuplicationException {
-        super(container, name);
-	create();
-    }
-
-
-    public void create() {
-
+    public void init() {
+	super.init();
 	try {
 	    
 
 
-	    System.out.println("Create invoked");
-
-            SDFDirector _sdfDirector = new SDFDirector(this, "SDFDirector");
-
-            SDFScheduler scheduler = new SDFScheduler(_workspace);
-
-            _sdfDirector.setScheduler(scheduler);
-            _sdfDirector.setScheduleValid(false);
+	   
 
 
 	    // gui stuff goes here.
 
 	    //Create the top-level container and add contents to it.
-	    JFrame frame = new JFrame("FM Synthesizer");
+	    //JFrame frame = new JFrame("FM Synthesizer");
 	    
-	     JPanel controlpanel = new JPanel();
+	    //JPanel controlpanel = new JPanel();
 
-	     controlpanel.setLayout(new BorderLayout());
+	    //controlpanel.setLayout(new BorderLayout());
+	    getContentPane().add(_createRunControls(2), BorderLayout.SOUTH);
+
+	    //getContentPane().setLayout(
+	    //      new BoxLayout(getContentPane(), BorderLayout.CENTER));
 
 	     //PtolemyQuery _ptQuery = new PtolemyQuery(_sdfDirector);
 	     PtolemyQuery _ptQuery = new PtolemyQuery();
-	    
-	    controlpanel.add("West", _ptQuery);
+	     getContentPane().add(_ptQuery);
+
+	     //controlpanel.add("West", _ptQuery);
 
 	    _ptQuery.addSlider("carrierFreqSlider", "Carrier Frequency",
 			     1884, 200, 8000);
 	    _ptQuery.addSlider("harmonicitySlider", "Harmonicity Ratio",
-			     1000, 200, 8000);
+			     2000, 200, 8000);
 	    _ptQuery.addSlider("modulationSlider", "Modulation",
-			     100, 0, 1000);
+			     100, 0, 500);
 
 	    // _ptQuery.addLine("pitchLine", "Pitch Scale Factor x 1000",
 	    //	     "1000");
 
-	    frame.getContentPane().add(controlpanel, BorderLayout.CENTER);
+	    //frame.getContentPane().add(controlpanel, BorderLayout.CENTER);
 	    //Finish setting up the frame, and show it.
-	    frame.addWindowListener(new WindowAdapter() {
-		    public void windowClosing(WindowEvent e) {
-			System.exit(0);
-		    }
-		});
-	    frame.pack();
-	    frame.setVisible(true);
+	    //frame.addWindowListener(new WindowAdapter() {
+	    //    public void windowClosing(WindowEvent e) {
+	    //	System.exit(0);
+	    //    }
+	    //});
+	    //frame.pack();
+	    //frame.setVisible(true);
 
 	   
 
@@ -189,7 +171,7 @@ public class FMSynthDemo extends TypedCompositeActor {
 	    //soundSource.channels.setToken(new IntToken(channels));
 	    //soundSource.bufferSize.setToken(new IntToken(buffSize));
 
-            AudioSink soundSink = new AudioSink(this, "soundSink");
+            AudioSink soundSink = new AudioSink(_toplevel, "soundSink");
 	  soundSink.pathName.setToken(new StringToken("outputFile.wav"));
 	  soundSink.sink.setToken(new StringToken("speaker"));
 	  //soundSink.sink.setToken(new StringToken("file"));
@@ -202,7 +184,7 @@ public class FMSynthDemo extends TypedCompositeActor {
 	  //**************************************
 	  //Ramp ramp1 = new Ramp(this, "rampSource1");
 	  //   ** OR **
-	  SDFRamp ramp1 = new SDFRamp(this, "ramp1");
+	  SDFRamp ramp1 = new SDFRamp(_toplevel, "ramp1");
 	  ramp1.rate.setToken(new IntToken(cPRate));
 	  ramp1.step.setToken(new DoubleToken(1.0/sampleRate));
 	  //**************************************
@@ -211,88 +193,88 @@ public class FMSynthDemo extends TypedCompositeActor {
 	  //ramp2.rate.setToken(new IntToken(cPRate));
 	  //ramp2.step.setToken(new DoubleToken(1.0/sampleRate));
 
-	  SDFConst carrierFreq = new SDFConst(this, "carrierFreq");
+	  SDFConst carrierFreq = new SDFConst(_toplevel, "carrierFreq");
 	  carrierFreq.rate.setToken(new IntToken(cPRate));
 	  carrierFreq.value.setToken(new DoubleToken(1884));
 	  //carrierFreq.output.setTypeEquals(BaseType.DOUBLE);
 
-	  SDFConst harmonicityRatio = new SDFConst(this, "harmonicityRatio");
+	  SDFConst harmonicityRatio = new SDFConst(_toplevel, "harmonicityRatio");
 	  harmonicityRatio.rate.setToken(new IntToken(cPRate));
-	  harmonicityRatio.value.setToken(new DoubleToken(1000));
+	  harmonicityRatio.value.setToken(new DoubleToken(2000));
 	  //harmonicityRatio.output.setTypeEquals(BaseType.DOUBLE);
 
-	  SDFConst modulatorAmp = new SDFConst(this, "modulatorAmp");
+	  SDFConst modulatorAmp = new SDFConst(_toplevel, "modulatorAmp");
 	  modulatorAmp.rate.setToken(new IntToken(cPRate));
 	  modulatorAmp.value.setToken(new DoubleToken(100));
 	  //modulatorAmp.output.setTypeEquals(BaseType.DOUBLE);
 	  
-	  SDFConst modulatorConst = new SDFConst(this, "modulatorConst");
+	  SDFConst modulatorConst = new SDFConst(_toplevel, "modulatorConst");
 	  modulatorConst.rate.setToken(new IntToken(cPRate));
 	  modulatorConst.value.setToken(new DoubleToken(0.001));
 	  //modulatorConst.output.setTypeEquals(BaseType.DOUBLE);
 
-	  SDFConst harmonicityConst = new SDFConst(this, "harmonicityConst");
+	  SDFConst harmonicityConst = new SDFConst(_toplevel, "harmonicityConst");
 	  harmonicityConst.rate.setToken(new IntToken(cPRate));
 	  harmonicityConst.value.setToken(new DoubleToken(0.001));
 	  //harmonicityConst.output.setTypeEquals(BaseType.DOUBLE);
 
-	  SDFMultiply mult1 = new SDFMultiply(this, "mult1");
+	  SDFMultiply mult1 = new SDFMultiply(_toplevel, "mult1");
 	  mult1.rate.setToken(new IntToken(cPRate));
 	  //mult1.input.setTypeEquals(BaseType.DOUBLE);
 	  //mult1.input2.setTypeEquals(BaseType.DOUBLE);
 
-	  SDFMultiply mult2 = new SDFMultiply(this, "mult2");
+	  SDFMultiply mult2 = new SDFMultiply(_toplevel, "mult2");
 	  mult2.rate.setToken(new IntToken(cPRate));
 	  //mult2.input.setTypeEquals(BaseType.DOUBLE);
 	  //mult2.input2.setTypeEquals(BaseType.DOUBLE);
 
-	  SDFMultiply mult3 = new SDFMultiply(this, "mult3");
+	  SDFMultiply mult3 = new SDFMultiply(_toplevel, "mult3");
 	  mult3.rate.setToken(new IntToken(cPRate));
 	  //mult3.input.setTypeEquals(BaseType.DOUBLE);
 	  //mult3.input2.setTypeEquals(BaseType.DOUBLE);
 
-	  SDFMultiply mult4 = new SDFMultiply(this, "mult4");
+	  SDFMultiply mult4 = new SDFMultiply(_toplevel, "mult4");
 	  mult4.rate.setToken(new IntToken(cPRate));
 	  //mult4.input.setTypeEquals(BaseType.DOUBLE);
 	  //mult4.input2.setTypeEquals(BaseType.DOUBLE);
 
-	  SDFAdd add1 = new SDFAdd(this, "add1");
+	  SDFAdd add1 = new SDFAdd(_toplevel, "add1");
 	  add1.rate.setToken(new IntToken(cPRate));
 	  //add1.input.setTypeEquals(BaseType.DOUBLE);
 	  //add1.input2.setTypeEquals(BaseType.DOUBLE);
 	  
 
 
-	  SineFM carrier = new SineFM(this, "carrier");
+	  SineFM carrier = new SineFM(_toplevel, "carrier");
 	  carrier.rate.setToken(new IntToken(cPRate));
 	  carrier.amplitude.setToken(new DoubleToken(0.5));
 
-	  SineFM modulator = new SineFM(this, "modulator");
+	  SineFM modulator = new SineFM(_toplevel, "modulator");
 	  modulator.rate.setToken(new IntToken(cPRate));
 
 
-	  this.connect(carrier.output, soundSink.input);
-	  this.connect(mult1.output, add1.input2);
-	  this.connect(add1.output, carrier.omega);
-	  this.connect(modulator.output, mult1.input);
+	  _toplevel.connect(carrier.output, soundSink.input);
+	  _toplevel.connect(mult1.output, add1.input2);
+	  _toplevel.connect(add1.output, carrier.omega);
+	  _toplevel.connect(modulator.output, mult1.input);
 	  //this.connect(modulatorAmp.output, mult1.input2);
-	  this.connect(harmonicityRatio.output, mult2.input);
-	  this.connect(harmonicityConst.output, mult2.input2);
-	  this.connect(mult2.output, mult3.input2);
-	  this.connect(mult3.output, modulator.omega);
-	  this.connect(modulatorAmp.output, mult4.input);
-	  this.connect(modulatorConst.output, mult4.input2);
-	  this.connect(mult4.output, mult1.input2);
+	  _toplevel.connect(harmonicityRatio.output, mult2.input);
+	  _toplevel.connect(harmonicityConst.output, mult2.input2);
+	  _toplevel.connect(mult2.output, mult3.input2);
+	  _toplevel.connect(mult3.output, modulator.omega);
+	  _toplevel.connect(modulatorAmp.output, mult4.input);
+	  _toplevel.connect(modulatorConst.output, mult4.input2);
+	  _toplevel.connect(mult4.output, mult1.input2);
 
 	  TypedIORelation rel1 =
-	      (TypedIORelation)this.newRelation("rel1");
+	      (TypedIORelation)_toplevel.newRelation("rel1");
 	  //rel1.setWidth(2);
 	  carrierFreq.output.link(rel1);
 	  mult3.input.link(rel1);
 	  add1.input.link(rel1);
 
 	  TypedIORelation rel2 =
-	      (TypedIORelation)this.newRelation("rel2");
+	      (TypedIORelation)_toplevel.newRelation("rel2");
 	  //rel1.setWidth(2);
 	  ramp1.output.link(rel2);
 	  carrier.input.link(rel2);
@@ -308,16 +290,13 @@ public class FMSynthDemo extends TypedCompositeActor {
 	  _ptQuery.attachParameter(carrierFreq.value, "carrierFreqSlider");
 	  _ptQuery.attachParameter(harmonicityRatio.value, "harmonicitySlider");
 	  _ptQuery.attachParameter(modulatorAmp.value, "modulationSlider");
-	 
 
 	  //exportMoML(new OutputStreamWriter(System.out));
 	  
-	  try {
-	      Thread.sleep(5000);
-	  } catch (InterruptedException e){
-	  }
-
-
+	  //try {
+	  //    Thread.sleep(5000);
+	  //} catch (InterruptedException e){
+	  //}
 
 	  //sine1.output.link(rel1);
 	  //soundSink.input.link(rel1);
@@ -327,8 +306,26 @@ public class FMSynthDemo extends TypedCompositeActor {
         }
     }
 
+     /** Report that the manager state has changed.  This is
+     *  called by the manager. Override this method so that
+     *  state changes are not displayed by the applet. When
+     *  the slider is moved, "performing mutations" is displayed,
+     * and slows the model down. Overriding that behavior here
+     *  will hopefully improved performance.
+     */
+    public void managerStateChanged(Manager manager) {
+        
+    }
 
+    ///////////////////////////////////////////////////////////////////
+    ////                         protected methods                 ////
 
-
+    /** Execute the system.  This overrides the base class to read the
+     *  values in the query box first.
+     *  @exception IllegalActionException Not thrown in this base class.
+     */
+    protected void _go() throws IllegalActionException {
+        super._go();
+    }
 }
 
