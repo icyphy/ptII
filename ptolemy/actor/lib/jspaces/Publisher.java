89d5485be59ca2c43dcb2d8fd660d47fc758fba5
yuhong <yuhong>
2000-03-07 19:34:27 +0000

    First checkin, unfinished.

---
+++ ptolemy/actor/lib/jspaces/Publisher.java
@@ -0,0 +1,141 @@
+/* An actor that sends entries to a Java Space.
+
+ Copyright (c) 1998-2000 The Regents of the University of California.
+ All rights reserved.
+ Permission is hereby granted, without written agreement and without
+ license or royalty fees, to use, copy, modify, and distribute this
+ software and its documentation for any purpose, provided that the above
+ copyright notice and the following two paragraphs appear in all copies
+ of this software.
+
+ IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+ FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+ ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+ THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+ SUCH DAMAGE.
+
+ THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+ INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+ PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+ CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ ENHANCEMENTS, OR MODIFICATIONS.
+
+                                        PT_COPYRIGHT_VERSION_2
+                                        COPYRIGHTENDKEY
+
+@ProposedRating Red (liuj@eecs.berkeley.edu)
+@AcceptedRating Red (yuhong@eecs.berkeley.edu)
+*/
+
+package ptolemy.actor.lib.jspaces;
+
+import ptolemy.data.expr.Parameter;
+import ptolemy.data.Token;
+import ptolemy.actor.TypedCompositeActor;
+// import ptolemy.kernel.util.*;
+
+//////////////////////////////////////////////////////////////////////////
+//// Publisher
+/**
+An actor that sends entries to a Java Space.
+
+@author Jie Liu, Yuhong Xiong
+@version $Id$
+*/
+
+public class Publisher extends Sink {
+
+    /** Construct an actor with the given container and name.
+     *  @param container The container.
+     *  @param name The name of this actor.
+     *  @exception IllegalActionException If the entity cannot be contained
+     *   by the proposed container.
+     *  @exception NameDuplicationException If the container already has an
+     *   actor with this name.
+     */
+    public Publisher(TypedCompositeActor container, String name)
+            throws NameDuplicationException, IllegalActionException  {
+        super(container, name);
+    	jspaceName = new Parameter(this, "jspaceName", 
+                new StringToken("JaveSpaces"));
+        jspaceName.setTypeEquals(BaseType.STRING);
+        entryName = new Parameter(this, "entryName", 
+                new StringToken(""));
+        entryName.setTypeEquals(BaseType.STRING);
+        startingSerialNumber = new Parameter(this, "startingSerialNumber", 
+                new LongToken(0));
+        startingSerialNumber.setTypeEquals(BaseType.LONG);
+    }
+
+    ///////////////////////////////////////////////////////////////////
+    ////                     ports and parameters                  ////
+
+    /** The Java Space name. The default name is "JavaSpaces" of 
+     *  type StringToken.
+     */
+    public Parameter jspaceName;
+
+    /** The name for the entries to be published. The default value is
+     *  an empty string of type StringToken.
+     */
+    public Parameter entryName;
+
+    /** The starting serial number of the entries to be published.
+     *  The entries will have increasing serial numbers.
+     *  The default value is 0 of type LongToken.
+     */  
+    public Parameter startingSerialNumber;
+
+    ///////////////////////////////////////////////////////////////////
+    ////                         public methods                    ////
+
+    /** Clone the actor into the specified workspace.
+     *  @param ws The workspace for the new object.
+     *  @return A new actor.
+     *  @exception CloneNotSupportedException If a derived class contains
+     *   an attribute that cannot be cloned.
+     */
+    public Object clone(Workspace ws)
+	    throws CloneNotSupportedException {
+	try {
+	    Publisher newobj = (Publisher)super.clone(ws);
+	    newobj.jspaceName = (Parameter)newobj.getAttribute("jspaceName");
+            newobj.entryName = (Parameter)newobj.getAttribute("entryName");
+            newobj.startingSerialNumber = 
+                (Parameter)newobj.getAttribute("startingSerialNumber");
+	    return newobj;
+        } catch (CloneNotSupportedException ex) {
+            // Errors should not occur here...
+            throw new InternalErrorException(
+                    "Clone failed: " + ex.getMessage());
+        }
+    }
+
+    /** Find the JavaSpaces according to the jspaceName parameter.
+     */
+    public void preinitialize() throws IllegalActionException {
+    }
+
+
+    /** Read at most one input token from each channel of the input
+     *  and write an entry into the space for each token read.
+     *  @exception IllegalActionException Not thrown in this base class.
+     */
+    public void fire() throws IllegalActionException {
+        for (int i = 0; i < input.getWidth(); i++) {
+            if (input.hasToken(i)) {
+                Token token = input.get(i);
+
+
+
+
+            }
+        }
+    }
+
+    ///////////////////////////////////////////////////////////////////
+    ////                         private methods                   ////
+    private long _currentSerialNumber;
+}
+
