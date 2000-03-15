8d6b753e04e71a016188f23316ba096c6a23e8b2
davisj <davisj>
2000-03-15 01:47:11 +0000

    Added ProcessSink which extends ptolemy.actor.lib.Sink so that the fire method communicates.

---
+++ ptolemy/actor/process/test/ProcessSink.java
@@ -0,0 +1,80 @@
+/* Base class for simple sink actors.
+
+ Copyright (c) 1998-2000 The Regents of the University of California.
+ All rights reserved.
+ Permission is hereby granted, without written agreement and without
+ license or royalty fees, to use, copy, modify, and distribute this
+ software and its documentation for any purpose, provided that the above
+ copyright notice and the following two paragraphs appear in all copies
+ of this software.
+
+ IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+ FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+ ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+ THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+ SUCH DAMAGE.
+
+ THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+ INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+ PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+ CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ ENHANCEMENTS, OR MODIFICATIONS.
+
+                                        PT_COPYRIGHT_VERSION_2
+                                        COPYRIGHTENDKEY
+
+@ProposedRating Green (eal@eecs.berkeley.edu)
+@AcceptedRating Green (bilung@eecs.berkeley.edu)
+*/
+
+package ptolemy.actor.process.test;
+
+import ptolemy.actor.*;
+import ptolemy.actor.lib.*;
+import ptolemy.kernel.util.*;
+
+//////////////////////////////////////////////////////////////////////////
+//// ProcessSink
+/**
+Base class for simple data sinks.  This class provides an input port,
+exposed as a public variable, and a clone method that ensures that the
+public variable is correctly set in the clone.  The main reason for this
+class to ensure that the input ports of all sinks have the same name
+and are correctly handled in cloned actors.
+
+@author John S. Davis II
+@version $Id$
+*/
+
+public class ProcessSink extends Sink {
+
+    /** Construct an actor with an input multiport.
+     *  @param container The container.
+     *  @param name The name of this actor.
+     *  @exception IllegalActionException If the entity cannot be contained
+     *   by the proposed container.
+     *  @exception NameDuplicationException If the container already has an
+     *   actor with this name.
+     */
+    public ProcessSink(TypedCompositeActor container, String name)
+            throws NameDuplicationException, IllegalActionException  {
+        super(container, name);
+    }
+
+    ///////////////////////////////////////////////////////////////////
+    ////                         public methods                    ////
+
+    /** 
+     */
+    public void fire() throws IllegalActionException {
+        input.get(0);
+    }
+    
+    /** 
+     */
+    public boolean postfire() {
+        return false;
+    }
+
+}
