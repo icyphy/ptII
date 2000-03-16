805a8871bba5cce6a7f0b34c0206240c17a865f7
davisj <davisj>
2000-03-16 01:50:15 +0000

    Test file.

--- ptolemy/actor/process/test/fire.java
+++ ptolemy/actor/process/test/fire.java
@@ -1,14 +1,64 @@
 
 
 
-/** Psuedocode. 
- *  Includes all functionality. Does not include efficient
- *  function calls.
+/**
  */
 public class fire extends ProcessDirector {
-    /**
+    /** Psuedocode. 
+     *  Includes all functionality. Does not include efficient
+     *  function calls.
      */
     public void testFire1() throws IllegalActionException {
+        _continueFireMethod = true;
+        synchronized(this) {
+            while( _continueFireMethod ) {
+                _continueFireMethod = false;
+                while( !_areActorsDeadlocked() && !_areActorsStopped() ) {
+                    workspace.wait(this);
+                }
+                
+                if( _areActorsDeadlocked() ) {
+                    if( externalReadBlock ) {
+                        // This avoids a race condition.
+                        while( _areActorsDeadlocked() && !inputBlocked() ) {
+                            workspace.wait();
+                        }
+                        while( _areActorsDeadlocked() && inputBlocked() ) {
+                            if( execDir instanceof ProcessDirector ) {
+                                execDir.registerBlockedBranchReceivers();
+                                workspace.wait();
+                            } else {
+                                stopInputBranchController();
+                                if( _areActorsDeadlocked() ) {
+                                    _notDone = false;
+                                    return;
+                                } else if( !_areActorsDeadlocked() ) {
+                         	    _continueFireMethod = true;
+                                }
+                                _notDone = false;
+                                return;
+                            }
+                        }
+                    } else {
+                        
+                    }
+                    if( !_areActorsDeadlocked() || !inputBlocked() ) {
+                        _continueFireMethod = true;
+                    }
+                } else if( _areActorsStopped() ) {
+                    stopInputBranchController();
+                    _continueFireMethod = false;
+                    _notDone = true;
+                }
+            }
+        }
+    }
+
+    /** Psuedocode. 
+     *  Assumes external read blocks. Does not include efficient
+     *  function calls.
+     */
+    public void testFire2() throws IllegalActionException {
         _continueFireMethod = true;
         synchronized(this) {
             while( _continueFireMethod ) {
@@ -42,43 +92,6 @@ public class fire extends ProcessDirector {
                         workspace.wait(this);
                     }
                     _notDone = true;
-                }
-            }
-        }
-    }
-
-    /**
-     */
-    public void testFire2() throws IllegalActionException {
-        _continueFireMethod = true;
-        synchronized(this) {
-            while( _continueFireMethod ) {
-                _continueFireMethod = false;
-                while( !_areActorsDeadlocked() && !_areActorsStopped() ) {
-                    workspace.wait(this);
-                }
-            
-                if( externalReadBlock ) {
-                    while( _areActorsDeadlocked() && !inputBlocked() ) {
-                        workspace.wait();
-                    }
-                }
-                
-                while( _areActorsDeadlocked() && inputBlocked() ) {
-                    if( externalReadBlock && (execDir instanceof ProcessDirector ) {
-                        execDir.registerBlockedBranchReceivers();
-                        workspace.wait();
-                    } else if( externalReadBlock ) {
-                        _notDone = true;
-                        return;
-                    } else {
-                        _notDone = false;
-                        return;
-                    }
-                }
-                
-                if( !_areActorsDeadlocked && inputBlocked() ) {
-                    _continueFireMethod = true;
                 }
             }
         }
