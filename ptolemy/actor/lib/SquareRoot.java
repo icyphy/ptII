c330e99a964e7cadd139573988c8a4415ef0d921
shankarr <shankarr>
2000-04-21 20:55:47 +0000

    Added Square Root actor and updated makefile

---
+++ ptolemy/actor/lib/SquareRoot.java
@@ -0,0 +1,96 @@
+/* An actor that outputs the square root of the input.
+
+ Copyright (c) 1998-2000 The Regents of the University of California.
+ All rights reserved.
+ Permission is hereby granted, without written agreement and without
+ license or royalty fees, to use, copy, modify, and distribute this
+ software and its documentation for any purpose, provided that the above
+ copyright notice and the following two paragraphs appear in all copies
+ of this software.
+
+ IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+ FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+ ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+ THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+ SUCH DAMAGE.
+
+ THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+ INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+ PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+ CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ ENHANCEMENTS, OR MODIFICATIONS.
+
+                                        PT_COPYRIGHT_VERSION_2
+                                        COPYRIGHTENDKEY
+
+@ProposedRating Red (srao@eecs.berkeley.edu)
+@AcceptedRating Red (srao@eecs.berkeley.edu)
+*/
+
+package ptolemy.actor.lib;
+
+import ptolemy.actor.*;
+import ptolemy.kernel.util.*;
+import ptolemy.data.*;
+import ptolemy.data.type.BaseType;
+import ptolemy.data.expr.Parameter;
+
+//////////////////////////////////////////////////////////////////////////
+//// SquareRoot
+/**
+Produce an output token on each firing with a value that is
+equal to the square root of the input. The input and output types
+are DoubleToken. 
+@author Shankar Rao
+@version $Id$
+*/
+
+public class SquareRoot extends Transformer {
+
+    /** Construct an actor with the given container and name.
+     *  @param container The container.
+     *  @param name The name of this actor.
+     *  @exception IllegalActionException If the actor cannot be contained
+     *   by the proposed container.
+     *  @exception NameDuplicationException If the container already has an
+     *   actor with this name.
+     */
+    public SquareRoot(TypedCompositeActor container, String name)
+            throws NameDuplicationException, IllegalActionException  {
+        super(container, name);
+
+        // parameters
+        input.setTypeEquals(BaseType.DOUBLE);
+        output.setTypeEquals(BaseType.DOUBLE);
+    }
+
+    ///////////////////////////////////////////////////////////////////
+    ////                         public methods                    ////
+
+    /** Clone the actor into the specified workspace. This calls the
+     *  base class and then sets the parameters of the new actor.
+     *  @param ws The workspace for the new object.
+     *  @return A new actor.
+     *  @exception CloneNotSupportedException If a derived class contains
+     *   an attribute that cannot be cloned.
+     */
+    public Object clone(Workspace ws)
+	    throws CloneNotSupportedException {
+        SquareRoot newobj = (SquareRoot)super.clone(ws);
+        return newobj;
+    }
+
+    /** Compute the square root of the input.  If there is no input, then
+     *  produce no output.
+     *  @exception IllegalActionException If there is no director.
+     */
+    public void fire() throws IllegalActionException {
+        if (input.hasToken(0)) {
+            DoubleToken in = (DoubleToken)input.get(0);
+            double result = Math.sqrt(in.doubleValue());
+            output.send(0, new DoubleToken(result));
+        }
+    }
+}
+
