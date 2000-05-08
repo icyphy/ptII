4d54f016ee77180d4707ef9383fb433ced4da75c
cxh <cxh>
2000-05-08 08:31:31 +0000

    clone(): declare it to trhow CloneNotSupported

--- ptolemy/domains/ct/kernel/CTMultiSolverDirector.java
+++ ptolemy/domains/ct/kernel/CTMultiSolverDirector.java
@@ -177,8 +177,11 @@ public class CTMultiSolverDirector extends CTDirector {
      *  actor will have the same parameter values as the old.
      *  @param ws The workspace for the new object.
      *  @return A new actor.
+     *  @exception CloneNotSupportedException If a derived class contains
+     *   an attribute that cannot be cloned.
      */
-    public Object clone(Workspace ws) {
+    public Object clone(Workspace ws) 
+	    throws CloneNotSupportedException {
         CTMultiSolverDirector newobj =
             (CTMultiSolverDirector)(super.clone(ws));
         newobj.ODESolver =
