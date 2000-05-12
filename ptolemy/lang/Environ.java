69fe74f3c37b5d86cfb33da084c983f8bc60e981
ctsay <ctsay>
2000-05-12 00:40:57 +0000

    Formating changes

--- ptolemy/lang/Environ.java
+++ ptolemy/lang/Environ.java
@@ -49,22 +49,22 @@ public class Environ {
 
     /** Construct an empty environment. */
     public Environ() {
-      this(null, new LinkedList());
+        this(null, new LinkedList());
     }
  
     /** Constuct an environment nested inside the parent argument, without its own
      *  proper Decl's. 
      */
     public Environ(Environ parent) {
-      this(parent, new LinkedList());
+        this(parent, new LinkedList());
     }
 
     /** Constuct an environment nested inside the parent argument, with the given
      *  List of Decl's in this environment itself. 
      */
     public Environ(Environ parent, List declList) {
-      _parent = parent;
-      _declList = declList;
+        _parent = parent;
+        _declList = declList;
     }
 
     public Environ parent() {
@@ -75,149 +75,149 @@ public class Environ {
      *  affect any Environs in which this is nested.
      */
     public void add(Decl decl) {
-      _declList.add(decl);
+        _declList.add(decl);
     }
 
     public void copyDeclList(Environ env) {
-      _declList.clear();
-      _declList.addAll(env._declList);
+        _declList.clear();
+        _declList.addAll(env._declList);
     }
 
     public Decl lookup(String name) {
-      return lookup(name, Decl.CG_ANY, new boolean[1], false);
+        return lookup(name, Decl.CG_ANY, new boolean[1], false);
     }
 
     public Decl lookup(String name, int mask) {
-      return lookup(name, mask, new boolean[1], false);
+        return lookup(name, mask, new boolean[1], false);
     }
 
     public Decl lookup(String name, boolean[] more) {
-      return lookup(name, Decl.CG_ANY, more, false);
+        return lookup(name, Decl.CG_ANY, more, false);
     }
   
     public Decl lookup(String name, int mask, boolean[] more) {
-      return lookup(name, mask, more, false);
+        return lookup(name, mask, more, false);
     }
 
     public Decl lookupProper(String name) {
-      return lookup(name, Decl.CG_ANY, new boolean[1], true);
+        return lookup(name, Decl.CG_ANY, new boolean[1], true);
     }
 
     public Decl lookupProper(String name, int mask) {
-      return lookup(name, mask, new boolean[1], true);
+        return lookup(name, mask, new boolean[1], true);
     }
 
     public Decl lookupProper(String name, boolean[] more) {
-      return lookup(name, Decl.CG_ANY, more, true);
+        return lookup(name, Decl.CG_ANY, more, true);
     }
 
     public Decl lookupProper(String name, int mask, boolean[] more) {
-      return lookup(name, mask, more, true);
+        return lookup(name, mask, more, true);
     }
 
     public Decl lookup(String name, int mask, boolean[] more, boolean proper) {
-      EnvironIter itr = lookupFirst(name, mask, proper);
+        EnvironIter itr = lookupFirst(name, mask, proper);
  
-      if (itr.hasNext()) {
-         Decl retval = (Decl) itr.next();
-         more[0] = itr.hasNext();
-         return retval;
-      }
-      more[0] = false;
-      return null;
+        if (itr.hasNext()) {
+           Decl retval = (Decl) itr.next();
+           more[0] = itr.hasNext();
+           return retval;
+        }
+        more[0] = false;
+        return null;
     }
 
     public EnvironIter lookupFirst(String name) {
-      return lookupFirst(name, Decl.CG_ANY, false);
+        return lookupFirst(name, Decl.CG_ANY, false);
     }
  
     public EnvironIter lookupFirst(String name, int mask) {
-      return lookupFirst(name, mask, false);
+        return lookupFirst(name, mask, false);
     }
 
     public EnvironIter lookupFirstProper(String name) {
-      return lookupFirst(name, Decl.CG_ANY, true);
+        return lookupFirst(name, Decl.CG_ANY, true);
     }
 
     public EnvironIter lookupFirstProper(String name, int mask) {
-      return lookupFirst(name, mask, true);
+        return lookupFirst(name, mask, true);
     }
 
     public EnvironIter lookupFirst(String name, int mask, boolean proper) {
-      Environ parent = proper ? null : _parent;
+        Environ parent = proper ? null : _parent;
 
-      return new EnvironIter(parent, _declList.listIterator(), name, mask);
+        return new EnvironIter(parent, _declList.listIterator(), name, mask);
     }
 
     public EnvironIter allDecls() {
-      return lookupFirst("*", Decl.CG_ANY, false);
+        return lookupFirst("*", Decl.CG_ANY, false);
     }
 
     public EnvironIter allDecls(int mask) {
-      return lookupFirst(Decl.ANY_NAME, mask, false);
+        return lookupFirst(Decl.ANY_NAME, mask, false);
     }
 
     public EnvironIter allDecls(String name) {
-      return lookupFirst(name, Decl.CG_ANY, false);
+        return lookupFirst(name, Decl.CG_ANY, false);
     }
 
     public ListIterator allProperDecls() {
-      return _declList.listIterator();
+        return _declList.listIterator();
     }
 
     public EnvironIter allProperDecls(int mask) {
-      return lookupFirst(Decl.ANY_NAME, mask, true);
+        return lookupFirst(Decl.ANY_NAME, mask, true);
     }
 
     public EnvironIter allProperDecls(String name) {
-      return lookupFirst(name, Decl.CG_ANY, true);
+        return lookupFirst(name, Decl.CG_ANY, true);
     }
 
     /** Return true if there is more than one matching Decl only in this Environ. */
     public boolean moreThanOne(String name, int mask) {
-      return moreThanOne(name, mask, false);
+        return moreThanOne(name, mask, false);
     }
 
     /** Return true if there is more than one matching Decl in this Environ, and 
      *  if proper is true, in the enclosing Environs.
      */
     public boolean moreThanOne(String name, int mask, boolean proper) {
-      boolean[] more = new boolean[1];
+        boolean[] more = new boolean[1];
 
-      lookup(name, mask, more, proper);
-
-      return more[0];
+        lookup(name, mask, more, proper);
+  
+        return more[0];
     }
 
     public String toString() {
-      return toString(true);
+        return toString(true);
     }
 
     public String toString(boolean recursive) {
-      ListIterator declItr = _declList.listIterator();
+        ListIterator declItr = _declList.listIterator();
 
-      StringBuffer retval = new StringBuffer("[");
+        StringBuffer retval = new StringBuffer("[");
 
-      while (declItr.hasNext()) {
-        Decl d = (Decl) declItr.next();
-        retval.append(d.toString());
-        if (declItr.hasNext()) {
-           retval.append(", ");
+        while (declItr.hasNext()) {
+          Decl d = (Decl) declItr.next();
+          retval.append(d.toString());
+          if (declItr.hasNext()) {
+             retval.append(", ");
+          }
         }
-      }
 
-      retval.append("] ");
+        retval.append("] ");
   
-      if (_parent != null) {
-         retval.append("has parent\n");
+        if (_parent != null) {
+           retval.append("has parent\n");
 
-         if (recursive) {
-            retval.append(_parent.toString(true));
-         }
-      } else {
-       retval.append("no parent\n");
-      }
-      return retval.toString();
+           if (recursive) {
+              retval.append(_parent.toString(true));
+           }
+        } else {
+           retval.append("no parent\n");
+        }
+        return retval.toString();
     }
 
     protected Environ _parent;
