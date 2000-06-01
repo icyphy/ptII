4d65f70786bfcf10fe7efd093fb7c1fd5820d855
vogel <vogel>
2000-06-01 00:56:51 +0000

    Reverted to non-vectorized version.

--- ptolemy/actor/lib/Sine.java
+++ ptolemy/actor/lib/Sine.java
@@ -57,7 +57,7 @@ the phase to pi/2.
 @version $Id$
 */
 
-public class Sine extends VectorizableTransformer {
+public class Sine extends Transformer {
 
     /** Construct an actor with the given container and name.
      *  @param container The container.
@@ -127,50 +127,14 @@ public class Sine extends VectorizableTransformer {
      *  @exception IllegalActionException If there is no director.
      */
     public void fire() throws IllegalActionException {
-        if (input.hasToken(0, _vectorLength)) {
-            input.get(0, (DoubleToken[])_inArray);
+        if (input.hasToken(0)) {
+            DoubleToken in = (DoubleToken)input.get(0);
             double A = ((DoubleToken)amplitude.getToken()).doubleValue();
             double w = ((DoubleToken)omega.getToken()).doubleValue();
             double p = ((DoubleToken)phase.getToken()).doubleValue();
-	    for (int i = 0; i < _inArray.length; i++) {
-		double result = A*Math.sin(w*_inArray[i].doubleValue()+p);
-		_resultArray[i] = new DoubleToken(result);
-	    }
-            output.send(0, _resultArray);
+
+            double result = A*Math.sin(w*in.doubleValue()+p);
+            output.send(0, new DoubleToken(result));
         }
     }
-
-    /**  Allocate DoubleToken arrays for use in the fire() method.
-      *  @exception IllegalActionException If the parent class throws it.
-      */
-    public void initialize() throws IllegalActionException {
-        super.initialize();
-        _inArray = new DoubleToken[_vectorLength];
-	_resultArray = new DoubleToken[_vectorLength];
-    }
-
-    /** Set the vector length. The vector length of an actor is
-     *  defined as the number of tokens that are consumed and/or
-     *  produced when the actor is fired. 
-     *  An exception will occur if the requested vector length is
-     *  less than 1.
-     *  @param vectorLength The requested vector length to use for
-     *   the actor.
-     *
-     *  @exception IllegalActionException If the requested vector length
-     *   is invalid or cannot be set.
-     */
-    public void setVectorLength(int vectorLength) 
-	throws IllegalActionException {
-	super.setVectorLength(vectorLength);
-	_inArray = new DoubleToken[_vectorLength];
-	_resultArray = new DoubleToken[_vectorLength];
-    }
-
-    ///////////////////////////////////////////////////////////////////
-    ////                         private variables                 ////
-
-    private DoubleToken[] _inArray;
-    private DoubleToken[] _resultArray;
-
 }
