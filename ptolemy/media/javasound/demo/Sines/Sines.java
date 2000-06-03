c33251ea1b08c48fe26b72d57ab0651e503dad12
vogel <vogel>
2000-06-03 22:44:40 +0000

    Added new demo.

--- ptolemy/media/javasound/demo/Sines/Sines.java
+++ ptolemy/media/javasound/demo/Sines/Sines.java
@@ -1,5 +1,5 @@
-/* A simple application that demonstrates the use of SoundCapture
-and SoundPlayback by performing soft clipping in real-time.
+/* A simple application that demonstrates the use of SoundPlayback 
+  by performing simple additive synthesis in real-time.
 
  Copyright (c) 2000 The Regents of the University of California.
  All rights reserved.
@@ -29,41 +29,36 @@ and SoundPlayback by performing soft clipping in real-time.
 @AcceptedRating
 */
 
-package ptolemy.media.javasound.demo.SoftClip;
+package ptolemy.media.javasound.demo.Sines;
 
 import ptolemy.media.javasound.*;
 
 ////////////////////////////////////////////////////
-/** A simple application that demonstrates the use of SoundCapture
-   and SoundPlayback. This application performs real-time
-   capture, processing, and playback of audio data.
-   Sound samples are captured from the computer's audio
-   input port. The processing consists of a simple
-   soft-clipping funtion (the arc tangent is used). The
-   soft-clipped audio data is then played out the speaker.
+/** A simple application that demonstrates the use of SoundPlayback. 
+   This application synthesizes and plays a simple harmonic
+   signal, using simple additive synthesis. The signal
+   is the some of a few harmonically related sinusoids.
    @author Brian K. Vogel (vogel@eecs.berkeley.edu)
    @version $Id$
  */
-public class SoftClip {
+public class Sines {
     public static void main(String[] args) {
 	// Create a sound capture object that captures audio
 	// from the computer's audio input port (mic or
 	// line-in).
+
+	// The pitch of the signal to synthesize.
+	double fundamental_Hz = 220;
+
 	float sampleRate = 44100; // in Hz
 	int sampleSizeInBits = 16;
 	int channels = 2; // stereo.
-	int inBufferSize = 4096;  // Internal buffer size for capture.
 	int outBufferSize = 4096; // Internal buffer size for playback.
 
 	// Amount of data to read or write from/to the internal buffer
 	// at a time. This should be set smaller than the internal buffer
 	// size!
 	int getSamplesSize = 256; 
-
-	SoundCapture soundCapture = 
-	    new SoundCapture(sampleRate, sampleSizeInBits,
-			     channels, inBufferSize,
-			     getSamplesSize);
 
 	int putSamplesSize = getSamplesSize;
 
@@ -77,21 +72,18 @@ public class SoftClip {
 
 	// Initialize and begin real-time capture and playback.
 	try{
-	    soundCapture.startCapture();
 	    soundPlayback.startPlayback();
 	} catch (Exception ex) {
 	    System.err.println(ex);
 	}
 
-	
-	double[][] capturedSamplesArray = 
+	double[][] samplesArray = 
 	    new double[channels][getSamplesSize];
+	double[] samples = new double[channels];
 
 	try{
 	    // Loop forever.
 	    while(true) {
-		// Read in some captured audio.
-		capturedSamplesArray = soundCapture.getSamples();
 
 		// Do some simple processing on the
 		// captured audio.
@@ -99,17 +91,23 @@ public class SoftClip {
 		    for (int i=0; i< getSamplesSize; i++) {
 			//  ********** INSERT PROCESSING CODE HERE ****
 
-			// Perform soft clipping using the arc tangent.
-			capturedSamplesArray[j][i] =
-			    java.lang.Math.atan(capturedSamplesArray[j][i])*0.6;
+			// Generate a harmonic signal.
+			samplesArray[j][i] =
+			    java.lang.Math.sin(fundamental_Hz*2*java.lang.Math.PI*samples[j])*0.4 +
+			    java.lang.Math.sin(2*fundamental_Hz*2*java.lang.Math.PI*samples[j])*0.3 +
+			    java.lang.Math.sin(3*fundamental_Hz*2*java.lang.Math.PI*samples[j])*0.25 +
+			    java.lang.Math.sin(4*fundamental_Hz*2*java.lang.Math.PI*samples[j])*0.2;
+			// Increment time.
+			samples[j] = samples[j] + 1.0/sampleRate;
 		    }
 		}
 
 		// Play the processed audio samples.
-		soundPlayback.putSamples(capturedSamplesArray);
+		soundPlayback.putSamples(samplesArray);
 		}
 	} catch (Exception ex) {
 	    System.err.println(ex);
 	}   
     }
+
 }
