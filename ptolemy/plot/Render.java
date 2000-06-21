f0baf0c5ea4f35b12295fd252bc99b28e760ca57
net <net>
2000-06-21 22:03:46 +0000

    Working zoom on the image.

--- ptolemy/plot/Render.java
+++ ptolemy/plot/Render.java
@@ -32,6 +32,7 @@ package ptolemy.plot;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.util.LinkedList;
+import java.util.ListIterator;
 
 
 //////////////////////////////////////////////////////////////////////////
@@ -80,14 +81,14 @@ public class Render extends PlotBox {
 
         _imageData.add(newStripe);
 
-        // Get the graphics context.
-
-        Graphics graphics = getGraphics();
-
-        // Draw the stripe if the graphics context ... ISTHERE(?)...
-        if (graphics != null) {
-            _drawStripe(graphics, newStripe, x);
-        }
+//         // Get the graphics context.
+// 
+//         Graphics graphics = getGraphics();
+// 
+//         // Draw the stripe if the graphics context ... ISTHERE(?)...
+//         if (graphics != null) {
+//             _drawStripe(graphics, newStripe, x);
+//         }
     }
 
     /** Create a sample image.
@@ -105,7 +106,45 @@ public class Render extends PlotBox {
                     setTitle("Sample image");
                     setXRange(-1.0, 1.0);
                     setYRange(-1.0, 1.0);
+
+                    _topPadding = 0;
+                    _bottomPadding = 0;
+                    _rightPadding = 0;
+                    _leftPadding = 0;
+
+//                     System.out.println("_xlowgiven = " +_xlowgiven);
+//                     System.out.println("_xhighgiven = " +_xhighgiven);
+//                     System.out.println("_ylowgiven = " + _ylowgiven);
+//                     System.out.println("_yhighgiven = " + _yhighgiven);
+//                     System.out.println();
+//                     System.out.println("_height = " + _height);
+// 
+//                     System.out.println("_xMin = " + _xMin); 
+
+                    // Create the stripes in data form (arrays).
+                    int[] stripe1 = new int[10];
+                    int colorValue = _HIGHCOLOR;
+                    for (int i = 0; i < stripe1.length; i++) {
+                        stripe1[i] = colorValue;
+                        if (colorValue == _LOWCOLOR) {
+                            colorValue = _HIGHCOLOR;
+                        } else {
+                            colorValue = _LOWCOLOR;
+                        }
+                    }
                     
+                    int[] stripe2 = new int[10];
+                    System.arraycopy(stripe1, 0, stripe2, 1,
+                                     stripe2.length - 1);
+                    stripe2[0] = _LOWCOLOR;
+                    
+
+                    // Add the stripes to the data structure.
+                    for (int i = 1; i <= 5; i++) {
+                        addStripe((double)i - .5, stripe1);
+                        addStripe((double)i + .5, stripe2);
+                    }
+
                     // FIXME fill in.
 
                 }
@@ -115,18 +154,18 @@ public class Render extends PlotBox {
         repaint();
     }
 
-    /** Set a colormap.
+    /** Set the colormap.
     * The user needs to give a 3-by-256 integer array as a colormap.
     */
 
-    public void setColormap(int[][] colormap){
+    public synchronized void setColormap(int[][] colormap){
         _colormap = colormap;
     }
 
     /** Get current colormap.
     */
 
-    public int[][] getColormap() {
+    public synchronized int[][] getColormap() {
         return _colormap;
     }
 
@@ -145,52 +184,41 @@ public class Render extends PlotBox {
         super._drawPlot(graphics, clearfirst);
 
 
-        // Draw a few filled in rectangles alternating colors (e.g. black and white).
 
-        // Create the stripes in data form (arrays).
-        int[] stripe1 = new int[10];
-        int colorValue = _HIGHCOLOR;
-        for (int i = 0; i < stripe1.length; i++) {
-            stripe1[i] = colorValue;
-            if (colorValue == _LOWCOLOR) {
-                colorValue = _HIGHCOLOR;
-            } else {
-                colorValue = _LOWCOLOR;
-            }
+        ListIterator imageDataIterator = _imageData.listIterator(0);
+
+        int x = _ulx + (int)((-1.1 - _xMin) * _xscale) + 1; //(_ulx + 1);
+
+//         System.out.println("The width of the area is " + (_xMax - _xMin) +
+//                            " units.");
+//         System.out.println("The width of the area is " +
+//                            ((_xMax - _xMin) * _xscale) +
+//                            " pixels according to the scaling factor.");
+//         System.out.println("But, the width is " +
+//                            (int)(_lrx - _ulx) +
+//                            " pixels according to the actual values.");
+//         System.out.println("The number of stripes to be rendered is " +
+//                            _imageData.size() + ".");
+//         System.out.println("Therefore, there are ~" +
+//                             (int)((_xMax - _xMin) * _xscale) /
+//                             _imageData.size()
+//                            + " pixels per stripe.");
+
+
+        int width = (int)(2.2 * _xscale) / _imageData.size();
+        // divided by the
+        // number of stripes (i.e _imageData.size())
+
+
+        while (imageDataIterator.hasNext()) {
+            int[] currentStripe = (int[])imageDataIterator.next();
+            _drawStripe(graphics, currentStripe, x, width);
+            // System.out.println("x = " + x);
+            x += width;
         }
-
-        int[] stripe2 = new int[10];
-        System.arraycopy(stripe1, 0, stripe2, 1, stripe2.length - 1);
-        stripe2[0] = _LOWCOLOR;
-
-        int length = stripe1.length;
-
-
-        // Draw the stripe one patch (data element) at a time.
-        int y = (_uly + 1);
-        int x = (_ulx + 1);
-        int width = (int)((_xMax - _xMin) * _xscale) - 1; // divided by the number of stripes (i.e _imageData.size())
-        int height = (int)(((_yMax - _yMin) * _yscale) / length); 
-
-        for (int i = 0; i < length; i++) {
-            
-            // Convert the color value into it's r, g, and b.
-            int r = _colormap[0][stripe1[i]];
-            int g = _colormap[1][stripe1[i]];
-            int b = _colormap[2][stripe1[i]];
-
-            // Set the color.
-            graphics.setColor(new Color(r, g, b));
-
-            // Draw the patch.
-            graphics.fillRect(x, y, width, height);
-
-            // Increment the y value so the next patch is printed in the right place.
-            y += height;
-        }
-
-        // Indicate that the plot is showing.
-        _showing = true;
+           
+            // Indicate that the plot is showing.
+            _showing = true;
     }
 
     ///////////////////////////////////////////////////////////////////
@@ -199,13 +227,25 @@ public class Render extends PlotBox {
     /** Draw the specified stripe.
      */
 
-     private void _drawStripe(Graphics graphics, int[] stripe, double x) {
+     private void _drawStripe(Graphics graphics, int[] stripe,
+                              double x, int width) {
 
          // Draw the stripe one patch at a time.
 
          int length = stripe.length;
+
+ 
+         // Draw the stripe one patch (data element) at a time.
+         int y = _lry - (int)((1.0 - _yMin) * _yscale) + 1; //(_uly + 1);
+
+         int height = (int)((2.0 * _yscale) / length); 
+
          for (int i = 0; i < length; i++) {
-             _drawPatch(graphics, (int)x, i, 5, 5, stripe[i]);
+             _drawPatch(graphics, (int)x, y, width, height, stripe[i]);
+
+             // Increment the y value so the next patch is printed in the
+             // right place.
+             y += height;
          }
      }
 
@@ -243,6 +283,8 @@ public class Render extends PlotBox {
     /** Stores the image data to be rendered. */
     private LinkedList _imageData = new LinkedList();
 
+
+
     /** The colormap used in rendering the image.
     */
     private int[][] _colormap = new int[3][256];
@@ -254,5 +296,14 @@ public class Render extends PlotBox {
     /** A test color value.
     */
     private static final int _LOWCOLOR = 175;
+
+    /** The given height of the stripes in units of the y axis.
+    */
+    // I set _height to 2.2 for testing only.  This should be set by the
+    // application.
+    // private double _height = _yhighgiven - _ylowgiven;
+
+    // I discovered that _height is already used in PlotBox.  I'm waitng on
+    // this (commenting it out) to see what i need to do.
 
 }
