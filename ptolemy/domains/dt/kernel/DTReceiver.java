0b425d316392d695cdb718a4fb3eea013953485e
chf <chf>
2000-07-11 00:05:48 +0000

    changes in DTReceiver for design review

--- ptolemy/domains/dt/kernel/DTReceiver.java
+++ ptolemy/domains/dt/kernel/DTReceiver.java
@@ -51,15 +51,15 @@ import javax.swing.*;
 //////////////////////////////////////////////////////////////////////////
 //// DTReceiver
 /**
-A first-in, first-out (FIFO) queue receiver with variable capacity and
-optional history. Tokens are put into the receiver with the put() method,
-and removed from the receiver with the get() method. The token removed is
-the oldest one in the receiver. By default, the capacity is unbounded, but
-it can be set to any nonnegative size. If the history capacity is greater
-than zero (or infinite, indicated by a capacity of INFINITE_CAPACITY),
-then tokens removed from the receiver are stored in a history queue rather
-than simply removed. By default, the history capacity is zero.
-
+A first-in, first-out (FIFO) queue receiver with variable capacity. Tokens
+are put into the receiver with the put() method, and removed from the
+receiver with the get() method. The token removed is the oldest one in 
+the receiver.  Time is incremented by a fixed amount deltaT everytime the
+get() method is called. We calculate deltaT as "period / (rate * repeats)"
+where:  - period is the execution time of the director per iteration 
+        - rate   is the rate of the port that holds this receiver
+        - repeats is the firing count per iteration of the actor 
+                  that holds this receiver
 @author C. Fong
 @version $Id$
 */
@@ -98,16 +98,145 @@ public class DTReceiver extends SDFReceiver implements Receiver {
     }
     
 
+    ///////////////////////////////////////////////////////////////////
+    ////                         public methods                    ////
+
+    /** Calculate _deltaT for this receiver. 
+     *
+     *  @exception IllegalActionException If there is an error in
+     *  getting attribute information from the ports.
+     */
+    public void calculateDeltaT() throws IllegalActionException {
+        int repeats;
+        double periodValue;
+        boolean isCompositeContainer = !((ComponentEntity) _to).isAtomic();
+    	
+    	if (_from == null)  {
+    	    throw new InternalErrorException(
+    	        "internal DT error: Receiver with null source");
+    	} else {
+    	    
+    	    Parameter param = (Parameter) 
+    	                        _fromPort.getAttribute("tokenProductionRate");
+    	    if(param == null) {
+               outrate = 1;
+            } else {
+               outrate = ((IntToken)param.getToken()).intValue();
+            }
+
+            if ((isCompositeContainer) && (_toPort.isOutput())) {
+                inrate = 1;
+            } else {
+    		    param = (Parameter) _toPort.getAttribute("tokenConsumptionRate");
+    	        if(param == null) {
+                    inrate = 1;
+                } else {
+                    inrate = ((IntToken)param.getToken()).intValue();
+                }
+            }       		
+        	
+            repeats = _localDirector.getRepeats(_to);
+        	periodValue = _localDirector.getPeriod();
+        	_periodDivider = repeats * inrate; 
+        	_deltaT = periodValue / _periodDivider;
+        }
+    }
+
+
+    /** Determine the source and destination ports that use this
+     *  receiver in their communications.  The source and destination
+     *  ports are distinct for each receiver. 
+     *  @param dtDirector The director that directs this receiver
+     */
+    public void determineEnds(DTDirector dtDirector) {
+        _toPort = this.getContainer();
+    	_to = (Actor) _toPort.getContainer();
+    	_fromPort = null;
+        IOPort connectedPort = null;
+        List listOfConnectedPorts = null;
+        boolean isCompositeContainer = !((ComponentEntity) _to).isAtomic();
+    	
+    	_localDirector = dtDirector;
+    	
+    	if (isCompositeContainer && (_toPort.isOutput()) ) {
+    	    listOfConnectedPorts = _toPort.insidePortList();
+    	} else {
+    	    listOfConnectedPorts = _toPort.connectedPortList();
+    	}
+    	    
+    	Iterator portListIterator = listOfConnectedPorts.iterator();
+    	
+    	foundReceiver:
+    	while (portListIterator.hasNext()) {
+    	    connectedPort = (IOPort) portListIterator.next();
+    	
+    	    if (connectedPort.isOutput() == true) {
+    		    Receiver[][] remoteReceivers = connectedPort.getRemoteReceivers();
+    		    
+    		    for(int i=0;i<connectedPort.getWidth();i++) {
+    			    for(int j=0;j<remoteReceivers[i].length;j++) {
+    			        if (remoteReceivers[i][j] == this) {
+                            _from = (Actor) connectedPort.getContainer();
+                            _fromPort = connectedPort;
+                            if (_fromPort == null) {
+                                throw new InternalErrorException(
+                                    "internal DT error: Receiver with null source");
+                            }
+                            break foundReceiver;
+    			        } 
+    			    }
+    		    }
+    	    } else if (connectedPort.getContainer() instanceof TypedCompositeActor) {
+    	        // FIXME: should use at isAtomic() insteadof instanceof?
+    	        _from = (Actor) connectedPort.getContainer();
+    	        _fromPort = connectedPort;
+    	        if (_fromPort == null) {
+    	            throw new InternalErrorException(
+                        "internal DT error: Receiver with null source");
+    	        }
+    	        break foundReceiver;
+    	    } else if (connectedPort.isInput() == true) {
+    	       // This case occurs when the destination port and 
+    	       // the queried connected port are both inputs.
+    	       // This case should be ignored.
+    	    } 
+    	}
+    	
+    	if (_fromPort == null) {
+    	    throw new InternalErrorException(
+    	        "internal DT error: Receiver with null source");
+    	}
+    }
+    
+    /**  For debugging purposes. Display pertinent information about
+     *   this receiver.
+     */
+    public void displayReceiverInfo() {
+        String fromString;
+        String toString;
+        
+        if (_from == null) {
+              fromString="0";
+        } else {
+              fromString=((Nameable) _from).getName();
+        }
+        
+        if (_to == null) {
+              toString="0";
+        } else {
+              toString=((Nameable) _to).getName();
+        }
+        
+        debug.println(fromString+" "+toString+" "+_deltaT);
+    }
+
+
     /** Remove the first token (the oldest one) from the receiver and
      *  return it. If there is no token in the receiver, throw an
      *  exception.  Increment the time of the local director by
-     *  the value period/(rate * repeats) where:
-     *  - period is the time needed by the director per iteration 
-     *  - rate   is the rate of the port that holds this receiver
-     *  - repeats is the firing count per iteration the actor 
-     *            that holds this receiver
+     *  the deltaT.
+     *  
      *  @return The oldest token in the receiver.
-     *  @exception IllegalActionException If current time cannot be modified.
      */
     public Token get() {
 
@@ -115,28 +244,24 @@ public class DTReceiver extends SDFReceiver implements Receiver {
         IOPort currentPort = (IOPort) super.getContainer();
         DTDirector director = (DTDirector) ((Actor) actor).getDirector();
 
-        // FIXME: may need to consider different cases for TypedComposositeActor ports
-        director.setCurrentTime(_localGetTime);
+        // FIXME: need to consider different cases for TypedComposositeActor ports
+        director.setCurrentTime(_localTime);
 
-        String sourceName = ((Nameable) to).getName();
-        String destinationName = ((Nameable) from).getName();
-        debug.print("   == get call by "+sourceName+" from "+destinationName+" time:"+_localGetTime);
-        if (currentPort.getWidth()>1) {
-            debug.println(" _multiport_");
-        } else debug.println(" ");
+        String sourceName = ((Nameable) _to).getName();
+        String destinationName = ((Nameable) _from).getName();
         
-        
-        // FIXME: timing does not work for DT inside DT
-        _localGetTime = _localGetTime + _deltaT;
+        // FIXME: timing has bugs for DT inside DT
+        _localTime = _localTime + _deltaT;
         return super.get();
     }
 
    
     /** Remove the first tokens (the oldest ones) from the receiver and
-     *  fill the array with them.
-     *  If there are not enough tokens in the receiver, throw an exception
-     *  and remove none of the tokens from the receiver.
-     *  @exception NoTokenException If there is no token in the receiver.
+     *  fill the array with them. Increment the time of the local director
+     *  by the value ' n * _deltaT ' where:
+     *    -  n is the length of the token array
+     *    -  _deltaT is  period / (rate * repeats)
+     *  @param t[]  Array to hold the first n oldest tokens.
      */
     public void getArray(Token t[]) {
         super.getArray(t);
@@ -144,199 +269,81 @@ public class DTReceiver extends SDFReceiver implements Receiver {
         Actor actor = (Actor) super.getContainer().getContainer();
         DTDirector director = (DTDirector) (actor.getDirector());
         
-        _localGetTime = _localGetTime + t.length * _deltaT;
+        _localTime = _localTime + t.length * _deltaT;
     }
 
-   
-    public void initialPut(MatrixToken outputsArray) {
-    }
-    
-    
+
+    /** Put a token to the receiver. If the port feeding is this
+     *  receiver is null, report internal error.
+     *  @param token The token to be put to the receiver.
+     *  @exception InternalErrorException If the source port is null.
+     */
     public void put(Token token) {
-
-    	Actor actor = (Actor) super.getContainer().getContainer();
-        IOPort currentPort = (IOPort) super.getContainer();
-        DTDirector director = (DTDirector) (actor.getDirector());
-        String fromName = ((Nameable)from).getName();
-        String toName = ((Nameable)to).getName();
-
-        // FIXME: may need to consider different cases for TypedComposositeActor ports
-        //director.setCurrentTime(_localPutTime);
-        
-        
-        debug.print("   --put token from "+fromName+" to "+toName+" with divider "+_periodDivider+" // time is:"+_localPutTime);
-        if (fromPort == null) debug.prompt("null fromPort");     
-        if (fromPort.getWidth()>1) {
-            debug.println(" _multiport_");
-        } else debug.println(" ");
-        _localPutTime = _localPutTime + _deltaT;
+        if (_fromPort == null) {
+            throw new InternalErrorException(
+                      "internal DT error: Receiver with null source");
+        } 
         super.put(token);        
     }
     
     
-    
-
-    /** Put an array of tokens in the receiver.
-     *  If the receiver has insufficient room, throw an
-     *  exception, and add none of the tokens to the receiver.
-     *  @param token The token to be put to the receiver.
+    /** Return the port that feeds this Receiver
+     *  @return TypedIOPort  The port that feeds this receivers.
      */
-    public void putArray(Token token[]) {
-        super.putArray(token);
-
-        Actor actor = (Actor) super.getContainer().getContainer();
-        DTDirector director = (DTDirector) (actor.getDirector());
-        director.setCurrentTime(_localPutTime);
-        _localPutTime = _localPutTime + token.length * _deltaT;
+    public TypedIOPort getSourcePort() {
+        return (TypedIOPort) _fromPort;
     }
     
     
-    /** Determine the source and destination ports that use this
-     *  receiver in their communications.  The source and destination
-     *  ports are unique  
-     *  @param dtDirector 
+    ///////////////////////////////////////////////////////////////////
+    ////                         private methods                   ////
+
+    /** Initialize the DTReceiver.  Set the cached information on source
+     *  and destination actors to null.  Set the local time to zero.  Set
+     *  deltaT to zero.  
      */
-    public void determineEnds(DTDirector dtDirector) {
-        toPort = this.getContainer();
-    	to = (Actor) toPort.getContainer();
-    	fromPort = null;
-        IOPort connectedPort = null;
-        List listOfConnectedPorts = null;
-        boolean isCompositeContainer = !((ComponentEntity) to).isAtomic();
-    	
-    	_localDirector = dtDirector;
-    	
-    	if (isCompositeContainer && (toPort.isOutput()) ) {
-    	    listOfConnectedPorts = toPort.insidePortList();
-    	} else {
-    	    listOfConnectedPorts = toPort.connectedPortList();
-    	}
-    	    
-    	Iterator portListIterator = listOfConnectedPorts.iterator();
-    	
-    	debug.println("determine ends "+this+" of "+toPort);
-    	foundReceiver:
-    	while (portListIterator.hasNext()) {
-    	    connectedPort = (IOPort) portListIterator.next();
-      	    debug.println("portListIterator listing "+connectedPort+" connected to "+toPort);
-    	
-    	    if (connectedPort.isOutput() == true) {
-    	        debug.println("is output "+connectedPort);
-    		    Receiver[][] remoteReceivers = connectedPort.getRemoteReceivers();
-    		    
-    		    for(int i=0;i<connectedPort.getWidth();i++) {
-    		        debug.println("width of "+connectedPort+" :"+connectedPort.getWidth());
-    			    for(int j=0;j<remoteReceivers[i].length;j++) {
-    			        debug.println("length of receiver "+remoteReceivers[i].length);
-    			        debug.println(remoteReceivers[i][j]);
-    			        debug.println(this);
-    			        //debug.prompt(remoteReceivers[i][j] +" "+ this);
-    			        if (remoteReceivers[i][j] == this) {
-                            from = (Actor) connectedPort.getContainer();
-                            fromPort = connectedPort;
-                            debug.println("fromPort "+connectedPort);
-                            if (fromPort == null) debug.prompt("fromport is null");
-                            break foundReceiver;
-    			        } else {
-    			            debug.println("not equal receivers");
-    			        }
-    			    }
-    		    }
-    	    } else if (connectedPort.getContainer() instanceof TypedCompositeActor) {
-    	    // FIXME: should use at isAtomic()?
-    	    // FIXME: code body 
-    	        from = (Actor) connectedPort.getContainer();
-    	        fromPort = connectedPort;
-    	        if (fromPort == null) debug.prompt("fromport is null");
-    	        break foundReceiver;
-    	    } else if (connectedPort.isInput() == true) {
-    	    // This case occurs when the destination port and the queried connected
-    	    // port are both inputs. This case should be ignored
-    	    } else {
-    	    // should throw exception
-    	        debug.prompt("internal bug: unexpected case");
-    	    }
-    	}
-    	
-    	if (fromPort == null) {
-    	    debug.prompt(" container to actor " + ((ComponentEntity)to).getContainer());
-    	    debug.println("to   "+toPort);
-    	    debug.println("from "+fromPort);
-    	}
-    }
-    
-    
-    public void calculateArcSamplingRate() throws IllegalActionException {
-        int repeats;
-        boolean isCompositeContainer = !((ComponentEntity) to).isAtomic();
-    	
-    	if ((from == null)/*||to instanceof TypedCompositeActor*/)  {
-    	    debug.prompt("illegal from==null in calculateArcSamplingRate");
-    	} else {
-    	    
-    	    Parameter param = (Parameter) fromPort.getAttribute("tokenProductionRate");
-    	    if(param == null) {
-               outrate = 1;
-            } else {
-               outrate = ((IntToken)param.getToken()).intValue();
-            }
-
-            if ((isCompositeContainer) && (toPort.isOutput())) {
-                inrate = 1;
-            } else {
-    		    param = (Parameter) toPort.getAttribute("tokenConsumptionRate");
-    	        if(param == null) {
-                    inrate = 1;
-                } else {
-                    inrate = ((IntToken)param.getToken()).intValue();
-                    if (inrate==0) debug.prompt("alright found bad seed "+to);
-                }
-            }       		
-        	
-            repeats = _localDirector.getRepeats(to);
-        	_periodDivider = repeats * inrate; 
-        	
-        	double period = _localDirector.getPeriod();
-        	
-        	_deltaT = period/_periodDivider;
-        }
-
-    }
-    
-    /** This function is to be used in hierarchical DT where an inside director
-     *  may need to resync with an outside director.
-     */ 
-    public void resyncLocalTime(double time) {
-      
-    }
-    
-    
     private void _init() {
-        from = null;
-        to = null;
+        _from = null;
+        _to   = null;
         _localTime = 0.0; 
-        _localPutTime = 0.0;
-        _localGetTime = 0.0; 
         _periodDivider = 0; 
         _deltaT = 0.0;
-        debug = new DTDebug(false);
+        debug = new DTDebug(true);
     }
     
     ///////////////////////////////////////////////////////////////////
     ////                         private variables                 ////
-
     
+    // The director that directs this receiver; should be a DTDirector
     private DTDirector _localDirector;
+    
+    // The dividing factor to the discrete time period
     private int _periodDivider;
-    double _deltaT;
+    
+    // The amount of time increment per get() method call
+    private double _deltaT;
+    
+    // The local cached time
     private double _localTime;
-    private double _localPutTime;
-    private double _localGetTime;
-    int inrate;
-    int outrate;
-    Actor from;
-    Actor to;
-    IOPort fromPort;
-    IOPort toPort;
+    
+    // The local cached value of the destination token consumption rate 
+    private int inrate;
+    
+    // The local cached value of the source token production rate
+    private int outrate;
+    
+    // The actor feeding this receiver
+    private Actor _from;
+    
+    // The actor containing this receiver
+    private Actor _to;
+    
+    // The port feeding this receiver
+    private IOPort _fromPort;
+    
+    // The port containing this receiver
+    private IOPort _toPort;
+    
+    // display for debugging purposes
     private DTDebug debug;
 }
