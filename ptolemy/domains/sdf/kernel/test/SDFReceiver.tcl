744c6a2157740366bd51a9376d5995ed29b4b390
vogel <vogel>
2000-07-27 19:27:32 +0000

    Added some tests for SDFReceiver. There were previously no tests!

--- ptolemy/domains/sdf/kernel/test/SDFReceiver.tcl
+++ ptolemy/domains/sdf/kernel/test/SDFReceiver.tcl
@@ -1,6 +1,6 @@
 # Tests for the SDFReceiver class
 #
-# @Author: Christopher Hylands
+# @Author: Brian K. Vogel
 #
 # @Version: : NamedObj.tcl,v 1.33 1998/12/05 05:37:32 cxh Exp $
 #
@@ -47,5 +47,186 @@ if {[info procs enumToObjects] == "" } then {
 ######################################################################
 ####
 #
-test SDFReceiver-2.1 {} {
-} {}
+test SDFReceiver-1.1 {Check put and hasToken} {
+    set receiver [java::new ptolemy.domains.sdf.kernel.SDFReceiver]
+    # Check that hasToken returns false when the receiver is empty
+    # and returns true after a token has been put in the
+    # receiver.
+
+    # token to put
+    set token [java::new ptolemy.data.StringToken foo]
+
+    # Should return false (0), since no token has yet been put.
+    set result1 [$receiver hasToken 1]
+
+    # Now put a token.
+    $receiver {put ptolemy.data.Token} $token
+
+    # Should return true (1), since there is now a token in
+    # the receiver.
+    set result2 [$receiver hasToken 1]
+    # Should return false (0), since there is only a single token
+    # in the receiver.
+    set result3 [$receiver hasToken 2]
+
+    list $result1 $result2 $result3
+} {0 1 0}
+
+######################################################################
+####
+#
+test SDFReceiver-2.1 {Check put and get and hasToken} {
+    set receiver [java::new ptolemy.domains.sdf.kernel.SDFReceiver]
+    # Check that hasToken returns false when the receiver is empty
+    # and returns true after a token has been put in the
+    # receiver and returns false after the token has been
+    # removed from the receiver.
+
+    # token to put
+    set token [java::new ptolemy.data.StringToken foo]
+
+    # Should return false (0), since no token has yet been put.
+    set result1 [$receiver hasToken 1]
+
+    # Now put a token.
+    $receiver {put ptolemy.data.Token} $token
+
+    # Should return true (1), since there is now a token in
+    # the receiver.
+    set result2 [$receiver hasToken 1]
+    
+    # Now get the token, but don't remove it from the receiver.
+    set receivedToken [$receiver get 0]
+
+    # Should return true (1), since there is still a token in
+    # the receiver.
+    set result3 [$receiver hasToken 1]
+
+    # Now get the token, and remove it from the receiver.
+    set receivedToken2 [$receiver get]
+
+    # Should return false (0), since there is no longer a token in
+    # the receiver.
+    set result4 [$receiver hasToken 1]
+
+    list $result1 $result2 $result3 $result4 [$receivedToken toString] [$receivedToken2 toString]
+} {0 1 1 0 {"foo"} {"foo"}}
+
+test SDFReceiver-2.2 {Check put and get and hasToken with more than 1 token in the queue} {
+    set receiver [java::new ptolemy.domains.sdf.kernel.SDFReceiver]
+    # Check that hasToken returns false when the receiver is empty
+    # and returns true after a token has been put in the
+    # receiver and returns false after the token has been
+    # removed from the receiver.
+
+    # tokens to put
+    set token [java::new ptolemy.data.StringToken foo]
+    set token2 [java::new ptolemy.data.StringToken bar]
+
+    # Should return false (0), since no token has yet been put.
+    set result1 [$receiver hasToken 1]
+
+    # Now put a token.
+    $receiver {put ptolemy.data.Token} $token
+
+    # Now put another token.
+    $receiver {put ptolemy.data.Token} $token2
+
+    # Should return true (1), since there is now a token in
+    # the receiver.
+    set result2 [$receiver hasToken 1]
+    
+    # Now get the token, but don't remove it from the receiver.
+    set receivedToken [$receiver get 0]
+
+    # Now get the token, but don't remove it from the receiver.
+    set receivedToken2 [$receiver get 1]
+
+    # Should return true (1), since there is still a token in
+    # the receiver.
+    set result3 [$receiver hasToken 1]
+
+    # Now get the token, and remove it from the receiver.
+    set receivedToken3 [$receiver get]
+
+    # Now get the token, and remove it from the receiver.
+    set receivedToken4 [$receiver get]
+
+    # Should return false (0), since there is no longer a token in
+    # the receiver.
+    set result4 [$receiver hasToken 1]
+
+    list $result1 $result2 $result3 $result4 [$receivedToken toString] [$receivedToken2 toString] [$receivedToken3 toString] [$receivedToken4 toString]
+} {0 1 1 0 {"foo"} {"bar"} {"foo"} {"bar"}}
+
+######################################################################
+####
+#
+test SDFReceiver-3.1 {Check putArray} {
+    # Do a vectorized put of two tokens, then do two scalar
+    # get()s.
+    set receiver [java::new ptolemy.domains.sdf.kernel.SDFReceiver]
+
+    # tokens to put
+    set token [java::new ptolemy.data.StringToken foo]
+    set token2 [java::new ptolemy.data.StringToken bar]
+
+    set tokenArray [java::new {ptolemy.data.Token[]} {2}]
+    $tokenArray set 0 $token
+    $tokenArray set 1 $token2
+
+    # Should return false (0), since no token has yet been put.
+    set result1 [$receiver hasToken 1]
+
+    # Now put the token array.
+    $receiver {putArray ptolemy.data.Token[] int} $tokenArray 2
+
+    # Should return true (1), since there are now 2 tokens in
+    # the receiver.
+    set result2 [$receiver hasToken 2]
+
+    # Now do two scalar get()s.
+    set receivedToken1 [$receiver get]
+    set receivedToken2 [$receiver get]
+
+    # Should return false (0), since there is no longer a token in
+    # the receiver.
+    set result3 [$receiver hasToken 1]
+
+    list $result1 $result2 $result3 [$receivedToken1 toString] [$receivedToken2 toString]
+} {0 1 0 {"foo"} {"bar"}}
+
+######################################################################
+####
+#
+test SDFReceiver-4.1 {Check getArray} {
+    # Do two scalar put()s of two tokens, then do a vectorized
+    # get by using getArray().
+    set receiver [java::new ptolemy.domains.sdf.kernel.SDFReceiver]
+
+    # tokens to put
+    set token [java::new ptolemy.data.StringToken foo]
+    set token2 [java::new ptolemy.data.StringToken bar]
+
+    # Should return false (0), since no token has yet been put.
+    set result1 [$receiver hasToken 1]
+
+    # Scalar put.
+    $receiver put $token
+
+    # Another scalr put.
+    $receiver put $token2
+
+    # Should return true (1), since there are now 2 tokens in
+    # the receiver.
+    set result2 [$receiver hasToken 2]
+
+    # Now do a vectorized get.
+    set receivedTokenArray [$receiver {getArray int} 2]
+
+    # Should return false (0), since there is no longer a token in
+    # the receiver.
+    set result3 [$receiver hasToken 1]
+
+    list $result1 $result2 $result3 [jdkPrintArray $receivedTokenArray]
+} {0 1 0 {{"foo"} {"bar"}}}
