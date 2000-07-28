ccb872c8acfb5b19714bf7e8e200a114c75f5007
cxh <cxh>
2000-07-28 02:47:36 +0000

    Moved from ../../hello

---
+++ ptolemy/kvm/demo/hello/Hello.java
@@ -0,0 +1,64 @@
+/* Simple PalmOS Java KVM program
+
+ Copyright (c) 1999-2000 The Regents of the University of California.
+ All rights reserved.
+ Permission is hereby granted, without written agreement and without
+ license or royalty fees, to use, copy, modify, and distribute this
+ software and its documentation for any purpose, provided that the above
+ copyright notice and the following two paragraphs appear in all copies
+ of this software.
+
+ IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+ FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+ ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+ THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+ SUCH DAMAGE.
+
+ THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+ INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+ PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+ CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ ENHANCEMENTS, OR MODIFICATIONS.
+
+                                        PT_COPYRIGHT_VERSION_2
+                                        COPYRIGHTENDKEY
+@ProposedRating Red (yourname@eecs.berkeley.edu)
+@AcceptedRating Red (reviewmoderator@eecs.berkeley.edu)
+*/
+
+package ptolemy.kvm.hello;
+
+import com.sun.kjava.*;
+
+
+//////////////////////////////////////////////////////////////////////////
+//// Hello
+/**
+Simple PalmOS Java program
+@author Christopher Hylands
+@version $Id$
+*/
+public class Hello extends Spotlet{
+    public Hello() {
+                g = Graphics.getGraphics();
+                textBox = new ScrollTextBox("Hello, PalmOS",5,5,150,130);
+                paint();
+    }
+
+    public static void main(String args[]) {
+        hello = new Hello();
+	Graphics.getGraphics().clearScreen();
+	textBox.paint();
+        hello.register(NO_EVENT_OPTIONS);
+    }
+
+    void paint() {
+        // display the frames
+        textBox.paint();
+    }
+
+    public Graphics g;
+    public static Hello hello;
+    public static ScrollTextBox textBox;
+}
