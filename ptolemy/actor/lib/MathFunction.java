8cf8458dd4f4207cf36fc1581b7327503f2070d8
chf <chf>
2000-09-28 22:19:02 +0000

    added MathFunction actor

---
+++ ptolemy/actor/lib/MathFunction.java
@@ -0,0 +1,314 @@
+/* An actor that outputs a specified math function of the input.
+
+ Copyright (c) 1998-2000 The Regents of the University of California.
+ All rights reserved.
+ Permission is hereby granted, without written agreement and without
+ license or royalty fees, to use, copy, modify, and distribute this
+ software and its documentation for any purpose, provided that the above
+ copyright notice and the following two paragraphs appear in all copies
+ of this software.
+
+ IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+ FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+ ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+ THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+ SUCH DAMAGE.
+
+ THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+ INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+ PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+ CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ ENHANCEMENTS, OR MODIFICATIONS.
+
+                                        PT_COPYRIGHT_VERSION_2
+                                        COPYRIGHTENDKEY
+
+@ProposedRating Red (chf@eecs.berkeley.edu)
+@AcceptedRating Red (eal@eecs.berkeley.edu)
+*/
+
+package ptolemy.actor.lib;
+
+import ptolemy.actor.*;
+import ptolemy.kernel.util.*;
+import ptolemy.data.*;
+import ptolemy.data.type.BaseType;
+import ptolemy.data.expr.Parameter;
+
+// NOTE: If you update the list of functions, then you will want
+// to update the list in actor/lib/math.xml.
+
+//////////////////////////////////////////////////////////////////////////
+//// MathFunction
+/**
+Produce an output token on each firing with a value that is
+equal to the specified math function of the input.
+The input and output types are DoubleToken.  The functions
+are exactly those in the java.lang.Math class.  They are:
+<ul>
+<li> <b>EXP</b>: The exponential function.
+If the argument is NaN, then the result is NaN.
+<li> <b>LOG</b>: The natural logarithm function.
+If the argument is NaN, then the result is NaN.
+<li> <b>SQUARE</b>: The square function
+If the argument is NaN, then the result is NaN.
+<li> <b>SQRT</b>: The square root function.
+If the argument is NaN, then the result is NaN.
+<li> <b>REMAINDER</b>: The remainder after division.
+If the second operand is zero, then the result is NaN.
+</ul>
+<p>
+(NOTE: Some functions like EXP, LOG, SQUARE, and SQRT act on a single 
+operand only.  Other functions like REMAINDER act on two operands.)
+
+
+@author C. Fong
+@version $Id$
+@see AbsoluteValue
+@see Scale
+@see conversions.RectangularToPolar
+*/
+
+public class MathFunction extends TypedAtomicActor {
+
+    /** Construct an actor with the given container and name.
+     *  @param container The container.
+     *  @param name The name of this actor.
+     *  @exception IllegalActionException If the actor cannot be contained
+     *   by the proposed container.
+     *  @exception NameDuplicationException If the container already has an
+     *   actor with this name.
+     */
+    public MathFunction(TypedCompositeActor container, String name)
+            throws NameDuplicationException, IllegalActionException  {
+        super(container, name);
+
+        // parameters
+        function = new Parameter(this, "function", new StringToken("exp"));
+        _function = EXP;
+        function.setTypeEquals(BaseType.STRING);
+        
+        // ports
+        firstOperand = new TypedIOPort(this, "firstOperand", true, false);
+	    output = new TypedIOPort(this, "output", false, true);
+        firstOperand.setTypeEquals(BaseType.DOUBLE);
+        output.setTypeEquals(BaseType.DOUBLE);
+    }
+
+    ///////////////////////////////////////////////////////////////////
+    ////                     ports and parameters                  ////
+
+    /** The function to compute.  This is a string-valued parameter
+     *  that defaults to "exp".
+     */
+    public Parameter function;
+
+    public TypedIOPort firstOperand = null;
+    public TypedIOPort secondOperand = null;
+
+
+    /** Output port.  The type is inferred from the connections.
+     */
+    public TypedIOPort output = null;
+
+
+    ///////////////////////////////////////////////////////////////////
+    ////                         public methods                    ////
+
+            
+    /** Override the base class to determine which function is being
+     *  specified.
+     *  @param attribute The attribute that changed.
+     *  @exception IllegalActionException If the function is not recognized.
+     */
+    public void attributeChanged(Attribute attribute)
+            throws  IllegalActionException {
+        try {
+            if (attribute == function) {
+                String spec = ((StringToken)(function.getToken())).stringValue();
+                if (spec.equals("exp")) {
+                    _function = EXP;
+                    if (secondOperand != null) 
+                        secondOperand.setContainer(null);
+                } else if (spec.equals("log")) {
+                    _function = LOG;
+                    if (secondOperand != null) 
+                        secondOperand.setContainer(null);
+                } else if (spec.equals("square")) {
+                    _function = SQUARE;
+                    if (secondOperand != null) 
+                        secondOperand.setContainer(null);
+                } else if (spec.equals("sqrt")) {
+                    _function = SQRT;
+                    if (secondOperand != null)
+                        secondOperand.setContainer(null);
+                } else if (spec.equals("remainder")) {
+                    _function = REMAINDER;
+                    _createSecondPort();
+                } else {
+                    throw new IllegalActionException(this,
+                        "Unrecognized trigonometric function: " + spec);
+                }
+            } else {
+                super.attributeChanged(attribute);
+            }
+        } catch (NameDuplicationException e) {
+            throw new IllegalActionException("Name duplication not allowed");
+        }
+    }
+
+    /** Clone the actor into the specified workspace. This calls the
+     *  base class and then sets the parameters of the new actor.
+     *  @param ws The workspace for the new object.
+     *  @return A new actor.
+     *  @exception CloneNotSupportedException If a derived class contains
+     *   an attribute that cannot be cloned.
+     */
+    public Object clone(Workspace ws)
+	    throws CloneNotSupportedException {
+        MathFunction newobj = (MathFunction)super.clone(ws);
+        newobj.function = (Parameter)newobj.getAttribute("function");
+        newobj.firstOperand = (TypedIOPort)newobj.getPort("firstOperand");
+        if (secondOperand != null) {
+          newobj.secondOperand = (TypedIOPort)newobj.getPort("secondOperand");
+        }
+        newobj.output = (TypedIOPort)newobj.getPort("output");
+        return newobj;
+    }
+
+    /** Compute the specified math function of the input.
+     *  If there is no input, then produce no output.
+     *  @exception IllegalActionException If there is no director.
+     */
+    public void fire() throws IllegalActionException {
+        if (firstOperand.hasToken(0)) {
+            double in1 = ((DoubleToken) firstOperand.get(0)).doubleValue();
+            double in2 = 0;
+            if (_function == REMAINDER) {
+                if (secondOperand.hasToken(0)) {
+                    in2 = ((DoubleToken) secondOperand.get(0)).doubleValue();
+                }
+            } 
+            output.send(0, new DoubleToken(_doFunction(in1,in2)));
+        }
+    }
+
+    /** Invoke a specified number of iterations of this actor. Each
+     *  iteration computes the math function specified by the
+     *  <i>function</i> parameter on a single token. An invocation
+     *  of this method therefore applies the function to <i>count</i>
+     *  successive input tokens.
+     *  <p>
+     *  This method should be called instead of the usual prefire(),
+     *  fire(), postfire() methods when this actor is used in a
+     *  domain that supports vectorized actors.  This leads to more
+     *  efficient execution.
+     *  @param count The number of iterations to perform.
+     *  @return COMPLETED if the actor was successfully iterated the
+     *   specified number of times. Otherwise, return NOT_READY, and do
+     *   not consume any input tokens.
+     *  @exception IllegalActionException If iterating cannot be
+     *  performed.
+     */
+    public int iterate(int count) throws IllegalActionException {
+	// Check whether we need to reallocate the output token array.
+	    if (count > _resultArray.length) {
+	        _resultArray = new DoubleToken[count];
+	    }
+        
+        if (firstOperand.hasToken(0,count)) {
+            Token[] inArray1 = firstOperand.get(0,count);
+            
+            
+            if (_function == REMAINDER) {
+                if (secondOperand.hasToken(0,count)) {
+                    Token[] inArray2 = secondOperand.get(0,count);
+                    for (int i = 0; i < count; i++) {
+                        double input1 = ((DoubleToken)(inArray1[i])).doubleValue();
+                        double input2 = ((DoubleToken)(inArray2[i])).doubleValue();
+                        _resultArray[i] = new DoubleToken(_doFunction(input1,input2));
+                    }
+                    output.send(0, _resultArray, count);
+                    return COMPLETED;
+                } else {
+                    return NOT_READY;
+                }
+            } else {
+                for(int i = 0; i < count ; i++) {
+                    double input1 = ((DoubleToken)(inArray1[i])).doubleValue();
+                    _resultArray[i] = new DoubleToken(_doFunction(input1,0));
+                }
+                output.send(0, _resultArray, count);
+                return COMPLETED;
+            }
+        } else {
+            return NOT_READY;
+        }
+    }
+
+    ///////////////////////////////////////////////////////////////////
+    ////                         private methods                   ////
+
+     
+    /** Create the second port needed by remainder function
+     */
+    private void _createSecondPort() 
+        throws NameDuplicationException, IllegalActionException {
+        
+        if (secondOperand == null)
+            secondOperand = new TypedIOPort(this, "secondOperand", true, false);
+        else if (secondOperand.getContainer() == null)
+            secondOperand = new TypedIOPort(this, "secondOperand", true, false);
+        
+        secondOperand.setTypeEquals(BaseType.DOUBLE);
+    }
+
+    
+    /** Calculate the function on the given argument.
+     *  @param in The input value.
+     *  @return The result of applying the function.
+     */
+    private double _doFunction(double in1, double in2) {
+        double result;
+        switch(_function) {
+        case EXP:
+            result = Math.exp(in1);
+            break;
+        case LOG:
+            result = Math.log(in1);
+            break;
+        case SQUARE:
+            result = in1 * in1;
+            break;
+        case SQRT:
+            result = Math.sqrt(in1);
+            break;
+        case REMAINDER:
+            result = in1 % in2;
+            break;
+        default:
+            throw new InternalErrorException(
+                    "Invalid value for _function private variable. "
+                    + "TrigFunction actor (" + getFullName()
+                    + ")");
+        }
+        return result;
+    }
+
+    ///////////////////////////////////////////////////////////////////
+    ////                         private variables                 ////
+
+    private DoubleToken[] _resultArray = new DoubleToken[1];
+
+    // An indicator for the function to compute.
+    private int _function;
+
+    // Constants used for more efficient execution.
+    private static final int EXP = 0;
+    private static final int LOG = 1;
+    private static final int SQUARE = 2;
+    private static final int SQRT = 3;
+    private static final int REMAINDER = 4;
+}
+            
