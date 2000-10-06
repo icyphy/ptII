69830334e9c83e3f006e8ee2a84eeb49fcd8efeb
chf <chf>
2000-10-06 01:37:34 +0000

    updated documentation

---
+++ ptolemy/domains/dd3d/lib/CircularSweep.java
@@ -0,0 +1,250 @@
+/* A DD3D Shape consisting of a circularly-swept surface
+
+ Copyright (c) 1998-2000 The Regents of the University of California.
+ All rights reserved.
+ Permission is hereby granted, without written agreement and without
+ license or royalty fees, to use, copy, modify, and distribute this
+ software and its documentation for any purpose, provided that the above
+ copyright notice and the following two paragraphs appear in all copies
+ of this software.
+
+ IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+ FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+ ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+ THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+ SUCH DAMAGE.
+
+ THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+ INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+ PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+ CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ ENHANCEMENTS, OR MODIFICATIONS.
+
+                                        PT_COPYRIGHT_VERSION_2
+                                        COPYRIGHTENDKEY
+
+@ProposedRating Red (chf@eecs.berkeley.edu)
+@AcceptedRating Red (chf@eecs.berkeley.edu)
+*/
+package ptolemy.domains.dd3d.lib;
+
+import ptolemy.kernel.util.*;
+import ptolemy.data.*;
+import ptolemy.data.expr.Parameter;
+import ptolemy.data.type.*;
+import ptolemy.actor.*;
+import ptolemy.actor.lib.*;
+import ptolemy.domains.dt.kernel.DTDebug;
+
+
+import com.sun.j3d.utils.geometry.*;
+import javax.media.j3d.*;
+import javax.vecmath.*;
+
+//////////////////////////////////////////////////////////////////////////
+//// CircularSweep
+/**
+
+@author C. Fong
+*/
+public class CircularSweep extends Shaded3DActor {
+    /** Construct an actor with the given container and name.
+     *  @param container The container.
+     *  @param name The name of this actor.
+     *  @exception IllegalActionException If the actor cannot be contained
+     *   by the proposed container.
+     *  @exception NameDuplicationException If the container already has an
+     *   actor with this name.
+     */
+    public CircularSweep(TypedCompositeActor container, String name)
+            throws IllegalActionException, NameDuplicationException {
+
+        super(container, name);
+        DoubleToken[] tokenArray = new DoubleToken[10];
+        tokenArray[0] = new DoubleToken(0.5);
+        tokenArray[1] = new DoubleToken(0.25);
+        tokenArray[2] = new DoubleToken(0.5);
+        tokenArray[3] = new DoubleToken(-0.25);
+        tokenArray[4] = new DoubleToken(0.25);
+        tokenArray[5] = new DoubleToken(-0.25);
+        tokenArray[6] = new DoubleToken(0.25);
+        tokenArray[7] = new DoubleToken(0.25);
+        tokenArray[8] = new DoubleToken(0.5);
+        tokenArray[9] = new DoubleToken(0.25);
+      	ArrayToken defaultPolygon = new ArrayToken(tokenArray);
+        polyline = new Parameter(this, "polyline", defaultPolygon);
+        polyline.setTypeEquals(new ArrayType(BaseType.ANY));
+        angleSpan = new Parameter(this, "angleSpan", new DoubleToken(2*Math.PI));
+        slices = new Parameter(this, "slices", new IntToken(32));
+    }
+    
+    ///////////////////////////////////////////////////////////////////
+    ////                     ports and parameters                  ////
+
+    /** The line segment array that is to be swept
+     *  This parameter should contain a DoubleToken.
+     */
+    public Parameter polyline;
+    
+    /** The span of sweep angle
+     *  This parameter should contain a DoubleToken.
+     */
+    public Parameter angleSpan;
+    
+    /** The number of slices
+     *  This parameter should contain a IntToken.
+     */
+    public Parameter slices;
+    
+    ///////////////////////////////////////////////////////////////////
+    ////                         public methods                    ////
+    
+    /** Clone the actor into the specified workspace. This calls the
+     *  base class and then sets the parameters of the new actor.
+     *  @param ws The workspace for the new object.
+     *  @return A new actor.
+     *  @exception CloneNotSupportedException If a derived class contains
+     *   an attribute that cannot be cloned.
+     */
+    
+    public Object clone(Workspace workspace) throws CloneNotSupportedException {
+        CircularSweep newobj = (CircularSweep)super.clone(workspace);
+        newobj.polyline = (Parameter) newobj.getAttribute("polyline");
+        newobj.angleSpan = (Parameter) newobj.getAttribute("angleSpan");
+        newobj.slices = (Parameter) newobj.getAttribute("slices");
+        return newobj;
+    }
+
+    /** Return the encapsulated Java3D node of this 3D actor. The encapsulated
+     *  node for this actor is a circular sweep.
+     *  @return the Java3D circular sweep
+     */    
+    public Node getNodeObject() {
+        return (Node) containedNode;
+    }
+
+    ///////////////////////////////////////////////////////////////////
+    ////                         protected methods                 ////
+
+    /** Create the shape and appearance of the encapsulated swept surface
+     */
+    protected void _createModel() throws IllegalActionException {
+    
+        super._createModel();
+        int numberOfSlices = _getSlices();
+        float data[] = _getPolyline();
+        int numberOfSweepVertices = _getVertexCount();
+        int numberOfQuads = (numberOfSweepVertices -1 ) * numberOfSlices;
+        int totalVertices = numberOfQuads * 4;
+        float[] polydata = new float[totalVertices * 3];
+        double span = _getAngleSpan();
+        
+        int[] stripCount = new int[numberOfQuads];
+        int i;
+        for(i=0; i < numberOfQuads; i++) {
+            stripCount[i] = 4;
+        }
+        
+        int j;
+        int k=0;
+        int m=0;
+        for(i=0; i<numberOfSweepVertices-1;i++) {
+            for(j=0; j<numberOfSlices; j++) {
+                float cosFactor1 = (float) Math.cos(span*j/numberOfSlices);
+                float sinFactor1 = (float) Math.sin(span*j/numberOfSlices);
+                float cosFactor2 = (float) Math.cos(span*(j+1)/numberOfSlices);
+                float sinFactor2 = (float) Math.sin(span*(j+1)/numberOfSlices);
+               
+                
+                polydata[k]   = data[m] * cosFactor1;
+                polydata[k+1] = data[m+1];
+                polydata[k+2] = data[m] * sinFactor1;
+                k = k + 3;
+                polydata[k]   = data[m] * cosFactor2;
+                polydata[k+1] = data[m+1];
+                polydata[k+2] = data[m] * sinFactor2;
+                k = k + 3;
+                polydata[k]   = data[m+2] * cosFactor2;
+                polydata[k+1] = data[m+3];
+                polydata[k+2] = data[m+2] * sinFactor2;
+                k = k + 3;
+                polydata[k]   = data[m+2] * cosFactor1;
+                polydata[k+1] = data[m+3];
+                polydata[k+2] = data[m+2] * sinFactor1;
+                k = k + 3;
+            }
+            m = m + 2;
+        }
+        
+        
+        GeometryInfo gi = new GeometryInfo(GeometryInfo.POLYGON_ARRAY);
+        gi.setCoordinates(polydata);
+        gi.setStripCounts(stripCount);
+        
+        Triangulator tr = new Triangulator();
+        tr.triangulate(gi);
+        gi.recomputeIndices();
+
+        NormalGenerator ng = new NormalGenerator();
+        ng.generateNormals(gi);
+        gi.recomputeIndices();
+
+        Stripifier st = new Stripifier();
+        st.stripify(gi);
+        gi.recomputeIndices();
+        
+        containedNode = new Shape3D();
+        containedNode.setAppearance(_appearance);
+        containedNode.setGeometry(gi.getGeometryArray());
+    }
+    
+    
+    ///////////////////////////////////////////////////////////////////
+    ////                         private methods                   ////
+
+    /** Return the polyline
+     *  @return the polyline
+     */
+    private float[] _getPolyline() throws IllegalActionException {
+        ArrayToken arrayToken = ((ArrayToken) polyline.getToken());
+        int numberOfElements = arrayToken.length()/2;
+        float[] data = new float[numberOfElements*2];
+
+        for(int i=0 ;i < numberOfElements * 2; i++) {
+            data[i] = (float) ((DoubleToken)arrayToken.getElement(i)).doubleValue();
+        }
+        
+        return data;
+    }
+    
+    /** Return the vertex count
+     *  @return the vertex count
+     */
+    private int _getVertexCount() throws IllegalActionException {
+        ArrayToken arrayToken = ((ArrayToken) polyline.getToken());
+        int numberOfElements = arrayToken.length()/2;
+        
+        return numberOfElements;
+    }
+
+    /** Return the number of slices
+     *  @return the number of slices
+     */
+    private int _getSlices() throws IllegalActionException {
+       return ((IntToken) slices.getToken()).intValue();
+    }
+    
+    /** Return the angle span of the sweep
+     *  @return the angle span of the sweep
+     */
+    private double _getAngleSpan() throws IllegalActionException  {
+        return ((DoubleToken) angleSpan.getToken()).doubleValue();
+        
+    }
+    
+    ///////////////////////////////////////////////////////////////////
+    ////                         private variables                 ////
+   
+    private Shape3D containedNode;
+}
