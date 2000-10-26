efdd4eb4f44ae2e2bd6fb18a02c795f92419c1b1
neuendor <neuendor>
2000-10-26 22:56:04 +0000

    moved the MMI stuff into the debugger package

--- ptolemy/vergil/debugger/Breakpoint.java
+++ ptolemy/vergil/debugger/Breakpoint.java
@@ -1,27 +1,29 @@
 /* A class that allows to stop a model's execution if a condition returns
    true.
 
- Copyright (c) 1999-2000 SUPELEC.
- All rights reserved.
- Permission is hereby granted, without written agreement and without
- license or royalty fees, to use, copy, modify, and distribute this
- software and its documentation for any purpose, provided that the above
- copyright notice and the following two paragraphs appear in all copies
- of this software.
+ Copyright (c) 1999-2000 SUPELEC and The Regents of the University of
+ California.  All rights reserved.  Permission is hereby granted,
+ without written agreement and without license or royalty fees, to
+ use, copy, modify, and distribute this software and its documentation
+ for any purpose, provided that the above copyright notice and the
+ following two paragraphs appear in all copies of this software.
 
- IN NO EVENT SHALL SUPELEC BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT,
- SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES ARISING OUT OF THE USE 
- OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF SUPELEC HAS BEEN 
- ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ IN NO EVENT SHALL SUPELEC OR THE UNIVERSITY OF CALIFORNIA BE LIABLE
+ TO ANY PARTY FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR
+ CONSEQUENTIAL DAMAGES ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS
+ DOCUMENTATION, EVEN IF SUPELEC OR THE UNIVERSITY OF CALIFORNIA HAVE
+ BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
- SUPELEC SPECIFICALLY DISCLAIMS ANY WARRANTIES, INCLUDING, BUT NOT
- LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
- A PARTICULAR PURPOSE. THE SOFTWARE PROVIDED HEREUNDER IS ON AN "AS IS"
- BASIS, AND SUPELEC HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT,
- UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+ THE UNIVERSITY OF CALIFORNIA AND SUPELEC SPECIFICALLY DISCLAIM ANY
+ WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+ PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+ CALIFORNIA AND SUPELEC HAVE NO OBLIGATION TO PROVIDE MAINTENANCE,
+ SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
 
                                         PT_COPYRIGHT_VERSION_2
                                         COPYRIGHTENDKEY
+
 @ProposedRating Yellow (frederic.boulanger@supelec.fr)
 @AcceptedRating Red 
 */
@@ -42,7 +44,7 @@ this class does not actually halt execution.  Instead, an instance of the
 DebugController class inspects for its presence in an actor 
 and acts accordingly.
 
-@author SUPELEC team and Steve Neuendorffer
+@author  B. Desoutter, P. Domecq & G. Vibert and Steve Neuendorffer
 @version $Id$
 */
 public class Breakpoint extends Variable {
