e768b9129257935edea025927b1156e454f76d72
cxh <cxh>
2000-11-23 02:57:42 +0000

    Converted from import ptolemy.kernel.util.* to individual imports

--- ptolemy/kernel/Relation.java
+++ ptolemy/kernel/Relation.java
@@ -30,7 +30,11 @@
 
 package ptolemy.kernel;
 
-import ptolemy.kernel.util.*;
+import ptolemy.kernel.util.CrossRefList;
+import ptolemy.kernel.util.IllegalActionException;
+import ptolemy.kernel.util.InternalErrorException;
+import ptolemy.kernel.util.NamedObj;
+import ptolemy.kernel.util.Workspace;
 
 import java.util.Collections;
 import java.util.Enumeration;
