0f2c222b053c3bfdfc6ce69c89253707b376416c
cxh <cxh>
2000-11-28 01:13:54 +0000

    import classes individually

--- ptolemy/actor/lib/AbsoluteValue.java
+++ ptolemy/actor/lib/AbsoluteValue.java
@@ -30,14 +30,16 @@
 
 package ptolemy.actor.lib;
 
-import ptolemy.actor.*;
-import ptolemy.kernel.CompositeEntity;
-import ptolemy.kernel.util.*;
-import ptolemy.graph.InequalityTerm;
-import ptolemy.data.*;
+import ptolemy.actor.TypedIOPort;
+import ptolemy.data.ScalarToken;
+import ptolemy.data.expr.Parameter;
 import ptolemy.data.type.Type;
 import ptolemy.data.type.BaseType;
-import ptolemy.data.expr.Parameter;
+import ptolemy.graph.InequalityTerm;
+import ptolemy.kernel.CompositeEntity;
+import ptolemy.kernel.util.IllegalActionException;
+import ptolemy.kernel.util.NameDuplicationException;
+import ptolemy.kernel.util.Workspace;
 
 import java.util.List;
 
