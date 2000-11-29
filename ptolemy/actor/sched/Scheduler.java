935cfd82a817d1b94216b5efc0f6b7dcdb0ac23c
cxh <cxh>
2000-11-29 23:26:58 +0000

    Cleaned up to follow naming conventions

--- ptolemy/actor/sched/Scheduler.java
+++ ptolemy/actor/sched/Scheduler.java
@@ -9,7 +9,7 @@
  of this software.
 
  IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
- FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+ FOR DIRECT, INDIRCT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
  ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
  THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
  SUCH DAMAGE.
@@ -51,19 +51,19 @@ provides the schedule for it.
 The director will use this schedule to govern the execution of a
 CompositeActor.
 <p>
- A schedule is simply a
-collection of objects. It could be the firing order of actors in a
-particular composite actor, and it also could consist of sub-schedules,
-each of which is another collection. We leave for the director to
-interpret what a schedule means. In this base class, the default schedule
-is an Enumeration of deep contained actors in their construction order.
-<P>
+A schedule is simply a collection of objects. It could be the firing
+order of actors in a particular composite actor, and it also could
+consist of sub-schedules, each of which is another collection. We
+leave for the director to interpret what a schedule means. In this
+base class, the default schedule is an Enumeration of deep contained
+actors in their construction order.
+<p>
 The schedule, once constructed, is cached and reused in the next time
 if the schedule is still valid. The validation of a schedule is set by
 the setValid() method. If the current schedule is set to be not valid,
 the schedule() method will call the protected _schedule() method to
-reconstruct it. The _schedule() method is the place
-the scheduling algorithm goes, and the derived class should override it.
+reconstruct it. The _schedule() method is the place the scheduling
+algorithm goes, and the derived class should override it.
 <p>
 Scheduler does perform any mutations, and it is not a topology change
 listener. The director who uses this scheduler should set the validation
@@ -112,7 +112,6 @@ public class Scheduler extends NamedObj {
      *  <i>not</i> added to the directory of that workspace (you must do this
      *  yourself if you want it there).
      *  The result is a new scheduler with no container, and no valid schedule.
-     *
      *  @param workspace The workspace for the cloned object.
      *  @exception CloneNotSupportedException If one of the attributes
      *   cannot be cloned.
@@ -137,17 +136,16 @@ public class Scheduler extends NamedObj {
     }
 
     /** Return the scheduling sequence as an instance of Schedule.
-     *  For efficiency, this method returns a cached version of the schedule,
-     *  if it is valid.  Otherwise, it calls the protected method _getSchedule()
-     *  to update the schedule.  Derived classes would normally override
-     *  the protected method, not this one.
-     *  The validity of the current schedule is set by the setValid() method.
-     *  This method is read-synchronized on the workspace.
-     *
-     * @return The Schedule returned by the _getSchedule() method.
-     * @exception IllegalActionException If the scheduler has no container
+     *  For efficiency, this method returns a cached version of the
+     *  schedule, if it is valid.  Otherwise, it calls the protected
+     *  method _getSchedule() to update the schedule.  Derived classes
+     *  would normally override the protected method, not this one.
+     *  The validity of the current schedule is set by the setValid()
+     *  method.  This method is read-synchronized on the workspace.
+     *  @return The Schedule returned by the _getSchedule() method.
+     *  @exception IllegalActionException If the scheduler has no container
      *  (a director), or the director has no container (a CompositeActor).
-     * @exception NotSchedulableException If the _getSchedule() method
+     *  @exception NotSchedulableException If the _getSchedule() method
      *  throws it. Not thrown in this base class, but may be needed
      *  by the derived schedulers.
      */
@@ -155,14 +153,15 @@ public class Scheduler extends NamedObj {
             IllegalActionException, NotSchedulableException {
         try {
             workspace().getReadAccess();
-            StaticSchedulingDirector dir =
+            StaticSchedulingDirector director =
                 (StaticSchedulingDirector)getContainer();
-            if (dir == null) {
+            if (director == null) {
                 throw new IllegalActionException(this,
                         "Scheduler has no director.");
             }
-            CompositeActor ca = (CompositeActor)(dir.getContainer());
-            if (ca == null) {
+            CompositeActor compositeActor =
+                (CompositeActor)(director.getContainer());
+            if (compositeActor == null) {
                 throw new IllegalActionException(this,
                         "Director has no container.");
             }
@@ -175,18 +174,18 @@ public class Scheduler extends NamedObj {
         }
     }
 
-    /** Return the scheduling sequence as an enumeration.
-     *  For efficiency, this method returns a cached version of the schedule,
-     *  if it is valid.  Otherwise, it calls the protected method _schedule()
-     *  to update the schedule.  Derived classes would normally override
-     *  the protected method, not this one.
-     *  The validity of the current schedule is set by the setValid() method.
-     *  This method is read-synchronized on the workspace.
+    /** Return the scheduling sequence as an enumeration.  For
+     *  efficiency, this method returns a cached version of the
+     *  schedule, if it is valid.  Otherwise, it calls the protected
+     *  method _schedule() to update the schedule.  Derived classes
+     *  would normally override the protected method, not this one.
+     *  The validity of the current schedule is set by the setValid()
+     *  method.  This method is read-synchronized on the workspace.
      *
-     * @return The Enumeration returned by the _schedule() method.
-     * @exception IllegalActionException If the scheduler has no container
+     *  @return The Enumeration returned by the _schedule() method.
+     *  @exception IllegalActionException If the scheduler has no container
      *  (a director), or the director has no container (a CompositeActor).
-     * @exception NotSchedulableException If the _schedule() method
+     *  @exception NotSchedulableException If the _schedule() method
      *  throws it. Not thrown in this base class, but may be needed
      *  by the derived schedulers.
      */
@@ -194,14 +193,15 @@ public class Scheduler extends NamedObj {
             IllegalActionException, NotSchedulableException {
         try {
             workspace().getReadAccess();
-            StaticSchedulingDirector dir =
+            StaticSchedulingDirector director =
                 (StaticSchedulingDirector)getContainer();
-            if (dir == null) {
+            if (director == null) {
                 throw new IllegalActionException(this,
                         "Scheduler has no director.");
             }
-            CompositeActor ca = (CompositeActor)(dir.getContainer());
-            if (ca == null) {
+            CompositeActor compositeActor =
+                (CompositeActor)(director.getContainer());
+            if (compositeActor == null) {
                 throw new IllegalActionException(this,
                         "Director has no container.");
             }
@@ -218,13 +218,11 @@ public class Scheduler extends NamedObj {
         }
     }
 
-    /** Validate/invalidate the current schedule by giving a true/false
-     *  argument.
-     *  A true argument will indicate that the current
-     *  schedule is valid
-     *  and can be returned immediately when schedule() is called without
-     *  running the scheduling algorithm. A false argument
-     *  will invalidate it.
+    /** Validate/invalidate the current schedule by giving a
+     *  true/false argument.  A true argument will indicate that the
+     *  current schedule is valid and can be returned immediately when
+     *  schedule() is called without running the scheduling
+     *  algorithm. A false argument will invalidate it.
      *  @param valid True to set the current schedule to valid.
      */
     public void setValid(boolean valid) {
@@ -252,35 +250,35 @@ public class Scheduler extends NamedObj {
     ////                         protected methods                 ////
 
     /** Return the scheduling sequence. In this base class, it returns
-     *  the containees of the CompositeActor in the order of construction.
-     *  (Same as calling CompositeActor.deepGetEntities()).
-     *  The derived classes should
+     *  the containees of the CompositeActor in the order of
+     *  construction.  (Same as calling
+     *  CompositeActor.deepGetEntities()).  The derived classes should
      *  override this method and add their scheduling algorithms here.
      *  This method should not be called directly, but rather the
-     *  getSchedule() method will call it when the schedule is invalid. So
-     *  it is not synchronized on the workspace.
-     *
-     * @see ptolemy.kernel.CompositeEntity#deepGetEntities()
-     * @return A Schedule of the deeply contained opaque entities
+     *  getSchedule() method will call it when the schedule is
+     *  invalid. So it is not synchronized on the workspace.
+     *  @return A Schedule of the deeply contained opaque entities
      *  in the firing order.
-     * @exception NotSchedulableException If the CompositeActor is not
+     *  @exception NotSchedulableException If the CompositeActor is not
      *  schedulable. Not thrown in this base class, but may be needed
      *  by the derived scheduler.
+     *  @see ptolemy.kernel.CompositeEntity#deepGetEntities()
      */
     protected Schedule _getSchedule() throws NotSchedulableException {
-	StaticSchedulingDirector dir =
+	StaticSchedulingDirector director =
             (StaticSchedulingDirector)getContainer();
-        CompositeActor ca = (CompositeActor)(dir.getContainer());
-        List actors = ca.deepEntityList();
-	Schedule schdule = new Schedule();
+        CompositeActor compositeActor =
+            (CompositeActor)(director.getContainer());
+        List actors = compositeActor.deepEntityList();
+	Schedule schedule = new Schedule();
 	Iterator actorIterator = actors.iterator();
 	while (actorIterator.hasNext()) {
 	    Executable actor = (Executable)actorIterator.next();
 	    Firing firing = new Firing();
 	    firing.setActor(actor);
-	    schdule.add(firing);
+	    schedule.add(firing);
 	}
-	return schdule;
+	return schedule;
     }
 
     /** Make this scheduler the scheduler of the specified director, and
@@ -289,35 +287,35 @@ public class Scheduler extends NamedObj {
      *  setScheduler() method of the StaticSchedulingDirector class
      *  (or a derived class).
      */
-    protected void _makeSchedulerOf(StaticSchedulingDirector dir) {
-        _container = dir;
-        if (dir != null) {
+    protected void _makeSchedulerOf(StaticSchedulingDirector director) {
+        _container = director;
+        if (director != null) {
             workspace().remove(this);
         }
     }
 
     /** Return the scheduling sequence. In this base class, it returns
-     *  the containees of the CompositeActor in the order of construction.
-     *  (Same as calling CompositeActor.deepGetEntities()).
-     *  The derived classes should
+     *  the containees of the CompositeActor in the order of
+     *  construction.  (Same as calling
+     *  CompositeActor.deepGetEntities()).  The derived classes should
      *  override this method and add their scheduling algorithms here.
-     *  This method should not be called directly, rather the schedule()
-     *  method
-     *  will call it when the schedule is invalid. So it is not
-     *  synchronized on the workspace.
+     *  This method should not be called directly, rather the
+     *  schedule() method will call it when the schedule is
+     *  invalid. So it is not synchronized on the workspace.
      *
-     * @see ptolemy.kernel.CompositeEntity#deepGetEntities()
-     * @return An Enumeration of the deeply contained opaque entities
+     *  @return An Enumeration of the deeply contained opaque entities
      *  in the firing order.
-     * @exception NotSchedulableException If the CompositeActor is not
+     *  @exception NotSchedulableException If the CompositeActor is not
      *  schedulable. Not thrown in this base class, but may be needed
      *  by the derived scheduler.
+     *  @see ptolemy.kernel.CompositeEntity#deepGetEntities()
      */
     protected Enumeration _schedule() throws NotSchedulableException {
-        StaticSchedulingDirector dir =
+        StaticSchedulingDirector director =
             (StaticSchedulingDirector)getContainer();
-        CompositeActor ca = (CompositeActor)(dir.getContainer());
-        return Collections.enumeration(ca.deepEntityList());
+        CompositeActor compositeActor =
+            (CompositeActor)(director.getContainer());
+        return Collections.enumeration(compositeActor.deepEntityList());
     }
 
     ///////////////////////////////////////////////////////////////////
