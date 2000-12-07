71213380093f52816cec476dbfa20e663e49436d
cxh <cxh>
2000-12-07 00:25:47 +0000

    Removed clone(), TypedAtomicActor does what want

--- ptolemy/domains/gr/lib/Box3D.java
+++ ptolemy/domains/gr/lib/Box3D.java
@@ -98,21 +98,6 @@ public class Box3D extends GRShadedShape {
     ///////////////////////////////////////////////////////////////////
     ////                         public methods                    ////
 
-    /** Clone the actor into the specified workspace. This calls the
-     *  base class and then sets the parameters of the new actor.
-     *  @param workspace The workspace for the new object.
-     *  @return A new actor.
-     *  @exception CloneNotSupportedException If a derived class contains
-     *   an attribute that cannot be cloned.
-     */
-    public Object clone(Workspace workspace) throws CloneNotSupportedException {
-        Box3D newObject = (Box3D)super.clone(workspace);
-        newObject.xLength = (Parameter) newObject.getAttribute("xLength");
-        newObject.yHeight = (Parameter) newObject.getAttribute("yHeight");
-        newObject.zWidth = (Parameter) newObject.getAttribute("zWidth");
-        return newObject;
-    }
-
     /** Return the encapsulated Java3D node of this 3D actor. The encapsulated
      *  node for this actor is a Java3D box.
      *  @return the Java3D box.
