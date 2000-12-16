2518ef9b0b2ee5bf6b9635de246768b5bc95f83c
liuj <liuj>
2000-12-16 01:17:30 +0000

    use the new applet infrastructure

--- ptolemy/domains/ct/demo/Helicopter/ClimbLinearizer.java
+++ ptolemy/domains/ct/demo/Helicopter/ClimbLinearizer.java
@@ -54,7 +54,7 @@ public class ClimbLinearizer extends TypedAtomicActor
     implements TimedActor{
     /** Constructor
      */
-    public ClimbLinearizer(TypedCompositeActor container, String name)
+    public ClimbLinearizer(CompositeEntity container, String name)
             throws NameDuplicationException, IllegalActionException  {
         super(container, name);
         inputPx = new TypedIOPort(this, "inputPx");
