9a7e4e2004e3295788ba2192f27607802bb7af85
yuhong <yuhong>
2000-12-17 19:36:08 +0000

    improve exception message.

--- ptolemy/actor/lib/test/RecordDisassembler.tcl
+++ ptolemy/actor/lib/test/RecordDisassembler.tcl
@@ -84,7 +84,8 @@ test RecordDisassembler-2.1 {run with one input port, has label error} {
     set m [$e0 getManager]
     catch {$m execute} msg
     list $msg
-} {{ptolemy.kernel.util.InvalidStateException: InequalitySolver.solve: Can't update variable. TypedIOPort$TypeTerm.setValue: The new type violates the declared type of this port. Port: .top.disassembler.input, Port Type: {disRampOut:NaT}, newType: {}}}
+} {{ptolemy.kernel.util.InvalidStateException: InequalitySolver.solve: Can't update variable. TypedIOPort$TypeTerm.setValue: Cannot update the type of this port to the new type. Port: .top.disassembler.input, Port type: {disRampOut:NaT}, New type: {}}}
+
 
 test RecordDisassembler-2.2 {correct above label error} {
     $disRampOut setName fromRamp
