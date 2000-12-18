fc0764531251399eae548ad89afda68411fd7db0
eal <eal>
2000-12-18 23:23:29 +0000

    interrim checkin with test succeeding

--- ptolemy/codegen/saveasjava/SaveAsJava.java
+++ ptolemy/codegen/saveasjava/SaveAsJava.java
@@ -34,14 +34,13 @@ import ptolemy.kernel.ComponentEntity;
 import ptolemy.kernel.CompositeEntity;
 import ptolemy.kernel.Relation;
 import ptolemy.kernel.Port;
+import ptolemy.kernel.util.Attribute;
 import ptolemy.kernel.util.NamedObj;
 import ptolemy.kernel.util.IllegalActionException;
 import ptolemy.kernel.util.NameDuplicationException;
 
-
 import java.util.LinkedList;
-import java.util.List;
-import java.util.ListIterator;
+import java.util.Iterator;
 
 //////////////////////////////////////////////////////////////////////////
 //// SaveAsJava
@@ -99,6 +98,9 @@ class SaveAsJava {
                 + " IllegalActionException {\n" + _indent(2) 
                 + "super(w);\n\n" + _indent(2) + "try {\n";
 
+        // Generate attributes.
+        code += _generateAttributes(compositeModel);
+
         // Generate code to instantiate and connect the system components
         code += _generateComponents(compositeModel);
 
@@ -111,7 +113,6 @@ class SaveAsJava {
         "}\n";
          
         // Generate statements for importing classes.
-        _insertIfUnique("ptolemy.actor.GeneratorTableau", _importList);
         _insertIfUnique("ptolemy.kernel.util.Workspace", _importList);
         _insertIfUnique("ptolemy.kernel.util.IllegalActionException", 
                         _importList);
@@ -119,10 +120,10 @@ class SaveAsJava {
                         _importList);
         _insertIfUnique("ptolemy.actor.TypedIORelation", _importList);
         try {
-            ListIterator iter = _importList.listIterator(0);
+            Iterator iter = _importList.iterator();
             while (iter.hasNext()) {
-              String p = (String)(iter.next());
-              importCode += "import " + p + ";\n";
+                String p = (String)(iter.next());
+                importCode += "import " + p + ";\n";
             }
             code = importCode + "\n" + code; 
         } catch (Exception ex) {
@@ -138,16 +139,48 @@ class SaveAsJava {
     ///////////////////////////////////////////////////////////////////
     ////                         protected methods                 ////
 
+    /** Generate Java code that creates attributes, if necessary,
+     *  and initializes them with their initial values.
+     *  @param component The component with attributes.
+     *  @return The Java code defining attributes for the specified
+     *   component.
+     */
+    protected String _generateAttributes(NamedObj component) {
+        StringBuffer result = new StringBuffer();
+        Iterator attributes = component.attributeList().iterator();
+        while (attributes.hasNext()) {
+            Attribute attribute = (Attribute)attributes.next();
+
+            // FIXME: For now, creating new attributes always.
+            // Really should check to see whether the component has
+            // a public member whose name and type match the attribute,
+            // and if so, just set that.
+
+            String attributeClass = _getClassName(attribute);
+            result.append(_indent(3));
+            result.append(attributeClass);
+            result.append(" ");
+            result.append(attribute.getName());
+            result.append(" = new ");
+            result.append(attributeClass);
+            result.append("(this, \"");
+            result.append(attribute.getName());
+            result.append("\");");
+            result.append("\n");
+        }
+        return result.toString();
+    }
+
     /** Generate Ptolemy II Java code to instantiate a component,
      *  and for a composite component, the code to instantiate and
      *  connect all components that are nested within the component. 
-
+     *
      *  @param model The component for which code is to be generated.
      *  @return The generated Ptolemy II Java code for implementing
      *   the component.
      */
     protected String _generateComponents(ComponentEntity model) 
-    throws IllegalActionException {
+            throws IllegalActionException {
         String code = new String(); 
         String clname = _getClassName(model);
 
@@ -160,27 +193,26 @@ class SaveAsJava {
         else {
 
              // Instantiate the actors inside the composite actor
-             List components = ((CompositeEntity)model).entityList();
-             ListIterator compiter = components.listIterator(0);
-             while (compiter.hasNext()) {
+             Iterator components = ((CompositeEntity)model)
+                     .entityList().iterator();
+             while (components.hasNext()) {
                  code += _generateComponents((ComponentEntity)
-                         (compiter.next())); 
+                         (components.next())); 
              }
 
              // Instantiate the connections between actors
-             List relations = ((CompositeEntity)model).relationList();
-             ListIterator reliter = relations.listIterator(0);
-             while (reliter.hasNext()) {
-                 Relation rel = (Relation)(reliter.next());
-                 List ports = rel.linkedPortList();
-                 ListIterator portsiter = ports.listIterator(0);
+             Iterator relations = ((CompositeEntity)model)
+                    .relationList().iterator();
+             while (relations.hasNext()) {
+                 Relation relation = (Relation)(relations.next());
+                 Iterator ports = relation.linkedPortList().iterator();
 
                  // We adopt the convention of using the "connect"
                  // method for relations that are incident to exactly
                  // two links.
-                 if (rel.numLinks() == 2) {
-                     Port port1 = (Port) portsiter.next();
-                     Port port2 = (Port) portsiter.next();
+                 if (relation.numLinks() == 2) {
+                     Port port1 = (Port) ports.next();
+                     Port port2 = (Port) ports.next();
                      code += _indent(3);
                      code += "connect (" + port1.getContainer().getName()
                      + "." + port1.getName() + ", "
@@ -190,15 +222,17 @@ class SaveAsJava {
                  // Explicitly instantiate the relation, and generate 
                  // a link() call for each port associated with the relation.
                  else {
-                     code += _indent(3) + "TypedIORelation " + rel.getName();
+                     code += _indent(3)
+                             + "TypedIORelation "
+                             + relation.getName();
                      code += " = new TypedIORelation(" + "this";
-                     code += ", \"" + rel.getName() + "\");\n";
-                     while (portsiter.hasNext()) {
-                         Port p = (Port)portsiter.next();
+                     code += ", \"" + relation.getName() + "\");\n";
+                     while (ports.hasNext()) {
+                         Port p = (Port)ports.next();
                          code += _indent(3);
                          code += p.getContainer().getName() + ".";
                          code += p.getName() + ".link(";
-                         code += rel.getName() + ");\n"; 
+                         code += relation.getName() + ");\n"; 
                      }
                  }
              }
@@ -208,29 +242,6 @@ class SaveAsJava {
         return code;
        
     }
-     
-
-    /** Split the specified name at the last period and return the
-     *  two parts as a two-element array.  If there is no period, the second
-     *  element is null.
-     *  (This method is ported from a similarly-named protected method 
-     *   in NamedObj that splits at the first, rather than last, period)
-     *  @param name The name to split.
-     *  @return The name before and after the last period as a two-element
-     *   array.
-     */
-    protected static final String[] _splitName(String name) {
-        String[] result = new String[2];
-        int period = name.lastIndexOf(".");
-        if (period < 0) {
-            result[0] = name;
-        } else {
-            result[0] = name.substring(0, period);
-            result[1] = name.substring(period + 1);
-        }
-        return result;
-    }
-
 
     /** Split the specified name at the last period, and return the
      *  last part. If there is no period, return the original name.
@@ -239,9 +250,7 @@ class SaveAsJava {
      */
      
     protected static final String _extractSuffix(String name) {
-        String[] clnames = _splitName(name);
-        if (clnames[1].length() == 0) return clnames[0];
-        else return clnames[1];
+        return name.substring(name.lastIndexOf(".") + 1);
     }
 
     ///////////////////////////////////////////////////////////////////
@@ -273,7 +282,7 @@ class SaveAsJava {
     // imported in the generated Java code, insert the class name
     // (fully qualified) into this list.
     private String _getClassName(NamedObj object) {
-            String clfullname = object.getName();
+            String clfullname = object.getClass().getName();
             String clname = _extractSuffix(clfullname);
             _insertIfUnique(clfullname, _importList);
             return clname;
