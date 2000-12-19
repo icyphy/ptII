4dc30f2431d19affe9445e116a70a548ad8c864d
cxh <cxh>
2000-12-19 21:17:36 +0000

    set the ptolemy.ptII.dir property

--- bin/vergil.bat
+++ bin/vergil.bat
@@ -6,5 +6,5 @@ rem @version $Id$
 rem @copyright: Copyright (c) 1997-2000
 rem The Regents of the University of California.
 
-java -classpath %PTII%;%PTII%\lib\diva.jar ptolemy.vergil.VergilApplication %1 %2 %3 %4 %5 %6 %7 %8 %9
+java -Dptolemy.ptII.dir=%PTII% -classpath %PTII%;%PTII%\lib\diva.jar ptolemy.vergil.VergilApplication %1 %2 %3 %4 %5 %6 %7 %8 %9
 
