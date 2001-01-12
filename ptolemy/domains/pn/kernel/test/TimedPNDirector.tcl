b6a94d4361b6e8ea025a16f66895e58eda29ad63
cxh <cxh>
2001-01-12 16:59:02 +0000

    Mark the test as a known failure

--- ptolemy/domains/pn/kernel/test/TimedPNDirector.tcl
+++ ptolemy/domains/pn/kernel/test/TimedPNDirector.tcl
@@ -137,10 +137,10 @@ test TimedPNDirector-6.1 {Test an application} {
     $e0 connect \
 	  [java::field [java::cast ptolemy.actor.lib.Source $ramp] output] \
 	  [java::field [java::cast ptolemy.actor.lib.Sink $rec] input]
-    set a "This test hangs when we all execute"
+    set a "This test hangs when we call execute"
 #    [$e0 getManager] execute
 #    enumToTokenValues [$rec getRecord 0]
 
-} {0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19}
+} {0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19} {KNOWN_FAILURE: This test hangs when we call execute}
 
 
