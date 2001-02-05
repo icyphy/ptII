fa8bc61ad67986680b4cfa603d250b2c6b62c6f2
shuvra <shuvra>
2001-02-05 21:06:20 +0000

    Generized to regenerate code based on an arbitrary code generator or list of code generation passes. Also added optional static resolution and verbose output modes.

--- ptolemy/lang/java/RegenerateCode.java
+++ ptolemy/lang/java/RegenerateCode.java
@@ -36,16 +36,74 @@ package ptolemy.lang.java;
 
 import ptolemy.lang.*;
 import ptolemy.lang.java.nodetypes.CompileUnitNode;
+import java.util.List;
+import java.util.Iterator;
+import java.util.LinkedList;
+import ptolemy.lang.java.StaticResolution;
+
+
+
+
 
 //////////////////////////////////////////////////////////////////////////
 //// RegenerateCode
-/** An application that parses a Java program, then reconstructs the source
- *  program using the AST. This is used for testing purposes.
+/** A class that parses Java source files, and converts them
+ *  using the resulting abstract syntax trees, and a code generator, or 
+ *  list of visitors (passes), that is supplied
+ *  in the class constructor. If no code generator is specified in
+ *  the constructor then use the Java code generator. 
+ *  This class is used for testing purposes. It can also be used
+ *  as a standalone program for converting Java source files 
+ *  into other languages or formats.
  *
- *  @author Jeff Tsay
+ *  @author Jeff Tsay and Shuvra S. Bhattacharyya 
  */
-public class RegenerateCode {
-    public static void main(String[] args) {
+public class RegenerateCode implements JavaStaticSemanticConstants {
+
+    /**
+     * Use the specified visitor as a single-pass code generator.
+     *
+     * @param visitor The visitor to use during code generation. 
+     */
+    public RegenerateCode(JavaVisitor visitor) {
+        _passList = new LinkedList();
+        _passList.add(visitor);  
+    }
+    
+    /**
+     * Use the specified list of visitors as a sequence of code
+     * generation passes.
+     *
+     * @param passList The list of passes to use during code generation. 
+     */
+    public RegenerateCode(LinkedList passList) {
+        Iterator passes = passList.iterator();
+        while(passes.hasNext()) {
+            if (!(passes.next() instanceof JavaVisitor)) {
+                // FIXME: insert exception
+            }
+        }
+        _passList = passList;
+    }
+   
+     
+    /**
+     * Use the Java code generator to generate the output.
+     */
+    public RegenerateCode() {
+         _passList = new LinkedList();
+         _passList.add(new JavaCodeGenerator());
+    }
+
+    /**
+     *  Parse a list of one or more Java source files, and
+     *  generate code using the resulting abstract syntax trees
+     *  and the code generator pass(es) with which this
+     *  object has been configured.
+     *
+     * @param args The Java source files that are to be converted.
+     */
+    public void regenerate(String[] args) {
         int files = args.length;
         int fileStart = 0;
         boolean debug = false;
@@ -73,16 +131,81 @@ public class RegenerateCode {
                 System.err.println(e.toString());
             }
 
+            // Parse the Java input, and construct the abstract syntax tree.
             javaParser.yydebug = debug;
-
             javaParser.yyparse();
-
             CompileUnitNode ast = javaParser.getAST();
 
-            String codeString = (String) ast.accept(new JavaCodeGenerator());
+            if (_verbose) {
+                System.out.println("Input Java parsed. The AST follows.");
+                System.out.println(ast.toString());
+            }
 
-            System.out.println("// Regenerated file : " + args[f + fileStart]);
-            System.out.println(codeString);
+            ast.setProperty(IDENT_KEY, args[f + fileStart]);
+
+            if (_performStaticResolution) {
+                // Perform all three passes of static resolution
+                if (_verbose) {
+                    System.out.println("Beginning static resolution");
+                }
+                StaticResolution.loadCompileUnit(ast, 2);
+                if (_verbose) {
+                    System.out.println("Static resolution complete. The new AST:");
+                    System.out.println(ast.toString());
+                }
+            }
+
+            // Invoke the specified passes on the abstract syntax tree.
+            Iterator passes = _passList.iterator();
+            String resultString = new String();
+            int passNumber = 1;
+            Object passResult = null;
+            while(passes.hasNext()) {
+                Object pass = passes.next();
+                if (_verbose) {
+                    System.out.print("Starting Pass #" + passNumber + ": ");
+                    System.out.println(pass.getClass().getName());
+                }
+                passResult = ast.accept((JavaVisitor)pass);
+                if (_verbose) {
+                    System.out.println("The AST after Pass #" + passNumber + ":");
+                    System.out.println(ast.toString());
+                }
+                passNumber++;
+            }
+
+
+            System.out.println("/* Regenerated source file for : " + 
+                    args[f + fileStart] + " */");
+            if (passResult != null) System.out.println((String)passResult);
         }
     }
+
+    /**
+     * Configure the output format and static resolution flag of
+     * code generation. No configuration is required if
+     * the default behavior (no verbose output and no static resolution)
+     * is desired.
+     * @param verbose Indicates whether or not verbose output should
+     * be generated.
+     * @param performStaticResolution Indicates whether or not
+     * static resolution should be performed prior to invoking the
+     * given list of code generation passes.
+     */ 
+    public void configure(boolean verbose, boolean performStaticResolution) {
+        _verbose = verbose;
+        _performStaticResolution = performStaticResolution;
+    }
+
+    // The "back-end" code generator used to regenerate the input Java. 
+    private LinkedList _passList;
+
+    // Indicates whether or not verbose output format should be
+    // used. This includes diagnostic output, and abstract syntax tree displays 
+    // after each code generation pass.
+    private boolean _verbose = false;
+
+    // Indicates whether or not static resolution should be peformed
+    // prior to invoking the given list of code generation passes.
+    private boolean _performStaticResolution = false;
 }
