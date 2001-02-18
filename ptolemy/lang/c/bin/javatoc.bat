597e86403b508f26e9377c950432b9354059f2e9
shuvra <shuvra>
2001-02-18 18:32:19 +0000

    Updated to run from any directory (not just the ptolemy.lang.c directory)

--- ptolemy/lang/c/bin/javatoc.bat
+++ ptolemy/lang/c/bin/javatoc.bat
@@ -2,14 +2,15 @@
 rem
 rem A script to convert Java files to equivalent C code (.h and .c files).
 rem Usage: 'javatoc <arg>',
-rem where test\<arg>.java is a Java source file in .\test
+rem where <arg>.java is a Java source file.
 rem
 rem The following output is generated:
-rem test\<arg-out>.txt:  diagnostic output
-rem test\<arg>.c:  generated .c file
-rem test\<arg>.h:  generated .h file
+rem <arg-out>.txt:  diagnostic output
+rem <arg>.c:  generated .c file
+rem <arg>.h:  generated .h file
 rem
 rem @author Shuvra S. Bhattacharyya 
 rem @version $Id$
 rem
-java ptolemy.lang.c.JavaToC test\%1.java > test\%1-out.txt
+if exist %1-out.txt del %1-out.txt
+java ptolemy.lang.c.JavaToC %1.java > %1-out.txt 
