d7b8ee45bf47562cf89b802eaff6f8cb85b7c03d
neuendor <neuendor>
2001-02-19 21:07:07 +0000

    added a test to detect a bad configuration

---
+++ ptolemy/configs/test/allConfigs.tcl
@@ -0,0 +1,74 @@
+# Tests for the TypedAtomicActor class
+#
+# @Author: Edward A. Lee, Yuhong Xiong
+#
+# $Id$
+#
+# @Copyright (c) 2000-2001 The Regents of the University of California.
+# All rights reserved.
+#
+# Permission is hereby granted, without written agreement and without
+# license or royalty fees, to use, copy, modify, and distribute this
+# software and its documentation for any purpose, provided that the
+# above copyright notice and the following two paragraphs appear in all
+# copies of this software.
+#
+# IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+# FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+# ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+# THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+#
+# THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+# INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+# PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+# CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+# ENHANCEMENTS, OR MODIFICATIONS.
+#
+# 						PT_COPYRIGHT_VERSION_2
+# 						COPYRIGHTENDKEY
+#######################################################################
+
+# Tycho test bed, see $TYCHO/doc/coding/testing.html for more information.
+
+# Load up the test definitions.
+if {[string compare test [info procs test]] == 1} then {
+    source testDefs.tcl
+} {}
+
+# Uncomment this to get a full report, or set in your Tcl shell window.
+# set VERBOSE 1
+
+# If a file contains non-graphical tests, then it should be named .tcl
+# If a file contains graphical tests, then it should be called .itcl
+#
+# It would be nice if the tests would work in a vanilla itkwish binary.
+# Check for necessary classes and adjust the auto_path accordingly.
+#
+
+# NOTE:  All of the following tests use this director,
+# pretty much as a dummy.
+set director [java::new ptolemy.actor.Director]
+set manager [java::new ptolemy.actor.Manager]
+
+######################################################################
+####
+#
+
+
+cd ..
+set configs [glob *Configuration*.xml]
+cd test
+foreach i $configs {
+    test "$i-1.1" "Test to see if $i contains any bad XML" {
+	set parser [java::new ptolemy.moml.MoMLParser]
+	set loader [[$parser getClass] getClassLoader]
+	
+	set URL [$loader getResource ptolemy/configs/$i]
+	set object [$parser {parse java.net.URL java.net.URL} $URL $URL]
+	# force everything to get expanded
+	set configuration [java::cast ptolemy.kernel.CompositeEntity $object]
+	catch {$configuration description} result
+    } {0}
+}
