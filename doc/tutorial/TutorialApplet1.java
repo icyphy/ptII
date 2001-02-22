287d9ed1ab134853d57b9f871c40f5513c2aaa78
cxh <cxh>
2001-02-22 19:22:03 +0000

    Starting work on new infrastructure

--- doc/tutorial/TutorialApplet1.java
+++ doc/tutorial/TutorialApplet1.java
@@ -1,15 +1,22 @@
 package doc.tutorial;
-import ptolemy.domains.de.gui.DEApplet;
+import ptolemy.actor.TypedCompositeActor;
+import ptolemy.actor.gui.PtolemyApplet;
 import ptolemy.actor.lib.Clock;
 import ptolemy.actor.lib.gui.TimedPlotter;
+import ptolemy.domains.de.kernel.DEDirector;
 
-public class TutorialApplet extends DEApplet {
+public class TutorialApplet extends PtolemyApplet {
     public void init() {
         super.init();
+        TypedCompositeActor toplevel = new TypedCompositeActor(_workspace);
+        _toplevel = toplevel;
         try {
-            Clock clock = new Clock(_toplevel,"clock");
-            TimedPlotter plotter = new TimedPlotter(_toplevel,"plotter");
-            _toplevel.connect(clock.output, plotter.input);
-        } catch (Exception ex) {}
+            new DEDirector(toplevel, "DEDirector");
+            Clock clock = new Clock(toplevel,"clock");
+            TimedPlotter plotter = new TimedPlotter(toplevel,"plotter");
+            toplevel.connect(clock.output, plotter.input);
+        } catch (Exception exception) {
+            System.out.println(exception);
+        }
     }
 }
