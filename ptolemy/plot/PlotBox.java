7f3aa0e72bc2ebc6c4039d9dab5ef42d8e324512
cxh <cxh>
2001-02-22 17:50:02 +0000

    write(): Changed plotml.dtd to PlotML_1.dtd

--- ptolemy/plot/PlotBox.java
+++ ptolemy/plot/PlotBox.java
@@ -1171,7 +1171,7 @@ public class PlotBox extends JPanel implements Printable {
             output.println(
                     "<!DOCTYPE plot PUBLIC \"-//UC Berkeley//DTD PlotML 1//EN\"");
             output.println(
-                    "    \"http://ptolemy.eecs.berkeley.edu/xml/dtd/plotml.dtd\">");
+                    "    \"http://ptolemy.eecs.berkeley.edu/xml/dtd/PlotML_1.dtd\">");
         } else {
             output.println("<?xml version=\"1.0\" standalone=\"no\"?>");
             output.println("<!DOCTYPE plot SYSTEM \"" + dtd + "\">");
