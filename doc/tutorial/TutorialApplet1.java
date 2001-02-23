d3af2acc5018ebd8f572964fce343c501e123e0c
cxh <cxh>
2001-02-23 21:06:47 +0000

    Updated to handle new PtolemyApplet infrastructure

--- doc/tutorial/TutorialApplet1.java
+++ doc/tutorial/TutorialApplet1.java
@@ -1,15 +1,29 @@
 package doc.tutorial;
-import ptolemy.domains.de.gui.DEApplet;
+import ptolemy.actor.TypedCompositeActor;
+import ptolemy.actor.gui.PtolemyApplet;
 import ptolemy.actor.lib.Clock;
 import ptolemy.actor.lib.gui.TimedPlotter;
+import ptolemy.domains.de.kernel.DEDirector;
+import ptolemy.kernel.util.IllegalActionException;
+import ptolemy.kernel.util.NameDuplicationException;
+import ptolemy.kernel.util.Workspace;
 
-public class TutorialApplet extends DEApplet {
-    public void init() {
-        super.init();
-        try {
-            Clock clock = new Clock(_toplevel,"clock");
-            TimedPlotter plotter = new TimedPlotter(_toplevel,"plotter");
-            _toplevel.connect(clock.output, plotter.input);
-        } catch (Exception ex) {}
+public class TutorialApplet1 extends TypedCompositeActor {
+    public TutorialApplet1(Workspace workspace)
+	throws IllegalActionException, NameDuplicationException {
+	super(workspace);
+
+	// Create the director
+	DEDirector director = new DEDirector(this, "director");
+	setDirector(director);
+	director.stopTime.setExpression("10.0");
+
+	// Create two actors.
+	Clock clock = new Clock(this,"clock");
+	TimedPlotter plotter = new TimedPlotter(this,"plotter");
+
+	// Connect them.
+	connect(clock.output, plotter.input);
     }
 }
+
--- doc/tutorial/TutorialApplet5.java
+++ doc/tutorial/TutorialApplet5.java
@@ -1,33 +1,61 @@
 package doc.tutorial;
-import ptolemy.domains.de.gui.DEApplet;
+
+import ptolemy.actor.TypedCompositeActor;
+import ptolemy.actor.gui.PtolemyApplet;
 import ptolemy.actor.lib.Clock;
 import ptolemy.actor.lib.gui.TimedPlotter;
+import ptolemy.domains.de.kernel.DEDirector;
 import ptolemy.gui.Query;
 import ptolemy.gui.QueryListener;
-import java.awt.Panel;
-import java.awt.Dimension;
+import ptolemy.kernel.util.NamedObj;
+import ptolemy.kernel.util.Workspace;
 
-public class TutorialApplet extends DEApplet implements QueryListener {
-    private Query _query = new Query();
+import java.awt.BorderLayout;
+
+public class TutorialApplet5 extends PtolemyApplet implements QueryListener {
     private Clock _clock;
-    public void init() {
-        super.init();
-        try {
-            _clock = new Clock(_toplevel,"clock");
-            TimedPlotter plotter = new TimedPlotter(_toplevel,"plotter");
-            plotter.place(getContentPane());
-            Dimension size = getSize();
-            plotter.plot.setSize(700, 250);
-            _toplevel.connect(_clock.output, plotter.input);
-            add(_createRunControls(2));
-            _query.setBackground(getBackground());
-            _query.addLine("period", "Period", "2.0");
-            _query.addQueryListener(this);
-            add(_query);
-        } catch (Exception ex) {
-            report("Error constructing model.", ex);
-        }
+    private Query _query = new Query();
+
+    public NamedObj _createModel(Workspace workspace) 
+	throws Exception {
+        TypedCompositeActor toplevel = new TypedCompositeActor(workspace);
+        _toplevel = toplevel;
+
+	// Create the director.
+	DEDirector director = new DEDirector(toplevel, "director");
+	String stopTime = getParameter("stopTime");
+	if (stopTime == null) {
+	    director.stopTime.setExpression("10.0");
+	} else {
+	    director.stopTime.setExpression(stopTime);
+	}
+
+	// Create two actors.
+	_clock = new Clock(toplevel,"clock");
+	TimedPlotter plotter = new TimedPlotter(toplevel,"plotter");
+
+	// Configure the plotter.
+	plotter.place(getContentPane());
+	plotter.plot.setBackground(getBackground());
+	plotter.plot.setTitle("clock signal");
+        plotter.plot.setXLabel("time");
+        plotter.plot.setImpulses(true);
+        plotter.plot.setConnected(false);
+        plotter.plot.setMarksStyle("dots");
+
+	// Connect them.
+	toplevel.connect(_clock.output, plotter.input);
+	return toplevel;
     }
+
+    protected void _createView() {
+	super._createView();
+	_query.setBackground(getBackground());
+	_query.addLine("period", "Period", "2.0");
+	_query.addQueryListener(this);
+	getContentPane().add(_query, BorderLayout.SOUTH);
+    }
+
     public void changed(String name) {
         _clock.period.setExpression(_query.stringValue("period"));
         try {
