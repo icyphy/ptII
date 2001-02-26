5e1cec1f7f777a34e711a851acd0fb92513a67b0
cxh <cxh>
2001-02-26 21:43:58 +0000

    Marked tests as known failures

--- ptolemy/domains/pn/test/TimedPNDirector.tcl
+++ ptolemy/domains/pn/test/TimedPNDirector.tcl
@@ -77,4 +77,4 @@ test TimedPNDirector-6.1 {Test an application} {
 #    [$e0 getManager] execute
 #    enumToTokenValues [$rec getRecord 0]
 
-} {0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19}
+} {0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19} {Running this test sometimes causes an endless loop}
