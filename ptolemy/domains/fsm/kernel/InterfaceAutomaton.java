51fd4ca31767d89d2405ccbcb62f7d3ab1bc2981
yuhong <yuhong>
2001-02-26 18:10:04 +0000

    first check in. Compiles, not tested yet.

---
+++ ptolemy/domains/fsm/kernel/InterfaceAutomaton.java
@@ -0,0 +1,225 @@
+/* An Interface Automaton.
+
+ Copyright (c) 1999-2001 The Regents of the University of California.
+ All rights reserved.
+ Permission is hereby granted, without written agreement and without
+ license or royalty fees, to use, copy, modify, and distribute this
+ software and its documentation for any purpose, provided that the above
+ copyright notice and the following two paragraphs appear in all copies
+ of this software.
+
+ IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+ FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+ ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+ THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+ SUCH DAMAGE.
+
+ THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+ INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+ PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+ CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ ENHANCEMENTS, OR MODIFICATIONS.
+
+                                        PT_COPYRIGHT_VERSION_2
+                                        COPYRIGHTENDKEY
+@ProposedRating Yellow (liuxj@eecs.berkeley.edu)
+@AcceptedRating Yellow (kienhuis@eecs.berkeley.edu)
+*/
+
+package ptolemy.domains.fsm.kernel;
+
+import ptolemy.kernel.CompositeEntity;
+// import ptolemy.kernel.Port;
+// import ptolemy.kernel.Relation;
+// import ptolemy.kernel.ComponentEntity;
+// import ptolemy.kernel.ComponentPort;
+import ptolemy.kernel.ComponentRelation;
+// import ptolemy.kernel.util.Workspace;
+// import ptolemy.kernel.util.Attribute;
+// import ptolemy.kernel.util.StringAttribute;
+// import ptolemy.kernel.util.KernelException;
+import ptolemy.kernel.util.IllegalActionException;
+import ptolemy.kernel.util.NameDuplicationException;
+// import ptolemy.kernel.util.InvalidStateException;
+// import ptolemy.kernel.util.InternalErrorException;
+import ptolemy.kernel.util.Settable;
+import ptolemy.kernel.util.Workspace;
+// import ptolemy.actor.CompositeActor;
+// import ptolemy.actor.Director;
+// import ptolemy.actor.IOPort;
+// import ptolemy.actor.IORelation;
+// import ptolemy.actor.Manager;
+// import ptolemy.actor.Receiver;
+// import ptolemy.actor.TypedActor;
+// import ptolemy.actor.Actor;
+// import ptolemy.actor.TypedIOPort;
+// import ptolemy.actor.TypedCompositeActor;
+// import ptolemy.data.Token;
+// import ptolemy.data.BooleanToken;
+// import ptolemy.data.StringToken;
+// import ptolemy.data.expr.Variable;
+// import ptolemy.data.expr.Parameter;
+// import ptolemy.data.type.Typeable;
+// import ptolemy.data.type.BaseType;
+// import ptolemy.graph.Inequality;
+
+// import java.util.Collections;
+// import java.util.Iterator;
+// import java.util.List;
+// import java.util.LinkedList;
+// import java.util.Map;
+// import java.util.HashMap;
+// import java.util.Enumeration;
+
+//////////////////////////////////////////////////////////////////////////
+//// InterfaceAutomaton
+/**
+This class models an Interface Automaton. Interface automata is an automata
+model defined by Luca de Alfaro in the paper "Interface Automata". 
+An InterfaceAutomaton contains a set of states and
+InterfaceAutomatonTransitions. There are three kinds transitions:
+input transition, output transition, and internal transitions.
+<p>
+(FIXME: Are interface automata that are fired required to be deterministic?
+or just randomly choose a transition.)
+
+When an InterfaceAutomaton is fired, the outgoing transitions of the current
+state are examined. An IllegalActionException is thrown if there is more than
+one enabled transition. If there is exactly one enabled transition then it is
+taken.
+<p>
+An InterfaceAutomaton enters its initial state during initialization. The
+name of the initial state is specified by the <i>initialStateName</i> string
+attribute.
+<p>
+An InterfaceAutomaton contains a set of variables for the input ports that
+can be referenced in the labels of input transitions.
+<p>
+
+@author Yuhong Xiong, Xiaojun Liu and Edward A. Lee
+@version $Id$
+@see State
+@see InterfaceAutomatonTransition
+*/
+public class InterfaceAutomaton extends FSMActor {
+
+    /** Construct an InterfaceAutomaton in the default workspace with an
+     *  empty string as its name. Add the actor to the workspace directory.
+     *  Increment the version number of the workspace.
+     */
+    public InterfaceAutomaton() {
+        super();
+    }
+
+    /** Construct an InterfaceAutomaton in the specified workspace with an
+     *  empty string as its name. The name can be changed later with
+     *  setName().
+     *  If the workspace argument is null, then use the default workspace.
+     *  Add the actor to the workspace directory.
+     *  Increment the version number of the workspace.
+     *  @param workspace The workspace that will list the actor.
+     */
+    public InterfaceAutomaton(Workspace workspace) {
+	super(workspace);
+    }
+
+    /** Create an InterfaceAutomaton in the specified container with the
+     *  specified name. The name must be unique within the container or an
+     *  exception is thrown. The container argument must not be null, or a
+     *  NullPointerException will be thrown.
+     *  @param container The container.
+     *  @param name The name of this automaton within the container.
+     *  @exception IllegalActionException If the entity cannot be contained
+     *   by the proposed container.
+     *  @exception NameDuplicationException If the name coincides with
+     *   an entity already in the container.
+     */
+    public InterfaceAutomaton(CompositeEntity container, String name)
+            throws IllegalActionException, NameDuplicationException {
+        super(container, name);
+    }
+
+    ///////////////////////////////////////////////////////////////////
+    ////                         public variables                  ////
+
+    ///////////////////////////////////////////////////////////////////
+    ////                         public methods                    ////
+
+    /** Return a new InterfaceAutomaton that is the composition of the
+     *  specified InterfaceAutomaton and this one.
+     *  @param automaton An InterfaceAutomaton to compose with this one.
+     *  @return An InterfaceAutomaton that is the composition.
+     */
+    public InterfaceAutomaton compose(InterfaceAutomaton automaton) {
+        // FIXME: implement this.
+        return null;
+    }
+
+    // FIXME: do we allow non-determinism? how to handle it?
+    /** Set the values of input variables. Choose the enabled transition
+     *  among the outgoing transitions of the current state. Throw an
+     *  exception if there is more than one transition enabled.
+     *  @exception IllegalActionException If there is more than one
+     *   transition enabled.
+     */
+    public void fire() throws IllegalActionException {
+        super.fire();
+    }
+
+    /** Create a new instance of InterfaceAutomatonTransition with the
+     *  specified name in this actor, and return it.
+     *  This method is write-synchronized on the workspace.
+     *  @param name The name of the new transition.
+     *  @return An InterfaceAutomatonTransition with the given name.
+     *  @exception IllegalActionException If the name argument is null.
+     *  @exception NameDuplicationException If name collides with that
+     *   of a transition already in this actor.
+     */
+    public ComponentRelation newRelation(String name)
+            throws IllegalActionException, NameDuplicationException {
+        try {
+            workspace().getWriteAccess();
+            InterfaceAutomatonTransition transition =
+	            new InterfaceAutomatonTransition(this, name);
+            return transition;
+        } finally {
+            workspace().doneWriting();
+        }
+    }
+
+    ///////////////////////////////////////////////////////////////////
+    ////                         protected methods                 ////
+
+    /** Add an InterfaceAutomatonTransition to this InterfaceAutomaton.
+     *  This method should not be used directly.  Call the setContainer()
+     *  method of the transition instead. This method does not set the
+     *  container of the transition to refer to this container. This method
+     *  is <i>not</i> synchronized on the workspace, so the caller should be.
+     *
+     *  @param relation The InterfaceAutomatonTransition to contain.
+     *  @exception IllegalActionException If the transition has no name, or
+     *   is not an instance of Transition.
+     *  @exception NameDuplicationException If the name collides with a name
+     *   already on the contained transitions list.
+     */
+    protected void _addRelation(ComponentRelation relation)
+            throws IllegalActionException, NameDuplicationException {
+        if (!(relation instanceof InterfaceAutomatonTransition)) {
+            throw new IllegalActionException(this, relation,
+                    "InterfaceAutomaton can only contain instances of "
+		    + "InterfaceAutomatonTransition.");
+        }
+        super._addRelation(relation);
+    }
+
+    ///////////////////////////////////////////////////////////////////
+    ////                         protected variables               ////
+
+    ///////////////////////////////////////////////////////////////////
+    ////                         private methods                   ////
+
+    ///////////////////////////////////////////////////////////////////
+    ////                         private variables                 ////
+
+}
---
+++ ptolemy/domains/fsm/kernel/InterfaceAutomatonTransition.java
@@ -0,0 +1,229 @@
+/* A transition in an Interface Automaton.
+
+ Copyright (c) 1999-2001 The Regents of the University of California.
+ All rights reserved.
+ Permission is hereby granted, without written agreement and without
+ license or royalty fees, to use, copy, modify, and distribute this
+ software and its documentation for any purpose, provided that the above
+ copyright notice and the following two paragraphs appear in all copies
+ of this software.
+
+ IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+ FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+ ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+ THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+ SUCH DAMAGE.
+
+ THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+ INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+ PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+ CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ ENHANCEMENTS, OR MODIFICATIONS.
+
+                                        PT_COPYRIGHT_VERSION_2
+                                        COPYRIGHTENDKEY
+@ProposedRating Red (yuhong@eecs.berkeley.edu)
+@AcceptedRating Red (reviewmoderator@eecs.berkeley.edu)
+*/
+
+package ptolemy.domains.fsm.kernel;
+
+// import ptolemy.kernel.Port;
+import ptolemy.kernel.CompositeEntity;
+// import ptolemy.kernel.ComponentRelation;
+// import ptolemy.kernel.util.Workspace;
+import ptolemy.kernel.util.Attribute;
+import ptolemy.kernel.util.StringAttribute;
+// import ptolemy.kernel.util.NamedObj;
+import ptolemy.kernel.util.IllegalActionException;
+import ptolemy.kernel.util.NameDuplicationException;
+import ptolemy.kernel.util.InternalErrorException;
+import ptolemy.kernel.util.Settable;
+// import ptolemy.data.Token;
+// import ptolemy.data.BooleanToken;
+// import ptolemy.data.StringToken;
+// import ptolemy.data.expr.Variable;
+// import ptolemy.data.expr.Parameter;
+// import ptolemy.data.type.BaseType;
+
+// import java.util.List;
+// import java.util.Iterator;
+// import java.util.LinkedList;
+
+//////////////////////////////////////////////////////////////////////////
+//// InterfaceAutomatonTransition
+/**
+A Transition for Interface Automaton. In the Interface Automata paper
+writen by Luca de Alfaro, transitions are called actions. However,
+the name action conflicts with the Action class in this package, so this
+class uses the name transition.  There are three types of transitions:
+input, output, and internal. Each transition has a label. The labels for
+input, output, and internal transitions end with "?", "!", and ";",
+respectively. The type of transition is determined by the ending character
+of the label this way. Each input transition corresponds to an input port of
+the InterfaceAutomaton that contains it, and its label, excluding the
+ending "?", must be the same as the input port name; Similarly, each output
+transition corresponds to an output port, and its label, excluding the
+ending "!", must be the same as the output port name;  Each internal
+transitions corresponds to a parameter of the InterfaceAutomaton that
+contains it, and its label, excluding the ending ";", must be the same as
+the parameter name.
+<p>
+The guard of each transition is set automatically. Users should not set
+the guard. For an input transition, the guard is set to <inputPort>_isPresent,
+where <inputPort> is the port corresponding to this transition; For ouput
+and internal transitions, the guard is set to true. Each of the output
+and internal transitions contain an Action. The expression of the Action
+is also set automatically. For output transition, the action is set to
+<outputPort>=true, where <outputPort> is the output port corresponding to
+this transition; for internal transition, the action is set to
+<parameter>=true, where <parameter> is the parameter corresponding to this
+transition.
+
+@author Yuhong Xiong, Xiaojun Liu and Edward A. Lee
+@version $Id$
+@see InterfaceAutomaton
+@see Action
+*/
+
+// FIXME: parameters corresponding to internal transitions are not created
+// yet.
+public class InterfaceAutomatonTransition extends Transition {
+
+    /** Construct a transition with the specified container and name.
+     *  The container argument must not be null, or a NullPointerException
+     *  will be thrown. This transition will use the workspace of the
+     *  container for synchronization and version counts.
+     *  If the name argument is null, then the name is set to the empty string.
+     *  @param container An InterfaceAutomaton.
+     *  @param name The name of this transition.
+     *  @exception IllegalActionException If the container is incompatible
+     *   with this transition.
+     *  @exception NameDuplicationException If the name coincides with
+     *   any relation already in the container.
+     */
+    public InterfaceAutomatonTransition(InterfaceAutomaton container,
+                                        String name)
+	    throws IllegalActionException, NameDuplicationException {
+
+        super(container, name);
+	guardExpression.setVisibility(Settable.NONE);
+	triggerExpression.setVisibility(Settable.NONE);
+    }
+
+    ///////////////////////////////////////////////////////////////////
+    ////                         public variables                  ////
+
+    /** The label of this transition. Must ends with "?" or "!" or ";"
+     */
+    public StringAttribute label = null;
+
+    ///////////////////////////////////////////////////////////////////
+    ////                         public methods                    ////
+
+    /** React to a change in an attribute. If the changed attribute is
+     *  <i>label</i>, check the ending character of the label to
+     *  determine the transition type. Increment the version number of
+     *  the workspace.
+     *  @param attribute The attribute that changed.
+     *  @exception IllegalActionException If thrown by the superclass
+     *   attributeChanged() method, or the changed attribute is 
+     *   <i>label</i> and it does not ends with "?" or "!" or ";".
+     */
+    public void attributeChanged(Attribute attribute)
+            throws IllegalActionException {
+        super.attributeChanged(attribute);
+        if (attribute == label) {
+	    String labelString = label.getExpression();
+	    String name = labelString.substring(0, labelString.length()-1);
+            if (labelString.endsWith("?")) {
+	        setGuardExpression(name + "_isPresent");
+		outputActions.setExpression("");
+	    } else if (labelString.endsWith("!")) {
+	        setGuardExpression("true");
+		outputActions.setExpression(name + "=true");
+	    } else if (labelString.endsWith(";")) {
+	        // FIXME: create(?) set parameter, set action to <param>=true
+		setGuardExpression("true");
+	    } else {
+	        throw new IllegalArgumentException(
+	                "InterfaceAutomatonTransition.setLabel: The argument "
+		        + label + " does not end with ? or ! or ;");
+	    }
+        }
+    }
+
+    /** Return the transition type. The transition type is one of
+     *  INPUT_TRANSITION, OUTPUT_TRANSITION, or INTERNAL_TRANSITION.
+     *  @return the transition type.
+     */
+    public int getTransitionType() {
+        String labelString = label.getExpression();
+        if (labelString.endsWith("?")) {
+	    return INPUT_TRANSITION;
+	} else if (labelString.endsWith("!")) {
+	    return OUTPUT_TRANSITION;
+	} else if (labelString.endsWith(";")) {
+	    return INTERNAL_TRANSITION;
+	} else {
+	    throw new InternalErrorException(
+	            "InterfaceAutomatonTransition.getTransitionType: "
+		    + "The label does not end with ? or ! or ;");
+        }
+    }
+
+    /** Override the base class to ensure that the proposed container
+     *  is an instance of InterfaceAutomaton or null. If it is, call the
+     *  base class setContainer() method. A null argument will remove
+     *  this transition from its container.
+     *
+     *  @param container The proposed container.
+     *  @exception IllegalActionException If setting the container would
+     *   result in a recursive containment structure, or if
+     *   this transition and container are not in the same workspace, or
+     *   if the argument is not an InterfaceAutomaton or null.
+     *  @exception NameDuplicationException If the container already has
+     *   an relation with the name of this transition.
+     */
+    public void setContainer(CompositeEntity container)
+            throws IllegalActionException, NameDuplicationException {
+        if (!(container instanceof InterfaceAutomaton) &&
+                (container != null)) {
+            throw new IllegalActionException(container, this,
+                    "Transition can only be contained by instances of "
+		    + "InterfaceAutomaton.");
+        }
+        super.setContainer(container);
+    }
+
+    /** Throw an exception. Trigger expression is not used in
+     *  InterfaceAutomaton, so this method should not be called in this class.
+     *  @param expression The trigger expression.
+     *  @exception UnsupportedOperationException Always thrown.
+     */
+    public void setTriggerExpression(String expression) {
+        throw new UnsupportedOperationException(
+	        "InterfaceAutomatonTransition.setTriggerExpression: "
+	        + "The trigger expression is not used in InterfaceAutomaton, "
+		+ "so this method should not be called.");
+    }
+
+    ///////////////////////////////////////////////////////////////////
+    ////                       protected variables                 ////
+
+    // transition types.
+    protected static final int INPUT_TRANSITION = 0;
+    protected static final int OUTPUT_TRANSITION = 1;
+    protected static final int INTERNAL_TRANSITION = 2;
+
+    ///////////////////////////////////////////////////////////////////
+    ////                         protected methods                 ////
+
+    ///////////////////////////////////////////////////////////////////
+    ////                         private methods                   ////
+
+    ///////////////////////////////////////////////////////////////////
+    ////                         private variables                 ////
+
+}
