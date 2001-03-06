dd09700fb598846822ecf90024c646650b0e41a4
cxh <cxh>
2001-03-06 01:15:23 +0000

    doubele -> double

--- ptolemy/domains/ct/lib/CTRateLimiter.java
+++ ptolemy/domains/ct/lib/CTRateLimiter.java
@@ -104,7 +104,7 @@ public class CTRateLimiter extends Transformer {
     ///////////////////////////////////////////////////////////////////
     ////                     ports and parameters                  ////
     /** The rising slew rate.
-     *  The parameter has type doubele and default value 1.0.
+     *  The parameter has type double and default value 1.0.
      */
     public Parameter risingSlewRate;
 
