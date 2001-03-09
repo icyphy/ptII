957c2d6110b70911e5bf27e3ae5e704d4dc67eed
cxh <cxh>
2001-03-09 18:03:15 +0000

    Better backward time message

--- ptolemy/actor/Director.java
+++ ptolemy/actor/Director.java
@@ -475,8 +475,8 @@ public class Director extends Attribute implements Executable {
      *  methods. This method is called by CompositeActor when an actor
      *  sets its container to that composite actor.  Typically, that
      *  will occur when a model is first constructed, and during the
-     *  execute() method of a ChangeRequest that is queued using
-     *  requestChange().  In this base class, the request is delegated
+     *  execute() method of a ChangeRequest.
+     *  In this base class, the request is delegated
      *  to the manager. If there is no manager, or if the container
      *  is not an instance of CompositeActor, then do nothing.
      *  @param actor The actor to initialize.
@@ -601,8 +601,9 @@ public class Director extends Attribute implements Executable {
      */
     public void setCurrentTime(double newTime) throws IllegalActionException {
         if (newTime < getCurrentTime()) {
-            throw new IllegalActionException(this,
-                    "Attempt to move current time backwards.");
+            throw new IllegalActionException(this, "Attempt to move current "
+                    + "time backwards. (newTime = " + newTime
+                    + ") < (getCurrentTime() = " + getCurrentTime() + ")");
         }
         _currentTime = newTime;
     }
