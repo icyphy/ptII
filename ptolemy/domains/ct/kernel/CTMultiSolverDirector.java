14a8e31522704129d77c67ca0c152dcb2fd085f9
liuj <liuj>
2001-03-09 21:50:52 +0000

    test for minimum step sizes even in variable step size methods. This solves the devide by zero problem

--- ptolemy/domains/ct/kernel/CTMultiSolverDirector.java
+++ ptolemy/domains/ct/kernel/CTMultiSolverDirector.java
@@ -645,6 +645,12 @@ public class CTMultiSolverDirector extends CTDirector {
                     + size);
             refinedStep = Math.min(refinedStep, size);
         }
+        if(refinedStep < 0.5*getMinStepSize()) {
+            throw new IllegalActionException(this,
+                    "Cannot maintain the accuracy even using "+
+                    "the minimum step size, at time "+
+                    getCurrentTime());
+        }
         return refinedStep;
     }
 
@@ -665,6 +671,12 @@ public class CTMultiSolverDirector extends CTDirector {
             CTStepSizeControlActor actor =
                 (CTStepSizeControlActor)actors.next();
             refinedStep = Math.min(refinedStep, actor.refinedStepSize());
+        }
+        if(refinedStep < 0.5*getMinStepSize()) {
+            throw new IllegalActionException(this,
+                    "Cannot maintain the accuracy even using "+
+                    "the minimum step size, at time "+
+                    getCurrentTime());
         }
         return refinedStep;
     }
