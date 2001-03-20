04b15f135df0f4b0725e4ea6630a894d5b153e8c
johane <johane>
2001-03-20 01:33:03 +0000

    make ZeroOrderHold a DynamicActor

--- ptolemy/domains/ct/lib/ZeroOrderHold.java
+++ ptolemy/domains/ct/lib/ZeroOrderHold.java
@@ -52,7 +52,7 @@ event until the next discrete event arrives.
 */
 
 public class ZeroOrderHold extends Transformer
-    implements CTWaveformGenerator{
+    implements CTWaveformGenerator, CTDynamicActor {
 
     /** Construct an actor in the specified container with the specified
      *  name.  The name must be unique within the container or an exception
@@ -115,6 +115,13 @@ public class ZeroOrderHold extends Transformer
                     " with token " + _lastToken.toString());
         }
     }
+
+    /** FIXME: This may not be the right way to use Waveform Generators.
+     *  This implements the emitTentativeOutput.
+     */
+  public void emitTentativeOutputs() throws IllegalActionException {
+    output.send(0, _lastToken);
+ }
 
     /** Output the latest token consumed from the consumeCurrentEvents()
      *  call.
