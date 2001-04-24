b1803556c295f184ccde0ab43dc2317044fedf2d
liuj <liuj>
2001-04-24 17:23:59 +0000

    add comments

--- ptolemy/domains/rtos/kernel/RTOSReceiver.java
+++ ptolemy/domains/rtos/kernel/RTOSReceiver.java
@@ -39,11 +39,44 @@ import ptolemy.data.expr.Parameter;
 //////////////////////////////////////////////////////////////////////////
 //// RTOSReceiver
 /**
-@author Edward A. Lee
+The receiver for the RTOS domain. This receiver extends the DEReciever.
+However, instead of attaching a time stamp to the received token before
+sending it to the event queue, it attaches the priority of the token.
+The priority of a received token is determined in the following order:
+<UL>
+<li> If the container of the receiver has a parameter named <i>priority</i>
+then the priority equals to the value of the parameter.
+<li> If the container of the receiver does not have a <i>priority</i>
+parameter, but the actor, which contains the container of this receiver,
+has a <i>priority</i> parameter, then the priority equals to the value
+of the <i>priority<i> of the actor.
+<li> If neither the container nor the container of the container of this
+reciever has the <i>priority</i> parameter, then the priority of the
+token is the default priority, which is 5.0.
+
+@author Edward A. Lee, Jie Liu
 @version $Id$
 */
 public class RTOSReceiver extends DEReceiver {
 
+
+    ////////////////////////////////////////////////////////////////////////
+    ////                     public methods                             ////
+
+    /** Put a token into this receiver. Note that
+     *  this token does not become immediately available to the get() method.
+     *  Instead, the token is queued with the director, and the director
+     *  must put the token back into this receiver using the _triggerEvent()
+     *  protected method in order for the token to become available to
+     *  the get() method.  By default, this token will be enqueued by
+     *  the director with the default priority -- 5.0.
+     *  However, by setting a <i>priority</i> parameter to the container
+     *  of this receiver, or the container's container, 
+     *  you can enqueue the event with any priority.
+     *  This method is synchronized since the actor may not
+     *  execute in the same thread as the director.
+     *  @param token The token to be put.
+     */
     public synchronized void put(Token token) {
         try {
             RTOSDirector dir = (RTOSDirector)getDirector();
@@ -52,7 +85,7 @@ public class RTOSReceiver extends DEReceiver {
             if (priority == null) {
                 priority = (Parameter)((NamedObj)port.getContainer()).getAttribute("priority");
             }
-            double priorityValue = 10.0;
+            double priorityValue = 5.0;
             if (priority != null) {
                 priorityValue = ((DoubleToken)priority.getToken()).doubleValue();
             }
@@ -62,6 +95,15 @@ public class RTOSReceiver extends DEReceiver {
         }
     }
 
+    /** Override the put(token, time) method of the super class, and ignore
+     *  the time argument. Only the priority of a token is of interests,
+     *  and the priority is obtained from parameters of the container
+     *  or container's container.
+     *  This method is synchronized since the actor may not
+     *  execute in the same thread as the director.
+     *  @param token The token to be put.
+     *  @param time Ignored.
+     */
     public synchronized void put(Token token, double time)
             throws IllegalActionException{
        put(token);
