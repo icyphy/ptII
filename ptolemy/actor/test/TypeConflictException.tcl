e67fe36a1008749fe8e070bd5334c4651a2fd592
yuhong <yuhong>
2001-05-08 02:25:58 +0000

    accept inequalities instead of Typeables.

--- ptolemy/actor/test/TypeConflictException.tcl
+++ ptolemy/actor/test/TypeConflictException.tcl
@@ -54,26 +54,30 @@ test TypeConflictException-1.0 {Constructor that takes a List} {
     $e0 setManager $manager
     set e1 [java::new ptolemy.actor.TypedAtomicActor $e0 E1]
     set p1 [java::new ptolemy.actor.TypedIOPort $e1 P1]
+    set t1 [$p1 getTypeTerm]
     set p2 [java::new ptolemy.actor.TypedIOPort $e1 P2]
+    set t2 [$p2 getTypeTerm]
     set tDouble [[java::new ptolemy.data.DoubleToken] getType]
     $p1 setTypeEquals $tDouble
 
+    set ineq1 [java::new ptolemy.graph.Inequality $t1 $t2]
+    set ineq2 [java::new ptolemy.graph.Inequality $t2 $t1]
+
     set conflicts [java::new java.util.LinkedList]
-    $conflicts add $p1
-    $conflicts add $p2
+    $conflicts add $ineq1
+    $conflicts add $ineq2
 
     set ex1 [java::new ptolemy.actor.TypeConflictException $conflicts]
     set ex2 [java::new ptolemy.actor.TypeConflictException $conflicts \
 	    "Detail Message"]
-    list [$ex1 getMessage] [$ex2 getMessage] \
-	    [listToFullNames [$ex1 objectList]]
-} {{Type conflicts occurred at the following places:
-  ..E1.P1: double
-  ..E1.P2: unknown
+    list [$ex1 getMessage] [$ex2 getMessage]
+} {{Type conflicts occurred at the following inequalities:
+  (ptolemy.actor.TypedIOPort {..E1.P1}, double) <= (ptolemy.actor.TypedIOPort {..E1.P2}, unknown)
+  (ptolemy.actor.TypedIOPort {..E1.P2}, unknown) <= (ptolemy.actor.TypedIOPort {..E1.P1}, double)
 } {Detail Message
-  ..E1.P1: double
-  ..E1.P2: unknown
-} {..E1.P1 ..E1.P2}}
+  (ptolemy.actor.TypedIOPort {..E1.P1}, double) <= (ptolemy.actor.TypedIOPort {..E1.P2}, unknown)
+  (ptolemy.actor.TypedIOPort {..E1.P2}, unknown) <= (ptolemy.actor.TypedIOPort {..E1.P1}, double)
+}}
 
 ######################################################################
 ####
@@ -81,6 +85,8 @@ test TypeConflictException-1.0 {Constructor that takes a List} {
 test TypeConflictException-1.1 {Test with structured types} {
     set port [java::new ptolemy.actor.TypedIOPort $e1 port]
     set param [java::new ptolemy.data.expr.Parameter $e1 param]
+    set port2 [java::new ptolemy.actor.TypedIOPort $e1 port2]
+    set param2 [java::new ptolemy.data.expr.Parameter $e1 param2]
 
     # an ArrayType
     set unknown [java::field ptolemy.data.type.BaseType UNKNOWN]
@@ -95,18 +101,38 @@ test TypeConflictException-1.1 {Test with structured types} {
 
     set recordT [java::new {ptolemy.data.type.RecordType} $l $v]
 
+    # set port2 to have array type and param2 to have record type
+    $port2 setTypeEquals $arrayT
+    $param2 setTypeEquals $recordT
+
+    set portTerm [$port getTypeTerm]
+    set paramTerm [$param getTypeTerm]
+    set port2Term [$port2 getTypeTerm]
+    set param2Term [$param2 getTypeTerm]
+
+    set ineq1 [java::new ptolemy.graph.Inequality $portTerm $paramTerm]
+    set ineq2 [java::new ptolemy.graph.Inequality $portTerm $port2Term]
+    set ineq3 [java::new ptolemy.graph.Inequality $portTerm $param2Term]
+    set ineq4 [java::new ptolemy.graph.Inequality $port2Term $paramTerm]
+    set ineq5 [java::new ptolemy.graph.Inequality $param2Term $paramTerm]
+    set ineq6 [java::new ptolemy.graph.Inequality $port2Term $param2Term]
+
     # construct the exception
     set conflicts [java::new java.util.LinkedList]
-    $conflicts add $port
-    $conflicts add $param
-    $conflicts add $arrayT
-    $conflicts add $recordT
+    $conflicts add $ineq1
+    $conflicts add $ineq2
+    $conflicts add $ineq3
+    $conflicts add $ineq4
+    $conflicts add $ineq5
+    $conflicts add $ineq6
 
     set ex [java::new ptolemy.actor.TypeConflictException $conflicts]
-    list [$ex getMessage] [listToFullNames [$ex objectList]]
-} {{Type conflicts occurred at the following places:
-  ..E1.port: unknown
-  ..E1.param: unknown
-  {unknown}
-  {name=string, value=unknown}
-} {..E1.port ..E1.param NOT_NAMEABLE. NOT_NAMEABLE.}}
+    list [$ex getMessage]
+} {{Type conflicts occurred at the following inequalities:
+  (ptolemy.actor.TypedIOPort {..E1.port}, unknown) <= (ptolemy.data.expr.Parameter {..E1.param} value undefined, unknown)
+  (ptolemy.actor.TypedIOPort {..E1.port}, unknown) <= (ptolemy.actor.TypedIOPort {..E1.port2}, {unknown})
+  (ptolemy.actor.TypedIOPort {..E1.port}, unknown) <= (ptolemy.data.expr.Parameter {..E1.param2} value undefined, {name=string, value=unknown})
+  (ptolemy.actor.TypedIOPort {..E1.port2}, {unknown}) <= (ptolemy.data.expr.Parameter {..E1.param} value undefined, unknown)
+  (ptolemy.data.expr.Parameter {..E1.param2} value undefined, {name=string, value=unknown}) <= (ptolemy.data.expr.Parameter {..E1.param} value undefined, unknown)
+  (ptolemy.actor.TypedIOPort {..E1.port2}, {unknown}) <= (ptolemy.data.expr.Parameter {..E1.param2} value undefined, {name=string, value=unknown})
+}}
