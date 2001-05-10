a2734337bb23c8a300ec96ea15e20bf634808368
cxh <cxh>
2001-05-10 23:16:16 +0000

    set debug to false

--- ptolemy/lang/java/extended/ExtendedJavaTypePolicy.java
+++ ptolemy/lang/java/extended/ExtendedJavaTypePolicy.java
@@ -140,5 +140,5 @@ public class ExtendedJavaTypePolicy extends TypePolicy {
     ///////////////////////////////////////////////////////////////////
     ////                         private variables                 ////
 
-    private final static boolean _debug = true;
+    private final static boolean _debug = false;
 }
