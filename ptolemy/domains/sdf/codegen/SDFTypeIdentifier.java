9d8589308aba99ba41e32792835c06f05928a7bf
shuvra <shuvra>
2001-05-15 21:54:12 +0000

    Modified to take into account a bug fix with the storage of cached ASTs in StaticResolution.java.

--- ptolemy/domains/sdf/codegen/SDFTypeIdentifier.java
+++ ptolemy/domains/sdf/codegen/SDFTypeIdentifier.java
@@ -117,14 +117,16 @@ public class SDFTypeIdentifier extends PtolemyTypeIdentifier {
         try {
             CompileUnitNode sdfAtomicActorUnit = StaticResolution.loadFile(
                     SearchPath.NAMED_PATH.openSource(
-                            "ptolemy.actor.TypedAtomicActor"), 1);
+                            "ptolemy.actor.TypedAtomicActor"), 1,
+                            "ptolemy.actor.TypedAtomicActor");
             SDF_ATOMIC_ACTOR_DECL = (ClassDecl) StaticResolution.findDecl(
                     sdfAtomicActorUnit, "TypedAtomicActor", CG_CLASS);
             SDF_ATOMIC_ACTOR_TYPE = SDF_ATOMIC_ACTOR_DECL.getDefType();
 
             CompileUnitNode sdfIOPortUnit = StaticResolution.loadFile(
                     SearchPath.NAMED_PATH.openSource(
-                            "ptolemy.domains.sdf.kernel.SDFIOPort"), 1);
+                            "ptolemy.domains.sdf.kernel.SDFIOPort"), 1,
+                            "ptolemy.domains.sdf.kernel.SDFIOPort");
             SDF_IO_PORT_DECL = (ClassDecl) StaticResolution.findDecl(
                     sdfIOPortUnit,  "SDFIOPort", CG_CLASS);
             SDF_IO_PORT_TYPE = SDF_IO_PORT_DECL.getDefType();
