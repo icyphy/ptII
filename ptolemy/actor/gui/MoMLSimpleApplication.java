fe052ff304ac84ab0758241bfaddd95d00f15689
cxh <cxh>
2001-05-16 23:07:47 +0000

    Added a simple non-graphical MoMLApplication class

---
+++ ptolemy/actor/gui/MoMLSimpleApplication.java
@@ -0,0 +1,68 @@
+/* An application that executes non-graphical 
+   models specified on the command line.
+
+ Copyright (c) 2001 The Regents of the University of California.
+ All rights reserved.
+ Permission is hereby granted, without written agreement and without
+ license or royalty fees, to use, copy, modify, and distribute this
+ software and its documentation for any purpose, provided that the above
+ copyright notice and the following two paragraphs appear in all copies
+ of this software.
+
+ IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+ FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+ ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+ THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+ SUCH DAMAGE.
+
+ THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+ INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+ PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+ CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ ENHANCEMENTS, OR MODIFICATIONS.
+
+                                        PT_COPYRIGHT_VERSION_2
+                                        COPYRIGHTENDKEY
+
+@ProposedRating Yellow (eal@eecs.berkeley.edu)
+@AcceptedRating Red (eal@eecs.berkeley.edu)
+*/
+
+package ptolemy.actor.gui;
+
+// Ptolemy imports
+import ptolemy.actor.CompositeActor;
+import ptolemy.actor.Manager;
+import ptolemy.kernel.util.Workspace;
+import ptolemy.moml.MoMLParser;
+
+//////////////////////////////////////////////////////////////////////////
+//// MoMLSimpleApplication
+/** A simple application that reads in a .xml file as a command
+line argument and runs it.
+
+@author Christopher Hylands
+@version $Id$
+*/
+public class MoMLSimpleApplication {
+     ///////////////////////////////////////////////////////////////////
+    ////                         public methods                    ////
+
+    /** Create an instance of a single model and run it
+     *  @param args The command-line arguments naming the .xml file to run
+     */
+    public static void main(String args[]) {
+	try {
+            MoMLParser parser = new MoMLParser();
+            CompositeActor toplevel =
+                (CompositeActor) parser.parseFile(args[0]);
+            Manager manager = new Manager(toplevel.workspace(),
+                    "MoMLSimpleApplication");
+            toplevel.setManager(manager);
+            manager.execute();
+        } catch (Exception ex) {
+            System.err.println("Command failed: " + ex);
+        }
+    }
+}
