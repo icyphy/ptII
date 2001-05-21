efaaa427dc6a07da102130597f3d391ee40d8cc4
cxh <cxh>
2001-05-21 23:58:48 +0000

    Print the systemClassSet size

--- ptolemy/lang/java/test/SearchPath.tcl
+++ ptolemy/lang/java/test/SearchPath.tcl
@@ -153,6 +153,7 @@ test SearchPath-3.2 {Do we need to resize the systemClassSet HashSet?} {
     # and the number of class files in JavaScope.zip was 385
     # If the number gets too large then modify the initial size 
     # of the hashset.
+    puts "SearchPatch-3.2: systemClassSet size: [$systemClassSet size]"
     list [expr {[$systemClassSet size] > 5700}]
 } {0}
 
