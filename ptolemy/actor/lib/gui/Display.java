a8685d85d823bfe372444b7563431ecc499729d2
liuj <liuj>
2001-05-22 04:56:31 +0000

    make it not a SequenceActor so that CT can use it for debugging

--- ptolemy/actor/lib/gui/Display.java
+++ ptolemy/actor/lib/gui/Display.java
@@ -70,7 +70,7 @@ converge to a fixed point.
 @author  Yuhong Xiong, Edward A. Lee
 @version $Id$
  */
-public class Display extends Sink implements Placeable, SequenceActor {
+public class Display extends Sink implements Placeable {
 
     /** Construct an actor with an input multiport of type GENERAL.
      *  @param container The container.
