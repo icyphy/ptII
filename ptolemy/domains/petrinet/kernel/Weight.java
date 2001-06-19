3ec1b73814da4547112023df7de4c319ad38e7ee
yukewang <yukewang>
2001-06-19 00:43:42 +0000

    Added Weight.java

---
+++ ptolemy/domains/petrinet/kernel/Weight.java
@@ -0,0 +1,122 @@
+/* A Petri net Weight.
+
+ Copyright (c) 1999 The Regents of the University of California.
+ All rights reserved.
+ Permission is hereby granted, without written agreement and without
+ license or royalty fees, to use, copy, modify, and distribute this
+ software and its documentation for any purpose, provided that the above
+ copyright notice and the following two paragraphs appear in all copies
+ of this software.
+
+ IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+ FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+ ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+ THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+ SUCH DAMAGE.
+
+ THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+ INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+ PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+ CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ ENHANCEMENTS, OR MODIFICATIONS.
+ 
+                                        PT_COPYRIGHT_VERSION_2
+                                        COPYRIGHTENDKEY
+@ProposedRating Red (yourname@eecs.berkeley.edu)
+@AcceptedRating Red (reviewmoderator@eecs.berkeley.edu)
+*/
+
+package ptolemy.domains.petrinet.kernel;
+
+import ptolemy.actor.lib.Transformer;
+import ptolemy.data.IntToken;
+import ptolemy.data.Token;
+import ptolemy.data.expr.Parameter;
+import ptolemy.data.type.BaseType;
+import ptolemy.kernel.CompositeEntity;
+import ptolemy.kernel.util.IllegalActionException;
+import ptolemy.kernel.util.NameDuplicationException;
+
+
+//////////////////////////////////////////////////////////////////////////
+//// Weight
+/**
+A Petri net arc Weight
+
+@author  Yuke Wang and Edward A. Lee
+@version $Id$
+*/
+public class Weight extends Transformer {
+
+    /** Create a new actor in the specified container with the specified
+     *  name.  The name must be unique within the container or an exception
+     *  is thrown. The container argument must not be null, or a
+     *  NullPointerException will be thrown.
+     *
+     *  @param container The container.
+     *  @param name The name of this actor within the container.
+     *  @exception IllegalActionException If this actor cannot be contained
+     *   by the proposed container (see the setContainer() method).
+     *  @exception NameDuplicationException If the name coincides with
+     *   an entity already in the container.
+     */
+    public Weight(CompositeEntity container, String name)
+            throws IllegalActionException, NameDuplicationException {
+        super(container, name);
+        input.setMultiport(false);
+        input.setTypeEquals(BaseType.GENERAL);
+        output.setMultiport(false);
+        output.setTypeEquals(BaseType.GENERAL);
+        
+        initialWeight= new Parameter(this, "initialWeight");
+        initialWeight.setExpression("1");
+        
+    }
+
+    ///////////////////////////////////////////////////////////////////
+    ////                         public varialbes                    //
+
+    public Parameter initialWeight;
+
+    
+
+    ///////////////////////////////////////////////////////////////////
+    ////                         public methods                    ////
+   
+
+    public void initialize() throws IllegalActionException {
+        _currentWeight = ((IntToken)initialWeight.getToken()).intValue();
+    }
+
+    public int getWeight() {
+        return _currentWeight;
+    }
+
+  
+    public void printWeight() {
+
+        
+        System.out.println("The weight here is " + _currentWeight);
+
+    }
+
+   
+    ///////////////////////////////////////////////////////////////////
+    ////                         protected methods                 ////
+
+
+    ///////////////////////////////////////////////////////////////////
+    ////                         protected variables               ////
+
+
+    ///////////////////////////////////////////////////////////////////
+    ////                         private methods                   ////
+
+    ///////////////////////////////////////////////////////////////////
+    ////                         private variables                 ////
+
+    
+
+    private int _currentWeight = 1;
+}
