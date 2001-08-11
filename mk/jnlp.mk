8d06b3534bb62ce13dec2529aac6f0df64388045
cxh <cxh>
2001-08-11 00:11:22 +0000

    Further tuning

--- mk/jnlp.mk
+++ mk/jnlp.mk
@@ -42,27 +42,26 @@ DOC_CODEDOC_JAR =
 
 # Jar files that will appear in all Ptolemy II JNLP files.
 CORE_JNLP_JARS = \
-	ptolemy/vergil/vergil.jar \
 	doc/docConfig.jar \
 	lib/diva.jar \
+	ptolemy/domains/domains.jar \
 	ptolemy/domains/sdf/demo/demo.jar \
+	ptolemy/ptsupport.jar \
+	ptolemy/vergil/vergil.jar \
 	$(DOC_CODEDOC_JAR)
 
 #######
-# Full
+# DSP - The smallest runtime
 #
-# Jar files that will appear in a full JNLP Ptolemy II Runtime
-FULL_ONLY_JNLP_JARS = \
-	ptolemy/domains/experimentalDomains.jar \
-	ptolemy/ptolemy.jar
+# Jar files that will appear in a DSP only JNLP Ptolemy II Runtime.
+DSP_ONLY_JNLP_JARS =
 
-FULL_MAIN_JAR = \
-	ptolemy/vergil/vergil.jar
+DSP_MAIN_JAR = \
+	ptolemy/actor/gui/jnlp/DSPApplication.jar
 
-FULL_JNLP_JARS = \
-	$(FULL_MAIN_JAR) \
-	$(CORE_JNLP_JARS) \
-	$(FULL_ONLY_JNLP_JARS)
+DSP_JNLP_JARS =	\
+	$(DSP_MAIN_JAR) \
+	$(CORE_JNLP_JARS)
 
 
 #######
@@ -70,31 +69,47 @@ FULL_JNLP_JARS = \
 #
 # Jar files that will appear in a smaller (Ptiny) JNLP Ptolemy II Runtime.
 PTINY_ONLY_JNLP_JARS = \
+	ptolemy/actor/lib/javasound/javasound.xml \
 	ptolemy/actor/lib/javasound/demo/demo.jar \
 	ptolemy/data/type/demo/demo.jar \
-	ptolemy/domains/domains.jar \
 	ptolemy/domains/ct/demo/demo.jar \
 	ptolemy/domains/de/demo/demo.jar \
-	ptolemy/moml/demo/demo.jar \
+	ptolemy/moml/demo/demo.jar
 
 PTINY_MAIN_JAR = \
-	ptolemy/ptsupport.jar
+	ptolemy/actor/gui/jnlp/PtinyApplication.jar
 
 PTINY_JNLP_JARS = \
 	$(PTINY_MAIN_JAR) \
 	$(CORE_JNLP_JARS) \
+	$(DSP_ONLY_JNLP_JARS) \
 	$(PTINY_ONLY_JNLP_JARS)
 
-
-
 #######
-# DSP
+# Full
 #
-# Jar files that will appear in a DSP only JNLP Ptolemy II Runtime.
-DSP_ONLY_JNLP_JARS =
-DSP_JNLP_JARS =		$(PTINY_JNLP_JARS) 
-DSP_MAIN_JAR = 		$(PTINY_MAIN_JAR)
+# Jar files that will appear in a full JNLP Ptolemy II Runtime
+FULL_ONLY_JNLP_JARS = \
+	ptolemy/domains/experimentalDomains.jar \
+	ptolemy/codegen/codegen.jar \
+	ptolemy/copernicus/copernicus.jar \
+	ptolemy/domains/dt/demo/demo.jar \
+	ptolemy/domains/giotto/demo/demo.jar \
+	ptolemy/domains/fsm/demo/demo.jar \
+	ptolemy/domains/pn/demo/demo.jar \
+	ptolemy/domains/rtos/demo/demo.jar
 
+FULL_MAIN_JAR = \
+	ptolemy/actor/gui/jnlp/FullApplication.jar
+
+FULL_JNLP_JARS = \
+	$(FULL_MAIN_JAR) \
+	$(CORE_JNLP_JARS) \
+	$(DSP_ONLY_JNLP_JARS) \
+	$(PTINY_ONLY_JNLP_JARS) \
+	$(FULL_ONLY_JNLP_JARS)
+
+#########
 
 # All the JNLP Jar files, hopefully without duplicates so that 
 # we don't sign jars twice.
@@ -107,34 +122,42 @@ ALL_JNLP_JARS = \
 # Script to update a *.jnlp file with the proper jar files
 MKJNLP =		$(PTII)/bin/mkjnlp
 
-JNLPS =	vergil.jnlp vergilPtiny.jnlp #vergilDSP.jnlp 
+
+# JNLP files that do the actual installation
+JNLPS =	vergilDSP.jnlp vergilPtiny.jnlp  vergil.jnlp 
 
 jnlp_all: $(JNLPS) jnlp_sign
 jnlps: $(JNLPS)
 jnlp_clean: 
 	rm -f $(JNLPS)
 
-# Web Start: Full Runtime version of Vergil - No sources or build env.
-vergil.jnlp: vergil.jnlp.in
-	sed 	-e 's%@PTII_LOCALURL@%$(PTII_LOCALURL)%' \
-		-e 's%@PTVERSION@%$(PTVERSION)%' \
-			$< > $@
-	@echo "# Adding jar files to $@"
-	"$(MKJNLP)" $@ \
-		$(FULL_MAIN_JAR) \
-		$(FULL_JNLP_JARS)
-	@echo "# Updating JNLP-INF/APPLICATION.JNLP with $@"
-	rm -rf JNLP-INF
-	mkdir JNLP-INF
-	cp $@ JNLP-INF/APPLICATION.JNLP
-	@echo "# $(FULL_MAIN_JAR) contains the main class"
-	"$(JAR)" -uf $(FULL_MAIN_JAR) JNLP-INF/APPLICATION.JNLP
-	rm -rf JNLP-INF
-	"$(PTJAVA_DIR)/bin/jarsigner" \
+# Makefile variables used to set up keys for jar signing.
+# To use Web Start, we have to sign the jars.
+KEYDNAME = "CN=Claudius Ptolemaus, OU=Ptolemy Project, O=UC Berkeley, L=Berkeley, S=California, C=US"
+KEYSTORE = ptKeystore
+KEYALIAS = claudius
+# The password should not be stored in a makefile, for production
+# purposes, run:
+#  make STOREPASSWORD= KEYPASSWORD= jnlp_sign
+STOREPASSWORD = -storepass this.is.not.secure,it.is.for.testing.only
+KEYPASSWORD = -keypass this.is.not.secure,it.is.for.testing.only
+KEYTOOL = $(PTJAVA_DIR)/bin/keytool
+$(KEYSTORE): 
+	"$(KEYTOOL)" -genkey \
+		-dname $(KEYDNAME) \
 		-keystore $(KEYSTORE) \
+		-alias $(KEYALIAS) \
 		$(STOREPASSWORD) \
-		$(FULL_MAIN_JAR) $(KEYALIAS)
+		$(KEYPASSWORD)
+	"$(KEYTOOL)" -selfcert \
+		-keystore $(KEYSTORE) \
+		-alias $(KEYALIAS) \
+		$(STOREPASSWORD)
+	"$(KEYTOOL)" -list \
+		-keystore $(KEYSTORE) \
+		$(STOREPASSWORD)
 
+# vergil*.jnlp is for Web Start.  For jar signing to work with Web Start,
 # Web Start: DSP version of Vergil - No sources or build env.
 vergilDSP.jnlp: vergilDSP.jnlp.in
 	sed 	-e 's%@PTII_LOCALURL@%$(PTII_LOCALURL)%' \
@@ -177,33 +200,28 @@ vergilPtiny.jnlp: vergilPtiny.jnlp.in
 		$(STOREPASSWORD) \
 		$(PTINY_MAIN_JAR) $(KEYALIAS)
 
-# Makefile variables used to set up keys for jar signing.
-# To use Web Start, we have to sign the jars.
-KEYDNAME = "CN=Claudius Ptolemaus, OU=Ptolemy Project, O=UC Berkeley, L=Berkeley, S=California, C=US"
-KEYSTORE = ptKeystore
-KEYALIAS = claudius
-# The password should not be stored in a makefile, for production
-# purposes, run:
-#  make STOREPASSWORD= KEYPASSWORD= jnlp_sign
-STOREPASSWORD = -storepass this.is.not.secure,it.is.for.testing.only
-KEYPASSWORD = -keypass this.is.not.secure,it.is.for.testing.only
-KEYTOOL = $(PTJAVA_DIR)/bin/keytool
-$(KEYSTORE): 
-	"$(KEYTOOL)" -genkey \
-		-dname $(KEYDNAME) \
-		-keystore $(KEYSTORE) \
-		-alias $(KEYALIAS) \
-		$(STOREPASSWORD) \
-		$(KEYPASSWORD)
-	"$(KEYTOOL)" -selfcert \
-		-keystore $(KEYSTORE) \
-		-alias $(KEYALIAS) \
-		$(STOREPASSWORD)
-	"$(KEYTOOL)" -list \
-		-keystore $(KEYSTORE) \
-		$(STOREPASSWORD)
 
-# vergil*.jnlp is for Web Start.  For jar signing to work with Web Start,
+# Web Start: Full Runtime version of Vergil - No sources or build env.
+vergil.jnlp: vergil.jnlp.in
+	sed 	-e 's%@PTII_LOCALURL@%$(PTII_LOCALURL)%' \
+		-e 's%@PTVERSION@%$(PTVERSION)%' \
+			$< > $@
+	@echo "# Adding jar files to $@"
+	"$(MKJNLP)" $@ \
+		$(FULL_MAIN_JAR) \
+		$(FULL_JNLP_JARS)
+	@echo "# Updating JNLP-INF/APPLICATION.JNLP with $@"
+	rm -rf JNLP-INF
+	mkdir JNLP-INF
+	cp $@ JNLP-INF/APPLICATION.JNLP
+	@echo "# $(FULL_MAIN_JAR) contains the main class"
+	"$(JAR)" -uf $(FULL_MAIN_JAR) JNLP-INF/APPLICATION.JNLP
+	rm -rf JNLP-INF
+	"$(PTJAVA_DIR)/bin/jarsigner" \
+		-keystore $(KEYSTORE) \
+		$(STOREPASSWORD) \
+		$(FULL_MAIN_JAR) $(KEYALIAS)
+
 # the .jnlp file itself must be included in the signed jar file
 # and not be changed (See Section 5.4 of the JNLP specification).
 jnlp_sign: $(JNLPS) $(KEYSTORE)
@@ -217,4 +235,19 @@ jnlp_sign: $(JNLPS) $(KEYSTORE)
 	done;
 
 
+sign_jar: 
+	"$(PTJAVA_DIR)/bin/jarsigner" \
+		-keystore $(KEYSTORE) \
+		$(STOREPASSWORD) \
+		$(JARFILE) $(KEYALIAS)
 
+
+# Verify the jar files.  This is useful for debugging if you are
+# getting errors about unsigned applications
+ 
+verify_jars:
+	set $(ALL_JNLP_JARS); \
+	for x do \
+		echo "$$x"; \
+		"$(PTJAVA_DIR)/bin/jarsigner" -verify $$x; \
+	done;
