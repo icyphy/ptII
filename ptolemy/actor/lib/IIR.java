ce86e977839f5e254bb83539d7c5ca6308edcad6
cxh <cxh>
2001-09-25 05:44:03 +0000

    Type-polymorphic IIR from Research In Motion:
    We sorely needed a type-polymorphic IIR, specifically for the Complex
    domain, so we finally reworked ptolemy.actor.lib.IIR to be type-polymorphic
    (before this, we've been using a hacked ComplexIIR...)
    
    We'd like to contribute this back and hope you will find it useful. Now IIR
    should work over any algebraic ring of Tokens... :-)
    
    A note about the _denominator[0] override to "one" in attributeChanged()...
    This is not really necessary, since _denominator[0] itself is not used in
    the computation. A better solution would be to normalize the entire
    denominator using _denominator[0] (like matlab does), but that would require
    a divide() operation and reduce the range of applications so we left it as
    is...
    
    Regards from our DSP Group that includes Aleksandar Necakov who did and
    tested this change...

--- ptolemy/actor/lib/IIR.java
+++ ptolemy/actor/lib/IIR.java
@@ -1,6 +1,7 @@
 /* An IIR filter actor that uses a direct form II implementation.
 
- Copyright (c) 1998-2001 The Regents of the University of California.
+ Copyright (c) 1998-2001 The Regents of the University of California and
+ Research in Motion Limited.
  All rights reserved.
  Permission is hereby granted, without written agreement and without
  license or royalty fees, to use, copy, modify, and distribute this
@@ -8,19 +9,20 @@
  copyright notice and the following two paragraphs appear in all copies
  of this software.
 
- IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
- FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
- ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
- THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+ IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA OR RESEARCH IN MOTION
+ LIMITED BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT, SPECIAL,
+ INCIDENTAL, OR CONSEQUENTIAL DAMAGES ARISING OUT OF THE USE OF THIS
+ SOFTWARE AND ITS DOCUMENTATION, EVEN IF THE UNIVERSITY OF CALIFORNIA
+ OR RESEARCH IN MOTION LIMITED HAVE BEEN ADVISED OF THE POSSIBILITY OF
  SUCH DAMAGE.
 
- THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
- INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
- MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
- PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
- CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ THE UNIVERSITY OF CALIFORNIA AND RESEARCH IN MOTION LIMITED
+ SPECIFICALLY DISCLAIM ANY WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ PARTICULAR PURPOSE. THE SOFTWARE PROVIDED HEREUNDER IS ON AN "AS IS"
+ BASIS, AND THE UNIVERSITY OF CALIFORNIA AND RESEARCH IN MOTION
+ LIMITED HAVE NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
  ENHANCEMENTS, OR MODIFICATIONS.
-
                                         PT_COPYRIGHT_VERSION_2
                                         COPYRIGHTENDKEY
 
@@ -30,32 +32,38 @@
 
 package ptolemy.actor.lib;
 
-import ptolemy.actor.*;
 import ptolemy.gui.MessageHandler;
 import ptolemy.gui.CancelException;
 import ptolemy.kernel.CompositeEntity;
-import ptolemy.kernel.util.*;
-import ptolemy.data.*;
-import ptolemy.data.type.BaseType;
+import ptolemy.kernel.util.Attribute;
+import ptolemy.kernel.util.IllegalActionException;
+import ptolemy.kernel.util.NameDuplicationException;
+import ptolemy.data.Token;
+import ptolemy.data.ArrayToken;
+import ptolemy.data.type.TypeLattice;
 import ptolemy.data.expr.Parameter;
 
 ///////////////////////////////////////////////////////////////////
 //// IIR
 /**
-This actor is an implementation of an infinite impulse response IIR filter.
-A direct form II [1] implementation is used. The input and output types
-are DoubleToken.
+
+This actor is an implementation of an infinite impulse response IIR
+filter.  A direct form II [1] implementation is used. This actor is type
+polymorphic. Its input, output,
+numerator and denominator types can be any type of Token supporting the
+basic arithmetic operations (add, subtract and multiply).
 <p>
 This filter has a transfer function given by:
 <pre>
-       b<sub>0</sub> + b<sub>1</sub>z<sup>-1</sup> + ... + b<sub>M</sub>z<sup>-M</sup>
+       b<sub>0</sub> + b<sub>1</sub>z<sup>-1</sup> + ... +
+b<sub>M</sub>z<sup>-M</sup>
       ------------------------
        1 + a<sub>1</sub>z<sup>-1</sup> + ... + a<sub>N</sub>z<sup>-N</sup>
 </pre>
 The constant terms of the numerator polynomial are specified by the
 <i>numerator</i> parameter and the constant terms of the denominator
 polynomial are specified by the <i>denominator</i> parameter.
-Both of these are given as arrays of doubles, using the syntax
+Both of these are given as arrays, using the syntax
 <pre>
    {b<sub>0</sub>, b<sub>1</sub>, ..., b<sub>M</sub>}
 </pre>
@@ -63,9 +71,10 @@ The default numerator and denominator are both {1.0}, resulting in
 a filter with unity transfer function.
 <p>
 The first coefficient of the
-<i>denominator</i> parameter is required to be 1.0. This
+<i>denominator</i> parameter is required to be 1 of the denominator token
+type. This
 implementation will issue a warning if the user enters something
-other than 1.0, and will use 1.0 instead.  The value is shown explicitly
+other than 1, and will use 1 instead.  The value is shown explicitly
 nonetheless so that the numerator and denominator polynomials are easy
 to read.
 <p>
@@ -76,6 +85,7 @@ A. V. Oppenheim, R. W. Schafer, <i>Discrete-Time Signal Processing</i>,
 Prentice Hall, 1989.
 
 @author Brian K. Vogel
+@author Aleksandar Necakov, Research in Motion Limited
 @version $Id$
 */
 public class IIR extends Transformer {
@@ -98,22 +108,20 @@ public class IIR extends Transformer {
 	denominator = new Parameter(this, "denominator");
         denominator.setExpression("{1.0}");
 	attributeChanged(denominator);
-        input.setTypeEquals(BaseType.DOUBLE);
-        output.setTypeEquals(BaseType.DOUBLE);
     }
 
     ///////////////////////////////////////////////////////////////////
     ////                     ports and parameters                  ////
 
     /** This parameter represents the numerator coefficients as an array
-     *  of a tokens of type double. The format is
+     *  of tokens. The format is
      *  {b<sub>0</sub>, b<sub>1</sub>, ..., b<sub>M</sub>}. The default
      *  value of this parameter is {1.0}.
      */
     public Parameter numerator;
 
     /** This  parameter represents the denominator coefficients as an
-     *  array of a tokens of type double. The format is
+     *  array of a tokens. The format is
      *  {a<sub>0</sub>, a<sub>1</sub>, ..., a<sub>N</sub>}. Note that
      *  the value of a<sub>0</sub> is constrained to be 1.0. This
      *  implementation will issue a warning if it is not.
@@ -135,32 +143,32 @@ public class IIR extends Transformer {
             throws IllegalActionException {
         if (attribute == numerator) {
             ArrayToken numeratorValue = (ArrayToken)numerator.getToken();
-            _numerator = new double[numeratorValue.length()];
+            _numerator = new Token[numeratorValue.length()];
             for (int i = 0; i < numeratorValue.length(); i++) {
-                _numerator[i] = ((DoubleToken)numeratorValue.getElement(i))
-                    .doubleValue();
+                _numerator[i] = numeratorValue.getElement(i);
             }
 	} else if (attribute == denominator) {
-            ArrayToken denominatorValue = (ArrayToken)denominator.getToken();
-            _denominator = new double[denominatorValue.length()];
+            ArrayToken denominatorValue =
+(ArrayToken)denominator.getToken();
+            _denominator = new Token[denominatorValue.length()];
             for (int i = 0; i < denominatorValue.length(); i++) {
-                _denominator[i] = ((DoubleToken)denominatorValue.getElement(i))
-                    .doubleValue();
+                _denominator[i] = denominatorValue.getElement(i);
             }
 
 	    // Note: a<sub>0</sub> must always be 1.
             // Issue a warning if it isn't.
-            if (_denominator[0] != 1.0) {
+            if
+(!_denominator[0].isEqualTo(_denominator[0].one()).booleanValue()) {
                 try {
                     MessageHandler.warning(
-                            "First denominator value is required to be 1.0. "
-                            + "Using 1.0.");
+                            "First denominator value is required to be 1. "
+                            + "Using 1.");
                 } catch (CancelException ex) {
                     throw new IllegalActionException(this,
                             "Canceled parameter change.");
                 }
                 // Override the user and just use 1.
-                _denominator[0] = 1.0;
+                _denominator[0] = _denominator[0].one();
             }
 	} else {
             super.attributeChanged(attribute);
@@ -168,10 +176,8 @@ public class IIR extends Transformer {
         }
 	// Initialize filter state.
 	if ((_numerator != null) && (_denominator != null)) {
-	    int stateSize = (int)java.lang.Math.max(_numerator.length,
-                    _denominator.length);
-	    _stateVector = new double[stateSize];
-	}
+            _initStateVector();
+        }
     }
 
     /** If at least one input token is available, consume a single
@@ -184,33 +190,16 @@ public class IIR extends Transformer {
      *  @exception IllegalActionException Not thrown in this base class.
      */
     public void fire() throws IllegalActionException {
-	// The current input sample.
-	double xCurrent;
-	// The current output sample.
-	double yCurrent;
-	double window;
         if (input.hasToken(0)) {
-            DoubleToken in = (DoubleToken)input.get(0);
-	    xCurrent = (in).doubleValue();
-	    window = xCurrent;
-	    for (int j = 1; j < _denominator.length; j++) {
-		window += -_denominator[j]*_stateVector[(_currentTap + j) %
-                        _stateVector.length];
-	    }
+            // Save state vector value.
+	    Token savedState = _stateVector[_currentTap];
+            // Compute the current output sample given the input sample.
+            Token yCurrent = _computeOutput((Token)input.get(0));
 	    // Shadowed state. used in postfire().
-	    _latestWindow = window;
-	    // Save state vector value.
-	    double savedState = _stateVector[_currentTap];
-	    _stateVector[_currentTap] = window;
-	    yCurrent = 0;
-	    for (int k = 0; k < _numerator.length; k++) {
-		yCurrent += _numerator[k]*_stateVector[(_currentTap +k) %
-                        _stateVector.length];
-	    }
+	    _latestWindow = _stateVector[_currentTap];
 	    // Restore state vector to previous state.
 	    _stateVector[_currentTap] = savedState;
-	    DoubleToken out = new DoubleToken(yCurrent);
-            output.send(0, out);
+            output.send(0, yCurrent);
 	}
     }
 
@@ -221,10 +210,8 @@ public class IIR extends Transformer {
     public void initialize() throws IllegalActionException {
 	super.initialize();
 	// Initialize filter state.
-	int stateSize =
-	    (int)java.lang.Math.max(_numerator.length, _denominator.length);
-	_stateVector = new double[stateSize];
-	_currentTap = 0;
+        _initStateVector();
+        _currentTap = 0;
     }
 
     /** Invoke a specified number of iterations of this actor. Each
@@ -245,36 +232,17 @@ public class IIR extends Transformer {
      *  performed.
      */
     public int iterate(int count) throws IllegalActionException {
-	// Check if we need to reallocate the output token array.
-	if (count > _resultArray.length) {
-	    _resultArray = new DoubleToken[count];
-	}
-	// The current input sample.
-	double xCurrent;
-	// The current output sample.
-	double yCurrent;
-	double window;
+        Token[] _resultArray = new Token[count];
         if (input.hasToken(0, count)) {
 	    // NOTE: inArray.length may be > count, in which case
 	    // only the first count tokens are valid.
             Token[] inArray = input.get(0, count);
 	    for (int i = 0; i < count; i++) {
-		xCurrent = ((DoubleToken)(inArray[i])).doubleValue();
-
-		window = xCurrent;
-		for (int j = 1; j < _denominator.length; j++) {
-		    window += -_denominator[j]*_stateVector[(_currentTap + j) %
-                            _stateVector.length];
-		}
-		_stateVector[_currentTap] = window;
-		yCurrent = 0;
-		for (int k = 0; k < _numerator.length; k++) {
-		    yCurrent +=	_numerator[k]*_stateVector[(_currentTap +k) %
-                            _stateVector.length];
-		}
-		_resultArray[i] = new DoubleToken(yCurrent);
+               	// Compute the current output sample given the input sample.
+                _resultArray[i] = _computeOutput(inArray[i]);
 		// Update the state vector pointer.
-		if (--_currentTap < 0) _currentTap = _stateVector.length - 1;
+		if (--_currentTap < 0) _currentTap = _stateVector.length -
+1;
 	    }
             output.send(0, _resultArray, count);
             return COMPLETED;
@@ -297,18 +265,61 @@ public class IIR extends Transformer {
     ///////////////////////////////////////////////////////////////////
     ////                         private variables                 ////
 
-    private DoubleToken[] _resultArray = new DoubleToken[1];
-
     // Filter parameters
-    private double[] _numerator;
-    private double[] _denominator;
+    private Token[] _numerator;
+    private Token[] _denominator;
 
     // Filter state vector
-    private double[] _stateVector;
+    private Token[] _stateVector;
 
     // State vector pointer
     private int _currentTap;
 
     // Shadow state.
-    private double _latestWindow;
+    private Token _latestWindow;
+
+    private void _initStateVector() throws  IllegalActionException {
+        if(_numerator == null || _denominator == null) {
+            throw new IllegalActionException(
+             "Cannot initialize the IIR filter. Invalid specification "
+             + "of the numerator or the denominator polynomial.");
+        }
+        // Get the first token from the Matrix
+	// Uses this token to extract its type.
+	Token tmpToken = numerator.getToken();
+	Token tmpDenomToken = denominator.getToken();
+	if (TypeLattice.compare(tmpToken, tmpDenomToken) ==
+            ptolemy.graph.CPO.LOWER) {
+            tmpToken = tmpDenomToken;
+	}
+	// Set the type to the input and output port.
+	input.setTypeEquals(((ArrayToken)tmpToken).getElementType());
+	output.setTypeEquals(((ArrayToken)tmpToken).getElementType());
+
+	int stateSize = (int)java.lang.Math.max(_numerator.length,
+                  _denominator.length);
+	_stateVector = new Token[stateSize];
+
+        for (int j = 0; j < _stateVector.length; j++) {
+            _stateVector[j] = ((ArrayToken)tmpToken).getElement(0).zero();
+        }
+    }
+
+    private Token _computeOutput(Token xCurrent) throws
+IllegalActionException {
+	for (int j = 1; j < _denominator.length; j++) {
+	    xCurrent = xCurrent.subtract(_denominator[j].multiply(
+                       _stateVector[(_currentTap + j) %
+_stateVector.length]));
+	}
+	_stateVector[_currentTap] = xCurrent;
+	Token yCurrent = _numerator[0].zero();
+	for (int k = 0; k < _numerator.length; k++) {
+	    yCurrent = yCurrent.add(_numerator[k].multiply(
+                        _stateVector[(_currentTap +k) %
+_stateVector.length]));
+	}
+        return yCurrent;
+    }
 }
+
