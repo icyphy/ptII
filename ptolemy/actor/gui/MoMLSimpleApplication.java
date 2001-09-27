dbbbc4ff95a71fc64874a0cc695c463505848ebe
cxh <cxh>
2001-09-27 01:40:55 +0000

    Implement ChangeListener interface so that we can use this in the auto tests

--- ptolemy/actor/gui/MoMLSimpleApplication.java
+++ ptolemy/actor/gui/MoMLSimpleApplication.java
@@ -34,6 +34,8 @@ package ptolemy.actor.gui;
 // Ptolemy imports
 import ptolemy.actor.CompositeActor;
 import ptolemy.actor.Manager;
+import ptolemy.kernel.util.ChangeListener;
+import ptolemy.kernel.util.ChangeRequest;
 import ptolemy.kernel.util.Workspace;
 import ptolemy.moml.MoMLParser;
 
@@ -41,28 +43,67 @@ import ptolemy.moml.MoMLParser;
 //// MoMLSimpleApplication
 /** A simple application that reads in a .xml file as a command
 line argument and runs it.
-MoMLApplication sets the look and feel, which starts up Swing,
+
+<p>MoMLApplication sets the look and feel, which starts up Swing,
 so we can't use MoMLApplication for non-graphical simulations.
+
+<p>We implement the ChangeListener interface so that this
+class will get exceptions thrown by failed change requests.
 
 @author Christopher Hylands
 @version $Id$
 */
-public class MoMLSimpleApplication {
+public class MoMLSimpleApplication implements ChangeListener {
+    /** Parse the xml file and run it.
+     */
+    public MoMLSimpleApplication(String xmlFilename) throws Exception{
+        MoMLParser parser = new MoMLParser();
+        CompositeActor toplevel =
+            (CompositeActor) parser.parseFile(xmlFilename);
+        Manager manager = new Manager(toplevel.workspace(),
+                "MoMLSimpleApplication");
+        toplevel.setManager(manager);
+        toplevel.addChangeListener(this);
+        manager.execute();
+    }
+
     ///////////////////////////////////////////////////////////////////
     ////                         public methods                    ////
+
+    /** React to a change request has been successfully executed.
+     *  This method is called after a change request
+     *  has been executed successfully.  In this class, we 
+     *  do nothing.
+     *  @param change The change that has been executed, or null if
+     *   the change was not done via a ChangeRequest.
+     */
+    public void changeExecuted(ChangeRequest change) {
+    }
+
+    /** React to a change request has resulted in an exception.
+     *  This method is called after a change request was executed,
+     *  but during the execution an exception was thrown.
+     *  @param change The change that was attempted or null if
+     *   the change was not done via a ChangeRequest.
+     *  @param exception The exception that resulted.
+     */
+    public void changeFailed(ChangeRequest change, Exception exception) {
+        // If we do not implement ChangeListener, then ChangeRequest
+        // will print any errors to stdout and continue.
+        // This causes no end of trouble with the test suite
+
+        // We can't throw and Exception here because this method in
+        // the base class does not throw Exception.
+        throw new RuntimeException(exception);
+    }
 
     /** Create an instance of a single model and run it
      *  @param args The command-line arguments naming the .xml file to run
      */
     public static void main(String args[]) {
-	try {
-            MoMLParser parser = new MoMLParser();
-            CompositeActor toplevel =
-                (CompositeActor) parser.parseFile(args[0]);
-            Manager manager = new Manager(toplevel.workspace(),
-                    "MoMLSimpleApplication");
-            toplevel.setManager(manager);
-            manager.execute();
+        try {
+            MoMLSimpleApplication simpleApplication =
+                new MoMLSimpleApplication(args[0]);
         } catch (Exception ex) {
             System.err.println("Command failed: " + ex);
         }
