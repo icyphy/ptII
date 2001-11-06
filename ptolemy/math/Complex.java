b97865fa0c13350384f6f347576912858b11336f
cxh <cxh>
2001-11-06 22:01:53 +0000

    Changed acceptedrating tag

--- ptolemy/math/Complex.java
+++ ptolemy/math/Complex.java
@@ -26,7 +26,7 @@ ENHANCEMENTS, OR MODIFICATIONS.
                                         COPYRIGHTENDKEY
 
 @ProposedRating Yellow (eal@eecs.berkeley.edu)
-@AcceptedRating Red (cxh@eecs.berkeley.edu) isCloseTo(), epsilon
+@AcceptedRating Red (cxh@eecs.berkeley.edu) isCloseTo(t), isCloseTo(t,e), epsilon
 */
 
 package ptolemy.math;
