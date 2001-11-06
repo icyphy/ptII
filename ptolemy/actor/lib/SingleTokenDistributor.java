372985e3e724b757b5e4da162a5381f1f000f2a1
cxh <cxh>
2001-11-06 21:48:41 +0000

    The review notes say that this needs reviewing again

--- ptolemy/actor/lib/SingleTokenDistributor.java
+++ ptolemy/actor/lib/SingleTokenDistributor.java
@@ -24,8 +24,8 @@
                                         PT_COPYRIGHT_VERSION_2
                                         COPYRIGHTENDKEY
 
-@ProposedRating Yellow (cxh@eecs.berkeley.edu)
-@AcceptedRating Yellow (cxh@eecs.berkeley.edu)
+@ProposedRating Red (cxh@eecs.berkeley.edu)
+@AcceptedRating Red (cxh@eecs.berkeley.edu)
 */
 
 package ptolemy.actor.lib;
