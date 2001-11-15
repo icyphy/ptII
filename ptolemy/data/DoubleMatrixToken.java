0fc08c6a466cb0277750ea2c983185687e15663a
cxh <cxh>
2001-11-15 22:21:52 +0000

    Demoted to Red

--- ptolemy/data/DoubleMatrixToken.java
+++ ptolemy/data/DoubleMatrixToken.java
@@ -25,8 +25,7 @@
                                         COPYRIGHTENDKEY
 
 @ProposedRating Yellow (yuhong@eecs.berkeley.edu)
-@AcceptedRating Yellow (wbwu@eecs.berkeley.edu)
-
+@AcceptedRating Red (cxh@eecs.berkeley.edu) isCloseTo(t), isCloseTo(t,e)
 */
 
 package ptolemy.data;
