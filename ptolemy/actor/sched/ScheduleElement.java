ff4eae8e6b88a7392bd7792ca5b5d2e4a65facb5
cxh <cxh>
2001-11-29 01:02:58 +0000

    Removed trailing spaces

--- ptolemy/actor/sched/ScheduleElement.java
+++ ptolemy/actor/sched/ScheduleElement.java
@@ -156,7 +156,7 @@ public abstract class ScheduleElement {
 	}
     }
 
-   
+
 
     ///////////////////////////////////////////////////////////////////
     ////                         private variables                 ////
