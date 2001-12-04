59af507a8bbfb92bd1abef95d52f23c385799a09
cxh <cxh>
2001-12-04 00:14:16 +0000

    Version -> VersionAttribute

---
+++ ptolemy/kernel/util/VersionAttribute.java
@@ -0,0 +1,228 @@
+/* Ptolemy II Version identifiers
+
+ Copyright (c) 2001 The Regents of the University of California.
+ All rights reserved.
+ Permission is hereby granted, without written agreement and without
+ license or royalty fees, to use, copy, modify, and distribute this
+ software and its documentation for any purpose, provided that the above
+ copyright notice and the following two paragraphs appear in all copies
+ of this software.
+
+ IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+ FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+ ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+ THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+ SUCH DAMAGE.
+
+ THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+ INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+ PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+ CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ ENHANCEMENTS, OR MODIFICATIONS.
+
+                                        PT_COPYRIGHT_VERSION_2
+                                        COPYRIGHTENDKEY
+
+@ProposedRating Red (cxh@eecs.berkeley.edu)
+@AcceptedRating Red (cxh@eecs.berkeley.edu)
+*/
+
+package ptolemy.kernel.util;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+//////////////////////////////////////////////////////////////////////////
+//// VersionAttribute
+/** 
+An attribute that identifies the version of an object.  
+The value of the attribute contains a String version-id that represents
+the version.
+A version-id is a string with substrings separated by one of '.', '-' or '_'.
+The substrings may consist of any characters except space.  Version-ids can
+be compared against each other other with the compareTo() method.
+
+<p>The JNLP specification at
+<a href="http://jcp.org/jsr/detail/056.jsp"><code>http://jcp.org/jsr/detail/056.jsp</code></a>
+gives the following syntax for version-ids:
+<pre>
+version-id ::= string ( separator string ) *
+string ::= char ( char ) *
+char ::= Any ASCII character except a space, a separator or a
+modifier
+separator ::= "." | "-" | "_"
+</pre>
+Valid version-id include "1.3", "1.3.1", "1.3-beta_01".
+
+<p>The JNLP specification includes version-strings, which are used for
+matching one or more version-ids in a fashion similar to wildcard
+matches within a regular expression.  At this time, this class does
+not implement version-strings.
+
+<p>
+@author Christopher Hylands
+@version $Id$ */
+public class VersionAttribute extends StringAttribute implements Settable, Comparable {
+    
+    /** Construct an attribute with the given name contained by the specified
+     *  container. The container argument must not be null, or a
+     *  NullPointerException will be thrown.  This attribute will use the
+     *  workspace of the container for synchronization and version counts.
+     *  If the name argument is null, then the name is set to the empty
+     *  string. The object is added to the directory of the workspace
+     *  if the container is null.
+     *  Increment the version of the workspace.
+     *  @param container The container.
+     *  @param name The name of this attribute.
+     *  @exception IllegalActionException If the attribute is not of an
+     *   acceptable class for the container, or if the name contains a period.
+     *  @exception NameDuplicationException If the name coincides with
+     *   an attribute already in the container.
+     */
+    public VersionAttribute(NamedObj container, String name)
+            throws IllegalActionException, NameDuplicationException {
+        super(container, name);
+        _tupleList = new LinkedList();
+    }
+
+    public VersionAttribute(NamedObj container, String name, String expression)
+            throws IllegalActionException, NameDuplicationException {
+        super(container, name);
+	setExpression(expression);
+    }
+
+    /** Set the value of the string attribute and notify the container
+     *  of the value of this attribute by calling attributeChanged().
+     *  Notify any value listeners of this attribute.
+     *
+     *  @param expression The version string, consisting of 
+     *  version ID tuples separated by '.', '-' or '_', for example
+     *  "1.2", "1.2_beta-4".
+     *  @exception IllegalActionException If the argument contains a
+     *  space, which violates the JNLP Version format specification
+     */
+    public void setExpression(String expression)
+	throws IllegalActionException {
+	super.setExpression(expression);
+        if (expression.indexOf(' ') != -1 ) {
+            throw new IllegalActionException(this,
+					     "Versions cannot contain "
+					     + "spaces: '"
+					     + expression + "'");
+        }
+	_tupleList = new LinkedList();
+        StringTokenizer tokenizer = new StringTokenizer(expression, ".-_");
+        while (tokenizer.hasMoreTokens()) {
+            _tupleList.add(tokenizer.nextToken());
+        }
+        Iterator tuples = _tupleList.iterator();
+    }
+
+    ///////////////////////////////////////////////////////////////
+    ////                     public methods                    ////
+
+    /** Compare the value of this VersionAttribute against the argument
+     *  according to the VersionAttribute syntax and padding rules.  
+     *  <p> "1.2.2-005" is greater than "1.2.2.4", 
+     *  <br> "1.3.1" is an greater than "1.3"
+     *  Version-id contain one or more elements,
+     *  when two version-id's are compared, they are normalized by
+     *  padding the shortest version-id with additional elements containing
+     *  "0".
+     *  During comparison, if both elements can be parsed as Java
+     *  <code>int</code>s, then they are compared integers.  If the
+     *  elements cannot be parsed as integers, they are compared as Strings.
+     *
+     *  @param object The VersionAttribute to compare against. 
+     *  @return 0 if the argument is an exact match according to the
+     *  Version syntax and padding rules, a number less than 0 if the
+     *  argument is less than this Version, a number greater than 0 if
+     *  the argument is greater than this Version */
+    public int compareTo(Object object) {
+	VersionAttribute version = (VersionAttribute) object;
+        Iterator versionTuples = version.iterator();
+        Iterator tuples = _tupleList.iterator();
+        while (versionTuples.hasNext() || tuples.hasNext()){
+            String versionTuple, tuple;
+
+            // FIXME: deal with * and + in the JNLP Version String spec.
+
+            // Normalize the shortest tuple by padding with 0
+            if (versionTuples.hasNext()) {
+                versionTuple = (String)versionTuples.next();
+            } else {
+                versionTuple = "0";
+            }
+            if (tuples.hasNext()) {
+                tuple = (String)tuples.next();
+            } else {
+                tuple = "0";
+            }
+
+	    // If both elements can be parsed as Java ints, then
+	    // compare them as ints.  If not, then compare them
+	    // as Strings.
+
+	    try {
+		// Try parsing as ints.
+		int tupleInt = Integer.parseInt(tuple);
+		int versionInt = Integer.parseInt(versionTuple);
+		if (tupleInt < versionInt) {
+		    return -1;
+		} else if (tupleInt > versionInt) {
+		    return 1;
+		}
+	    } catch (NumberFormatException ex) {
+		// Compare as Strings.
+		int compare = tuple.compareTo(versionTuple);
+		if (compare < 0) {
+		    return -1;
+		} else if (compare > 0) {
+		    return 1;
+		}
+	    }
+        }
+	return 0;
+    }
+    
+    public Iterator iterator() {
+	return _tupleList.iterator();
+    }
+
+    ///////////////////////////////////////////////////////////////
+    ////                     public variables                  ////
+
+
+    /** The VersionAttribute that contains the version of the Ptolemy II
+     *  release.  This variable may be read to change the Ptolemy II
+     *  functionality depending on the version number:
+     *  <pre>
+     *  if (VersionAttribute.PTOLEMYII_VERSION.compareTo("2.0") >= 0 ) {
+     *      // Perform some operation if the current version is
+     *      // Ptolemy II 2.0 or later.
+     *  }
+     *  </pre>
+     */ 
+    public static final VersionAttribute PTOLEMYII_VERSION;
+    static {
+	try {
+	    NamedObj version = new NamedObj("PTOLEMYII_VERSION container");
+	    PTOLEMYII_VERSION = new VersionAttribute(version,
+						     "PTOLEMYII_VERSION",
+						     "2.0-devel");
+	} catch (Exception ex) {
+	    throw new ExceptionInInitializerError("Failed to create "
+						  + "PTOLEMYII_VERSION: "
+						  + KernelException.stackTraceToString(ex));
+	}
+    }
+
+    ///////////////////////////////////////////////////////////////
+    ////                     private variables                  ////
+
+    // A List representation of the version.
+    private List _tupleList;
+}
---
+++ ptolemy/kernel/util/test/VersionAttribute.tcl
@@ -0,0 +1,86 @@
+# Tests for the Version class
+#
+# @Author: Christopher Hylands
+#
+# @Version: $Id$ 
+#
+# @Copyright (c) 2001 The Regents of the University of California.
+# All rights reserved.
+#
+# Permission is hereby granted, without written agreement and without
+# license or royalty fees, to use, copy, modify, and distribute this
+# software and its documentation for any purpose, provided that the
+# above copyright notice and the following two paragraphs appear in all
+# copies of this software.
+#
+# IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+# FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+# ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+# THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+#
+# THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+# INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+# PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+# CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+# ENHANCEMENTS, OR MODIFICATIONS.
+#
+# 						PT_COPYRIGHT_VERSION_2
+# 						COPYRIGHTENDKEY
+#######################################################################
+
+# Tycho test bed, see $PTII/doc/coding/testing.htm for more information.
+
+# Load up the test definitions.
+if {[string compare test [info procs test]] == 1} then {
+    source testDefs.tcl
+} {}
+
+# Uncomment this to get a full report, or set in your Tcl shell window.
+# set VERBOSE 1
+
+
+######################################################################
+####
+#
+test VersionAttribute-1.0 {Constructor} {
+    set n [java::new ptolemy.kernel.util.NamedObj "my NamedObj"]
+    set v [java::new ptolemy.kernel.util.VersionAttribute $n "my Version"]
+    set result1 [$v toString]
+    set PTOLEMYII_VERSION [java::field \
+	    ptolemy.kernel.util.VersionAttribute PTOLEMYII_VERSION]
+    $v setExpression [$PTOLEMYII_VERSION getExpression]
+
+    set result2 [$v toString]
+    set result3 [$v getExpression]
+    list $result1 $result2 $result3
+} {{ptolemy.kernel.util.VersionAttribute {.my NamedObj.my Version}} {ptolemy.kernel.util.VersionAttribute {.my NamedObj.my Version}} 2.0-devel}
+
+
+test VersionAttribute-2.0 {compareTo} {
+    set n [java::new ptolemy.kernel.util.NamedObj "my NamedObj"]
+    set v [java::new ptolemy.kernel.util.VersionAttribute $n \
+	    "testValue"]
+
+    set PTOLEMYII_VERSION [java::field \
+	    ptolemy.kernel.util.VersionAttribute PTOLEMYII_VERSION]
+
+    set results {}
+    set testValues [list "1.0" "1.0.0" "1.0-beta" \
+	    "2.0" "2.0-devel" "2.0.alpha" "2.0_beta" "2.0-build003" \
+	    "2.0-release-1" \
+	    "3.0" \
+	    [$PTOLEMYII_VERSION getExpression] \
+	    ]
+    foreach testValue $testValues {
+	$v setExpression $testValue
+	lappend results \
+		[list \
+		[$v getExpression] \
+		[$PTOLEMYII_VERSION getExpression] \
+		[$v compareTo $PTOLEMYII_VERSION] \
+		[$PTOLEMYII_VERSION compareTo $v]]
+    }
+    list $results
+} {{{1.0 2.0-devel -1 1} {1.0.0 2.0-devel -1 1} {1.0-beta 2.0-devel -1 1} {2.0 2.0-devel -1 1} {2.0-devel 2.0-devel 0 0} {2.0.alpha 2.0-devel -1 1} {2.0_beta 2.0-devel -1 1} {2.0-build003 2.0-devel -1 1} {2.0-release-1 2.0-devel 1 -1} {3.0 2.0-devel 1 -1} {2.0-devel 2.0-devel 0 0}}}
