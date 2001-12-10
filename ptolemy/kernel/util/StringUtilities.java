9a63d4aeb5977fa1b0ad9a2b31fc8c371aead909
neuendor <neuendor>
2001-12-10 22:45:59 +0000

    added SanitizeName

--- ptolemy/kernel/util/StringUtilities.java
+++ ptolemy/kernel/util/StringUtilities.java
@@ -68,6 +68,42 @@ public class StringUtilities {
         string = substitute(string, ">", "&gt;");
         return string;
     }
+   
+    /** Sanitize a String so that it can be used as a Java identifier.
+     *  Section 3.8 of the Java language spec says:
+     *  <blockquote>
+     *  "An identifier is an unlimited-length sequence of Java letters
+     *  and Java digits, the first of which must be a Java letter. An
+     *  identifier cannot have the same spelling (Unicode character
+     *  sequence) as a keyword (3.9), boolean literal (3.10.3), or
+     *  the null literal (3.10.7).  "
+     *  </blockquote>
+     *  Java characters are A-Z, a-z, $ and _.
+     *  <p> Characters that are not permitted in a Java identifier are changed
+     *  to an underscores. 
+     *  This method does not check that the returned string is a 
+     *  keyword or literal.
+     *  Note that two different strings can sanitize to the same
+     *  string.
+     *  This method is commonly used during code generation to map the
+     *  name of a ptolemy object to a valid identifier name.
+     *  @param name A string with spaces and other characters that
+     *  cannot be in a Java name.
+     *  @return A String that follows the Java identifier rules.
+     */
+    public static String sanitizeName(String name) {
+	char [] nameArray = name.toCharArray();
+       	for(int i = 0; i < nameArray.length; i++) {
+	    if (!Character.isJavaIdentifierPart(nameArray[i])) {
+		nameArray[i] = '_';
+	    }
+	}
+      	if (!Character.isJavaIdentifierStart(nameArray[0])) {
+            return "_" + new String(nameArray);
+	} else {
+            return new String(nameArray);
+        }
+    }    
 
     /** Replace all occurrences of <i>old</i> in the specified
      *  string with <i>replacement</i>.
