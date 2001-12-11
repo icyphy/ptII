969193da71c3bb228090aa2a341891e2e8814d5f
bldmastr <bldmastr>
2001-12-11 20:30:41 +0000

    printStackTrace: flush the printWriter when we are done.

--- ptolemy/kernel/util/KernelException.java
+++ ptolemy/kernel/util/KernelException.java
@@ -185,6 +185,7 @@ public class KernelException extends Exception {
             printWriter.print("Caused by: ");
             _cause.printStackTrace(printWriter);
         }
+	printWriter.flush();
     }
 
     /** Return the stack trace of the given argument as a String.
