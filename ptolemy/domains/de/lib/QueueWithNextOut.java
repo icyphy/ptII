e243768691cd6b248599fb0a886589dc5ddbbe15
winthrop <winthrop>
2002-01-24 19:33:59 +0000

    Fixed my email address. (The plain eecs one takes hours to come through.)

--- ptolemy/domains/de/lib/QueueWithNextOut.java
+++ ptolemy/domains/de/lib/QueueWithNextOut.java
@@ -24,8 +24,8 @@
                                         PT_COPYRIGHT_VERSION_2
                                         COPYRIGHTENDKEY
 
-@ProposedRating Red (winthrop@eecs.berkeley.edu)
-@AcceptedRating Red (winthrop@eecs.berkeley.edu)
+@ProposedRating Red (winthrop@robotics.eecs.berkeley.edu)
+@AcceptedRating Red (winthrop@robotics.eecs.berkeley.edu)
 */
 
 package ptolemy.domains.de.lib;
