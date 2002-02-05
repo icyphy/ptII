38c877b7f8150625ae246a73f2c51c5879bc2c9e
cxh <cxh>
2002-02-05 17:56:55 +0000

    Another interim fix

--- ptolemy/data/expr/ASTPtMethodCallNode.java
+++ ptolemy/data/expr/ASTPtMethodCallNode.java
@@ -1,7 +1,8 @@
 /* ASTPtMethodCallNode represents method calls on other Tokens and functional
    if-then else (?:) constructs.
 
- Copyright (c) 1998-2001 The Regents of the University of California.
+ Copyright (c) 1998-2001 The Regents of the University of California and
+ Research in Motion Limited.
  All rights reserved.
  Permission is hereby granted, without written agreement and without
  license or royalty fees, to use, copy, modify, and distribute this
@@ -9,21 +10,20 @@
  copyright notice and the following two paragraphs appear in all copies
  of this software.
 
- IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
- FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
- ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
- THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+ IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA OR RESEARCH IN MOTION
+ LIMITED BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT, SPECIAL,
+ INCIDENTAL, OR CONSEQUENTIAL DAMAGES ARISING OUT OF THE USE OF THIS
+ SOFTWARE AND ITS DOCUMENTATION, EVEN IF THE UNIVERSITY OF CALIFORNIA
+ OR RESEARCH IN MOTION LIMITED HAVE BEEN ADVISED OF THE POSSIBILITY OF
  SUCH DAMAGE.
 
- THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
- INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
- MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
- PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
- CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ THE UNIVERSITY OF CALIFORNIA AND RESEARCH IN MOTION LIMITED
+ SPECIFICALLY DISCLAIM ANY WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ PARTICULAR PURPOSE. THE SOFTWARE PROVIDED HEREUNDER IS ON AN "AS IS"
+ BASIS, AND THE UNIVERSITY OF CALIFORNIA AND RESEARCH IN MOTION
+ LIMITED HAVE NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
  ENHANCEMENTS, OR MODIFICATIONS.
-
-                                        PT_COPYRIGHT_VERSION_2
-                                        COPYRIGHTENDKEY
 
 @ProposedRating Yellow (nsmyth@eecs.berkeley.edu)
 @AcceptedRating Red (cxh@eecs.berkeley.edu)
@@ -36,14 +36,22 @@ package ptolemy.data.expr;
 
 import ptolemy.data.Token;
 import ptolemy.data.DoubleToken;
+import ptolemy.data.DoubleMatrixToken;
+import ptolemy.data.ArrayToken;
 import ptolemy.data.FixToken;
 import ptolemy.data.ComplexToken;
+import ptolemy.data.ComplexMatrixToken;
 import ptolemy.data.IntToken;
+import ptolemy.data.IntMatrixToken;
 import ptolemy.data.LongToken;
 import ptolemy.data.StringToken;
 import ptolemy.data.BooleanToken;
 import ptolemy.math.Complex;
+import ptolemy.math.ComplexMatrixMath;
+import ptolemy.math.DoubleMatrixMath;
+import ptolemy.math.IntegerMatrixMath;
 import ptolemy.math.FixPoint;
+
 import ptolemy.kernel.util.*;
 import java.lang.reflect.*;
 
@@ -117,12 +125,19 @@ public class ASTPtMethodCallNode extends ASTPtRootNode {
         Class[] argTypes = new Class[num - 1];
         Object[] argValues = new Object[num - 1];
         try {
+	    if (num == 1)
+		argValues = argTypes = null;
             for (int i = 1; i < num; i++) {
                 argValues[i-1] = (ptolemy.data.Token)_childTokens[i];
                 argTypes[i-1] = Class.forName("ptolemy.data.Token");
             }
             Class destTokenClass = _childTokens[0].getClass();
 	    Method m = null;
+            Method mBase = null;        // Base class for corresponding Token
+                                        // (e.g. ptolemy.math.Complex for
+                                        // ComplexToken)
+            Object mInstance = null;    // Base class instance to invoke method on.
+
 	    // first try to find a method whose arguments are all of type
 	    // ptolemy.data.Token
 	    try {
@@ -159,31 +174,78 @@ public class ASTPtMethodCallNode extends ASTPtRootNode {
 		    } else if (child instanceof FixToken) {
 			argValues[i] = ((FixToken)child).fixValue();
 			argTypes[i] = argValues[i].getClass();
+                    } else if (child instanceof IntMatrixToken) {
+                        argValues[i] = ((IntMatrixToken)child).intMatrix();
+                        argTypes[i] = argValues[i].getClass();
+                    } else if (child instanceof DoubleMatrixToken) {
+                        argValues[i] = ((DoubleMatrixToken)child).doubleMatrix();
+                        argTypes[i] = argValues[i].getClass();
+                    } else if (child instanceof ArrayToken) {
+                        argValues[i] = ((ArrayToken)child).arrayValue();
+                        argTypes[i] = argValues[i].getClass();
+                    } else if (child instanceof ComplexMatrixToken) {
+                        argValues[i] = ((ComplexMatrixToken)child).complexMatrix();
+                        argTypes[i] = argValues[i].getClass();
 		    } else {
 			argValues[i] = child;
 			argTypes[i] = argValues[i].getClass();
 		    }
 		}
-		m = destTokenClass.getMethod(_methodName, argTypes);
+                try {
+                    m = destTokenClass.getMethod(_methodName, argTypes);
+                } catch (java.lang.NoSuchMethodException ex) {
+                    if (_childTokens[0] instanceof ComplexToken) {
+                        mBase = ((new Complex(0.0)).getClass())
+                            .getMethod(_methodName, argTypes);
+                        mInstance = ((ComplexToken)_childTokens[0]).complexValue();
+                    } else if (_childTokens[0] instanceof StringToken) {
+                        mBase = ("String".getClass()).getMethod(_methodName, argTypes);
+                        mInstance = ((StringToken)_childTokens[0]).stringValue();
+                    } else {
+                        throw ex;       // throw it again
+                    }
+                }
 	    }
-            Object result = m.invoke(_childTokens[0], argValues);
+            Object result = null;
+            if (m != null)
+                result = m.invoke(_childTokens[0], argValues);
+            else if (mBase != null)
+                result = mBase.invoke(mInstance, argValues);
+            ptolemy.data.Token retval;
             // Method call can only return ptolemy.data.Token, we want?
             if (result instanceof ptolemy.data.Token) {
-                return (ptolemy.data.Token)result;
+                retval = (ptolemy.data.Token)result;
             } else if (result instanceof Double) {
-		return new DoubleToken(((Double)result).doubleValue());
+		retval = new DoubleToken(((Double)result).doubleValue());
 	    } else if (result instanceof Integer) {
-		return new IntToken(((Integer)result).intValue());
+		retval = new IntToken(((Integer)result).intValue());
 	    } else if (result instanceof Long) {
-		return new LongToken(((Long)result).longValue());
+		retval = new LongToken(((Long)result).longValue());
 	    } else if (result instanceof String) {
-		return new StringToken((String)result);
+		retval = new StringToken((String)result);
 	    } else if (result instanceof Boolean) {
-		return new BooleanToken(((Boolean)result).booleanValue());
+		retval = new BooleanToken(((Boolean)result).booleanValue());
 	    } else if (result instanceof Complex) {
-		return new ComplexToken((Complex)result);
+		retval = new ComplexToken((Complex)result);
 	    } else if (result instanceof FixPoint) {
-		return new FixToken((FixPoint)result);
+		//retval = ((FixPoint)result).tokenValue();
+	    } else if (result instanceof int[][]) {
+		retval = new IntMatrixToken((int[][])result);
+	    } else if (result instanceof double[][]) {
+		retval = new DoubleMatrixToken((double[][])result);
+	    } else if (result instanceof Token[]) {
+		retval = new ArrayToken((Token[])result);
+	    } else if (result instanceof Complex[][]) {
+		retval = new ComplexMatrixToken((Complex[][])result);
+	    } else if (result instanceof double[]) {
+		double[] res = (double[])result;
+		retval = new DoubleMatrixToken(DoubleMatrixMath.toMatrixFromArray(res,1,res.length));
+	    } else if (result instanceof Complex[]) {
+		Complex[] res = (Complex[])result;
+		retval = new ComplexMatrixToken(ComplexMatrixMath.toMatrixFromArray(res,1,res.length));
+	    } else if (result instanceof int[]) {
+		int[] res = (int[])result;
+		retval = new IntMatrixToken(IntegerMatrixMath.toMatrixFromArray(res,1,res.length));
 	    } else {
                 throw new IllegalActionException(
                         "Result of method call is not of a supported type: "
@@ -191,6 +253,7 @@ public class ASTPtMethodCallNode extends ASTPtRootNode {
 			+ "are: boolean, complex, fixpoint, double, int, "
 			+ "long, String, and ptolemy.data.Token.");
             }
+            return retval;
         } catch (java.lang.NoSuchMethodException ex) {
             // The detailed message of the caught exception is included in
             // the generated exception message.
