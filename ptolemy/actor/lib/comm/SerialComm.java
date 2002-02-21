7922b91c835c0cf6846bf7ebe742b154a91f2fb5
cxh <cxh>
2002-02-21 22:09:10 +0000

    Fixed @since

--- ptolemy/actor/lib/comm/SerialComm.java
+++ ptolemy/actor/lib/comm/SerialComm.java
@@ -121,8 +121,9 @@ port to use
 
 @author Winthrop Williams, Joern Janneck, Xiaojun Liu, Edward A. Lee
 (Based on my RxDatagram, and on the IRLink class writen by Xiaojun Liu)
-@version $Id$ */
+@version $Id$
 @since Ptolemy II 2.0
+ */
 public class SerialComm extends TypedAtomicActor
         implements SerialPortEventListener {
 
