aee96528dcc126285ad8585994d3e59a9098900f
cxh <cxh>
2002-02-21 18:20:48 +0000

    Spelling: Unitialized

--- com/JLex/Main.java
+++ com/JLex/Main.java
@@ -4483,7 +4483,7 @@ class CError
         "Unexpected end-of-file found.",
         "Undefined or badly-formed JLex directive.",
         "Internal JLex error.",
-        "Unitialized state name.",
+        "Unintialized state name.",
         "Badly formed macro definition.",
         "Syntax error.",
         "Missing brace at start of lexical action.",
