06935df3a77fb3a2f3ea785d4cf244ec4ee2b7ef
cxh <cxh>
2002-03-06 17:44:38 +0000

    neuendor: changes for lockups

--- ptolemy/vergil/icon/ValueIcon.java
+++ ptolemy/vergil/icon/ValueIcon.java
@@ -45,21 +45,13 @@ import java.io.Writer;
 //// ValueIcon
 /**
 An icon that displays the value of the container, which is assumed
-to be an instance of Settable.  This attribute also implements Settable,
-but when setExpression() is called, it simply delegates to the container.
-Similarly, when getExpression() is called, it gets the expression from
-the container. Moreover, when this attribute is given a container,
-it attempts to give itself the same name as the container, and when its
-name is requested, it first tries to ensure that the name matches that
-of the container.  This creates the effect that editing the parameters
-of the container (which include this attribute), sets the value of the
-container (via the setExpression() method of this attribute).
+to be an instance of Settable. 
 
-@author Edward A. Lee
+@author Edward A. Lee, Steve Neuendorffer
 @version $Id$
 @since Ptolemy II 2.0
 */
-public class ValueIcon extends XMLIcon implements Settable {
+public class ValueIcon extends XMLIcon {
 
     /** Create a new icon with the given name in the given container.
      *  The container is required to implement Settable, or an exception
@@ -74,18 +66,6 @@ public class ValueIcon extends XMLIcon implements Settable {
 
     ///////////////////////////////////////////////////////////////////
     ////                         public methods                    ////
-
-    /** Add a listener to be notified when the value of this settable
-     *  object changes. This implementation delegates to the container,
-     *  if there is one.
-     *  @param listener The listener to add.
-     */
-    public void addValueListener(ValueListener listener) {
-        Settable container = (Settable)getContainer();
-        if (container != null) {
-            container.addValueListener(listener);
-        }
-    }
 
     /** Create a background figure based on this icon, which is a text
      *  element with the name of the container, a colon, and its value.
@@ -144,139 +124,8 @@ public class ValueIcon extends XMLIcon implements Settable {
                 + getMoMLInfo().elementName + ">\n");
     }
 
-    /** Get the value of the attribute that has been set by setExpression(),
-     *  or null if there is none. This implementation delegates to the
-     *  container, if there is one, and returns null otherwise.
-     *  @return The expression.
-     */
-    public String getExpression() {
-        Settable container = (Settable)getContainer();
-        if (container != null) {
-            return container.getExpression();
-        } else {
-            return null;
-        }
-    }
-
-    /** Get the name. This method may have the side effect of changing
-     *  the name of this attribute to match that of the container.
-     *  @return The name of the object.
-     */
-    public String getName() {
-        Nameable container = getContainer();
-        if (container != null && !_settingName) {
-            try {
-                // Try to set the name of this to match the container.
-                // This apparently results in a call to getName(), so we
-                // use the variable _settingName to prevent infinite loop.
-                _settingName = true;
-                setName(container.getName());
-            } catch (KernelException ex) {
-                // Ignore and leave name as is.
-            } finally {
-                _settingName = false;
-            }
-        }
-        return super.getName();
-    }
-
-    /** Get the visibility of this Settable, as set by setVisibility().
-     *  If setVisibility() has not been called, then return FULL.
-     *  @return The visibility of this Settable.
-     */
-    public Settable.Visibility getVisibility() {
-        return _visibility;
-    }
-
-    /** Remove a listener from the list of listeners that are
-     *  notified when the value of this variable changes.  If no such listener
-     *  exists, do nothing. This implementation delegates to the container,
-     *  if there is one.
-     *  @param listener The listener to remove.
-     */
-    public void removeValueListener(ValueListener listener) {
-        Settable container = (Settable)getContainer();
-        if (container != null) {
-            container.removeValueListener(listener);
-        }
-    }
-
-    /** Specify the container, which is required to be an instance
-     *  of Settable.
-     *  @param container The container to attach this attribute to..
-     *  @exception IllegalActionException If this attribute does not
-     *   implement Settable, or it has no name,
-     *   or the attribute and container are not in the same workspace, or
-     *   the proposed container would result in recursive containment.
-     *  @exception NameDuplicationException If the container already has
-     *   an attribute with the name of this attribute.
-     */
-    public void setContainer(NamedObj container)
-            throws IllegalActionException, NameDuplicationException {
-        if (!(container instanceof Settable)) {
-            throw new IllegalActionException(this, container,
-                    "Cannot put a ValueAttribute in something that "
-                    + "is not Settable.");
-        }
-        super.setContainer(container);
-        try {
-            // Try to set the name of this to match the container.
-            setName(container.getName());
-        } catch (NameDuplicationException ex) {
-            // Ignore and leave name as is.
-        }
-    }
-
-    /** Set the value of the attribute by giving some expression.
-     *  This implementation delegates to the container, if there
-     *  is one, and returns null otherwise.
-     *  @return The expression.
-     */
-    public void setExpression(String expression) throws IllegalActionException {
-        Settable container = (Settable)getContainer();
-        if (container != null) {
-            container.setExpression(expression);
-        }
-    }
-
-    /** Set the visibility of this Settable.  The argument should be one
-     *  of the static public instances of the inner class Visibility.
-     *  If this method is not called, then the visibility will be FULL.
-     *  @param visibility The visibility of this Settable.
-     */
-    public void setVisibility(Settable.Visibility visibility) {
-        _visibility = visibility;
-    }
-
-    /** Check the validity of the expression set in setExpression().
-     *  This implementation delegates to the container, if there is one.
-     *  @exception IllegalActionException If the expression is not valid, or
-     *   its value is not acceptable to the container or the listeners.
-     */
-    public void validate() throws IllegalActionException {
-        Settable container = (Settable)getContainer();
-        if (container != null && !_validating) {
-            try {
-                // Prevent infinite loop, since container may try to
-                // validate this.
-                _validating = true;
-                container.validate();
-            } finally {
-                _validating = false;
-            }
-        }
-    }
-
     ///////////////////////////////////////////////////////////////////
     ////                         private members                   ////
 
     private static Font _labelFont = new Font("SansSerif", Font.PLAIN, 12);
-
-    // Flag to prevent infinite loop setting name.
-    private boolean _settingName = false;
-
-    // Flag to prevent infinite loop validating.
-    private boolean _validating = false;
-
-    private Settable.Visibility _visibility = FULL;
 }
