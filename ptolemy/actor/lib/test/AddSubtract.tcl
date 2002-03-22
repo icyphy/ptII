8fd28447b1cf36db779b046d9fc36ccd5e5e8d42
cxh <cxh>
2002-03-22 16:48:17 +0000

    Updated 3.2

--- ptolemy/actor/lib/test/AddSubtract.tcl
+++ ptolemy/actor/lib/test/AddSubtract.tcl
@@ -109,4 +109,6 @@ test AddSubtract-3.2 {test with run-time type error} {
     $minus link $r2
     catch {[$e0 getManager] execute} msg
     list $msg
-} {{ptolemy.kernel.util.IllegalActionException: Subtraction not supported on ptolemy.data.StringToken minus ptolemy.data.BooleanToken.}}
+} {{ptolemy.kernel.util.IllegalActionException: subtract operation not supported between ptolemy.data.StringToken '"a"' and ptolemy.data.BooleanToken 'true'
+Caused by:
+ptolemy.kernel.util.IllegalActionException: subtract operation not supported between ptolemy.data.StringToken '"a"' and ptolemy.data.StringToken '"true"'}}
