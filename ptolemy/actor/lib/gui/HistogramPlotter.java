82fe29f09f4a39ab2c1483279152ba3d2a24cf56
cxh <cxh>
2002-03-24 18:14:16 +0000

    Modified exportMoML format

--- ptolemy/actor/lib/gui/HistogramPlotter.java
+++ ptolemy/actor/lib/gui/HistogramPlotter.java
@@ -466,7 +466,7 @@ public class HistogramPlotter extends Sink implements Configurable, Placeable {
         String header
             = "<!DOCTYPE plot PUBLIC \"-//UC Berkeley//DTD PlotML 1//EN\"\n"                + "\"http://ptolemy.eecs.berkeley.edu/xml/dtd/PlotML_1.dtd\">";
 	if (histogram != null) {
-	    output.write(_getIndentPrefix(depth) + "<configure><?plotml\n"
+	    output.write(_getIndentPrefix(depth) + "<configure>\n<?plotml"
                     + header + "\n<plot>\n");
 	    PrintWriter print = new PrintWriter(output);
 	    // The second (null) argument indicates that PlotML PUBLIC DTD
@@ -490,10 +490,12 @@ public class HistogramPlotter extends Sink implements Configurable, Placeable {
                             + source
                             + "\">");
                 } else {
-                    output.write(_getIndentPrefix(depth) + "<configure>");
+                    output.write(_getIndentPrefix(depth) + "<configure>\n");
                 }
-                if (text != null) output.write(text);
-                output.write(_getIndentPrefix(depth) + "</configure>");
+                if (text != null) {
+		    output.write(text.trim() + "\n");
+		}
+                output.write(_getIndentPrefix(depth) + "</configure>\n");
             }
         }
     }
