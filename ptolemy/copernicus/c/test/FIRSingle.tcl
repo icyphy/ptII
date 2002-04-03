6d4a6746b5455175359a7c9ee15ec5fd44a5e3d4
shuvra <shuvra>
2002-04-03 13:11:33 +0000

    Test for new FIR single demo.

--- ptolemy/copernicus/c/test/FIRSingle.tcl
+++ ptolemy/copernicus/c/test/FIRSingle.tcl
@@ -1,1 +1,138 @@
-place holder
+# Tests Copernicus C Code generation for the singleClass FIR example.
+#
+# @Author Ankush Varma
+#
+# @Version: $Id$
+#
+# @Copyright (c) 2000-2002 The Regents of the University of California.
+# All rights reserved.
+#
+# Permission is hereby granted, without written agreement and without
+# license or royalty fees, to use, copy, modify, and distribute this
+# software and its documentation for any purpose, provided that the
+# above copyright notice and the following two paragraphs appear in all
+# copies of this software.
+#
+# IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+# FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+# ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+# THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+#
+# THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+# INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+# PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+# CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+# ENHANCEMENTS, OR MODIFICATIONS.
+#
+# 						PT_COPYRIGHT_VERSION_2
+# 						COPYRIGHTENDKEY
+#######################################################################
+
+# Ptolemy II bed, see /users/cxh/ptII/doc/coding/testing.html for more
+# information.
+
+# Load up the test definitions.
+if {[string compare test [info procs test]] == 1} then {
+    source testDefs.tcl
+} {}
+
+if {[info procs jdkClassPathSeparator] == "" } then {
+    source [file join $PTII util testsuite jdktools.tcl]
+}
+
+# Uncomment this to get a full report, or set in your Tcl shell window.
+# set VERBOSE 1
+
+######################################################################
+####
+#
+
+# This test generates three files: FIRSingle.c, FIRSingle.h, and
+# FIRSingle_i.h), and compiles,links, and executes these.  The output will
+# be that produced by a single invocation of the filter on the given data
+# (as specified in FIRconfig.h).  It uses FIRSingleMain.c as a wrapper. To
+# change parameters of the filter, change the corresponding values in
+# FIRconfig.h. 
+#
+# A corresponding Ptolemy II model is provided in FIRtest.xml. This model
+# drives an equivalent FIR filter with an equivalent input stream. Thus,
+# one can compare the output of the generated C code with the output of
+# what a corresponding Ptolemy II actor would produce.
+
+
+test FIRSingle-1.1 {Generate .c, _i.h, and .h files for FIR \
+        in singlclass mode and compile into an exe} {
+    
+    # Check if the FIRSingle.out directory exists.
+    if {[file isdirectory FIRSingle.out]} {
+        cd FIRSingle.out
+        # Remove all files generated in the previous run.
+        exec "rm *"
+        cd ..
+        # Now back in c/test directory.
+    } else {
+        # Create the FIRSingle.out directory.
+        exec mkdir FIRSingle.out
+    }
+
+
+    # We need to get the classpath so that we can run if we are running
+    # under Javascope, which includes classes in a zip file
+    set builtinClasspath [java::call System getProperty "java.class.path"]
+    set rtjar [java::call System getProperty "sun.boot.class.path"]
+    set classpath .[java::field java.io.File \
+            pathSeparator]$builtinClasspath[java::field java.io.File \
+            pathSeparator]$rtjar
+
+    # Generate the class file.
+    exec javac FIRSingle.java
+
+    # Generate the code using singleClass compilation mode.
+    exec java ptolemy.copernicus.c.JavaToC $classpath \
+            -singleClass FIRSingle
+
+    # NOTE: JavaToC expects the class file to be converted (in this case
+    # FIRSingle.class) to be in the directory from which it is invoked. It
+    # outputs the generated code files to this directory.  However, here
+    # FIRSingle.class is in c/test/ whereas we want the generated code to
+    # go to c/test/FIRSingle.out/ . We solve this by automatically moving
+    # the generated files to the FIRSingle.out directory after they are
+    # created. A better method to solve this might exist.
+
+    # Move the generated files to the FIRSingle.out directory.
+    foreach fileName {FIRSingle.c FIRSingle.h FIRSingle_i.h} {
+        if ([file readable $fileName]) {
+            exec mv $fileName FIRSingle.out/$fileName
+        }
+    }
+
+
+    cd FIRSingle.out
+
+    # Generate the required .o files.
+    exec gcc -c -I ../../runtime FIRSingle.c
+    exec gcc -c -I ../../runtime -I . ../FIRSingleMain.c
+    exec gcc -c ../../runtime/pccg_runtime.c
+
+    # Link the .o files into the executable.
+    # We use the double quotes so that we can use the * wildcard.
+    exec "gcc -o firSingle *.o"
+
+    # Run the executable.
+    exec firSingle
+
+
+} {11.000000
+4.000000
+9.000000
+0.000000
+0.000000
+0.000000
+0.000000
+0.000000
+0.000000
+0.000000
+}
+
