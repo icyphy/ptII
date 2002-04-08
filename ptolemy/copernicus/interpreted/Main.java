f76cb450a7c14a11009b0f03480c549cdd4dd38d
cxh <cxh>
2002-04-08 19:12:54 +0000

    Added interpreted style of code generator

---
+++ ptolemy/copernicus/interpreted/Main.java
@@ -0,0 +1,126 @@
+/* Main for interpreted generation, that is no code generation at all, just save the model as a .xml file
+
+ Copyright (c) 2001-2002 The Regents of the University of California.
+ All rights reserved.
+ Permission is hereby granted, without written agreement and without
+ license or royalty fees, to use, copy, modify, and distribute this
+ software and its documentation for any purpose, provided that the above
+ copyright notice and the following two paragraphs appear in all copies
+ of this software.
+
+ IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+ FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+ ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+ THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+ SUCH DAMAGE.
+
+ THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+ INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+ PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+ CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ ENHANCEMENTS, OR MODIFICATIONS.
+
+                                        PT_COPYRIGHT_VERSION_2
+                                        COPYRIGHTENDKEY
+@ProposedRating Red (cxh@eecs.berkeley.edu)
+@AcceptedRating Red (cxh@eecs.berkeley.edu)
+*/
+
+package ptolemy.copernicus.interpreted;
+
+import ptolemy.actor.CompositeActor;
+import ptolemy.copernicus.kernel.KernelMain;
+import ptolemy.kernel.util.IllegalActionException;
+import ptolemy.kernel.util.NameDuplicationException;
+import ptolemy.kernel.util.NamedObj;
+
+import soot.*;
+import soot.jimple.*;
+import soot.toolkits.scalar.*;
+import soot.util.*;
+import soot.toolkits.graph.*;
+
+//////////////////////////////////////////////////////////////////////////
+//// Main
+/**
+Read in a MoML model and generate a .xml file
+that will run the model as in standard interpreted mode.
+
+@author Christopher Hylands
+@version $Id$
+@since Ptolemy II 2.0
+*/
+public class Main extends KernelMain {
+
+    /** Read in a MoML model.
+     *  @params args The first element of the array is the MoML class
+     *  name or file name, subsequent optional arguments are Soot
+     *  command line options, see the superclass documentation for details.
+     *  @exception IllegalActionException If the model cannot be parsed.
+     */
+    public Main(String [] args) throws IllegalActionException {
+	// args[0] contains the MoML class name.
+	super(args[0]);
+    }
+
+    ///////////////////////////////////////////////////////////////////
+    ////                         public methods                    ////
+
+
+    /** Add transforms to the Scene.
+     */
+    public void addTransforms() {
+	super.addTransforms();
+
+	// Generate the interpreted files in outDir
+        Scene.v().getPack("wjtp").add(new Transform("wjtp.interpretedWriter",
+                InterpretedWriter.v(_toplevel)));
+    }
+
+    /** Read in a MoML model, generate java files
+     *  @exception IllegalActionException If the model cannot be parsed.
+     *  @exception NameDuplicationException If the name of the
+     *  model cannot be changed to a Java identifier String.
+     */
+    public static void main(String[] args)
+            throws IllegalActionException, NameDuplicationException {
+
+        long startTime = System.currentTimeMillis();
+
+	Main main = new Main(args);
+
+	// Parse the model.
+	CompositeActor toplevel = main.readInModel(args[0]);
+
+	// Create instance classes for the actors.
+	main.initialize(toplevel);
+
+	// Add Transforms to the Scene.
+	main.addTransforms();
+
+	main.generateCode(args);
+
+        // Print out memory usage info
+	System.out.println(ptolemy.actor.Manager.timeAndMemory(startTime));
+
+        //WatchDogTimer.v().cancel();
+
+        // For some reason, we need to call exit here, perhaps because
+        // the WatchDog timer thread is still running in the background?
+        System.exit(0);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
