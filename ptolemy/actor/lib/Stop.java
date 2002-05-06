02f7a46172b0de34dd033e2d56710ec0c7603262
eal <eal>
2002-05-06 21:58:49 +0000

    better documention and more tests for Stop

--- ptolemy/actor/lib/Stop.java
+++ ptolemy/actor/lib/Stop.java
@@ -40,8 +40,50 @@ import ptolemy.kernel.util.*;
 /**
 An actor that stops a model executing when it receives a true token
 on any input channel. This is accomplished by calling stop() on the
-director.  When exactly this stops the execution depends on the domain.
-The inputs are read and checked in the postfire() method only.
+director, which for some directors causes the director to return
+<i>false</i> when its postfire() method is called, and by returning
+<i>false</i> from the postfire() method of this actor, which for
+some other directors causes execution to stop.  A return value
+of <i>false</i> from postfire() indicates to the
+enclosing context that an actor should not be invoked again.
+<p>
+This actor reads inputs in its postfire() method, and if a true-valued
+input is found, then the director's stop() method is called.
+For most directors, this results in a call to stopFire() for each
+actor, followed by the setting of a flag that indicates that the
+next call to postfire() should return false.
+<p>
+When exactly this stops the execution depends on the domain.
+For example, in DE, if an event with time stamp <i>T</i> and
+value <i>true</i> arrives at this actor, then the current
+iteration will be concluded, and then the model will halt.
+Concluding the current iteration means processing all events
+in the event queue with time stamp <i>T</i>. Thus, it is possible
+for actors to be invoked after this one is invoked with a <i>true</i>
+input.
+<p>
+In SDF, if this actor receives <i>true</i>, then the current
+iteration is concluded and then execution is stopped.  The
+SDF director reacts to this actor returning <i>false</i> from
+postfire(), which indicates that this actor does not wish to
+be fired again.  In SDF, if any single actor does not wish to
+be fired again, then none of the actors are fired again.
+Because of the static scheduling, it would not make sense to
+continue execution with only some of the actors being active.
+<p>
+In PN, where each actor has its own thread, the actors continue
+executing until their next attempt to read an input or write an
+output, at which point the thread is stopped. When all actor threads
+have stopped, the PN director concludes, returns from fire(),
+and returns <i>false</i> in postfire(). <b>NOTE</b>:
+<i>This is not the best way to stop a PN model!</i> 
+This mechanism is nondeterministic in the sense that there is
+no way to control exactly what data is produced or consumed on
+the connections before the model stops.  To stop a PN model,
+it is better to design the model so that all actors are starved
+of data when the model is to stop.  The director will detect
+this starvation, and halt the model.  Nonetheless, if
+the nondeterminism is acceptable, this actor can be used.
 
 @author Edward A. Lee
 @version $Id$
