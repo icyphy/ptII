e206cc0394b4fce063e6697db7cc35c3a4711092
cxh <cxh>
2002-05-09 17:50:29 +0000

    BYTE -> UNSIGNED_BYTE

--- ptolemy/copernicus/kernel/PtolemyUtilities.java
+++ ptolemy/copernicus/kernel/PtolemyUtilities.java
@@ -1138,7 +1138,7 @@ public class PtolemyUtilities {
         booleanTypeField = baseTypeClass.getFieldByName("BOOLEAN");
         booleanMatrixTypeField =
             baseTypeClass.getFieldByName("BOOLEAN_MATRIX");
-        byteTypeField = baseTypeClass.getFieldByName("BYTE");
+        byteTypeField = baseTypeClass.getFieldByName("UNSIGNED_BYTE");
         complexTypeField = baseTypeClass.getFieldByName("COMPLEX");
         complexMatrixTypeField =
             baseTypeClass.getFieldByName("COMPLEX_MATRIX");
