81f422c857012cb035d063be10144313f0ec5e98
cxh <cxh>
2002-05-14 15:57:27 +0000

    Fixed @AcceptedRating

--- ptolemy/actor/lib/OrderedMerge.java
+++ ptolemy/actor/lib/OrderedMerge.java
@@ -25,7 +25,7 @@
                                         COPYRIGHTENDKEY
 
 @ProposedRating Red (eal@eecs.berkeley.edu)
-@AcceptedRating Red (yuhong@eecs.berkeley.edu)
+@AcceptedRating Red (eal@eecs.berkeley.edu)
 */
 
 package ptolemy.actor.lib;
