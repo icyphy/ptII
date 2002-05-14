97242e721b5f5e1b63de11a383cb7e8de266df64
cxh <cxh>
2002-05-14 23:00:18 +0000

    (Elaine Cheong): Cleaned up import statements

--- ptolemy/domains/ct/demo/StickyMasses/StickyMasses.java
+++ ptolemy/domains/ct/demo/StickyMasses/StickyMasses.java
@@ -30,23 +30,30 @@
 
 package ptolemy.domains.ct.demo.StickyMasses;
 
-import java.awt.BorderLayout;
-import java.awt.event.*;
-
-import ptolemy.domains.fsm.kernel.*;
-import ptolemy.domains.ct.kernel.*;
-import ptolemy.domains.ct.lib.*;
-import ptolemy.kernel.*;
-import ptolemy.kernel.util.*;
-import ptolemy.gui.Query;
-import ptolemy.gui.QueryListener;
-import ptolemy.actor.lib.*;
+import ptolemy.actor.TypedCompositeActor;
+import ptolemy.actor.TypedIOPort;
+import ptolemy.actor.TypedIORelation;
+import ptolemy.actor.lib.AddSubtract;
+import ptolemy.actor.lib.Expression;
+import ptolemy.actor.lib.Scale;
 import ptolemy.actor.lib.gui.TimedPlotter;
-import ptolemy.actor.*;
-import ptolemy.data.*;
-import ptolemy.data.type.BaseType;
+import ptolemy.data.DoubleToken;
+import ptolemy.data.StringToken;
 import ptolemy.data.expr.Parameter;
-import java.util.Enumeration;
+import ptolemy.data.type.BaseType;
+import ptolemy.domains.ct.kernel.CTCompositeActor;
+import ptolemy.domains.ct.kernel.CTEmbeddedDirector;
+import ptolemy.domains.ct.kernel.CTMultiSolverDirector;
+import ptolemy.domains.ct.lib.Integrator;
+import ptolemy.domains.ct.lib.ZeroCrossingDetector;
+import ptolemy.domains.fsm.kernel.FSMActor;
+import ptolemy.domains.fsm.kernel.HSDirector;
+import ptolemy.domains.fsm.kernel.State;
+import ptolemy.domains.fsm.kernel.Transition;
+import ptolemy.kernel.Relation;
+import ptolemy.kernel.util.IllegalActionException;
+import ptolemy.kernel.util.NameDuplicationException;
+import ptolemy.kernel.util.Workspace;
 import ptolemy.plot.Plot;
 
 //////////////////////////////////////////////////////////
