8b690d3c67b3be45d2ee9b428fe6c03c7d80c7f7
cxh <cxh>
2002-06-14 15:13:26 +0000

    Fixed imporst

--- ptolemy/plot/EditablePlot.java
+++ ptolemy/plot/EditablePlot.java
@@ -32,7 +32,11 @@ package ptolemy.plot;
 
 import java.awt.Color;
 import java.awt.Graphics;
-import java.awt.event.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 import java.util.Vector;
 import java.util.Stack;
 import java.util.Enumeration;
