ac181f1b0f520338b716ee77269dec02cab899c8
cxh <cxh>
2002-07-12 04:46:59 +0000

    Modified directory where jnlp goes

--- mk/jnlp.mk
+++ mk/jnlp.mk
@@ -421,7 +421,7 @@ jnlp_verify:
 	done;
 
 # Update a location with the files necessary to download
-DIST_BASE = ptolemyII/ptII2.0/jnlp
+DIST_BASE = ptolemyII/ptII2.0/jnlp-2.0.1
 DIST_DIR = /vol/ptolemy/pt0/ptweb/$(DIST_BASE)
 DIST_URL = http://ptolemy.eecs.berkeley.edu/$(DIST_BASE)
 OTHER_FILES_TO_BE_DISTED = doc/img/PtolemyIISmall.gif
