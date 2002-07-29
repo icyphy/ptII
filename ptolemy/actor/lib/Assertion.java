8e31fe94ab48f0ae6eef61c235f6d8b8f2e05dad
hyzheng <hyzheng>
2002-07-29 22:50:44 +0000

    A threshold parameter is added to adjust the step size of integrator.

--- ptolemy/actor/lib/Assertion.java
+++ ptolemy/actor/lib/Assertion.java
@@ -42,6 +42,8 @@ import ptolemy.data.Token;
 import ptolemy.data.type.BaseType;
 import ptolemy.data.expr.Parameter;
 import ptolemy.data.expr.Variable;
+import ptolemy.domains.ct.kernel.CTDirector;
+import ptolemy.domains.ct.kernel.CTStepSizeControlActor;
 import ptolemy.kernel.CompositeEntity;
 import ptolemy.kernel.Entity;
 import ptolemy.kernel.Port;
@@ -49,63 +51,19 @@ import ptolemy.kernel.util.*;
 
 import java.math.BigDecimal;
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 
 //////////////////////////////////////////////////////////////////////////
 //// Assertion
 /**
-On each firing, evaluate an assertion that may include references
-to the inputs, current time, and a count of the firing.  The ports are
-referenced by the variables that have the same name as the port.
-If the evaluation of the assertion fails, an IllegalActionException will
-be thrown at run time.
-To use this class, instantiate it, then add ports (instances of TypedIOPort).
-In vergil, you can add ports by right clicking on the icon and selecting
-"Configure Ports".  In MoML you can add ports by just including ports
-of class TypedIOPort, set to be inputs, as in the following example:
-<p>
-<pre>
-   &lt;entity name="assertion" class="ptolemy.actor.lib.Assertion"&gt;
-      &lt;port name="in" class="ptolemy.actor.TypedIOPort"&gt;
-          &lt;property name="input"/&gt;
-      &lt;/port&gt;
-   &lt;/entity&gt;
-</pre>
-<p>
-The type is polymorphic.
-<p>
-The <i>assertion</i> parameter specifies an assertion that can
-refer to the inputs by name.  By default, the assertion
-is empty, and attempting
-to execute the actor without setting it triggers an exception.
-<p>
-The assertion language understood by this actor is the same as
-<a href="../../../../assertion.htm">that used to set any parameter value</a>,
-with the exception that
-the assertions evaluated by this actor can refer to the values
-of inputs, and to the current
-time by the variable name "time", and to the current iteration count
-by the variable named "iteration."
-<p>
-NOTE: Multiports are not supported. Also, if name duplications occur,
-for example if a parameter and a port have the same name, then
-the results are unpredictable.  They will depend on the order
-in which things are defined, which may not be the same in the
-constructor as in the clone method.  This class attempts to
-detect name duplications and throw an exception.  Furthermore, the
-values of the ports are given during the last execution.  It is possible
-to reconnect the ports so that their types change, and enter a legal
-assertion (given those types) that is not accepted by this actor.
-Lastly, the connection
-between ports and the assertion can be easily broken.  For example,
-removing a port that the assertion depends on.
 
 @author Haiyang Zheng
 @version $Id$
 @since Ptolemy II 2.0
 */
 
-public class Assertion extends TypedAtomicActor {
+public class Assertion extends TypedAtomicActor implements CTStepSizeControlActor {
 
     /** Construct an actor with the given container and name.
      *  @param container The container.
@@ -116,17 +74,18 @@ public class Assertion extends TypedAtomicActor {
      *   actor with this name.
      */
     public Assertion(CompositeEntity container, String name)
-            throws NameDuplicationException, IllegalActionException  {
+	throws NameDuplicationException, IllegalActionException  {
         super(container, name);
 
         assertion = new Parameter(this, "assertion");
+	threshold = new Parameter(this, "threshold");
 
         _time = new Variable(this, "time", new DoubleToken(0.0));
         _iteration = new Variable(this, "iteration", new IntToken(1));
 
         _errorTolerance = (double)1e-4;
         errorTolerance = new Parameter(this, "errorTolerance",
-                new DoubleToken(_errorTolerance));
+				       new DoubleToken(_errorTolerance));
 
     }
 
@@ -138,6 +97,7 @@ public class Assertion extends TypedAtomicActor {
      *  the inputs.
      */
     public Parameter assertion;
+    public Parameter threshold;
 
     /** The parameter of error tolerance of type double. By default,
      *  it contains a DoubleToken of 1e-4.
@@ -153,13 +113,12 @@ public class Assertion extends TypedAtomicActor {
      *  @exception IllegalActionException If the attribute change failed.
      */
     public void attributeTypeChanged(Attribute attribute)
-            throws IllegalActionException {
+	throws IllegalActionException {
         if (attribute == errorTolerance) {
-	    double p = ((DoubleToken)errorTolerance.getToken()
-			).doubleValue();
+	    double p = ((DoubleToken)errorTolerance.getToken()).doubleValue();
 	    if (p <= 0) {
 		throw new IllegalActionException(this,
-			"Error tolerance must be greater than 0.");
+						 "Error tolerance must be greater than 0.");
 	    }
 	    _errorTolerance = p;
 	}
@@ -174,7 +133,7 @@ public class Assertion extends TypedAtomicActor {
      */
 
     public Object clone(Workspace workspace)
- 	    throws CloneNotSupportedException {
+	throws CloneNotSupportedException {
         Assertion newObject = (Assertion)super.clone(workspace);
         newObject._iterationCount = 1;
         newObject._time = (Variable)newObject.getAttribute("time");
@@ -189,6 +148,67 @@ public class Assertion extends TypedAtomicActor {
      *   yields an incompatible type, or if there is no director.
      */
     public void fire() throws IllegalActionException {
+	// consume the input.
+	// The assertion actor always has only one input port.
+	TypedIOPort input = (TypedIOPort) inputPortList().get(0);
+	_thisInput = ((DoubleToken) input.get(0)).doubleValue();
+        _threshold = ((DoubleToken) threshold.getToken()).doubleValue();
+		
+	if (_debugging)
+	    _debug(getFullName() + " consuming input Token" + _thisInput);
+    }
+
+    /** Return true if this step does not cross the threshold.
+     *  The current trigger
+     *  token will be compared to the previous trigger token. If they
+     *  cross the level threshold, this step is not accurate.
+     *  A special case is taken care so that if the previous trigger
+     *  and the current trigger both equal to the level value,
+     *  then no new event is
+     *  detected. If this step crosses the level threshold,
+     *  then the refined integration
+     *  step size is computed by linear interpolation.
+     *  If this is the first iteration after initialize() is called,
+     *  then always return false, since there is no history to compare with.
+     *  @return True if the trigger input in this integration step
+     *          does not cross the level threshold.
+     */
+    public boolean isThisStepAccurate() {
+	if (_first) {
+            _first = false;
+            return true;
+        }
+        if (_debugging) {
+            _debug(this.getFullName() + " This input " + _thisInput);
+            _debug(this.getFullName() + " The last input " + _lastInput);
+        }
+        if (Math.abs(_thisInput - _threshold) < _errorTolerance) {
+            if (_enabled) {
+                _enabled = false;
+            }
+            _eventMissed = false;
+            return true;
+        } else {
+            if (!_enabled) {  // if last step is a level, always accurate.
+                _enabled = true;
+            } else {
+                if ((_lastInput - _threshold) * (_thisInput - _threshold)
+		    < 0.0) {
+
+                    CTDirector dir = (CTDirector)getDirector();
+                    _eventMissed = true;
+                    // The refined step size is a linear interpolation.
+                    _refineStep = (Math.abs(_lastInput - _threshold)
+				   *dir.getCurrentStepSize())
+                        /Math.abs(_thisInput-_lastInput);
+                    if (_debugging) _debug(getFullName() +
+					   " Refined step at" +  _refineStep);
+                    return false;
+                }
+            }
+            _eventMissed = false;
+            return true;
+        }
     }
 
     /** Initialize the iteration count to 1.
@@ -196,6 +216,7 @@ public class Assertion extends TypedAtomicActor {
      */
     public void initialize() throws IllegalActionException {
         super.initialize();
+        _eventMissed = false;
         _iterationCount = 1;
         _iteration.setToken(new IntToken(_iterationCount));
     }
@@ -204,6 +225,8 @@ public class Assertion extends TypedAtomicActor {
      *  @exception IllegalActionException If the superclass throws it.
      */
     public boolean postfire() throws IllegalActionException {
+
+	_lastInput = _thisInput;
 
         Director director = getDirector();
         if (director == null) {
@@ -253,8 +276,8 @@ public class Assertion extends TypedAtomicActor {
 
         if (!result.booleanValue()) {
             throw new IllegalActionException(this,
-                    "Assertion fails! " +
-                    assertion.getExpression());
+					     "Assertion fails! " +
+					     assertion.getExpression());
         }
 
         _iterationCount++;
@@ -280,6 +303,25 @@ public class Assertion extends TypedAtomicActor {
         }
     }
 
+    /** Return the maximum Double, since this actor does not predict
+     *  step size.
+     *  @return java.Double.MAX_VALUE.
+     */
+    public double predictedStepSize() {
+        return java.lang.Double.MAX_VALUE;
+    }
+
+    /** Return the refined step size if there is a missed event,
+     *  otherwise return the current step size.
+     *  @return The refined step size.
+     */
+    public double refinedStepSize() {
+        if (_eventMissed) {
+            return _refineStep;
+        }
+        return ((CTDirector)getDirector()).getCurrentStepSize();
+    }
+
     ///////////////////////////////////////////////////////////////////
     ////                         protected methods                 ////
 
@@ -295,11 +337,11 @@ public class Assertion extends TypedAtomicActor {
      *   name already in the entity.
      */
     protected void _addPort(Port port)
-            throws IllegalActionException, NameDuplicationException {
+	throws IllegalActionException, NameDuplicationException {
         if (!(port instanceof TypedIOPort)) {
             throw new IllegalActionException(this,
-                    "Cannot add an input port that is not a TypedIOPort: "
-                    + port.getName());
+					     "Cannot add an input port that is not a TypedIOPort: "
+					     + port.getName());
         }
         super._addPort(port);
         String portName = port.getName();
@@ -313,9 +355,9 @@ public class Assertion extends TypedAtomicActor {
             Variable variable =
                 new Variable(this, portName, new DoubleToken(1.0));
         } else if ((there instanceof Parameter)
-                || !(there instanceof Variable)) {
+		   || !(there instanceof Variable)) {
             throw new IllegalActionException(this, "Port name collides with"
-                    + " another attribute name: " + portName);
+					     + " another attribute name: " + portName);
         }
         // NOTE: We assume that if there is already a variable with
         // this name then that is the variable we are intended to use.
@@ -345,6 +387,23 @@ public class Assertion extends TypedAtomicActor {
 
     // Parameter, the error tolerance, local copy
     private double _errorTolerance;
+
+    private double _thisInput;
+    private double _lastInput;
+
+    private double _threshold;
+
+    // flag indicating if the event detection is enable for this step
+    private boolean _enabled;
+
+    // flag indicating if this is the first iteration in the execution,
+    private boolean _first = true;
+
+    // flag for indicating a missed event
+    private boolean _eventMissed = false;
+
+    // refined step size.
+    private double _refineStep;
 
     private Variable _time;
     private Variable _iteration;
