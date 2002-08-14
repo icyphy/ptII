d3a09ae0d6df7a32b9f4befe78ad8f0380a2811e
cxh <cxh>
2002-08-14 02:35:36 +0000

    Use a platform independent file separator to execute the model

--- ptolemy/copernicus/c/test/Simple.tcl
+++ ptolemy/copernicus/c/test/Simple.tcl
@@ -127,7 +127,9 @@ test Simple-1.1 {Generate all required files for Simple.java} {
     # Run the automatically generated executible.
     cd $outputDir
 
-    exec ./$exeFile
+    # The nightly build does not have . in the path, so we use ./ here.
+    set exeFile ".[java::call System getProperty file.separator]$exeFile"
+    set output [exec $exeFile]
 
     list \
         [file readable $exeFile]
