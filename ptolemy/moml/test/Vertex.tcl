dbe97ac05214681a46f156f601057ff2cc93cb8e
cxh <cxh>
2002-08-16 16:33:12 +0000

    Vertex.tcl

---
+++ ptolemy/moml/test/Vertex.tcl
@@ -0,0 +1,59 @@
+# Tests for the Vertex class
+#
+# @Author: Christopher Hylands, Based on Location.tcl by Steve Neuendorffer
+#
+# @Version: $Id$
+#
+# @Copyright (c) 1997-2002 The Regents of the University of California.
+# All rights reserved.
+#
+# Permission is hereby granted, without written agreement and without
+# license or royalty fees, to use, copy, modify, and distribute this
+# software and its documentation for any purpose, provided that the
+# above copyright notice and the following two paragraphs appear in all
+# copies of this software.
+#
+# IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+# FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+# ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+# THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+#
+# THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+# INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+# PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+# CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+# ENHANCEMENTS, OR MODIFICATIONS.
+#
+# 						PT_COPYRIGHT_VERSION_2
+# 						COPYRIGHTENDKEY
+#######################################################################
+
+# Load the test definitions.
+if {[string compare test [info procs test]] == 1} then {
+    source testDefs.tcl
+} {}
+
+# Uncomment this to get a full report, or set in your Tcl shell window.
+# set VERBOSE 1
+
+######################################################################
+####
+#
+
+set header {<?xml version="1.0" standalone="no"?>
+<!DOCTYPE entity PUBLIC "-//UC Berkeley//DTD MoML 1//EN"
+    "http://ptolemy.eecs.berkeley.edu/xml/dtd/MoML_1.dtd">}
+
+######################################################################
+####
+#
+test Vertex-1.1 {Call workspace constructor, exportMoML and toString } {
+    set w0 [java::new ptolemy.kernel.util.Workspace "myWorkspace"]
+    set v1 [java::new ptolemy.moml.Vertex $w0]
+    set output [java::new java.io.StringWriter]
+    $v1 exportMoML $output 1
+    list [$v1 toString] [$output toString]
+} {{(ptolemy.moml.Vertex, Location = null)} {    <vertex name="">
+    </vertex>}
