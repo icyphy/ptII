4a95cca1ce7631422e870cd726a5394af2ab8a5f
cxh <cxh>
2002-08-19 23:36:52 +0000

    Beefed up coverage

--- ptolemy/kernel/util/test/ConfigurableAttribute.tcl
+++ ptolemy/kernel/util/test/ConfigurableAttribute.tcl
@@ -1,6 +1,6 @@
 # Tests for the ConfigurableAttribute class
 #
-# @Author: Steve Neuendorffer and Edward A. Lee
+# @Author: Steve Neuendorffer, Edward A. Lee, Christopher Hylands
 #
 # @Version: $Id$
 #
@@ -68,6 +68,92 @@ test ConfigurableAttribute-1.1 {test export moml.} {
 ######################################################################
 ####
 #
-test ConfigurableAttribute-1.2 {test value method.} {
+test ConfigurableAttribute-1.5 {test value method.} {
+    # Uses Test 1.1 above 
     $p1 value
 } {My Test String}
+
+
+######################################################################
+####
+#
+test ConfigurableAttribute-1.6 {test export moml with null text} {
+    set n0 [java::new ptolemy.kernel.util.NamedObj]
+    $n0 setName N0
+    set p1 [java::new ptolemy.kernel.util.ConfigurableAttribute $n0 P1]
+    $p1 configure [java::null] "NotASource.xml" [java::null]
+    $n0 exportMoML
+} {<?xml version="1.0" standalone="no"?>
+<!DOCTYPE entity PUBLIC "-//UC Berkeley//DTD MoML 1//EN"
+    "http://ptolemy.eecs.berkeley.edu/xml/dtd/MoML_1.dtd">
+<entity name="N0" class="ptolemy.kernel.util.NamedObj">
+    <property name="_createdBy" class="ptolemy.kernel.util.VersionAttribute" value="2.1-devel">
+    </property>
+    <property name="P1" class="ptolemy.kernel.util.ConfigurableAttribute">
+        <configure source="NotASource.xml"/>
+    </property>
+</entity>
+}
+
+######################################################################
+####
+#
+test ConfigurableAttribute-2.1 {addValueListener} {
+    set n [java::new ptolemy.kernel.util.NamedObj "my NamedObj"]
+    set s2_1 [java::new ptolemy.kernel.util.ConfigurableAttribute $n "my ConfigurableAttribute"]
+    set stream [java::new java.io.ByteArrayOutputStream]
+    set printStream [java::new \
+            {java.io.PrintStream java.io.OutputStream} $stream]
+    set listener [java::new ptolemy.kernel.util.test.StreamValueListener \
+	    $printStream]
+
+    # Try removing the listener before adding it.
+    $s2_1 removeValueListener $listener
+
+    $s2_1 addValueListener $listener
+
+    # Add the listener twice to get coverage of a basic block.
+    $s2_1 addValueListener $listener
+
+    $s2_1 setExpression "a string"
+
+    # Remove the listener and verify that we are not updating it.
+    $s2_1 removeValueListener $listener
+    $s2_1 setExpression "another string"
+    $printStream flush
+    regsub -all [java::call System getProperty "line.separator"] \
+	        [$stream toString] "\n" output
+    list $output
+} {{ptolemy.kernel.util.ConfigurableAttribute {.my NamedObj.my ConfigurableAttribute} changed, new expression: a string
+}}
+
+
+test ConfigurableAttribute-3.1 {getVisibility, setVisibility, getBase} {
+    set c1 [java::new ptolemy.kernel.util.ConfigurableAttribute]
+    set full [java::field ptolemy.kernel.util.Settable NONE]
+    set r1 [expr {[$c1 getVisibility] == $full}]
+
+    set none [java::field ptolemy.kernel.util.Settable FULL]
+    $c1 setVisibility $none
+
+    list $r1 [expr {[$c1 getVisibility] == $none}] \
+	    [expr {[$c1 getBase] == [java::null]}] 
+} {1 1 1}
+
+test ConfigurableAttribute-4.1 {setExpression, getExpression} {
+    set c1 [java::new ptolemy.kernel.util.ConfigurableAttribute]
+    set s1 [java::new ptolemy.kernel.util.StringAttribute $c1 "_s1"]
+    set s2 [java::new ptolemy.kernel.util.StringAttribute $c1 "_s2"]
+    $c1 configure [java::null] [java::null] {My Test String}
+    set r1 [$c1 getExpression]
+    $c1 setExpression "Another Test String"
+    list $r1 [$c1 getExpression]
+} {{My Test String} {Another Test String}}
+
+
+test ConfigurableAttribute-4.2 {getExpression exception} {
+    set c1 [java::new ptolemy.kernel.util.ConfigurableAttribute]
+    $c1 configure [java::null] "NotAFile" {My Test String}
+    catch {$c1 getExpression} errMsg
+    list $errMsg
+} {{java.net.MalformedURLException: no protocol: NotAFile}}
