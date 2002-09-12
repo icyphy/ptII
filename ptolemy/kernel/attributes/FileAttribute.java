454a84dd9ab37bd91b70c2887c89187500883752
eal <eal>
2002-09-12 17:21:21 +0000

    added file attribute

---
+++ ptolemy/kernel/attributes/FileAttribute.java
@@ -0,0 +1,98 @@
+/* An attribute that specifies a file or URL.
+
+ Copyright (c) 2001-2002 The Regents of the University of California.
+ All rights reserved.
+ Permission is hereby granted, without written agreement and without
+ license or royalty fees, to use, copy, modify, and distribute this
+ software and its documentation for any purpose, provided that the above
+ copyright notice and the following two paragraphs appear in all copies
+ of this software.
+
+ IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+ FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+ ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+ THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+ SUCH DAMAGE.
+
+ THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+ INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+ PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+ CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ ENHANCEMENTS, OR MODIFICATIONS.
+
+                                        PT_COPYRIGHT_VERSION_2
+                                        COPYRIGHTENDKEY
+
+@ProposedRating Yellow (eal@eecs.berkeley.edu)
+@AcceptedRating Red (cxh@eecs.berkeley.edu)
+*/
+
+package ptolemy.kernel.attributes;
+
+import ptolemy.kernel.util.*;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+
+//////////////////////////////////////////////////////////////////////////
+//// FileAttribute
+/**
+This is an attribute that specifies a file or URL.
+
+FIXME: more
+
+<p>
+@author Edward A. Lee
+@version $Id$
+*/
+public class FileAttribute extends StringAttribute {
+
+    /** Construct an attribute with the given name contained by the
+     *  specified container. The container argument must not be null, or a
+     *  NullPointerException will be thrown.  This attribute will use the
+     *  workspace of the container for synchronization and version counts.
+     *  If the name argument is null, then the name is set to the empty
+     *  string. Increment the version of the workspace.
+     *  @param container The container.
+     *  @param name The name of this attribute.
+     *  @exception IllegalActionException If the attribute is not of an
+     *   acceptable class for the container, or if the name contains a period.
+     *  @exception NameDuplicationException If the name coincides with
+     *   an attribute already in the container.
+     */
+    public FileAttribute(NamedObj container, String name)
+            throws IllegalActionException, NameDuplicationException {
+        super(container, name);
+    }
+
+    ///////////////////////////////////////////////////////////////////
+    ////                         public methods                    ////
+
+    /** Return the file as a URL.
+     *  @return A URL.
+     *  @exception IllegalActionException If the file cannot be read.
+     */
+    public URL asURL() throws IllegalActionException {
+        String name = getExpression();
+        if (name == null || name.trim().equals("")) {
+            throw new IllegalActionException(this,
+            "Empty file name.");
+        }
+        File file = new File(name);
+        try {
+            return file.toURL();
+        } catch (MalformedURLException ex) {
+            throw new IllegalActionException(this,
+            "Cannot open file: " + ex.toString());
+        }
+    }
+
+    ///////////////////////////////////////////////////////////////////
+    ////                         public variables                  ////
+
+    ///////////////////////////////////////////////////////////////////
+    ////                         private variables                 ////
+}
