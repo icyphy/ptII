d4cd4f810fc4fa34bdffcae05bec18136ffe75a5
cxh <cxh>
2002-10-16 05:15:35 +0000

    Typo

--- ptolemy/actor/gui/PlotTableauFrame.java
+++ ptolemy/actor/gui/PlotTableauFrame.java
@@ -220,7 +220,7 @@ public class PlotTableauFrame extends TableauFrame {
             // The default on Windows is to open at user.home, which is
             // typically an absurd directory inside the O/S installation.
             // So we use the current directory instead.
-            String cwd = GUIStringUtilites.getProperty("user.dir");
+            String cwd = GUIStringUtilities.getProperty("user.dir");
             if (cwd != null) {
                 fileDialog.setCurrentDirectory(new File(cwd));
             }
