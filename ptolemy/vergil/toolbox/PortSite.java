8dff3a70334a1a403c80f25fdc4368efbb2cc255
cxh <cxh>
2002-11-27 03:31:07 +0000

    Added changes for port site menu choice

--- ptolemy/vergil/toolbox/PortSite.java
+++ ptolemy/vergil/toolbox/PortSite.java
@@ -33,6 +33,7 @@ package ptolemy.vergil.toolbox;
 import diva.canvas.AbstractSite;
 import diva.canvas.Figure;
 import ptolemy.actor.IOPort;
+import ptolemy.kernel.util.StringAttribute;
 
 import java.awt.Rectangle;
 import java.awt.Shape;
@@ -42,9 +43,9 @@ import java.awt.geom.Rectangle2D;
 A site that locates itself on the bounds of a figure's shape, designed
 for placing ports.
 
-@version	$Id$
+@version $Id$
+@author Edward A. Lee
 @since Ptolemy II 2.0
-@author 	Edward A. Lee
 */
 public class PortSite extends AbstractSite {
 
@@ -60,6 +61,13 @@ public class PortSite extends AbstractSite {
         _port = port;
         _number = number;
         _count = count;
+        StringAttribute cardinalAttribute =
+            (StringAttribute)port.getAttribute("_cardinal");
+        if ( cardinalAttribute != null ) {
+           _cardinal = cardinalAttribute.getExpression();
+        }
+        _normal = _getNormal();
+
     }
 
     ///////////////////////////////////////////////////////////////////
@@ -81,17 +89,7 @@ public class PortSite extends AbstractSite {
     /** Get the normal of the site.
      */
     public double getNormal() {
-        if (_port.isInput()) {
-            if (_port.isOutput()) {
-                // Port is both an input and an output.
-                return Math.PI/2;
-            } else {
-                // Port is an input only.
-                return Math.PI;
-            }
-        }
-        // Port is an output only, or neither.
-        return 0.0;
+       return _normal;
     }
 
     /** Get the x-coordinate of the site.
@@ -99,22 +97,44 @@ public class PortSite extends AbstractSite {
     public double getX() {
         Rectangle2D bounds = _parentFigure.getShape().getBounds();
         double x = 0.0;
-        if (_port.isInput() && _port.isOutput()) {
-            // Port is both an input and an output.
-            double offset = bounds.getWidth()/2.0 - (_count/2)*_snap;
-            // If there are an even number of ports, skip the middle
-            // position to get symmetry.
-            boolean skipOne = (_count/2)*2 == _count;
-            if (skipOne && _number >= _count/2) offset += _snap;
-
-            x = bounds.getX() + _snap * _number + offset;
-        } else if (_port.isInput()) {
-            // Port is an input only.
-            x = bounds.getX();
-        } else if (_port.isOutput()) {
-            // Port is an output only.
-            x = bounds.getX() + bounds.getWidth();
+        if (_cardinal == null) {
+           if (_port.isInput() && _port.isOutput()) {
+               // Port is both an input and an output.
+               double offset = bounds.getWidth()/2.0 - (_count/2)*_snap;
+               // If there are an even number of ports, skip the middle
+               // position to get symmetry.
+               boolean skipOne = (_count/2)*2 == _count;
+               if (skipOne && _number >= _count/2) {
+                   offset += _snap;
+               }
+               x = bounds.getX() + _snap * _number + offset;
+           } else if (_port.isInput()) {
+               // Port is an input only.
+               x = bounds.getX();
+           } else if (_port.isOutput()) {
+               // Port is an output only.
+               x = bounds.getX() + bounds.getWidth();
+           }
+        } else {
+           if (_cardinal.equalsIgnoreCase( "WEST" ) ) {
+               // Port is on the left.
+               x = bounds.getX();
+           } else if (_cardinal.equalsIgnoreCase( "EAST" ) ) {
+               // Port is on the right.
+               x = bounds.getX() + bounds.getWidth();
+           } else {
+               // Port is on the top or bottom side.
+               double offset = bounds.getWidth()/2.0 - (_count/2)*_snap;
+               // If there are an even number of ports, skip the middle
+               // position to get symmetry.
+               boolean skipOne = (_count/2)*2 == _count;
+               if (skipOne && _number >= _count/2) {
+                   offset += _snap;
+               }
+               x = bounds.getX() + _snap * _number + offset;
+           }
         }
+
         return x;
     }
 
@@ -123,19 +143,40 @@ public class PortSite extends AbstractSite {
     public double getY() {
         Rectangle2D bounds = _parentFigure.getShape().getBounds();
         double y = 0.0;
-        if (_port.isInput() && _port.isOutput()) {
-            // Port is both an input and an output.
-            y = bounds.getY() + bounds.getHeight();
-        } else if (_port.isInput() || _port.isOutput()) {
-            // Port is an input or output only.
-            double offset = bounds.getHeight()/2.0 - (_count/2)*_snap;
-            // If there are an even number of ports, skip the middle
-            // position to get symmetry.
-            boolean skipOne = (_count/2)*2 == _count;
-            if (skipOne && _number >= _count/2) offset += _snap;
 
-            y = bounds.getY() + _snap * _number + offset;
+        if (_cardinal == null ) {
+           if (_port.isInput() && _port.isOutput()) {
+               // Port is both an input and an output.
+               y = bounds.getY() + bounds.getHeight();
+           } else if (_port.isInput() || _port.isOutput()) {
+               // Port is an input or output only.
+               double offset = bounds.getHeight()/2.0 - (_count/2)*_snap;
+               // If there are an even number of ports, skip the middle
+               // position to get symmetry.
+               boolean skipOne = (_count/2)*2 == _count;
+               if (skipOne && _number >= _count/2) offset += _snap;
+
+               y = bounds.getY() + _snap * _number + offset;
+           }
+        } else {
+           if (_cardinal.equalsIgnoreCase( "SOUTH" ) ) {
+               // Port is on the bottom.
+               y = bounds.getY() + bounds.getHeight();
+           } else if (_cardinal.equalsIgnoreCase( "NORTH" ) ) {
+               // Port is on the top.
+               y = bounds.getY();
+           } else {
+               // Port is on the left or right.
+               double offset = bounds.getHeight()/2.0 - (_count/2)*_snap;
+               // If there are an even number of ports, skip the middle
+               // position to get symmetry.
+               boolean skipOne = (_count/2)*2 == _count;
+               if (skipOne && _number >= _count/2) offset += _snap;
+
+               y = bounds.getY() + _snap * _number + offset;
+           }
         }
+
         return y;
     }
 
@@ -144,7 +185,44 @@ public class PortSite extends AbstractSite {
     }
 
     ///////////////////////////////////////////////////////////////////
+    ////                         private methods                   ////
+
+    /** Get the normal of the site.
+     */
+    private double _getNormal () {
+        if (_cardinal == null ) {
+           if (_port.isInput()) {
+               if (_port.isOutput()) {
+                   // Port is both an input and an output.
+                   return Math.PI/2;
+               } else {
+                   // Port is an input only.
+                   return Math.PI;
+               }
+           }
+           // Port is an output only, or neither.
+           return 0.0;
+        } else {
+           if (_cardinal.equalsIgnoreCase("NORTH") ) {
+              return -Math.PI/2;
+           } else if (_cardinal.equalsIgnoreCase("SOUTH") ) {
+              return Math.PI/2;
+           } else if (_cardinal.equalsIgnoreCase("EAST") ) {
+              return 0.0;
+           } else if (_cardinal.equalsIgnoreCase("WEST") ) {
+              return Math.PI;
+           } else {
+               // somebody misspelled something
+               return Math.PI/2;
+           }
+        }
+    }
+
+    ///////////////////////////////////////////////////////////////////
     ////                         private variables                 ////
+
+    /** The cardinal location string **/
+    private String _cardinal = null;
 
     /** The number of ports of the kind as this one. */
     private int _count;
