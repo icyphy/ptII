782146cd55e8acd036d3a0b943d3a9b037be777e
cxh <cxh>
2003-01-07 16:56:16 +0000

    Updated to 2.2-alpha

--- ptolemy/kernel/attributes/VersionAttribute.java
+++ ptolemy/kernel/attributes/VersionAttribute.java
@@ -276,7 +276,7 @@ public class VersionAttribute
 
     static {
         try {
-            CURRENT_VERSION = new VersionAttribute("2.1-devel-2");
+            CURRENT_VERSION = new VersionAttribute("2.2-alpha");
         } catch (Exception ex) {
             throw new ExceptionInInitializerError(
                     "Failed to create CURRENT_VERSION: "
