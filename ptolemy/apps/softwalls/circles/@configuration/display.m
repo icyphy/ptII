b44b53409e785664c3c41030f659a447b178c250
acataldo <acataldo>
2003-01-08 01:53:34 +0000

    unneeded

--- ptolemy/apps/softwalls/circles/@configuration/display.m
+++
@@ -1,7 +0,0 @@
-function display(c)
-% CONFINGURATION/DISPLAY Display method for polynomial class.
-
-newline = sprintf('\n');
-str = sprintf(' =\n\n    X: %d\n    Y: %d\nTheta: %d', get(c, 'X'), ...
-    get(c, 'Y'), get(c, 'Theta'));
-disp([newline inputname(1) str newline])
--- ptolemy/apps/softwalls/circles/@configuration/double.m
+++
@@ -1,4 +0,0 @@
-function d = double(c)
-% DOUBLE(C) Returns a [x y theta] double vector.
-
-d = [c.x c.y c.theta];
