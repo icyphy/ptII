b8232f44c67c04959fbf324b1831eaec181dbd20
cxh <cxh>
2003-01-14 21:31:18 +0000

    Beefed up transform(Document, String) to look in the classpath. Added toString()

--- ptolemy/util/XSLTUtilities.java
+++ ptolemy/util/XSLTUtilities.java
@@ -44,6 +44,7 @@ import java.net.URL;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Properties;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.transform.Transformer;
@@ -53,6 +54,8 @@ import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamSource;
 import javax.xml.transform.stream.StreamResult;
 
+import org.apache.xalan.serialize.Serializer;
+import org.apache.xalan.serialize.SerializerFactory;
 import org.w3c.dom.Document;
 
 //////////////////////////////////////////////////////////////////////////
@@ -79,43 +82,89 @@ public class XSLTUtilities {
     /** Parse a document.
      * @param filename The file name of the xml file to be read in
      * @return the parsed document.
-     * @exception Thrown if there is a problem with the transformation.
+     * @exception Exception Thrown if there is a problem with the
+     * transformation.
      */
     public static Document parse(String filename) throws Exception {
-        DocumentBuilderFactory factory
-            = DocumentBuilderFactory.newInstance();
+	DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
         DocumentBuilder builder = factory.newDocumentBuilder();
         return builder.parse(new File(filename));
+    }
 
+    /** Given a Document, generate a String.
+     *  @param document The document to be converted to a string.
+     *  @returns A string representation of the Document.
+     *  @exception Exception
+     */
+    public static String toString(Document document) throws Exception {
+	// FIXME: Joern's sample code had this in it, but if we
+	// include it, then we get Provider   not found errors 
+	//String defaultDBFI =
+	//    System.getProperty("javax.xml.parsers.DocumentBuilderFactory");
+	//System.setProperty("javax.xml.parsers.DocumentBuilderFactory",
+	//		   defaultDBFI == null ? "" : defaultDBFI);
+
+        Properties format = org.apache.xalan.templates.OutputProperties
+	    .getDefaultMethodProperties("xml");
+        format.setProperty("indent", "yes");
+        format.setProperty("{http://xml.apache.org/xslt}indent-amount",
+			   "4");
+        Serializer serializer = SerializerFactory.getSerializer(format);
+        OutputStream outputStream = new ByteArrayOutputStream();
+        serializer.setOutputStream(outputStream);
+        serializer.asDOMSerializer().serialize(document);
+        outputStream.close();
+        return outputStream.toString();
     }
 
     /** Transform a document
      * @param inputDocument The Document to be transformed
-     * @param transformFileName The file name of the xsl file to be used
+     * @param xslFileName The file name of the xsl file to be used.
+     * If the file cannot be found, then we look up the file in the classpath.
      * @return a transformed document
      * @exception Thrown if there is a problem with the transformation.
      */
     public static Document transform(Document inputDocument,
-            String transformFileName) throws Exception {
+            String xslFileName) throws Exception {
         TransformerFactory transformerFactory =
             TransformerFactory.newInstance();
-        Transformer transformer = transformerFactory
-            .newTransformer(new StreamSource(transformFileName));
+	Transformer transformer = null;
+	try {
+	    transformer = transformerFactory
+		.newTransformer(new StreamSource(xslFileName));
+	} catch (javax.xml.transform.TransformerConfigurationException ex) {
+	    try {
+                // We might be in the Swing Event thread, so
+                // Thread.currentThread().getContextClassLoader()
+                // .getResource(entry) probably will not work.
+                Class refClass = Class.forName("ptolemy.util.XSLTUtilities");
+                URL entryURL =
+		    refClass.getClassLoader().getResource(xslFileName);
+		if (entryURL != null) {
+		    transformer = transformerFactory
+			.newTransformer(new StreamSource(entryURL.toString()));
+		} else {
+		    throw ex;
+		}
+	    } catch (Exception ex2) {
+		throw ex;
+	    }
+	}
         DOMResult result = new DOMResult();
         transformer.transform(new DOMSource(inputDocument), result);
         return (Document)result.getNode();
     }
 
-    /** Transform a document 
+    /** Transform a document.
      * @param inputDocument The Document to be transformed
-     * @param transformFileNames A list of Strings naming the
-     * xsl files to be applied sequentially
+     * @param xslFileNames A list of Strings naming the
+     * xsl files to be applied sequentially.
      * @return a transformed document
      * @exception Thrown if there is a problem with the transformation.
      */
     public static Document transform(Document inputDocument,
-            List transformFileNames) throws Exception {
-        Iterator fileNames = transformFileNames.iterator();
+            List xslFileNames) throws Exception {
+        Iterator fileNames = xslFileNames.iterator();
         while (fileNames.hasNext()) {
             String fileName = (String) fileNames.next();
             inputDocument = transform(inputDocument, fileName);
