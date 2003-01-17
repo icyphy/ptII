e66fcfc7a7f0b00c779e727f5bae221fddd95523
cxh <cxh>
2003-01-17 21:32:04 +0000

    Typo

--- ptolemy/actor/lib/io/ExpressionWriter.java
+++ ptolemy/actor/lib/io/ExpressionWriter.java
@@ -60,7 +60,7 @@ without asking.  If <i>true</i> (the default), then if the file
 exists, then this actor will ask for confirmation before overwriting.
 
 @see ExpressionReader
-@see ptolemy.kernel.attribute.FileAttribute
+@see ptolemy.kernel.attribute.FileAttribute
 @see LineWriter
 
 @author Edward A. Lee
