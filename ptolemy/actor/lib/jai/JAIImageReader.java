27babfb2a39a3fed24cfabb64d6761e905f09d37
jiahau <jiahau>
2003-01-27 22:29:45 +0000

    *** empty log message ***

--- ptolemy/actor/lib/jai/JAIImageReader.java
+++ ptolemy/actor/lib/jai/JAIImageReader.java
@@ -1,5 +1,5 @@
-/* An actor that produces a javax.media.jai.RenderedOp from an
-   image file specified as a URL.
+/* An actor that produces a JAIImageToken from an image file specified 
+as a URL.
 
 @Copyright (c) 2001-2003 The Regents of the University of California.
 All rights reserved.
@@ -35,6 +35,8 @@ import java.io.IOException;
 
 import javax.media.jai.JAI;
 import javax.media.jai.RenderedOp;
+
+import ptolemy.actor.Director;
 import ptolemy.actor.lib.Source;
 import ptolemy.data.ObjectToken;
 import ptolemy.data.StringToken;
@@ -46,8 +48,8 @@ import ptolemy.kernel.util.*;
 //////////////////////////////////////////////////////////////////////////
 //// JAIImageReader
 /**
-Load an image and create a RendredOp from it.  The file to be loaded is
-specified as a relative URL from the base URL path.  Usually the base
+Load an image and create a JAIImageToken from it.  The file to be loaded
+is specified as a relative URL from the base URL path.  Usually the base
 path should be set to the root ptolemy classpath.  Supports BMP, FPX,
 GIF, JPEG, PNG, PBM, PGM, PPM, and TIFF file formats.
 
@@ -72,7 +74,7 @@ public class JAIImageReader extends Source {
         imageURLTemplate = new Parameter(this, "imageURLTemplate",
             new StringToken("ptolemy/actor/lib/jmf/"
                             + "goldhill.gif"));
-
+        imageURLTemplate.setTypeEquals(BaseType.STRING);
 }
 
     ///////////////////////////////////////////////////////////////////
@@ -82,42 +84,61 @@ public class JAIImageReader extends Source {
     public Parameter imageURLTemplate;
 
     ///////////////////////////////////////////////////////////////////
-    ////                         public variables                  ////
-
-    /** The RenderedOp being created from the URL template */
-    public RenderedOp image1;
-
-    ///////////////////////////////////////////////////////////////////
     ////                         public methods                    ////
 
-    /** Fire this actor.
-     *  Output the RenderedOp.
-     *  @exception IllegalActionException If a contained method throws it,
-     *   or if a token is received that contains a null image.
-     */
-    public void fire() throws IllegalActionException {
-        super.fire();
-        output.send(0, new JAIImageToken(image1));
-        //output.send(0, new ObjectToken(image1));
-    }
-
-    /** Initialize this actor.
-     *  Read in the image file.
-     *  @exception IllegalActionException If a contained method throws it.
+    /** An attempt is made at loading the file.  If this is successful,
+     *  then at time 0.0, request firing.
+     *  @exception IllegalActionException If the filename is null, if
+     *  the filename doesn't produce a loadable image, or if there is
+     *  no director.
      */
     public void initialize() throws IllegalActionException {
         super.initialize();
-
-        StringToken urlToken = (StringToken)imageURLTemplate.getToken();
-        String fileRoot = urlToken.stringValue();
-
-        FileSeekableStream stream = null;
-        try {
-            stream = new FileSeekableStream(fileRoot);
+        _urlToken = (StringToken)imageURLTemplate.getToken();
+        if (_urlToken == null) {
+            throw new IllegalActionException("URLToken was null");
+        } else {
+            _fileRoot = _urlToken.stringValue();
+            try {
+                _stream = new FileSeekableStream(_fileRoot);
+            } catch (IOException error) {
+                throw new IllegalActionException("Unable to load file");
+            }
+            _outputtedImage = JAI.create("stream", _stream);
         }
-        catch (IOException e) {
+        Director director = getDirector();
+        if (director != null) {
+            director.fireAt(this, 0.0);
+        } else {
+            throw new IllegalActionException(this, "No director.");
         }
-        image1 = JAI.create("stream", stream);
     }
-}
+    
+    /** If the current time is 0.0, then produce a JAIImageToken 
+     *  containing the image.
+     *  @exception IllegalActionException If there is no director.
+     */
+    public boolean postfire() throws IllegalActionException {
+        if (getDirector().getCurrentTime() == 0.0) {
+            output.send(0, new JAIImageToken(_outputtedImage));
+        }
+        return super.postfire();
+    }
+    
+    ///////////////////////////////////////////////////////////////////
+    ////                         private variables                 ////
+    
+    /** The String that specifes where the file is located. */
+    private String _fileRoot;    
 
+    /** The RenderedOp created by JAI from the stream.  This is then
+     *  encapsulated by a JAIImageToken.
+     */
+    private RenderedOp _outputtedImage;
+
+    /** A stream which JAI uses to create RenderedOp's */
+    private FileSeekableStream _stream;
+    
+    /** The StringToken that contains the file's URL */
+    private StringToken _urlToken;
+}
