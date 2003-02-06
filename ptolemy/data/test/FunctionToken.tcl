59d3f5298e265397fb825e0426730a98b4ca5bfb
neuendor <neuendor>
2003-02-06 22:12:40 +0000

    *** empty log message ***

---
+++ ptolemy/data/test/FunctionToken.tcl
@@ -0,0 +1,95 @@
+# Tests for the FunctionToken class
+#
+# @Author: Yuhong Xiong
+#
+# @Version $Id$
+#
+# @Copyright (c) 1997-2003 The Regents of the University of California.
+# All rights reserved.
+# 
+# Permission is hereby granted, without written agreement and without
+# license or royalty fees, to use, copy, modify, and distribute this
+# software and its documentation for any purpose, provided that the
+# above copyright notice and the following two paragraphs appear in all
+# copies of this software.
+# 
+# IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+# FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+# ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+# THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+# 
+# THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+# INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+# PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+# CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+# ENHANCEMENTS, OR MODIFICATIONS.
+# 
+# 						PT_COPYRIGHT_VERSION_2
+# 						COPYRIGHTENDKEY
+#######################################################################
+
+# Tycho test bed, see $TYCHO/doc/coding/testing.html for more information.
+
+# Load up the test definitions.
+if {[string compare test [info procs test]] == 1} then { 
+    source testDefs.tcl
+} {}
+
+# Uncomment this to get a full report, or set in your Tcl shell window.
+# set VERBOSE 1
+
+# If a file contains non-graphical tests, then it should be named .tcl
+# If a file contains graphical tests, then it should be called .itcl
+# 
+# It would be nice if the tests would work in a vanilla itkwish binary.
+# Check for necessary classes and adjust the auto_path accordingly.
+#
+
+######################################################################
+####
+# 
+test FunctionToken-1.0 {Create an empty instance} {
+    set r [java::new {ptolemy.data.FunctionToken} "function() 4"]
+    list [$r toString] [[$r getType] toString]
+} {{(function() 4)} {() -> double}}
+
+######################################################################
+####
+# 
+test FunctionToken-1.1 {Create a non-empty instance} {
+    set r [java::new {ptolemy.data.FunctionToken} "function(x,y) 4+x+y"]
+    list [$r toString] [[$r getType] toString]
+} {{(function(x, y) 4+x+y)} {(unknown, unknown) -> unknown}}
+
+######################################################################
+####
+# 
+test FunctionToken-2.1 {Test add} {
+    # first record is {name="foo", value=1, extra1=2}
+    set r1 [java::new {ptolemy.data.FunctionToken} "function(x,y) 4+x+y"] 
+    set r2 [java::new {ptolemy.data.FunctionToken} "function(a,b) 8+2"]
+
+    [$r1 add $r2] toString
+} {{(function(x,y) (function(x,y) 4+x+y)(x,y) + (function(a,b) 8+2)(x,y)}}
+
+######################################################################
+####
+# 
+test FunctionToken-10.0 {test equals} {
+    set r1 [java::new {ptolemy.data.FunctionToken} "function(x,y) 4+x+y"] 
+    set r2 [java::new {ptolemy.data.FunctionToken} "function(x,y) 4+x+y"] 
+    set r3 [java::new {ptolemy.data.FunctionToken} "function(a,b) 8+2"]
+    
+    list [$r1 equals $r1] [$r1 equals $r2] [$r1 equals $r3]
+} {1 1 0}
+
+######################################################################
+####
+# 
+test FunctionToken-11.0 {test hashCode} {
+    # use t1, t2, t3 above
+    list [$r1 hashCode] [$r2 hashCode] [$r3 hashCode]
+} {8 8 14}
+
