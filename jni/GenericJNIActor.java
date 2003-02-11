c65ca7ae6219d0957e759cec1816604d6ce89295
cxh <cxh>
2003-02-11 20:43:07 +0000

    UC Copyright

--- jni/GenericJNIActor.java
+++ jni/GenericJNIActor.java
@@ -1,5 +1,5 @@
 /** An actor able to call a C function
- Copyright (c) 2003 Thales Group
+ Copyright (c) 2003 The Regents of the University of California.
  All rights reserved.
  Permission is hereby granted, without written agreement and without
  license or royalty fees, to use, copy, modify, and distribute this
