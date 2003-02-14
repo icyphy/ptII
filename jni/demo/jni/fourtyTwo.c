006330500a213fd3ff51dce8dae3807418c44652
cxh <cxh>
2003-02-14 21:48:55 +0000

    Added fourtyTwo.c and main.c

---
+++ jni/demo/jni/fourtyTwo.c
@@ -0,0 +1,5 @@
+// Return the answer to life
+// Return "the meaning of life, the universe, and everything"
+int meaningOfLife() {
+  return 42;
+}
---
+++ jni/demo/jni/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+// Test driver for fourtyTwo
+int main() {
+  printf("the meaning of life, the universe, and everything is: %d\n",
+	 meaningOfLife());
+}
