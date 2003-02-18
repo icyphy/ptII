a68ea28b53d2f95aad52128e24b2c7dd8f9a95c1
jiahau <jiahau>
2003-02-18 22:37:03 +0000

    *** empty log message ***

--- ptolemy/actor/lib/jai/JAIInvert.java
+++ ptolemy/actor/lib/jai/JAIInvert.java
@@ -1,3 +1,31 @@
+/* An actor that inverts a javax.media.jai.RenderedOp
+
+@Copyright (c) 1998-2002 The Regents of the University of California.
+All rights reserved.
+
+Permission is hereby granted, without written agreement and without
+license or royalty fees, to use, copy, modify, and distribute this
+software and its documentation for any purpose, provided that the
+above copyright notice and the following two paragraphs appear in all
+copies of this software.
+
+IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+SUCH DAMAGE.
+
+THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ENHANCEMENTS, OR MODIFICATIONS.
+
+                                                PT_COPYRIGHT_VERSION 2
+                                                COPYRIGHTENDKEY
+*/
+
 package ptolemy.actor.lib.jai;
 
 import ptolemy.actor.lib.Transformer;
@@ -10,8 +38,25 @@ import java.awt.image.renderable.ParameterBlock;
 import javax.media.jai.JAI;
 import javax.media.jai.RenderedOp;
 
+//////////////////////////////////////////////////////////////////////////
+//// JAIInvert
+/**
+   Invert a RenderedOp using the javax.media.jai.JAI class.
+
+   @author James Yeh
+   @version $Id$
+ */
+
 public class JAIInvert extends Transformer {
-    
+
+    /** Construct an actor with the given container and name.
+     *  @param container The container.
+     *  @param name The name of this actor.
+     *  @exception IllegalActionException If the actor cannot be contained
+     *   by the proposed container.
+     *  @exception NameDuplicationException If the container already has an
+     *   actor with this name.
+     */    
     public JAIInvert(CompositeEntity container, String name)
             throws IllegalActionException, NameDuplicationException {
         super(container, name);
@@ -19,15 +64,22 @@ public class JAIInvert extends Transformer {
         output.setTypeEquals(BaseType.OBJECT);
     }
 
+    ///////////////////////////////////////////////////////////////////
+    ////                         public methods                    ////
+    
+    /** Fire this actor.
+     *  Output the scaled RenderedOp.
+     *  @exception IllegalActionException If a contained method throws it,
+     *   or if a token is received that contains a null image.
+     */
     public void fire() throws IllegalActionException {
         super.fire();
-        _parameters = new ParameterBlock();
+        ParameterBlock parameters = new ParameterBlock();
         JAIImageToken jaiImageToken = (JAIImageToken) input.get(0);
         RenderedOp oldImage = jaiImageToken.getValue();
-        _parameters.addSource(oldImage);
-        RenderedOp newImage = JAI.create("invert", _parameters);
+        parameters.addSource(oldImage);
+        RenderedOp newImage = JAI.create("invert", parameters);
         output.send(0, new JAIImageToken(newImage));
     }
-    
-    private ParameterBlock _parameters;
+       
 }
