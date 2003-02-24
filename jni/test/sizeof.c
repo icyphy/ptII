27d42fbe03df9ee9477f7f65a605b5aa326e0450
cxh <cxh>
2003-02-24 16:31:51 +0000

    sizeof.c

---
+++ jni/test/sizeof.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(int argc, char* argv) {
+    printf("char: %d\n", sizeof(char));
+    printf("short: %d\n", sizeof(short));
+    printf("int: %d\n", sizeof(int));
+    printf("long: %d\n", sizeof(long));
+    printf("float: %d\n", sizeof(float));
+    printf("double: %d\n", sizeof(double));
+}
