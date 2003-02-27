9f837f043ae763d5bddac72f643ff69d94932bde
cxh <cxh>
2003-02-27 18:00:27 +0000

    Downgraded to Red: no comments

--- ptolemy/domains/gr/kernel/GRDebug.java
+++ ptolemy/domains/gr/kernel/GRDebug.java
@@ -24,8 +24,8 @@
                                         PT_COPYRIGHT_VERSION_2
                                         COPYRIGHTENDKEY
 
-@ProposedRating Yellow (chf@eecs.berkeley.edu)
-@AcceptedRating Yellow (chf@eecs.berkeley.edu)
+@ProposedRating Red (cxh@eecs.berkeley.edu)
+@AcceptedRating Red (cxh@eecs.berkeley.edu)
 */
 
 package ptolemy.domains.gr.kernel;
