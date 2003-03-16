fe890e1dcb600cf0e9af40ff784f220eba9c9487
acataldo <acataldo>
2003-03-16 01:29:03 +0000

    Extended to work with matrices and arrays

--- ptolemy/domains/sdf/lib/DotProduct.java
+++ ptolemy/domains/sdf/lib/DotProduct.java
@@ -25,7 +25,7 @@
                                         COPYRIGHTENDKEY
 
 @ProposedRating Yellow (pwhitake@eecs.berkeley.edu)
-@AcceptedRating Yellow (pwhitake@eecs.berkeley.edu)
+@AcceptedRating Red (acataldo@eecs.berkeley.edu)
 */
 
 package ptolemy.domains.sdf.lib;
@@ -35,11 +35,18 @@ import ptolemy.actor.IOPort;
 import ptolemy.actor.TypedAtomicActor;
 import ptolemy.actor.TypedIOPort;
 import ptolemy.data.ArrayToken;
+import ptolemy.data.MatrixToken;
 import ptolemy.data.ScalarToken;
 import ptolemy.data.Token;
 import ptolemy.data.type.ArrayType;
 import ptolemy.data.type.BaseType;
+import ptolemy.data.type.BaseType.ScalarType;
+import ptolemy.data.type.BaseType.UnknownType;
+import ptolemy.data.type.UnsizedMatrixType;
 import ptolemy.data.type.Type;
+import ptolemy.data.type.TypeConstant;
+import ptolemy.data.type.TypeLattice;
+import ptolemy.graph.CPO;
 import ptolemy.graph.InequalityTerm;
 import ptolemy.kernel.CompositeEntity;
 import ptolemy.kernel.util.Workspace;
@@ -49,17 +56,19 @@ import ptolemy.kernel.util.NameDuplicationException;
 //////////////////////////////////////////////////////////////////////////
 //// DotProduct
 /**
-Compute the dot product of two arrays. This actor has two
-input ports, from which it receives two ArrayTokens. The elements of the
-ArrayTokens must be of type ScalarToken. The output is the dot product of
-the two arrays.
-<p>
-This actor requires that each input port have a token upon firing. On each
-firing, it produces exactly one token, which is of type ScalarToken.
+   Compute the dot product of two arrays or matrices. This actor has two
+   input ports, from which it receives two ArrayTokens or two Matrix
+   Tokens. The elements of the ArrayTokens or MatrixTokens must be of
+   type ScalarToken. The output is the dot product of the two arrays or
+   matrices.  
 
-@author Jeff Tsay, Paul Whitaker
-@version $Id$
-@since Ptolemy II 1.0
+   <p> This actor requires that each input port have a token
+   upon firing. On each firing, it produces exactly one token, which is
+   of type ScalarToken.
+
+   @author Jeff Tsay, Paul Whitaker, Adam Cataldo
+   @version $Id$
+   @since Ptolemy II 1.0
 */
 
 public class DotProduct extends TypedAtomicActor {
@@ -81,20 +90,10 @@ public class DotProduct extends TypedAtomicActor {
         input2 = new TypedIOPort(this, "input2", true, false);
         output = new TypedIOPort(this, "output", false, true);
 
-        // set input types to array
-        input1.setTypeEquals(new ArrayType(BaseType.UNKNOWN));
-        input2.setTypeEquals(new ArrayType(BaseType.UNKNOWN));
+        // Set the type constraints.
+        output.setTypeAtLeast(new PortFunction(input1, input2));
 
-        // set the output type to be no less than the element type of the
-        // input arrays.
-        ArrayType input1Type = (ArrayType)input1.getType();
-        InequalityTerm elementTerm1 = input1Type.getElementTypeTerm();
-        output.setTypeAtLeast(elementTerm1);
-
-        ArrayType input2Type = (ArrayType)input2.getType();
-        InequalityTerm elementTerm2 = input2Type.getElementTypeTerm();
-        output.setTypeAtLeast(elementTerm2);
-    }
+     }
 
     ///////////////////////////////////////////////////////////////////
     ////                     ports and parameters                  ////
@@ -117,36 +116,77 @@ public class DotProduct extends TypedAtomicActor {
     ////                         public methods                    ////
 
     /** Clone the actor into the specified workspace. This calls the
-     *  base class and then sets the value public variable in the new
-     *  object to equal the cloned parameter in that new object.
+     *  base class and then sets the type constraints.
      *  @param workspace The workspace for the new object.
      *  @return A new actor.
-     *  @exception CloneNotSupportedException If a derived class contains
+     *  @exception CloneNotSupportedException If a derived class has
      *   an attribute that cannot be cloned.
      */
     public Object clone(Workspace workspace)
-	    throws CloneNotSupportedException {
+            throws CloneNotSupportedException {
         DotProduct newObject = (DotProduct)super.clone(workspace);
-
-        // set the type constraints
-        ArrayType input1Type = (ArrayType)newObject.input1.getType();
-        InequalityTerm elementTerm1 = input1Type.getElementTypeTerm();
-        newObject.output.setTypeAtLeast(elementTerm1);
-
-        ArrayType input2Type = (ArrayType)newObject.input2.getType();
-        InequalityTerm elementTerm2 = input2Type.getElementTypeTerm();
-        newObject.output.setTypeAtLeast(elementTerm2);
+        PortFunction function =
+            new PortFunction(newObject.input1, newObject.input2);
+        newObject.output.setTypeAtLeast(function);
         return newObject;
     }
 
-    /** Read an ArrayToken from each of the input ports, and output the
+    /** Read a Token from each of the input ports, and output the
      *  dot product.
      *  @exception IllegalActionException If there is no director, if
      *  the input arrays have unequal widths, or if the input arrays
-     *  have no elements..
+     *  have no elements, or if the input types.
      */
     public void fire() throws IllegalActionException {
         super.fire();
+        if ((input1.getType() instanceof ArrayType) &&
+                (input2.getType() instanceof ArrayType)) {
+            try {
+                _arrayFire();
+            }
+            catch (IllegalActionException e) {
+                throw e;
+            }
+        }
+        else if ((input1.getType() instanceof UnsizedMatrixType) &&
+                (input1.getType() instanceof UnsizedMatrixType)) {
+            try {
+                _matrixFire();
+            }
+            catch (IllegalActionException e) {
+                throw e;
+            }
+        }
+        else {
+            throw new IllegalActionException("Invalid types");
+        }
+    }
+
+    /** If both of the input ports have at least one token, return
+     *  what the superclass returns (presumably true).  Otherwise return
+     *  false.
+     *  @exception IllegalActionException If there is no director.
+     */
+    public boolean prefire() throws IllegalActionException {
+        if (!input1.hasToken(0) || !input2.hasToken(0)) {
+            if (_debugging) {
+                _debug("Called prefire(), which returns false.");
+            }
+            return false;
+        } else {
+            return super.prefire();
+        }
+    }
+
+    ///////////////////////////////////////////////////////////////////
+    ////                         private methods                   ////
+
+    /*  Read an ArrayToken from each of the input ports, and output the
+     *  dot product.
+     *  @exception IllegalActionException If the input arrays have
+     *  unequal widths or if the input arrays have no elements..
+     */
+    private void _arrayFire() throws IllegalActionException {
         ArrayToken token1 = (ArrayToken) input1.get(0);
         ArrayToken token2 = (ArrayToken) input2.get(0);
 
@@ -178,19 +218,175 @@ public class DotProduct extends TypedAtomicActor {
         output.broadcast(dotProd);
     }
 
-    /** If both of the input ports have at least one token, return
-     *  what the superclass returns (presumably true).  Otherwise return
-     *  false.
-     *  @exception IllegalActionException If there is no director.
+    /*  Read a MatrixToken from each of the input ports, and output
+     *  the dot product.
+     *  @exception IllegalActionException If the input matrices have
+     *  unequal sizes.
      */
-    public boolean prefire() throws IllegalActionException {
-        if (!input1.hasToken(0) || !input2.hasToken(0)) {
-            if (_debugging) {
-                _debug("Called prefire(), which returns false.");
+    public void _matrixFire() throws IllegalActionException {
+        MatrixToken token1 = (MatrixToken)input1.get(0);
+        MatrixToken token2 = (MatrixToken)input2.get(0);
+
+        int columnCount1 = token1.getColumnCount();
+        int rowCount1 = token1.getRowCount();
+        int columnCount2 = token2.getColumnCount();
+        int rowCount2 = token2.getRowCount();
+
+        Token element1, element2, sum;
+
+        if ((columnCount1 == columnCount2) && 
+                (rowCount1 == rowCount2)) {
+            sum = token1.getElementAsToken(0,0).zero();
+            for (int i = 0; i < rowCount1; i += 1) {
+                for (int j = 0; j < columnCount1; j += 1) {
+                    element1 = token1.getElementAsToken(i, j);
+                    element2 = token2.getElementAsToken(i, j);
+                    sum = sum.add(element1.multiply(element2));
+                }
             }
-            return false;
-        } else {
-            return super.prefire();
+            output.send(0, sum);
+        }
+        else {
+            String matrix1 = rowCount1 + " by " + columnCount1;
+            String matrix2 = rowCount2 + " by " + columnCount2;
+            throw new IllegalActionException("Tried to multiply a " +
+                    matrix1 + " matrix with a " + matrix2 + " matrix");
         }
     }
+
+    ///////////////////////////////////////////////////////////////////
+    ////                         inner classes                     ////
+
+    /** This class implements a function on the two input port types.
+     *  f(port1, port2) == 
+     *     LUB(elementType(port1), elementType(port2)) if both are matrices
+     *     LUB(elementType(port1), elementType(port2)) if both are arrays
+     *     UNKNOWN                                     otherwise
+     *  This function's value determinese the output port type.
+     */
+
+    private class PortFunction implements InequalityTerm {
+
+        private PortFunction(TypedIOPort port1, TypedIOPort port2) {
+            _port1 = port1;
+            _port2 = port2;
+        }
+
+        ///////////////////////////////////////////////////////////////
+        ////                       public inner methods            ////
+
+        /** Return null.  This method is only used because the class
+         *  implements InequalityTerm, which requires this method.
+         *  @return null.
+         */
+        public Object getAssociatedObject() {
+            return null;
+        }
+
+        /** Return the function result.
+         *  @return A Type.
+         */
+        public Object getValue() {
+            Type type1 = _port1.getType();
+            Type type2 = _port2.getType();
+            if ((type1 == BaseType.UNKNOWN) 
+                    || (type2 == BaseType.UNKNOWN)) {
+                return BaseType.UNKNOWN;
+            }
+            else if ((type1 instanceof ArrayType) && 
+                    (type2 instanceof ArrayType)) {
+                Type elType1 = ((ArrayType)type1).getElementType();
+                Type elType2= ((ArrayType)type2).getElementType();
+                CPO lattice = TypeLattice.lattice();
+                return lattice.leastUpperBound(elType1, elType2);
+            }
+            else if ((type1 instanceof UnsizedMatrixType) &&
+                    (type2 instanceof UnsizedMatrixType)) {
+                Type elType1 = 
+                    ((UnsizedMatrixType)type1).getElementType();
+                Type elType2 = 
+                    ((UnsizedMatrixType)type2).getElementType();
+                CPO lattice = TypeLattice.lattice();
+                return lattice.leastUpperBound(elType1, elType2);
+            }
+            else {
+                return BaseType.UNKNOWN;
+            }
+        }
+
+        /** Return the type variables in this InequalityTerm.  If
+         *  neither type variable has been set, return an array of
+         *  size two, containing the InequalityTerms corresponding to
+         *  the port.  If exactly one is set, return the
+         *  InequalityTerm corresponding to the other port (in a size
+         *  one array).  If both have been set, return an empty
+         *  InequalityTerm array.
+         *  @return An array of InequalityTerm.
+         */
+        public InequalityTerm[] getVariables() {
+            InequalityTerm term1 = _port1.getTypeTerm();
+            InequalityTerm term2 = _port2.getTypeTerm();
+            if ((term1.isSettable()) && (term2.isSettable())) {
+                InequalityTerm[] array = {term1, term2};
+                return array;
+            }
+            else if (term1.isSettable()) {
+                InequalityTerm[] array = {term1};
+                return array;
+            }
+            else if (term2.isSettable()) {
+                InequalityTerm[] array = {term2};
+                return array;
+            }
+            return (new InequalityTerm[0]);
+        }
+
+        /** Throw an Exception. This method cannot be called on a function
+         *  term.
+         *  @exception IllegalActionException Always thrown.
+         */
+        public void initialize(Object e)
+                throws IllegalActionException {
+            throw new IllegalActionException(getClass().getName()
+                    + ": Cannot initialize a function term.");
+        }
+           
+        /** Return false, because this is a function.
+         *  @return false.
+         */
+        public boolean isSettable() {
+            return false;
+        }
+
+        /** Return true.
+         *  @return True.
+         */
+        public boolean isValueAcceptable() {
+            return true;
+        }
+
+
+        /** Throw an Exception. The value of a function term cannot be set.
+         *  @exception IllegalActionException Always thrown.
+         */
+        public void setValue(Object e) throws IllegalActionException {
+            throw new IllegalActionException(getClass().getName()
+                    + ": The type is not settable.");
+        }
+
+        /** Override the base class to give a description of this term.
+         *  @return A description of this term.
+         */
+        public String toString() {
+            return "(" + getClass().getName() + ", " + getValue() + ")";
+        }
+
+        ///////////////////////////////////////////////////////////////
+        ////                       private inner variable          ////
+        
+        private TypedIOPort _port1;
+        private TypedIOPort _port2;
+
+    }
+
 }
