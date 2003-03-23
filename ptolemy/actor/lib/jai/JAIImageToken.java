34eee68c886a41c69c1c85dbfda7ef980be4687b
jiahau <jiahau>
2003-03-23 03:48:30 +0000

    JAIImageToken.java

--- ptolemy/actor/lib/jai/JAIImageToken.java
+++ ptolemy/actor/lib/jai/JAIImageToken.java
@@ -28,26 +28,30 @@
 package ptolemy.actor.lib.jai;
 
 import java.awt.image.BufferedImage;
+import java.awt.image.renderable.ParameterBlock;
 import java.awt.Image;
 
 import javax.media.jai.PlanarImage;
+import javax.media.jai.JAI;
 import javax.media.jai.RenderedOp;
 
 import ptolemy.data.ImageToken;
+import ptolemy.data.ScalarToken;
+import ptolemy.data.Token;
 import ptolemy.data.type.*;
-import ptolemy.graph.CPO;
 import ptolemy.kernel.util.IllegalActionException;
 
 //////////////////////////////////////////////////////////////////////////
 //// JAIImageToken
+
 /**
-   A token that contains a javax.media.jai.RenderedOp.  This token is used
-   when dealing with images in the Java Advanced Imaging (JAI) library.
-   Because it extends ImageToken, it can be used with the standard image
-   processing tools by simply casting it, and calling asAWTImage().
-   @author James Yeh
-   @version $Id$
-*/
+ A token that contains a javax.media.jai.RenderedOp.  This token is used
+ when dealing with images in the Java Advanced Imaging (JAI) library.
+ Because it extends ImageToken, it can be used with the standard image
+ processing tools by simply calling asAWTImage().
+ @author James Yeh
+ @version $Id$
+ */
 public class JAIImageToken extends ImageToken {
 
     /** Construct a token with a specified RenderedOp.
@@ -59,6 +63,28 @@ public class JAIImageToken extends ImageToken {
     ///////////////////////////////////////////////////////////////////
     ////                         public methods                    ////
 
+    /** Return a new token whose value is the sum of this token and
+     *  the argument.  The image size is the size of the intersection of 
+     *  the two images.  The number of bands in the image is equal to
+     *  the smallest number of bands of the two sources.  The data type
+     *  is the smallest data type to have sufficient range for both input
+     *  data types.
+     *
+     * @param rightArgument The token to add to this token.
+     * @return A new token containing the result.
+     * @throws IllegalActionException if the data type is not supported.
+     */
+    public Token add(Token rightArgument)
+            throws IllegalActionException {
+        if (rightArgument instanceof JAIImageToken) {
+            ParameterBlock parameters =
+                _parameterize(_renderedOp, (JAIImageToken) rightArgument);
+            return new JAIImageToken(JAI.create("add", parameters));
+        } else {
+            throw new IllegalActionException("illegal data type");
+        }
+    }
+
     /** Convert a javax.media.jai.RenderedOp to a java.awt.Image and
      *  return it.
      */
@@ -67,6 +93,27 @@ public class JAIImageToken extends ImageToken {
         _bufferedImage = _planarImage.getAsBufferedImage();
         _awtImage = (Image) _bufferedImage;
         return _awtImage;
+    }
+
+    /** Return a new token whose value is the division of this 
+     *  token and the argument.  The image size is the size of the 
+     *  intersection of the two images.  The number of bands in the image 
+     *  is equal to the smallest number of bands of the two sources.  The 
+     *  data type is the bigger of the two input data types
+     *
+     * @param rightArgument The token to divide this token by.
+     * @return A new token containing the result.
+     * @throws IllegalActionException if the data type is not supported.
+     */
+    public Token divide(Token rightArgument)
+            throws IllegalActionException {
+        if (rightArgument instanceof JAIImageToken) {
+            ParameterBlock parameters =
+                    _parameterize(_renderedOp, (JAIImageToken) rightArgument);
+            return new JAIImageToken(JAI.create("divide", parameters));
+        } else {
+            throw new IllegalActionException("illegal data type");
+        }
     }
 
     /** Return the type of this token.
@@ -83,8 +130,86 @@ public class JAIImageToken extends ImageToken {
         return _renderedOp;
     }
 
+    /** Return a new token whose value is the multiplication of this 
+     *  token and the argument.  The image size is the size of the 
+     *  intersection of the two images.  The number of bands in the image 
+     *  is equal to the smallest number of bands of the two sources.  The 
+     *  data type is the bigger of the two input data types
+     *
+     * @param rightArgument The token to multiply this token by.
+     * @return A new token containing the result.
+     * @throws IllegalActionException if the data type is not supported.
+     */
+    public Token multiply(Token rightArgument)
+            throws IllegalActionException {
+        if (rightArgument instanceof JAIImageToken) {
+            ParameterBlock parameters =
+                    _parameterize(_renderedOp, (JAIImageToken) rightArgument);
+            return new JAIImageToken(JAI.create("multiply", parameters));
+        } else {
+            throw new IllegalActionException("illegal data type");
+        }
+    }
+
+    /** Return a new token whose value is the subtraction of this token 
+     *  from the argument.  The image size is the size of the intersection 
+     *  of the two images.  The number of bands in the image is equal to
+     *  the smallest number of bands of the two sources.  The data type
+     *  is the smallest data type to have sufficient range for both input
+     *  data types.
+     *
+     * @param rightArgument The token to subtract from this token.
+     * @return A new token containing the result.
+     * @throws IllegalActionException if the data type is not supported.
+     */
+    public Token subtract(Token rightArgument)
+            throws IllegalActionException {
+        if (rightArgument instanceof JAIImageToken) {
+            ParameterBlock parameters =
+                    _parameterize(_renderedOp, (JAIImageToken) rightArgument);
+            return new JAIImageToken(JAI.create("subtract", parameters));
+        } else {
+            throw new IllegalActionException("illegal data type");
+        }
+    }
+
+    ///////////////////////////////////////////////////////////////////
+    ////                         private methods                   ////
+
+    // Create a ParameterBlock containing two RenderedOp's, the first
+    // being the internal Image, the second being from an ImageToken.
+    private ParameterBlock _parameterize(RenderedOp left, ImageToken right) {
+        ParameterBlock parameters = new ParameterBlock();
+        parameters.addSource(right.asAWTImage());
+        RenderedOp rightOp = JAI.create("awtImage", parameters);
+        parameters = new ParameterBlock();
+        parameters.addSource(left);
+        parameters.addSource(rightOp);
+        return parameters;
+    }
+
+    // Create a ParameterBlock containing two RenderedOp's, the first
+    // being the internal Image, the second being from a JAIImageToken.
+    private ParameterBlock _parameterize(RenderedOp left, JAIImageToken right) {
+        ParameterBlock parameters = new ParameterBlock();
+        parameters.addSource(left);
+        parameters.addSource(right.getValue());
+        return parameters;
+    }
+
+    // Create a ParameterBlock containing one RenderedOp and one double,
+    // the first being the internal Image, the second from a ScalarToken.
+    private ParameterBlock _parameterize(RenderedOp left, ScalarToken right)
+            throws IllegalActionException {
+        ParameterBlock parameters = new ParameterBlock();
+        parameters.addSource(left);
+        parameters.add(right.doubleValue());
+        return parameters;
+    }
+
     ///////////////////////////////////////////////////////////////////
     ////                         private variables                 ////
+
     private Image _awtImage;
     private BufferedImage _bufferedImage;
     private PlanarImage _planarImage;
