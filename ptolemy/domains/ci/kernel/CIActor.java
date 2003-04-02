ead90f023939e7b0003883e92996425778d395c3
ellen_zh <ellen_zh>
2003-04-02 07:00:27 +0000

    add CIActor.java, by Yang

---
+++ ptolemy/domains/ci/kernel/CIActor.java
@@ -0,0 +1,86 @@
+/* An actor that delays the input for a certain amount of real time.
+
+ Copyright (c) 1998-2003 The Regents of the University of California.
+ All rights reserved.
+ Permission is hereby granted, without written agreement and without
+ license or royalty fees, to use, copy, modify, and distribute this
+ software and its documentation for any purpose, provided that the above
+ copyright notice and the following two paragraphs appear in all copies
+ of this software.
+
+ IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+ FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+ ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+ THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+ SUCH DAMAGE.
+
+ THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+ INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+ PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+ CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ ENHANCEMENTS, OR MODIFICATIONS.
+
+                                        PT_COPYRIGHT_VERSION_2
+                                        COPYRIGHTENDKEY
+
+@ProposedRating Yellow (cxh@eecs.berkeley.edu)
+@AcceptedRating Yellow (cxh@eecs.berkeley.edu)
+*/
+
+package ptolemy.domains.ci.kernel;
+
+import ptolemy.actor.lib.Transformer;
+import ptolemy.kernel.CompositeEntity;
+import ptolemy.kernel.util.NameDuplicationException;
+import ptolemy.kernel.util.IllegalActionException;
+
+
+//////////////////////////////////////////////////////////////////////////
+///CIActor
+/*
+
+
+@author Yang Zhao
+@version $Id$
+@since Ptolemy II 2.2
+*/
+public class CIActor extends Transformer {
+
+    /** Construct an actor with the given container and name.
+     *  @param container The container.
+     *  @param name The name of this actor.
+     *  @exception IllegalActionException If the actor cannot be contained
+     *   by the proposed container.
+     *  @exception NameDuplicationException If the container already has an
+     *   actor with this name.
+     */
+    public CIActor(CompositeEntity container, String name)
+            throws NameDuplicationException, IllegalActionException  {
+        super(container, name);
+        input.setMultiport(false);
+        output.setMultiport(false);
+    }
+
+    ///////////////////////////////////////////////////////////////////
+    ////                     ports and parameters                  ////
+
+
+    ///////////////////////////////////////////////////////////////////
+    ////                         public methods                    ////
+    public void enableActor() {
+        _director = (CIDirector) getDirector();
+        _director._actorEnabled(this);
+    }
+
+    public boolean isPulled() {
+        _director = (CIDirector) getDirector();
+        return _director._isPulled(this);
+    }
+
+    ///////////////////////////////////////////////////////////////////
+    ////                         private variables                 ////
+     // The director.
+    private CIDirector _director;
+
+}
