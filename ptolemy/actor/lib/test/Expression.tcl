ada67058cd6272242f466ef9d5a199c6566ba971
cxh <cxh>
2003-04-04 01:31:17 +0000

    Expression-2.1 fixed results to print only a range

--- ptolemy/actor/lib/test/Expression.tcl
+++ ptolemy/actor/lib/test/Expression.tcl
@@ -71,11 +71,16 @@ test Expression-2.1 {run with default empty expression} {
             [java::field [java::cast ptolemy.actor.lib.Sink $rec] input]
     set m [$e0 getManager]
     catch {$m execute} msg
-    list $msg
-} {{ptolemy.actor.TypeConflictException: Type conflicts occurred in .top on the following inequalities:
-  (ptolemy.actor.TypedIOPort {.top.expr.output}, unknown) <= (ptolemy.actor.TypedIOPort {.top.rec.input}, unknown)
-  (, unknown) <= (ptolemy.actor.TypedIOPort {.top.expr.output}, unknown)
-}}
+    string range $msg 0 240
+} {ptolemy.kernel.util.InternalErrorException: Type resolution failed because of an error during type inference
+  in .top
+Because:
+Error parsing expression "":
+Encountered "<EOF>" at line 0, column 0.
+Was expecting one of:
+    "-" ...
+    "!" .}
+
 
 test Expression-3.1 {run with a simple expression} {
     set expression [java::field $expr expression]
