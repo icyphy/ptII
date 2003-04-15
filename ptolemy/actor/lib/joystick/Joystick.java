0098b86aac7a16feb68675894d261a7459ee9e52
cxh <cxh>
2003-04-15 16:23:40 +0000

    Checked in a changes from Paul, did a major rewrite.

--- ptolemy/actor/lib/joystick/Joystick.java
+++ ptolemy/actor/lib/joystick/Joystick.java
@@ -1,4 +1,5 @@
-/* Ptolemy Actor that reads data from a Joystick
+/* Reads data from a Joystick
+
  Copyright (c) 2003 The Regents of the University of California.
  All rights reserved.
  Permission is hereby granted, without written agreement and without
@@ -23,8 +24,8 @@
                                         PT_COPYRIGHT_VERSION_2
                                         COPYRIGHTENDKEY
 
-@ProposedRating red (winthrop@eecs.berkeley.edu)
-@AcceptedRating red (winthrop@eecs.berkeley.edu)
+@ProposedRating red (cxh@eecs.berkeley.edu)
+@AcceptedRating red (cxh@eecs.berkeley.edu)
 */
 
 package ptolemy.actor.lib.joystick;
@@ -33,82 +34,59 @@ import ptolemy.actor.lib.Source;
 import ptolemy.kernel.CompositeEntity;
 import ptolemy.kernel.util.*;
 
+import ptolemy.actor.TypedAtomicActor;
+import ptolemy.actor.TypedIOPort;
+import ptolemy.data.ObjectToken;
+import ptolemy.data.DoubleToken;
+import ptolemy.data.IntToken;
+import ptolemy.data.type.BaseType;
+import ptolemy.data.expr.Parameter;
+import ptolemy.kernel.CompositeEntity;
+import ptolemy.data.DoubleMatrixToken;
+import java.util.Iterator;
+import java.util.Vector;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.io.IOException;
+
+import com.centralnexus.input.*;
+
 //////////////////////////////////////////////////////////////////////////
 ////
 /**
 This actor reads data from a Joystick using the Joystick interface
-from
-<a href="http://sourceforge.net/projects/javajoystick/" target="_top"><code>http://sourceforge.net/projects/javajoystick/</code></a>
+from 
+<a href="http://sourceforge.net/projects/javajoystick/" 
+target="_top"><code>http://sourceforge.net/projects/javajoystick/</code></a>
 
-<p>Currently, this actor will only work under Windows, though
+<p>Currently, this actor will only work under Windows, though 
 the Joystick interface also supports Linux.
 
 <p>If you get the following error on the console:
 <pre>
 TBA
 </pre>
-then you need to be sure that the
-<code>jjstick.dll</code> is in your path.
-<p>The default location of the Joystick interface is
-add
-<code>$PTII/vendors/misc/joystick/lib</code>, so you could either
+then you need to be sure that the 
+<code>jjstick.dll</code> is in your path.  
+<p>By default, $PTII/configure looks for the Joystick
+interface in <code>$PTII/vendors/misc/joystick/lib</code>, so you could either
 add that directory to your path, or copy <code>jjstick.dll</code>
 to <code>$PTII/bin</code>
 
+<p> When the fire() method is called, a DoubleMatrixToken is
+produced on the output.  The X value is in [0][0] and the
+Y value is in [1][0] (FIXME: what is the range?)
+(FIXME: it looks like there is some confusion about polling
+vs. callbacks?)
 
-<b>FIXME: the information below is about the SerialComm actor, but
-might be relevant</b>
-
-<p>This actor is designed for use in DE and SDF.  By default, this actor
-does not block in fire().  When fired, if no data is available on the
-serial port, then no token is produced.  However, when using this actor in
-SDF, you may want to experiment with the <i>blocking</i> parameter.
-Setting this true has the actor block in fire() ultil data arrives.
-
-<p>Bytes to be sent must enter the actor as an array of integers.
-The lowest order byte from each integer is used.  (Negative numbers
-are treated as though 256 has been just added enough times to make them
-non-negative.)  Likewise, bytes received are broadcast out of the actor
-as an integer array of which only the low bytes carry data.  This
-actor makes no guarantee as to the contents of the other three bytes
-of the integer.
-
-<p>This actor is a class which implements SerialPortEventListener.
-This means that when serial events (such as DATA_AVAILABLE) occur,
-this actor's serialEvent() method gets called.  the serialEvent()
-method calls the director's fireAtCurrentTime() method, triggering
-a call to fire().
-
-<p>By the time fire() executes, there may be several bytes of available
-data.  This is primarily because the UART only signals the software every
-8 bytes or so.  These are packaged by fire() into an array of integers
-(one int per byte) and broadcast.
-
-<p>The fire() method's reading of serial data is also governed by the
-<i>blocking</i>, <i>threshold</i>, and <i>truncation</i> parameters.
-Fire() first tests to see if there are at least <i>threshold</i> bytes
-of data available.  If so, it reads them, or the <i>truncation</i>
-most recent bytes, whichever is fewer.  (Or all the bytes, regardless
-of how many, if <i>truncation</i> is set to zero.)  If fewer than
-<i>threshold</i> bytes are available, then if <i>blocking</i> is false
-it will not read any data, but if <i>blocking</i> is true it will wait
-until at least <i>threshold</i> bytes are available.
-
-<p>Because the DATA_AVAILABLE event typically occurs every 8 bytes,
-continuous data arriving at 115200 baud can wake up the actor 1440
-times per second!  This is to often to be calling fireAt*() on
-the director of a DE model.  Thus, after the first fireAtCurrentTime()
-call, the serialEvent() callback only notifys the fire() method
-(in case it is awaiting additional data to meet its <i>threshold</i>)
-and does not call fireAt*() again until the actor has completed a
-firing since the last time fireAt*() was called.
-
-@author Paul Yang
+@author Christopher Hylands, David Lee, Paul Yang
 @version $Id$
-@since Ptolemy II 2.1
-@see ptolemy.actor.lib.comm.SerialComm
+@since Ptolemy II 3.0
+@see ptolemy.actor.lib.io.comm.SerialComm
  */
-public class Joystick extends Source {
+public class Joystick extends Source implements 
+com.centralnexus.input.JoystickListener {
 
     /** Construct a Joystick actor with the given container and name.
      *  @param container The container.
@@ -119,70 +97,148 @@ public class Joystick extends Source {
      *   actor with this name.
      */
     public Joystick(CompositeEntity container, String name)
-            throws NameDuplicationException, IllegalActionException {
+            throws NameDuplicationException, IllegalActionException, 
+            IOException {
         super(container, name);
+        // FIXME: Why Double Matrix?  Why not an array or two separate ports?
+        // FIXME: What is the range of the output?
+	output.setTypeEquals(BaseType.DOUBLE_MATRIX);
+	output.setMultiport(false);
+	pollingInterval
+            = new Parameter(this, "pollingInterval", new IntToken("50"));
+	deadZone = new Parameter(this, "deadZone", new DoubleToken("0.01"));
     }
+
+    ///////////////////////////////////////////////////////////////////
+    ////                     ports and parameters                  ////
+
+    /** The polling interval of the Joystick in milliseconds.  The
+     *  default value is an IntToken with a value of 50
+     */  
+    public Parameter pollingInterval;
+
+    /** The deadzone of the Joystick: Under this absolute value, the
+     *  joystick coordinate is 0.0.  The default value is a 
+     *  DoubleToken of value 0.01
+     */
+    public Parameter deadZone;
 
     ///////////////////////////////////////////////////////////////////
     ////                         public methods                    ////
 
-    /** If the parameter changed is <i>serialPortName</i>, then hope
-     *  the model is not running and do nothing.  Likewise for baudRate.
+    /** If the attribute is pollingInterval or deadZone and the
+     *  joystick has already been initialized by calling initialize()
+     *  then update the appropriate value in the joystick interface.
+     *  If initialized() has not yet been called, then do nothing. 
      *  @param attribute The attribute that changed.
      *  @exception IllegalActionException Maybe thrown (?)
      */
     public void attributeChanged(Attribute attribute)
             throws IllegalActionException {
-        super.attributeChanged(attribute);
+        // FIXME: not sure about this, but it seems like a good idea.
+        if (attribute == pollingInterval && _joy != null) {
+            int pollingIntervalValue
+                = ((IntToken)pollingInterval.getToken()).intValue();
+            _joy.setPollInterval(pollingIntervalValue);
+        } else if (attribute == deadZone && _joy != null) {
+            double deadZoneValue
+                = ((DoubleToken)deadZone.getToken()).doubleValue();
+            _joy.setDeadZone(deadZoneValue);
+        } else {
+            super.attributeChanged(attribute);
+        }
     }
 
-    /** Transfers data between the Ptolemy model and the built in
-     *  buffers associated with the serial port.  Actual serial
-     *  input and output occur right before or right after fire().
-     *  For example, serial output occurs in response to the .flush()
-     *  call below.  This data written to the serial port out to the
-     *  serial hardware.  The .flush() method does not wait for the
-     *  hardware to complete the transmission, as this might take
-     *  many milliseconds (roughly 1mS for every 10 bytes at 115200
-     *  baud).
-     *  <p>
-     *  This fire() method checks for either or both of the following
-     *  conditions.  Data may have been received and is available in
-     *  the serial port.  A Token may have been received by this actor.
-     *  If at least 1 byte is available, broadcast it.
-     *  If an integer-array token is available, take a byte out
-     *  of each integer and send the byte stream to the serial port.
-     *  @exception IllegalActionException Thrown if the try fails.
+    
+    /** Close this JoyDriver by removing it from the JoystickListeners. */
+    public void close() {
+	_joy.removeJoystickListener(this);
+    }
+
+    /** Get the current location values from the joystick
+     *  and generate a DoubleMatrixToken on the output. 
      */
     public synchronized void fire() throws IllegalActionException {
-        super.fire();
+	super.fire();
+	double currentX = _joy.getX();
+	double currentY = _joy.getY();
+	double[][] data = new double[2][1];
+	data[0][0] = currentX;
+	data[1][0] = -currentY;
+	DoubleMatrixToken result = new DoubleMatrixToken(data);
+	output.send(0, result);
     }
 
-    /** [Pre]initialize does the resource allocation for this actor.
-     *  Specifically, it opens the serial port (setting the baud rate
-     *  and other communication settings) and then activates the
-     *  serial port's event listening resource, directing events to the
-     *  serialEvent() method of this actor.  (serialEvent() is the
-     *  required name for this method.  It is required since this actor
-     *  implements SerialPortEventListener.  It is not explicitly named in
-     *  the calls to .addEventListener() and .notifyOnDataAvailable()
-     *  below.)
-     *  @exception IllegalActionException if the try fails.
+    /** Get the values of the parameters and initialize the joystick
+     *
+     *  @exception IllegalActionException If the joystick cannot
+     *  be initialized or if the parent class throws it.
      */
     public void initialize() throws IllegalActionException {
         super.initialize();
+        int pollingIntervalValue
+                = ((IntToken)pollingInterval.getToken()).intValue();
+        double deadZoneValue
+                = ((DoubleToken)deadZone.getToken()).doubleValue();
+
+	try {
+	    _joy = com.centralnexus.input.Joystick.createInstance();
+            if (_debugging){
+                _debug("JoystickID: " + _joy.getID());
+            }
+	}
+	catch (IOException ex) {
+            throw new IllegalActionException(this, ex,
+                    "Failed to create a joystick instance");
+	}
+        _joy.setDeadZone(deadZoneValue);
+	_joy.setPollInterval(pollingIntervalValue);
+	_joy.addJoystickListener(this);
     }
 
 
-    /** Wrap up deallocates resources, specifically the serial port.
-     *  @exception IllegalActionException Maybe thrown (?).
+    /** Indicates the joystick's input when the joystick is polled. */
+    public void joystickChanged(com.centralnexus.input.Joystick j) {
+        // FIXME: Why not use this method?
+	//	current_X = _joy.getX();
+	//	current_Y = _joy.getY();
+    }
+
+    /** Called when the joystick axis has changed.
+     *  Indicates the joystick's input when the joystick is polled.
+     */
+    public void joystickAxisChanged(com.centralnexus.input.Joystick j) {
+        // FIXME: Why not use this method?
+	//	current_X = _joy.getX();
+	//	current_Y = _joy.getY();
+    }
+
+    /** Called when the joystick button is changed.
+    public void joystickButtonChanged(com.centralnexus.input.Joystick j) {
+        // FIXME: Why not use these
+
+    }
+
+    /** Sets the polling interval.
+     *  @param pollMillis The number of milliseconds between polls.
+     */
+    public void setPollInterval(int pollMillis) {
+        // FIXME: Does not do anything? we are polling every time
+        // we call fire.
+        _joy.setPollInterval(pollMillis);
+    }
+
+    /** Close this JoyDriver by removing it from the JoystickListeners.
+     *  @exception IllegalActionException Not thrown in this baseclass.
      */
     public void wrapup() throws IllegalActionException {
+	close();
     }
 
     ///////////////////////////////////////////////////////////////////
     ////                         private variables                 ////
 
+    // The joystick
+    private com.centralnexus.input.Joystick _joy;
 }
-
 
