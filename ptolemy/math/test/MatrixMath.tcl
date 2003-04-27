60c6edeb741d4fcfce20e3cb84ab342ac69903b6
cxh <cxh>
2003-04-27 19:48:40 +0000

    within(xxx[], xxx[], xxx[]) now returns false if the errorArray has a negative value. Note that there are problems with Complex still

--- ptolemy/math/test/MatrixMath.tcl
+++ ptolemy/math/test/MatrixMath.tcl
@@ -1803,12 +1803,14 @@ testMatrixMatrixScalar within $types
 ####
 ##  *ArrayMath Test out boolean within(xxx[], xxx[], xxx[])
 
+# FIXME: Why is it ok to have negative values for complex numbers
+# but not the others
 set types [list \
-        [list Complex ptolemy.math.Complex complex {1}] \
-  	[list Double double double {1}] \
-  	[list Float float float {1}] \
-  	[list Integer int int {1}] \
-  	[list Long long long {1}]]
+        [list Complex ptolemy.math.Complex complex {0}] \
+  	[list Double double double {0}] \
+  	[list Float float float {0}] \
+  	[list Integer int int {0}] \
+  	[list Long long long {0}]]
 
 testArrayMathArrayArrayArray within $types
 
@@ -1818,6 +1820,9 @@ testArrayMathArrayArrayArray within $types
 
 # The matrices contain negative elements, which will cause
 # this to return within(xxx[][], xxx[][], xxx[][]) to return false
+
+# FIXME: Why is it ok to have negative values for complex numbers
+# but not the others
 
 set types [list \
 	[list Complex ptolemy.math.Complex complex {0}] \
