fa36ec764ea18890b4074d3927f7f6940c54c496
cxh <cxh>
2003-05-01 23:19:09 +0000

    preinitialize(): If we get a LinkageError the first time we run, then the second and subsequent times we run we may get a NoClassDefFound exception.

--- ptolemy/matlab/Expression.java
+++ ptolemy/matlab/Expression.java
@@ -213,6 +213,16 @@ public class Expression extends TypedAtomicActor {
                  + "requires that Matlab be installed on the local machine."
                  + "Refer to $PTII/ptolemy/matlab/makefile for more"
                  + "information.");
+        } catch (NoClassDefFound ex2) {
+            // If we get a LinkageError the first time we run, then
+            // the second and subsequent times we run we may get a 
+            // NoClassDefFound exception 
+            throw new IllegalActionException(this, ex2
+                 "There was a problem invoking the Ptolemy II Matlab interface"
+                 + ".\nThe interface has been tested under Windows and Linux,\n"
+                 + "requires that Matlab be installed on the local machine."
+                 + "Refer to $PTII/ptolemy/matlab/makefile for more"
+                 + "information.");
         }
 
         // First set default debugging level, then check for more
