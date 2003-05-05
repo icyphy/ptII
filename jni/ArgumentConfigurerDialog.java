e5a1a375d6d10054eabbd2ead664a8dec464de27
cxh <cxh>
2003-05-05 21:09:46 +0000

    Fixed copyright

--- jni/ArgumentConfigurerDialog.java
+++ jni/ArgumentConfigurerDialog.java
@@ -1,7 +1,7 @@
 /* A top-level dialog window for configuring the arguments of an entity.
     Largely inspired of PortConfigurerDialog
 
-    Copyright (c) 2003 The Regents of the University of California.
+    Copyright (c) 1998-2003 The Regents of the University of California.
     All rights reserved.
     Permission is hereby granted, without written agreement and without
     license or royalty fees, to use, copy, modify, and distribute this
