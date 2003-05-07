d7fe34b6607417972727ef24aed999c0b51ad07a
cxh <cxh>
2003-05-07 03:51:03 +0000

    Fixed @see tag

--- ptolemy/gui/ShellInterpreter.java
+++ ptolemy/gui/ShellInterpreter.java
@@ -44,7 +44,7 @@ public interface ShellInterpreter {
     /** Evaluate the specified command. If this method returns null, then
      *  the associated ShellTextArea will not display a prompt until
      *  returnResult() is called.
-     *  @see ShellTextArea.returnResult(String)
+     *  @see #ShellTextArea.returnResult(String)
      *  @param command The command.
      *  @return The return value of the command, or null if there is none.
      *  @exception Exception If something goes wrong processing the command.
