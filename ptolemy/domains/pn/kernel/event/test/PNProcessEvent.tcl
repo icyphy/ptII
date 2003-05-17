06006f6579733dca57ff35a7e2589135f7dc66b3
cxh <cxh>
2003-05-17 07:58:05 +0000

    Added trailing curly bracket

--- ptolemy/domains/pn/kernel/event/test/PNProcessEvent.tcl
+++ ptolemy/domains/pn/kernel/event/test/PNProcessEvent.tcl
@@ -169,4 +169,5 @@ test PNProcessEvent-2.2 {get Methods} {
 	[$p3 getBlockingCause] [$p3 getCurrentState] \
 	[[$p3 getException] toString] \
 	[$p3 getFinishingCause] \
-} {.E0.A1 736 368 {ptolemy.kernel.util.IllegalActionException: This is a test exception
+    } {.E0.A1 736 368 {ptolemy.kernel.util.IllegalActionException: This is a test exception}}
+
