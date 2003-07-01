2960ad0824fb2428af5ad6fb86a521edb349ddee
cxh <cxh>
2003-07-01 15:28:27 +0000

    typo

--- ptolemy/actor/lib/Writer.java
+++ ptolemy/actor/lib/Writer.java
@@ -96,7 +96,8 @@ public class Writer extends Sink {
             _writer.flush();
             return super.postfire();
         } catch (IOException ex) {
-            throw new IllegalActionException(this, ex.getMessage());
+            throw new IllegalActionException(this, ex,
+                    "attributeChanged(" + attribute + ") failed");
         }
     }
 
@@ -107,9 +108,12 @@ public class Writer extends Sink {
      */
     public void setWriter(java.io.Writer writer) throws IllegalActionException {
         try {
-            if (_writer != null && _writer != _stdOut) _writer.close();
+            if (_writer != null && _writer != _stdOut) {
+                _writer.close();
+            }
         } catch (IOException ex) {
-            throw new IllegalActionException(this, ex.getMessage());
+            throw new IllegalActionException(this, ex,
+                    "setWriter(" + writer + ") failed");
         }
         if (writer != null) {
             _writer = writer;
@@ -123,9 +127,12 @@ public class Writer extends Sink {
      */
     public void wrapup() throws IllegalActionException {
         try {
-            if (_writer != null) _writer.flush();
+            if (_writer != null) {
+                _writer.flush();
+            }
         } catch (IOException ex) {
-            throw new IllegalActionException(this, ex.getMessage());
+            throw new IllegalActionException(this, ex,
+                    "wrapup(" + _writer + ") failed");
         }
     }
 
