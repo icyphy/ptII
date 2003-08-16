fa9b423dcd029c2d7045b84b709492a17f386ae1
cxh <cxh>
2003-08-16 21:44:59 +0000

    self loop error message changed

--- ptolemy/domains/de/lib/test/SimpleDelay.tcl
+++ ptolemy/domains/de/lib/test/SimpleDelay.tcl
@@ -90,8 +90,9 @@ test SimpleDelay-4.1 {test a self loop with the zero delay} {
     [java::field $add plus] link $r
     catch {[$e0 getManager] execute} msg
     list $msg
-} {{ptolemy.kernel.util.IllegalActionException: Zero delay self-loop on actor: .top.add
-  in .top.DEDirector}}
+} {{ptolemy.graph.GraphConstructionException: Cannot add a self loop in an acyclic graph.
+A self loop was attempted on the following node.
+ptolemy.actor.lib.AddSubtract {.top.add}}
 
 test SimpleDelay-5.1 {test a more complex loop with the zero delay} {
     set e0 [deModel 3.0]
--- ptolemy/domains/de/lib/test/TimedDelay.tcl
+++ ptolemy/domains/de/lib/test/TimedDelay.tcl
@@ -104,8 +104,9 @@ test TimedDelay-4.1 {test a self loop with the zero TimedDelay} {
     [java::field $add plus] link $r
     catch {[$e0 getManager] execute} msg
     list $msg
-} {{ptolemy.kernel.util.IllegalActionException: Zero delay self-loop on actor: .top.add
-  in .top.DEDirector}}
+} {ptolemy.graph.GraphConstructionException: Cannot add a self loop in an acyclic graph.
+A self loop was attempted on the following node.
+ptolemy.actor.lib.AddSubtract {.top.add}}
 
 test TimedDelay-5.1 {test a more complex loop with the zero TimedDelay} {
     set e0 [deModel 3.0]
