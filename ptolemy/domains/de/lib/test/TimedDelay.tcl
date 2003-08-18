d8be3a848822c0cf3350a0bd2689afa6b284c68f
cxh <cxh>
2003-08-18 14:55:14 +0000

    Added curlybrackets to output from TimedDelay-4.1

--- ptolemy/domains/de/lib/test/TimedDelay.tcl
+++ ptolemy/domains/de/lib/test/TimedDelay.tcl
@@ -104,9 +104,9 @@ test TimedDelay-4.1 {test a self loop with the zero TimedDelay} {
     [java::field $add plus] link $r
     catch {[$e0 getManager] execute} msg
     list $msg
-} {ptolemy.graph.GraphConstructionException: Cannot add a self loop in an acyclic graph.
+} {{ptolemy.graph.GraphConstructionException: Cannot add a self loop in an acyclic graph.
 A self loop was attempted on the following node.
-ptolemy.actor.lib.AddSubtract {.top.add}}
+ptolemy.actor.lib.AddSubtract {.top.add}}}
 
 test TimedDelay-5.1 {test a more complex loop with the zero TimedDelay} {
     set e0 [deModel 3.0]
