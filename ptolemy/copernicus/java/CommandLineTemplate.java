01e6f0485db0f146aace378b80eb67dd229f2502
cxh <cxh>
2003-08-18 05:23:10 +0000

    Refer to static field in a static way, reorgainized imports

--- ptolemy/copernicus/java/CommandLineTemplate.java
+++ ptolemy/copernicus/java/CommandLineTemplate.java
@@ -31,13 +31,12 @@
 
 package ptolemy.copernicus.java;
 
+import java.util.Iterator;
+import java.util.List;
+
 import ptolemy.actor.CompositeActor;
 import ptolemy.actor.Manager;
 import ptolemy.kernel.util.IllegalActionException;
-
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
 
 
 /////////////////////////////////////////////////////////////////
@@ -78,7 +77,7 @@ public class CommandLineTemplate {
         // If the -test arg was set, then exit after 2 seconds.
         if (_test) {
             try {
-                Thread.currentThread().sleep(2000);
+                Thread.sleep(2000);
             } catch (InterruptedException e) {
             }
             System.exit(0);
