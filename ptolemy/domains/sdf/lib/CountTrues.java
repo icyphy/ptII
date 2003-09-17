df85e09a09bab466cbe2c9732337e533542c27f5
neuendor <neuendor>
2003-09-17 18:40:34 +0000

    added CountTrues

---
+++ ptolemy/domains/sdf/lib/CountTrues.java
@@ -0,0 +1,147 @@
+/* An actor that counts true inputs.
+
+ Copyright (c) 1998-2003 The Regents of the University of California.
+ All rights reserved.
+ Permission is hereby granted, without written agreement and without
+ license or royalty fees, to use, copy, modify, and distribute this
+ software and its documentation for any purpose, provided that the above
+ copyright notice and the following two paragraphs appear in all copies
+ of this software.
+
+ IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+ FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+ ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+ THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+ SUCH DAMAGE.
+
+ THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+ INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+ PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+ CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ ENHANCEMENTS, OR MODIFICATIONS.
+
+                                        PT_COPYRIGHT_VERSION_2
+                                        COPYRIGHTENDKEY
+
+@ProposedRating Red (eal@eecs.berkeley.edu)
+@AcceptedRating Red (yuhong@eecs.berkeley.edu)
+*/
+
+package ptolemy.domains.sdf.lib;
+
+import ptolemy.actor.TypedAtomicActor;
+import ptolemy.actor.TypedIOPort;
+import ptolemy.actor.lib.Transformer;
+import ptolemy.data.BooleanToken;
+import ptolemy.data.IntToken;
+import ptolemy.data.ScalarToken;
+import ptolemy.data.Token;
+import ptolemy.data.expr.Parameter;
+import ptolemy.data.type.BaseType;
+import ptolemy.kernel.CompositeEntity;
+import ptolemy.kernel.util.IllegalActionException;
+import ptolemy.kernel.util.NameDuplicationException;
+import ptolemy.kernel.util.Workspace;
+
+//////////////////////////////////////////////////////////////////////////
+//// CountTrues
+/**
+Read the given number of input booleans and output the number that are true.
+
+@author Steve Neuendorffer
+@version $Id$
+@since Ptolemy II 0.3
+*/
+
+public class CountTrues extends Transformer {
+
+    /** Construct an actor with the given container and name.
+     *  @param container The container.
+     *  @param name The name of this actor.
+     *  @exception IllegalActionException If the actor cannot be contained
+     *   by the proposed container.
+     *  @exception NameDuplicationException If the container already has an
+     *   actor with this name.
+     */
+    public CountTrues(CompositeEntity container, String name)
+            throws NameDuplicationException, IllegalActionException  {
+        super(container, name);
+
+        input.setTypeEquals(BaseType.BOOLEAN);
+        output.setTypeEquals(BaseType.INT);
+
+        blockSize = new Parameter(this, "blockSize", new IntToken(1));
+        blockSize.setTypeEquals(BaseType.INT);
+
+        input_tokenConsumptionRate = 
+            new Parameter(input, "tokenConsumptionRate");
+        input_tokenConsumptionRate.setExpression("blockSize");
+        input_tokenConsumptionRate.setTypeEquals(BaseType.INT);
+    }
+
+    ///////////////////////////////////////////////////////////////////
+    ////                     ports and parameters                  ////
+
+    /** The number of input tokens to read before outputting a count.
+     *  The input is of type integer and has a default value of 1.
+     */
+    public Parameter blockSize;
+
+    /** The number of tokens consumed by the input port.
+     */
+    public Parameter input_tokenConsumptionRate;
+
+    ///////////////////////////////////////////////////////////////////
+    ////                         public methods                    ////
+
+    /** Clone the actor into the specified workspace. This calls the
+     *  base class and then sets the type constraints.
+     *  @param workspace The workspace for the new object.
+     *  @return A new actor.
+     *  @exception CloneNotSupportedException If a derived class has
+     *   an attribute that cannot be cloned.
+     */
+    public Object clone(Workspace workspace)
+            throws CloneNotSupportedException {
+        CountTrues newObject = (CountTrues)super.clone(workspace);
+        return newObject;
+    }
+
+    /** Read at most one token from each input channel and broadcast the one
+     *  with the largest value to the <i>maximumValue</i>output.
+     *  In addition, broadcast its channel number to the <i>channelNumber</i>
+     *  output.  If there is no input, then produce no output.
+     *  @exception IllegalActionException If there is no director.
+     */
+    public void fire() throws IllegalActionException {
+        int count = ((IntToken)blockSize.getToken()).intValue();
+        Token[] inputBlock = input.get(0, count);
+        int trueCount = 0;
+        for (int i = 0; i < count; i += 1) {
+            if(((BooleanToken)inputBlock[i]).booleanValue()) {
+                trueCount++;
+            }
+        }
+
+        output.send(0, new IntToken(trueCount));
+    }
+
+    /** Return true if the input port has enough tokens for this actor to
+     *  fire. The number of tokens required is determined by the
+     *  value of the <i>blockSize</i> parameter.
+     *  @return boolean True if there are enough tokens at the input port
+     *   for this actor to fire.
+     *  @exception IllegalActionException If the hasToken() query to the
+     *   input port throws it.
+     *  @see ptolemy.actor.IOPort#hasToken(int, int)
+     */
+    public boolean prefire() throws IllegalActionException {
+        int length = ((IntToken)blockSize.getToken()).intValue();
+        boolean result = input.hasToken(0, length);
+        if (_debugging) {
+            _debug("Called prefire(), which returns " + result + ".");
+        }
+        return result;
+    }
+}
