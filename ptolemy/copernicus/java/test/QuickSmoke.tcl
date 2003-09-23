42d8ae09ec1cabf2557bbcc59155d178f5e1b2c4
neuendor <neuendor>
2003-09-23 17:45:43 +0000

    created

---
+++ ptolemy/copernicus/java/test/QuickSmoke.tcl
@@ -0,0 +1,78 @@
+# Tests for deep codegen
+#
+# @Author: Steve Neuendorffer, Christopher Hylands
+#
+# @Version: $Id$
+#
+# @Copyright (c) 2000-2003 The Regents of the University of California.
+# All rights reserved.
+#
+# Permission is hereby granted, without written agreement and without
+# license or royalty fees, to use, copy, modify, and distribute this
+# software and its documentation for any purpose, provided that the
+# above copyright notice and the following two paragraphs appear in all
+# copies of this software.
+#
+# IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+# FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+# ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+# THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+#
+# THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+# INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+# PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+# CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+# ENHANCEMENTS, OR MODIFICATIONS.
+#
+# 						PT_COPYRIGHT_VERSION_2
+# 						COPYRIGHTENDKEY
+#######################################################################
+
+# Ptolemy II bed, see /users/cxh/ptII/doc/coding/testing.html for more information.
+
+# Load up the test definitions.
+if {[string compare test [info procs test]] == 1} then {
+    source testDefs.tcl
+} {}
+
+if {[string compare test [info procs autoShallowCG]] == 1} then {
+    source sootDeepCodeGeneration.tcl
+} {}
+
+
+if {[info procs jdkClassPathSeparator] == "" } then { 
+    source [file join $PTII util testsuite jdktools.tcl]
+}
+
+# Uncomment this to get a full report, or set in your Tcl shell window.
+#set VERBOSE 1
+
+if {[info procs sootCodeGeneration] == "" } then { 
+    source [file join $PTII util testsuite codegen.tcl]
+}
+
+# Uncomment this to get a full report, or set in your Tcl shell window.
+# set VERBOSE 1
+
+
+######################################################################
+####
+#
+
+# First, do an SDF test just to be sure things are working
+test QuickSmoke-1.1 {Compile and run the SDF IIR test} {
+    set result [sootCodeGeneration \
+	    [file join $relativePathToPTII ptolemy actor lib test auto \
+		 IIR.xml] "Deep" 1000 0 0 runSnapshots]
+    list {}
+} {{}}
+
+
+test QuickSmoke-1.2 {Compile and run the SDF OrthogonalCom test} {
+    set result [sootCodeGeneration \
+  	    [file join $relativePathToPTII ptolemy domains sdf demo OrthogonalCom \
+		 OrthogonalCom.xml] "Deep" 1000 0 0 runSnapshots]
+    list {}
+} {{}}
