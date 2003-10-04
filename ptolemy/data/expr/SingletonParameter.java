e1f21f8d8a1a728012ac48a78efa98143551cb83
eal <eal>
2003-10-04 16:55:50 +0000

    Added SingletonParameter.

---
+++ ptolemy/data/expr/SingletonParameter.java
@@ -0,0 +1,124 @@
+/* A parameter that is a singleton.
+
+ Copyright (c) 1998-2003 The Regents of the University of California.
+ All rights reserved.
+ Permission is hereby granted, without written agreement and without
+ license or royalty fees, to use, copy, modify, and distribute this
+ software and its documentation for any purpose, provided that the above
+ copyright notice and the following two paragraphs appear in all copies
+ of this software.
+
+ IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+ FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+ ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+ THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+ SUCH DAMAGE.
+
+ THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+ INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+ PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+ CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ ENHANCEMENTS, OR MODIFICATIONS.
+
+                                        PT_COPYRIGHT_VERSION_2
+                                        COPYRIGHTENDKEY
+
+@ProposedRating Yellow (eal@eecs.berkeley.edu)
+@AcceptedRating Red (cxh@eecs.berkeley.edu)
+
+*/
+
+package ptolemy.data.expr;
+
+import ptolemy.kernel.util.Attribute;
+import ptolemy.kernel.util.IllegalActionException;
+import ptolemy.kernel.util.NameDuplicationException;
+import ptolemy.kernel.util.NamedObj;
+
+//////////////////////////////////////////////////////////////////////////
+//// SingletonParameter
+/**
+This subclass of Parameter is identical to Parameter except that it
+is a singleton, meaning that when its container is set, if the container
+already contains an attribute with the same name, then that attribute
+is first removed.
+
+@author Edward A. Lee
+@version $Id$
+*/
+
+public class SingletonParameter extends Parameter {
+
+    /** Construct a parameter with the given name contained by the specified
+     *  entity. The container argument must not be null, or a
+     *  NullPointerException will be thrown.  This parameter will use the
+     *  workspace of the container for synchronization and version counts.
+     *  If the name argument is null, then the name is set to the empty string.
+     *  The object is not added to the list of objects in the workspace
+     *  unless the container is null.
+     *  Increment the version of the workspace.
+     *  @param container The container.
+     *  @param name The name of the parameter.
+     *  @exception IllegalActionException If the parameter is not of an
+     *   acceptable class for the container.
+     *  @exception NameDuplicationException If the name coincides with
+     *   a parameter already in the container.
+     */
+    public SingletonParameter(NamedObj container, String name)
+            throws IllegalActionException, NameDuplicationException {
+        super(container, name);
+    }
+    
+    ///////////////////////////////////////////////////////////////////
+    ////                         public methods                    ////
+
+    /** Remove any previous attribute in the container that has the same
+     *  name as this attribute, and then call the base class method to set
+     *  the container. If the container is not in the same workspace as
+     *  this attribute, throw an exception.
+     *  If this attribute is already contained by the NamedObj, do nothing.
+     *  If the attribute already has a container, remove
+     *  this attribute from its attribute list first.  Otherwise, remove
+     *  it from the directory of the workspace, if it is there.
+     *  If the argument is null, then remove this attribute from its
+     *  container. It is not added to the workspace directory, so this
+     *  could result in this object being garbage collected.
+     *  <p>
+     *  This method is write-synchronized on the
+     *  workspace and increments its version number.
+     *  @param container The container to attach this attribute to.
+     *  @exception IllegalActionException If this attribute is not of the
+     *   expected class for the container, or it has no name,
+     *   or the attribute and container are not in the same workspace, or
+     *   the proposed container would result in recursive containment.
+     *  @exception NameDuplicationException If the container already has
+     *   an attribute with the name of this attribute that is of class
+     *   SingletonConfigurableAttribute.
+     */
+    public void setContainer(NamedObj container)
+            throws IllegalActionException, NameDuplicationException {
+        Attribute previous = null;
+        if (container != null) {
+            previous = container.getAttribute(getName());
+            if (previous != null) {
+                previous.setContainer(null);
+            }
+        }
+        try {
+            super.setContainer(container);
+        } catch (IllegalActionException ex) {
+            // Restore previous.
+            if (previous != null) {
+                previous.setContainer(container);
+            }
+            throw ex;
+        } catch (NameDuplicationException ex) {
+            // Restore previous.
+            if (previous != null) {
+                previous.setContainer(container);
+            }
+            throw ex;
+        }
+    }
+}
