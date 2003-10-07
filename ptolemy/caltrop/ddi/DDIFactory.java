8b6393cea7751f540d527e4f9163e2a18355f828
cxh <cxh>
2003-10-07 16:46:44 +0000

    Split up long lines

--- ptolemy/caltrop/ddi/DDIFactory.java
+++ ptolemy/caltrop/ddi/DDIFactory.java
@@ -43,12 +43,13 @@ import ptolemy.caltrop.actors.CalInterpreter;
 public interface DDIFactory {
     /**
      * Create a domain dependent plugin.
-     * @param ptActor The instance of {@link ptolemy.actor.Actor ptolemy.actor.Actor} that the plugin will be associated
-     * with.
+     * @param ptActor The instance of {@link ptolemy.actor.Actor
+     * ptolemy.actor.Actor} that the plugin will be associated with.
      * @param actor The abstract syntax tree of the CAL source.
      * @param context The context that the plugin will use.
      * @param env The environment that the plugin will use.
      * @return A <tt>DDI</tt> for this domain.
      */
-    DDI create(CalInterpreter ptActor, Actor actor, Context context, Environment env);
+    DDI create(CalInterpreter ptActor, Actor actor, Context context,
+            Environment env);
 }
