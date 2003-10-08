9734a4ac25612b3064abb71882bdf149ecba1ca7
cxh <cxh>
2003-10-08 21:15:49 +0000

    Initial setup of security actors

---
+++ ptolemy/actor/lib/security/Cryptography.java
@@ -0,0 +1,338 @@
+/* A base class for cryptographic actors.
+
+ Copyright (c) 1998-2003 The Regents of the University of California.
+ All rights reserved.
+ Permission is hereby granted, without written agreement and without
+ license or royalty fees, to use, copy, modify, and distribute this
+ software and its documentation for any purpose, provided that the above
+ copyright notice and the following two paragraphs appear in all copies
+ of this software.
+
+ IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+ FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+ ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+ THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+ SUCH DAMAGE.
+
+ THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+ INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+ PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+ CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ ENHANCEMENTS, OR MODIFICATIONS.
+
+                                        PT_COPYRIGHT_VERSION_2
+                                        COPYRIGHTENDKEY
+
+@ProposedRating Yellow (rnreddy@ptolemy.eecs.berkeley.edu)
+@AcceptedRating Red (ptolemy@ptolemy.eecs.berkeley.edu)
+*/
+
+package ptolemy.actor.lib.security;
+
+
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+
+import javax.crypto.Cipher;
+import javax.crypto.NoSuchPaddingException;
+
+import ptolemy.actor.TypedAtomicActor;
+import ptolemy.data.ArrayToken;
+import ptolemy.data.IntToken;
+import ptolemy.data.StringToken;
+import ptolemy.data.Token;
+import ptolemy.data.UnsignedByteToken;
+import ptolemy.data.expr.Parameter;
+import ptolemy.data.type.ArrayType;
+import ptolemy.data.type.BaseType;
+import ptolemy.domains.sdf.kernel.SDFIOPort;
+import ptolemy.kernel.CompositeEntity;
+import ptolemy.kernel.util.IllegalActionException;
+import ptolemy.kernel.util.NameDuplicationException;
+
+//////////////////////////////////////////////////////////////////////////
+//// Cryptography
+/**
+This a base class that implements general and helper functions used by
+cryptographic actors. Actors extending this class take in an unsigned byte
+array at the <i>input</i>, perform the transformation specified in the
+<i>algorithm</i> parameter and send a unsigned byte array on the <i>output</i>.
+The algorithms that maybe implemented are limited to the ciphers that are
+implemented by "providers" following the JCE specifications and installed in
+the machine being run. The mode and padding can also be specified in the mode
+and padding parameters.  In case a provider specific instance of an algorithm
+is needed, the provider may also be specified in the <i>provider</i> parameter.
+This class takes care of basic initialization of the subclasses. The
+<i>keySize</i> also allows implementations of algorithms with various key sizes.
+
+This class and its subclasses rely on the Java Cryptography Extension (JCE)
+and Java Cryptography Architecture(JCA).
+
+@author Rakesh Reddy
+@version $Id$
+*/
+
+public class Cryptography extends TypedAtomicActor {
+
+    /** Construct an actor with the given container and name.
+     *  @param container The container.
+     *  @param name The name of this actor.
+     *  @exception IllegalActionException If the actor cannot be contained
+     *   by the proposed container.
+     *  @exception NameDuplicationException If the container already has an
+     *   actor with this name.
+     */
+    public Cryptography(CompositeEntity container, String name)
+            throws NameDuplicationException, IllegalActionException  {
+        super(container, name);
+
+        input = new SDFIOPort(this, "input", true, false);
+        input.setTypeEquals(new ArrayType(BaseType.UNSIGNED_BYTE));
+
+        output = new SDFIOPort(this, "output", false, true);
+        output.setTypeEquals(new ArrayType(BaseType.UNSIGNED_BYTE));
+
+        algorithm = new Parameter(this, "algorithm");
+        algorithm.setTypeEquals(BaseType.STRING);
+        algorithm.setExpression("");
+
+        provider = new Parameter(this, "provider");
+        provider.setTypeEquals(BaseType.STRING);
+        provider.setExpression("");
+
+        mode = new Parameter(this, "mode");
+        mode.setTypeEquals(BaseType.STRING);
+        mode.setExpression("");
+
+        padding = new Parameter(this, "padding");
+        padding.setTypeEquals(BaseType.STRING);
+        padding.setExpression("");
+
+        keySize = new Parameter(this, "keySize");
+        keySize.setTypeEquals(BaseType.INT);
+        keySize.setExpression("0");
+    }
+
+    ///////////////////////////////////////////////////////////////////
+    ////                     ports and parameters                  ////
+
+    /** Specifies the algorithm of the cipher to use for decrypting the data
+     *  and making the key.  The algorithm is specified as a string input. The
+     *  algorithms are limited to those implemented by providers using the Java
+     *  JCE which are found on the system.
+     */
+    public Parameter algorithm;
+
+    /** Used to specify a provider for the given algorithm.  Takes the
+     *  algorithm name in a string format. If left blank the system chooses the
+     *  provider based the JCE architecture.
+     */
+    public Parameter provider;
+
+    /** The padding scheme used by the cipher during encryption.  The padding
+     *  is specified as a string and includes the options of NOPADDING and
+     *  PKCS#5.  This should be the same as the encryption actor.  If left
+     *  blank the default setting for the algorithm is used.
+     */
+    public Parameter padding;
+
+    /** The mode of the block cipher that was for used encryption.  The mode is
+     *  specified as a string and includes ECB, CBC, CFB and OFB.  The modes
+     *  are limited by their implementation in the provider library.  If left
+     *  blank, the default setting for the algorithm used.  This should be the
+     *  same as the mode used for in the corresponding encryption actor.
+     */
+    public Parameter mode;
+
+    /** Specifies the size of the key to be created.  This is an int value
+     *  representing the number of bits.  A default value exists for certain
+     *  algorithms.  If a key is not specified and no default value is found
+     *  then an error is produced.
+     */
+    public Parameter keySize;
+
+
+    /** This port takes in as an UnsignedByteArray and performs a
+     *  transformation on the data.
+     */
+    public SDFIOPort input;
+
+    /** This port sends out the transformed data received from <i>input</i> in
+     *  the form of an UnsignedByteArray.
+     */
+    public SDFIOPort output;
+
+
+    ///////////////////////////////////////////////////////////////////
+    ////                         public methods                    ////
+
+    /** This method takes the data from the <i>input</i> and transforms the
+     *  data based on the <i>algorithm</i>, <i>provider</i>, <i>mode</i> and
+     *  <i>padding</i>.  This is the sent on the <i>output</i>.  All parameters
+     *  should be the same as the corresponding encryption or decryption
+     *  actor.
+     * @exception IllegalActionException if thrown by the base class.
+     */
+    public void fire() throws IllegalActionException {
+        super.fire();
+        try{
+            if(input.hasToken(0)){
+                byte[] dataBytes =
+                    _arrayTokenToUnsignedByteArray((ArrayToken)input.get(0));
+                dataBytes=_crypt(dataBytes);
+                output.send(0, _unsignedByteArrayToArrayToken(dataBytes));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new IllegalActionException(this.getName() + e.getMessage());
+        }
+
+    }
+
+    /** This method retrieves the <i>algorithm</i>, <i>provider</i>,
+     *  <i>mode</i>, <i>keySize</i> and <i>padding</i>.  The cipher is also
+     *  initialized.
+     *
+     * @exception IllegalActionException if thrown by the base class.
+     * @exception NoSuchAlgorihmException when the algorithm is not found.
+     * @exception NoSuchPaddingException when the padding scheme is illegal
+     *     for the given algorithm.
+     * @exception NoSuchProviderException if the specified provider does not
+     *     exist.
+     */
+    public void initialize() throws IllegalActionException {
+        super.initialize();
+        _algorithm = ((StringToken)algorithm.getToken()).stringValue();
+        _provider = ((StringToken)provider.getToken()).stringValue();
+        _padding = ((StringToken)padding.getToken()).stringValue();
+        _mode = ((StringToken)mode.getToken()).stringValue();
+        _keySize = ((IntToken)keySize.getToken()).intValue();
+        if(_keySize == 0){
+            _defaultKeySize();
+        }
+
+        try{
+            _cipher =
+                Cipher.getInstance(_algorithm+"/"+mode+"/"+padding, _provider);
+        } catch(NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (NoSuchPaddingException e) {
+            e.printStackTrace();
+        } catch (NoSuchProviderException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+    ///////////////////////////////////////////////////////////////////
+    ////                         Protected Methods                 ////
+
+    /** Take an ArrayToken and converts it to an array of unsigned bytes.
+     *
+     * @param dataArrayToken to be converted to a unsigned byte array.
+     * @return dataBytes the resulting unsigned byte array.
+     */
+    protected byte[] _arrayTokenToUnsignedByteArray(ArrayToken dataArrayToken){
+        byte[] dataBytes = new byte[dataArrayToken.length()];
+        for (int j = 0; j < dataArrayToken.length(); j++) {
+            UnsignedByteToken dataToken =
+                (UnsignedByteToken)dataArrayToken.getElement(j);
+            dataBytes[j] = (byte)dataToken.byteValue();
+        }
+        return dataBytes;
+    }
+
+    /** Determine what key to use and transforms the data with the specified
+     *  key.  Receives the data to be encrypted in as a byte array and returns
+     *  a byte array.  The base class simply returns the data but all base
+     *  classes should implement this method.
+     *
+     * @param initialData data to be transformed.
+     * @return byte[] the transformed data.
+     * @throws IllegalActionException when exception is thrown.
+     */
+    protected byte[] _crypt(byte[] initialData)throws IllegalActionException{
+        return initialData;
+    }
+
+    /** If a key is not specified in the parameter, this function checks for a
+     *  default key size. If the specified algorithm is found in the list below
+     *  then the key size is set to this value.  If the algorithm is not found
+     *  in this list then and exception is thrown.
+     *
+     * @throws IllegalActionException when a key size is not specified and the
+     *          does not exist in the list below.
+     */
+    protected void _defaultKeySize() throws IllegalActionException{
+        if (_algorithm=="Blowfish"){
+            // valid values are: starting from 40bit up to
+            // 448 in 8-bit increments
+            _keySize=448;
+        }
+        else if (_algorithm=="CAST5"){
+            //valid values are: starting from 40bit up to 128bit using
+            // 8bit steps.
+            _keySize=128;
+        }
+        else if (_algorithm=="DES"){
+            _keySize=56;
+        }
+        else if (_algorithm=="TripleDES"||_algorithm=="DESede"){
+            _keySize=3*56;
+        }
+        else if (_algorithm=="Rijndael"){
+            _keySize=256; //valid values are: 128, 192, 256
+        }
+        else if (_algorithm=="SKIPJACK"){
+            // fixed size: 80 bits
+            _keySize=80;
+        }
+        else if (_algorithm=="Square"){
+            _keySize=128;
+        }
+        else{
+            throw new IllegalActionException(this.getName()+
+                    "No default key size found.  " +
+                    "Must be specified in parameters");
+        }
+    }
+
+    /** Take an array of unsigned bytes and convert it to an ArrayToken.
+     *
+     * @param dataBytes data to be converted to an ArrayToken.
+     * @return dataArrayToken the resulting ArrayToken.
+     * @throws IllegalActionException if ArrayTOken can not be created.
+     */
+    protected ArrayToken _unsignedByteArrayToArrayToken( byte[] dataBytes)
+            throws IllegalActionException{
+        int bytesAvailable = dataBytes.length;
+        Token[] dataArrayToken = new Token[bytesAvailable];
+        for (int j = 0; j < bytesAvailable; j++) {
+            dataArrayToken[j] = new UnsignedByteToken(dataBytes[j]);
+        }
+        return new ArrayToken(dataArrayToken);
+    }
+
+    ///////////////////////////////////////////////////////////////////
+    ////                         protected variables               ////
+
+    // The name of the algorithm to be used.
+    protected String _algorithm;
+
+    // The key size to be used for the transformation.
+    protected int _keySize;
+
+    // The mode for transformation.
+    protected String _mode;
+
+    //The padding for transformation.
+    protected String _padding;
+
+    // The provider for transformation.
+    protected String _provider;
+
+    // The cipher to be used for transformation.
+    protected Cipher _cipher;
+}
---
+++ ptolemy/actor/lib/security/SymmetricEncryption.java
@@ -0,0 +1,299 @@
+/* Creates and sends a key to a SymmetricDecryption and encrypts incoming data
+   based on a given symmetric algorithm.
+
+ Copyright (c) 1998-2003 The Regents of the University of California.
+ All rights reserved.
+ Permission is hereby granted, without written agreement and without
+ license or royalty fees, to use, copy, modify, and distribute this
+ software and its documentation for any purpose, provided that the above
+ copyright notice and the following two paragraphs appear in all copies
+ of this software.
+
+ IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+ FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+ ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+ THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+ SUCH DAMAGE.
+
+ THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+ INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+ PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+ CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ ENHANCEMENTS, OR MODIFICATIONS.
+
+                                        PT_COPYRIGHT_VERSION_2
+                                        COPYRIGHTENDKEY
+
+@ProposedRating Yellow (rnreddy@andrew.cmu.edu)
+@AcceptedRating Red (ptolemy@ptolemy.eecs.berkeley.edu)
+*/
+
+package ptolemy.actor.lib.security;
+
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.security.AlgorithmParameters;
+import java.security.InvalidKeyException;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
+
+import ptolemy.actor.NoRoomException;
+import ptolemy.data.type.ArrayType;
+import ptolemy.data.type.BaseType;
+import ptolemy.domains.sdf.kernel.SDFIOPort;
+import ptolemy.kernel.CompositeEntity;
+import ptolemy.kernel.util.IllegalActionException;
+import ptolemy.kernel.util.NameDuplicationException;
+
+//////////////////////////////////////////////////////////////////////////
+//// SymmetricEncryption
+/**
+This actor takes an unsigned byte array at the input and encrypts the message.
+The resulting output is an unsigned byte array. Various ciphers that are
+implemented by "providers" and installed maybe used by specifying the algorithm
+in the <i>algorithm</i> parameter.  The specified algorithm must be symmetric.
+The mode and padding can also be specified in the mode and padding parameters.
+In case a provider specific instance of an algorithm is needed the provider may
+also be specified in the provider parameter. This actor sends its secret key on
+the <i>keyOut</i> port to a decryption actor as an unsigned byte array.  This
+key should be protected in some manner as the security of the encrypted message
+relies on the secrecy of this key. Key creation is done in pre-initialization
+and is put on the <i>keyOut</i> port during initialization so the decryption
+actor has a key to use when its first fired.
+
+The following actor relies on the Java Cryptography Architecture (JCA) and Java
+Cryptography Extension (JCE).
+
+
+TODO: include sources of information on JCE cipher and algorithms
+
+TODO: Use cipher streaming to allow for easier file input reading.
+@author Rakesh Reddy
+@version $Id$
+*/
+
+public class SymmetricEncryption extends CipherActor {
+
+    /** Construct an actor with the given container and name.
+     *  @param container The container.
+     *  @param name The name of this actor.
+     *  @exception IllegalActionException If the actor cannot be contained
+     *   by the proposed container.
+     *  @exception NameDuplicationException If the container already has an
+     *   actor with this name.
+     */
+    public SymmetricEncryption(CompositeEntity container, String name)
+            throws NameDuplicationException, IllegalActionException  {
+        super(container, name);
+
+        keyOut = new SDFIOPort(this, "keyOut", false, true);
+        keyOut.setTypeEquals(new ArrayType(BaseType.UNSIGNED_BYTE));
+
+        parameters = new SDFIOPort(this, "parameters", false, true);
+        parameters.setTypeEquals(new ArrayType(BaseType.UNSIGNED_BYTE));
+
+    }
+
+    ///////////////////////////////////////////////////////////////////
+    ////                     ports and parameters                  ////
+
+
+    /** This port outputs the key to be used by the SymmetricDecryption actor
+     *  as an unsigned byte array.
+     */
+    public SDFIOPort keyOut;
+
+    public SDFIOPort parameters;
+
+
+
+    ///////////////////////////////////////////////////////////////////
+    ////                         public methods                    ////
+
+    /** If there is a token on the <i>input</i> port, this method takes the
+     *  data from the <i>input</i> and encrypts the data based on the
+     *  <i>algorithm</i>, <i>provider</i>, <i>mode</i> and <i>padding</i>
+     *  using the created secret key.  This is then sent on the
+     *  <i>output</i>.  The public key is also sent out on the <i>keyOut</i>
+     *  port.  All parameters should be the same as the corresponding
+     *  decryption actor.  The call for encryption is done in the base class.
+     *
+     *  @exception IllegalActionException if thrown by base class.
+     */
+    public void fire() throws IllegalActionException {
+
+        keyOut.send(0, _unsignedByteArrayToArrayToken(_keyToBytes(_secretKey)));
+        if(_algParams != null){
+            try {
+
+                parameters.send(0, _unsignedByteArrayToArrayToken(_algParams.getEncoded()));
+
+            } catch (NoRoomException e) {
+                e.printStackTrace();
+                throw new IllegalActionException(this.getName()+e.getMessage());
+            } catch (IllegalActionException e) {
+                e.printStackTrace();
+                throw new IllegalActionException(this.getName()+e.getMessage());
+            } catch (IOException e) {
+                e.printStackTrace();
+                throw new IllegalActionException(this.getName()+e.getMessage());
+            }
+        }
+
+        //            if(FIRST_RUN == true){
+        //                _baos = new ByteArrayOutputStream();
+        //                try {
+        //
+        //                    _cipher.init(Cipher.ENCRYPT_MODE, _secretKey, _algParams);
+        //
+        //                } catch (InvalidKeyException e) {
+        //                    // TODO Auto-generated catch block
+        //                    e.printStackTrace();
+        //                } catch (InvalidAlgorithmParameterException e) {
+        //                    // TODO Auto-generated catch block
+        //                    e.printStackTrace();
+        //                }
+        //                _cos = new CipherOutputStream(_baos, _cipher);
+        //                FIRST_RUN = false;
+        //            }
+        //        //} catch (NoRoomException e) {
+        //       e.printStackTrace();
+        //  } catch (IllegalActionException e) {
+        //    e.printStackTrace();
+        //        } catch (IOException e) {
+        //            e.printStackTrace();
+        //        } catch (InvalidKeyException e) {
+        //            e.printStackTrace();
+        //        } catch (InvalidAlgorithmParameterException e) {
+        //            e.printStackTrace();
+        //        }
+        //        super.fire();
+    }
+
+    /** Get an instance of the cipher and outputs the key required for
+     *  decryption.
+     *  @exception IllegalActionException if thrown by base class.
+     *  @exception NoSuchAlgorihmException if the algorithm is not found.
+     *  @exception NoSuchPaddingException if the padding scheme is illegal
+     *      for the given algorithm.
+     *  @exception NoSuchProviderException if the specified provider does not
+     *      exist.
+     */
+    public void initialize() throws IllegalActionException {
+        try{
+            super.initialize();
+            _secretKey = (SecretKey)_createSymmetricKey();
+            //keyOut.send(0, _unsignedByteArrayToArrayToken(_keyToBytes(_secretKey)));
+
+            _cipher.init(Cipher.ENCRYPT_MODE, _secretKey);
+
+            _algParams = _cipher.getParameters();
+            //if(_algParams != null){
+            //    parameters.send(0, _unsignedByteArrayToArrayToken(_algParams.getEncoded()));
+            //}
+
+            FIRST_RUN=true;
+        } catch (InvalidKeyException e) {
+            e.printStackTrace();
+            throw new IllegalActionException (this.getName()+e.getMessage());
+        } catch (NoRoomException e) {
+            e.printStackTrace();
+            throw new IllegalActionException (this.getName()+e.getMessage());
+        } catch (IllegalActionException e) {
+            e.printStackTrace();
+            throw new IllegalActionException (this.getName()+e.getMessage());
+        }
+    }
+
+    /** Sets token production for initialize to one and resolves scheduling.
+     *
+     * @throws IllegalActionException if thrown by base class.
+     */
+    public void preinitialize() throws IllegalActionException {
+        super.preinitialize();
+        keyOut.setTokenInitProduction(1);
+        parameters.setTokenInitProduction(1);
+        getDirector().invalidateResolvedTypes();
+    }
+
+
+
+    /** Encrypt the data with the specified key.  Receives the data to be
+     *  encrypted as a byte array and returns a byte array.  Also creates
+     *  and sends an initialization vector if necessary.
+     *
+     * @param dataBytes the data to be encrypted.
+     * @return byte[] the encrypted data.
+     * @throws IllegalActionException if exception below it thrown.
+     * @exception IOException if error occurs in ByteArrayOutputStream.
+     * @exception InvalideKeyException if key is invalid.
+     * @exception BadPaddingException if padding is bad.
+     * @exception IllegalBockSizeException if illegal block size.
+     */
+    protected byte[] _process(byte[] dataBytes)throws IllegalActionException{
+        //      ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        _baos.reset();
+        //        bais = new ByteArrayInputStream(initialData);
+        //        int length = 0;
+        //        byte [] buffer = new byte [BUFFER_SIZE];
+        //        try{
+        //            while ((length = bais.read(buffer)) != -1) {
+        //                _cos.write(buffer, 0, length);
+        //            }
+        //            _cos.flush();
+        //        } catch (IOException e){
+        //            e.printStackTrace();
+        //            throw new IllegalActionException(this.getName()+e.getMessage());
+        //        }
+
+        try {
+
+            _baos.write(_cipher.doFinal(dataBytes));
+
+        } catch (IllegalStateException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IllegalBlockSizeException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (BadPaddingException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        return _baos.toByteArray();
+
+    }
+
+    ///////////////////////////////////////////////////////////////////
+    ////                         private variables                 ////
+    /* The public key to be used for asymmetric encryption. This key is null
+     * for symmetric decryption.
+     */
+    private SecretKey _secretKey = null;
+
+    //The initilization parameter used in a block ciphering mode.
+    private IvParameterSpec _spec;
+
+    private AlgorithmParameters _algParams;
+
+    private static int BUFFER_SIZE = 8192;
+
+    private boolean FIRST_RUN;
+
+    //    private CipherOutputStream _cos;
+
+    private ByteArrayOutputStream _baos;
+
+    private ByteArrayInputStream bais;
+}
