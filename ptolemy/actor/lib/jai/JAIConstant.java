1b2cd59dff1e9a0d599eb7e0bd94e23064e1033a
jiahau <jiahau>
2003-10-08 02:25:28 +0000

    Changed JAIConstant to allow you to choose which data type to use, mainly do to the fact that PNG supports 16 bit, i.e. short, color channels...although you can create from byte, int, short, float, and double|

--- ptolemy/actor/lib/jai/JAIConstant.java
+++ ptolemy/actor/lib/jai/JAIConstant.java
@@ -1,7 +1,12 @@
 package ptolemy.actor.lib.jai;
 
 import java.awt.image.renderable.ParameterBlock;
+import java.lang.Byte;
 import java.lang.Double;
+import java.lang.Float;
+import java.lang.Integer;
+import java.lang.Long;
+import java.lang.Short;
 
 import javax.media.jai.JAI;
 import javax.media.jai.RenderedOp;
@@ -10,6 +15,7 @@ import ptolemy.actor.lib.Source;
 import ptolemy.data.ArrayToken;
 import ptolemy.data.DoubleToken;
 import ptolemy.data.IntToken;
+import ptolemy.data.ScalarToken;
 import ptolemy.data.Token;
 import ptolemy.data.expr.Parameter;
 import ptolemy.data.type.BaseType;
@@ -26,25 +32,49 @@ public class JAIConstant extends Source {
         height = new Parameter(this, "height", new IntToken(0));
         bandValues = new Parameter(this, "bandValues", 
                 new ArrayToken(_defaultValues));
+        dataFormat = new StringAttribute(this, "dataFormat");
+        dataFormat.setExpression("byte");
+        _dataFormat = _BYTE;
     }
 
     public Parameter bandValues;
     public Parameter height;
     public Parameter width;
 
+    /** The type to cast the data to.  This is a string valued
+     *  attribute that defaults to "byte".
+     */
+    public StringAttribute dataFormat;   
+
     public void attributeChanged(Attribute attribute)
             throws IllegalActionException {
         if (attribute == bandValues) {
-            Token values[] = ((ArrayToken)bandValues.getToken()).arrayValue();
-            _bandValues = new Double[values.length];
-            for (int i = 0; i < values.length; i++) {
-                _bandValues[i] = 
-                    new Double(((DoubleToken)values[i]).doubleValue());
-            }
+            values = ((ArrayToken)bandValues.getToken()).arrayValue();
+            //_bandValues = new Double[values.length];
+            //for (int i = 0; i < values.length; i++) {
+            //    _bandValues[i] = 
+            //        new Double(((DoubleToken)values[i]).doubleValue());
+            //}
         } else if (attribute == height) {
             _height = ((IntToken)height.getToken()).intValue();
         } else if (attribute == width) {
             _width = ((IntToken)width.getToken()).intValue();
+        } else if (attribute == dataFormat) {
+            String dataFormatName = dataFormat.getExpression();
+            if (dataFormatName.equals("byte")) {
+                _dataFormat = _BYTE;
+            } else if (dataFormatName.equals("double")) {
+                _dataFormat = _DOUBLE;
+            } else if (dataFormatName.equals("float")) {
+                _dataFormat = _FLOAT;
+            } else if (dataFormatName.equals("int")) {
+                _dataFormat = _INT;
+            } else if (dataFormatName.equals("short")) {
+                _dataFormat = _SHORT;
+            } else {
+                throw new IllegalActionException(this,
+                        "Unrecognized data type: " + dataFormatName);
+            }
         } else {
             super.attributeChanged(attribute);
         }
@@ -55,7 +85,43 @@ public class JAIConstant extends Source {
         ParameterBlock parameters = new ParameterBlock();
         parameters.add((float)_width);
         parameters.add((float)_height);
-        parameters.add(_bandValues);
+        //parameters.add(_bandValues);
+        if (_dataFormat == _BYTE) {
+            Byte[] byteValues = new Byte[values.length];
+            for (int i = 0; i < values.length; i++) {
+                byteValues[i] = 
+                    new Byte((byte)(((ScalarToken)values[i]).intValue()));
+            }
+            parameters.add(byteValues);
+        } else if (_dataFormat == _DOUBLE) {
+            Double[] doubleValues = new Double[values.length];
+            for (int i = 0; i < values.length; i++) {
+                doubleValues[i] = 
+                    new Double(((ScalarToken)values[i]).doubleValue());
+            }
+            parameters.add(doubleValues);
+        } else if (_dataFormat == _FLOAT) {
+            Float[] floatValues = new Float[values.length];
+            for (int i = 0; i < values.length; i++) {
+                floatValues[i] = 
+                    new Float((float)(((ScalarToken)values[i]).doubleValue()));
+            }
+            parameters.add(floatValues);
+        } else if (_dataFormat == _INT) {
+            Integer[] intValues = new Integer[values.length];
+            for (int i = 0; i < values.length; i++) {
+                intValues[i] = 
+                    new Integer(((ScalarToken)values[i]).intValue());
+            }
+            parameters.add(intValues);
+        } else if (_dataFormat == _SHORT) {
+            Short[] shortValues = new Short[values.length];
+            for (int i = 0; i < values.length; i++) {
+                shortValues[i] = 
+                    new Short((short)(((ScalarToken)values[i]).intValue()));
+            }
+            parameters.add(shortValues);
+        }
         RenderedOp newImage = JAI.create("constant", parameters);
         output.send(0, new JAIImageToken(newImage));        
     }
@@ -65,4 +131,15 @@ public class JAIConstant extends Source {
     private Double _bandValues[];
     private int _height;
     private int _width;
+
+    private Token values[];
+
+    private int _dataFormat;
+
+    // Constants used for more efficient execution.
+    private static final int _BYTE = 0;
+    private static final int _DOUBLE = 1;
+    private static final int _FLOAT = 2;
+    private static final int _INT = 3;
+    private static final int _SHORT = 4;
 }
