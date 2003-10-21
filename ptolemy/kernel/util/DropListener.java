0782dadbad5f60a2727d2362404bf295a6af29a2
cxh <cxh>
2003-10-21 02:20:40 +0000

    Fixed @since and copyright

--- ptolemy/kernel/util/DropListener.java
+++ ptolemy/kernel/util/DropListener.java
@@ -1,6 +1,6 @@
 /* Interface for objects that wish to be informed of drops.
 
- Copyright (c) 1998-2003 The Regents of the University of California.
+ Copyright (c) 2003 The Regents of the University of California.
  All rights reserved.
  Permission is hereby granted, without written agreement and without
  license or royalty fees, to use, copy, modify, and distribute this
@@ -42,7 +42,7 @@ editor are expected to trigger notifications.
 
 @author Edward A. Lee
 @version $Id$
-@since Ptolemy II 1.0
+@since Ptolemy II 3.1
 @see ChangeRequest
 */
 public interface DropListener {
