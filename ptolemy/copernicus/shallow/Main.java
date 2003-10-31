0a9cfc411f8bd6f468781531f2bd4449ef08ed1a
cxh <cxh>
2003-10-31 16:18:21 +0000

    Fixed @param tag

--- ptolemy/copernicus/shallow/Main.java
+++ ptolemy/copernicus/shallow/Main.java
@@ -63,9 +63,7 @@ MoML from shipped code.
 public class Main extends KernelMain {
 
     /** Read in a MoML model.
-     *  @param args The first element of the array is the MoML class
-     *  name or file name, subsequent optional arguments are Soot
-     *  command line options, see the superclass documentation for details.
+     *  @param modelName The MoML class name or file name.
      *  @exception IllegalActionException If the model cannot be parsed.
      */
     public Main(String modelName) throws IllegalActionException {
