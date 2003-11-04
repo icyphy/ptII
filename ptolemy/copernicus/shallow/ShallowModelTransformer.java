3c8497be35ad0279afe50e18e9fecfc8d12fb1ad
neuendor <neuendor>
2003-11-04 23:32:37 +0000

    extra double quote

--- ptolemy/copernicus/shallow/ShallowModelTransformer.java
+++ ptolemy/copernicus/shallow/ShallowModelTransformer.java
@@ -793,7 +793,7 @@ public class ShallowModelTransformer extends SceneTransformer  implements HasPha
         if (object == null || object.attributeList() == null) { 
             System.out.println("ShallowModelTransformer: Warning: "
                     + "object == null, or object.attributeList() == null?, "
-                    + "object: " + object);"
+                    + "object: " + object);
         } else { 
             for (Iterator attributes = object.attributeList().iterator();
                  attributes.hasNext();) {
