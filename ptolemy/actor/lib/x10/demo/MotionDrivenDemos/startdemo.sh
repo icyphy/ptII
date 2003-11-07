c72bcf93117b577e18080c4ce76017df99796dd1
cxh <cxh>
2003-11-07 01:37:34 +0000

    Use an absolute path for the output

--- ptolemy/actor/lib/x10/demo/MotionDrivenDemos/startdemo.sh
+++ ptolemy/actor/lib/x10/demo/MotionDrivenDemos/startdemo.sh
@@ -1,9 +1,11 @@
 # Script that starts up the demo
-echo "The output will be in demo.txt"
-echo "--------------------" >> demo.txt
-date >> demo.txt
+OUTPUT=${PTII}/ptolemy/actor/lib/x10/demo/MotionDrivenDemos/demo.txt
+echo "The output will be in"
+echo "   $OUTPUT"
+echo "--------------------" >> $OUTPUT
+date >> $OUTPUT
 java -Dptolemy.ptII.dir=${PTII} \
     -classpath "${PTII};${PTII}/lib/diva.jar;${PTII}/vendors/sun/commapi/comm.jar;${PTII}/vendors/misc/x10/tjx10p-11/lib/x10.jar" \
     ptolemy.actor.gui.PtExecuteApplication \
     -full ${PTII}/ptolemy/actor/lib/x10/demo/MotionDrivenDemos/MotionDrivenDemosRandom.xml \
- >> demo.txt 2>&1
+ >> $OUTPUT 2>&1
