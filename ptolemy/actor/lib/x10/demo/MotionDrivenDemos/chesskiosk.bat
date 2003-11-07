c8c46b0815990a235d11e8f5606c38119a443ab7
cxh <cxh>
2003-11-07 01:32:41 +0000

    Use bash to start up the demo so that we can more easily save the results

--- ptolemy/actor/lib/x10/demo/MotionDrivenDemos/chesskiosk.bat
+++ ptolemy/actor/lib/x10/demo/MotionDrivenDemos/chesskiosk.bat
@@ -1,1 +1,1 @@
-c:\j2sdk1.4.1_02\bin\java -Dptolemy.ptII.dir=%PTII% -classpath %PTII%;%PTII%\lib\diva.jar;%PTII%\vendors\sun\commapi\comm.jar;%PTII%\vendors\misc\x10\tjx10p-11\lib\x10.jar ptolemy.actor.gui.PtExecuteApplication -full %PTII%\ptolemy\actor\lib\x10\demo\MotionDrivenDemos\MotionDrivenDemosRandom.xml
+bash -C %PTII%/ptolemy/actor/lib/x10/demo/MotionDrivenDemos/startdemo.sh
