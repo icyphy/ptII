cc5dcb317c5f85241c34b6989c136700d4c00431
ptII <ptII>
2003-11-14 15:54:33 +0000

    Increased max memory usage to 128mb

--- ptolemy/actor/lib/x10/demo/MotionDrivenDemos/startdemo.sh
+++ ptolemy/actor/lib/x10/demo/MotionDrivenDemos/startdemo.sh
@@ -5,6 +5,7 @@ echo "   $OUTPUT"
 echo "--------------------" >> $OUTPUT
 date >> $OUTPUT
 java -Dptolemy.ptII.dir=${PTII} \
+    -Xmx128M \  
     -classpath "${PTII};${PTII}/lib/diva.jar;${PTII}/vendors/sun/commapi/comm.jar;${PTII}/vendors/misc/x10/tjx10p-11/lib/x10.jar" \
     ptolemy.actor.gui.PtExecuteApplication \
     -full ${PTII}/ptolemy/actor/lib/x10/demo/MotionDrivenDemos/MotionDrivenDemosRandom.xml \
