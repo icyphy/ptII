8975d98d0bed95e196c819562b6826aae80d290d
cxh <cxh>
2003-11-26 05:14:26 +0000

    SecretKeyReader.java

---
+++ ptolemy/actor/lib/security/SecretKeyReader.java
@@ -0,0 +1,71 @@
+/* Open a keystore from a FileParameter and output a SecretKey.
+
+@Copyright (c) 2003 The Regents of the University of California.
+All rights reserved.
+
+Permission is hereby granted, without written agreement and without
+license or royalty fees, to use, copy, modify, and distribute this
+software and its documentation for any purpose, provided that the
+above copyright notice and the following two paragraphs appear in all
+copies of this software.
+
+IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+SUCH DAMAGE.
+
+THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ENHANCEMENTS, OR MODIFICATIONS.
+
+                                                PT_COPYRIGHT_VERSION 2
+                                                COPYRIGHTENDKEY
+@ProposedRating Red (cxh@eecs.berkeley.edu)
+@AcceptedRating Red (cxh@eecs.berkeley.edu)
+*/
+
+package ptolemy.actor.lib.security;
+
+import ptolemy.kernel.CompositeEntity;
+import ptolemy.kernel.util.IllegalActionException;
+import ptolemy.kernel.util.NameDuplicationException;
+import ptolemy.kernel.util.Settable;
+
+//////////////////////////////////////////////////////////////////////////
+//// SecretKeyReader
+/** Open a keystore from a FileParameter and output a SecretKey.
+
+<p>This class is a wrapper class for {@link KeyReader} that always
+returns a private key.
+
+@see PrivateKeyReader
+@see PublicKeyReader
+@see SignatureVerifier
+@author  Christopher Hylands Brooks
+@version $Id$
+@since Ptolemy II 3.1
+*/
+public class SecretKeyReader extends PrivateKeyReader {
+    /** Construct an actor with the given container and name.
+     *  @param container The container.
+     *  @param name The name of this actor.
+     *  @exception IllegalActionException If the actor cannot be contained
+     *   by the proposed container.
+     *  @exception NameDuplicationException If the container already has an
+     *   actor with this name.
+     */
+    public SecretKeyReader(CompositeEntity container, String name)
+            throws IllegalActionException, NameDuplicationException {
+        super(container, name);
+
+        // SecretKeys do not have certificates.
+        verifyCertificate.setExpression("false");
+
+        // Hide the verifyCertificate Parameter.
+        verifyCertificate.setVisibility(Settable.EXPERT);
+    }
+}
