5ec32d50ebbcc6cdedf6c6db1d7a3142c71e9a56
cxh <cxh>
2003-12-22 22:56:17 +0000

    Fixed up class comment

--- thales/actor/gui/NavigableEffigy.java
+++ thales/actor/gui/NavigableEffigy.java
@@ -1,9 +1,27 @@
 /*
- * Created on 01 sept. 2003
- *
- * @ProposedRating Yellow (jerome.blanc@thalesgroup.com)
- * @AcceptedRating
- */
+Copyright (c) 2003 THALES.
+All rights reserved.
+
+Permission is hereby granted, without written agreement and without
+license or royalty fees, to use, copy, modify, and distribute this
+software and its documentation for any purpose, provided that the
+above copyright notice and the following two paragraphs appear in all
+copies of this software.
+
+IN NO EVENT SHALL THALES BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT,
+SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES ARISING OUT OF THE USE
+OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF THALES HAS BEEN
+ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+THALES SPECIFICALLY DISCLAIMS ANY WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+A PARTICULAR PURPOSE. THE SOFTWARE PROVIDED HEREUNDER IS ON AN "AS IS"
+BASIS, AND THALES HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT,
+UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+
+@ProposedRating Yellow (jerome.blanc@thalesgroup.com)
+@AcceptedRating
+*/
 package thales.actor.gui;
 
 import ptolemy.actor.gui.Effigy;
@@ -34,31 +52,15 @@ import ptolemy.moml.ParserAttribute;
 import ptolemy.util.StringUtilities;
 import thales.vergil.navigable.NavigationTreeModel;
 
+//////////////////////////////////////////////////////////////////////////
+//// NavigableEffigy
 /**
- * <p>Titre : NavigableEffigy</p>
- * <p>Description : An effigy for navigable models</p>
-Copyright (c) 2003 THALES.
-All rights reserved.
+An effigy for navigable models.
 
-Permission is hereby granted, without written agreement and without
-license or royalty fees, to use, copy, modify, and distribute this
-software and its documentation for any purpose, provided that the
-above copyright notice and the following two paragraphs appear in all
-copies of this software.
 
-IN NO EVENT SHALL THALES BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT,
-SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES ARISING OUT OF THE USE
-OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF THALES HAS BEEN
-ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
-THALES SPECIFICALLY DISCLAIMS ANY WARRANTIES, INCLUDING, BUT NOT
-LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-A PARTICULAR PURPOSE. THE SOFTWARE PROVIDED HEREUNDER IS ON AN "AS IS"
-BASIS, AND THALES HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT,
-UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
- * <p>Société : Thales Research and technology</p>
- * @author Jérôme Blanc & Benoit Masson
- * 01 sept. 2003
+@author Jérôme Blanc & Benoit Masson, Thales Research and technology, 01 sept. 2003
+@version $Id$
+@since Ptolemy II 3.1
  */
 public class NavigableEffigy extends PtolemyEffigy {
 
