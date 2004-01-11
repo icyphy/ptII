9488a10236bd1d031d5f505c0444237be98a146c
eal <eal>
2004-01-11 00:48:32 +0000

    Still more robust in the absense of text to display ... never return null.

--- ptolemy/vergil/icon/AttributeValueIcon.java
+++ ptolemy/vergil/icon/AttributeValueIcon.java
@@ -219,7 +219,7 @@ public class AttributeValueIcon extends XMLIcon {
                 return truncated;
             }
         }
-        return null;
+        return " ";
     }
 
     ///////////////////////////////////////////////////////////////////
