a267982a794219de26bffe0a39dc1ae7350ca096
sanjeev <sanjeev>
2004-01-13 04:24:30 +0000

    Fixed Copyright year to 2004

--- ptolemy/apps/cacheAwareScheduler/lib/ExperimentalActor.java
+++ ptolemy/apps/cacheAwareScheduler/lib/ExperimentalActor.java
@@ -1,6 +1,6 @@
 /* Used by CacheAwareScheduler to conduct experiments.
 
- Copyright (c) 1998-2003 The Regents of the University of California.
+ Copyright (c) 1998-2004 The Regents of the University of California.
  All rights reserved.
  Permission is hereby granted, without written agreement and without
  license or royalty fees, to use, copy, modify, and distribute this
