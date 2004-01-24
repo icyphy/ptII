6ff24ab0f95cd517c66cfcf7ed317555bea38500
cxh <cxh>
2004-01-24 00:47:39 +0000

    Added space around rating tags

--- ptolemy/domains/sdf/kernel/SDFUtilities.java
+++ ptolemy/domains/sdf/kernel/SDFUtilities.java
@@ -24,8 +24,8 @@
                                         PT_COPYRIGHT_VERSION_2
                                         COPYRIGHTENDKEY
 
-@ProposedRating Red(neuendor@eecs.berkeley.edu)
-@AcceptedRating Red(neuendor@eecs.berkeley.edu)
+@ProposedRating Red (neuendor@eecs.berkeley.edu)
+@AcceptedRating Red (neuendor@eecs.berkeley.edu)
 */
 
 package ptolemy.domains.sdf.kernel;
