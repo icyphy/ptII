ec6d13b7d87c91e0174d364e69d533ac45207e5c
cxh <cxh>
2004-03-14 05:32:44 +0000

    Added compat/Autocorrelation3.xml

--- ptolemy/moml/filter/test/BackwardCompatibility.tcl
+++ ptolemy/moml/filter/test/BackwardCompatibility.tcl
@@ -676,6 +676,7 @@ proc createAndExecute {file} {
 	    || "$file" == "compat2/multirate.xml" \
 	    || "$file" == "compat2/testAudioReaderAudioPlayer.xml" \
 	    || "$file" == "compat2/VariableFIR2.xml" \
+	    || "$file" == "compat/Autocorrelation3.xml" \
 	    || "$file" == "compat2/Autocorrelation3.xml" \
 	    || "$file" == "compat2/test-pn-composite-1.xml" \
 	} {
