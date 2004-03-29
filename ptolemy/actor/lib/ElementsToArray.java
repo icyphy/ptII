7eed7d034608a0f7f75b2fc36b59e1a9cf567676
cxh <cxh>
2004-03-29 23:42:44 +0000

    Updated copyright

--- ptolemy/actor/lib/ElementsToArray.java
+++ ptolemy/actor/lib/ElementsToArray.java
@@ -1,7 +1,7 @@
 /* An actor that reads a token from each input channel to
    assemble an ArrayToken.
 
- Copyright (c) 1998-2003 The Regents of the University of California.
+ Copyright (c) 2003 The Regents of the University of California.
  All rights reserved.
  Permission is hereby granted, without written agreement and without
  license or royalty fees, to use, copy, modify, and distribute this
