4f36e5909b54e828a6face5ea697105742340443
cxh <cxh>
2004-03-29 23:00:02 +0000

    Added copyright and oneline summary

--- ptolemy/actor/lib/jmf/VideoPlayer.java
+++ ptolemy/actor/lib/jmf/VideoPlayer.java
@@ -1,3 +1,32 @@
+/* An actor that displays a AVI, Quicktime or MPEG video file.
+
+ Copyright (c) 2003 The Regents of the University of California.
+ All rights reserved.
+ Permission is hereby granted, without written agreement and without
+ license or royalty fees, to use, copy, modify, and distribute this
+ software and its documentation for any purpose, provided that the above
+ copyright notice and the following two paragraphs appear in all copies
+ of this software.
+
+ IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+ FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+ ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+ THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+ SUCH DAMAGE.
+
+ THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+ INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+ PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+ CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ ENHANCEMENTS, OR MODIFICATIONS.
+
+                                        PT_COPYRIGHT_VERSION_2
+                                        COPYRIGHTENDKEY
+@ProposedRating Red (cxh@eecs.berkeley.edu)
+@AcceptedRating Red (cxh@eecs.berkeley.edu)
+*/
+
 package ptolemy.actor.lib.jmf;
 
 import ptolemy.actor.TypedIOPort;
@@ -24,16 +53,16 @@ import javax.swing.JFrame;
 
 //////////////////////////////////////////////////////////////////////////
 //// VideoPlayer
-/**
-   This actor accepts an ObjectToken that contains a DataSource.
-   This is typically obtained from the output of the StreamLoader
-   actor.  This actor will play Datasources containing a AVI, Quicktime,
-   or MPEG video file.  After the model is runned, a window will pop up
-   allowing control of playing, rate of playback, and volume control.
+/** An actor that displays a AVI, Quicktime or MPEG video file.
 
-   @author James Yeh
-   @version $Id$
-   @since Ptolemy II 3.1
+This actor accepts an ObjectToken that contains a DataSource.
+This is typically obtained from the output of the StreamLoader
+actor.  After the model is runned, a window will pop up
+allowing control of playing, rate of playback, and volume control.
+
+@author James Yeh
+@version $Id$
+@since Ptolemy II 3.1
 */
 
 public class VideoPlayer extends Sink implements ControllerListener {
