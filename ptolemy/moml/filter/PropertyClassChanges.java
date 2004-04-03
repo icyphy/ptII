4e18af106f2b077463416e020b6fd1e90c98e847
cxh <cxh>
2004-04-03 22:00:34 +0000

    Added fix for DE VariableDelay delay PortParameter.

--- ptolemy/moml/filter/PropertyClassChanges.java
+++ ptolemy/moml/filter/PropertyClassChanges.java
@@ -384,5 +384,16 @@ public class PropertyClassChanges implements MoMLFilter {
         _actorsWithPropertyClassChanges.put(
                 "ptolemy.domains.sdf.lib.VariableFIR",
                 rateParameterChanges);
+
+        // DE VariableDelay, the delay DEIOPort is now
+        // a PortParameter
+
+        HashMap variableDelayClassChanges = new HashMap();
+        variableDelayClassChanges.put("delay",
+                "ptolemy.actor.parameters.PortParameter");
+
+        _actorsWithPropertyClassChanges
+            .put("ptolemy.domains.de.lib.VariableDelay",
+                    variableDelayClassChanges);
     }
 }
