2d66ce74b0c642851f12225fb4dbe8614cc6a978
cxh <cxh>
2004-04-10 17:47:28 +0000

    Use parse(URL, URL) instead of parseFile?

--- jni/test/meaningOfLife.c
+++ jni/test/meaningOfLife.c
@@ -1,5 +1,8 @@
+#include <stdio.h>
 // Return the answer to "the meaning of life, the universe, and everything"
 double meaningOfLife() {
+  printf("meaningOfLife.c: about to return 42.0\n");
+  fflush(stdout);
   return 42.0;
 }
 
