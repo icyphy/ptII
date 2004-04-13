ab0b1579f74063924f1e35394a0a430a4989312d
cxh <cxh>
2004-04-13 17:00:56 +0000

    Fixed indenting

--- ptolemy/actor/lib/security/SignatureSigner.java
+++ ptolemy/actor/lib/security/SignatureSigner.java
@@ -64,8 +64,8 @@ actor on the <i>signature</i> port as an unsigned byte array.
 on the <i>output</i> port.
 
 <p>This actor relies on the Java Cryptography Architecture (JCA) and Java
-Cryptography Extension (JCE).  See the
-{@link ptolemy.actor.lib.security.CryptographyActor} documentation for
+Cryptography Extension (JCE).  
+See the {@link ptolemy.actor.lib.security.CryptographyActor} documentation for
 resources about JCA and JCE.
 
 @see PrivateKeyReader
@@ -75,88 +75,88 @@ resources about JCA and JCE.
 @Pt.ProposedRating Yellow (cxh)
 @Pt.AcceptedRating Red (cxh)
 */
-    public class SignatureSigner extends SignatureActor {
+public class SignatureSigner extends SignatureActor {
 
-        /** Construct an actor with the given container and name.
-         *  @param container The container.
-         *  @param name The name of this actor.
-         *  @exception IllegalActionException If the actor cannot be contained
-         *   by the proposed container.
-         *  @exception NameDuplicationException If the container already has an
-         *   actor with this name.
-         */
-        public SignatureSigner(CompositeEntity container, String name)
-                throws NameDuplicationException, IllegalActionException  {
-            super(container, name);
+    /** Construct an actor with the given container and name.
+     *  @param container The container.
+     *  @param name The name of this actor.
+     *  @exception IllegalActionException If the actor cannot be contained
+     *   by the proposed container.
+     *  @exception NameDuplicationException If the container already has an
+     *   actor with this name.
+     */
+    public SignatureSigner(CompositeEntity container, String name)
+	throws NameDuplicationException, IllegalActionException  {
+	super(container, name);
 
-            privateKey = new TypedIOPort(this, "privateKey", true, false);
-            privateKey.setTypeEquals(KeyToken.KEY);
+	privateKey = new TypedIOPort(this, "privateKey", true, false);
+	privateKey.setTypeEquals(KeyToken.KEY);
 
-            signature = new TypedIOPort(this, "signature", false, true);
-            signature.setTypeEquals(new ArrayType(BaseType.UNSIGNED_BYTE));
-        }
-
-        ///////////////////////////////////////////////////////////////////
-        ////                     ports and parameters                  ////
-
-        /** The private key to be used by the SignatureVerifier actor
-         *  to verify the data on the <i>output</i> port.
-         *  The type of this input port is an ObjectToken containing
-         *  a java.security.PrivateKey.
-         */
-        public TypedIOPort privateKey;
-
-        /** The signature of the data.  The type of this output port
-         *  is unsigned byte array.  The data is read in on the <i>input</i>
-         *  port and the signature is generated on this port.  The
-         *  <i>output</i> port contains the data in clear text.
-         */
-        public TypedIOPort signature;
-
-        ///////////////////////////////////////////////////////////////////
-        ////                         public methods                    ////
-
-        /** Create a signature for the input data and send the signature
-         *  to the signature port.  The <i>output</i> port contains the data
-         *  in clear text.
-         *
-         *  @exception IllegalActionException If calling send(), super.fire()
-         *  throws it, or if there is a problem cryptographic configuration.
-         */
-        public void fire() throws IllegalActionException {
-            // super.fire() should be called before accessing _signature
-            // so that we handle any updates of _signature made necessary
-            // by attribute changes.
-            super.fire();
-            if (privateKey.hasToken(0)) {
-                KeyToken keyToken = (KeyToken)privateKey.get(0);
-                _privateKey = (PrivateKey)keyToken.getValue();
-            }
-
-            if (input.hasToken(0)) {
-                try {
-                    // Process the input data to generate a signature.
-                    byte[] dataBytes = ArrayToken.arrayTokenToUnsignedByteArray(
-                            (ArrayToken)input.get(0));
-
-                    _signature.initSign(_privateKey);
-                    _signature.update(dataBytes);
-
-                    output.send(0, ArrayToken.unsignedByteArrayToArrayToken(
-                                        dataBytes));
-                    signature.send(0, ArrayToken.unsignedByteArrayToArrayToken(
-                                           _signature.sign()));
-                } catch (Exception ex) {
-                    throw new IllegalActionException(this, ex,
-                            "Problem sending data");
-                }
-            }
-        }
-
-        ///////////////////////////////////////////////////////////////////
-        ////                         private variables                 ////
-
-        /* The private key to be used for the signature.
-         */
-        private PrivateKey _privateKey = null;
+	signature = new TypedIOPort(this, "signature", false, true);
+	signature.setTypeEquals(new ArrayType(BaseType.UNSIGNED_BYTE));
     }
+
+    ///////////////////////////////////////////////////////////////////
+    ////                     ports and parameters                  ////
+
+    /** The private key to be used by the SignatureVerifier actor
+     *  to verify the data on the <i>output</i> port.
+     *  The type of this input port is an ObjectToken containing
+     *  a java.security.PrivateKey.
+     */
+    public TypedIOPort privateKey;
+
+    /** The signature of the data.  The type of this output port
+     *  is unsigned byte array.  The data is read in on the <i>input</i>
+     *  port and the signature is generated on this port.  The
+     *  <i>output</i> port contains the data in clear text.
+     */
+    public TypedIOPort signature;
+
+    ///////////////////////////////////////////////////////////////////
+    ////                         public methods                    ////
+
+    /** Create a signature for the input data and send the signature
+     *  to the signature port.  The <i>output</i> port contains the data
+     *  in clear text.
+     *
+     *  @exception IllegalActionException If calling send(), super.fire()
+     *  throws it, or if there is a problem cryptographic configuration.
+     */
+    public void fire() throws IllegalActionException {
+	// super.fire() should be called before accessing _signature
+	// so that we handle any updates of _signature made necessary
+	// by attribute changes.
+	super.fire();
+	if (privateKey.hasToken(0)) {
+	    KeyToken keyToken = (KeyToken)privateKey.get(0);
+	    _privateKey = (PrivateKey)keyToken.getValue();
+	}
+
+	if (input.hasToken(0)) {
+	    try {
+		// Process the input data to generate a signature.
+		byte[] dataBytes = ArrayToken.arrayTokenToUnsignedByteArray(
+									    (ArrayToken)input.get(0));
+
+		_signature.initSign(_privateKey);
+		_signature.update(dataBytes);
+
+		output.send(0, ArrayToken.unsignedByteArrayToArrayToken(
+									dataBytes));
+		signature.send(0, ArrayToken.unsignedByteArrayToArrayToken(
+									   _signature.sign()));
+	    } catch (Exception ex) {
+		throw new IllegalActionException(this, ex,
+						 "Problem sending data");
+	    }
+	}
+    }
+
+    ///////////////////////////////////////////////////////////////////
+    ////                         private variables                 ////
+
+    /* The private key to be used for the signature.
+     */
+    private PrivateKey _privateKey = null;
+}
