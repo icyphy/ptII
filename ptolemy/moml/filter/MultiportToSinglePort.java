27c0c2d8251df5b62d36db5b5efdb022b8693798
cxh <cxh>
2004-04-21 05:07:04 +0000

    Comment out println

--- ptolemy/moml/filter/MultiportToSinglePort.java
+++ ptolemy/moml/filter/MultiportToSinglePort.java
@@ -86,8 +86,8 @@ public class MultiportToSinglePort implements MoMLFilter {
     public String filterAttributeValue(NamedObj container,
             String attributeName, String attributeValue) {
 
-        System.out.println("filterAttributeValue: " + container + "\t"
-          +  attributeName + "\t" + attributeValue);
+        // System.out.println("filterAttributeValue: " + container + "\t"
+        //  +  attributeName + "\t" + attributeValue);
 
         // This method gets called many times by the MoMLParser,
         // so we try to be smart about the number of comparisons
