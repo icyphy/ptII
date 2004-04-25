c136624f25df59c4b1f463334ec73cbe63747d17
cxh <cxh>
2004-04-25 21:27:25 +0000

    Use jar -i $@ instead of jar -i tmp.jar so as to avoid trying to look up tmp.jar

--- mk/ptcommon.mk
+++ mk/ptcommon.mk
@@ -325,8 +325,9 @@ $(PTCLASSJAR): $(JSRCS) $(JCLASS)
 	mkdir -p $(PTJAR_TMPDIR)/$(ME)
 	-cp *.class $(OTHER_FILES_TO_BE_JARED) $(PTJAR_TMPDIR)/$(ME)
 	@echo "Creating $@"
-	(cd $(PTJAR_TMPDIR); "$(JAR)" -cvf tmp.jar .; $(JAR_INDEX))
+	(cd $(PTJAR_TMPDIR); "$(JAR)" -cvf tmp.jar .)
 	mv $(PTJAR_TMPDIR)/tmp.jar $@
+	$(JAR_INDEX)
 	rm -rf $(PTJAR_TMPDIR)
 
 subjars:
@@ -358,8 +359,9 @@ $(PTCLASSALLJAR): $(PTCLASSALLJARS) $(JCLASS) $(OTHER_FILES_TO_BE_JARED)
 	done
 	rm -rf $(PTJAR_TMPDIR)/META-INF
 	@echo "Creating $@"
-	(cd $(PTJAR_TMPDIR); "$(JAR)" -cvf tmp.jar .; $(JAR_INDEX))
+	(cd $(PTJAR_TMPDIR); "$(JAR)" -cvf tmp.jar .)
 	mv $(PTJAR_TMPDIR)/tmp.jar $@
+	$(JAR_INDEX)
 	rm -rf $(PTJAR_TMPDIR)
 
 
@@ -379,8 +381,9 @@ $(PTAUXALLJAR): $(PTAUXALLJARS) $(JCLASS) $(OTHER_FILES_TO_BE_JARED)
 	done
 	rm -rf $(PTJAR_TMPDIR)/META-INF
 	@echo "Creating $@"
-	(cd $(PTJAR_TMPDIR); "$(JAR)" -cvf tmp.jar .; $(JAR_INDEX))
+	(cd $(PTJAR_TMPDIR); "$(JAR)" -cvf tmp.jar .)
 	mv $(PTJAR_TMPDIR)/tmp.jar $@
+	$(JAR_INDEX)
 	rm -rf $(PTJAR_TMPDIR)
 
 ##############
