310610be3479b83caa9fc053be535c39b47be74e
cxh <cxh>
2004-07-12 23:35:46 +0000

    Added ptolemy.vergil.kernel.attributes.AttributeValueAttribute so that vergil tests pass

--- util/testsuite/removeGraphicalClasses.tcl
+++ util/testsuite/removeGraphicalClasses.tcl
@@ -73,5 +73,6 @@ proc removeGraphicalClasses {parser} {
     $filter put "ptolemy.domains.wireless.demo.WirelessSoundDetection.SoundSensor" [java::null]
     $filter put "ptolemy.domains.wireless.lib.GraphicalLocator" [java::null]
     $filter put "ptolemy.vergil.icon.CopyCatIcon" [java::null]
+    $filter put "ptolemy.vergil.kernel.attributes.AttributeValueAttribute" [java::null]
     $parser addMoMLFilter $filter
 }
