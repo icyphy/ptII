b70ff7fb91c9a102d2d7adf11c980c56ac20100b
hyzheng <hyzheng>
2004-09-17 06:02:01 +0000

    since time objects are used to represent model time. we only need a general comparater but not fuzzy comparator.

---
+++ ptolemy/domains/ct/kernel/util/GeneralComparator.java
@@ -0,0 +1,59 @@
+/* Compare two comparable objects.
+
+Copyright (c) 1998-2004 The Regents of the University of California.
+All rights reserved.
+Permission is hereby granted, without written agreement and without
+license or royalty fees, to use, copy, modify, and distribute this
+software and its documentation for any purpose, provided that the above
+copyright notice and the following two paragraphs appear in all copies
+of this software.
+
+IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+SUCH DAMAGE.
+
+THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ENHANCEMENTS, OR MODIFICATIONS.
+
+PT_COPYRIGHT_VERSION_2
+COPYRIGHTENDKEY
+*/
+
+package ptolemy.domains.ct.kernel.util;
+
+import java.util.Comparator;
+
+//////////////////////////////////////////////////////////////////////////
+//// PreciseComparator
+/**
+   Compare two Comparable objects.
+   @author Haiyang Zheng
+   @version $Id$
+   @since Ptolemy II 4.1
+   @Pt.ProposedRating Red (hyzheng)
+   @Pt.AcceptedRating Red (hyzheng)
+*/
+public class GeneralComparator implements Comparator{
+
+    ///////////////////////////////////////////////////////////////////
+    ////                         public variables                  ////
+
+    /** Return -1, 0, or 1 if the first object is less than, equal to, or 
+     *  bigger than the second object. 
+     *
+     *  <p>If any of the argument is not a Comparable object, a 
+     *  ClassCastException will be thrown.
+     *  @param first The first Comparable object.
+     *  @param second The second Comparable object.
+     *  @return The comparison result, -1, 0, or 1.
+     */
+    public int compare(Object first, Object second) {
+        return ((Comparable)first).compareTo((Comparable)second);
+    }
+}
