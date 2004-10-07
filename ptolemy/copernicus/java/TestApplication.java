05be59c3b5fbe2c20eeab83fe4d16a306bf0ef3e
cxh <cxh>
2004-10-07 02:49:43 +0000

    Fixed compilation failure

--- ptolemy/copernicus/java/TestApplication.java
+++ ptolemy/copernicus/java/TestApplication.java
@@ -107,12 +107,6 @@ public class TestApplication implements ChangeListener {
                     + url
                     + "'");
 
-            // Reset the timers, no need to penalize the test because
-            // the URL was wrong.
-            startTime = System.currentTimeMillis();
-            totalMemory1 = runtime.totalMemory()/1024;
-            freeMemory1 = runtime.freeMemory()/1024;
-
             toplevel = (CompositeActor) parser.parse(null, url);
         }
 
