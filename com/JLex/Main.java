da2acde471a8e897a8a7f5a1a8eb32acef57b24f
cxh <cxh>
2004-11-10 04:43:08 +0000

    Typo

--- com/JLex/Main.java
+++ com/JLex/Main.java
@@ -96,7 +96,7 @@ import java.io.BufferedOutputStream;
 import java.io.DataOutputStream;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.I0Exception;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.Enumeration;
