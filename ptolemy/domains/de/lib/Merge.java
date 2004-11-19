8e8aedb4cb22ef2216ed9f119d7042d0ddee4936
cxh <cxh>
2004-11-19 19:27:51 +0000

    Cleaned up class comment

--- ptolemy/domains/de/lib/Merge.java
+++ ptolemy/domains/de/lib/Merge.java
@@ -39,25 +39,27 @@ import ptolemy.kernel.util.NameDuplicationException;
 //////////////////////////////////////////////////////////////////////////
 //// Merge
 /**
-   This is a timed merge actor, which merges a set of input signals into
-   a single output signal based on the order of the tags associated with
-   the events of signals. A tag is a tuple of a time (as double) and an index
-   (as non-negative integer). The tags have a lexicographic order.
+   A timed merge actor, that merges a set of input signals
+   into a single output signal based on the order of the tags
+   associated with the events of signals. A tag is a tuple of a time
+   (as double) and an index (as non-negative integer). The tags have a
+   lexicographic order.
 
-   <p> This actor has an input port (a multiport) and an output port (a single
-   port). The types of the ports are undeclared and will be resolved by the type
-   resolution mechanism, with the constraint that the output type must be
-   greater than or equal to the input type.
+   <p> This actor has an input port (a multiport) and an output port
+   (a single port). The types of the ports are undeclared and will be
+   resolved by the type resolution mechanism, with the constraint that
+   the output type must be greater than or equal to the input type.
 
-   <p> There is a boolean parameter <i>discardEvents</i> associated with
-   this actor, which decides how to handle simultaneously available inputs.
-   Each time this actor fires, it reads the first available
-   token from the input channels and sends it to the output port. If the
-   <i>discardEvents</i> parameter is configured to true, then this actor
-   discards all the remaining inputs in other channels. Otherwise, this actor
-   requests refirings at the current time till no more events are left in the
-   channels. By this way, we construct an output signal that no two events share
-   the same tag. By default, the discardEvents parameter is false.
+   <p> There is a boolean parameter <i>discardEvents</i> associated
+   with this actor, which decides how to handle simultaneously
+   available inputs.  Each time this actor fires, it reads the first
+   available token from the input channels and sends it to the output
+   port. If the <i>discardEvents</i> parameter is configured to true,
+   then this actor discards all the remaining inputs in other
+   channels. Otherwise, this actor requests refirings at the current
+   time till no more events are left in the channels. By this way, we
+   construct an output signal that no two events share the same
+   tag. By default, the discardEvents parameter is false.
 
    @author Edward A. Lee, Haiyang Zheng
    @version $Id$
