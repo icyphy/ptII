978d5f3d36cb1c5a4da50b8de566fc7144c5f5e5
neuendor <neuendor>
2004-11-20 23:45:08 +0000

    Made the value 'live'

--- ptolemy/vergil/kernel/attributes/AttributeValueAttribute.java
+++ ptolemy/vergil/kernel/attributes/AttributeValueAttribute.java
@@ -39,6 +39,7 @@ import ptolemy.kernel.util.StringAttribute;
 import ptolemy.kernel.util.IllegalActionException;
 import ptolemy.kernel.util.NameDuplicationException;
 import ptolemy.kernel.util.NamedObj;
+import ptolemy.kernel.util.ValueListener;
 
 //////////////////////////////////////////////////////////////////////////
 //// AttributeValueAttribute
@@ -52,7 +53,13 @@ import ptolemy.kernel.util.NamedObj;
    @Pt.ProposedRating Yellow (eal)
    @Pt.AcceptedRating Red (cxh)
 */
-public class AttributeValueAttribute extends AbstractTextAttribute {
+public class AttributeValueAttribute extends AbstractTextAttribute
+    implements ValueListener, Settable {
+    // NOTE: This attribute only implements settable as a workaround
+    // to ensure that it gets notified of the start of execution.
+    // Unfortunately, most of the code in the Variable class is
+    // written to be specific to Variables, making it difficult for
+    // this class to properly listen to a variable by name.
 
     /** Construct an attribute with the given name contained by the
      *  specified container. The container argument must not be null, or a
@@ -91,8 +98,7 @@ public class AttributeValueAttribute extends AbstractTextAttribute {
     ///////////////////////////////////////////////////////////////////
     ////                         public methods                    ////
 
-    /** React to a changes in the attributes by changing
-     *  the icon.
+    /** React to a changes in the attributes by changing the icon.
      *  @param attribute The attribute that changed.
      *  @exception IllegalActionException If the change is not acceptable
      *   to this container (should not be thrown).
@@ -100,8 +106,7 @@ public class AttributeValueAttribute extends AbstractTextAttribute {
     public void attributeChanged(Attribute attribute)
             throws IllegalActionException {
         if (attribute == attributeName) {
-            _attributeName = attributeName.getExpression();
-            _icon.setText(_getText());
+            _setAttributeName(attributeName.getExpression());
         } else if(attribute == displayWidth) {
             _displayWidth =
                 ((IntToken) displayWidth.getToken()).intValue();
@@ -111,9 +116,119 @@ public class AttributeValueAttribute extends AbstractTextAttribute {
         }
     }
 
+    /** React to a change in the value of the associated attribute.
+     */
+    public void valueChanged(Settable settable) {
+        try {
+            _setAttributeName(attributeName.getExpression());
+        } catch(IllegalActionException ex) {
+            // Ignore?  or reset attribute?
+        }
+    }
+
+    /** Add a listener to be notified when the value of this settable
+     *  object changes. An implementation of this method should ignore
+     *  the call if the specified listener is already on the list of
+     *  listeners.  In other words, it should not be possible for the
+     *  same listener to be notified twice of a value update.
+     *  @param listener The listener to add.
+     */
+    public void addValueListener(ValueListener listener) {
+    }
+    
+    /** Return the default value of this attribute, if there is
+     *  one, or null if there is none.
+     *  @return The default value of this attribute, or null
+     *   if there is none.
+     */
+    public String getDefaultExpression() {
+        return "";
+    }
+
+    /** Get the value of the attribute that has been set by setExpression(),
+     *  or null if there is none.
+     *  @return The expression.
+     */
+    public String getExpression() {
+        return "";
+    }
+
+    /** Get the visibility of this Settable, as set by setVisibility().
+     *  If setVisibility() has not been called, then implementations of
+     *  this interface should return some default, not null, indicating
+     *  user-level visibility. The returned value is one of the static
+     *  instances of the Visibility inner class.
+     *  @return The visibility of this Settable.
+     */
+    public Settable.Visibility getVisibility() {
+        return Settable.NONE;
+    }
+
+    /** Remove a listener from the list of listeners that are
+     *  notified when the value of this variable changes.  If no such listener
+     *  exists, do nothing.
+     *  @param listener The listener to remove.
+     */
+    public void removeValueListener(ValueListener listener) {
+    }
+    
+    /** Set the value of the attribute by giving some expression.
+     *  In some implementations, the listeners and the container will
+     *  be notified immediately.  However, some implementations may
+     *  defer notification until validate() is called.
+     *  @param expression The value of the attribute.
+     *  @exception IllegalActionException If the expression is invalid.
+     */
+    public void setExpression(String expression) throws IllegalActionException {
+    }
+
+    /** Set the visibility of this Settable.  The argument should be one
+     *  of the static public instances of the inner class Visibility.
+     *  This is enforced by making it impossible to construct instances
+     *  of this inner class outside this interface definition.
+     *  If this method is not called, then implementations of
+     *  this interface should return some default, not null.
+     *  @param visibility The visibility of this Settable.
+     */
+    public void setVisibility(Settable.Visibility visibility) {
+    }
+
+    /** Check the validity of the expression set in setExpression().
+     *  Implementations of this method should notify the container
+     *  by calling attributeChanged(), unless the container has already
+     *  been notified in setExpression().  They should also notify any
+     *  registered value listeners if they have not already been notified.
+     *  @exception IllegalActionException If the expression is not valid, or
+     *   its value is not acceptable to the container or the listeners.
+     */
+    public void validate() throws IllegalActionException {
+        _setAttributeName(attributeName.getExpression());
+    }
+
     ///////////////////////////////////////////////////////////////////
     ////                        protected methods                  ////
 
+    protected void _setAttributeName(String attributeName) 
+            throws IllegalActionException {
+        NamedObj container = (NamedObj)getContainer();
+        if (container != null) {
+            Attribute newAttribute = ModelScope.getScopedVariable(
+                    null, container, attributeName);
+            if(_attribute != newAttribute) {
+                if(_attribute != null) {
+                    _attribute.removeValueListener(this);
+                }
+                if(newAttribute instanceof Settable) {
+                    _attribute = (Settable)newAttribute;
+                    _attribute.addValueListener(this);
+                } else {
+                    _attribute = null;
+                }
+            }
+        }
+        _icon.setText(_getText());
+    }
+    
     /** Return the a new string that contains the expression of the
      *  referred to attribute.
      *  @return A new shape.
@@ -122,11 +237,9 @@ public class AttributeValueAttribute extends AbstractTextAttribute {
         NamedObj container = (NamedObj)getContainer();
         try {
             if (container != null) {
-                Attribute associatedAttribute = 
-                    ModelScope.getScopedVariable(null, container, _attributeName);
-                if (associatedAttribute instanceof Variable) {
+                if (_attribute instanceof Variable) {
                     String value = 
-                        ((Variable)associatedAttribute).getToken().toString();
+                        ((Variable)_attribute).getToken().toString();
                     String truncated = value;
                     int width = _displayWidth;
                     if (value.length() > width) {
@@ -136,8 +249,8 @@ public class AttributeValueAttribute extends AbstractTextAttribute {
                         truncated = " ";
                     }
                     return truncated;
-                } else if (associatedAttribute instanceof Settable) {
-                    String value = ((Settable)associatedAttribute).getExpression();
+                } else if (_attribute instanceof Settable) {
+                    String value = ((Settable)_attribute).getExpression();
                     String truncated = value;
                     int width = _displayWidth;
                     if (value.length() > width) {
@@ -161,6 +274,6 @@ public class AttributeValueAttribute extends AbstractTextAttribute {
     /** Most recent value of the rounding parameter. */
     protected int _displayWidth = 0;
 
-    /** The name of the attribute. Defaults to the empty string. */
-    protected String _attributeName = "";
+    /** The associated attribute. */
+    protected Settable _attribute = null;
 }
