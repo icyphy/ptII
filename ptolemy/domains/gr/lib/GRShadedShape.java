a5b4dfcfbe2f0288d2bc33c85959f7f1f2686693
eal <eal>
2005-01-03 17:21:13 +0000

    Response to review, and made properties optionally interactive.

--- ptolemy/domains/gr/lib/GRShadedShape.java
+++ ptolemy/domains/gr/lib/GRShadedShape.java
@@ -1,4 +1,4 @@
-/* An abstract base class for shaded GR Actors
+/* An abstract base class for shaded GR Actors.
 
 Copyright (c) 1998-2004 The Regents of the University of California.
 All rights reserved.
@@ -41,6 +41,7 @@ import javax.vecmath.Color3f;
 
 import ptolemy.actor.TypedIOPort;
 import ptolemy.actor.gui.ColorAttribute;
+import ptolemy.actor.parameters.DoubleRangeParameter;
 import ptolemy.data.BooleanToken;
 import ptolemy.data.DoubleToken;
 import ptolemy.data.expr.FileParameter;
@@ -50,8 +51,10 @@ import ptolemy.domains.gr.kernel.GRActor;
 import ptolemy.domains.gr.kernel.GRActor3D;
 import ptolemy.domains.gr.kernel.SceneGraphToken;
 import ptolemy.kernel.CompositeEntity;
+import ptolemy.kernel.util.Attribute;
 import ptolemy.kernel.util.IllegalActionException;
 import ptolemy.kernel.util.NameDuplicationException;
+import ptolemy.kernel.util.Workspace;
 
 import com.sun.j3d.utils.image.TextureLoader;
 
@@ -73,20 +76,33 @@ import com.sun.j3d.utils.image.TextureLoader;
     <p>
     The parameter <i>shininess</i> determines the shininess of the object.
     It ranges from 1.0 (the default) to 128.0, meaning not shiny to very
-    shiny.
+    shiny. A shiny object reflects the <i>specularColor</i>, unless
+    it is black, in which case shininess has no effect.
+    <p>
+    The <i>texture</i> parameter can be used to specify an image file.
+    The specified image will be mapped onto the shape.
     <p>
     The parameter <i>transparency</i> determines the transparency of the
     object. It ranges from 0.0 (the default) to 1.0, meaning opaque to
-    fully transparent.
+    fully transparent (which makes the object invisible).
     <p>
     The <i>wireFrame</i> parameter can be used to view only the lines
     that outline the polygons of the object and not the surface.
+    The <i>flat</i> parameter can be set to make rendered polygons
+    flat rather than rounded at the corners.
+    <p>
+    The <i>allowRuntimeChanges</i> parameter, if true, specifies
+    that changes to parameter values during the execution of the model
+    take effect immediately. By default, this parameter is false,
+    which means that changes to parameter values take effect only
+    on the next run of the model. A value of false yields better
+    performance, but less interactivity.
 
     @author C. Fong, Steve Neuendorffer, Edward A. Lee
     @version $Id$
     @since Ptolemy II 1.0
-    @Pt.ProposedRating Red (chf)
-    @Pt.AcceptedRating Red (chf)
+    @Pt.ProposedRating Green (eal)
+    @Pt.AcceptedRating Green (liuxj)
 */
 abstract public class GRShadedShape extends GRActor3D {
 
@@ -116,12 +132,17 @@ abstract public class GRShadedShape extends GRActor3D {
         specularColor.setExpression("{1.0, 1.0, 1.0, 1.0}");
 
         texture = new FileParameter(this, "texture");
+        // The following ensures that revert to defaults works properly.
+        texture.setExpression("");
 
-        shininess = new Parameter(this,"shininess");
+        shininess = new DoubleRangeParameter(this,"shininess");
+        shininess.min.setExpression("1.0");
+        shininess.max.setExpression("128.0");
+        shininess.precision.setExpression("128");
         shininess.setExpression("1.0");
         shininess.setTypeEquals(BaseType.DOUBLE);
 
-        transparency = new Parameter(this,"transparency");
+        transparency = new DoubleRangeParameter(this, "transparency");
         transparency.setExpression("0.0");
         transparency.setTypeEquals(BaseType.DOUBLE);
 
@@ -132,11 +153,20 @@ abstract public class GRShadedShape extends GRActor3D {
         flat = new Parameter(this,"flat");
         flat.setExpression("false");
         flat.setTypeEquals(BaseType.BOOLEAN);
+        
+        allowRuntimeChanges = new Parameter(this, "allowRuntimeChanges");
+        allowRuntimeChanges.setExpression("false");
+        allowRuntimeChanges.setTypeEquals(BaseType.BOOLEAN);
     }
 
     ///////////////////////////////////////////////////////////////////
     ////                         parameters                        ////
 
+    /** If true, then changes to parameter values can be made during
+     *  execution of the model. This is a boolean that defaults to false.
+     */
+    public Parameter allowRuntimeChanges;
+    
     /** The diffuse color, which is the color of the object reflecting
      *  illumination. Note that the alpha value (the fourth
      *  element of the array), which would normally specify transparency,
@@ -153,7 +183,7 @@ abstract public class GRShadedShape extends GRActor3D {
     public ColorAttribute emissiveColor;
 
     /** The output port for connecting to other GR Actors in
-     *  the scene graph.
+     *  the scene graph. The type is SceneGraphToken.
      */
     public TypedIOPort sceneGraphOut;
 
@@ -162,7 +192,7 @@ abstract public class GRShadedShape extends GRActor3D {
      *  to 128.0, where 1.0 represents not shiny and 128.0 represents
      *  very shiny.  This is a double with default 1.0.
      */
-    public Parameter shininess;
+    public DoubleRangeParameter shininess;
 
     /** The specular color, which is a color of a highlight reflecting
      *  ambient illumination. Note that the alpha value (the fourth
@@ -172,15 +202,16 @@ abstract public class GRShadedShape extends GRActor3D {
      */
     public ColorAttribute specularColor;
 
-    /** Texture URL. FIXME.
-     *
+    /** Texture URL, which if non-empty, specifies an image file
+     *  or URL. The image from the file is mapped onto the shape
+     *  as a texture.
      */
     public FileParameter texture;
 
     /** The transparency, where 0.0 means opaque (the default) and 1.0
      *  means fully transparent. The type is double.
      */
-    public Parameter transparency;
+    public DoubleRangeParameter transparency;
 
     /** If true, render the shape using a wire frame. This is a boolean
      *  that defaults to false.
@@ -194,8 +225,94 @@ abstract public class GRShadedShape extends GRActor3D {
 
     ///////////////////////////////////////////////////////////////////
     ////                         public methods                    ////
+    
+    /** Adjust the appearance when an attribute changes if such
+     *  an update is supported by the <i>allowRuntimeChanges</i> parameter. 
+     */
+    public void attributeChanged(Attribute attribute)
+    		throws IllegalActionException {
+    	// If allowRuntimeChanges is null, then we are in the
+    	// constructor, and don't need to do any of this.
+    	if (allowRuntimeChanges != null) {
+        	boolean allowChanges
+					= ((BooleanToken)allowRuntimeChanges.getToken())
+					.booleanValue();
+			if (allowChanges) {
+		    	if (attribute == transparency && _transparencyAttributes != null) {
+		            float transparent = (float)
+							((DoubleToken)transparency.getToken()).doubleValue();
+		            if (transparent > 0.0) {
+		            	_transparencyAttributes.setTransparencyMode(
+		            			TransparencyAttributes.NICEST);
+		            } else {
+		            	_transparencyAttributes.setTransparencyMode(
+		            			TransparencyAttributes.NONE);
+		            }
+				    _transparencyAttributes.setTransparency(transparent);
+				}
+		    	if (attribute == flat && _coloringAttributes != null) {
+		            boolean flatValue
+							= ((BooleanToken)flat.getToken()).booleanValue();
+		            int shadeModel = ColoringAttributes.SHADE_GOURAUD;
+		            if (flatValue) {
+		            	shadeModel = ColoringAttributes.SHADE_FLAT;
+		            }
+				    _coloringAttributes.setShadeModel(shadeModel);
+				}
+				if (_material != null) {
+					if (attribute == diffuseColor) {
+				        Color3f color = new Color3f(diffuseColor.asColor());
+				        _material.setDiffuseColor(color);
+					} else if (attribute == emissiveColor) {
+				        Color3f color = new Color3f(emissiveColor.asColor());
+				        _material.setEmissiveColor(color);
+					} else if (attribute == specularColor) {
+				        Color3f color = new Color3f(specularColor.asColor());
+				        _material.setSpecularColor(color);
+					} else if (attribute == shininess) {
+				        float shine = (float)
+			            		((DoubleToken)shininess.getToken())
+								.doubleValue();
+			        	_material.setShininess(shine);
+					}
+				}
+				if (attribute == texture && _appearance != null) {
+			        URL textureURL = texture.asURL();
+			        if (_viewScreen != null && textureURL != null) {
+			            TextureLoader loader;
+			            loader = new TextureLoader(textureURL, _viewScreen.getCanvas());
+			            Texture loadedTexture = loader.getTexture();
+			            if (loadedTexture != null) {
+			                _appearance.setTexture(loadedTexture);
+			            }
+			        }					
+				}
+				if (attribute == wireFrame && _polygonAttributes != null) {
+			        int mode = PolygonAttributes.POLYGON_FILL;
+			        if (((BooleanToken)wireFrame.getToken()).booleanValue()) {
+			            mode = PolygonAttributes.POLYGON_LINE;
+			        }
+			        _polygonAttributes.setPolygonMode(mode);
+				}
+			}
+    	}
+    	super.attributeChanged(attribute);
+    }
+    
+    /** Override the base class to null out private variables. 
+     */
+    public Object clone(Workspace workspace)
+    		throws CloneNotSupportedException {
+    	GRShadedShape newObject = (GRShadedShape)super.clone(workspace);
+    	newObject._appearance = null;
+    	newObject._coloringAttributes = null;
+    	newObject._material = null;
+    	newObject._polygonAttributes = null;
+    	newObject._transparencyAttributes = null;
+    	return newObject;
+    }
 
-    /*  Create the Java3D geometry and appearance for this GR actors.
+    /** Create the Java3D geometry and appearance for this GR actor.
      *  @exception IllegalActionException If the current director
      *  is not a GRDirector.
      */
@@ -216,6 +333,35 @@ abstract public class GRShadedShape extends GRActor3D {
         }
     }
 
+    /** Override the base class to ensure that material and
+     *  appearance objects are created anew.
+     *  @exception IllegalActionException If the current director
+     *   is not a GRDirector.
+     */
+    public void preinitialize() throws IllegalActionException {
+    	_appearance = null;
+    	_coloringAttributes = null;
+    	_material = null;
+    	_polygonAttributes = null;
+    	_transparencyAttributes = null;
+        super.preinitialize();
+    }
+
+    /** Override the base class to set to null the references
+     *  to appearance and material. This prevents changes
+     *  after the model has finished executing.
+     *  @exception IllegalActionException If the current director
+     *   is not a GRDirector.
+     */
+    public void wrapup() throws IllegalActionException {
+    	_appearance = null;
+    	_coloringAttributes = null;
+    	_material = null;
+    	_polygonAttributes = null;
+    	_transparencyAttributes = null;
+        super.wrapup();
+    }
+
     ///////////////////////////////////////////////////////////////////
     ////                         protected methods                 ////
 
@@ -227,39 +373,49 @@ abstract public class GRShadedShape extends GRActor3D {
         _material = new Material();
         _appearance = new Appearance();
 
+    	boolean allowChanges
+				= ((BooleanToken)allowRuntimeChanges.getToken())
+				.booleanValue();
+
         Color3f color = new Color3f(emissiveColor.asColor());
         _material.setEmissiveColor(color);
 
         color = new Color3f(diffuseColor.asColor());
         _material.setDiffuseColor(color);
 
+        color = new Color3f(specularColor.asColor());
+        _material.setSpecularColor(color);
+
         float shine = (float)
             ((DoubleToken)shininess.getToken()).doubleValue();
         if (shine > 1.0) {
-            color = new Color3f(specularColor.asColor());
-            _material.setSpecularColor(color);
             _material.setShininess(shine);
-        } else {
-            _material.setSpecularColor(color);
         }
+        
         _appearance.setMaterial(_material);
 
         // Deal with transparent attributes.
         float transparent = (float)
             ((DoubleToken)transparency.getToken()).doubleValue();
-        if (transparent > 0.0) {
-            TransparencyAttributes attributes
-                = new TransparencyAttributes(
-                        TransparencyAttributes.NICEST, transparent);
-            _appearance.setTransparencyAttributes(attributes);
+        if (transparent > 0.0 || allowChanges) {
+        	int mode = TransparencyAttributes.NICEST;
+        	if (transparent == 0.0) {
+        		mode = TransparencyAttributes.NONE;
+        	}
+        	_transparencyAttributes = new TransparencyAttributes(
+        			mode, transparent);
+            _appearance.setTransparencyAttributes(_transparencyAttributes);
         }
 
         // Deal with flat attribute.
-        if (((BooleanToken)flat.getToken()).booleanValue()) {
-            ColoringAttributes coloring
-                = new ColoringAttributes(0.7f, 0.7f, 0.7f,
-                        ColoringAttributes.SHADE_FLAT);
-            _appearance.setColoringAttributes(coloring);
+        boolean flatValue = ((BooleanToken)flat.getToken()).booleanValue();
+        if (flatValue) {
+            _coloringAttributes = new ColoringAttributes(0.7f, 0.7f, 0.7f,
+                    ColoringAttributes.SHADE_FLAT);
+            _appearance.setColoringAttributes(_coloringAttributes);
+        } else if (allowChanges) {
+            _coloringAttributes = new ColoringAttributes();
+            _appearance.setColoringAttributes(_coloringAttributes);        	
         }
 
         // Deal with wireFrame attribute.
@@ -269,19 +425,35 @@ abstract public class GRShadedShape extends GRActor3D {
         }
         // Default culls back facing polygons, which is weird.
         // We disable that here.
-        PolygonAttributes polygonAttributes = new PolygonAttributes(
+        _polygonAttributes = new PolygonAttributes(
                 mode,
                 PolygonAttributes.CULL_NONE,
                 0.0f);
-        _appearance.setPolygonAttributes(polygonAttributes);
+        _appearance.setPolygonAttributes(_polygonAttributes);
 
         // Turn on antialiasing.
-        // FIXME: Doesn't seem to work.
         LineAttributes lineAttributes = new LineAttributes(
                 1.0f,
                 LineAttributes.PATTERN_SOLID,
                 true);
         _appearance.setLineAttributes(lineAttributes);
+
+        // If runtime changes are allowed, we need to set the
+        // appropriate capabilities.
+        if (allowChanges) {
+	        _transparencyAttributes.setCapability(
+	        		TransparencyAttributes.ALLOW_VALUE_WRITE);
+	        _transparencyAttributes.setCapability(
+	        		TransparencyAttributes.ALLOW_MODE_WRITE);
+	        _material.setCapability(
+	        		Material.ALLOW_COMPONENT_WRITE);
+	        _coloringAttributes.setCapability(
+	        		ColoringAttributes.ALLOW_SHADE_MODEL_WRITE);
+	        _appearance.setCapability(
+	        		Appearance.ALLOW_TEXTURE_WRITE);
+	        _polygonAttributes.setCapability(
+	        		PolygonAttributes.ALLOW_MODE_WRITE);
+        }
     }
 
     /** Set the color and appearance of this 3D object.
@@ -291,6 +463,7 @@ abstract public class GRShadedShape extends GRActor3D {
         _createAppearance();
     }
 
+    /** Send the scene graph token on the output. */
     protected void _makeSceneGraphConnection() throws IllegalActionException {
         sceneGraphOut.send(0, new SceneGraphToken(_getNodeObject()));
     }
@@ -304,21 +477,28 @@ abstract public class GRShadedShape extends GRActor3D {
             throws IllegalActionException {
         super._setViewScreen(actor);
         URL textureURL = texture.asURL();
+        TextureAttributes attributes = null;
         if (textureURL != null) {
             TextureLoader loader;
             loader = new TextureLoader(textureURL, _viewScreen.getCanvas());
             Texture loadedTexture = loader.getTexture();
 
             if (loadedTexture != null) {
-
-                TextureAttributes attributes = new TextureAttributes();
+                attributes = new TextureAttributes();
                 attributes.setTextureMode(TextureAttributes.MODULATE);
                 _appearance.setTextureAttributes(attributes);
 
                 _appearance.setTexture(loadedTexture);
             }
         }
-
+        // If runtime changes are allowed, then we need to set texture
+        // attributes even if not needed now.
+        if (attributes == null 
+        		&& ((BooleanToken)allowRuntimeChanges.getToken()).booleanValue()) {
+            attributes = new TextureAttributes();
+            attributes.setTextureMode(TextureAttributes.MODULATE);
+           _appearance.setTextureAttributes(attributes);
+        }
     }
 
     ///////////////////////////////////////////////////////////////////
@@ -326,7 +506,16 @@ abstract public class GRShadedShape extends GRActor3D {
 
     /** The appearance of this 3D object. */
     protected Appearance _appearance;
+    
+    /** The coloring attributes, or null if not created. */
+    protected ColoringAttributes _coloringAttributes;
 
     /** The material of this 3D object. */
     protected Material _material;
+    
+    /** Polygon attributes. */
+    protected PolygonAttributes _polygonAttributes;
+    
+    /** The transparency attributes, or null if not created. */
+    protected TransparencyAttributes _transparencyAttributes;
 }
