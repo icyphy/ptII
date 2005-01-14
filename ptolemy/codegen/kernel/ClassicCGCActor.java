2cc1606fb81db75240f749ff5c300ee4d5e32172
cxh <cxh>
2005-01-14 17:15:00 +0000

    Added constructors and imports

---
+++ ptolemy/codegen/kernel/ClassicCGCActor.java
@@ -0,0 +1,111 @@
+/* Backward compatibility with Ptolemy Classic code generation.
+
+Copyright (c) 2005 The Regents of the University of California.
+All rights reserved.
+Permission is hereby granted, without written agreement and without
+license or royalty fees, to use, copy, modify, and distribute this
+software and its documentation for any purpose, provided that the above
+copyright notice and the following two paragraphs appear in all copies
+of this software.
+
+IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+SUCH DAMAGE.
+
+THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ENHANCEMENTS, OR MODIFICATIONS.
+
+PT_COPYRIGHT_VERSION_2
+COPYRIGHTENDKEY
+
+*/
+
+package ptolemy.codegen.kernel;
+
+import ptolemy.actor.TypedAtomicActor;
+import ptolemy.kernel.CompositeEntity;
+import ptolemy.kernel.util.IllegalActionException;
+import ptolemy.kernel.util.InternalErrorException;
+import ptolemy.kernel.util.NameDuplicationException;
+import ptolemy.kernel.util.Workspace;
+
+//////////////////////////////////////////////////////////////////////////
+//// CodeGenActor
+/**
+   Backward compatibility with Ptolemy Classic code generation.
+
+   @author Edward A. Lee
+   @version $Id$
+   @since Ptolemy II 4.1
+   @Pt.ProposedRating Red (eal)
+   @Pt.AcceptedRating Red (eal)
+*/
+public class ClassicCGCActor extends CodeGenActor {
+
+    // All the constructors are wrappers of the super class constructors.
+
+    /** Construct an actor in the default workspace with an empty string
+     *  as its name.  The object is added to the workspace directory.
+     *  Increment the version number of the workspace.
+     */
+    public ClassicCGCActor() {
+        super();
+    }
+
+    /** Construct an actor in the specified workspace with an empty
+     *  string as a name. You can then change the name with setName().
+     *  If the workspace argument is null, then use the default workspace.
+     *  The object is added to the workspace directory.
+     *  Increment the version number of the workspace.
+     *  @param workspace The workspace that will list the entity.
+     */
+    public ClassicCGCActor(Workspace workspace) {
+        super(workspace);
+    }
+
+    /** Create a new actor in the specified container with the specified
+     *  name.  The name must be unique within the container or an exception
+     *  is thrown. The container argument must not be null, or a
+     *  NullPointerException will be thrown.
+     *
+     *  @param container The container.
+     *  @param name The name of this actor within the container.
+     *  @exception IllegalActionException If this actor cannot be contained
+     *   by the proposed container (see the setContainer() method).
+     *  @exception NameDuplicationException If the name coincides with
+     *   an entity already in the container.
+     */
+    public ClassicCGCActor(CompositeEntity container, String name)
+            throws IllegalActionException, NameDuplicationException {
+        super(container, name);
+    }
+
+    ///////////////////////////////////////////////////////////////////
+    ////                         public methods                    ////
+
+   public void addInclude(String code) {
+       addCode(code, "include");
+   }
+
+   public void addGlobal(String code) {
+       addCode(code, "global");
+   }
+
+   public void addProcedure(String code) {
+       addCode(code, "procedure");
+   }
+
+   public String getCode(String streamName) {
+       if (streamName == null) {
+           return "FIXME: _myCode";
+       } else {
+           return "FIXME: return named stream " + streamName;
+       }
+   }
+}
