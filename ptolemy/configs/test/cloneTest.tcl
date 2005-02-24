7487446567d2875f60179c70c0547f35627369bf
cxh <cxh>
2005-02-24 01:07:36 +0000

    cloneTest.tcl

---
+++ ptolemy/configs/test/cloneTest.tcl
@@ -0,0 +1,14 @@
+source testDefs.tcl
+set workspace [java::new ptolemy.kernel.util.Workspace]
+set compositeEntity [java::new ptolemy.kernel.CompositeEntity $workspace]
+$compositeEntity setName Top
+set actor [java::new ptolemy.actor.lib.colt.ColtZeta $compositeEntity "Zeta"]
+set clone [java::cast ptolemy.actor.TypedAtomicActor [$actor clone $workspace]]
+set constraints [$actor typeConstraintList]
+set c [jdkPrintArray [$constraints toArray] "\n" ]
+
+set cloneConstraints [$clone typeConstraintList]
+set cc [jdkPrintArray [$cloneConstraints toArray] "\n" ]
+
+puts "$c"
+puts "$cc"
