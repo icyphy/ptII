e35a37e883e5209d25d1e089ec8cac41de6e9d81
cxh <cxh>
2005-02-26 18:14:45 +0000

    Added summary

--- ptolemy/configs/test/cloneTest.tcl
+++ ptolemy/configs/test/cloneTest.tcl
@@ -1,8 +1,11 @@
+# Simple standalone clone test
+
 source testDefs.tcl
 set workspace [java::new ptolemy.kernel.util.Workspace]
 set compositeEntity [java::new ptolemy.kernel.CompositeEntity $workspace]
 $compositeEntity setName Top
-set actor [java::new ptolemy.actor.lib.colt.ColtZeta $compositeEntity "Zeta"]
+#set actor [java::new ptolemy.actor.lib.colt.ColtZeta $compositeEntity "Zeta"]
+set actor [java::new ptolemy.actor.lib.Gaussian $compositeEntity "Gaussian"]
 set clone [java::cast ptolemy.actor.TypedAtomicActor [$actor clone $workspace]]
 set constraints [$actor typeConstraintList]
 set c [jdkPrintArray [$constraints toArray] "\n" ]
