b3110e12d58b2c254c9b11909a36cec2d5f79d08
cxh <cxh>
2005-02-28 05:19:21 +0000

    Added javadoc tag to suggestedModalModelDirectors() because (non-Javadoc) does not work.

--- ptolemy/domains/ct/kernel/CTDirector.java
+++ ptolemy/domains/ct/kernel/CTDirector.java
@@ -762,7 +762,10 @@ public abstract class CTDirector extends StaticSchedulingDirector
         }
     }
 
-    /** (non-Javadoc)
+    /** Return an array of suggested directors to use with
+     *  ModalModel. Each director is specified by its full class
+     *  name.  The first director in the array will be the default
+     *  director used by a modal model.
      *  @see ptolemy.actor.Director#suggestedModalModelDirectors()
      */
     public String[] suggestedModalModelDirectors() {
