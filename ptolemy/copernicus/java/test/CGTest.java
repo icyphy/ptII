73b6996c09751f23cb7578a2733e09dd07dc3c09
cxh <cxh>
2005-03-31 22:41:31 +0000

    Further hacks for SharedParameter

--- ptolemy/copernicus/java/test/CGTest.java
+++ ptolemy/copernicus/java/test/CGTest.java
@@ -70,5 +70,150 @@ public class CGTest extends CGNonStrictTest {
     public CGTest(CompositeEntity container, String name)
         throws NameDuplicationException, IllegalActionException {
         super(container, name);
+
+        // These liens are copied from T
+        output = new TypedIOPort(this, "output", false, true);
+        output.setTypeEquals(BaseType.BOOLEAN);
+        input.setMultiport(true);
+
+    }
+    ///////////////////////////////////////////////////////////////////
+    ////                         ports                             ////
+
+    /** Boolean output that is false as long as there is data to
+     *  compare against the input, but becomes true on the first
+     *  firing after such data has been exhausted.
+     */
+    public TypedIOPort output;
+
+    ///////////////////////////////////////////////////////////////////
+    ////                         public methods                    ////
+
+    /** Read one token from each input channel and compare against
+     *  the value specified in <i>correctValues</i>. If the value
+     *  matches, then output false (to indicate that the test is not
+     *  complete yet) and return.  Otherwise, throw an exception.
+     *  If the iteration count is larger than the length of
+     *  <i>correctValues</i>, then output <i>true</i> and return,
+     *  indicating that the test is complete, i.e. that all
+     *  values in <i>correctValues</i> have been matched.
+     *
+     *  @exception IllegalActionException If an input is missing,
+     *   or if its value does not match the required value.
+     */
+    public void fire() throws IllegalActionException {
+        super.fire();
+
+        int width = input.getWidth();
+
+        // If we are in training mode, read the inputs and add to the
+        // training data.
+        boolean training = ((BooleanToken) trainingMode.getToken())
+            .booleanValue();
+
+        if (training) {
+            if (_trainingTokens == null) {
+                _trainingTokens = new ArrayList();
+            }
+
+            if (width == 1) {
+                if (input.hasToken(0)) {
+                    _trainingTokens.add(input.get(0));
+                }
+            } else {
+                ArrayList arrayList = new ArrayList();
+                _trainingTokens.add(arrayList);
+
+                for (int i = 0; i < width; i++) {
+                    arrayList.add(input.get(i));
+                }
+            }
+
+            return;
+        }
+
+        if (_numberOfInputTokensSeen >= ((ArrayToken) (correctValues.getToken()))
+                .length()) {
+            // Consume and discard input values.  We are beyond the end
+            // of the correctValues array.
+            for (int i = 0; i < width; i++) {
+                if (input.hasToken(i)) {
+                    input.get(i);
+                }
+            }
+
+            // Indicate that the test has passed.
+            output.send(0, new BooleanToken(true));
+            return;
+        }
+
+        output.send(0, new BooleanToken(false));
+
+        Token referenceToken = ((ArrayToken) (correctValues.getToken()))
+            .getElement(_numberOfInputTokensSeen);
+        Token[] reference;
+
+        if ((width == 1) && !(referenceToken instanceof ArrayToken)) {
+            reference = new Token[1];
+            reference[0] = referenceToken;
+        } else {
+            try {
+                reference = ((ArrayToken) referenceToken).arrayValue();
+            } catch (ClassCastException ex) {
+                throw new IllegalActionException(this,
+                    "Test fails in iteration " + _numberOfInputTokensSeen
+                    + ".\n" + "Width of input is " + width
+                    + ", but correctValues parameter " + "is not an array "
+                    + "of arrays.");
+            }
+
+            if (width != reference.length) {
+                throw new IllegalActionException(this,
+                    "Test fails in iteration " + _numberOfInputTokensSeen
+                    + ".\n" + "Width of input is " + width
+                    + ", which does not match " + "the  width of the "
+                    + _numberOfInputTokensSeen + "-th element of"
+                    + " correctValues, " + reference.length);
+            }
+        }
+
+        for (int i = 0; i < width; i++) {
+            if (!input.hasToken(i)) {
+                throw new IllegalActionException(this,
+                    "Test fails in iteration " + _numberOfInputTokensSeen
+                    + ".\n" + "Empty input on channel " + i);
+            }
+
+            Token token = input.get(i);
+            boolean isClose;
+
+            try {
+                isClose = token.isCloseTo(reference[i], _tolerance)
+                               .booleanValue();
+            } catch (IllegalActionException ex) {
+                // Chain the exceptions together so we know which test
+                // actor failed if there was more than one...
+                throw new IllegalActionException(this, ex,
+                    "Test fails in iteration " + _numberOfInputTokensSeen
+                    + ".\n" + "Value was: " + token + ". Should have been: "
+                    + reference[i]);
+            }
+
+            if (!isClose) {
+                throw new IllegalActionException(this,
+                    "Test fails in iteration " + _numberOfInputTokensSeen
+                    + ".\n" + "Value was: " + token + ". Should have been: "
+                    + reference[i]);
+            }
+        }
+
+        _numberOfInputTokensSeen++;
+    }
+
+    /** Override the base class to do nothing and return true.
+     *  @return True.
+     */
+    public boolean postfire() {
+        return true;
     }
 }
