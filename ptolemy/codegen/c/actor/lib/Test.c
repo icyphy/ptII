34d822a87ab34ddeee76bb8fd20a32ea6c7a3c14
cxh <cxh>
2005-04-01 02:04:17 +0000

    Test.c

---
+++ ptolemy/codegen/c/actor/lib/Test.c
@@ -0,0 +1,13 @@
+/***initBlock*/
+// FIXME: how do we reset count?
+int count = 0; 
+/**/
+
+/***codeBlock1*/
+// Test
+if (abs($ref(input) - $ref(correctValues, count)) > $ref(tolerance)) {
+    // FIXME: what about types other than double?
+    printf("Test fails in iteration %d.\n Value was: %f. Should have been: %f\n",
+                    , count, $ref(input), $ref(correctValues, count));
+}
+/**/
