079dbcd6020111d075eb39a3906314644e6ebe97
cxh <cxh>
2005-04-01 17:39:35 +0000

    Handle multiports

--- ptolemy/codegen/c/actor/lib/Test.c
+++ ptolemy/codegen/c/actor/lib/Test.c
@@ -5,10 +5,11 @@ int count = 0;
 
 /***codeBlock1*/
 // Test
-if (abs($ref(input) - $ref(correctValues, count)) > $ref(tolerance)) {
+// FIXME: Need to handle all channels in the multiport input.
+if (abs($ref(input#0) - $ref(correctValues, count)) > $ref(tolerance)) {
     // FIXME: what about types other than double?
     printf("Test fails in iteration %d.\n Value was: %f. Should have been: %f\n",
-        count, (double)$ref(input), (double)$ref(correctValues, count));
+        count, (double)$ref(input#0), (double)$ref(correctValues, count));
 }
 count++;
 /**/
