ad0e4eb87579124cc150003b145023f1572e9086
cxh <cxh>
2005-04-01 02:16:36 +0000

    Slightly closer to compiling

--- ptolemy/codegen/c/actor/lib/Test.c
+++ ptolemy/codegen/c/actor/lib/Test.c
@@ -8,6 +8,7 @@ int count = 0;
 if (abs($ref(input) - $ref(correctValues, count)) > $ref(tolerance)) {
     // FIXME: what about types other than double?
     printf("Test fails in iteration %d.\n Value was: %f. Should have been: %f\n",
-                    , count, $ref(input), $ref(correctValues, count));
+        count, (double)$ref(input), (double)$ref(correctValues, count));
 }
+count++;
 /**/
