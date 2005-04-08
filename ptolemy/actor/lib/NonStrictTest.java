042fd1fe60101896fb9c5ba4d0254bb1b13dd28d
cxh <cxh>
2005-04-08 22:41:35 +0000

    FindBugs: mark static String as final so as to avoid warning.

--- ptolemy/actor/lib/NonStrictTest.java
+++ ptolemy/actor/lib/NonStrictTest.java
@@ -373,7 +373,7 @@ public class NonStrictTest extends Sink {
     /** Exception message that is used if we are running under
      *  the nightly build and the trainingMode parameter is true.
      */
-    public static String TRAINING_MODE_ERROR_MESSAGE =
+    public static final String TRAINING_MODE_ERROR_MESSAGE =
     "Training Mode set for test actor and isRunningNightlyBuild()\n"
     + "  returned true, indicating that the\n"
     + "  ptolemy.ptII.isRunningNightlyBuild property is set.\n"
