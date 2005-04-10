b67db821dc7fdd6c82772ad09caebbaf34e51634
cxh <cxh>
2005-04-10 17:14:44 +0000

    FindBugs: Use StringBuffer instead of String.

--- ptolemy/graph/GraphWeightException.java
+++ ptolemy/graph/GraphWeightException.java
@@ -76,14 +76,15 @@ public class GraphWeightException extends GraphException {
      */
     static private String _argumentsToString(Object weight, Element element,
             Graph graph, String message) {
-        String outputMessage = new String() + message + weightDump(weight);
+        StringBuffer outputMessage = new StringBuffer(message 
+                + weightDump(weight));
 
         if (element != null) {
-            outputMessage += elementDump(element, graph);
+            outputMessage.append(elementDump(element, graph));
         } else {
-            outputMessage += graphDump(graph);
+            outputMessage.append(graphDump(graph));
         }
 
-        return outputMessage;
+        return outputMessage.toString();
     }
 }
