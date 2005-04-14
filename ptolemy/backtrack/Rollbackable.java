11c9ff8c163a1eacc77d1c0b960abdd00ecb5900
tfeng <tfeng>
2005-04-14 23:32:04 +0000

    Documented.

--- ptolemy/backtrack/Rollbackable.java
+++ ptolemy/backtrack/Rollbackable.java
@@ -1,4 +1,4 @@
-/* 
+/* The interface of rollbackable objects.
 
 Copyright (c) 2005 The Regents of the University of California.
 All rights reserved.
@@ -14,11 +14,11 @@ ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
 THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
 SUCH DAMAGE.
 
-THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES, 
+THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
 INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
 MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
 PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
-CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES, 
+CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
 ENHANCEMENTS, OR MODIFICATIONS.
 
 PT_COPYRIGHT_VERSION_2
@@ -31,19 +31,40 @@ package ptolemy.backtrack;
 //////////////////////////////////////////////////////////////////////////
 //// Rollbackable
 /**
- 
- 
- @author Thomas Feng
- @version $Id$
- @since Ptolemy II 5.1
- @Pt.ProposedRating Red (tfeng)
- @Pt.AcceptedRating Red (tfeng)
- */
+   The interface of rollbackable objects.
+
+   @author Thomas Feng
+   @version $Id$
+   @since Ptolemy II 5.1
+   @Pt.ProposedRating Red (tfeng)
+   @Pt.AcceptedRating Red (tfeng)
+*/
 public interface Rollbackable {
-    
+
+    /** Get the checkpoint object that monitors this rollbackable object.
+     *
+     *  @return The checkpoint object.
+     */
     public Checkpoint $GET$CHECKPOINT();
 
-    public void $RESTORE(long timestamp, boolean trim);
-    
+    /** Set the checkpoint object of this rollbackable object. A merge
+     *  operation is performed on the current checkpoint object and the new
+     *  checkpoint object, so that the two checkpoint objects monitor the same
+     *  set of objects when this function returns.
+     *
+     *  @param checkpoint The new checkpoint object.
+     *  @return This rollbackable object itself.
+     *  @see Checkpoint#setCheckpoint(Checkpoint)
+     */
     public Object $SET$CHECKPOINT(Checkpoint checkpoint);
+
+    /** Restore a previous state to all the private fields of this rollbackable
+     *  object.
+     *
+     *  @param timestamp The timestamp taken at the time when the previous
+     *   state was recorded.
+     *  @param trim Whether to delete the records used for the rollback.
+     *  @see Checkpoint#rollback(long, boolean)
+     */
+    public void $RESTORE(long timestamp, boolean trim);
 }
