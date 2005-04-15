de49816aad40766d26e3c83457eeacfb862adb5e
zhouye <zhouye>
2005-04-15 04:29:59 +0000

    fix bug of local variable "count" for Test helper.

--- ptolemy/codegen/c/actor/lib/Test.c
+++ ptolemy/codegen/c/actor/lib/Test.c
@@ -1,15 +1,15 @@
 /***initBlock*/
 // FIXME: how do we reset count?
-int count = 0; 
+int $actorSymbol(count) = 0; 
 /**/
 
 /***codeBlock1*/
 // Test
 // FIXME: Need to handle all channels in the multiport input.
-if (count < $size(correctValues) && abs($ref(input#0) - $ref(correctValues, count)) > $ref(tolerance)) {
+if ($actorSymbol(count) < $size(correctValues) && abs($ref(input#0) - $ref(correctValues, $actorSymbol(count))) > $ref(tolerance)) {
     // FIXME: what about types other than double?
     printf("Test fails in iteration %d.\n Value was: %f. Should have been: %f\n",
-        count, (double)$ref(input#0), (double)$ref(correctValues, count));
+        $actorSymbol(count), (double)$ref(input#0), (double)$ref(correctValues, $actorSymbol(count)));
 }
-count++;
+$actorSymbol(count) ++;
 /**/
