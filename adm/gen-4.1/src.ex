3cb9317b5af5d00a09971baa29491883e370527f
cxh <cxh>
2005-04-19 15:15:30 +0000

    Include ptolemy/domains/de/demo/HierarchicalModel

--- adm/gen-4.1/src.ex
+++ adm/gen-4.1/src.ex
@@ -143,7 +143,6 @@ ptolemy/domains/ct/lib/IPCInterface.java
 ptolemy/domains/dde/kernel/test/DDEDirector.tcl
 ptolemy/domains/dde/kernel/test/test.tcl
 ptolemy/domains/dde/kernel/DEThreadActor.java
-ptolemy/domains/de/demo/HierarchicalModel
 ptolemy/domains/de/kernel/DEEDirector.java
 ptolemy/domains/de/kernel/DETheadActor.java
 ptolemy/domains/dt/kernel/test/bugs
