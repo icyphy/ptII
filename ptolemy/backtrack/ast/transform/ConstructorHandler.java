dc7e609595ef36763000fd6c84855be10ad46a62
tfeng <tfeng>
2005-05-06 00:51:40 +0000

    Documentation updated.

--- ptolemy/backtrack/ast/transform/ConstructorHandler.java
+++ ptolemy/backtrack/ast/transform/ConstructorHandler.java
@@ -1,4 +1,4 @@
-/*
+/* Interface of the constructor handlers called by TypeAnalyzer.
 
 Copyright (c) 2005 The Regents of the University of California.
 All rights reserved.
@@ -39,22 +39,66 @@ import ptolemy.backtrack.ast.TypeAnalyzerState;
 //// ConstructorHandler
 
 /**
+   Interface of the constructor handlers called by {@link TypeAnalyzer}.
+   Users may register constructor handlers (and other kinds of supported
+   handlers) to the {@link TypeAnalyzer} used to analyze Java source code.
+   When the analyzer detects a constructor declaration or a call to a
+   constructor, it calls back those constructor handlers after proper types are
+   assigned to all the arguments.
+   <p>
+   Constructor handlers are allowed to modify the constructor, either by
+   modifying its children in the AST, or by replacing the whole constructor
+   with another valid AST node. This is because the handler is called after the
+   subtree rooted at the constructor is completely visited by the analyzer.
+   However, modifying any node out of this subtree (e.g., changing the parent
+   of this constructor to another one) may cause unexpected effect.
 
-
-@author Thomas Feng
-@version $Id$
-@since Ptolemy II 5.1
-@Pt.ProposedRating Red (tfeng)
-@Pt.AcceptedRating Red (tfeng)
+   @author Thomas Feng
+   @version $Id$
+   @since Ptolemy II 5.1
+   @Pt.ProposedRating Red (tfeng)
+   @Pt.AcceptedRating Red (tfeng)
 */
 public interface ConstructorHandler {
+    
+    /** Enter a field declaration.
+     * 
+     *  @param node The field declaration node.
+     *  @param state The current state of the type analyzer.
+     */
     public void enter(FieldDeclaration node, TypeAnalyzerState state);
 
+    /** Exit a field declaration.
+     * 
+     *  @param node The field declaration node.
+     *  @param state The current state of the type analyzer.
+     */
     public void exit(FieldDeclaration node, TypeAnalyzerState state);
 
-    public void handle(MethodDeclaration node, TypeAnalyzerState state);
-
+    /** Handle a class instance creation (with the <tt>new</tt> operator).
+     * 
+     *  @param node The class instance creation node.
+     *  @param state The current state of the type analyzer.
+     */
     public void handle(ClassInstanceCreation node, TypeAnalyzerState state);
 
-    public void handle(SuperConstructorInvocation node, TypeAnalyzerState state);
+    /** Handle a method declaration node that corresponds to a constructor. The
+     *  type analyzer calls back this method when it reaches a constructor. In
+     *  an Eclipse AST, a constructor is defined with a {@link
+     *  MethodDeclaration} as other methods, but its <tt>isConstructor</tt>
+     *  method returns <tt>true</tt>.
+     * 
+     *  @param node The constructor node.
+     *  @param state The current state of the type analyzer.
+     */
+    public void handle(MethodDeclaration node, TypeAnalyzerState state);
+
+    /** Handle a super constructor invocation (with the <tt>super</tt>
+     *  keyword).
+     * 
+     *  @param node The super constructor invocation node.
+     *  @param state The current state of the type analyzer.
+     */
+    public void handle(SuperConstructorInvocation node,
+            TypeAnalyzerState state);
 }
