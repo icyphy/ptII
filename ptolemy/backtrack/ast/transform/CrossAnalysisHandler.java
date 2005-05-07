63e10476ee527cf523234c306972fb512f1c0c20
tfeng <tfeng>
2005-05-07 06:59:08 +0000

    Comments in source code supported.

--- ptolemy/backtrack/ast/transform/CrossAnalysisHandler.java
+++ ptolemy/backtrack/ast/transform/CrossAnalysisHandler.java
@@ -1,4 +1,4 @@
-/*
+/* Interface of the cross-analysis handlers called by TypeAnalyzer.
 
 Copyright (c) 2005 The Regents of the University of California.
 All rights reserved.
@@ -33,16 +33,35 @@ import ptolemy.backtrack.ast.TypeAnalyzerState;
 //////////////////////////////////////////////////////////////////////////
 //// CrossAnalysisHandler
 /**
+   Interface of the cross-analysis handlers called by {@link TypeAnalyzer}.
+   Users may register cross-analysis handlers (and other kinds of supported
+   handlers) to the {@link TypeAnalyzer} used to analyze Java source code.
+   When the analyzer detects more cross-analyzed types, it calls back those
+   cross-analysis.
+   <p>
+   Cross-analyzed types are the other types to be refactored at the same time.
+   Special care is taken about those types because their instances are
+   monitored by checkpoint objects. It is not possible to know all the
+   cross-analyzed types at the beginning of the refactoring. It is then
+   necessary to fix the refactoring result whenever more cross-analyzed types
+   are found as the type analysis goes on.
 
-
-@author Thomas Feng
-@version $Id$
-@since Ptolemy II 5.1
-@Pt.ProposedRating Red (tfeng)
-@Pt.AcceptedRating Red (tfeng)
+   @author Thomas Feng
+   @version $Id$
+   @since Ptolemy II 5.1
+   @Pt.ProposedRating Red (tfeng)
+   @Pt.AcceptedRating Red (tfeng)
 */
 public interface CrossAnalysisHandler {
 
+    /** Handle new cross-analyzed types recorded in the state of the type
+     *  analyzer. This method may be triggered whenever a new cross-analyzed
+     *  type is found by the type analyzer. Implementing classes may implement
+     *  this method to update the refactoring result.
+     * 
+     *  @param state The current state of the type analyzer.
+     *  @see TypeAnalyzerState#getCrossAnalyzedTypes()
+     */
     public void handle(TypeAnalyzerState state);
 
 }
