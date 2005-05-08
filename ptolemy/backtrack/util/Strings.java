bf833d3a1dd7d53a2ecfbbb93e5063ceacd34b60
tfeng <tfeng>
2005-05-08 04:01:11 +0000

    Documentation updated.

--- ptolemy/backtrack/util/Strings.java
+++ ptolemy/backtrack/util/Strings.java
@@ -1,4 +1,4 @@
-/* 
+/* A tool for string manipulation.
 
 Copyright (c) 2005 The Regents of the University of California.
 All rights reserved.
@@ -29,22 +29,36 @@ COPYRIGHTENDKEY
 package ptolemy.backtrack.util;
 
 import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.StringTokenizer;
+
+import ptolemy.backtrack.ast.UnknownASTException;
 
 //////////////////////////////////////////////////////////////////////////
 //// Strings
 /**
+   A tool for string manipulation.
 
-
- @author Thomas Feng
- @version $Id$
- @since Ptolemy II 5.1
- @Pt.ProposedRating Red (tfeng)
- @Pt.AcceptedRating Red (tfeng)
- */
+   @author Thomas Feng
+   @version $Id$
+   @since Ptolemy II 5.1
+   @Pt.ProposedRating Red (tfeng)
+   @Pt.AcceptedRating Red (tfeng)
+*/
 public class Strings {
 
+    /** Combine two arrays and return the result of their combination. If any
+     *  one of the two arrays is <tt>null</tt>, the other one is return. If
+     *  both of them are <tt>null</tt>, <tt>null</tt> is returned. In the
+     *  result, elements in <tt>array1</tt> appears before those in
+     *  <tt>array2</tt>.
+     * 
+     *  @param array1 The first array.
+     *  @param array2 The second array.
+     *  @return The combination result of the two array.
+     */
     public static String[] combineArrays(String[] array1, String[] array2) {
         if (array1 == null)
             return array2;
@@ -58,6 +72,14 @@ public class Strings {
         return result;
     }
     
+    /** Decode a string encoded with file names (or directory names) in it.
+     *  Each file name in the string is separated with a path separator ({@link
+     *  File#pathSeparator}) or a new line character.
+     * 
+     *  @param encode The string with file names encoded in it.
+     *  @return The array of decoded file names.
+     *  @see #encodeFileNames(String[])
+     */
     public static String[] decodeFileNames(String encode) {
         StringTokenizer tokenizer =
             new StringTokenizer(encode, File.pathSeparator
@@ -68,6 +90,13 @@ public class Strings {
         return (String[])list.toArray(new String[list.size()]);
     }
     
+    /** Encode file names (or directory names) in to a string. Each file name
+     *  is separated with a path separator ({@link File#pathSeparator}).
+     * 
+     *  @param fileNames The array of file names.
+     *  @return The string with file names encoded in it.
+     *  @see #decodeFileNames(String)
+     */
     public static String encodeFileNames(String[] fileNames) {
         StringBuffer path = new StringBuffer("");
         for (int i = 0; i < fileNames.length; i++) {
@@ -75,5 +104,25 @@ public class Strings {
             path.append(File.pathSeparator);
         }
         return path.toString();
+    }
+
+    /** Convert an array of strings to an array of {@link URL}s with
+     *  {@link File#toURL()}.
+     *
+     *  @param strings The array of strings.
+     *  @return The array of urls.
+     */
+    public static URL[] stringsToUrls(String[] strings) {
+        if (strings == null)
+            return new URL[0];
+
+        URL[] urls = new URL[strings.length];
+        for (int i = 0; i < strings.length; i++)
+            try {
+                urls[i] = new File(strings[i]).toURL();
+            } catch (MalformedURLException e) {
+                throw new UnknownASTException();
+            }
+        return urls;
     }
 }
