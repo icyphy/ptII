14af3c64452432c96ed82b374117f321345f1008
cxh <cxh>
2005-05-17 15:21:08 +0000

    Shallow cg of wireless domains requires that we don't filter out CG

--- ptolemy/copernicus/kernel/Copernicus.java
+++ ptolemy/copernicus/kernel/Copernicus.java
@@ -563,8 +563,9 @@ public class Copernicus {
         RemoveGraphicalClasses removeGraphicalClasses = new RemoveGraphicalClasses();
 
         // FIXME: Not sure why this is necessary, but it helps
-        // when generating an applet for moml/demo/spectrum.xml
-        removeGraphicalClasses.put("ptolemy.kernel.util.Location", null);
+        // when generating an applet for moml/demo/spectrum.xml.
+        // However, if we remove Location, then shallow CG of Wireless fails.
+        //removeGraphicalClasses.put("ptolemy.kernel.util.Location", null);
 
         // shallow/test/IIRGUI.xml has a GeneratorTableauAttribute in it.
         removeGraphicalClasses.put("ptolemy.copernicus.gui.GeneratorTableauAttribute",
