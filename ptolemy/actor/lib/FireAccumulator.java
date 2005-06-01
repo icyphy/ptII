57d1e984b6da3a684bb3dcfe45e7fa406281c6c4
acataldo <acataldo>
2005-06-01 14:44:00 +0000

    An actor which sums all input values available at each firing

---
+++ ptolemy/actor/lib/FireAccumulator.java
@@ -0,0 +1,130 @@
+/* An actor that outputs the sum of the inputs available at firing.
+
+Copyright (c) 1998-2005 The Regents of the University of California.
+All rights reserved.
+Permission is hereby granted, without written agreement and without
+license or royalty fees, to use, copy, modify, and distribute this
+software and its documentation for any purpose, provided that the above
+copyright notice and the following two paragraphs appear in all copies
+of this software.
+
+IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+SUCH DAMAGE.
+
+THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ENHANCEMENTS, OR MODIFICATIONS.
+
+PT_COPYRIGHT_VERSION_2
+COPYRIGHTENDKEY
+
+*/
+package ptolemy.actor.lib;
+
+import ptolemy.data.Token;
+import ptolemy.data.expr.Parameter;
+import ptolemy.kernel.CompositeEntity;
+import ptolemy.kernel.util.IllegalActionException;
+import ptolemy.kernel.util.NameDuplicationException;
+import ptolemy.kernel.util.Workspace;
+
+//////////////////////////////////////////////////////////////////////////
+//// FireAccumulator
+
+/**
+   Output the sum of all the inputs available at each
+   firing.
+
+   @author Adam Cataldo
+*/
+public class FireAccumulator extends Transformer {
+    /** Construct an actor with the given container and name.
+     *  @param container The container.
+     *  @param name The name of this actor.
+     *  @exception IllegalActionException If the actor cannot be contained
+     *   by the proposed container.
+     *  @exception NameDuplicationException If the container already has an
+     *   actor with this name.
+     */
+    public FireAccumulator(CompositeEntity container, String name)
+            throws NameDuplicationException, IllegalActionException {
+        super(container, name);
+
+        init = new Parameter(this, "init");
+        init.setExpression("0");
+
+        // set the type constraints.
+        output.setTypeAtLeast(init);
+        output.setTypeAtLeast(input);
+    }
+
+    ///////////////////////////////////////////////////////////////////
+    ////                     ports and parameters                  ////
+
+    /** The value produced by the actor on its first iteration.
+     *  The default value of this parameter is the integer 0.
+     */
+    public Parameter init;
+
+    ///////////////////////////////////////////////////////////////////
+    ////                         public methods                    ////
+
+    /** Clone the actor into the specified workspace. This calls the
+     *  base class and then sets up the type constraints.
+     *  @param workspace The workspace for the new object.
+     *  @return A new actor.
+     *  @exception CloneNotSupportedException If a derived class contains
+     *   an attribute that cannot be cloned.
+     */
+    public Object clone(Workspace workspace) throws CloneNotSupportedException {
+        FireAccumulator newObject = (FireAccumulator) super.clone(workspace);
+
+        // set the type constraints.
+        newObject.output.setTypeAtLeast(newObject.init);
+        newObject.output.setTypeAtLeast(newObject.input);
+        return newObject;
+    }
+
+    /** Consume all tokens from the input channel, 
+     *  add the tokens and send them through the output
+     *  port.
+     *  @exception IllegalActionException If addition is not
+     *   supported by the supplied tokens.
+     */
+    public void fire() throws IllegalActionException {
+        while(input.hasToken(0)) {
+        	Token in = input.get(0);
+            _sum = _sum.add(in);
+        }
+        output.broadcast(_sum);
+     }
+
+    /** Reset the running sum to equal the value of <i>init</i>.
+     *  @exception IllegalActionException If the parent class throws it.
+     */
+    public void initialize() throws IllegalActionException {
+        super.initialize();
+        _sum = output.getType().convert(init.getToken());
+    }
+
+    /** Reset the running sum to equal the value of <i>init</i>.
+     *  @exception IllegalActionException If the base class throws it.
+     */
+    public boolean postfire() throws IllegalActionException {
+        _sum = output.getType().convert(init.getToken());
+        return super.postfire();
+    }
+
+    ///////////////////////////////////////////////////////////////////
+    ////                         private members                   ////
+
+    /** The running sum. */
+    private Token _sum;
+
+}
