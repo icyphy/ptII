d368be1421e780325db6fdc39679c339050ab003
tiffcraw <tiffcraw>
2005-06-16 18:35:50 +0000

    *** empty log message ***

---
+++ ptolemy/domains/gr/lib/vr/Reslice.java
@@ -0,0 +1,142 @@
+/* An actor that reads an array of images.   */
+ 
+  
+package ptolemy.domains.gr.lib.vr;
+
+
+import java.awt.Image;
+import java.lang.String;
+import java.net.URL;
+import java.util.LinkedList;
+import java.util.List;
+
+
+import ij.ImagePlus;
+import ij.ImageStack;
+import ij.process.ColorProcessor;
+import ij.plugin.Slicer;
+
+
+
+
+
+import ptolemy.actor.TypedIOPort;
+import ptolemy.kernel.CompositeEntity;
+
+import ptolemy.kernel.util.IllegalActionException;
+import ptolemy.kernel.util.NameDuplicationException;
+import ptolemy.actor.TypedAtomicActor;
+import ptolemy.domains.sdf.lib.SDFTransformer;
+import ptolemy.data.AWTImageToken;
+import ptolemy.data.ObjectToken;
+import ptolemy.data.StringToken;
+import ptolemy.data.Token;
+import ptolemy.data.IntToken;
+import ptolemy.data.type.ArrayType;
+import ptolemy.data.type.BaseType;
+import ptolemy.actor.parameters.FilePortParameter;
+import ptolemy.actor.parameters.ParameterPort;
+import ptolemy.data.expr.Parameter;
+import ptolemy.graph.Inequality;
+import ptolemy.graph.InequalityTerm;
+
+//////////////////////////////////////////////////////////////////////////
+////Slicer
+/**
+ An actor that reads an array of images. 
+
+@see ptolemy.actor.lib.medicalimaging
+
+@author T. Crawford
+@version 
+@since 
+@Pt.ProposedRating Red
+@Pt.AcceptedRating Red
+
+*/public class Reslice extends TypedAtomicActor{
+	    /**Construct an actor with the given container and name.
+         * @param container The container
+         * @param name The name of this actor
+         * @exception IllegalActionException If the actor cannot be contained
+         *   by the proposed container.
+         * @exception NameDuplicationException If the container already has an
+         *   actor with this name.
+         */
+    
+	public Reslice(CompositeEntity container, String name)
+            throws IllegalActionException, NameDuplicationException {
+         super(container, name);
+        
+         input = new TypedIOPort(this, "input", true, false);
+         input.setTypeEquals(BaseType.OBJECT);
+         
+         output = new TypedIOPort(this, "output", false, true);
+         output.setTypeEquals(BaseType.OBJECT);
+        
+        
+        xResolution = new Parameter(this, "xResolution");
+        xResolution.setExpression("256");
+        xResolution.setTypeEquals(BaseType.INT);
+        
+        yResolution = new Parameter(this, "yResolution");
+        yResolution.setExpression("256");
+        yResolution.setTypeEquals(BaseType.INT);
+        
+        stackSize = new Parameter(this, "stackSize");
+        stackSize.setExpression("50");
+        stackSize.setTypeEquals(BaseType.INT);
+        
+        
+              
+    }
+
+    ////////////////////////////////////////////////////////////////////
+    ////////               ports and parameters                  ////////     
+    
+    //public FilePortParameter input;
+    public TypedIOPort input;
+    public TypedIOPort output;
+    public Parameter xResolution;
+    public Parameter yResolution;
+    public Parameter stackSize;
+    
+       
+    ////////////////////////////////////////////////////////////////////
+    ////////                public methods                     ////////     
+
+    
+    /** Output the data read in the prefire.
+     *  @exception IllegalActionException If there's no director.
+     */
+    public void fire() throws IllegalActionException {
+        super.fire();   
+        ObjectToken objectToken = (ObjectToken) input.get(0);
+        ImagePlus  imagePlus = (ImagePlus)objectToken.getValue(); 
+        Slicer slicer = new Slicer();
+        _imagePlus = slicer.reslice(imagePlus);
+        output.broadcast(new ObjectToken(_imagePlus));
+    }
+    
+    
+    public void initialize() throws IllegalActionException
+    {
+    
+      _xResolution = ((IntToken)xResolution.getToken()).intValue();
+      _yResolution = ((IntToken)yResolution.getToken()).intValue();
+      _stackSize = ((IntToken)stackSize.getToken()).intValue();
+      
+    }
+    
+    
+    ///////////////////////////////////////////////////////////////////
+    ////                         private members                   ////
+    
+    //Image that is readin
+    private ImagePlus _imagePlus;  
+    
+    private int _stackSize;
+    
+    private int _xResolution;
+    
+    private int _yResolution;
+ }
---
+++ ptolemy/domains/gr/lib/vr/StackReader.java
@@ -0,0 +1,225 @@
+/* An actor that reads an array of images.   */
+ 
+  
+package ptolemy.domains.gr.lib.vr;
+
+
+import java.awt.Image;
+import java.lang.String;
+import java.net.URL;
+import java.util.LinkedList;
+import java.util.List;
+
+
+import ij.ImagePlus;
+import ij.ImageStack;
+import ij.process.ColorProcessor;
+
+
+
+
+
+
+import ptolemy.actor.TypedIOPort;
+import ptolemy.kernel.CompositeEntity;
+
+import ptolemy.kernel.util.IllegalActionException;
+import ptolemy.kernel.util.NameDuplicationException;
+import ptolemy.actor.TypedAtomicActor;
+import ptolemy.domains.sdf.lib.SDFTransformer;
+import ptolemy.data.AWTImageToken;
+import ptolemy.data.ObjectToken;
+import ptolemy.data.StringToken;
+import ptolemy.data.Token;
+import ptolemy.data.IntToken;
+import ptolemy.data.type.ArrayType;
+import ptolemy.data.type.BaseType;
+import ptolemy.actor.parameters.FilePortParameter;
+import ptolemy.actor.parameters.ParameterPort;
+import ptolemy.data.expr.Parameter;
+import ptolemy.graph.Inequality;
+import ptolemy.graph.InequalityTerm;
+
+//////////////////////////////////////////////////////////////////////////
+////StackReader
+/**
+ An actor that reads an array of images. 
+
+@see ptolemy.actor.lib.medicalimaging
+
+@author T. Crawford
+@version 
+@since 
+@Pt.ProposedRating Red
+@Pt.AcceptedRating Red
+
+*/public class StackReader extends SDFTransformer{
+	    /**Construct an actor with the given container and name.
+         * @param container The container
+         * @param name The name of this actor
+         * @exception IllegalActionException If the actor cannot be contained
+         *   by the proposed container.
+         * @exception NameDuplicationException If the container already has an
+         *   actor with this name.
+         */
+    
+	public StackReader(CompositeEntity container, String name)
+            throws IllegalActionException, NameDuplicationException {
+         super(container, name);
+        
+                   
+        
+        input_tokenConsumptionRate.setExpression("stackSize");
+        
+        output.setTypeEquals(BaseType.OBJECT);
+        
+        xResolution = new Parameter(this, "xResolution");
+        xResolution.setExpression("256");
+        xResolution.setTypeEquals(BaseType.INT);
+        
+        yResolution = new Parameter(this, "yResolution");
+        yResolution.setExpression("256");
+        yResolution.setTypeEquals(BaseType.INT);
+        
+       stackSize = new Parameter(this, "stackSize");
+       stackSize.setExpression("50");
+       stackSize.setTypeEquals(BaseType.INT);
+        
+        
+              
+    }
+
+    ////////////////////////////////////////////////////////////////////
+    ////////               ports and parameters                  ////////     
+    
+    //public FilePortParameter input;
+    public Parameter xResolution;
+    public Parameter yResolution;
+    public Parameter stackSize;
+    
+       
+    ////////////////////////////////////////////////////////////////////
+    ////////                public methods                     ////////     
+
+    
+    /** Output the data read in the prefire.
+     *  @exception IllegalActionException If there's no director.
+     */
+    public void fire() throws IllegalActionException {
+        super.fire();
+        _readStack();
+        _imagePlus = new ImagePlus("Image Stack", _imageStack); 
+        System.out.println("stackSize = " + _imageStack.getSize());
+       // output.broadcast(new AWTImageToken(_image));
+        output.broadcast(new ObjectToken(_imagePlus));
+    }
+    
+    
+    public void initialize() throws IllegalActionException
+    {
+     // _parameterPort =  input.getPort();
+      _xResolution = ((IntToken)xResolution.getToken()).intValue();
+      _yResolution = ((IntToken)yResolution.getToken()).intValue();
+      _stackSize = ((IntToken)stackSize.getToken()).intValue();
+      
+    }
+    
+    
+  /*  public boolean prefire() throws IllegalActionException {
+       public boolean prefire() throws IllegalActionException {
+        Token rateToken = input_tokenConsumptionRate.getToken();
+        int required = ((IntToken) rateToken).intValue();
+
+        // Derived classes may convert the input port to a multiport.
+        for (int i = 0; i < input.getWidth(); i++) {
+            if (!input.hasToken(i, required)) {
+                if (_debugging) {
+                    _debug("Called prefire(), which returns false");
+                }
+
+                return false;
+            }
+        }
+
+        return super.prefire();
+    }  
+    }*/
+    ///////////////////////////////////////////////////////////////////
+    ////                         private methods                   ////
+    private void _readStack() throws IllegalActionException {
+        _imageStack = new ImageStack(_xResolution, _yResolution);
+        for(int i = 0; i< _stackSize; i++){
+       	    _fileRoot = new String[_stackSize];
+            Token[] token = input.get(0, _stackSize); 
+        	_fileRoot[i] = ((StringToken)token[i]).stringValue();
+            System.out.println("_fileRoot = " + _fileRoot[i]);
+        
+        	if (_fileRoot[i] == null) {
+        		throw new IllegalActionException("sourceURL was null");
+        	}
+        	//_fileRoot = _url.getFile();
+        
+            
+           
+        //if (imagePlus == null) {
+            //FIXME Should check each image to see if valid
+            ImagePlus imagePlus = new ImagePlus(_fileRoot[i]);
+           if (imagePlus != null){
+            _image = imagePlus.getImage();
+           
+            _colorProcessor = new ColorProcessor(_image);
+            _imageStack.addSlice(_fileRoot[i], _colorProcessor);
+            imagePlus = null;
+            System.out.println("stackSize = " + _imageStack.getSize());
+        }else{
+         throw new IllegalActionException("_image is null");
+        }
+        }
+        
+    }
+    
+    /** Return the type constraint that the type of the elements of the
+     *  output array is no less than the type of the input port.
+     *  @return A list of inequalities.
+     */
+    /*public List typeConstraintList() {
+        BaseType outType = (BaseType) output.getType();
+        InequalityTerm elementTerm = outType.getTypeTerm();
+        Inequality ineq = new Inequality(input.getTypeTerm(), elementTerm);
+
+        List result = new LinkedList();
+        result.add(ineq);
+        return result;
+    }*/
+
+    
+    ///////////////////////////////////////////////////////////////////
+    ////                         private members                   ////
+    
+    //Image that is readin
+    private ImagePlus _imagePlus;
+    
+    //Image that is readin
+    private ColorProcessor _colorProcessor;
+    
+    
+    //Image that is readin
+    private ImageStack _imageStack;
+    
+    
+//  The URL as a string.
+    private String _fileRoot[];
+
+    // Image that is read in.
+    private Image _image;
+
+    // The URL of the file.
+    private URL _url;
+    
+    
+    private int _stackSize;
+    
+    private int _xResolution;
+    
+    private int _yResolution;
+ }
