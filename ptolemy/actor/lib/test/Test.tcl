4b8454b0ff9dd4c58f9039ade41d384fe8716664
eal <eal>
2005-06-28 23:39:33 +0000

    Updated tests to show width parameter of IORelation in descriptions.

--- ptolemy/actor/lib/test/Test.tcl
+++ ptolemy/actor/lib/test/Test.tcl
@@ -181,7 +181,7 @@ test Test-1.5 {Export} {
 <!DOCTYPE entity PUBLIC "-//UC Berkeley//DTD MoML 1//EN"
     "http://ptolemy.eecs.berkeley.edu/xml/dtd/MoML_1.dtd">
 <entity name="top" class="ptolemy.actor.TypedCompositeActor">
-    <property name="_createdBy" class="ptolemy.kernel.attributes.VersionAttribute" value="4.1">
+    <property name="_createdBy" class="ptolemy.kernel.attributes.VersionAttribute" value="5.1-devel">
     </property>
     <property name="" class="ptolemy.domains.sdf.kernel.SDFDirector">
         <property name="timeResolution" class="ptolemy.moml.SharedParameter" value="1E-10">
@@ -231,6 +231,8 @@ test Test-1.5 {Export} {
         </port>
     </entity>
     <relation name="_R" class="ptolemy.actor.TypedIORelation">
+        <property name="width" class="ptolemy.data.expr.Parameter" value="1">
+        </property>
     </relation>
     <link port="ramp.output" relation="_R"/>
     <link port="test.input" relation="_R"/>
