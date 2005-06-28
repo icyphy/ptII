5722ca266d2075e2f8e19f33073ecd1bf408bf00
mankit <mankit>
2005-06-28 02:28:33 +0000

    successfully produced sounds

--- ptolemy/codegen/demo/Sound/audioPlayer.c
+++ ptolemy/codegen/demo/Sound/audioPlayer.c
@@ -1,15 +1,35 @@
+/* 
+ * audioPlayer.c converts some random sample data from type double to
+ * 16-bit unsigned int. Then it puts the 16-bit sample into a buffer, while
+ * SDL constantly clear the buffer by mixing the sample data in the buffer 
+ * into the audio stream. No separate thread is created in this process.
+ * However, a semaphore is used to avoid overfilling the buffer. Also, 
+ * we have to avoid putting sample and mixing simultaneously by calling 
+ * SDL_LockAudio() and SDL_UnlockAudio() to prevent the callback function 
+ * interferring.
+ * 
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <signal.h>
+#include <math.h>
 
 #include "SDL.h"
 #include "SDL_audio.h"
 #include "SDL_thread.h"
 
 
-#define USE_CALLBACK 0
-#define NUM_SOUNDS 100
+/**
+ * @author Man-Kit Leung
+ * @version $Id$
+ */
 
+#define NUM_SOUNDS 2    // mono or stereo
+
+// FIXME: how should we determine the buffer size??
+#define BUFFER_SIZE 65500   // ~50 KB buffer size
+
+#define _debug 0
 
 struct sample {
     Uint8 *data;
@@ -17,104 +37,28 @@ struct sample {
     Uint32 dlen;
 } sounds[NUM_SOUNDS];
 
-int g_index;
-int done;
 
 SDL_sem *sem;
 SDL_mutex *mut;
-SDL_AudioCVT cvt;
 SDL_AudioSpec fmt;
 
-putSample (short data) {
-    fprintf(stderr, "putSample running ( %d )\n", data);
 
-    SDL_AudioCVT cvt;
-    int l_index;
-    int i;
-
-    /////////////// LOCK //////////////////////////////
-    if (SDL_SemWait(sem) == -1) {
-        fprintf(stderr, "Error waiting for semaphore: %s\n",SDL_GetError());
-        exit(1);
-    }
-    /////////////// LOCK //////////////////////////////
-
-
-    /* Look for an empty (or finished) sound slot */
-    fprintf(stderr, "putSample looking for empty slot\n");
-
-    for ( l_index=0; l_index<NUM_SOUNDS; ++l_index ) {
-        if ( sounds[l_index].dpos == sounds[l_index].dlen ) {
-            break;
-        }
-    }
-    fprintf(stderr, "putSample finishes looking\n");
-
-    if ( l_index == NUM_SOUNDS ) {
-        // FIXME: use semaphore
-        SDL_Delay(1000);
-    }
-
-    fprintf(stderr, "putSample convert audio\n");
-
-    SDL_BuildAudioCVT(&cvt, AUDIO_S16, 2, 22050, AUDIO_S16,2,22050);
-    cvt.buf = malloc(16*cvt.len_mult);
-    memcpy(cvt.buf, &data, 16);
-    cvt.len = 16;
-    SDL_ConvertAudio(&cvt);
-
-    fprintf(stderr, "putSample finishes convert audio\n");
-
-    /* Put the sound data in the slot (it starts playing immediately) */
-    fprintf(stderr, "putSample going to lock audio\n");
-
-    SDL_LockAudio();
-
-    fprintf(stderr, "putSample locked audio\n");
-    sounds[l_index].data = cvt.buf;
-    sounds[l_index].dlen = cvt.len_cvt;
-    sounds[l_index].dpos = 0;
-    fprintf(stderr, "putSample going to unlock\n");
-    SDL_UnlockAudio();
-
-    fprintf(stderr, "putSample exit\n");
+Uint16 convertSample(double sample) {
+    return  (Uint16) floor(sample * 32767); //2^15-1
 }
 
 
-void mixaudio(void *unused, Uint8 *stream, int len)
+main(int argc, char *argv[])
 {
-    if(SDL_mutexP(mut)==-1){
-        fprintf(stderr, "Couldn't lock mutex\n");
-        exit(-1);
-    }
-
-    fprintf(stderr, "*******************************mixaudio running (%d)\n", g_index);
-
-    SDL_MixAudio(stream, &sounds[g_index].data[sounds[g_index].dpos], 1, SDL_MIX_MAXVOLUME);
-
-    sounds[g_index].dpos = sounds[g_index].dlen;
-
-    g_index++;
-    g_index %= NUM_SOUNDS;
-    ///////////// LOCK //////////////////////////////
-    if (SDL_SemPost(sem) == -1) {
-        fprintf(stderr, "Error unlocking semaphor: %s\n",SDL_GetError());
+    /*************** initialize ************************/   
+    int* data;
+    int i, j;
+    double _sample;
+    
+    if ((sem = SDL_CreateSemaphore(BUFFER_SIZE/2)) == NULL) {
+        fprintf(stderr, "Error creating semaphor: %s\n",SDL_GetError());
         exit(1);
     }
-    ///////////// LOCK //////////////////////////////
-
-    fprintf(stderr, "*******************************mixaudio exit\n");
-
-    if(SDL_mutexV(mut)==-1){
-        fprintf(stderr, "Couldn't unlock mutex\n");
-        exit(-1);
-    }
-}
-
-
-int fireAudio (void *data) {
-
-    fprintf(stderr, "fireAudio running\n");
 
     /* Set 16-bit stereo audio at 22Khz */
     fmt.freq = 22050;
@@ -129,59 +73,112 @@ int fireAudio (void *data) {
         fprintf(stderr, "Unable to open audio: %s\n", SDL_GetError());
         exit(1);
     }
+    for ( i=0; i<NUM_SOUNDS; ++i ) {
+        sounds[i].data = (Uint8*) malloc(BUFFER_SIZE);
+        sounds[i].dpos = 0;
+        sounds[i].dlen = 0;
+    }
+
+
     SDL_PauseAudio(0);
-    while ( done == 0 ) {
-        // **have really short delays for the mixing thread
-        SDL_Delay(1);   
-    }
 
-    fprintf(stderr, "closing audio\n");
-
-    SDL_CloseAudio();
-    return 0;
-}
-
-
-
-
-
-
-int main(int argc, char *argv[])
-{
-    /*************** initialize ************************/
-    done = 0;
-    g_index = 0;
-    
-    mut=SDL_CreateMutex();
-    if ((sem = SDL_CreateSemaphore(NUM_SOUNDS)) == NULL) {
-        fprintf(stderr, "Error creating semaphor: %s\n",SDL_GetError());
-        exit(1);
-    }
-    fprintf(stderr, "main running\n");
-
-
-    SDL_Thread *fireAudioThread = SDL_CreateThread(fireAudio, NULL);
-
-    int* data;
-    int i;
 
     /*************** fire iterations *******************/
-    for (i=0; i<100000000; i++) {
-        putSample (i);          //((i%300)*11);
-        SDL_Delay(50);
+    for (i=0; i<1000000; i++) {
+        // ** we can feed some other interesting samples, this is just for testing
+        for ( j=0; j<NUM_SOUNDS; ++j ) {
+            _sample = i/31.3*(j+1);
+            putSample (convertSample(_sample), j);
+        }
     }
 
         
     /****************** wrap-up ************************/
-    done = 1;
-    SDL_WaitThread (fireAudioThread, NULL);
+    fprintf(stdout, "close audio\n");
+    //SDL_PauseAudio(1);
+    SDL_CloseAudio();
 
-    SDL_DestroyMutex(mut);
+    fprintf(stdout, "kill semaphore\n");
     if (sem != NULL) {
         SDL_DestroySemaphore(sem);
         sem = NULL;
     }
-    fprintf(stderr, "main finishes\n");
-    return 0;
+
+    fprintf(stdout, "done\n");
 }
+
+
+void mixaudio(void *unused, Uint8 *stream, int len)
+{
+    int i;
+    int _resetSize = 0;
+    Uint32 amount;
+
+    if (_debug) {
+        fprintf(stdout, "*******************************mixaudio running (data_length= %d), (len= %d)\n", sounds[0].dlen, len);
+    }
+
+    // mixing sound data into output stream
+    for ( i=0; i<NUM_SOUNDS; i++ ) {
+        while (sounds[i].dlen > sounds[i].dpos) {
+            amount = (sounds[i].dlen-sounds[i].dpos);
+            if ( amount > len ) {
+                amount = len;
+            }
+            SDL_MixAudio(stream, &sounds[i].data[sounds[i].dpos], amount, SDL_MIX_MAXVOLUME);
+            sounds[i].dpos += amount;
+        }
+    }
+    // reset DPOS, so mixing would start from the beginning of the buffer next time 
+    // reset DLEN, so we would put sample from the beginning
+    for ( i=0; i<NUM_SOUNDS; i++ ) {
+        _resetSize += sounds[i].dlen;
+        sounds[i].dlen = 0;
+        sounds[i].dpos = 0;
+    }
+
+    ///////////// UNLOCK //////////////////////////////
+    
+    for (i=0; i<_resetSize/2; i++) {
+        if (SDL_SemPost(sem) == -1) {
+            fprintf(stderr, "Error posting semaphor: %s\n",SDL_GetError());
+            exit(1);
+        }
+    }
+    ///////////// UNLOCK //////////////////////////////
+}
+
+putSample (Uint16 data, int channel) {
+    int i;
+    if (_debug) 
+        fprintf(stdout, "putSample running (data= %d), (sem= %d)\n", data, SDL_SemValue(sem));
+    /////////////// LOCK //////////////////////////////
+    if (SDL_SemWait(sem) != 0) {
+        fprintf(stderr, "Error waiting for semaphore: %s\n",SDL_GetError());
+        exit(1);
+    }
+    /////////////// LOCK //////////////////////////////
+    SDL_LockAudio();
+
+    sounds[channel].data[sounds[channel].dlen] = (data >> 8);
+    if (_debug) {
+        fprintf(stdout, "putSample locked audio, (data= %d), (sound.data= %d)\n", data >> 8, sounds[channel].data[sounds[channel].dlen]);
+    }
+    sounds[channel].dlen++;
+
+    sounds[channel].data[sounds[channel].dlen] = data;
+    if (_debug) {
+        fprintf(stdout, "putSample locked audio, (data= %d), (sound.data= %d)\n", data, sounds[channel].data[sounds[channel].dlen]);
+    }
+    sounds[channel].dlen++;
+
+    if (_debug) {
+        fprintf(stdout, "putSample unlocked audio\n");
+    }
+
+    SDL_UnlockAudio();
+}
+
+
+
 
