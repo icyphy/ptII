9d689b51175338fb9403c8d054ad68825999ff48
cxh <cxh>
2005-07-12 23:16:13 +0000

    Updated to 5.0.1

--- ptolemy/kernel/attributes/VersionAttribute.java
+++ ptolemy/kernel/attributes/VersionAttribute.java
@@ -288,7 +288,7 @@ public class VersionAttribute extends StringAttribute implements Comparable {
 
     static {
         try {
-            CURRENT_VERSION = new VersionAttribute("5.0");
+            CURRENT_VERSION = new VersionAttribute("5.0.1");
         } catch (Exception ex) {
             throw new ExceptionInInitializerError(
                     "Failed to create CURRENT_VERSION: "
