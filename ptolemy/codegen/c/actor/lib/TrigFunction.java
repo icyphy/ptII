eed42aa5a76e8359a59eaf055670e7b010aa690b
cxh <cxh>
2005-07-20 18:11:56 +0000

    Replaced tabs with eight spaces

--- ptolemy/codegen/c/actor/lib/TrigFunction.java
+++ ptolemy/codegen/c/actor/lib/TrigFunction.java
@@ -70,7 +70,7 @@ public class TrigFunction extends CCodeGeneratorHelper {
 
         String function = actor.function.getExpression();
         String codeBlockName = (function.equals("sin"))  ? "sinBlock"
-        		   : ((function.equals("cos"))  ? "cosBlock"
+                           : ((function.equals("cos"))  ? "cosBlock"
                    : ((function.equals("tan"))  ? "tanBlock"
                    : ((function.equals("asin")) ? "asinBlock"
                    : ((function.equals("acos")) ? "acosBlock"
