31658f2d186e56610e69dd654a7f625dc0e448df
cxh <cxh>
2005-08-01 05:50:13 +0000

    CodeGenerator.tcl

---
+++ ptolemy/codegen/kernel/test/CodeGenerator.tcl
@@ -0,0 +1,76 @@
+# Test CodeGenerator
+#
+# @Author: Christopher Brooks
+#
+# @Version: $Id$
+#
+# @Copyright (c) 2005 The Regents of the University of California.
+# All rights reserved.
+#
+# Permission is hereby granted, without written agreement and without
+# license or royalty fees, to use, copy, modify, and distribute this
+# software and its documentation for any purpose, provided that the
+# above copyright notice and the following two paragraphs appear in all
+# copies of this software.
+#
+# IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+# FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+# ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+# THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+#
+# THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+# INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+# PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+# CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+# ENHANCEMENTS, OR MODIFICATIONS.
+#
+# 						PT_COPYRIGHT_VERSION_2
+# 						COPYRIGHTENDKEY
+#######################################################################
+
+# Ptolemy II test bed, see $PTII/doc/coding/testing.html for more information.
+
+# Load up the test definitions.
+if {[string compare test [info procs test]] == 1} then {
+    source testDefs.tcl
+} {}
+
+if {[info procs sdfModel] == "" } then {
+    source [file join $PTII util testsuite models.tcl]
+}
+
+#####
+test CodeGenerator-1.1 {Instantiate a CodeGenerator, call a few methods} {
+    set model [sdfModel]
+    set codeGenerator \
+	    [java::new ptolemy.codegen.kernel.CodeGenerator \
+	    $model "myCodeGenerator"]
+    list \
+	[$codeGenerator toString] \
+	[$codeGenerator comment {This is a comment}] \
+} {{ptolemy.codegen.kernel.CodeGenerator {.top.myCodeGenerator}} {/* This is a comment */
+}}
+
+#####
+test CodeGenerator-2.1 {Call generateCode(StringBuffer)} {
+    set results [java::new StringBuffer]
+    $codeGenerator generateCode $results
+    list [$results toString]
+} {{/* Variable Declarations .top */
+main() {
+/* The preinitialization of the director. */
+int iteration = 0;
+/* Initialize .top */
+/* The initialization of the director. */
+/* Wrapup .top */
+/* The wrapup of the director. */
+}
+}}
+
+#####
+test CodeGenerator-3.1 {getComponent()} {
+    [$codeGenerator getComponent] toString
+} {ptolemy.actor.TypedCompositeActor {.top}}
+
