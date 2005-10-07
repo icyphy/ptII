1ff33a3ae843988c3c9d0d9d0ae24b2f60276ea8
cxh <cxh>
2005-10-07 00:37:13 +0000

    5.0.1 webstart details

--- mk/jnlp.mk
+++ mk/jnlp.mk
@@ -761,7 +761,7 @@ jnlp_verify:
 	done;
 
 # Update a location with the files necessary to download
-DIST_BASE = ptolemyII/ptII5.0/jnlp-5.0.1
+DIST_BASE = ptolemyII/ptII5.0/jnlp-5.0
 DIST_DIR = /export/home/pt0/ptweb/$(DIST_BASE)
 DIST_URL = http://ptolemy.eecs.berkeley.edu/$(DIST_BASE)
 OTHER_FILES_TO_BE_DISTED = doc/img/PtolemyIISmall.gif \
