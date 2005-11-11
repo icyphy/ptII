b2d34514d55c9287d7a4d8cd1cc5a5b1e0bf3c92
cxh <cxh>
2005-11-11 21:41:37 +0000

    More tests for PtinyOS Actor and Director

---
+++ ptolemy/domains/ptinyos/kernel/test/PtinyOSTestActor.java
@@ -0,0 +1,83 @@
+/* Dummy Test actor that extends PtinyOSActor, which is abstract.
+ @Copyright (c) 1998-2005 The Regents of the University of California.
+ All rights reserved.
+
+ Permission is hereby granted, without written agreement and without
+ license or royalty fees, to use, copy, modify, and distribute this
+ software and its documentation for any purpose, provided that the
+ above copyright notice and the following two paragraphs appear in all
+ copies of this software.
+
+ IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+ FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+ ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+ THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+ SUCH DAMAGE.
+
+ THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+ INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+ PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+ CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ ENHANCEMENTS, OR MODIFICATIONS.
+
+ PT_COPYRIGHT_VERSION 2
+ COPYRIGHTENDKEY
+
+ */
+package ptolemy.domains.ptinyos.kernel.test;
+
+import ptolemy.actor.TypedAtomicActor;
+import ptolemy.actor.TypedIOPort;
+import ptolemy.data.IntToken;
+import ptolemy.data.expr.Parameter;
+import ptolemy.data.type.BaseType;
+import ptolemy.domains.ptinyos.kernel.PtinyOSActor;
+import ptolemy.kernel.CompositeEntity;
+import ptolemy.kernel.util.IllegalActionException;
+import ptolemy.kernel.util.NameDuplicationException;
+import ptolemy.kernel.util.Workspace;
+
+/**
+ * Dummy Test actor that extends PtinyOSActor, which is abstract.
+ *
+ * @author Christopher Brooks
+ * @version $Id$
+ * @since Ptolemy II 5.1
+ * @Pt.ProposedRating Red
+ * @Pt.AcceptedRating Red
+ */
+public class PtinyOSTestActor extends PtinyOSActor {
+    /** Construct an actor in the default workspace with an empty string
+     *  as its name.  The object is added to the workspace directory.
+     *  Increment the version number of the workspace.
+     */
+    public PtinyOSTestActor() {
+        super();
+    }
+
+    /** Construct an actor in the specified workspace with an empty
+     *  string as a name. You can then change the name with setName().
+     *  If the workspace argument is null, then use the default workspace.
+     *  The object is added to the workspace directory.
+     *  Increment the version number of the workspace.
+     *  @param workspace The workspace that will list the entity.
+     */
+    public PtinyOSTestActor(Workspace workspace) {
+        super(workspace);
+    }
+
+    /** Construct an actor in the specified container with the specified
+     *  name.
+     *  @param container The container.
+     *  @param name The name of this actor within the container.
+     *  @exception IllegalActionException If the actor cannot be contained
+     *   by the proposed container.
+     *  @exception NameDuplicationException If the name coincides with
+     *   an actor already in the container.
+     */
+    public PtinyOSTestActor(CompositeEntity container, String name)
+            throws IllegalActionException, NameDuplicationException {
+        super(container, name);
+    }
+}
