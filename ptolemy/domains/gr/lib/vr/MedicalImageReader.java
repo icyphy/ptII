7c09b4389676babd9163da5348a4b02799a24d3c
tiffcraw <tiffcraw>
2005-12-19 17:01:46 +0000

    *** empty log message ***

--- ptolemy/domains/gr/lib/vr/MedicalImageReader.java
+++ ptolemy/domains/gr/lib/vr/MedicalImageReader.java
@@ -1,75 +1,75 @@
-/* An actor that reads DICOM files.    
+/* An actor that reads DICOM files.
 
-Copyright (c) 1998-2005 The Regents of the University of California.
-All rights reserved.
-Permission is hereby granted, without written agreement and without
-license or royalty fees, to use, copy, modify, and distribute this
-software and its documentation for any purpose, provided that the above
-copyright notice and the following two paragraphs appear in all copies
-of this software.
+ @Copyright (c) 2005 The Regents of the University of California.
+ All rights reserved.
 
-IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
-FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
-ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
-THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
-SUCH DAMAGE.
+ Permission is hereby granted, without written agreement and without
+ license or royalty fees, to use, copy, modify, and distribute this
+ software and its documentation for any purpose, provided that the
+ above copyright notice and the following two paragraphs appear in all
+ copies of this software.
 
-THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
-INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
-MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
-PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
-CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
-ENHANCEMENTS, OR MODIFICATIONS.
+ IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+ FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+ ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+ THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+ SUCH DAMAGE.
 
-PT_COPYRIGHT_VERSION_2
-COPYRIGHTENDKEY
+ THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+ INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+ PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+ CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ ENHANCEMENTS, OR MODIFICATIONS.
 
-*/
+ PT_COPYRIGHT_VERSION_2
+ COPYRIGHTENDKEY
 
-
+ */
 package ptolemy.domains.gr.lib.vr;
 
+import ij.IJ;
+import ij.ImagePlus;
 
 import java.awt.Image;
-import java.lang.String;
 import java.net.URL;
-
-
-import ij.ImagePlus;
-import ij.IJ;
-import ij.plugin.DICOM;
-
-
-
 
 import ptolemy.actor.TypedAtomicActor;
 import ptolemy.actor.TypedIOPort;
 import ptolemy.actor.parameters.FilePortParameter;
+import ptolemy.data.expr.Parameter;
 import ptolemy.actor.parameters.ParameterPort;
 import ptolemy.data.AWTImageToken;
-import ptolemy.data.expr.FileParameter;
-import ptolemy.data.expr.Parameter;
 import ptolemy.data.type.BaseType;
 import ptolemy.kernel.CompositeEntity;
 import ptolemy.kernel.util.IllegalActionException;
 import ptolemy.kernel.util.NameDuplicationException;
 
+import ptolemy.actor.TypedAtomicActor;
+import ptolemy.data.AWTImageToken;
+import ptolemy.data.type.BaseType;
+import ptolemy.data.BooleanToken;
+import ptolemy.data.ObjectToken;
+import ptolemy.actor.parameters.FilePortParameter;
+import ptolemy.actor.parameters.ParameterPort;
 
 //////////////////////////////////////////////////////////////////////////
-////MedicalImageReader
+////DICOMReader
+
 /**
-   An actor that reads image files including .dcm, using NIH ImageJ.
+ An actor that reads DICOM files.
 
-   @author T. Crawford
-   @version
-   @since
-   @Pt.ProposedRating Red
-   @Pt.AcceptedRating Red
+ @see ptolemy.actor.lib.medicalimaging
 
-*/public class MedicalImageReader extends TypedAtomicActor{
+ @author T. Crawford
+ @version $Id$
+ @since Ptolemy II 5.1
+ @Pt.ProposedRating Red
+ @Pt.AcceptedRating Red
+
+ */
+public class MedicalImageReader extends TypedAtomicActor {
     /**Construct an actor with the given container and name.
-      
-     
      * @param container The container
      * @param name The name of this actor
      * @exception IllegalActionException If the actor cannot be contained
@@ -77,123 +77,111 @@ import ptolemy.kernel.util.NameDuplicationException;
      * @exception NameDuplicationException If the container already has an
      *   actor with this name.
      */
-
     public MedicalImageReader(CompositeEntity container, String name)
             throws IllegalActionException, NameDuplicationException {
         super(container, name);
 
         fileOrURL = new FilePortParameter(this, "fileOrURL");
-        fileOrURL.setExpression("null");
 
+        isDicom = new Parameter(this, "isDicom");
+        isDicom.setExpression("false");
+        isDicom.setTypeEquals(BaseType.BOOLEAN);
         
-        StringOrURL = new Parameter(this, "URLOrString");
-        StringOrURL.setTypeEquals(BaseType.BOOLEAN);
-        StringOrURL.setExpression("True");
 
-        output = new TypedIOPort(this, "output");
-        output.setOutput(true);
-        output.setTypeEquals(BaseType.OBJECT);
+        outputImage = new TypedIOPort(this, "outputImage");
+        outputImage.setOutput(true);
+        outputImage.setTypeEquals(BaseType.OBJECT);
+        
+        outputURL = new TypedIOPort(this, "outputURL");
+        outputURL.setOutput(true);
+        outputURL.setTypeEquals(BaseType.OBJECT);
 
     }
 
     ////////////////////////////////////////////////////////////////////
     ////////               ports and parameters                  ////////
-    /** The directory name or URL from which to read.  This is a string with
-     *  any form accepted by FileParameter.
-     *  @see FileParameter
-     */
     public FilePortParameter fileOrURL;
     
-    /** The output port */
+    public Parameter isDicom;
+
+    public TypedIOPort outputImage;
+    public TypedIOPort outputURL;
     public TypedIOPort output;
-    
-    /** A boolean that is true if input is a URL */
-    public Parameter StringOrURL;
+
 
     ////////////////////////////////////////////////////////////////////
     ////////                public methods                     ////////
 
-
-    /** Output the data read in the prefire() method.
+    /** Output the data read in the prefire.
      *  @exception IllegalActionException If there's no director.
      */
     public void fire() throws IllegalActionException {
         super.fire();
-        output.broadcast(new AWTImageToken(_image));
+        if(_debugging){
+            _debug("Image = " + _image);   
+        }
+        outputImage.broadcast(new AWTImageToken(_image));
+        outputURL.broadcast(new ObjectToken(_url));
     }
 
-    /** Access ParameterPort assoicated with FilePortParamter
-     *  @exception IllegalActionException If there's no director.
-     */
-    public void initialize() throws IllegalActionException
-    {
-        _parameterPort =  fileOrURL.getPort();
-        //_parameterPort.setTypeEquals(BaseType.STRING);
+    public void initialize() throws IllegalActionException {
+        _parameterPort = fileOrURL.getPort();
+        _isDicom = ((BooleanToken) isDicom.getToken()).booleanValue();
     }
 
-
-    /** Check to see if data is present at port if so update paramter,
-     *  read in image and return true. If not return false.
-     *  @exception IllegalActionException If there's no director.
-     */
     public boolean prefire() throws IllegalActionException {
         super.prefire();
-        if (_parameterPort.hasToken(0)){
+
+        //FIXME Causes problems when parameter is direclty accessed without port
+
+        if (_parameterPort.hasToken(0)) {
+
             fileOrURL.update();
             _readImage();
             return true;
-        }else {
+        } else {
             return false;
         }
     }
+
     ///////////////////////////////////////////////////////////////////
     ////                         private methods                   ////
-    
-    /** Read the image from the URL or String provided */
-    protected void _readImage() throws IllegalActionException {
-        if (_isURL)
-        {
-        	_url = fileOrURL.asURL();
+    private void _readImage() throws IllegalActionException {
+        _url = fileOrURL.asURL();
 
-        	if (_url == null) {
-        		throw new IllegalActionException("sourceURL was null");
-        	}
-        	_fileRoot = _url.getFile();
-        }else{
-        	_fileRoot = fileOrURL.stringValue();
-        }
-        
-        //if (_imagePlus == null) {
-        if (_imagePlus == null) {
-            _image = ((ImagePlus) IJ.runPlugIn("ij.plugin.DICOM", _fileRoot))
-                    .getImage();
+        if (_url == null) {
+            throw new IllegalActionException("sourceURL was null");
         }
 
-      /*      _imagePlus = new ImagePlus(_fileRoot);
-            _image = _imagePlus.getImage();*/
-            
+        _fileRoot = _url.getFile();
+
+      //  if (_imagePlus == null) {
+        	if(_isDicom){
+                  _image = ((ImagePlus) IJ.runPlugIn("ij.plugin.DICOM", _fileRoot))
+                  .getImage();      
+            }else{
+            	_imagePlus = new ImagePlus(_fileRoot);
+            	_image = _imagePlus.getImage();
+            }
+       // }
     }
-
 
     ///////////////////////////////////////////////////////////////////
     ////                         private members                   ////
 
+    /** Image that is read in. */
+    private ImagePlus _imagePlus;
     
+    private boolean _isDicom;
+
     //  The URL as a string.
     private String _fileRoot;
 
-    // Image that is sent to output port.
+    // Image that is read in.
     private Image _image;
-    
-    //  Image that is read in, in ImageJ format
-    private ImagePlus _imagePlus;
-    
-    //  Boolean identifying input type
-    private boolean _isURL;
-    
-    //  Port associated with the FilePortParameter
-    private ParameterPort _parameterPort;
-    
-    //  The URL of the file.
+
+    // The URL of the file.
     private URL _url;
+
+    private ParameterPort _parameterPort;
 }
