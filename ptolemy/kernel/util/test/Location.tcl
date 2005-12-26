cc4a7f568bfa58a5e10def0e3220677359594cec
cxh <cxh>
2005-12-26 20:28:10 +0000

    Reordered tests to alphabetical, added more tests to increase coverage.

--- ptolemy/kernel/util/test/Location.tcl
+++ ptolemy/kernel/util/test/Location.tcl
@@ -45,7 +45,6 @@ if {[string compare test [info procs test]] == 1} then {
 ####
 #
 
-
 test Location-2.1 {addValueListener} {
     set n [java::new ptolemy.kernel.util.NamedObj "my NamedObj"]
     set s2_1 [java::new ptolemy.kernel.util.Location $n "my Location"]
@@ -99,8 +98,75 @@ test Location-2.1 {addValueListener} {
 (ptolemy.kernel.util.Location, Location = 3.0, 4.0) changed, new expression: 3.0, 4.0
 }}
 
+test Location-2.1 {clone with an empty location} {
+    set n [java::new ptolemy.kernel.util.NamedObj "my NamedObj"]
+    set s1 [java::new ptolemy.kernel.util.Location $n "my Location"]
+    set s2 [$s1 clone]
+    $s2 toString
+} {(ptolemy.kernel.util.Location, Location = {0.0, 0.0})}
 
-test Location-3.1 {getVisibility, setVisibility} {
+test Location-2.2 {clone with a non-empty location} {
+    set n [java::new ptolemy.kernel.util.NamedObj "my NamedObj"]
+    set s1 [java::new ptolemy.kernel.util.Location $n "my Location"]
+    set locations [java::new {double[]} {2} {1.0 2.0}]
+    $s1 setLocation $locations
+
+    set s2 [$s1 clone]
+    $s2 toString
+} {(ptolemy.kernel.util.Location, Location = {1.0, 2.0})}
+
+test Location-3.1 {exportMoML} {
+    set n [java::new ptolemy.kernel.util.NamedObj "my NamedObj3_1"]
+    set s1 [java::new ptolemy.kernel.util.Location $n "my Location3_1"]
+    set result1 [$s1 exportMoML]
+    list $result1	
+} {{<property name="my Location3_1" class="ptolemy.kernel.util.Location" value="{0.0, 0.0}">
+</property>
+}}
+
+test Location-3.2 {exportMoML, expression is the empty string} {
+    set n [java::new ptolemy.kernel.util.NamedObj "my NamedObj3_2"]
+    set s1 [java::new ptolemy.kernel.util.Location $n "my Location3_2"]
+    $s1 setExpression "" 	 
+    set result1 [$s1 exportMoML]
+    list $result1
+} {{<property name="my Location3_2" class="ptolemy.kernel.util.Location">
+</property>
+}}
+
+test Location-4.1 {getDefaultExpression} {
+    set n [java::new ptolemy.kernel.util.NamedObj "my NamedObj4_1"]
+    set s1 [java::new ptolemy.kernel.util.Location $n "my Location4_1"]
+    set result1 [$s1 getDefaultExpression]
+    set result2 [$s1 getExpression]
+    $s1 setExpression "a string 4_1"
+    set result3 [$s1 getDefaultExpression]
+    set result4 [$s1 getExpression]
+
+    list $result1 $result2 $result3 $result4		
+} {{} {{0.0, 0.0}} {a string 4_1} {a string 4_1}}
+
+test Location-5.1 {getDisplayName, getName} {
+    set n [java::new ptolemy.kernel.util.NamedObj "my NamedObj"]
+    set s1 [java::new ptolemy.kernel.util.Location $n "my Location"]
+    set result1 [$s1 getDisplayName]
+    set result2 [$s1 getName]
+    $s1 setName "myName"
+    set result3 [$s1 getDisplayName]
+    set result4 [$s1 getName]
+    list $result1 $result2 $result3 $result4
+} {{my Location} {my Location} myName myName}
+
+test Location-7.1 {getExpression} {
+    set n [java::new ptolemy.kernel.util.NamedObj "my NamedObj"]
+    set s1 [java::new ptolemy.kernel.util.Location $n "my Location"]
+    # Increase code coverage
+    $s1 setExpression ""
+    set result1 [$s1 getExpression]
+    list $result1
+} {{}}
+
+test Location-6.1 {getVisibility, setVisibility} {
     set n [java::new ptolemy.kernel.util.NamedObj "my NamedObj"]
     set s1 [java::new ptolemy.kernel.util.Location $n "my Location"]
     set full [java::field ptolemy.kernel.util.Settable NONE]
@@ -112,25 +178,7 @@ test Location-3.1 {getVisibility, setVisibility} {
     list $r1 [expr {[$s1 getVisibility] == $none}]
 } {1 1}
 
-
-test Location-4.1 {clone with an empty location} {
-    set n [java::new ptolemy.kernel.util.NamedObj "my NamedObj"]
-    set s1 [java::new ptolemy.kernel.util.Location $n "my Location"]
-    set s2 [$s1 clone]
-    $s2 toString
-} {(ptolemy.kernel.util.Location, Location = {0.0, 0.0})}
-
-test Location-4.2 {clone with a non-empty location} {
-    set n [java::new ptolemy.kernel.util.NamedObj "my NamedObj"]
-    set s1 [java::new ptolemy.kernel.util.Location $n "my Location"]
-    set locations [java::new {double[]} {2} {1.0 2.0}]
-    $s1 setLocation $locations
-
-    set s2 [$s1 clone]
-    $s2 toString
-} {(ptolemy.kernel.util.Location, Location = {1.0, 2.0})}
-
-test Location-5.1 {three dimensional location} {
+test Location-7.1 {setLocation: three dimensional location} {
     set n [java::new ptolemy.kernel.util.NamedObj "my NamedObj"]
     set s1 [java::new ptolemy.kernel.util.Location $n "my Location"]
     set locations [java::new {double[]} {3} [list -1.0 2.0 3.0]]
@@ -141,3 +189,34 @@ test Location-5.1 {three dimensional location} {
     set s2 [$s1 clone]
     $s2 toString
 } {(ptolemy.kernel.util.Location, Location = {-1.0, 2.0, Infinity})}
+
+test Location-7.2 {setLocation on a Location with no container} {
+    set w [java::new ptolemy.kernel.util.Workspace "my Workspace"]
+    set s1 [java::new ptolemy.kernel.util.Location $w]
+    set locations [java::new {double[]} {2} [list -7.2 7.2]]
+    $s1 setLocation $locations
+    $s1 toString
+} {(ptolemy.kernel.util.Location, Location = {-7.2, 7.2})}
+
+test Location-10.1 {validate} {
+    set n [java::new ptolemy.kernel.util.NamedObj "my NamedObj"]
+    set s1 [java::new ptolemy.kernel.util.Location $n "my Location"]
+    $s1 setExpression "{-10.1,10.1}"
+    set result1 [$s1 getExpression] 
+    # Need to call validate to get the new expression	
+    set result2 [[$s1 getLocation] getrange]
+    $s1 validate
+    set result3 [$s1 getExpression] 
+    set result4 [[$s1 getLocation] getrange]
+    $s1 setExpression [java::null]
+    set result5 [$s1 getExpression] 
+
+    # Note that the old location is still set to -10.1, 10.1
+    set result6 [[$s1 getLocation] getrange]
+
+    # Calling validate changes the location to 0.0, 0.0
+    $s1 validate
+    set result7 [$s1 getExpression] 
+    set result8 [[$s1 getLocation] getrange]
+    list $result1 $result2 $result3 $result4 $result5 $result6 $result7 $result8
+} {{{-10.1,10.1}} {0.0 0.0} {{-10.1,10.1}} {-10.1 10.1} {} {-10.1 10.1} {} {0.0 0.0}}
