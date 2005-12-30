c3113afbd9ca797abda185f3baacb0f27036d848
cxh <cxh>
2005-12-30 19:37:05 +0000

    Tests for move* methods

--- ptolemy/kernel/test/ComponentRelation.tcl
+++ ptolemy/kernel/test/ComponentRelation.tcl
@@ -75,6 +75,109 @@ test ComponentRelation-3.1 {Test for NameDuplicationException in constructor} {
     catch {[java::new ptolemy.kernel.ComponentRelation $a B]} msg
     list $msg
 } {{ptolemy.kernel.util.NameDuplicationException: Attempt to insert object named "B" into container named ".A", which already contains an object with that name.}}
+
+######################################################################
+####
+#
+test ComponentRelation-5.5 {move* methods with no container} {
+    set n [java::new ptolemy.kernel.ComponentRelation]
+    catch {$n moveDown} msg1
+    catch {$n moveToFirst} msg2
+    catch {$n moveToIndex 0} msg3
+    catch {$n moveToLast} msg4
+    catch {$n moveUp} msg5
+    list $msg1 $msg2 $msg3 $msg4 $msg5
+} {{ptolemy.kernel.util.IllegalActionException: Has no container.
+  in .<Unnamed Object>} {ptolemy.kernel.util.IllegalActionException: Has no container.
+  in .<Unnamed Object>} {ptolemy.kernel.util.IllegalActionException: Has no container.
+  in .<Unnamed Object>} {ptolemy.kernel.util.IllegalActionException: Has no container.
+  in .<Unnamed Object>} {ptolemy.kernel.util.IllegalActionException: Has no container.
+  in .<Unnamed Object>}}
+
+test ComponentRelation-5.5.1 {moveDown} {
+    set top [java::new ptolemy.kernel.CompositeEntity]
+    set a1  [java::new ptolemy.kernel.ComponentRelation $top a1]
+    set a2  [java::new ptolemy.kernel.ComponentRelation $top a2]
+    set a3  [java::new ptolemy.kernel.ComponentRelation $top a3]
+    set result1 [listToNames [$top relationList]]
+    $a1 moveDown
+    set result2 [listToNames [$top relationList]]
+    $a1 moveDown
+    # Can't go past the bottom
+    $a1 moveDown
+    set result3 [listToNames [$top relationList]]
+    list $result1 $result2 $result3
+} {{a1 a2 a3} {a2 a1 a3} {a2 a3 a1}}
+
+test ComponentRelation-5.5.2 {moveToFirst} {
+    set top [java::new ptolemy.kernel.CompositeEntity]
+    set a1  [java::new ptolemy.kernel.ComponentRelation $top a1]
+    set a2  [java::new ptolemy.kernel.ComponentRelation $top a2]
+    set a3  [java::new ptolemy.kernel.ComponentRelation $top a3]
+    set result1 [listToNames [$top relationList]]
+    $a1 moveToFirst
+    set result2 [listToNames [$top relationList]]
+    $a2 moveToFirst
+    set result3 [listToNames [$top relationList]]
+    $a3 moveToFirst
+    set result4 [listToNames [$top relationList]]
+    $a3 moveToFirst
+    set result5 [listToNames [$top relationList]]	
+    list $result1 $result2 $result3 $result4 $result5
+} {{a1 a2 a3} {a1 a2 a3} {a2 a1 a3} {a3 a2 a1} {a3 a2 a1}}
+
+test ComponentRelation-5.5.3 {moveToIndex} {
+    set top [java::new ptolemy.kernel.CompositeEntity]
+    set a1  [java::new ptolemy.kernel.ComponentRelation $top a1]
+    set a2  [java::new ptolemy.kernel.ComponentRelation $top a2]
+    set a3  [java::new ptolemy.kernel.ComponentRelation $top a3]
+    set result1 [listToNames [$top relationList]]
+    catch {$a1 moveToIndex -1} msg
+    set result2 $msg
+    $a2 moveToIndex 0
+    set result3 [listToNames [$top relationList]]
+    $a3 moveToIndex 1
+    set result4 [listToNames [$top relationList]]
+    $a3 moveToIndex 2
+    set result5 [listToNames [$top relationList]]	
+    catch {$a3 moveToIndex 3} result6
+    list $result1 $result2 $result3 $result4 $result5 $result6
+} {{a1 a2 a3} {ptolemy.kernel.util.IllegalActionException: Index out of range.
+  in .<Unnamed Object>.a1} {a2 a1 a3} {a2 a3 a1} {a2 a1 a3} {ptolemy.kernel.util.IllegalActionException: Index out of range.
+  in .<Unnamed Object>.a3}}
+
+test ComponentRelation-5.5.4 {moveToLast} {
+    set top [java::new ptolemy.kernel.CompositeEntity]
+    set a1  [java::new ptolemy.kernel.ComponentRelation $top a1]
+    set a2  [java::new ptolemy.kernel.ComponentRelation $top a2]
+    set a3  [java::new ptolemy.kernel.ComponentRelation $top a3]
+    set result1 [listToNames [$top relationList]]
+    $a1 moveToLast
+    set result2 [listToNames [$top relationList]]
+    $a2 moveToLast
+    set result3 [listToNames [$top relationList]]
+    $a3 moveToLast
+    set result4 [listToNames [$top relationList]]
+    $a3 moveToLast
+    set result5 [listToNames [$top relationList]]	
+    list $result1 $result2 $result3 $result4 $result5
+} {{a1 a2 a3} {a2 a3 a1} {a3 a1 a2} {a1 a2 a3} {a1 a2 a3}}
+
+test ComponentRelation-5.5.5 {moveUp} {
+    set top [java::new ptolemy.kernel.CompositeEntity]
+    set a1  [java::new ptolemy.kernel.ComponentRelation $top a1]
+    set a2  [java::new ptolemy.kernel.ComponentRelation $top a2]
+    set a3  [java::new ptolemy.kernel.ComponentRelation $top a3]
+    set result1 [listToNames [$top relationList]]
+    $a3 moveUp
+    set result2 [listToNames [$top relationList]]
+    $a1 moveUp
+    # Can't go past the top
+    $a1 moveUp
+    set result3 [listToNames [$top relationList]]
+    list $result1 $result2 $result3
+} {{a1 a2 a3} {a1 a3 a2} {a1 a3 a2}}
+
 ######################################################################
 ####
 #
