766ffabc1072b890a062c1445cfca36afaac7104
cxh <cxh>
2006-01-03 16:32:29 +0000

    -d option works

--- doc/doclets/PtDoclet.java
+++ doc/doclets/PtDoclet.java
@@ -1,14 +1,66 @@
+/* Javadoc Doclet that generates PtDoc XML
+
+ Copyright (c) 2006 The Regents of the University of California.
+ All rights reserved.
+ Permission is hereby granted, without written agreement and without
+ license or royalty fees, to use, copy, modify, and distribute this
+ software and its documentation for any purpose, provided that the above
+ copyright notice and the following two paragraphs appear in all copies
+ of this software.
+
+ IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+ FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+ ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+ THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+ SUCH DAMAGE.
+
+ THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+ INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+ PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+ CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ ENHANCEMENTS, OR MODIFICATIONS.
+
+ PT_COPYRIGHT_VERSION_2
+ COPYRIGHTENDKEY
+
+ */
+
 package doc.doclets;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import com.sun.javadoc.*;
 
-/** Generate PtDoc output
+/** Generate PtDoc output.
+ *  @author Christopher Brooks, Edward A. Lee
+ *  @version $Id$
+ *  @since Ptolemy II 5.2  
  */
 public class PtDoclet {
-    public static boolean start(RootDoc root) {
-        StringBuffer results = new StringBuffer(_header);
+    /** Given a command line option, return the number of command line
+     *  arguments needed by that option.
+     *  @param option The command line option
+     *  @return If the option is "-d", return 2; otherwise, return 0.
+     */
+    public static int optionLength(String option) {
+        if (option.equals("-d")) {
+	    return 2;
+        }
+        return 0;
+    }
+
+    /** Process the java files and generate PtDoc XML.
+     *  @param root The root of the java doc tree.
+     *  @return True
+     *  @exception IOException If there is a problem writing the documentation.
+     */
+    public static boolean start(RootDoc root) throws IOException {
+        String outputDirectory = _getOutputDirectory(root.options());
         ClassDoc[] classes = root.classes();
         for (int i = 0; i < classes.length; i++) {
+            StringBuffer documentation = new StringBuffer(_header);
             String className = classes[i].toString();
             String shortClassName = null;
             if (className.lastIndexOf(".") == -1) {
@@ -17,7 +69,7 @@ public class PtDoclet {
                 shortClassName = 
                     className.substring(className.lastIndexOf(".") + 1);
             }
-            results.append("<doc name=\"" + shortClassName + " class=\""
+            documentation.append("<doc name=\"" + shortClassName + " class=\""
                     + className + "\"\n"
                     + "<description>\n"
                     // FIXME: need to escape commentText
@@ -27,13 +79,65 @@ public class PtDoclet {
             // Handle other class tags
             String [] classTags = {"author", "version", "since", "Pt.ProposedRating", "Pt.AcceptedRating"};
             for (int j = 0; j< classTags.length; j++) {
-                results.append("<" + classTags[j] + ">" 
-                        + classes[i].tags(classTags[j]) + ">\n");
+                Tag [] tags = classes[i].tags(classTags[j]);
+                if (tags.length > 0) {
+                    documentation.append("<" + classTags[j] + ">" 
+                            + tags[0].text()
+                            + "</" + classTags[j] + ">\n");
+                }
+            }
+            _writeDoc(className, outputDirectory, documentation.toString());
+        }
+        return true;
+    }
+
+    /** Process the doclet command line arguments and return the value
+     *  of the -d parameter, if any.
+     *  @param options The command line options
+     *  @return the value of the -d parameter, if any, otherwise return null.
+     */
+    private static String _getOutputDirectory(String [][] options) {
+        for (int i = 0; i < options.length; i++) {
+            String[] option = options[i];
+            if (option[0].equals("-d")) {
+                return option[1];
             }
         }
-        // FIXME: print to a file, use the -d option for directory
-        System.out.println(results.toString());
-        return true;
+        return null;
+    }
+
+    
+    /** Write the output to a file.
+     *  @param className The dot separated fully qualified classname,
+     *  which is used to specify the directory and filename to which
+     *  the documentation is written.
+     *  @param directory The top level directory where the classes are written
+     *  @param documentation The documentation that is written.
+     *  @exception IOException If there is a problem writing the documentation.
+     */
+    private static void _writeDoc(String className, String directory,
+            String documentation) throws IOException {
+        String fileBaseName = className.replace('.', '/') + ".xml";
+        String fileName = null;
+        if (directory != null) {
+            fileName = directory + File.separator + fileBaseName;
+        } else {
+            fileName = fileBaseName;
+        }
+        // If necessary, create the directory.
+        File directoryFile = new File(fileName).getParentFile();
+        if (!directoryFile.exists()) {
+            directoryFile.mkdirs();
+        }
+        System.out.println("Writing " + documentation.length() + " chars to "
+                + System.getProperty("user.dir") + File.separator + fileName);
+
+        FileWriter writer = new FileWriter(fileName);
+        try {
+            writer.write(documentation);
+        } finally {
+            writer.close();
+        }
     }
 
     /** Header string for XML PtDoc output. */
