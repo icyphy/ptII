00c6a1f945117b395d3ce31e38c31cd6cc5b2f4d
cxh <cxh>
2006-02-09 07:19:45 +0000

    Added Welcome Window

---
+++ ptolemy/actor/gui/WelcomeWindow.java
@@ -0,0 +1,90 @@
+/* A viewer for Welcome Windows
+
+ Copyright (c) 2006 The Regents of the University of California.
+ All rights reserved.
+ Permission is hereby granted, without written agreement and without
+ license or royalty fees, to use, copy, modify, and distribute this
+ software and its documentation for any purpose, provided that the above
+ copyright notice and the following two paragraphs appear in all copies
+ of this software.
+
+ IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+ FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+ ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+ THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+ SUCH DAMAGE.
+
+ THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+ INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+ PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+ CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ ENHANCEMENTS, OR MODIFICATIONS.
+
+ PT_COPYRIGHT_VERSION_2
+ COPYRIGHTENDKEY
+
+ */
+package ptolemy.actor.gui;
+
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.geom.AffineTransform;
+import java.awt.print.PageFormat;
+import java.awt.print.Printable;
+import java.awt.print.PrinterException;
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.swing.BoxLayout;
+import javax.swing.JEditorPane;
+import javax.swing.JScrollPane;
+import javax.swing.event.HyperlinkEvent;
+import javax.swing.event.HyperlinkListener;
+import javax.swing.text.html.HTMLDocument;
+import javax.swing.text.html.HTMLFrameHyperlinkEvent;
+
+import ptolemy.gui.Top;
+import ptolemy.util.MessageHandler;
+
+//////////////////////////////////////////////////////////////////////////
+//// WelcomeWindow
+
+/**
+ A toplevel frame that can view HTML documents, but has no menus. 
+
+ @author Christopher Brooks
+ @version $Id$
+ @since Ptolemy II 6.0
+ @Pt.ProposedRating Red (cxh)
+ @Pt.AcceptedRating Red (cxh)
+ */
+public class WelcomeWindow extends HTMLViewer {
+    /** Construct a blank HTML Viewer with no menu items.
+     */
+    public WelcomeWindow() {
+        super(); 
+        _fileMenuItems = null;
+    }
+
+    ///////////////////////////////////////////////////////////////////
+    ////                         public methods                    ////
+
+    /** Center this window.  Do not add menus.
+     */
+    public void pack() {
+        Runnable doPack = new Runnable() {
+            public void run() {     
+                if (getCentering()) {
+                    centerOnScreen();
+                }
+            }
+        };
+
+        deferIfNecessary(doPack);
+    }
+}
+
---
+++ ptolemy/actor/gui/WelcomeWindowTableau.java
@@ -0,0 +1,77 @@
+/* A tableau representing an HTML Welcome Window (no menu, has don't show again)
+ Copyright (c) 2006 The Regents of the University of California.
+ All rights reserved.
+ Permission is hereby granted, without written agreement and without
+ license or royalty fees, to use, copy, modify, and distribute this
+ software and its documentation for any purpose, provided that the above
+ copyright notice and the following two paragraphs appear in all copies
+ of this software.
+
+ IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+ FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+ ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+ THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+ SUCH DAMAGE.
+
+ THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+ INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+ PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+ CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ ENHANCEMENTS, OR MODIFICATIONS.
+
+ PT_COPYRIGHT_VERSION_2
+ COPYRIGHTENDKEY
+ */
+package ptolemy.actor.gui;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+
+import ptolemy.kernel.util.Attribute;
+import ptolemy.kernel.util.IllegalActionException;
+import ptolemy.kernel.util.NameDuplicationException;
+import ptolemy.kernel.util.NamedObj;
+import ptolemy.kernel.util.Settable;
+import ptolemy.kernel.util.StringAttribute;
+import ptolemy.util.ClassUtilities;
+import ptolemy.util.StringUtilities;
+
+//////////////////////////////////////////////////////////////////////////
+//// WelcomeWindowTableau
+
+/**
+ A tableau representing a rendered HTML view in a toplevel window that
+ has no menu choices.
+
+ @author  Christopher Brooks
+ @version $Id$
+ @since Ptolemy II 6.0
+ @Pt.ProposedRating Red (cxh)
+ @Pt.AcceptedRating Red (cxh)
+ @see Effigy
+ @see WelcomeWindow
+ */
+public class WelcomeWindowTableau extends HTMLViewerTableau {
+
+    /** Construct a new tableau for the model represented by the given effigy.
+     *  This creates an instance of WelcomeWindow.  It does not make the frame
+     *  visible.  To do that, call show().
+     *  @param container The container.
+     *  @param name The name.
+     *  @exception IllegalActionException If the container does not accept
+     *   this entity (this should not occur).
+     *  @exception NameDuplicationException If the name coincides with an
+     *   attribute already in the container.
+     */
+    public WelcomeWindowTableau(Effigy container, String name)
+            throws IllegalActionException, NameDuplicationException {
+        super(container, name);
+        WelcomeWindow frame = new WelcomeWindow();
+        setFrame(frame);
+        frame.setTableau(this);
+    }
+}
