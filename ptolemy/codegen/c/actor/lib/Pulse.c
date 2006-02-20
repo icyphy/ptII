533cd631c43687aa2e834121f1e88f9a535814fa
zgang <zgang>
2006-02-20 01:45:51 +0000

    new helper

---
+++ ptolemy/codegen/c/actor/lib/Pulse.c
@@ -0,0 +1,31 @@
+/***preinitBlock***/
+    int $actorSymbol(iterationCount) = 0;
+    int $actorSymbol(indexColCount) = 0;
+    unsigned char $actorSymbol(match) = 0;
+/**/
+
+/***fireBlock***/
+    if ($actorSymbol(indexColCount) < $size(indexes) 
+            && $actorSymbol(iterationCount) == $ref(indexes, $actorSymbol(indexColCount))) {
+        $ref(output) = $ref(values, $actorSymbol(indexColCount));
+        $actorSymbol(match) = 1;
+    } else {
+        $ref(output) = 0;             
+    }
+    
+    if ($actorSymbol(iterationCount) <= $ref(indexes, $size(indexes) - 1)) {
+        $actorSymbol(iterationCount) ++;
+    }
+    
+    if ($actorSymbol(match)) {
+        $actorSymbol(indexColCount) ++;
+        $actorSymbol(match) = 0;
+    }
+    
+    if ($actorSymbol(indexColCount) >= $size(indexes) && $val(repeat)) {
+    	$actorSymbol(iterationCount) = 0;
+        $actorSymbol(indexColCount) = 0;
+    }	
+/**/
+
+
