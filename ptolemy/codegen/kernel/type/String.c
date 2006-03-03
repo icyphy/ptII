2ddd9e16bda718e1c01d05a50ff152c9bfa823c8
cxh <cxh>
2006-03-03 23:27:50 +0000

    String_new(): Allocate space for the string

--- ptolemy/codegen/kernel/type/String.c
+++ ptolemy/codegen/kernel/type/String.c
@@ -9,10 +9,10 @@ Token String_print(Token thisToken);
 
 /***newBlock***/
 // make a new integer token from the given value.
-Token String_new(char* s) {
-    Token result;
-    result.type = TYPE_String;
-    result.payload.String = s;
+Token *String_new(char* s) {
+    Token *result = (Token *) malloc(sizeof(Token));
+    result->type = TYPE_String;
+    result->payload.String = s;
     return result;
 }
 /**/
@@ -21,6 +21,7 @@ Token String_new(char* s) {
 /***deleteBlock***/
 Token String_delete(Token token) {   
     free(token.payload.String);    
+    free(&token);
 }    
 /**/
 
