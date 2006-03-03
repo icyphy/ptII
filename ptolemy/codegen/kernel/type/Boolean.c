e00c6ec020befa1d0d0039b078ddb5de39ad477c
cxh <cxh>
2006-03-03 23:33:32 +0000

    Boolean_new(): allocate space

--- ptolemy/codegen/kernel/type/Boolean.c
+++ ptolemy/codegen/kernel/type/Boolean.c
@@ -9,8 +9,8 @@ Token Boolean_print(Token thisToken);
 
 /***newBlock***/
 // make a new integer token from the given value.
-Token Boolean_new(char b) {
-    Token result;
+Token *Boolean_new(char b) {
+    Token *result = (Token *) malloc(sizeof(Token));
     result.type = TYPE_Boolean;
     result.payload.Boolean = b;
     return result;
@@ -20,6 +20,7 @@ Token Boolean_new(char b) {
 
 /***deleteBlock***/
 Token Boolean_delete(Token token) {   
+    free(&token);
 }    
 /**/
 
