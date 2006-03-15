ee8c791ff4dd2563a3a1443db34d88df06de57d5
mankit <mankit>
2006-03-15 22:18:35 +0000

    Added $ prefix to arguments for codeblock

---
+++ ptolemy/codegen/c/actor/lib/string/StringIndexOf.c
@@ -0,0 +1,19 @@
+/*** preinitBlock ***/
+	int i, j;
+/**/
+
+/*** matchCaseFireBlock ($step)***/
+	$ref(output) = -1;
+	for (i = $ref(startIndex); i < strlen($ref(inText)); i+= $step) {
+		for (j = 0; j < strlen($ref(searchFor)); j++) {
+			
+		}
+	}
+/**/
+
+/*** ignoreCaseFireBlock ($step)***/
+	for (i = $ref(startIndex); i < strlen($ref(inText)); i+= $step) {
+	
+	}
+	$ref(searchFor)	
+/**/
---
+++ ptolemy/codegen/c/actor/lib/string/StringIndexOf.java
@@ -0,0 +1,145 @@
+/* A code generation helper class for actor.lib.string.StringIndexOf
+ @Copyright (c) 2005 The Regents of the University of California.
+ All rights reserved.
+ Permission is hereby granted, without written agreement and without
+ license or royalty fees, to use, copy, modify, and distribute this
+ software and its documentation for any purpose, provided that the
+ above copyright notice and the following two paragraphs appear in all
+ copies of this software.
+
+ IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+ FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+ ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+ THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+ SUCH DAMAGE.
+
+ THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES, 
+ INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+ PROVIDED HEREUNDER IS ON AN \"AS IS\" BASIS, AND THE UNIVERSITY OF
+ CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES, 
+ ENHANCEMENTS, OR MODIFICATIONS.
+
+ PT_COPYRIGHT_VERSION_2
+ COPYRIGHTENDKEY
+
+ */
+package ptolemy.codegen.c.actor.lib.string;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import ptolemy.codegen.kernel.CCodeGeneratorHelper;
+import ptolemy.kernel.util.IllegalActionException;
+
+/**
+ A code generation helper class for ptolemy.actor.lib.string.StringIndexOf. 
+
+ @author Man-Kit Leung
+ @version $Id$
+ @since Ptolemy II 5.1
+ @Pt.ProposedRating Red (mankit) 
+ @Pt.AcceptedRating Red (mankit)
+ */
+public class StringIndexOf extends CCodeGeneratorHelper {
+
+    /**
+     * Constructor method for the StringIndexOf helper.
+     * @param actor The associated actor.
+     */
+    public StringIndexOf(ptolemy.actor.lib.string.StringIndexOf actor) {
+        super(actor);
+    }
+
+    /**
+     * Generate fire code.
+     * Read the <code>fireBlock</code> from StringIndexOf.c,
+     * replace macros with their values and append the processed code              
+     * block to the given code buffer.
+     * @return The processed code string.
+     * @exception IllegalActionException If the code stream encounters an
+     *  error in processing the specified code block(s).
+     */
+    public String  generateFireCode() throws IllegalActionException {
+        StringBuffer code = new StringBuffer();
+        code.append(super.generateFireCode());
+        code.append(_generateBlockCode("fireBlock"));
+        return code.toString();
+   }
+
+    /**
+     * Generate initialize code.
+     * Read the  from StringIndexOf.c,
+     * replace macros with their values and return the processed code string.
+     * @return The processed code string.
+     * @exception IllegalActionException If the code stream encounters an
+     *  error in processing the specified code block(s).
+     */
+    public String generateInitializeCode() throws IllegalActionException {
+        StringBuffer code = new StringBuffer();
+        code.append(super.generateInitializeCode());
+
+        return code.toString();
+    }
+   
+    /**
+     * Generate preinitialize code.
+     * Reads the <code>preinitBlock</code> from StringIndexOf.c,
+     * replace macros with their values and return the processed code string.
+     * @return The processed code string.
+     * @exception IllegalActionException If the code stream encounters an
+     *  error in processing the specified code block(s).
+     */
+    public String generatePreinitializeCode() throws IllegalActionException {
+        StringBuffer code = new StringBuffer();
+        code.append(super.generatePreinitializeCode());
+        code.append(_generateBlockCode("preinitBlock"));
+        return code.toString();
+    }
+
+    /**
+     * Generate shared code.
+     * Read the  from StringIndexOf.c,
+     * replace macros with their values and return the processed code string.
+     * @return The processed code string.
+     * @exception IllegalActionException If the code stream encounters an
+     *  error in processing the specified code block(s).
+     */
+    public Set generateSharedCode() throws IllegalActionException {
+        Set sharedCode = new HashSet();
+        sharedCode.addAll(super.getHeaderFiles());
+
+        return sharedCode;
+    }
+
+    /**
+     * Generate wrap up code.
+     * Read the  from StringIndexOf.c, 
+     * replace macros with their values and append the processed code block
+     * to the given code buffer.
+     * @return The processed code string.
+     * @exception IllegalActionException If the code stream encounters an
+     *  error in processing the specified code block(s).
+     */
+    public String generateWrapupCode() throws IllegalActionException {
+        StringBuffer code = new StringBuffer();
+        code.append(super.generateWrapupCode());
+        
+        return code.toString();
+    }
+
+    /**
+     * Get the files needed by the code generated for the
+     * StringIndexOf actor.
+     * @return A set of Strings that are names of the header files
+     *  needed by the code generated for the StringIndexOf actor.
+     * @exception IllegalActionException Not Thrown in this subclass.
+     */
+    public Set getHeaderFiles() throws IllegalActionException {
+        Set files = new HashSet();
+        files.addAll(super.getHeaderFiles());
+        files.add("<string.h>");
+
+        return files;
+    }
+}
