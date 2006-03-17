d213736072127727fddee2d2f5cd90fe5d1780bc
cxh <cxh>
2006-03-17 06:07:49 +0000

    Note that if the width is 0, then we have no connection to the port but the port might be a PortParameter, in which case we want the Parameter.  codegen/c/actor/lib/string/test/auto/StringCompare3.xml tests this.

--- ptolemy/codegen/kernel/CodeGeneratorHelper.java
+++ ptolemy/codegen/kernel/CodeGeneratorHelper.java
@@ -827,7 +827,13 @@ public class CodeGeneratorHelper implements ActorCodeGenerator {
                 return result.toString(); //
             }
 
-            if ((port.isInput() && !forComposite)
+            // Note that if the width is 0, then we have no connection to
+            // the port but the port might be a PortParameter, in which
+            // case we want the Parameter.
+            // codegen/c/actor/lib/string/test/auto/StringCompare3.xml
+            // tests this.
+
+            if ((port.isInput() && !forComposite && port.getWidth() > 0)
                     || (port.isOutput() && forComposite)) {
                 result.append(generateName(port));
 
