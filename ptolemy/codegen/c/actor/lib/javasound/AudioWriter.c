d0d2b4f3cd9e80607725ef8e169cca171dcbe906
cxh <cxh>
2006-03-17 14:45:54 +0000

    Print the name of the file

--- ptolemy/codegen/c/actor/lib/javasound/AudioWriter.c
+++ ptolemy/codegen/c/actor/lib/javasound/AudioWriter.c
@@ -4,7 +4,7 @@
 
 /*** initBlock ***/    
     if (!($actorSymbol(filePtr) = fopen ($ref(fileOrURL),"w"))) {
-        fprintf(stderr,"ERROR: cannot open file for AudioReader actor.\n");
+        fprintf(stderr,"ERROR: cannot open file \"%s\" for AudioWriter actor.\n", $ref(fileOrURL));
         exit(1);
     }
 /**/
