3630fa6ee7a619fbbef08a8746ffd6d6f77ad5dc
cxh <cxh>
2006-03-18 00:20:05 +0000

    Added more tests

--- ptolemy/codegen/c/kernel/test/CParseTreeCodeGenerator.tcl
+++ ptolemy/codegen/c/kernel/test/CParseTreeCodeGenerator.tcl
@@ -76,6 +76,136 @@ test Expression-2.1 {Define a variable in a regular parse tree } {
 
 } {{"1+42"}}
 
+
+proc parseTreeTraceTest {expression} {
+    set ptParser [java::new ptolemy.data.expr.PtParser]
+    set parseTreeCodeGenerator \
+    [java::new ptolemy.codegen.c.kernel.CParseTreeCodeGenerator]
+
+    set parseTree [$ptParser generateParseTree $expression ]
+    set tree [$parseTreeCodeGenerator traceParseTreeEvaluation \
+		$parseTree [java::null]]
+    return [list \
+		$tree \
+		[$parseTreeCodeGenerator generateFireCode]]
+}
+
+test Expression-10.1 {traceParseTreeEvaluation} {
+    parseTreeTraceTest {10+1}
+} {{Entering node ptolemy.data.expr.ASTPtSumNode
+  Entering node ptolemy.data.expr.ASTPtLeafNode
+  Node ptolemy.data.expr.ASTPtLeafNode evaluated to 10
+  Entering node ptolemy.data.expr.ASTPtLeafNode
+  Node ptolemy.data.expr.ASTPtLeafNode evaluated to 1
+Node ptolemy.data.expr.ASTPtSumNode evaluated to 10
+} (10+1)}
+
+test Expression-11.1 {visitFunctionDefinitionNode} {
+    parseTreeTraceTest {function(x:double) x*5.0}
+} {{Entering node ptolemy.data.expr.ASTPtFunctionDefinitionNode
+Node ptolemy.data.expr.ASTPtFunctionDefinitionNode evaluated to (function(x:double) (x*5.0))
+} {}}
+
+test Expression-12.1 {visitFunctionalIfNode} {
+    parseTreeTraceTest { 1==0 ? 0.5: 1.5 }
+} {{Entering node ptolemy.data.expr.ASTPtFunctionalIfNode
+  Entering node ptolemy.data.expr.ASTPtRelationalNode
+    Entering node ptolemy.data.expr.ASTPtLeafNode
+    Node ptolemy.data.expr.ASTPtLeafNode evaluated to 1
+    Entering node ptolemy.data.expr.ASTPtLeafNode
+    Node ptolemy.data.expr.ASTPtLeafNode evaluated to 0
+  Node ptolemy.data.expr.ASTPtRelationalNode evaluated to null
+  java.lang.NullPointerException
+} {(1 == 0)}} {Why is this null}
+
+
+test Expression-13.1 {visitMethodCallNode} {
+    parseTreeTraceTest {[1, 2; 3, 4; 5, 6].getRowCount()}
+} {{Entering node ptolemy.data.expr.ASTPtMethodCallNode
+  Entering node ptolemy.data.expr.ASTPtMatrixConstructNode
+    Entering node ptolemy.data.expr.ASTPtLeafNode
+    Node ptolemy.data.expr.ASTPtLeafNode evaluated to 1
+    Entering node ptolemy.data.expr.ASTPtLeafNode
+    Node ptolemy.data.expr.ASTPtLeafNode evaluated to 2
+    Entering node ptolemy.data.expr.ASTPtLeafNode
+    Node ptolemy.data.expr.ASTPtLeafNode evaluated to 3
+    Entering node ptolemy.data.expr.ASTPtLeafNode
+    Node ptolemy.data.expr.ASTPtLeafNode evaluated to 4
+    Entering node ptolemy.data.expr.ASTPtLeafNode
+    Node ptolemy.data.expr.ASTPtLeafNode evaluated to 5
+    Entering node ptolemy.data.expr.ASTPtLeafNode
+    Node ptolemy.data.expr.ASTPtLeafNode evaluated to 6
+  Node ptolemy.data.expr.ASTPtMatrixConstructNode evaluated to [1, 2; 3, 4; 5, 6]
+  Invoking public abstract int ptolemy.data.MatrixToken.getRowCount()
+  as [int].getRowCount()
+Node ptolemy.data.expr.ASTPtMethodCallNode evaluated to 3
+} {((Token*) $new(Matrix(3, 2, $new(Int(1)), TYPE_Int, $new(Int(2)), TYPE_Int, $new(Int(3)), TYPE_Int, $new(Int(4)), TYPE_Int, $new(Int(5)), TYPE_Int, $new(Int(6)), TYPE_Int)))->getRowCount()}}
+
+test Expression-14.1 {visitPowerNode} {
+    parseTreeTraceTest {2^3}
+} {{Entering node ptolemy.data.expr.ASTPtPowerNode
+  Entering node ptolemy.data.expr.ASTPtLeafNode
+  Node ptolemy.data.expr.ASTPtLeafNode evaluated to 2
+  Entering node ptolemy.data.expr.ASTPtLeafNode
+  Node ptolemy.data.expr.ASTPtLeafNode evaluated to 3
+Node ptolemy.data.expr.ASTPtPowerNode evaluated to 2
+} {(pow(2, 3))}}
+
+
+test Expression-15.1 {visitShiftNode} {
+    parseTreeTraceTest {16<<1} 
+} {{Entering node ptolemy.data.expr.ASTPtShiftNode
+  Entering node ptolemy.data.expr.ASTPtLeafNode
+  Node ptolemy.data.expr.ASTPtLeafNode evaluated to 16
+  Entering node ptolemy.data.expr.ASTPtLeafNode
+  Node ptolemy.data.expr.ASTPtLeafNode evaluated to 1
+Node ptolemy.data.expr.ASTPtShiftNode evaluated to 32
+} {(16 << 1)}}
+
+test Expression-16.1 {visitFunctionApplicationNode} {
+    parseTreeTraceTest {(function(x:double) x*5.0) (10.0)}
+} {} {Why does this have null(10.0}
+
+test Expression-16.2 {visitFunctionApplicationNode} {
+    parseTreeTraceTest {map(function(x:int) x+3, {0, 2, 3})}
+} {{Entering node ptolemy.data.expr.ASTPtFunctionApplicationNode
+  Entering node ptolemy.data.expr.ASTPtFunctionDefinitionNode
+  Node ptolemy.data.expr.ASTPtFunctionDefinitionNode evaluated to (function(x:int) (x+3))
+  Entering node ptolemy.data.expr.ASTPtArrayConstructNode
+    Entering node ptolemy.data.expr.ASTPtLeafNode
+    Node ptolemy.data.expr.ASTPtLeafNode evaluated to 0
+    Entering node ptolemy.data.expr.ASTPtLeafNode
+    Node ptolemy.data.expr.ASTPtLeafNode evaluated to 2
+    Entering node ptolemy.data.expr.ASTPtLeafNode
+    Node ptolemy.data.expr.ASTPtLeafNode evaluated to 3
+  Node ptolemy.data.expr.ASTPtArrayConstructNode evaluated to {0, 2, 3}
+  Invoking public static ptolemy.data.ArrayToken ptolemy.data.expr.UtilityFunctions.map(ptolemy.data.FunctionToken,ptolemy.data.ArrayToken) throws ptolemy.kernel.util.IllegalActionException
+  as map((function(a0:int) int), {int})
+Node ptolemy.data.expr.ASTPtFunctionApplicationNode evaluated to {3, 5, 6}
+} {map(, $new(Array(3, 3, $new(Int(0)), $new(Int(2)), $new(Int(3)))))}}
+
+test Expression-16.3 {visitFunctionApplicationNode} {
+    set ptParser [java::new ptolemy.data.expr.PtParser]
+    set parseTreeCodeGenerator \
+	[java::new ptolemy.codegen.c.kernel.CParseTreeCodeGenerator]
+    
+    set parseTree [$ptParser generateParseTree \
+		       {f = function(x:double) x*5.0}]
+    set tree [$parseTreeCodeGenerator traceParseTreeEvaluation \
+		$parseTree [java::null]]
+    set r1 [list \
+		$tree \
+		[$parseTreeCodeGenerator generateFireCode]]
+    set parseTree [$ptParser generateParseTree {f(10.0)}
+    set tree [$parseTreeCodeGenerator traceParseTreeEvaluation \
+    		$parseTree [java::null]]
+    set r2 [list \
+    		$tree \
+    		[$parseTreeCodeGenerator generateFireCode]]
+    list $r1 $r2
+} {} {known failure}
+
+
 # I've commented this out. The ParseTreeCodeGenerator should not be 
 # tested this way. -- Gang
 #
