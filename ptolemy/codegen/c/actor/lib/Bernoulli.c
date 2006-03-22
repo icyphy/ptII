2b9025c2fc45659c6637d50b8d01468020c41acd
cxh <cxh>
2006-03-22 23:09:34 +0000

    Added comment about generating code

--- ptolemy/codegen/c/actor/lib/Bernoulli.c
+++ ptolemy/codegen/c/actor/lib/Bernoulli.c
@@ -1,4 +1,8 @@
 /***randomBlock***/
+   /* Note that this code will generate different random numbers than
+    * the Java version.  To change this, RandomSource would need to be updated
+    * to generate numbers using the same algorithm as Java.
+    */ 
     if (rand_r(&$actorSymbol(seed)) < $val(trueProbability) * RAND_MAX) {
         $ref(output) = 1;
     } else {
