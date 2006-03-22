b857f1b80330bb4fd3c10c2f424e63c8fb86aad0
cxh <cxh>
2006-03-22 23:21:47 +0000

    Updated Green

--- ptolemy/codegen/c/actor/lib/conversions/BooleanToAnything.java
+++ ptolemy/codegen/c/actor/lib/conversions/BooleanToAnything.java
@@ -1,6 +1,6 @@
 /* A helper class for ptolemy.actor.lib.conversions.BooleanToAnything
 
- Copyright (c) 1997-2005 The Regents of the University of California.
+ Copyright (c) 2005-2006 The Regents of the University of California.
  All rights reserved.
  Permission is hereby granted, without written agreement and without
  license or royalty fees, to use, copy, modify, and distribute this
@@ -38,8 +38,8 @@ import ptolemy.codegen.c.kernel.CCodeGeneratorHelper;
  @author Gang Zhou
  @version $Id$
  @since Ptolemy II 5.1
- @Pt.ProposedRating Red (zgang)
- @Pt.AcceptedRating Red (zgang)
+ @Pt.ProposedRating Green (zgang)
+ @Pt.AcceptedRating Green (zgang)
  */
 public class BooleanToAnything extends CCodeGeneratorHelper {
     /** Constructor method for the BooleanToAnything helper.
