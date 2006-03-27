18f2261f095f1b7b87ff30aea2f58dd3011b8936
cxh <cxh>
2006-03-27 20:59:02 +0000

    Better messages for conversion problems

--- ptolemy/codegen/kernel/type/Int.c
+++ ptolemy/codegen/kernel/type/Int.c
@@ -40,16 +40,33 @@ Token Int_delete(Token token) {
 /***convertBlock***/
 Token Int_convert(Token token, ...) {
     switch (token.type) {
-        #ifdef TYPE_Double
-            case TYPE_Double:
-                token.payload.Int = floor(token.payload.Double);
-                break;
-        #endif
+#ifdef TYPE_String
+    case TYPE_String:
+        fprintf(stderr,
+                "Int_convert(): Converting an String (%s) to an Int is not supported.\n",
+                token.payload.String);
+        break;
+#endif
+#ifdef TYPE_Double
+    case TYPE_Double:
+        token.payload.Int = floor(token.payload.Double);
+        break;
+#endif
         
+    case TYPE_Int:
+        fprintf(stderr,
+                "Int_convert(): Converting an Int (%d) to an Int?\n",
+                token.payload.Int);
+        break;
+
         // FIXME: not finished
-        default:
-            fprintf(stderr, "Int_convert(): Conversion from an unsupported type. (%d)\n", token.type);
-            break;
+    default: 
+        {
+            char * typeNames[] = {"String", "Double", "Array", "Boolean",
+                                  "Int"};
+            fprintf(stderr, "Int_convert(): Conversion from an unsupported type. (%d: %s)\n", token.type, (token.type <= 5 ? typeNames[token.type] : "Unknown?") );
+        }
+        break;
     }    
     token.type = TYPE_Int;
     return token;
