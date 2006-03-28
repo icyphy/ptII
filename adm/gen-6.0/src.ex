d0cef03a3c6ddb3e3b1ba35aca3f92405e5954fc
cxh <cxh>
2006-03-28 23:23:12 +0000

    Exlude copernicus/fragment

--- adm/gen-6.0/src.ex
+++ adm/gen-6.0/src.ex
@@ -123,6 +123,7 @@ ptolemy/copernicus/c/test/java.lang.Object.h
 ptolemy/copernicus/c/test/natives
 ptolemy/copernicus/c/test/testOutput
 ptolemy/copernicus/deep
+ptolemy/copernicus/fragment
 ptolemy/copernicus/interpreted/test/codeGenerator.tmp
 ptolemy/copernicus/java/test/codeGenerator.tmp
 ptolemy/copernicus/java/test/longjtests.tcl
