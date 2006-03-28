843e523fdd9d213e0e34c6f26e686f107b7cd4d5
cxh <cxh>
2006-03-28 03:20:10 +0000

    Initial shot at getting FileReader to work

--- ptolemy/codegen/c/actor/lib/io/FileReader.c
+++ ptolemy/codegen/c/actor/lib/io/FileReader.c
@@ -1,6 +1,12 @@
 /***preinitBlock***/
     FILE * $actorSymbol(filePtr);
+    int $actorSymbol(charRead);
+    int $actorSymbol(index);
+    int $actorSymbol(length) = 80;
+/**/
 
+/***initBlock***/
+	$ref(output) = (char*) malloc($actorSymbol(length) * sizeof(char));
 /**/
 
 /***openForRead($fileName)***/
@@ -8,4 +14,30 @@
         fprintf(stderr,"ERROR: cannot open file \"$fileName\" for LineReader actor.\n");
         exit(1);
     }
+/**/
+
+/***fireBlock***/
+    $actorSymbol(index) = 0;
+    do {
+        $actorSymbol(charRead) = fgetc($actorSymbol(filePtr));
+        if ($actorSymbol(index) >= $actorSymbol(length)) {
+            $actorSymbol(length) *= 2;
+            $ref(output) = (char*) realloc ($ref(output), $actorSymbol(length) * sizeof(char));
+        }
+        if ($actorSymbol(charRead) != EOF ) {
+            if ($actorSymbol(charRead) != '\r') {
+                $ref(output)[$actorSymbol(index)++] = $actorSymbol(charRead);
+            } else {
+                fprintf(stderr, "Warning: dropping a \\r char\n");
+            }
+        } else {
+            $ref(output)[$actorSymbol(index)++] = '\n';
+        }
+    } while ( $actorSymbol(charRead) != EOF );
+/**/
+
+
+
+/***wrapupBlock***/
+    fclose($actorSymbol(filePtr));
 /**/
