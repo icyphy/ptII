35ee856bd0c43ea9cb8bc32ebcc0b6eeb7f6c118
cxh <cxh>
2006-03-28 01:43:39 +0000

    Updated Proposed rating tag

--- ptolemy/codegen/c/actor/lib/logic/Equals.java
+++ ptolemy/codegen/c/actor/lib/logic/Equals.java
@@ -39,7 +39,7 @@ import ptolemy.kernel.util.IllegalActionException;
  @author Man-Kit Leung
  @version $Id$
  @since Ptolemy II 5.1
- @Pt.ProposedRating Red (mankit) 
+ @Pt.ProposedRating Red (mankit) Works with Doubles and Strings, Needs to work with Arrays
  @Pt.AcceptedRating Red (mankit)
  */
 public class Equals extends CCodeGeneratorHelper {
