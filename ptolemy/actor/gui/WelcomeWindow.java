39c322bb6b1a929af645c3936b5b626bb84a1d03
cxh <cxh>
2006-03-30 03:37:30 +0000

    Works much better now that Nandita did some work on it

--- ptolemy/actor/gui/WelcomeWindow.java
+++ ptolemy/actor/gui/WelcomeWindow.java
@@ -28,11 +28,19 @@
 package ptolemy.actor.gui;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.JCheckBox;
+import javax.swing.JButton;
 
+import ptolemy.actor.gui.Configuration;
+import ptolemy.gui.MessageHandler;
 import ptolemy.data.BooleanToken;
+import ptolemy.data.expr.SingletonParameter;
 
 //////////////////////////////////////////////////////////////////////////
 //// WelcomeWindow
@@ -40,7 +48,51 @@ import ptolemy.data.BooleanToken;
 /**
  A toplevel frame that can view HTML documents, but has no menus. 
 
- @author Christopher Brooks
+ <p>This window uses the _showWelcomeWindowAtStartup
+ SingletonParameter preference in
+ {@link ptolemy.actor.gui.PtolemyPreferences} to determine whether the
+ window is shown or not.  If this parameter is not present or is true,
+ then the window is show, if the parameter is false, then it is not shown.
+ This parameter is set in ~/.ptolemyII/PtolemyPreferences.xml, which under
+ Windows might be in 
+ <code>c:/Documents and Settings/<i>yourlogin</i>/.ptolemyII/PtolemyPreferences.xml</code>
+ <p>The easiest way to adjust the configuration so that this window
+ is used is to edit the <code>welcomeWindow.xml</code> file that corresponds
+ with the configuration and set the tableau to be
+ <code>ptolemy.actor.gui.WelcomeWindowTableau</code>
+ For example, <code>$PTII/ptolemy/configs/full/welcomeWindow.xml</code>
+ might look like:
+ <pre>
+&lt;?xml version="1.0" standalone="no"?&gt;
+&lt;!DOCTYPE plot PUBLIC "-//UC Berkeley//DTD MoML 1//EN"
+    "http://ptolemy.eecs.berkeley.edu/xml/dtd/MoML_1.dtd"&gt;
+&lt;entity name="directory"&gt;
+  &lt;entity name="doc" class="ptolemy.actor.gui.HTMLEffigy"&gt;
+    &lt;entity name="tableau" class="ptolemy.actor.gui.WelcomeWindowTableau"&gt;
+      &lt;!-- If you adjust the size, be sure to try it under Java 1.5 --&gt;
+      &lt;property name="size" value="[600, 220]"/&gt;
+      &lt;property name="url" value="ptolemy/configs/full/intro.htm"/&gt;
+    &lt;/entity&gt;
+  &lt;/entity&gt;
+&lt;/entity&gt;
+ </pre>
+ The above configuration will create a WelcomeWindow with a 600x200
+ size that displays the contents of
+ <code>ptolemy/configs/full/intro.htm</code>
+
+ <p>Note that since this widow has no menus, if you use this window, then
+ you will want to set the
+ <code>_applicationBlankPtolemyEffigyAtStartup</code> parameter in the
+ configuration so that a blank Graph Editor window pops up along with
+ this welcome window.  For example:
+<pre>
+  &lt;property name="_applicationBlankPtolemyEffigyAtStartup"
+            class="ptolemy.data.expr.Parameter"
+            value="true"/&gt;
+<pre> 
+
+
+ @author Christopher Brooks, Nandita Mangal
  @version $Id$
  @since Ptolemy II 5.2
  @Pt.ProposedRating Red (cxh)
@@ -50,19 +102,31 @@ public class WelcomeWindow extends HTMLViewer {
     /** Construct a blank HTML Viewer with no menu items.
      */
     public WelcomeWindow() {
-        super(); 
+        super();
+        //setUndecorated(true);
+        setResizable(false);
         _statusBar = null;
 
         // Add the close panel
         _closePanel = new JPanel();
-        String message = "FIXME: need \"Show this Dialog on Startup\" "
-            + "and Close Button"; 
-        JTextField _message = new JTextField(message.length() + 2);
-        _message.setEditable(false);
-        _message.setAlignmentX(LEFT_ALIGNMENT);
-        //_message.setText(message);
+        _closePanel.setBackground(new Color(227, 231, 236));
+        _closePanel.setSize(401, 100);
+        _closePanel.setLayout(new BorderLayout());
 
-//         _closePanel.add(_message);
+        _startupCheck = new JCheckBox(
+                "<html><table cellpadding=0><tr><td width=9/><td><font size=3>Show this dialog upon startup </font></td></tr></table></html>", true);
+        _startupCheck.setBackground(new Color(227, 231, 236));
+
+        _closeButton = new JButton("Close");
+        _closeButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+                setVisible(false);
+                close();
+            }
+        });
+
+        _closePanel.add(_startupCheck, BorderLayout.WEST);
+        _closePanel.add(_closeButton, BorderLayout.EAST);
     }
 
     ///////////////////////////////////////////////////////////////////
@@ -75,22 +139,62 @@ public class WelcomeWindow extends HTMLViewer {
         hideMenuBar();
         getContentPane().add(_closePanel, BorderLayout.SOUTH);
         super.pack();
+
     }
 
-    /** Show the window if the _hideWelcomeWindow parameter is not set 
-     *  or is false.
+    /** Show the window if the _showWelcomeWindowAtStartup parameter
+     *  is not set or is true.
      */
     public void show() {
-        BooleanToken hideWelcomeWindow = 
-            ((BooleanToken) PtolemyPreferences.preferenceValue(getDirectory(),
-                    "_hideWelcomeWindow"));
 
-        if (hideWelcomeWindow != null && hideWelcomeWindow.booleanValue()) {
-            // The _hideWelcomeWindow parameter is true
-            System.out.println("Hide the welcome window");
+        Configuration configuration = getConfiguration();
+
+        _showWelcomeWindowAtStartup = ((BooleanToken) PtolemyPreferences
+                .preferenceValue(configuration, "_showWelcomeWindowAtStartup"));
+
+        if (_showWelcomeWindowAtStartup != null
+                && !_showWelcomeWindowAtStartup.booleanValue()) {
             return;
         }
         super.show();
+    }
+
+    ///////////////////////////////////////////////////////////////////
+    ////                         protected methods                 ////
+
+    /** Read the"Show this dialog on startup" checkbox, update the
+     *  preferences if necessary and then call the super class and close
+     *  this window.  This method is called by {@link ptolemy.gui.Top#close()}.
+     */
+    protected boolean _close() {
+        Configuration configuration = getConfiguration();
+        if ( (_showWelcomeWindowAtStartup == null 
+                     && !_startupCheck.isSelected())
+            || (_showWelcomeWindowAtStartup != null
+                    && _showWelcomeWindowAtStartup.booleanValue() 
+                    != _startupCheck.isSelected())) {
+            // Update the preferences if there is no preference and
+            // the user unchecked the "Show this dialog on startup"
+            // or if the value of the preference and the checkbox differ. 
+            try {
+                PtolemyPreferences preferences = PtolemyPreferences.getPtolemyPreferencesWithinConfiguration(configuration);
+                // FIXME: is ok to create a new parameter each time? 
+                SingletonParameter showWelcomeWindowAtStartupParameter =
+                    new SingletonParameter(preferences,
+                            "_showWelcomeWindowAtStartup");
+
+                // FIXME: is there a better way to set a BooleanToken?
+                showWelcomeWindowAtStartupParameter.setToken(
+                        _startupCheck.isSelected()
+                        ?  BooleanToken.TRUE : BooleanToken.FALSE);
+                preferences.save();
+            } catch (Exception ex) {
+                MessageHandler.error("Failed to update preferences and"
+                        + "save _showWelcomeWindowAtStarupPreferences",
+                        ex);
+            }
+        }
+        return super._close();
     }
 
     ///////////////////////////////////////////////////////////////////
@@ -100,5 +204,11 @@ public class WelcomeWindow extends HTMLViewer {
      *  "Show this dialog on startup" checkbox and the close button.
      */
     private JPanel _closePanel;
-}
 
+    /** True if the welcome window is shown at startup. */
+    private BooleanToken _showWelcomeWindowAtStartup;
+
+    private JCheckBox _startupCheck;
+
+    private JButton _closeButton;
+}
