85bc44b9cac84bc73c6f2e98e5bf9404efdfc94e
cxh <cxh>
2006-03-30 23:51:55 +0000

    Fixed to pass test

--- ptolemy/codegen/c/actor/lib/Quantizer.c
+++ ptolemy/codegen/c/actor/lib/Quantizer.c
@@ -1,31 +1,43 @@
 /*** preinitBlock ***/
-    double $actorSymbol(levels)[] = $val(levels);
-    double* $actorSymbol(transitionPoints);
-    int $actorSymbol(i);
+Token $actorSymbol(levels);
+double* $actorSymbol(transitionPoints);
+int $actorSymbol(i);
 /**/
 
 //$numPoints is number of transition points (# of levels - 1)
 /*** initBlock ($numPoints)***/
+    $actorSymbol(levels) = $val(levels);
     // allocate space to store levels and transitionPoints
     // We use CALLOC to optimizate array performance
-    $actorSymbol(transitionPoints) = (double*) calloc($numPoints, sizeof(double));
+    $actorSymbol(transitionPoints) =
+            (double*) calloc($numPoints, sizeof(double));
     
-    for ($actorSymbol(i) = 0; $actorSymbol(i) < $numPoints; $actorSymbol(i)++) {
+    for ($actorSymbol(i) = 0;
+            $actorSymbol(i) < $numPoints;
+            $actorSymbol(i)++) {
         // transitionPoint[I] = (levels[I] + levels[I+1]) / 2;
-        $actorSymbol(transitionPoints)[$actorSymbol(i)] = ($actorSymbol(levels)[$actorSymbol(i)] + $actorSymbol(levels)[$actorSymbol(i)]) / 2;
+        $actorSymbol(transitionPoints)[$actorSymbol(i)] =
+                ($actorSymbol(levels).payload.Array->elements[$actorSymbol(i)].payload.Double
+                        + $actorSymbol(levels).payload.Array->elements[$actorSymbol(i) + 1].payload.Double) / 2.0;
+
     }
 /**/
 
 /*** fireBlock ($numPoints)***/
-    $ref(output) = $actorSymbol(levels)[0];     // default
-    for ($actorSymbol(i) = 0; $actorSymbol(i) < $numPoints; $actorSymbol(i)++) {        
-        if ($ref(input) > $actorSymbol(transitionPoints)[$actorSymbol(i)]) {
-            $ref(output) = $actorSymbol(levels)[$actorSymbol(i) + 1];
+        $ref(output) = $actorSymbol(levels).payload.Double;
+        for ($actorSymbol(i) = 0;
+                 $actorSymbol(i) < $numPoints;
+                 $actorSymbol(i)++) {        
+            if ($ref(input)
+                     <= $actorSymbol(transitionPoints)[$actorSymbol(i)]) {
+                $ref(output) = $actorSymbol(levels).payload.Array->elements[$actorSymbol(i)].payload.Double;
+                break;
+            }
         }
-    }
+
 /**/
 
 /*** wrapupBlock ***/
-    free($actorSymbol(transitionPoints));
+        free($actorSymbol(transitionPoints));
 /**/
 
