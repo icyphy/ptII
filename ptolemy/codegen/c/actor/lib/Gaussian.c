ff34dd1b2b3dfab0a8e859d3beb1c58f0debedfd
cxh <cxh>
2006-03-30 23:09:08 +0000

    Renamed seed to actorSeed so as to avoid duplication with the seed parameter

--- ptolemy/codegen/c/actor/lib/Gaussian.c
+++ ptolemy/codegen/c/actor/lib/Gaussian.c
@@ -3,18 +3,20 @@
 // generates the same list of random numbers as the java.util.Random object.
 
 /*** sharedBlock ***/
-    int Gaussian_next(int bits, double* seed) {
-        *seed = (((long long) *seed * 0x5DEECE66DLL) + 0xBLL) & ((1LL << 48) - 1);
-        return (int)((signed long long) *seed >> (48 - bits));
-    }
+int Gaussian_next(int bits, double* seed) {
+    *seed = (((long long) *seed * 0x5DEECE66DLL) + 0xBLL)
+        & ((1LL << 48) - 1);
+    return (int)((signed long long) *seed >> (48 - bits));
+}
     
-    double Gaussian_nextDouble(double* seed) {
-        return (((long long)Gaussian_next(26, seed) << 27) + Gaussian_next(27, seed)) / (double)(1LL << 53);
-    }
+double Gaussian_nextDouble(double* seed) {
+    return (((long long)Gaussian_next(26, seed) << 27)
+            + Gaussian_next(27, seed)) / (double)(1LL << 53);
+}
 /**/
 
 /*** preinitBlock ***/
-    double $actorSymbol(seed);
+    double $actorSymbol(actorSeed);
     int $actorSymbol(haveNextNextGaussian) = 0;  // false
     double $actorSymbol(nextNextGaussian);
 
@@ -26,7 +28,7 @@
 /*** setSeedBlock ***/
     /* see documentation from http://java.sun.com/j2se/1.4.2/docs/api/java/util/Random.html#setSeed(long) */
     //this.seed = (seed ^ 0x5DEECE66DL) & ((1L << 48) - 1);
-    $actorSymbol(seed) = ((long long) $val(seed) ^ 0x5DEECE66DLL)  & ((1LL << 48) - 1);
+    $actorSymbol(actorSeed) = ((long long) $val(seed) ^ 0x5DEECE66DLL)  & ((1LL << 48) - 1);
 /**/
 
 /*** fireBlock ***/
@@ -35,8 +37,8 @@
         $ref(output) = $actorSymbol(nextNextGaussian);
     } else {
         do { 
-            $actorSymbol(v1) = 2 * Gaussian_nextDouble(&$actorSymbol(seed)) - 1;   // between -1.0 and 1.0
-            $actorSymbol(v2) = 2 * Gaussian_nextDouble(&$actorSymbol(seed)) - 1;   // between -1.0 and 1.0
+            $actorSymbol(v1) = 2 * Gaussian_nextDouble(&$actorSymbol(actorSeed)) - 1;   // between -1.0 and 1.0
+            $actorSymbol(v2) = 2 * Gaussian_nextDouble(&$actorSymbol(actorSeed)) - 1;   // between -1.0 and 1.0
             $actorSymbol(s) = $actorSymbol(v1) * $actorSymbol(v1) + $actorSymbol(v2) * $actorSymbol(v2);
         } while ($actorSymbol(s) >= 1 || $actorSymbol(s) == 0);
 
