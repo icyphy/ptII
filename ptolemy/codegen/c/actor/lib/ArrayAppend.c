46d950f01628c06edde4da3327da293c17e70691
mankit <mankit>
2006-04-04 22:15:56 +0000

    New Added helpers

---
+++ ptolemy/codegen/c/actor/lib/ArrayAppend.c
@@ -0,0 +1,34 @@
+/*** preinitBlock ***/
+	int $actorSymbol(length);
+	int $actorSymbol(i);
+	int $actorSymbol(j);
+	
+	boolean $actorSymbol(doDelete);
+/**/
+
+/*** initBlock ***/
+	$actorSymbol(doDelete) = false;
+/**/
+
+/*** fireBlock ***/
+	if ($actorSymbol(doDelete)) {
+		Array_delete($ref(output));
+	}
+	
+	$actorSymbol(length) = 0;
+	for ($actorSymbol(i) = 0; $actorSymbol(i) < $size(input); $actorSymbol(i)++) {
+		$actorSymbol(length) += $ref(input).payload.Array->size;
+	}
+	
+	$ref(output) = $new(Array($actorSymbol(length), 0));
+	
+	$actorSymbol(length) = 0;
+	for ($actorSymbol(i) = 0; $actorSymbol(i) < $size(input); $actorSymbol(i)++) {
+		for ($actorSymbol(j) = 0; $actorSymbol(j) < $ref(input#$actorSymbol(i)).payload.Array->size; $actorSymbol(j)++, $actorSymbol(length)++) {
+			$ref(output).payload.Array->elements[$actorSymbol(length)] = functionTable[Array_get($ref(input#$actorSymbol(i)), $actorSymbol(length)).type][FUNC_clone](Array_get($ref(input#$actorSymbol(i)), $actorSymbol(length)));
+		}
+	}
+	
+	$actorSymbol(doDelete) = true;
+/**/
+
