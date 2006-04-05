2ebc2cf978da89c53847d713a340e4755ea6585e
cxh <cxh>
2006-04-05 01:02:46 +0000

    set the last char to \0 after the copy

--- ptolemy/codegen/c/actor/lib/string/StringSubstring.c
+++ ptolemy/codegen/c/actor/lib/string/StringSubstring.c
@@ -9,9 +9,9 @@
 /*** fireBlock ***/
         $actorSymbol(length) = $ref(stop) - $ref(start);
         /* Realloc space for the null */
-	$ref(output) = (char*) realloc($ref(output), $actorSymbol(length) + 1);
+$ref(output) = (char*) realloc($ref(output), $actorSymbol(length) + 1);
+	$ref(output) = strncpy($ref(output), $ref(input) + $ref(start), $actorSymbol(length));
         /* Solaris: strncpy does not add a null if only n chars are copied. */ 
         $ref(output)[$actorSymbol(length)] = '\0';
-	$ref(output) = strncpy($ref(output), $ref(input) + $ref(start), $actorSymbol(length));
 /**/
 
