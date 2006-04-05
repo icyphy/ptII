9d1fc2f06ab3425e6abe0e29e5934d44e647239d
mankit <mankit>
2006-04-05 00:41:58 +0000

    New Added helpers

---
+++ ptolemy/codegen/c/actor/lib/Stop.c
@@ -0,0 +1,12 @@
+/*** preinitBlock ***/
+	int $actorSymbol(i);
+/**/
+
+/*** fireBlock ***/
+    for ($actorSymbol(i) = 0; $actorSymbol(i) < $size(input); $actorSymbol(i)++) {
+		if ($ref(input#$actorSymbol(i))) {
+            exit(0);
+        }
+    }
+/**/
+
