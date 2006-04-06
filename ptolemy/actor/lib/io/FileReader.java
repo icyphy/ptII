8763aad88508133931246afd7bc4d9d43a748849
cxh <cxh>
2006-04-06 01:20:01 +0000

    Set name of newline property to newline so that config/tests pass

--- ptolemy/actor/lib/io/FileReader.java
+++ ptolemy/actor/lib/io/FileReader.java
@@ -77,7 +77,7 @@ public class FileReader extends Source {
         fileOrURLPort = new TypedIOPort(this, "fileOrURL", true, false);
         fileOrURLPort.setTypeEquals(BaseType.STRING);
 
-        newline = new Parameter(this, "newLine");
+        newline = new Parameter(this, "newline");
         newline.setExpression("property(\"file.separarator\")");
 
         _attachText("_iconDescription", "<svg>\n"
