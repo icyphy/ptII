4f7f40849f201389b54e37548f19e1d8182541bf
tfeng <tfeng>
2006-04-10 07:13:10 +0000

    Supports TrialModuleHack.

--- ptolemy/backtrack/manual/ptolemy/actor/lib/Backtrack.java
+++ ptolemy/backtrack/manual/ptolemy/actor/lib/Backtrack.java
@@ -28,21 +28,20 @@ COPYRIGHTENDKEY
 
 package ptolemy.backtrack.manual.ptolemy.actor.lib;
 
-import java.io.StringWriter;
+import java.util.HashMap;
 import java.util.Iterator;
-import java.util.List;
 
+import ptolemy.actor.CompositeActor;
 import ptolemy.actor.TypedAtomicActor;
-import ptolemy.actor.TypedIORelation;
+import ptolemy.actor.TypedIOPort;
+import ptolemy.backtrack.Checkpoint;
+import ptolemy.backtrack.Rollbackable;
+import ptolemy.data.BooleanToken;
+import ptolemy.data.LongToken;
+import ptolemy.data.type.BaseType;
 import ptolemy.kernel.CompositeEntity;
-import ptolemy.kernel.Entity;
 import ptolemy.kernel.util.IllegalActionException;
-import ptolemy.kernel.util.Locatable;
 import ptolemy.kernel.util.NameDuplicationException;
-import ptolemy.kernel.util.NamedObj;
-import ptolemy.moml.MoMLChangeRequest;
-import ptolemy.moml.MoMLParser;
-import ptolemy.util.MessageHandler;
 
 //////////////////////////////////////////////////////////////////////////
 //// Backtrack
@@ -59,117 +58,95 @@ public class Backtrack extends TypedAtomicActor {
     public Backtrack(CompositeEntity container, String name)
             throws NameDuplicationException, IllegalActionException {
         super(container, name);
-        moveToFirst();
+        
+        _checkpoint = new TypedIOPort(this, "checkpoint", true, false);
+        _checkpoint.setTypeEquals(BaseType.BOOLEAN);
+        
+        _rollback = new TypedIOPort(this, "rollback", true, false);
+        _rollback.setTypeEquals(BaseType.LONG);
+        
+        _handle = new TypedIOPort(this, "handle", false, true);
+        _handle.setTypeEquals(BaseType.LONG);
     }
     
-    public void preinitialize() throws IllegalActionException {
-        try {
-            _replaceEntities();
-        } catch (IllegalActionException e1) {
-            throw e1;
-        } catch (Exception e2) {
-            throw new IllegalActionException(e2.getMessage());
+    public void fire() throws IllegalActionException {
+        super.fire();
+        
+        BooleanToken checkpointTrigger = (BooleanToken)_checkpoint.get(0);
+        this._checkpointTrigger = checkpointTrigger.booleanValue();
+        
+        LongToken rollbackHandle = (LongToken)_rollback.get(0);
+        this._rollbackHandle = rollbackHandle.longValue();
+        
+        if (_checkpointTrigger) {
+            HashMap handles = new HashMap();
+            _checkpoint(handles, (CompositeActor)getContainer());
+            _currentHandle++;
+            _handleMap.put(new Long(_currentHandle), handles);
+            _handle.send(0, new LongToken(_currentHandle));
         }
-        super.initialize();
+    }
+
+    public boolean postfire() throws IllegalActionException {
+        boolean result = super.postfire();
+        
+        if (_rollbackHandle > 0) {
+            HashMap handles = (HashMap)_handleMap.get(new Long(_rollbackHandle));
+            
+            if (handles != null) {
+                Iterator checkpoints = handles.keySet().iterator();
+                while (checkpoints.hasNext()) {
+                    Checkpoint checkpointObject = (Checkpoint)checkpoints.next();
+                    long handle = ((Long)handles.get(checkpointObject)).longValue();
+                    checkpointObject.rollback(handle);
+                }
+            }
+            
+            Iterator keys = _handleMap.keySet().iterator();
+            while (keys.hasNext()) {
+                long handle = ((Long)keys.next()).longValue();
+                if (handle > _rollbackHandle) {
+                    keys.remove();
+                }
+            }
+        }
+        
+        return result;
     }
     
-    /*protected void _replaceEntities() throws Exception {
-        CompositeEntity container = (CompositeEntity)getContainer();
-        StringWriter moml = new StringWriter();
-        container.toplevel().exportMoML(moml, 0);
+    private void _checkpoint(HashMap handles, CompositeActor container) {
+        Iterator entities = container.entityList(Rollbackable.class).
+                iterator();
+        while (entities.hasNext()) {
+            Rollbackable entity = (Rollbackable)entities.next();
+            Checkpoint checkpointObject = entity.$GET$CHECKPOINT();
+            if (!handles.containsKey(checkpointObject)) {
+                long handle = checkpointObject.createCheckpoint();
+                handles.put(checkpointObject, new Long(handle));
+            }
+        }
         
-        MoMLParser parser = new MoMLParser();
-        RenameClassMoMLFilter filter = new RenameClassMoMLFilter();
-        parser.addMoMLFilter(filter);
-        NamedObj topLevel = parser.parse(moml.toString());
-        
-        parser.getMoMLFilters().remove(filter);
-    }*/
-
-    protected void _replaceEntities() throws NameDuplicationException,
-            IllegalActionException {
-        final String backtrackPrefix = "ptolemy.backtrack.automatic"; 
-        
-        CompositeEntity container = (CompositeEntity)getContainer();
-        Iterator siblings = container.containedObjectsIterator();
-        while (siblings.hasNext()) {
-            NamedObj sibling = (NamedObj)siblings.next();
-            if (sibling instanceof Entity) {
-                Entity entity = (Entity)sibling;
-                String oldClassName = entity.getClassName();
-                String newClassName = backtrackPrefix + "." + oldClassName;
-                if (_classExists(newClassName)) {
-                    _changeEntityClass(entity, oldClassName, newClassName);
-                }
+        Iterator compositeActors = container.entityList(CompositeActor.class).
+                iterator();
+        while (compositeActors.hasNext()) {
+            CompositeActor compositeActor = (CompositeActor)compositeActors.next();
+            if (!(compositeActor instanceof Rollbackable)) {
+                _checkpoint(handles, compositeActor);
             }
         }
     }
     
-    protected void _changeEntityClass(Entity entity, String oldClassName,
-            String newClassName) {
-        CompositeEntity container = (CompositeEntity)getContainer();
-        
-        try {
-            StringWriter moml = new StringWriter();
-            moml.write("<group name=\"auto\">");
-            entity.exportMoML(moml, 0);
-            moml.write("</group>");
-            
-            List relations = entity.linkedRelationList();
-            Iterator relationsIterator = relations.iterator();
-            while (relationsIterator.hasNext()) {
-                TypedIORelation relation =
-                    (TypedIORelation)relationsIterator.next();
-            }
-            
-            String momlString = moml.toString().replace(
-                    "\"" + oldClassName + "\"",
-                    "\"" + newClassName + "\"");
-
-            MoMLChangeRequest change =
-                new MoMLChangeRequest(this, container, momlString) {
-                // Override this method to offset the locations of pasted objects.
-                protected void _postParse(MoMLParser parser) {
-                    Iterator topObjects = parser.topObjectsCreated().iterator();
-                    while (topObjects.hasNext()) {
-                        NamedObj topObject = (NamedObj)topObjects.next();
-                        try {
-                            Iterator locations = topObject.attributeList(Locatable.class).iterator();
-                            while (locations.hasNext()) {
-                                Locatable location = (Locatable)locations.next();
-                                double[] locationValue = location.getLocation();
-                                for (int i = 0; i < locationValue.length; i++) {
-                                    locationValue[i] += 10;
-                                }
-                                location.setLocation(locationValue);
-                            }
-                        } catch (IllegalActionException e) {
-                            MessageHandler.error("Paste failed", e);
-                        }
-                    }
-                    parser.clearTopObjectsList();
-                }
-
-                // Override this method to clear the list of top objects.
-                protected void _preParse(MoMLParser parser) {
-                    super._preParse(parser);
-                    parser.clearTopObjectsList();
-                }
-            };
-             
-            change.setUndoable(true);
-            container.requestChange(change);
-        } catch (Exception ex) {
-            MessageHandler.error("Paste failed", ex);
-        }
-    }
+    private TypedIOPort _checkpoint;
     
-    private boolean _classExists(String className) {
-        try {
-            Class.forName(className);
-            return true;
-        } catch (ClassNotFoundException e) {
-            return false;
-        }
-    }
+    private boolean _checkpointTrigger;
+    
+    private long _currentHandle = 0;
+    
+    private TypedIOPort _handle;
+    
+    private HashMap _handleMap = new HashMap();
+    
+    private TypedIOPort _rollback;
+    
+    private long _rollbackHandle;
 }
