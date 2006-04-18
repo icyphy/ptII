249f7c0165d4c231b49939da1254b25eaa6379b8
cxh <cxh>
2006-04-18 01:51:29 +0000

    Code written by Matthew Brooke for Kepler.  These classes use Batik SVG.  configure knows to check for the Batik jar file

---
+++ diva/util/java2d/svg/SVGRenderingListener.java
@@ -0,0 +1,49 @@
+/**
+ *    '$RCSfile$'
+ *
+ *     '$Author$'
+ *       '$Date$'
+ *   '$Revision$'
+ *
+ *  For Details: http://kepler.ecoinformatics.org
+ *
+ * Copyright (c) 2003 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * Permission is hereby granted, without written agreement and without
+ * license or royalty fees, to use, copy, modify, and distribute this
+ * software and its documentation for any purpose, provided that the
+ * above copyright notice and the following two paragraphs appear in
+ * all copies of this software.
+ *
+ * IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+ * FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+ * ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN
+ * IF THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY
+ * OF SUCH DAMAGE.
+ *
+ * THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+ * PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY
+ * OF CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT,
+ * UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+ */
+
+package diva.util.java2d.svg;
+
+
+/** A listener interface to handle callbacks after Batik has finished rendering
+ * an SVG document
+ *
+ * @version        $Id$
+ * @author         Matthew Brooke
+ */
+public interface SVGRenderingListener {
+
+    /**
+     * receive a callback after Batik has finished rendering an SVG document
+     */
+    public void svgRenderingComplete();
+
+}
