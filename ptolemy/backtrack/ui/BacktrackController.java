993cc336a4e49aa3dc11b385469bf181b9066d23
tfeng <tfeng>
2006-04-20 20:52:20 +0000

    Documented.

--- ptolemy/backtrack/ui/BacktrackController.java
+++ ptolemy/backtrack/ui/BacktrackController.java
@@ -1,4 +1,4 @@
-/* 
+/* Backtracking controller for composite actors.
 
 Copyright (c) 2005-2006 The Regents of the University of California.
 All rights reserved.
@@ -40,15 +40,31 @@ import ptolemy.backtrack.Rollbackable;
 //////////////////////////////////////////////////////////////////////////
 //// BacktrackController
 /**
+   Backtracking controller for composite actors. This controller is built on top
+   of the backtracking sub-system. It helps to create checkpoints for Ptolemy II
+   composite actors. Each of those checkpoints may correspond to multiple
+   checkpoints in the backtracking sub-system, due to the fact that entities in
+   a composite actor may be monitored by different checkpoint objects. This
+   controller also allows to roll back the composite actor to its previous
+   state.
 
-
- @author Thomas Feng
- @version $Id$
- @since Ptolemy II 5.1
- @Pt.ProposedRating Red (tfeng)
- @Pt.AcceptedRating Red (tfeng)
- */
+   @author Thomas Feng
+   @version $Id$
+   @since Ptolemy II 5.1
+   @Pt.ProposedRating Red (tfeng)
+   @Pt.AcceptedRating Red (tfeng)
+*/
 public class BacktrackController {
+    ///////////////////////////////////////////////////////////////////
+    ////                       public methods                      ////
+
+    /** Commit a checkpoint handle. After it is committed, the system can no
+     *  longer roll back to that checkpoint, or any other checkpoint taken
+     *  before that one. Memory allocated for the committed checkpoints will be
+     *  released.
+     * 
+     *  @param handle The handle of the checkpoint to be committed.
+     */
     public void commit(long handle) {
         Iterator handles = _checkpoints.keySet().iterator();
         while (handles.hasNext()) {
@@ -70,6 +86,22 @@ public class BacktrackController {
         }
     }
     
+    /** Create a checkpoint for the given composite actor, and return the
+     *  checkpoint handle.
+     *  
+     *  This checkpoint handle is different from the checkpoint handles returned
+     *  by {@link Checkpoint#createCheckpoint()}, which are the internal
+     *  representations of checkpoint timestamps. This checkpoint handle,
+     *  however, refers to a collection of checkpoint timestamps for different
+     *  checkpoint objects that manage different entities in the container.
+     *  
+     *  The returned checkpoint handle can only be used with {@link
+     *  #commit(long)} and {@link #rollback(long, boolean)} in this class.
+     * 
+     *  @param container The composite actor for which a checkpoint will be
+     *   created.
+     *  @return The checkpoint handle.
+     */
     public synchronized long createCheckpoint(CompositeActor container) {
         HashMap checkpointsAndHandles = new HashMap();
         Iterator objectsIterator = container.containedObjectsIterator();
@@ -89,6 +121,21 @@ public class BacktrackController {
         return _currentHandle++;
     }
     
+    /** Roll back the system state with the records in the checkpoint with the
+     *  given handle.
+     *  
+     *  If the checkpoint objects that monitor the entities in the composite
+     *  actor are not changed since the time when the checkpoint was created,
+     *  this rollback only affects that composite actor and its children. This
+     *  is true in most of the cases, but it is not guaranteed.
+     * 
+     *  @param handle The checkpoint handle previous returned by {@link
+     *   #createCheckpoint(CompositeActor)}.
+     *  @param trim Whether the records in the checkpoint should be deleted. If
+     *   the records are deleted, memory is freed, but lazy computation is
+     *   impossible.
+     *  @return Whether the given handle is valid.
+     */
     public boolean rollback(long handle, boolean trim) {
         HashMap checkpointsAndHandles =
             (HashMap)_checkpoints.get(new Long(handle));
@@ -106,7 +153,18 @@ public class BacktrackController {
         }
     }
     
+    ///////////////////////////////////////////////////////////////////
+    ////                       private fields                      ////
+
+    /** The map from checkpoint handles used in this class to checkpoint handles
+     *  used in the checkpointing sub-system. The keys of this map are handles
+     *  (of Long type); the values are HashMaps. In each of those HashMaps, the
+     *  keys are checkpoint objects, and the values are the checkpoint handles
+     *  returned by those checkpoint objects.
+     */
     private HashMap _checkpoints = new HashMap();
     
+    /** The current checkpoint handle.
+     */
     private long _currentHandle = 0;
 }
