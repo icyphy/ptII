20b53594b38b208774681ceae0f05e34986ca5f4
tfeng <tfeng>
2006-04-28 06:50:01 +0000

    Documented.

--- ptolemy/backtrack/eclipse/plugin/editor/PtolemyEditor.java
+++ ptolemy/backtrack/eclipse/plugin/editor/PtolemyEditor.java
@@ -1,4 +1,4 @@
-/*
+/* Editor with Ptolemy syntax highlighting.
 
  Copyright (c) 2005 The Regents of the University of California.
  All rights reserved.
@@ -28,17 +28,13 @@
 package ptolemy.backtrack.eclipse.plugin.editor;
 
 import org.eclipse.core.runtime.IProgressMonitor;
-
-import org.eclipse.jdt.core.IJavaElement;
 import org.eclipse.jdt.core.dom.CompilationUnit;
 import org.eclipse.jdt.internal.ui.JavaPlugin;
 import org.eclipse.jdt.internal.ui.javaeditor.CompilationUnitEditor;
 import org.eclipse.jdt.internal.ui.text.java.IJavaReconcilingListener;
 import org.eclipse.jdt.ui.text.IColorManager;
-
 import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.jface.util.ListenerList;
-
 import org.eclipse.swt.widgets.Composite;
 
 import ptolemy.backtrack.eclipse.plugin.EclipsePlugin;
@@ -47,31 +43,67 @@ import ptolemy.backtrack.eclipse.plugin.EclipsePlugin;
 //// PtolemyEditor
 
 /**
+   Editor with Ptolemy syntax highlighting. Ptolemy syntax highlighting is added
+   to Eclipse's Java syntax highlighting as an extension.
 
-
- @author Thomas Feng
- @version $Id$
- @since Ptolemy II 5.1
- @Pt.ProposedRating Red (tfeng)
- @Pt.AcceptedRating Red (tfeng)
- */
+   @author Thomas Feng
+   @version $Id$
+   @since Ptolemy II 5.1
+   @Pt.ProposedRating Red (tfeng)
+   @Pt.AcceptedRating Red (tfeng)
+*/
 public class PtolemyEditor extends CompilationUnitEditor {
+    
+    ///////////////////////////////////////////////////////////////////
+    ////                       public methods                      ////
+
+    /** Notify all the reconciling listeners before reconciliation is performed.
+     */
     public void aboutToBeReconciled() {
         super.aboutToBeReconciled();
 
-        Object[] listeners = fReconcilingListeners.getListeners();
+        Object[] listeners = _reconcilingListeners.getListeners();
 
         for (int i = 0, length = listeners.length; i < length; ++i) {
             ((IJavaReconcilingListener) listeners[i]).aboutToBeReconciled();
         }
     }
 
+    /** Add a reconciling listener to the list of reconciling listeners to be
+     *  notified.
+     *  
+     *  @param listener The reconciling listener to be added.
+     */
+    public void addJavaReconcileListener(IJavaReconcilingListener listener) {
+        synchronized (_reconcilingListeners) {
+            _reconcilingListeners.add(listener);
+        }
+    }
+
+    /** Create the controls for this editor, and install the syntax highlighting
+     *  handler.
+     * 
+     *  @param parent The parent of the editor.
+     */
+    public void createPartControl(Composite parent) {
+        super.createPartControl(parent);
+        _installSemanticHighlighting(JavaPlugin.getDefault().getJavaTextTools()
+                .getColorManager());
+    }
+
+    /** Notify all the reconsiling listeners after reconciliation is performed.
+     * 
+     *  @param ast The compilation unit of the Java source.
+     *  @param forced Whether the reconciliation is forced.
+     *  @param progressMonitor The progress monitor to handle the reconcilation
+     *   progress.
+     */
     public void reconciled(CompilationUnit ast, boolean forced,
             IProgressMonitor progressMonitor) {
         super.reconciled(ast, forced, progressMonitor);
 
         // Notify listeners
-        Object[] listeners = fReconcilingListeners.getListeners();
+        Object[] listeners = _reconcilingListeners.getListeners();
 
         for (int i = 0, length = listeners.length; i < length; ++i) {
             ((IJavaReconcilingListener) listeners[i]).reconciled(ast, forced,
@@ -79,28 +111,23 @@ public class PtolemyEditor extends CompilationUnitEditor {
         }
     }
 
-    public void addJavaReconcileListener(IJavaReconcilingListener listener) {
-        synchronized (fReconcilingListeners) {
-            fReconcilingListeners.add(listener);
-        }
-    }
-
+    /** Remove a reconciling listener from the list of reconciling listeners
+     *  to be notified.
+     *  @param listener The reconciling listener to be removed.
+     */
     public void removeJavaReconcileListener(IJavaReconcilingListener listener) {
-        synchronized (fReconcilingListeners) {
-            fReconcilingListeners.remove(listener);
+        synchronized (_reconcilingListeners) {
+            _reconcilingListeners.remove(listener);
         }
     }
 
-    public IJavaElement getInputJavaElement() {
-        return super.getInputJavaElement();
-    }
+    ///////////////////////////////////////////////////////////////////
+    ////                      protected methods                    ////
 
-    public void createPartControl(Composite parent) {
-        super.createPartControl(parent);
-        _installSemanticHighlighting(JavaPlugin.getDefault().getJavaTextTools()
-                .getColorManager());
-    }
-
+    /** Install the syntax highlighting handler.
+     * 
+     *  @param colorManager The color manager with the color settings.
+     */
     protected void _installSemanticHighlighting(IColorManager colorManager) {
         IPreferenceStore preferenceStore = EclipsePlugin.getDefault()
                 .getPreferenceStore();
@@ -109,7 +136,14 @@ public class PtolemyEditor extends CompilationUnitEditor {
         _reconciler.install(this, colorManager);
     }
 
+    ///////////////////////////////////////////////////////////////////
+    ////                       private fields                      ////
+
+    /** The reconciler.
+     */
     private SemanticHighlightingReconciler _reconciler;
 
-    private ListenerList fReconcilingListeners = new ListenerList();
+    /** The list of reconciling listeners to be notified.
+     */
+    private ListenerList _reconcilingListeners = new ListenerList();
 }
