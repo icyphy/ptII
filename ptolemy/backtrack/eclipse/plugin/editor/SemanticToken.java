1c71180ff18d5c658770558d5773badf4de580a5
tfeng <tfeng>
2006-04-29 22:08:46 +0000

    Documented.

--- ptolemy/backtrack/eclipse/plugin/editor/SemanticToken.java
+++ ptolemy/backtrack/eclipse/plugin/editor/SemanticToken.java
@@ -8,83 +8,130 @@
  * Contributors:
  *     IBM Corporation - initial API and implementation
  *******************************************************************************/
+
+/* Semantic token in the Java source.
+
+ Copyright (c) 2005 The Regents of the University of California.
+ All rights reserved.
+ Permission is hereby granted, without written agreement and without
+ license or royalty fees, to use, copy, modify, and distribute this
+ software and its documentation for any purpose, provided that the above
+ copyright notice and the following two paragraphs appear in all copies
+ of this software.
+
+ IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+ FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+ ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+ THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+ SUCH DAMAGE.
+
+ THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+ INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+ PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+ CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ ENHANCEMENTS, OR MODIFICATIONS.
+
+ PT_COPYRIGHT_VERSION_2
+ COPYRIGHTENDKEY
+
+ */
 package ptolemy.backtrack.eclipse.plugin.editor;
 
 import org.eclipse.jdt.core.dom.CompilationUnit;
 import org.eclipse.jdt.core.dom.IBinding;
 import org.eclipse.jdt.core.dom.SimpleName;
 
+//////////////////////////////////////////////////////////////////////////
+//// SemanticToken
 /**
- * Semantic token
- */
+   Semantic token in the Java source.
+
+   @author Thomas Feng
+   @version $Id$
+   @since Ptolemy II 5.1
+   @Pt.ProposedRating Red (tfeng)
+   @Pt.AcceptedRating Red (tfeng)
+*/
 public final class SemanticToken {
-    /** AST node */
-    private SimpleName fNode;
 
-    /** Binding */
-    private IBinding fBinding;
+    ///////////////////////////////////////////////////////////////////
+    ////                       public methods                      ////
 
-    /** Is the binding resolved? */
-    private boolean fIsBindingResolved;
-
-    /** AST root */
-    private CompilationUnit fRoot;
-
-    /**
-     * @return Returns the binding, can be <code>null</code>.
+    /** Return the binding, or null if undefined.
+     * 
+     *  @return The binding.
      */
     public IBinding getBinding() {
-        if (!fIsBindingResolved && (fNode != null)) {
-            fBinding = fNode.resolveBinding();
-            fIsBindingResolved = true;
+        if (!_isBindingResolved && (_node != null)) {
+            _binding = _node.resolveBinding();
+            _isBindingResolved = true;
         }
 
-        return fBinding;
+        return _binding;
     }
 
-    /**
-     * @return the AST node
+    /** Return the AST node of this token.
+     * 
+     *  @return The AST node.
      */
     public SimpleName getNode() {
-        return fNode;
+        return _node;
     }
 
-    /**
-     * @return the AST root
+    /** Return the AST root.
+     * 
+     *  @return The AST root.
      */
     public CompilationUnit getRoot() {
-        if (fRoot == null) {
-            fRoot = (CompilationUnit) fNode.getRoot();
+        if (_root == null) {
+            _root = (CompilationUnit) _node.getRoot();
         }
 
-        return fRoot;
+        return _root;
     }
 
-    /**
-     * Update this token with the given AST node.
-     * <p>
-     * NOTE: Allowed to be used by {@link SemanticHighlightingReconciler} only.
-     * </p>
+    ///////////////////////////////////////////////////////////////////
+    ////                      protected methods                    ////
+
+    /** Clear this token. This method should only be called by {@link
+     *  SemanticHighlightingReconciler}.
+     */
+    protected void _clear() {
+        _node = null;
+        _binding = null;
+        _isBindingResolved = false;
+        _root = null;
+    }
+
+    /** Update this token with the given AST node. This method should only be
+     *  called by {@link SemanticHighlightingReconciler}.
      *
-     * @param node the AST node
+     *  @param node The AST node.
      */
-    protected void update(SimpleName node) {
-        fNode = node;
-        fBinding = null;
-        fIsBindingResolved = false;
-        fRoot = null;
+    protected void _update(SimpleName node) {
+        _node = node;
+        _binding = null;
+        _isBindingResolved = false;
+        _root = null;
     }
 
-    /**
-     * Clears this token.
-     * <p>
-     * NOTE: Allowed to be used by {@link SemanticHighlightingReconciler} only.
-     * </p>
+    ///////////////////////////////////////////////////////////////////
+    ////                       private fields                      ////
+
+    /** The binding.
      */
-    protected void clear() {
-        fNode = null;
-        fBinding = null;
-        fIsBindingResolved = false;
-        fRoot = null;
-    }
+    private IBinding _binding;
+
+    /** Whether the binding resolved.
+     */
+    private boolean _isBindingResolved;
+
+    /** The AST node of this token.
+     */
+    private SimpleName _node;
+
+    /** The AST root.
+     */
+    private CompilationUnit _root;
 }
