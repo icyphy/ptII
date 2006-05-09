048078f9829e2636a08d5197b529fb8eb12a969f
cxh <cxh>
2006-05-09 21:44:31 +0000

    Fixed imports

--- ptolemy/codegen/kernel/CodeStream.java
+++ ptolemy/codegen/kernel/CodeStream.java
@@ -29,8 +29,8 @@
 package ptolemy.codegen.kernel;
 
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Hashtable;
