b66fc812c9118637b6517f8d31791629f354d16d
cxh <cxh>
2006-05-12 04:25:46 +0000

    Support for DocBuilder

---
+++ ptolemy/vergil/actor/DocBuilder.java
@@ -0,0 +1,183 @@
+/* Build Documentation for Java and Actors
+
+ Copyright (c) 2006 The Regents of the University of California.
+ All rights reserved.
+ Permission is hereby granted, without written agreement and without
+ license or royalty fees, to use, copy, modify, and distribute this
+ software and its documentation for any purpose, provided that the above
+ copyright notice and the following two paragraphs appear in all copies
+ of this software.
+
+ IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+ FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+ ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+ THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+ SUCH DAMAGE.
+
+ THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+ INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+ PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+ CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ ENHANCEMENTS, OR MODIFICATIONS.
+
+ PT_COPYRIGHT_VERSION_2
+ COPYRIGHTENDKEY
+
+ */
+package ptolemy.vergil.actor;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.Writer;
+import java.lang.reflect.Constructor;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import ptolemy.actor.CompositeActor;
+import ptolemy.codegen.c.actor.TypedCompositeActor;
+import ptolemy.codegen.gui.CodeGeneratorGUIFactory;
+import ptolemy.data.BooleanToken;
+import ptolemy.data.IntToken;
+import ptolemy.data.expr.FileParameter;
+import ptolemy.data.expr.Parameter;
+import ptolemy.data.expr.StringParameter;
+import ptolemy.data.expr.Variable;
+import ptolemy.data.type.BaseType;
+import ptolemy.kernel.CompositeEntity;
+import ptolemy.kernel.util.Attribute;
+import ptolemy.kernel.util.IllegalActionException;
+import ptolemy.kernel.util.InternalErrorException;
+import ptolemy.kernel.util.KernelException;
+import ptolemy.kernel.util.NameDuplicationException;
+import ptolemy.kernel.util.NamedObj;
+import ptolemy.moml.MoMLParser;
+import ptolemy.moml.filter.BackwardCompatibility;
+import ptolemy.moml.filter.RemoveGraphicalClasses;
+import ptolemy.util.ExecuteCommands;
+import ptolemy.util.FileUtilities;
+import ptolemy.util.MessageHandler;
+import ptolemy.util.StreamExec;
+import ptolemy.util.StringUtilities;
+
+//////////////////////////////////////////////////////////////////////////
+//// DocBuilder
+
+/** Build Documentation for Java and Actors.
+ *
+ *  <p>This class sets the commands that build the Java classes.
+ *
+ *  @author Christopher Brooks
+ *  @version $Id$
+ *  @since Ptolemy II 6.0
+ *  @Pt.ProposedRating Yellow (eal)
+ *  @Pt.AcceptedRating Yellow (eal)
+ */
+public class DocBuilder extends Attribute {
+    /** Create a new instance of the DocBuilder.
+     *  @param container The container.
+     *  @param name The name of the code generator.
+     *  @exception IllegalActionException If the super class throws the
+     *   exception or error occurs when setting the file path.
+     *  @exception NameDuplicationException If the super class throws the
+     *   exception or an error occurs when setting the file path.
+     */
+    public DocBuilder(NamedObj container, String name)
+            throws IllegalActionException, NameDuplicationException {
+        super(container, name);
+    }
+
+    ///////////////////////////////////////////////////////////////////
+    ////                         public methods                    ////
+
+    
+    /** Build the Java class and Actor documentation.
+     *  @return The return value of the last subprocess that was executed.
+     *  or -1 if no commands were executed.     
+     *  @exception IllegalActionException If there is a problem building
+     *  the documentation.
+     */
+    public int buildDocs() throws IllegalActionException {
+        if (_executeCommands == null) {
+            _executeCommands = new StreamExec();
+        }
+        return _executeCommands();
+    }
+
+    /** Get the command executor, which can be either non-graphical
+     *  or graphical.  The initial default is non-graphical, which
+     *  means that stderr and stdout from subcommands is written
+     *  to the console.
+     *  @return executeCommands The subprocess command executor.
+     *  @see #setExecuteCommands(ExecuteCommands)
+     */
+    public ExecuteCommands getExecuteCommands() {
+        return _executeCommands;
+    }
+
+    /** Set the command executor, which can be either non-graphical
+     *  or graphical.  The initial default is non-graphical, which
+     *  means that stderr and stdout from subcommands is written
+     *  to the console.
+     *  @param executeCommands The subprocess command executor.
+     *  @see #getExecuteCommands()
+     */
+    public void setExecuteCommands(ExecuteCommands executeCommands) {
+        _executeCommands = executeCommands;
+    }
+
+
+    ///////////////////////////////////////////////////////////////////
+    ////                         private methods                   ////
+
+    /** Execute the compile and run commands in the
+     *  <i>codeDirectory</i> directory.
+     *  @return The return value of the last subprocess that was executed
+     *  or -1 if no commands were executed.
+     */
+    private int _executeCommands() throws IllegalActionException {
+
+        List commands = new LinkedList();
+        commands.add("make ");
+
+        if (commands.size() == 0) {
+            return -1;
+        }
+
+        _executeCommands.setCommands(commands);
+        File ptII = new File(StringUtilities.getProperty("ptolemy.ptII.dir")
+                + "/doc");
+        _executeCommands.setWorkingDirectory(ptII);
+
+        try {
+            // FIXME: need to put this output in to the UI, if any. 
+            _executeCommands.start();
+        } catch (Exception ex) {
+            StringBuffer errorMessage = new StringBuffer();
+            Iterator allCommands = commands.iterator();
+            while (allCommands.hasNext()) {
+                errorMessage.append((String) allCommands.next() + "\n");
+            }
+            throw new IllegalActionException("Problem executing the "
+                    + "commands:\n" + errorMessage);
+        }
+        return _executeCommands.getLastSubprocessReturnCode();
+    }
+
+    
+    ///////////////////////////////////////////////////////////////////
+    ////                         private variables                 ////
+
+    /** The object that actually executes the commands.
+     */   
+    private ExecuteCommands _executeCommands;
+}
---
+++ ptolemy/vergil/actor/DocBuilderGUI.java
@@ -0,0 +1,201 @@
+/* A GUI that builds the Java and Actor documentation.
+
+ Copyright (c) 2006 The Regents of the University of California.
+ All rights reserved.
+ Permission is hereby granted, without written agreement and without
+ license or royalty fees, to use, copy, modify, and distribute this
+ software and its documentation for any purpose, provided that the above
+ copyright notice and the following two paragraphs appear in all copies
+ of this software.
+
+ IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+ FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+ ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+ THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+ SUCH DAMAGE.
+
+ THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+ INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+ PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+ CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ ENHANCEMENTS, OR MODIFICATIONS.
+
+ PT_COPYRIGHT_VERSION_2
+ COPYRIGHTENDKEY
+
+ */
+package ptolemy.vergil.actor;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.net.URL;
+
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JSplitPane;
+import javax.swing.JTextArea;
+
+import ptolemy.actor.gui.Configuration;
+import ptolemy.actor.gui.Configurer;
+import ptolemy.actor.gui.PtolemyFrame;
+import ptolemy.actor.gui.Tableau;
+import ptolemy.gui.JTextAreaExec;
+import ptolemy.kernel.util.IllegalActionException;
+import ptolemy.kernel.util.InternalErrorException;
+import ptolemy.kernel.util.NameDuplicationException;
+import ptolemy.util.MessageHandler;
+
+//////////////////////////////////////////////////////////////////////////
+//// DocBuilderGUI
+
+/**
+  A PtolemyFrame that builds the Java and Actor documentation.
+
+ @author Christopher Brooks
+ @version $Id$
+ @since Ptolemy II 6.0
+ @Pt.ProposedRating Green (cxh)
+ @Pt.AcceptedRating Red (eal)
+ */
+public class DocBuilderGUI extends PtolemyFrame {
+
+    /** Construct a frame to build the Java and Actor documentation.
+     *  After constructing this, it is necessary to
+     *  call setVisible(true) to make the frame appear.  This is
+     *  typically accomplished by calling show() on enclosing tableau.
+     *
+     *  @param tableau The tableau responsible for this frame.
+     *  @exception IllegalActionException If the model rejects the
+     *   configuration attribute.
+     *  @exception NameDuplicationException If a name collision occurs.
+     */
+    public DocBuilderGUI(final DocBuilder docBuilder, Tableau tableau)
+            throws IllegalActionException, NameDuplicationException {
+        super(docBuilder, tableau);
+
+        setTitle("Ptolemy II Java and Actor Documentation Builder");
+
+        if ((getEffigy() == null)) {
+            throw new InternalErrorException("Cannot get an effigy!");
+        }
+
+        // Caveats panel.
+        JPanel caveatsPanel = new JPanel();
+        caveatsPanel.setBorder(BorderFactory.createEmptyBorder(5, 0, 0, 0));
+        caveatsPanel.setLayout(new BoxLayout(caveatsPanel, BoxLayout.X_AXIS));
+
+        JTextArea messageArea = new JTextArea(
+                "NOTE: Use this tool to build the Java"
+                + " and Actor Documentation");
+        messageArea.setEditable(false);
+        messageArea.setBorder(BorderFactory.createEtchedBorder());
+        messageArea.setLineWrap(true);
+        messageArea.setWrapStyleWord(true);
+        caveatsPanel.add(messageArea);
+
+        JButton moreInfoButton = new JButton("More Info");
+        moreInfoButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+                String infoFile = "ptolemy/vergil/actor/docViewerHelp.htm";
+                try {
+                    Configuration configuration = getConfiguration();
+
+                    // FIXME: Help should bring this up as well.
+                    URL infoURL = Thread.currentThread()
+                        .getContextClassLoader().getResource(infoFile);
+                    configuration.openModel(null, infoURL, infoURL
+                            .toExternalForm());
+                } catch (Exception ex) {
+                    throw new InternalErrorException(docBuilder, ex,
+                            "Failed to open " + infoFile);
+                }
+            }
+        });
+        caveatsPanel.add(moreInfoButton);
+
+        JPanel left = new JPanel();
+        left.setLayout(new BoxLayout(left, BoxLayout.Y_AXIS));
+        caveatsPanel.setMaximumSize(new Dimension(500, 100));
+        left.add(caveatsPanel);
+
+        // Panel for push buttons.
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setLayout(new GridLayout(1, 4));
+
+        JButton goButton = new JButton("Generate");
+        goButton.setToolTipText("Generate documentation");
+        buttonPanel.add(goButton);
+
+        JButton stopButton = new JButton("Cancel");
+        stopButton.setToolTipText("Terminate executing processes");
+        buttonPanel.add(stopButton);
+
+        JButton clearButton = new JButton("Clear");
+        clearButton.setToolTipText("Clear Log");
+        buttonPanel.add(clearButton);
+
+        buttonPanel.setMaximumSize(new Dimension(500, 50));
+        left.add(buttonPanel);
+
+        Configurer configurer = new Configurer(docBuilder);
+        JPanel controlPanel = new JPanel();
+        controlPanel.add(configurer);
+
+        JScrollPane scrollPane = new JScrollPane(controlPanel);
+
+        left.add(scrollPane, BorderLayout.CENTER);
+
+        // Create a JTextAreaExec without Start and Cancel buttons.
+        final JTextAreaExec exec = new JTextAreaExec("Documentation Builder"
+                + "Commands",
+                false);
+
+        // If we execute any commands, print the output in the text area.
+        docBuilder.setExecuteCommands(exec);
+
+        JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT,
+                left, exec);
+        splitPane.setOneTouchExpandable(true);
+
+        // Adjust the divider so that the control panel does not
+        // have a horizontal scrollbar.
+        Dimension preferred = left.getPreferredSize();
+        splitPane.setDividerLocation(preferred.width + 20);
+
+        getContentPane().add(splitPane, BorderLayout.CENTER);
+
+        stopButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+                exec.cancel();
+            }
+        });
+
+        clearButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+                exec.clear();
+            }
+        });
+
+        goButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+                try {
+                    exec.updateStatusBar("// Starting Doc Building");
+
+                    docBuilder.buildDocs();
+
+                    exec.updateStatusBar("// Doc Building complete.");
+                } catch (Exception ex) {
+                    MessageHandler.error("Doc Building failed.", ex);
+                }
+            }
+        });
+    }
+}
