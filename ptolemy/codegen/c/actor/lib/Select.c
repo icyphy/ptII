90f9b19f12feeef0bb25b5fba6fe8869f2273ed2
cxh <cxh>
2006-05-16 23:38:38 +0000

    Cannot implement Select because we the variable channel syntax to work

--- ptolemy/codegen/c/actor/lib/Select.c
+++
@@ -1,7 +0,0 @@
-/*** fireBlock ***/
-    if (($ref(select) >= 0) && ($ref(select) < $size(input))) {
-    	// FIXME: variable channel index.
-        $ref(output) = $ref(input#$ref(select));
-    }
-/**/
-
