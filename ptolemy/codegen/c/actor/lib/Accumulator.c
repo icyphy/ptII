f40da5c509c4c8243d0d7f90c3213b630ff611f4
cxh <cxh>
2006-05-17 23:04:08 +0000

    Reindented with cindent

--- ptolemy/codegen/c/actor/lib/Accumulator.c
+++ ptolemy/codegen/c/actor/lib/Accumulator.c
@@ -7,50 +7,50 @@ static boolean $actorSymbol(resetTemp);
 /**/
 
 /***InitSum***/
-    $actorSymbol(sum) = $val(($cgType(output)) init);
+$actorSymbol(sum) = $val(($cgType(output)) init);
 /**/
 
 /***StringInitSum***/
-    $actorSymbol(sum) = strdup($val(($cgType(output)) init));
+$actorSymbol(sum) = strdup($val(($cgType(output)) init));
 /**/
 
 /***IntFireBlock($channel) ***/
-    $actorSymbol(sum) += $ref(($cgType(output)) input#$channel);
+$actorSymbol(sum) += $ref(($cgType(output)) input#$channel);
 /**/
 
 /***DoubleFireBlock($channel) ***/
-    $actorSymbol(sum) += $ref(($cgType(output)) input#$channel);
+$actorSymbol(sum) += $ref(($cgType(output)) input#$channel);
 /**/
 
 /***BooleanFireBlock($channel) ***/
-    $actorSymbol(sum) |= $ref(($cgType(output)) input#$channel);
+$actorSymbol(sum) |= $ref(($cgType(output)) input#$channel);
 /**/
 
 /***StringFireBlock($channel)***/
-    $actorSymbol(sum) = (char*) realloc($actorSymbol(sum), sizeof(char) * (strlen($actorSymbol(sum)) + strlen($ref(($cgType(output)) input#$channel)) + 1) );
-	strcat($actorSymbol(sum),  $ref(($cgType(output)) input#$channel));
+$actorSymbol(sum) = (char*) realloc($actorSymbol(sum), sizeof(char) * (strlen($actorSymbol(sum)) + strlen($ref(($cgType(output)) input#$channel)) + 1) );
+strcat($actorSymbol(sum),  $ref(($cgType(output)) input#$channel));
 /**/
 
 /***TokenFireBlock($channel)***/
-	$actorSymbol(sum) = $tokenFunc($actorSymbol(sum)::add($ref(($cgType(output)) input#$channel)));
+$actorSymbol(sum) = $tokenFunc($actorSymbol(sum)::add($ref(($cgType(output)) input#$channel)));
 /**/
 
 /***sendBlock***/
-    $ref(output) = $actorSymbol(sum);
+$ref(output) = $actorSymbol(sum);
 /**/
 
 /***initReset***/
-    $actorSymbol(resetTemp) = $ref(reset#0);
+$actorSymbol(resetTemp) = $ref(reset#0);
 /**/
 
 /***readReset($channel)***/
-    $actorSymbol(resetTemp) |= $ref(reset#$channel);
+$actorSymbol(resetTemp) |= $ref(reset#$channel);
 /**/
 
 /***ifReset***/
-    if ($actorSymbol(resetTemp))
-/**/
+if ($actorSymbol(resetTemp))
+        /**/
 
-/***StringWrapupBlock***/
-    free($actorSymbol(sum));
+        /***StringWrapupBlock***/
+        free($actorSymbol(sum));
 /**/
