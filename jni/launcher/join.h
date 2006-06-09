bac99e9fa5c093e107bf77f2f3e297d4534ea006
cxh <cxh>
2006-06-09 22:03:59 +0000

    From http://software.jessies.org/terminator/

---
+++ jni/launcher/join.h
@@ -0,0 +1,22 @@
+#ifndef JOIN_H_included
+#define JOIN_H_included
+
+template <class ValueInitializer, class Container>
+inline typename Container::value_type join(const ValueInitializer& separatorInitializer, const Container& container) {
+  typename Container::value_type separator(separatorInitializer);
+  typename Container::value_type joined;
+  if (container.empty()) {
+    return joined;
+  }
+  typename Container::const_iterator it = container.begin();
+  joined = *it;
+  ++ it;
+  while (it != container.end()) {
+    joined.insert(joined.end(), separator.begin(), separator.end());
+    joined.insert(joined.end(), it->begin(), it->end());
+    ++ it;
+  }
+  return joined;
+}
+
+#endif
---
+++ jni/launcher/synchronizeWindowsEnvironment.h
@@ -0,0 +1,6 @@
+#ifndef synchronizeWindowsEnvironment_h_
+#define synchronizeWindowsEnvironment_h_
+
+void synchronizeWindowsEnvironment();
+
+#endif // synchronizeWindowsEnvironment_h_
