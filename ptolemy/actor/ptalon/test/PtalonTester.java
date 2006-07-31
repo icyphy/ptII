e783922bb66452b0f9807fca4bc6e53573252781
acataldo <acataldo>
2006-07-31 05:15:38 +0000

    This is the start of the Ptalon test suite.  Later this should get hooked into the jacl test suite.

---
+++ ptolemy/actor/ptalon/test/PtalonTester.java
@@ -0,0 +1,35 @@
+package ptolemy.actor.ptalon.test;
+
+import java.util.*;
+import java.io.*;
+import antlr.collections.*;
+import ptolemy.actor.ptalon.*;
+
+
+public class PtalonTester {
+
+    /**
+     * Test the PtalonFiles to see what they parse.
+     * @param args 
+     */
+    public static void main(String[] args) {
+        try {
+            for (String arg : Arrays.asList(args)) {
+                File file = new File(arg);
+                FileReader reader = new FileReader(file);
+                PtalonLexer lexer = new PtalonLexer(reader);
+                PtalonRecognizer rec = new PtalonRecognizer(lexer);
+                rec.actor_definition();
+                AST ast = rec.getAST();
+                PtalonScopeChecker check = new PtalonScopeChecker();
+                check.actor_definition(ast);
+                PtalonCompilerInfo info = check.getCompilerInfo();
+                System.out.println(info);
+            }
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
