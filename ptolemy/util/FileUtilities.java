7481db8ac55422d6e4faadf9750faabc407c7737
cxh <cxh>
2006-09-25 22:59:17 +0000

    Added extractJarFile() for use with the installer

--- ptolemy/util/FileUtilities.java
+++ ptolemy/util/FileUtilities.java
@@ -34,11 +34,15 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.io.Writer;
 import java.net.URI;
 import java.net.URL;
+import java.util.Enumeration;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
 
 // Avoid importing any packages from ptolemy.* here so that we
 // can ship Ptplot.
@@ -104,43 +108,66 @@ public class FileUtilities {
             }
         }
 
-        // Copy the source file
-        BufferedInputStream input = null;
-
-        try {
-            input = new BufferedInputStream(sourceURL.openStream());
-
-            BufferedOutputStream output = null;
-
-            try {
-                output = new BufferedOutputStream(new FileOutputStream(
-                        destinationFile));
-
-                int c;
-
-                while ((c = input.read()) != -1) {
-                    output.write(c);
-                }
-            } finally {
-                if (output != null) {
-                    try {
-                        output.close();
-                    } catch (Throwable throwable) {
-                        throw new RuntimeException(throwable);
-                    }
-                }
-            }
-        } finally {
-            if (input != null) {
-                try {
-                    input.close();
-                } catch (Throwable throwable) {
-                    throw new RuntimeException(throwable);
-                }
-            }
-        }
+        _binaryCopyStream(sourceURL.openStream(), destinationFile);
 
         return true;
+    }
+
+    /** Extract a jar file into a directory.  This is a trivial
+     *  implementation of the <code>jar -xf</code> command.   
+     *  @param jarFileName The name of the jar file to extract
+     *  @param directoryName The name of the directory.  If this argument
+     *  is null, then the files are extracted in the current directory.
+     */
+    public static void extractJarFile(String jarFileName,
+            String directoryName) throws IOException, SecurityException {
+
+        JarFile jarFile = new JarFile(jarFileName);
+        Enumeration entries = jarFile.entries();
+        while (entries.hasMoreElements()) {
+            JarEntry jarEntry = (JarEntry) entries.nextElement();
+            File destinationFile = new File(directoryName, jarEntry.getName());
+            if (jarEntry.isDirectory()) {
+                if (!destinationFile.isDirectory()
+                        && !destinationFile.mkdirs()) {
+                    throw new IOException("Warning, failed to create "
+                            + "directory for \"" + destinationFile + "\".");
+                }
+            } else {
+                _binaryCopyStream(jarFile.getInputStream(jarEntry),
+                        destinationFile);
+            }
+        }
+    }
+
+    /** Extract the contents of a jar file.
+     *  @param args An array of arguments.  The first argument
+     *  names the jar file to be extracted.  The first argument
+     *  is required.  The second argument names the directory in
+     *  which to extract the files from the jar file.  The second
+     *  argument is optional.
+     */
+    public static void main(String [] args) {
+        if (args.length < 1 || args.length > 2) {
+            System.err.println("Usage: java -classpath $PTII "
+                    + "ptolemy.util.FileUtilities jarFile [directory]\n"
+                    + "where jarFile is the name of the jar file\n"
+                    + "and directory is the optional directory in which to "
+                    + "extract.");
+            StringUtilities.exit(2);
+        }
+        String jarFileName = args[0];
+        String directoryName = null;
+        if ( args.length >=2) {
+            directoryName = args[1];
+        }
+        try {
+            extractJarFile(jarFileName, directoryName);
+        } catch (Throwable throwable) {
+            System.err.println("Failed to extract \"" + jarFileName + "\"");
+            throwable.printStackTrace();
+            StringUtilities.exit(3);
+        }
     }
 
     /** Given a file name or URL, construct a java.io.File object that
@@ -498,6 +525,64 @@ public class FileUtilities {
      */
     public static PrintWriter STD_OUT = null;
 
+    ///////////////////////////////////////////////////////////////////
+    ////                         private methods                   ////
+
+    /** Copy files safely.  If there are problems, the streams are
+     *  close appropriately.
+     *  @param inputStream The input stream.
+     *  @param destinationFile The destination File. 
+     *  @exception IOException If the input stream cannot be created
+     *  or read, or * if there is a problem writing to the destination
+     *  file.
+     */
+    private static void _binaryCopyStream(InputStream inputStream,
+            File destinationFile) throws IOException {
+        // Copy the source file. 
+        BufferedInputStream input = null;
+
+        try {
+            input = new BufferedInputStream(inputStream);
+            
+            BufferedOutputStream output = null;
+
+            try {
+                File parent = destinationFile.getParentFile();
+                if (!parent.exists()) {
+                    if (!parent.mkdirs()) {
+                        throw new IOException("Failed to create directories "
+                                + "for \"" + parent + "\".");
+                    }
+                }
+
+                output = new BufferedOutputStream(new FileOutputStream(
+                        destinationFile));
+
+                int c;
+
+                while ((c = input.read()) != -1) {
+                    output.write(c);
+                }
+            } finally {
+                if (output != null) {
+                    try {
+                        output.close();
+                    } catch (Throwable throwable) {
+                        throw new RuntimeException(throwable);
+                    }
+                }
+            }
+        } finally {
+            if (input != null) {
+                try {
+                    input.close();
+                } catch (Throwable throwable) {
+                    throw new RuntimeException(throwable);
+                }
+            }
+        }
+    }
+    
     ///////////////////////////////////////////////////////////////////
     ////                         private members                   ////
 
