495b6f6c4ebddfbd2e42fe60b410dbe5eeec9fe7
mankit <mankit>
2006-10-23 23:55:54 +0000

    created vhdl code generation framework

---
+++ ptolemy/codegen/vhdl/actor/lib/Concat.vhdl
@@ -0,0 +1,7 @@
+/*** preinitBlock ***/
+	This is the preinit block. 	
+/**/
+
+/*** fireBlock ***/
+	This is the fire block. 	
+/**/
---
+++ ptolemy/codegen/vhdl/kernel/VHDLCodeGeneratorHelper.java
@@ -0,0 +1,90 @@
+/* Base class for VHDL code generator helper.
+
+ Copyright (c) 2005-2006 The Regents of the University of California.
+ All rights reserved.
+ Permission is hereby granted, without written agreement and without
+ license or royalty fees, to use, copy, modify, and distribute this
+ software and its documentation for any purpose, provided that the above
+ copyright notice and the following two paragraphs appear in all copies
+ of this software.
+
+ IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+ FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+ ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+ THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+ SUCH DAMAGE.
+
+ THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+ INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+ PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+ CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ ENHANCEMENTS, OR MODIFICATIONS.
+
+ PT_COPYRIGHT_VERSION_2
+ COPYRIGHTENDKEY
+
+ */
+package ptolemy.codegen.vhdl.kernel;
+
+import ptolemy.actor.CompositeActor;
+import ptolemy.codegen.kernel.CodeGeneratorHelper;
+import ptolemy.codegen.kernel.ParseTreeCodeGenerator;
+import ptolemy.kernel.util.IllegalActionException;
+import ptolemy.kernel.util.NamedObj;
+
+//////////////////////////////////////////////////////////////////////////
+//// VHDLCodeGeneratorHelper
+
+/**
+ Base class for C code generator helper. 
+
+ <p>Actor helpers extend this class and optionally define
+ generateFireCode(), generateInitializeCode(), generatePreinitializeCode(),
+ and generateWrapupCode() methods.  In derived classes, these methods,
+ if present, make actor specific changes to the corresponding code.
+ If these methods are not present, then the parent class will automatically
+ read the corresponding .c file and subsitute in the corresponding code
+ block.  For example, generateInitializeCode() reads the
+ <code>initBlock</code>, processes the macros and adds the resulting
+ code block to the output.
+
+ <p>For a complete list of methods to define, see 
+ {@link ptolemy.codegen.kernel.CodeGeneratorHelper}.
+
+ <p>For further details, see <code>$PTII/ptolemy/codegen/README.html</code>
+
+ @author Christopher Brooks, Edward Lee, Jackie Leung, Gang Zhou, Ye Zhou
+ @version $Id$
+ @since Ptolemy II 6.0
+ o @Pt.ProposedRating Yellow (cxh)
+ @Pt.AcceptedRating Red (cxh)
+ */
+public class VHDLCodeGeneratorHelper extends CodeGeneratorHelper {
+    /**
+     * Create a new instance of the C code generator helper.
+     * @param component The actor object for this helper.
+     */
+    public VHDLCodeGeneratorHelper(NamedObj component) {
+        super(component);
+    }
+
+    ///////////////////////////////////////////////////////////////////
+    ////                         public methods                    ////
+
+    /**
+     * Generate the fire code. In this base class, add the name of the
+     * associated component in the comment. Subclasses may extend this
+     * method to generate the fire code of the associated component.
+     * @return The generated code.
+     * @exception IllegalActionException Not thrown in this base class.
+     */
+    public String generateFireCode() throws IllegalActionException {
+        super.generateFireCode();
+        
+        // check the latency of the actor and register the output.
+        _codeStream.append("??? Latency code ???");
+        return processCode(_codeStream.toString());
+    }
+
+}
