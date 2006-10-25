c05a9731777ef5f57bf3a59a2b2122f7937f672d
tfiliba <tfiliba>
2006-10-25 21:58:10 +0000

    Initial version of Multiplexor (no latency yet) and modified makefile to include this.  Also, added all the actors inthe VHDL directory to vhdl.xml.

---
+++ ptolemy/apps/vhdl/Multiplexor.java
@@ -0,0 +1,141 @@
+/** An actor that slices the input bits and output a consecutive subset
+  of the input bits. 
+
+ Copyright (c) 1998-2006 The Regents of the University of California.
+ All rights reserved.
+ Permission is hereby granted, without written agreement and without
+ license or royalty fees, to use, copy, modify, and distribute this
+ software and its documentation for any purpose, provided that the above
+ copyright notice and the following two paragraphs appear in all copies
+ of this software.
+
+ IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+ FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+ ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+ THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+ SUCH DAMAGE.
+
+ THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+ INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+ PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+ CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ ENHANCEMENTS, OR MODIFICATIONS.
+
+ PT_COPYRIGHT_VERSION_2
+ COPYRIGHTENDKEY
+
+ */
+package ptolemy.apps.vhdl;
+
+import ptolemy.actor.lib.Transformer;
+import ptolemy.actor.TypedIOPort;
+import ptolemy.data.IntToken;
+import ptolemy.data.Token;
+import ptolemy.data.type.BaseType;
+import ptolemy.kernel.CompositeEntity;
+import ptolemy.kernel.util.Attribute;
+import ptolemy.kernel.util.IllegalActionException;
+import ptolemy.kernel.util.NameDuplicationException;
+
+//////////////////////////////////////////////////////////////////////////
+//// Slice
+
+/**
+ Produce an output token on each firing with a FixPoint value that is
+ equal to the slicing of the bits of the input token value. The bit width of
+ the output token value is determined by taking the difference of parameters
+ start and end. The width parameter specifies the bit width of the input 
+ value. The output FixPoint value is unsigned, and all its bits are integer
+ bits. The input can have any scalar type. 
+
+ @author Man-Kit Leung
+ @version $Id$
+ @since Ptolemy II 6.0
+ @Pt.ProposedRating Red (mankit)
+ @Pt.AcceptedRating Red (mankit)
+ */
+public class Multiplexor extends Transformer {
+    /** Construct an actor with the given container and name.
+     *  @param container The container.
+     *  @param name The name of this actor.
+     *  @exception IllegalActionException If the actor cannot be contained
+     *   by the proposed container.
+     *  @exception NameDuplicationException If the container already has an
+     *   actor with this name.
+     */
+    public Multiplexor(CompositeEntity container, String name)
+            throws NameDuplicationException, IllegalActionException {
+        super(container, name);
+        
+        input.setTypeEquals(BaseType.FIX);
+        input.setMultiport(true);
+        
+        output.setTypeEquals(BaseType.FIX);
+        
+        select = new TypedIOPort(this,"select",true,false);
+        select.setTypeEquals(BaseType.INT);
+    }
+
+    ///////////////////////////////////////////////////////////////////
+    ////                     ports and parameters                  ////
+    
+    public TypedIOPort select;
+
+    ///////////////////////////////////////////////////////////////////
+    ////                         public methods                    ////
+
+    /** Override the base class to determine which function is being
+     *  specified.
+     *  @param attribute The attribute that changed.
+     * @throws IllegalActionException 
+     *  @exception IllegalActionException If the function is not recognized.
+     */
+    public void attributeChanged (Attribute attribute) 
+            throws IllegalActionException {
+        super.attributeChanged(attribute);
+    }
+
+    
+    /** output a consecutive subset of the input bits. 
+     *  If there is no input, then produce no output.
+     *  @exception IllegalActionException If there is no director.
+     */
+    public void fire() throws IllegalActionException {
+        super.fire();
+        if (select.hasToken(0)) {
+            _channel = ((IntToken) select.get(0)).intValue();
+        }
+
+        boolean inRange = false;
+
+        for (int i = 0; i < input.getWidth(); i++) {
+            inRange = inRange || (i == _channel);
+
+            if (input.hasToken(i)) {
+                Token token = input.get(i);
+
+                if (i == _channel) {
+                    output.send(0, token);
+                }
+            }
+        }
+
+        if (!inRange) {
+            throw new IllegalActionException(this,
+                    "Select input is out of range: " + _channel + ".");
+        }
+    }
+    
+    /** Initialize to the default, which is to use channel zero. */
+    public void initialize() {
+        _channel = 0;
+    }
+
+    ///////////////////////////////////////////////////////////////////
+    ////                         private variables                 ////
+
+    /** The most recently read select input. */
+    private int _channel = 0;
+    
+}
