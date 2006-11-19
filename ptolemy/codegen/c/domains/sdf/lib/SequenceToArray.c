8151620a709fac62b6e35eeac0685dd37e8633db
cxh <cxh>
2006-11-19 18:09:03 +0000

    Creating a separate array works here.

--- ptolemy/codegen/c/domains/sdf/lib/SequenceToArray.c
+++ ptolemy/codegen/c/domains/sdf/lib/SequenceToArray.c
@@ -6,6 +6,7 @@ Token $actorSymbol(valueArray);
 /*** fireBlock ***/
 $actorSymbol(valueArray) = $new(Array($val(arrayLength), 0));
 for ($actorSymbol(i) = 0; $actorSymbol(i) < $val(arrayLength); $actorSymbol(i)++) {
-    Array_set($actorSymbol(valueArray),$actorSymbol(i), $ref((Token)input));
+    Array_set($actorSymbol(valueArray),$actorSymbol(i), $ref((Token)input, $actorSymbol(i)));
 }	
+$ref(output) = $actorSymbol(valueArray);
 /**/
