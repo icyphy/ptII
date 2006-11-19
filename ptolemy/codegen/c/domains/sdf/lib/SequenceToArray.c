a039cc4bf4c02c28e18e8eca683efa8e3b4826ea
cxh <cxh>
2006-11-19 17:59:18 +0000

    Set valueArray

--- ptolemy/codegen/c/domains/sdf/lib/SequenceToArray.c
+++ ptolemy/codegen/c/domains/sdf/lib/SequenceToArray.c
@@ -1,9 +1,11 @@
 /*** preinitBlock ***/
 int $actorSymbol(i);
+Token $actorSymbol(valueArray);
 /**/
 
 /*** fireBlock ***/
+$actorSymbol(valueArray) = $new(Array($val(arrayLength), 0));
 for ($actorSymbol(i) = 0; $actorSymbol(i) < $val(arrayLength); $actorSymbol(i)++) {
-    Array_set($ref(output),$actorSymbol(i), $ref((Token)input));
+    Array_set($actorSymbol(valueArray),$actorSymbol(i), $ref((Token)input));
 }	
 /**/
