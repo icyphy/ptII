103fabcc7a183471da0a107311aea7423e8c6edf
cxh <cxh>
2006-11-19 16:50:57 +0000

    SequenceToArray

---
+++ ptolemy/codegen/c/domains/sdf/lib/SequenceToArray.c
@@ -0,0 +1,9 @@
+/*** preinitBlock ***/
+int $actorSymbol(i);
+/**/
+
+/*** fireBlock ***/
+for ($actorSymbol(i) = 0; $actorSymbol(i) < $val(arrayLength); $actorSymbol(i)++) {
+    Array_set($ref(output),$actorSymbol(i), $ref((Token)input));
+}	
+/**/
