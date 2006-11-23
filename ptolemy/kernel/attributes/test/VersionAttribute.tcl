0d2d8392ba2305e30863cf42e3f93279e2d86795
cxh <cxh>
2006-11-23 00:38:33 +0000

    Updated to 6.1.devel

--- ptolemy/kernel/attributes/test/VersionAttribute.tcl
+++ ptolemy/kernel/attributes/test/VersionAttribute.tcl
@@ -55,7 +55,7 @@ test VersionAttribute-1.0 {Constructor} {
     set result2 [$v toString]
     set result3 [$v getExpression]
     list $result1 $result2 $result3
-} {{ptolemy.kernel.attributes.VersionAttribute {.my NamedObj.my Version}} {ptolemy.kernel.attributes.VersionAttribute {.my NamedObj.my Version}} 6.0.beta}
+} {{ptolemy.kernel.attributes.VersionAttribute {.my NamedObj.my Version}} {ptolemy.kernel.attributes.VersionAttribute {.my NamedObj.my Version}} 6.1.devel}
 
 
 test VersionAttribute-2.0 {compareTo} {
@@ -90,7 +90,7 @@ test VersionAttribute-2.0 {compareTo} {
 		[$CURRENT_VERSION compareTo $v]]
     }
     list $results
-} {{{1.0 6.0.beta -1 1} {1.0.0 6.0.beta -1 1} {1.0-beta 6.0.beta -1 1} {2.0 6.0.beta -1 1} {2.0-devel 6.0.beta -1 1} {2.0.alpha 6.0.beta -1 1} {2.0_beta 6.0.beta -1 1} {2.0-build003 6.0.beta -1 1} {2.0-release-1 6.0.beta -1 1} {3.0 6.0.beta -1 1} {3.0-devel 6.0.beta -1 1} {3.0-alpha 6.0.beta -1 1} {3.1 6.0.beta -1 1} {4 6.0.beta -1 1} {4.1 6.0.beta -1 1} {5.0 6.0.beta -1 1} {5.1 6.0.beta -1 1} {5.1-alpha 6.0.beta -1 1} {5.1-beta 6.0.beta -1 1} {5.2 6.0.beta -1 1} {5.2-alpha 6.0.beta -1 1} {5.2-beta 6.0.beta -1 1} {6.0-devel 6.0.beta 1 -1} {6.0-alpha 6.0.beta -1 1} {6.0.beta 6.0.beta 0 0} {6.0.1 6.0.beta -1 1} {6.0.beta 6.0.beta 0 0}}}
+} {{{1.0 6.1.devel -1 1} {1.0.0 6.1.devel -1 1} {1.0-beta 6.1.devel -1 1} {2.0 6.1.devel -1 1} {2.0-devel 6.1.devel -1 1} {2.0.alpha 6.1.devel -1 1} {2.0_beta 6.1.devel -1 1} {2.0-build003 6.1.devel -1 1} {2.0-release-1 6.1.devel -1 1} {3.0 6.1.devel -1 1} {3.0-devel 6.1.devel -1 1} {3.0-alpha 6.1.devel -1 1} {3.1 6.1.devel -1 1} {4 6.1.devel -1 1} {4.1 6.1.devel -1 1} {5.0 6.1.devel -1 1} {5.1 6.1.devel -1 1} {5.1-alpha 6.1.devel -1 1} {5.1-beta 6.1.devel -1 1} {5.2 6.1.devel -1 1} {5.2-alpha 6.1.devel -1 1} {5.2-beta 6.1.devel -1 1} {6.0-devel 6.1.devel -1 1} {6.0-alpha 6.1.devel -1 1} {6.0.beta 6.1.devel -1 1} {6.0.1 6.1.devel -1 1} {6.1.devel 6.1.devel 0 0}}}
 
 test VersionAttribute-3.0 {clone: This used to throw an exception because of NamedObj.clone() was not checking for final fields.} {
     set n [java::new ptolemy.kernel.util.NamedObj "my NamedObj"]
@@ -106,4 +106,4 @@ test VersionAttribute-3.0 {clone: This used to throw an exception because of Nam
 
 test VersionAttribute-3.0 {majorCurrentVersion} {
     java::call ptolemy.kernel.attributes.VersionAttribute majorCurrentVersion
-} {6.0}
+} {6.1}
