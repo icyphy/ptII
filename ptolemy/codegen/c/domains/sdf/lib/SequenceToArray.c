7f1f0ca12171c335cb30766557640e87d00db252
cxh <cxh>
2006-11-25 07:59:21 +0000

    Split long lines for better readability

--- ptolemy/codegen/c/domains/sdf/lib/SequenceToArray.c
+++ ptolemy/codegen/c/domains/sdf/lib/SequenceToArray.c
@@ -5,8 +5,13 @@ Token $actorSymbol(valueArray);
 
 /*** fireBlock ***/
 $actorSymbol(valueArray) = $new(Array($val(arrayLength), 0));
-for ($actorSymbol(i) = 0; $actorSymbol(i) < $val(arrayLength); $actorSymbol(i)++) {
-    Array_set($actorSymbol(valueArray),$actorSymbol(i), $ref((Token)input, $actorSymbol(i)));
+for ($actorSymbol(i) = 0;
+     $actorSymbol(i) < $val(arrayLength);
+     $actorSymbol(i)++) {
+    Array_set($actorSymbol(valueArray),
+            $actorSymbol(i),
+            $ref((Token)input, $actorSymbol(i)));
 }	
-$ref(output) = $actorSymbol(valueArray);
+$ref(output) = 
+        $actorSymbol(valueArray);
 /**/
