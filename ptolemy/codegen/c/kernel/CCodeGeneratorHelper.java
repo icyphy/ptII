33d37012614a75645aefdc9960c7c38cb2a5df49
mankit <mankit>
2006-11-27 23:09:02 +0000

    moved C specific methods from CodeGeneratorHelper

--- ptolemy/codegen/c/kernel/CCodeGeneratorHelper.java
+++ ptolemy/codegen/c/kernel/CCodeGeneratorHelper.java
@@ -27,8 +27,16 @@
  */
 package ptolemy.codegen.c.kernel;
 
+import java.util.Iterator;
+
+import ptolemy.actor.Actor;
+import ptolemy.actor.TypedIOPort;
+import ptolemy.actor.util.DFUtilities;
 import ptolemy.codegen.kernel.CodeGeneratorHelper;
 import ptolemy.codegen.kernel.ParseTreeCodeGenerator;
+import ptolemy.codegen.kernel.CodeGeneratorHelper.Channel;
+import ptolemy.data.expr.Parameter;
+import ptolemy.kernel.util.IllegalActionException;
 import ptolemy.kernel.util.NamedObj;
 
 //////////////////////////////////////////////////////////////////////////
@@ -81,4 +89,133 @@ public class CCodeGeneratorHelper extends CodeGeneratorHelper {
         _parseTreeCodeGenerator = new CParseTreeCodeGenerator();
         return _parseTreeCodeGenerator;
     }
+
+    /** Generate variable declarations for inputs and outputs and parameters.
+     *  Append the declarations to the given string buffer.
+     *  @return code The generated code.
+     *  @exception IllegalActionException If the helper class for the model
+     *   director cannot be found.
+     */
+    public String generateVariableDeclaration() throws IllegalActionException {
+        StringBuffer code = new StringBuffer();
+        code.append("\n"
+                + _codeGenerator.comment(0, getComponent().getName()
+                        + "'s variable declarations."));
+
+        //  Generate variable declarations for referenced parameters.    
+        if (_referencedParameters != null) {
+            Iterator parameters = _referencedParameters.iterator();
+
+            while (parameters.hasNext()) {
+                Parameter parameter = (Parameter) parameters.next();
+
+                // avoid duplicate declaration.
+                if (!_codeGenerator.getModifiedVariables().contains(parameter)) {
+                    code.append("static " + cType(parameter.getType()) + " "
+                            + generateVariableName(parameter) + ";\n");
+                }
+            }
+        }
+
+        // Generate variable declarations for input ports.
+        code.append(_generateInputVariableDeclaration());
+
+        // Generate variable declarations for output ports.
+        code.append(_generateOutputVariableDeclaration());
+
+        code.append(_codeGenerator.comment(0,
+                "Type convert variable declarations."));
+        Iterator channels = _getTypeConvertChannels().iterator();
+        while (channels.hasNext()) {
+            Channel channel = (Channel) channels.next();
+            code.append("static ");
+            code.append(cType(((TypedIOPort) channel.port).getType()));
+            code.append(" " + _getTypeConvertReference(channel));
+
+            int bufferSize = Math.max(DFUtilities
+                    .getTokenProductionRate(channel.port), DFUtilities
+                    .getTokenConsumptionRate(channel.port));
+
+            if (bufferSize > 1) {
+                code.append("[" + bufferSize + "]");
+            }
+            code.append(";\n");
+        }
+        return processCode(code.toString());
+    }
+    
+    
+
+    /** Generate input variable declarations.
+     *  @return a String that declares input variables.
+     *  @exception IllegalActionException If thrown while
+     *  getting port information.  
+     */
+    protected String _generateInputVariableDeclaration()
+            throws IllegalActionException {
+        StringBuffer code = new StringBuffer();
+
+        Iterator inputPorts = ((Actor) getComponent()).inputPortList().iterator();
+
+        while (inputPorts.hasNext()) {
+            TypedIOPort inputPort = (TypedIOPort) inputPorts.next();
+
+            if (inputPort.getWidth() == 0) {
+                continue;
+            }
+
+            code.append("static " + cType(inputPort.getType()) + " "
+                    + generateName(inputPort));
+
+            if (inputPort.isMultiport()) {
+                code.append("[" + inputPort.getWidth() + "]");
+            }
+
+            int bufferSize = getBufferSize(inputPort);
+
+            if (bufferSize > 1) {
+                code.append("[" + bufferSize + "]");
+            }
+            code.append(";\n");
+        }
+
+        return code.toString();
+    }
+    
+    /** Generate output variable declarations.
+     *  @return a String that declares output variables.
+     *  @exception IllegalActionException If thrown while
+     *  getting port information.  
+     */
+    protected String _generateOutputVariableDeclaration()
+            throws IllegalActionException {
+        StringBuffer code = new StringBuffer();
+
+        Iterator outputPorts = ((Actor) getComponent()).outputPortList().iterator();
+
+        while (outputPorts.hasNext()) {
+            TypedIOPort outputPort = (TypedIOPort) outputPorts.next();
+
+            // If either the output port is a dangling port or
+            // the output port has inside receivers.
+            if (outputPort.getWidth() == 0 || outputPort.getWidthInside() != 0) {
+                code.append("static " + cType(outputPort.getType()) + " "
+                        + generateName(outputPort));
+
+                if (outputPort.isMultiport()) {
+                    code.append("[" + outputPort.getWidthInside() + "]");
+                }
+
+                int bufferSize = getBufferSize(outputPort);
+
+                if (bufferSize > 1) {
+                    code.append("[" + bufferSize + "]");
+                }
+                code.append(";\n");
+            }
+        }
+
+        return code.toString();
+    }
+    
 }
