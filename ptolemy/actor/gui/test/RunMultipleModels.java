39f9562f796a40907f1eec7bd2d7395bfac603bf
cxh <cxh>
2006-12-01 20:53:34 +0000

    Use threadnames

--- ptolemy/actor/gui/test/RunMultipleModels.java
+++ ptolemy/actor/gui/test/RunMultipleModels.java
@@ -1,6 +1,7 @@
 package ptolemy.actor.gui.test;
 
 import java.net.URL;
+import java.util.Vector;
 
 import ptolemy.actor.CompositeActor;
 import ptolemy.actor.Manager;
@@ -9,27 +10,35 @@ import ptolemy.kernel.util.Workspace;
 import ptolemy.moml.MoMLParser;
 
 
+/** This test (from Rome AFRL) was causing a stack trace.
+ */
 public class RunMultipleModels {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         for(int i = 0; i < 25; i++) {
             Thread thread = new Thread(new Runnable() {
                 public void run() {
-                    URL url = RunMultipleModels.class.getResource("RunMultipleModelsModel.xml");
                     try {
-                        Workspace workspace = new Workspace("Workspace" + System.currentTimeMillis());
+                        String threadName = Thread.currentThread().getName();
+                        URL url = RunMultipleModels.class.getResource("RunMultipleModelsModel.xml");
+
+                        Workspace workspace = new Workspace("Workspace"
+                                + threadName);
                         MoMLParser parser = new MoMLParser(workspace);
                         NamedObj model = parser.parse(null, url);
-
-                        Manager manager = new Manager(workspace, "Manager" + System.currentTimeMillis());
+                        
+                        Manager manager = new Manager(workspace, "Manager"
+                                + threadName);
                         ((CompositeActor)model).setManager(manager);
                         manager.execute();
-                    } catch(Exception e) {
-                        e.printStackTrace();
+                    } catch (Exception ex) {
+                        //System.err.println("Thread "
+                        //        + Thread.currentThread().getName());
+                        ex.printStackTrace();
                     }
                 }
             });
 
-            System.out.println("Starting Thread:" + i);
+            System.out.println("Starting " + thread.getName());
             thread.start();
         }
     }
