c6b1ab5797d908f318d75fd4f0d740e34535b645
vnagpal <vnagpal>
2006-12-03 07:21:18 +0000

    New Helper

---
+++ ptolemy/codegen/vhdl/actor/lib/vhdl/RegisterSR.vhdl
@@ -0,0 +1,25 @@
+/*** sharedBlock ***/
+component ptregister is
+	generic
+	(
+		WIDTH 				: INTEGER := 32      --width of the register in bits.
+	);
+	port
+	(
+		clk 	: IN std_logic ;
+		D		: IN std_logic_vector (WIDTH-1 DOWNTO 0);
+		Q		: OUT std_logic_vector (WIDTH-1 DOWNTO 0) 
+	);
+end component ptregister;
+/**/
+
+/*** fireBlock ($width) ***/
+	GENERIC MAP ( 
+		WIDTH => $width, 
+	)
+	PORT MAP ( 
+		clk => clk,
+		D	=> $ref(input),
+		Q	=> $ref(output)
+	);
+/**/
