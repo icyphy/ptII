3a1e028eb35c268115ec67a3012bad455ee00b84
vnagpal <vnagpal>
2006-12-05 14:41:00 +0000

    Added types needed by pttest.vhd

--- ptolemy/codegen/vhdl/core/vhdl/pkg/pt_utility/pt_utility.vhd
+++ ptolemy/codegen/vhdl/core/vhdl/pkg/pt_utility/pt_utility.vhd
@@ -5,70 +5,17 @@
 
 package pt_utility is
 
-function sfixed_add_to_sfixed_high (
-	A_high	: integer; 
-	B_high : integer) 
-return integer; 
+--Types used by vhdl/tb/pttest actor
 
-function sfixed_add_to_sfixed_low (
-	A_low	: integer; 
-	B_low	: integer) 
-return integer;
-
-function sfixed_add_to_slv_high (
-	A_high	: integer;
-	A_low	: integer;
-	B_high	: integer;
-	B_low	: integer)
-return integer;
-
+--Type to read correctvalues from ptolemy test actor.
+type CORRECTVALS is array (integer range <>) of real;
+--Type to represent interpretation of fixed point number
+type FIXED_TYPE_SIGN is (SIGNED, UNSIGNED);
 
 end package pt_utility;
 
 package body pt_utility is
 
-function sfixed_add_to_sfixed_high (
-	A_high	: integer; 
-	B_high : integer) 
-return integer is
-variable ret : integer;
-begin
-		if A_high>B_high then
-			ret := A_high+1;
-		elsif B_high>A_high then
-			ret := B_high+1;
-		else
-			ret := A_high+1;
-		end if;
-		return ret;
-end function sfixed_add_to_sfixed_high;
 
-function sfixed_add_to_sfixed_low (
-	A_low	: integer; 
-	B_low : integer) 
-return integer is
-variable ret : integer;
-begin
-		if A_low>B_low then
-			ret := B_low;
-		elsif B_low>A_low then
-			ret := A_low;
-		else
-			ret := A_low;
-		end if;
-		return ret;
-end function sfixed_add_to_sfixed_low;
-
-function sfixed_add_to_slv_high (
-	A_high	: integer;
-	A_low	: integer;
-	B_high	: integer;
-	B_low	: integer)
-return integer is
-variable ret : integer;
-begin
-	ret := sfixed_add_to_sfixed_high(A_high,B_high)-sfixed_add_to_sfixed_low(A_low,B_low);
-	return ret;
-end function sfixed_add_to_slv_high;
 
 end package body pt_utility;
