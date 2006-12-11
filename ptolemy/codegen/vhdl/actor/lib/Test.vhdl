bf79bd1a8f41e749fb554c89b87ff5ed5cb6ca21
vnagpal <vnagpal>
2006-12-11 06:39:20 +0000

    Added reset

--- ptolemy/codegen/vhdl/actor/lib/Test.vhdl
+++ ptolemy/codegen/vhdl/actor/lib/Test.vhdl
@@ -6,11 +6,13 @@ component pttest is
 		INPUT_HIGH	:	integer	:= 0;
 		INPUT_LOW	:	integer := -15;
 		LIST		:	CORRECTVALS;
+		RESET_ACTIVE_VALUE : st_logic := '0';
 		FIXED_SIGN	:	FIXED_TYPE_SIGN := SIGNED_TYPE	
 	) ;
 	port
 	(
 		clk			:	in 	std_logic ;
+		reset		:  	in  std_logic ;
 		data_in		:	in	std_logic_vector (INPUT_HIGH-INPUT_LOW DOWNTO 0)
 	) ;
 end component pttest;
@@ -24,10 +26,12 @@ $actorSymbol(instance): pttest
 		INPUT_HIGH	=> $high,
 		INPUT_LOW	=> $low,
 		LIST		=> ($values),
+		RESET_ACTIVE_VALUE => '0',
 		FIXED_SIGN	=> $signed
 	)
 	PORT MAP ( 
 		clk	=> clk,
+		reset => reset,
 		data_in => $ref(input)
 	);
 /**/
