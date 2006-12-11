b67bf2222b9596603aa3a608322299db93849832
vnagpal <vnagpal>
2006-12-11 07:17:02 +0000

    added and tested the display actor and helper

--- ptolemy/codegen/vhdl/actor/lib/gui/Display.java
+++ ptolemy/codegen/vhdl/actor/lib/gui/Display.java
@@ -1,4 +1,4 @@
-/* A helper class for actor.lib.Uniform
+/* A helper class for actor.lib.FixConst
 
  @Copyright (c) 2005-2006 The Regents of the University of California.
  All rights reserved.
@@ -28,16 +28,23 @@
  */
 package ptolemy.codegen.vhdl.actor.lib.gui;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
 
+import ptolemy.actor.TypedAtomicActor;
+import ptolemy.actor.TypedIOPort;
 import ptolemy.codegen.vhdl.kernel.VHDLCodeGeneratorHelper;
+import ptolemy.data.ArrayToken;
+import ptolemy.data.FixToken;
+import ptolemy.data.expr.Parameter;
 import ptolemy.kernel.util.IllegalActionException;
+import ptolemy.math.Precision;
 
 /**
  * A helper class for ptolemy.actor.lib.Uniform.
  *
- * @author Man-Kit Leung
+ * @author Vinayak Nagpal
  * @version $Id$
  * @since Ptolemy II 6.0
  * @Pt.ProposedRating Green (mankit)
@@ -52,13 +59,64 @@ public class Display extends VHDLCodeGeneratorHelper {
         super(actor);
     }
 
-    /** Get the files needed by the code generated for the Display actor.
+    /**
+     * Generate fire code.
+     * The method reads in the <code>fireBlock</code> from FixConst.c,
+     * replaces macros with their values and returns the processed code
+     * block.
+     * @return The generated code.
+     * @exception IllegalActionException If the code stream encounters
+     *  an error in processing the specified code block(s).
+     */
+    public String generateFireCode() throws IllegalActionException {
+        super.generateFireCode();
+
+        ptolemy.actor.lib.gui.Display actor = 
+            (ptolemy.actor.lib.gui.Display) getComponent();
+
+        // FIXME: we should implement this later.
+        if (actor.input.getWidth() > 1) {
+            throw new IllegalActionException(this, 
+                    "VHDL Code generation does not support multiport connections.");
+        }
+        
+        ArrayList args = new ArrayList();
+
+        TypedIOPort source = (TypedIOPort) actor.input.sourcePortList().get(0);
+        TypedAtomicActor sourceActor = (TypedAtomicActor) source.getContainer();
+        
+        Precision precision = _getSourcePortPrecision(actor.input);
+        
+        int high = precision.getIntegerBitLength() - 1;
+        int low = -precision.getFractionBitLength();
+                
+        args.add(new Integer(high));
+        args.add(new Integer(low));
+        args.add(actor.getName());
+        
+        String signed = 
+            (precision.isSigned()) ? "SIGNED_TYPE" : "UNSIGNED_TYPE";
+        
+        args.add(signed);
+        
+        _codeStream.appendCodeBlock("fireBlock", args);
+
+        return processCode(_codeStream.toString());
+    }
+
+    /** Get the files needed by the code generated for the FixConst actor.
      *  @return A set of strings that are names of the library and package.
      *  @exception IllegalActionException Not Thrown in this subclass.
      */
     public Set getHeaderFiles() throws IllegalActionException {
         Set files = new HashSet();
+        
         files.add("ieee.std_logic_1164.all");
+        files.add("ieee.numeric_std.all");
+        files.add("ieee_proposed.math_utility_pkg.all");
+        files.add("ieee_proposed.fixed_pkg.all");
+        files.add("work.pt_utility.all");
         return files;
     }
 }
+
--- ptolemy/codegen/vhdl/actor/lib/gui/Display.vhdl
+++ ptolemy/codegen/vhdl/actor/lib/gui/Display.vhdl
@@ -1,25 +1,29 @@
 
 /*** sharedBlock ***/
 component ptdisplay is
-	generic
-	(
-		CONST_HIGH		:	integer		:= 15;
-		CONST_LOW		:	integer		:= 0;
-	) ;
-	port
-	(
-		input			: IN std_logic_vector (CONST_HIGH-CONST_LOW DOWNTO 0) 
-	) ;
+GENERIC(
+	INPUT_HIGH	:	integer	:= 0;
+	INPUT_LOW	:	integer := -15;
+	ACTORNAME	:	string;
+	FIXED_SIGN	:	FIXED_TYPE_SIGN := SIGNED_TYPE
+	);
+PORT (
+	clk			:	in std_logic ;
+	input		:	in	std_logic_vector (INPUT_HIGH-INPUT_LOW DOWNTO 0)
+);
 end component ptdisplay;
 /**/
 
-/*** fireBlock ($high, $low, $fixValue) ***/
+/*** fireBlock ($high, $low, $name, $sign ) ***/
 $actorSymbol(instance): ptdisplay
-	GENERIC MAP ( 
-		CONST_HIGH => $high, 
-		CONST_LOW => $low,
+GENERIC MAP (
+	INPUT_HIGH	=> $high,
+	INPUT_LOW	=> $low,
+	ACTORNAME	=> "$name",
+	FIXED_SIGN	=> $sign
 	)
-	PORT MAP ( 
-		input => $ref(input)
-	);
+PORT MAP(
+	clk			=> clk,
+	input		=> $ref(input)
+);
 /**/
