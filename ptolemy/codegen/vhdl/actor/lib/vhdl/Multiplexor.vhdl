4d526396e467507cd4942366a8c4a3d626bb40a7
vnagpal <vnagpal>
2006-12-12 01:51:06 +0000

    new actor

--- ptolemy/codegen/vhdl/actor/lib/vhdl/Multiplexor.vhdl
+++ ptolemy/codegen/vhdl/actor/lib/vhdl/Multiplexor.vhdl
@@ -1,18 +1,37 @@
-/*** preinitBlock ***/
-
-/**/
-
 /*** sharedBlock ***/
+component pt_mux2 is
+	generic
+	(
+		WIDTH		:	integer		:= 15;
+		RESET_ACTIVE_VALUE	:	std_logic	:= '0';
+		LATENCY				: 	integer		:= 3
+	) ;
+	port
+	(
+		clk				: IN std_logic;
+		reset			: IN std_logic;
+		A				: IN std_logic_vector (WIDTH-1 DOWNTO 0) ;	
+		B				: IN std_logic_vector (WIDTH-1 DOWNTO 0) ;
+		sel				: IN std_logic;
+		output			: OUT std_logic_vector (WIDTH-1 DOWNTO 0)
+	) ;
+end component pt_mux2;
 /**/
 
-/*** fireBlock ***/
-process($refList(input), $actorSymbol(S))
-begin
-	case $ref(select) is
-	  	for i in 0 to $size(input) loop
-	  		when i then
-		    	$ref(output) <= $ref(input#i);
-		end loop;
-	end case;
-end process;
+/*** fireBlock ($width, $latency) ***/
+generic map
+(
+	WIDTH				=> $width,
+	RESET_ACTIVE_VALUE	=> '0',
+	LATENCY				=> $latency
+) 
+port
+(
+	clk				=> clk,
+	reset			=> reset,
+	A				=> $ref(A),
+	B				=> $ref(B),
+	sel				=> $ref(select),
+	output			=> $ref(output)
+) ;
 /**/
