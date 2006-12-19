78404132d23bee61594d2349a0087bbf3a9c25ca
cxh <cxh>
2006-12-19 23:24:54 +0000

    Use thread.join() instead of activeCount

--- ptolemy/actor/gui/test/RunMultipleModels.java
+++ ptolemy/actor/gui/test/RunMultipleModels.java
@@ -1,6 +1,9 @@
 package ptolemy.actor.gui.test;
 
 import java.net.URL;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
 
 import ptolemy.actor.CompositeActor;
 import ptolemy.actor.Manager;
@@ -12,13 +15,16 @@ import ptolemy.moml.MoMLParser;
  */
 public class RunMultipleModels {
 
-    /** Create multiple models and execute them. */
+    /** Create multiple models and execute them.
+     *  @exception Exception The last exception thrown, if any
+     *  by any of the threads.
+     */
     public synchronized void run() throws Exception {
+        Set threadSet = new HashSet();
         for (int i = 0; i < 100; i++) {
             Thread thread = new Thread(new Runnable() {
                 public void run() {
                     try {
-                        _activeCount++;
                         String threadName = Thread.currentThread().getName();
                         URL url = RunMultipleModels.class
                                 .getResource("RunMultipleModelsModel.xml");
@@ -33,52 +39,44 @@ public class RunMultipleModels {
                                 + threadName);
                         ((CompositeActor) model).setManager(manager);
                         manager.execute();
-                        System.out.print(" " + _activeCount);
-                        _activeCount--;
 
-                        if (_activeCount == 0) {
-                            RunMultipleModels.this.notifyAll();
-                        }
-                    } catch (IllegalMonitorStateException ex) {
-                        Thread thread = Thread.currentThread();
-                        System.out.println("Thread: " + thread.getName()
-                                + " state: " + thread.getState());
-                        ex.printStackTrace();
                     } catch (Exception ex) {
                         _lastException = ex;
                         Thread thread = Thread.currentThread();
                         System.out.println("Thread: " + thread.getName()
                                 + " state: " + thread.getState());
                         ex.printStackTrace();
-                        _activeCount--;
-
-                        //if (_activeCount == 0) {
-                        //    notifyAll();
-                        //}
                     }
                 }
             });
 
             System.out.print(" " + thread.getName());
+            // Keep track of which threads we have created so that
+            // we can join them later
+            threadSet.add(thread);
             thread.start();
         }
-        System.out.println("");
-        System.out.println("Sleeping " + Thread.currentThread().getName()
-                + " 30 seconds so that other threads may run.");
-        Thread.sleep(30000);
-        while (_activeCount > 0) {
-            try {
-                wait();
-            } catch (InterruptedException ex) {
-                break;
-            }
+
+        // Join each thread and wait for it to die
+        // "Shot a thread in reno, just to watch it die"
+        Iterator threads = threadSet.iterator();
+        while(threads.hasNext()) {
+            Thread thread = (Thread) threads.next();
+            // Timeout after 300 seconds so that the nightly build
+            // does not hang.
+            thread.join(300000);
         }
+
+        System.out.println("");
+
         if (_lastException != null) {
             throw _lastException;
         }
     }
 
-    /** Create multiple models and execute them. */
+    /** Create multiple models and execute them by using Manager.startRun().
+     *  This is an alternative implementation that uses Manager.startRun(),
+     */
     public synchronized void run2() throws Exception {
         for (int i = 0; i < 100; i++) {
             URL url = RunMultipleModels.class
@@ -95,13 +93,12 @@ public class RunMultipleModels {
         }
     }
 
+    /** Run multiple models.
+     */
     public static void main(String[] args) throws Exception {
         RunMultipleModels runMultipleModels = new RunMultipleModels();
         runMultipleModels.run();
     }
-
-    // The count of currently executing runs.
-    private int _activeCount = 0;
 
     // The last exception that was thrown.
     private Exception _lastException;
