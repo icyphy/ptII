f5c23fd68fb83bcf0edcc7422feb1a4cce771582
cxh <cxh>
2006-12-30 22:26:44 +0000

    Class commment: info about rise v. run

--- ptolemy/vergil/kernel/attributes/LineAttribute.java
+++ ptolemy/vergil/kernel/attributes/LineAttribute.java
@@ -1,6 +1,6 @@
 /* An attribute with a reference to a line.
 
- Copyright (c) 2003-2005 The Regents of the University of California.
+ Copyright (c) 2003-2006 The Regents of the University of California.
  All rights reserved.
  Permission is hereby granted, without written agreement and without
  license or royalty fees, to use, copy, modify, and distribute this
@@ -43,6 +43,13 @@ import ptolemy.kernel.util.NamedObj;
 
 /**
  This is an attribute that is rendered as a line.
+ 
+ <p>
+ This contains two parameters, <i>x</i> and <i>y</i>, which control the
+ run and rise, respectively, of the line.  Note that the origin is in the
+ upper-left hand corner, so positive x values will extend to the right, and
+ positive y values will extend downwards on the screen.
+ 
  <p>
  @author Edward A. Lee
  @version $Id$
