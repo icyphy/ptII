454280ae6dc348d05f07b6eeefc38e001fddaa0a
cxh <cxh>
2007-01-03 21:44:32 +0000

    Typo from last change

--- ptolemy/data/type/test/ArrayType.tcl
+++ ptolemy/data/type/test/ArrayType.tcl
@@ -91,7 +91,7 @@ test ArrayType-2.0 {Test isCompatible} {
 test ArrayType-2.0.5 {Test isCompatible with GENERAL} {
     # Connecting a const with an array of ints to a Display was failing.
     list [$natArrayType isCompatible [java::field ptolemy.data.type.BaseType GENERAL]] \
-	 [$natArrayType isCompatible [java::field ptolemy.data.type.BaseType GENERAL]]
+	 [$strArrayType isCompatible [java::field ptolemy.data.type.BaseType GENERAL]]
 } {1 1}
 
 ######################################################################
