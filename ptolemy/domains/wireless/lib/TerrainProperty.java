dc6b4a725a4743a5838fc4b5f369f697979cc0aa
eal <eal>
2007-03-01 18:54:13 +0000

    Added attenuation feature.

--- ptolemy/domains/wireless/lib/TerrainProperty.java
+++ ptolemy/domains/wireless/lib/TerrainProperty.java
@@ -38,6 +38,7 @@ import ptolemy.data.RecordToken;
 import ptolemy.data.Token;
 import ptolemy.data.expr.Parameter;
 import ptolemy.data.expr.StringParameter;
+import ptolemy.data.type.BaseType;
 import ptolemy.domains.wireless.kernel.PropertyTransformer;
 import ptolemy.domains.wireless.kernel.WirelessChannel;
 import ptolemy.domains.wireless.kernel.WirelessIOPort;
@@ -54,12 +55,28 @@ import ptolemy.vergil.kernel.attributes.FilledShapeAttribute;
 //// TerrainProperty
 
 /**
+ This actor models an obstacle that attenuates signals that
+ traverse it. The obstacle has a geometry given by the <i>xyPoints</i>
+ parameter. By default, the attenuation is infinite, meaning
+ that the obstacle completely blocks a signal. By increasing
+ the <i>attenuationDepth</i> parameter you can specify partial
+ attenuation that depends on the geometry of the obstacle.
+ If <i>attenuationDepth</i> has value <i>x</i> and the signal
+ traverses distance <i>y</i> through the obstacle, then the
+ power of the signal is reduced by a factor of
+ <pre>
+   exp(log(0.5) * y / x)
+ </pre>
+ Thus, if <i>x</i> = <i>y</i>, then the power is reduced to
+ half (3dB).  The algorithm used here only works for convex
+ obstacle shapes.
+ <p>
  This actor implements the PropertyTransformer interface.
  It register itself with the wireless channel specified by
  the <i>channelName</i> parameter. The channel may call it
  getProperty() method to get the property.
 
- @author Yang Zhao
+ @author Yang Zhao and William Douglas (contributor: Edward A. Lee)
  @version $Id$
  @since Ptolemy II 4.0
  @Pt.ProposedRating Yellow (eal)
@@ -83,6 +100,10 @@ public class TerrainProperty extends TypedAtomicActor implements
 
         xyPoints = new Parameter(this, "xyPoints");
         xyPoints.setExpression("{{0, 0}, {0, 5}, {20, 5}, {20, 0}}");
+
+        attenuationDepth = new Parameter(this, "attenuationDepth");
+        attenuationDepth.setExpression("0.0");
+        attenuationDepth.setTypeEquals(BaseType.DOUBLE);
 
         //create the default icon.
         _numberOfPoints = 4;
@@ -120,10 +141,19 @@ public class TerrainProperty extends TypedAtomicActor implements
      */
     public StringParameter channelName;
 
-    /** The x/y coordinates of ? (FIXME)
+    /** The width of material that will attenuate a signal power by
+     *  50% (3dB).  This has the same units as the xyPoints parameter,
+     *  which specifies the geometry of the obstable.
+     *  The default value is 0.0, which means infinite attenuation
+     *  (no signal gets through).
+     */
+    public Parameter attenuationDepth;
+
+    /** The x/y coordinates of the obstable. This is an array
+     *  of integers specifying a geometry. The icon will have
+     *  the same geometry.
      *  The default value is an array of integer pairs:
      *  {{0, 0}, {0, 5}, {20, 5}, {20, 0}}
-     *  FIXME: need more info
      */
     public Parameter xyPoints;
 
@@ -167,6 +197,8 @@ public class TerrainProperty extends TypedAtomicActor implements
                 throw new IllegalActionException(this,
                         "xPoints is required to be an integer array");
             }
+        } else if (attribute == attenuationDepth) {
+            _attenuation = ((DoubleToken)attenuationDepth.getToken()).doubleValue();
         } else {
             super.attributeChanged(attribute);
         }
@@ -213,10 +245,6 @@ public class TerrainProperty extends TypedAtomicActor implements
     /** Check whether the path between the sender and receiver is
      *  intersected with the terrain shape. If yes, set the "power" field
      *  in the property to be zero, otherwise, do nothing.
-     *  FIXME: should the terrain property affect the power value
-     *  or should it affect the receivers in range?
-     *  FIXME: check java.util to see if there is standard method to
-     *  do this check...
      * @param properties The transform properties.
      * @param sender The sending port.
      * @param destination The receiving port.
@@ -226,66 +254,30 @@ public class TerrainProperty extends TypedAtomicActor implements
     public RecordToken transformProperties(RecordToken properties,
             WirelessIOPort sender, WirelessIOPort destination)
             throws IllegalActionException {
+
+        RecordToken newProperties = properties;
         double[] p1 = _locationOf(sender);
         double[] p2 = _locationOf(destination);
-        double a;
-        double b;
-        double c;
-        double d;
-        double x0;
-        double y0;
-        double x1;
-        double y1;
-        double k;
-        boolean cross = false;
+        double depth = _polygonDepthBetweenPorts(p1, p2);
 
-        for (int i = 0; i < _numberOfPoints; i++) {
-            for (int j = i + 1; j < _numberOfPoints; j++) {
-                x0 = _xPoints[i] + _offset[0];
-                y0 = _yPoints[i] + _offset[1];
-                x1 = _xPoints[j] + _offset[0];
-                y1 = _yPoints[j] + _offset[1];
-
-                if ((x1 - x0) != 0) {
-                    k = (y1 - y0) / (x1 - x0);
-                    a = p1[1] - y0 - (k * (p1[0] - x0));
-                    b = p2[1] - y0 - (k * (p2[0] - x0));
-                } else {
-                    a = p1[0] - x0;
-                    b = p2[0] - x0;
-                }
-
-                if ((p2[0] - p1[0]) != 0) {
-                    k = (p2[1] - p1[1]) / (p2[0] - p1[0]);
-                    c = y0 - p1[1] - (k * (x0 - p1[0]));
-                    d = y1 - p1[1] - (k * (x1 - p1[0]));
-                } else {
-                    c = x0 - p1[0];
-                    d = x1 - p1[0];
-                }
-
-                if (((a * b) < 0) && ((c * d) < 0)) {
-                    cross = true;
-                    break;
-                }
-            } //for j.
-        } //for i.
-
-        if (cross) {
-            // FIXME: transmitPower is never read?
-            //Token transmitPower = properties.get("power");
+        if (depth > 0.0) {
+            Token transmitPower = properties.get("power");
+            double currentPower = ((DoubleToken)transmitPower).doubleValue();
+            double multiplier = 0.0;
+            // Avoid divide by zero, though in theory it's OK.
+            if (_attenuation > 0.0) {
+                multiplier = Math.exp((Math.log(0.5) / _attenuation) * depth);
+            }
 
             // Create a record token with the receive power.
             String[] names = { "power" };
-            Token[] values = { new DoubleToken(0.0) };
+            Token[] values = { new DoubleToken(currentPower * multiplier) };
             RecordToken newPower = new RecordToken(names, values);
 
             // Merge the receive power into the merged token.
-            RecordToken result = RecordToken.merge(newPower, properties);
-            return result;
-        } else {
-            return properties;
+            newProperties = RecordToken.merge(newPower, properties);
         }
+        return newProperties;
     }
 
     /** Override the base class to call wrap up to unregister this with the
@@ -298,6 +290,9 @@ public class TerrainProperty extends TypedAtomicActor implements
             _channel.unregisterPropertyTransformer(this, null);
         }
     }
+
+    ///////////////////////////////////////////////////////////////////
+    ////                         private methods                   ////
 
     /** Return the location of the given WirelessIOPort.
      *  @param port A port with a location.
@@ -356,8 +351,107 @@ public class TerrainProperty extends TypedAtomicActor implements
         return center;
     }
 
+
+    /** Return the depth of the terrain that resides directly between the two
+     *  points. The order of the points is not important.
+     *  @param p1 The first reference point.
+     *  @param p2 The second reference point.
+     *  @return The depth of the terrain object between the supplied points.
+     */
+    private double _polygonDepthBetweenPorts(double[] p1, double[] p2) {
+
+        int state = 0;
+        double[] point = new double[2];
+
+        // To find the points where the terrain is between the two ports, we
+        // iterate over the sides of the terrain looking for sides that
+        // intersect the two ports.  When two such points are found we can
+        // calculate the depth.  If two points are not found then the terrain
+        // is not between the two ports.
+
+        for (int j = 0; j < _numberOfPoints; j++) {
+
+            int k = (j+1) % _numberOfPoints;
+            double[] a = {_xPoints[j] + _offset[0], _yPoints[j] + _offset[1]};
+            double[] b = {_xPoints[k] + _offset[0], _yPoints[k] + _offset[1]};
+
+            // Check to see if the side of the polygon is between the
+            // two ports.
+
+            try {
+                double[] p = _lineSegmentIntersectionPoint(p1, p2, a, b);
+                if (state++ == 0) {
+                    point = p;
+                } else {
+                    // Now that we found two points, return the depth.
+                    return Math.sqrt(Math.pow(p[0] - point[0], 2) +
+                                     Math.pow(p[1] - point[1], 2));
+                }
+            } catch(IllegalActionException e) {
+                // No intersection. Try the next side.
+            }
+        }
+
+        // Return a default value of 0.0 indicating that the terrain is not
+        // inbetween the two ports.
+        return 0.0;
+    }
+
+    /** Return the point where two lines segments intersect.  The two line
+     *  segments are identified by their start and end points.  Should the lines
+     *  not intersect, an IllegalActionException is thrown.
+     *  @param a1 Endpoint on the first line segment.
+     *  @param a2 Endpoint on the first line segment.
+     *  @param b1 Endpoint on the second line segment.
+     *  @param b2 Endpoint on the second line segment.
+     *  @return Location of the intersection point.
+     *  @exception IllegalActionException If the line segments do not intersect
+     *  or if the math results in a determinate of 0.
+     */
+    private double[] _lineSegmentIntersectionPoint(
+            double[] a1, double[] a2, double[] b1, double[] b2)
+            throws IllegalActionException {
+        try {
+            // Calculate the intersection point of the two lines.  This
+            // algorithm was found on the Mathematica web site.
+
+            double A1 = a2[1] - a1[1];
+            double B1 = a1[0] - a2[0];
+            double C1 = A1 * a1[0] + B1 * a1[1];
+            double A2 = b2[1] - b1[1];
+            double B2 = b1[0] - b2[0];
+            double C2 = A2 * b1[0] + B2 * b1[1];
+
+            double det = A1*B2 - A2*B1;
+
+            double[] p = new double[2];
+            p[0] = (B2*C1 - B1*C2)/det;
+            p[1] = (A1*C2 - A2*C1)/det;
+
+            // We have the intersection point of the two lines.  Now we make
+            // sure that this point is present on both line segments.
+
+                        // The value 0.00001 was chosen because of rounding errors in
+                        // the double values.  It is close enough to zero to not make a
+                        // difference while still accounding for rounding errors.
+
+            if (((p[0] - a1[0]) * (p[0] - a2[0]) <= 0.00001) &&
+                ((p[1] - a1[1]) * (p[1] - a2[1]) <= 0.00001) &&
+                ((p[0] - b1[0]) * (p[0] - b2[0]) <= 0.00001) &&
+                ((p[1] - b1[1]) * (p[1] - b2[1]) <= 0.00001)) {
+
+                return p;
+            }
+        } catch(ArithmeticException e) {
+            throw new IllegalActionException("Determinate equal to 0.");
+        }
+
+        throw new IllegalActionException("Line segments do not intersect.");
+    }
+
     ///////////////////////////////////////////////////////////////////
     ////                         private variables                 ////
+
     private WirelessChannel _channel;
 
     private int[] _xPoints;
@@ -369,6 +463,11 @@ public class TerrainProperty extends TypedAtomicActor implements
     private FilledShapeAttribute _terrain;
 
     private int _numberOfPoints;
+
+    // holds the attenuation value of the object.  The attenuation value
+    // is the distance a signal can travel while only losing 50% of it's
+    // strength.
+    private double _attenuation;
 
     // this variable is merely used to set the ShapeAttribute
     // with different width, so that is can update the shape.
