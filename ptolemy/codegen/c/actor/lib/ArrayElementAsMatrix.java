388a783f76625bfcb0109e64b326e929a694f554
cxh <cxh>
2007-03-06 15:26:05 +0000

    ArrayElementAsMatrix.java

---
+++ ptolemy/codegen/c/actor/lib/ArrayElementAsMatrix.java
@@ -0,0 +1,76 @@
+/* A code generation helper class for actor.lib.ArrayElementAsMatrix
+
+ @Copyright (c) 2006 The Regents of the University of California.
+ All rights reserved.
+ Permission is hereby granted, without written agreement and without
+ license or royalty fees, to use, copy, modify, and distribute this
+ software and its documentation for any purpose, provided that the
+ above copyright notice and the following two paragraphs appear in all
+ copies of this software.
+
+ IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+ FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+ ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+ THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+ SUCH DAMAGE.
+
+ THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES, 
+ INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+ PROVIDED HEREUNDER IS ON AN \"AS IS\" BASIS, AND THE UNIVERSITY OF
+ CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES, 
+ ENHANCEMENTS, OR MODIFICATIONS.
+
+ PT_COPYRIGHT_VERSION_2
+ COPYRIGHTENDKEY
+
+ */
+package ptolemy.codegen.c.actor.lib;
+
+import ptolemy.codegen.c.kernel.CCodeGeneratorHelper;
+import ptolemy.kernel.util.IllegalActionException;
+
+/**
+ A code generation helper class for ptolemy.actor.lib.ArrayElementAsMatrix. 
+
+ @author Christopher Brooks
+ @version $Id$
+ @since Ptolemy II 6.1
+ @Pt.ProposedRating Red (cxh)
+ @Pt.AcceptedRating Red (cxh)
+ */
+public class ArrayElementAsMatrix extends CCodeGeneratorHelper {
+
+    /**
+     * Construct an ArrayElementAsMatrix helper.
+     * @param actor The associated actor.
+     */
+    public ArrayElementAsMatrix(ptolemy.actor.lib.ArrayElementAsMatrix actor) {
+        super(actor);
+    }
+
+    /**
+     * Generate fire code.
+     * The method reads in <code>fireBlock</code> from Accumulator.c,
+     * replaces macros with their values and returns the processed code
+     * block.
+     * @return The generated code.
+     * @exception IllegalActionException If the code stream encounters an
+     *  error in processing the specified code block(s).
+     */
+    public String generateFireCode() throws IllegalActionException {
+        super.generateFireCode();
+
+        ptolemy.actor.lib.ArrayElementAsMatrix actor = (ptolemy.actor.lib.ArrayElementAsMatrix) getComponent();
+
+        if (isPrimitive(actor.output.getType())) {
+            // Indent to level 2
+            _codeStream.appendCodeBlock("PrimitiveFireBlock", false, 2);
+        } else {
+            // Indent to level 2
+            _codeStream.appendCodeBlock("TokenFireBlock", false, 2);
+        }
+
+        return processCode(_codeStream.toString());
+    }
+}
