bfca0af49029f679f72fddaf3a20bb51498e5eef
cxh <cxh>
2007-03-07 01:47:10 +0000

    Added a #define for platforms that want to use a different random number implementation

--- ptolemy/codegen/c/actor/lib/Uniform.c
+++ ptolemy/codegen/c/actor/lib/Uniform.c
@@ -1,3 +1,7 @@
 /*** randomBlock ***/
+#ifndef PT_NO_RANDOM_JAVA
+$ref(output) = random() % ($val(upperBound) - $val(lowerBound)) + $val(lowerBound);
+#else
 $ref(output) = (RandomSource_nextDouble(&$actorSymbol(seed)) * ($val(upperBound) - $val(lowerBound))) + $val(lowerBound);
+#endif
 /**/
