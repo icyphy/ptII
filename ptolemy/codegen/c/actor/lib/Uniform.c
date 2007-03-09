4aa99fc3e0a051de327e01070ff8cf2041b301ce
cxh <cxh>
2007-03-09 16:54:15 +0000

    If we don't want to use the random implementation in java, then use random()

--- ptolemy/codegen/c/actor/lib/Uniform.c
+++ ptolemy/codegen/c/actor/lib/Uniform.c
@@ -1,5 +1,5 @@
 /*** randomBlock ***/
-#ifndef PT_NO_RANDOM_JAVA
+#ifdef PT_NO_RANDOM_JAVA
 $ref(output) = random() % ($val(upperBound) - $val(lowerBound)) + $val(lowerBound);
 #else
 $ref(output) = (RandomSource_nextDouble(&$actorSymbol(seed)) * ($val(upperBound) - $val(lowerBound))) + $val(lowerBound);
