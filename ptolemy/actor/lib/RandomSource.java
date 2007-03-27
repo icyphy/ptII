3fc61e287e51dbffe2e0334667b4fa6b07413237
eal <eal>
2007-03-27 01:18:41 +0000

    Changed seed policy for values different from 0L.

--- ptolemy/actor/lib/RandomSource.java
+++ ptolemy/actor/lib/RandomSource.java
@@ -31,13 +31,14 @@ import java.util.Random;
 
 import ptolemy.data.BooleanToken;
 import ptolemy.data.LongToken;
-import ptolemy.data.expr.Parameter;
 import ptolemy.data.expr.SingletonParameter;
 import ptolemy.data.type.BaseType;
 import ptolemy.kernel.CompositeEntity;
+import ptolemy.kernel.util.Attribute;
 import ptolemy.kernel.util.IllegalActionException;
 import ptolemy.kernel.util.NameDuplicationException;
 import ptolemy.kernel.util.Workspace;
+import ptolemy.moml.SharedParameter;
 
 //////////////////////////////////////////////////////////////////////////
 //// RandomSource
@@ -45,9 +46,29 @@ import ptolemy.kernel.util.Workspace;
 /**
  A base class for sources of random numbers.
  It uses the class java.util.Random.
- This base class manages the seed.
+ This base class manages the seed. Specifically,
+ the seed is a shared parameter, so setting the seed
+ in any one instance of a RandomSource results in setting
+ the seed in all instances. If the seed is set to value 0L,
+ the default, then this is interpreted as not specifying a seed,
+ and the random number generators are set to use a seed that
+ depends on the current time in milliseconds. If the seed
+ is set to any value other than 0L, then a seed is computed
+ for the random number generator by adding that specified seed
+ to the hashcode for the full name of the actor. This ensures
+ that with high probability, multiple instances of RandomSource
+ (and derived classes) will have distinct seeds, but that the
+ executions are deterministically repeatable.
+<p>
+ If the <i>resetOnEachRun</i> parameter is true (it is
+ false by default), then each run resets the random number
+ generator. If the seed is non-zero, then this makes
+ each run identical.  This is useful for constructing
+ tests. If the seed is zero, then a new seed is generated
+ on each run using the same technique described above
+ (combining current time and the hash code).
 
- @author Edward A. Lee, Steve Neuendorffer
+ @author Edward A. Lee, Steve Neuendorffer, Elaine Cheong
  @version $Id$
  @since Ptolemy II 0.3
  @Pt.ProposedRating Green (eal)
@@ -65,17 +86,31 @@ public abstract class RandomSource extends Source {
     public RandomSource(CompositeEntity container, String name)
             throws NameDuplicationException, IllegalActionException {
         super(container, name);
-        seed = new Parameter(this, "seed", new LongToken(0));
+        seed = new SharedParameter(this, "seed", RandomSource.class, "0L");
         seed.setTypeEquals(BaseType.LONG);
 
-        new SingletonParameter(trigger, "_showName")
-                .setToken(BooleanToken.TRUE);
+        resetOnEachRun = new SharedParameter(this, "resetOnEachRun", RandomSource.class, "false");
+        resetOnEachRun.setTypeEquals(BaseType.BOOLEAN);
+
+        new SingletonParameter(trigger, "_showName").setToken(BooleanToken.TRUE);
     }
 
     ///////////////////////////////////////////////////////////////////
     ////                     ports and parameters                  ////
 
+    /** If true, this parameter specifies that the random number
+     *  generator should be reset on each run of the model (in
+     *  the initialize() method). It is a boolean that defaults
+     *  to false. This is a shared parameter, meaning that changing
+     *  it somewhere in the model causes it to be changed everywhere
+     *  in the model.
+     */
+    public SharedParameter resetOnEachRun;
+    
     /** The seed that controls the random number generation.
+     *  This is a shared parameter, meaning that all instances of
+     *  RandomSource or derived classes in the same model share the
+     *  same value.
      *  A seed of zero is interpreted to mean that no seed is specified,
      *  which means that each execution of the model could result in
      *  distinct data. For the value 0, the seed is set to
@@ -83,14 +118,36 @@ public abstract class RandomSource extends Source {
      *  with extremely high probability, two distinct actors will have
      *  distinct seeds.  However, current time may not have enough
      *  resolution to ensure that two subsequent executions of the
-     *  same model have distinct seeds.
+     *  same model have distinct seeds. For a value other than zero,
+     *  the seed is set to that value plus the hashCode() of the
+     *  full name of the actor. This means that with high probability,
+     *  two distinct actors will have distinct, but repeatable seeds.
      *
      *  This parameter contains a LongToken, initially with value 0.
      */
-    public Parameter seed;
+    public SharedParameter seed;
 
     ///////////////////////////////////////////////////////////////////
     ////                         public methods                    ////
+
+    /** If the attribute is <i>seed</i>
+     *  then create the base random number generator.
+     *  @param attribute The attribute that changed.
+     *  @exception IllegalActionException If the change is not acceptable
+     *   to this container (not thrown in this base class).
+     */
+    public void attributeChanged(Attribute attribute)
+            throws IllegalActionException {
+        if (attribute == seed) {
+            long seedValue = ((LongToken) (seed.getToken())).longValue();
+
+            if (seedValue != _generatorSeed) {
+                _createGenerator();
+            }
+        } else {
+            super.attributeChanged(attribute);
+        }
+    }
 
     /** Clone the actor into the specified workspace. This calls the
      *  base class and then creates new ports and parameters.
@@ -103,7 +160,11 @@ public abstract class RandomSource extends Source {
         RandomSource newObject = (RandomSource) (super.clone(workspace));
 
         // Get an independent random number generator.
-        newObject._random = new Random();
+        try {
+            newObject._createGenerator();
+        } catch (IllegalActionException e) {
+            throw new CloneNotSupportedException("Invalid seed.");
+        }
         return newObject;
     }
 
@@ -120,15 +181,6 @@ public abstract class RandomSource extends Source {
         }
     }
 
-    /** Calculate the next random number.
-     *  @exception IllegalActionException If the base class throws it.
-     *  @return True if it is ok to continue.
-     */
-    public boolean postfire() throws IllegalActionException {
-        _needNew = true;
-        return super.postfire();
-    }
-
     /** Initialize the random number generator with the seed, if it
      *  has been given.  A seed of zero is interpreted to mean that no
      *  seed is specified.  In such cases, a seed based on the current
@@ -139,20 +191,39 @@ public abstract class RandomSource extends Source {
     public void initialize() throws IllegalActionException {
         super.initialize();
 
-        long sd = ((LongToken) (seed.getToken())).longValue();
-
-        if (sd != 0) {
-            _random.setSeed(sd);
-        } else {
-            _random.setSeed(System.currentTimeMillis() + hashCode());
+        if (_random == null
+                || ((BooleanToken) resetOnEachRun.getToken()).booleanValue()) {
+            _createGenerator();
         }
-
         _needNew = true;
+    }
+
+    /** Calculate the next random number.
+     *  @exception IllegalActionException If the base class throws it.
+     *  @return True if it is ok to continue.
+     */
+    public boolean postfire() throws IllegalActionException {
+        _needNew = true;
+        return super.postfire();
     }
 
     ///////////////////////////////////////////////////////////////////
     ////                         protected methods                 ////
 
+    /** Create the random number generator using current parameter values. */
+    protected void _createGenerator() throws IllegalActionException {
+        long seedValue = ((LongToken) (seed.getToken())).longValue();
+        _generatorSeed = seedValue;
+
+        if (seedValue == 0L) {
+            seedValue = System.currentTimeMillis() + hashCode();
+        } else {
+            seedValue = seedValue + getFullName().hashCode();
+        }
+        _random = new Random(seedValue);
+        _needNew = true;
+    }
+    
     /** Generate a new random number.
      *  @exception IllegalActionException Not thrown in this base class.
      *  Derived classes may throw it if there are problems getting parameter
@@ -164,12 +235,12 @@ public abstract class RandomSource extends Source {
     ///////////////////////////////////////////////////////////////////
     ////                         protected variables               ////
 
-    /** The Random object. */
-    protected Random _random = new Random();
-
-    ///////////////////////////////////////////////////////////////////
-    ////                         private variables                 ////
+    /** The current value of the seed parameter. */
+    protected long _generatorSeed = 0L;
 
     /** Indicator that a new random number is needed. */
-    private boolean _needNew = false;
+    protected boolean _needNew = false;
+
+    /** The Random object. */
+    protected Random _random;
 }
