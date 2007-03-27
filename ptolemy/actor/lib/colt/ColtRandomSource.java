86948a4df28405b2bcea97acb0733db81864e8c2
eal <eal>
2007-03-27 01:19:17 +0000

    Extend RandomSource and change seed policy for values different from 0L.

--- ptolemy/actor/lib/colt/ColtRandomSource.java
+++ ptolemy/actor/lib/colt/ColtRandomSource.java
@@ -27,19 +27,15 @@
  */
 package ptolemy.actor.lib.colt;
 
-import java.util.Iterator;
+import java.util.Random;
 
-import ptolemy.actor.lib.Source;
-import ptolemy.data.BooleanToken;
+import ptolemy.actor.lib.RandomSource;
 import ptolemy.data.LongToken;
 import ptolemy.data.StringToken;
-import ptolemy.data.expr.SingletonParameter;
-import ptolemy.data.type.BaseType;
 import ptolemy.kernel.CompositeEntity;
 import ptolemy.kernel.util.Attribute;
 import ptolemy.kernel.util.IllegalActionException;
 import ptolemy.kernel.util.NameDuplicationException;
-import ptolemy.kernel.util.Workspace;
 import ptolemy.moml.SharedParameter;
 import cern.jet.random.engine.DRand;
 import cern.jet.random.engine.MersenneTwister;
@@ -52,44 +48,20 @@ import edu.cornell.lassp.houle.RngPack.Ranmar;
 //// ColtRandomSource
 
 /** Base class for Colt random sources.
- This base class manages the seed and the choice of
+ This base class manages the choice of
  random number generator class.  When the random number
  generator class is set for any one actor in a model,
  it gets set to match in all other actors within the
  same top-level container (unless this actor is within
- an EntityLibrary).  When the seed gets set
- to 0L in any one actor in a model, it gets set
- to 0L for all other actors within the same top-level
- container. When it gets set to anything other than
- 0L, then it will be set for all other actors in
- the model in a pattern that ensures that every
- actor has a different seed.
- <p>
- A seed of zero is interpreted to mean that no seed is specified,
- which means that each execution of the model could result in
- distinct data. For the value 0, the seed is set to
- System.currentTimeMillis() + hashCode(), which means that
- with extremely high probability, two distinct actors will have
- distinct seeds.  The seed is set when the seed parameter value
- is given, typically right after construction of the actor.
- Subsequent runs of the same model, therefore, will continue
- using the same seed.
- <p>
- If the <i>resetOnEachRun</i> parameter is true (it is
- false by default), then each run resets the random number
- generator. If the seed is non-zero, then this makes
- each run identical.  This is useful for constructing
- tests. If the seed is zero, then a new seed is generated
- on each run using the same technique described above
- (combining current time and the hash code).
-
+ an EntityLibrary).  
+ 
  @author David Bauer, Kostas Oikonomou, and Edward A. Lee
  @version $Id$
  @since Ptolemy II 4.1
  @Pt.ProposedRating Yellow (eal)
  @Pt.AcceptedRating Red (cxh)
  */
-public abstract class ColtRandomSource extends Source {
+public abstract class ColtRandomSource extends RandomSource {
     /** Construct an actor with the given container and name.
      *  @param container The container.
      *  @param name The name of this actor.
@@ -102,8 +74,6 @@ public abstract class ColtRandomSource extends Source {
             throws NameDuplicationException, IllegalActionException {
         super(container, name);
 
-        seed = new ColtSeedParameter(this, "seed", ColtRandomSource.class);
-
         generatorClass = new SharedParameter(this, "generatorClass",
                 ColtRandomSource.class, "DRand");
         generatorClass.setStringMode(true);
@@ -113,15 +83,6 @@ public abstract class ColtRandomSource extends Source {
         generatorClass.addChoice("Ranecu");
         generatorClass.addChoice("Ranlux");
         generatorClass.addChoice("Ranmar");
-
-        _inferSeed();
-
-        resetOnEachRun = new SharedParameter(this, "resetOnEachRun",
-                ColtRandomSource.class, "false");
-        resetOnEachRun.setTypeEquals(BaseType.BOOLEAN);
-
-        new SingletonParameter(trigger, "_showName")
-                .setToken(BooleanToken.TRUE);
     }
 
     ///////////////////////////////////////////////////////////////////
@@ -151,36 +112,10 @@ public abstract class ColtRandomSource extends Source {
      */
     public SharedParameter generatorClass;
 
-    /** If true, this parameter specifies that the random number
-     *  generator should be reset on each run of the model (in
-     *  the initialize() method). It is a boolean that defaults
-     *  to false. This is a shared parameter, meaning that changing
-     *  it somewhere in the model causes it to be changed everywhere
-     *  in the model.
-     */
-    public SharedParameter resetOnEachRun;
-
-    /** The seed that controls the random number generation.
-     *  A seed of zero is interpreted to mean that no seed is specified,
-     *  in which case, the seed is set to
-     *  System.currentTimeMillis() + hashCode(), which means that
-     *  with extremely high probability, two distinct actors will have
-     *  distinct seeds.  This is a "shared parameter", which means that
-     *  changing this parameter in any one actor will cause
-     *  a similar change to be applied to all other Colt
-     *  actors within the same top level (unless this is in
-     *  a library). In particular, if its value is set to zero,
-     *  then all other shared seeds will be set to zero. If it
-     *  is set to something other than zero, then all other shared
-     *  seeds are set to unique numbers.
-     *  This parameter has type long and defaults to zero.
-     */
-    public ColtSeedParameter seed;
-
     ///////////////////////////////////////////////////////////////////
     ////                         public methods                    ////
 
-    /** If the attribute is <i>generatorClass</i> or <i>seed</i>
+    /** If the attribute is <i>generatorClass</i>
      *  then create the base random number generator.
      *  @param attribute The attribute that changed.
      *  @exception IllegalActionException If the change is not acceptable
@@ -196,134 +131,26 @@ public abstract class ColtRandomSource extends Source {
                     && !generatorClassValue.equals(_generatorClassName)) {
                 _createGenerator();
             }
-        } else if (attribute == seed) {
-            long seedValue = ((LongToken) (seed.getToken())).longValue();
-
-            if (seedValue != _generatorSeed) {
-                _createGenerator();
-            }
         } else {
             super.attributeChanged(attribute);
         }
     }
 
-    /** Clone the actor into the specified workspace. This calls the
-     *  base class and then creates new ports and parameters.
-     *  @param workspace The workspace for the new object.
-     *  @return A new actor.
-     *  @exception CloneNotSupportedException If a derived class contains
-     *   an attribute that cannot be cloned.
-     */
-    public Object clone(Workspace workspace) throws CloneNotSupportedException {
-        ColtRandomSource newObject = (ColtRandomSource) (super.clone(workspace));
-        newObject._randomNumberGenerator = null;
-
-        // Force creation of a new generator.
-        try {
-            newObject._createGenerator();
-        } catch (IllegalActionException ex) {
-            throw new CloneNotSupportedException("Failed to create generator: "
-                    + ex);
-        }
-
-        return newObject;
-    }
-
-    /** Generate a new random number if this is the first firing
-     *  of the iteration.
-     *  @exception IllegalActionException If there is no director.
-     */
-    public void fire() throws IllegalActionException {
-        super.fire();
-
-        if (_needNew) {
-            _generateRandomNumber();
-            _needNew = false;
-        }
-    }
-
-    /** Initialize the random number generator with the seed, if it
-     *  has been given.  A seed of zero is interpreted to mean that no
-     *  seed is specified.  In such cases, a seed based on the current
-     *  time and this instance of a RandomSource is used to be fairly
-     *  sure that two identical sequences will not be returned.
-     *  @exception IllegalActionException If the parent class throws it.
-     */
-    public void initialize() throws IllegalActionException {
-        super.initialize();
-
-        if (((BooleanToken) resetOnEachRun.getToken()).booleanValue()) {
-            _createGenerator();
-        }
-
-        _needNew = true;
-    }
-
-    /** Calculate the next random number.
-     *  @exception IllegalActionException If the base class throws it.
-     *  @return True if it is ok to continue.
-     */
-    public boolean postfire() throws IllegalActionException {
-        _needNew = true;
-        return super.postfire();
-    }
-
-    /** Override the base class to infer the value of the
-     *  shared parameters from the new container.
-     *  @param container The new container.
-     *  @exception IllegalActionException If the action would result in a
-     *   recursive containment structure, or if
-     *   this entity and container are not in the same workspace.
-     *  @exception NameDuplicationException If the container already has
-     *   an entity with the name of this entity.
-     */
-    public void setContainer(CompositeEntity container)
-            throws IllegalActionException, NameDuplicationException {
-        super.setContainer(container);
-
-        if (generatorClass != null) {
-            generatorClass.inferValueFromContext("DRand");
-        }
-
-        if (resetOnEachRun != null) {
-            resetOnEachRun.inferValueFromContext("false");
-        }
-
-        _inferSeed();
-    }
-
     ///////////////////////////////////////////////////////////////////
     ////                         protected methods                 ////
 
-    /** Method that is called after _randomNumberGenerator is changed.
-     */
+    /** Method that is called after _randomNumberGenerator is changed. */
     protected abstract void _createdNewRandomNumberGenerator();
 
-    /** Generate a new random number.
-     *  @exception IllegalActionException Not thrown in this base class.
-     *  Derived classes may throw it if there is a problem processing
-     *  a parameter or if there is some other problem.
-     */
-    protected abstract void _generateRandomNumber()
-            throws IllegalActionException;
-
-    ///////////////////////////////////////////////////////////////////
-    ////                         protected variables               ////
-
-    /** The low-level random number generator.
-     */
-    protected RandomElement _randomNumberGenerator;
-
-    ///////////////////////////////////////////////////////////////////
-    ////                         private methods                   ////
-
     /** Create the random number generator using current parameter values. */
-    private void _createGenerator() throws IllegalActionException {
+    protected void _createGenerator() throws IllegalActionException {
         long seedValue = ((LongToken) (seed.getToken())).longValue();
         _generatorSeed = seedValue;
 
         if (seedValue == 0L) {
             seedValue = System.currentTimeMillis() + hashCode();
+        } else {
+            seedValue = seedValue + getFullName().hashCode();
         }
 
         StringToken generatorToken = ((StringToken) generatorClass.getToken());
@@ -347,47 +174,21 @@ public abstract class ColtRandomSource extends Source {
         } else if (generatorClassValue.equals("Ranmar")) {
             _randomNumberGenerator = new Ranmar((int) seedValue);
         }
+        
+        // In the base class, if _random is null, then initialize()
+        // will re-run this method. We don't want this, so even though
+        // we don't need it, we create an instance of Random.
+        _random = new Random();
 
         _createdNewRandomNumberGenerator();
     }
 
-    /** Infer the value of the seed from the container context.
-     *  The inferred value is zero if all shared parameters have value
-     *  zero, or if there are no shared parameters. Otherwise, it is
-     *  one larger than the largest value encountered.
+    ///////////////////////////////////////////////////////////////////
+    ////                         protected variables               ////
+
+    /** The low-level random number generator.
      */
-    private void _inferSeed() throws IllegalActionException {
-        // If the seed parameter has not yet been constructed, then
-        // do nothing.
-        if (seed == null) {
-            return;
-        }
-
-        String seedValue = "0L";
-        Iterator sharedParameters = seed.sharedParameterSet().iterator();
-        long value = 0L;
-        while (sharedParameters.hasNext()) {
-            ColtSeedParameter candidate = (ColtSeedParameter) sharedParameters
-                    .next();
-            if (candidate != seed) {
-                long candidateValue = ((LongToken) candidate.getToken())
-                        .longValue();
-                if (candidateValue != 0L) {
-                    if (candidateValue >= value) {
-                        value = candidateValue + 1L;
-                    }
-                }
-            }
-        }
-
-        seedValue = value + "L";
-        try {
-            seed.setSuppressingPropagation(true);
-            seed.setExpression(seedValue);
-        } finally {
-            seed.setSuppressingPropagation(false);
-        }
-    }
+    protected RandomElement _randomNumberGenerator;
 
     ///////////////////////////////////////////////////////////////////
     ////                         private variables                 ////
@@ -395,12 +196,4 @@ public abstract class ColtRandomSource extends Source {
     /** The class name of the current _randomNumberGenerator.
      */
     private String _generatorClassName;
-
-    /** The seed used by the current _randomNumberGenerator.
-     */
-    private long _generatorSeed = 0L;
-
-    /** Indicator that a new random number is needed.
-     */
-    private boolean _needNew = false;
 }
