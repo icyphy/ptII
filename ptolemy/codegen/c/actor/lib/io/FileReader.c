060b0b1d6bc656ddd386a9138ffea9eec9908c16
cxh <cxh>
2007-04-08 04:29:47 +0000

    Expand the single \n char to two chars, \ and n */.

--- ptolemy/codegen/c/actor/lib/io/FileReader.c
+++ ptolemy/codegen/c/actor/lib/io/FileReader.c
@@ -20,7 +20,10 @@ if (!($actorSymbol(filePtr) = fopen ("$fileName","r"))) {
 $actorSymbol(index) = 0;
 do {
     $actorSymbol(charRead) = fgetc($actorSymbol(filePtr));
-    if ($actorSymbol(index) >= $actorSymbol(length)) {
+    /* Resize.  If we have a \n it is expanded to two chars, \ and n, so we
+     * check the size + 1
+     */
+    if ($actorSymbol(index) + 1 >= $actorSymbol(length)) {
         $actorSymbol(length) *= 2;
         $ref(output) = (char*) realloc ($ref(output), ($actorSymbol(length) + 1) * sizeof(char));
         /* Solaris: strncpy does not add a null if only */
@@ -29,12 +32,20 @@ do {
     }
     if ($actorSymbol(charRead) != EOF ) {
         if ($actorSymbol(charRead) != '\r') {
-            $ref(output)[$actorSymbol(index)++] = $actorSymbol(charRead);
+            if ($actorSymbol(charRead) == '\n') {
+                /* Expand the single \n char to two chars, \ and n */
+                $ref(output)[$actorSymbol(index)++] = '\\';
+                $ref(output)[$actorSymbol(index)++] = 'n';
+            } else {
+                $ref(output)[$actorSymbol(index)++] = $actorSymbol(charRead);
+            }
         } else {
             fprintf(stderr, "Warning: dropping a \\r char\n");
         }
     } else {
-        $ref(output)[$actorSymbol(index)++] = '\n';
+        /* Expand the single \n char to two chars, \ and n */
+        $ref(output)[$actorSymbol(index)++] = '\\';
+        $ref(output)[$actorSymbol(index)++] = 'n';
     }
 } while ( $actorSymbol(charRead) != EOF );
 /**/
