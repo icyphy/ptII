2a06358f2e481ad2f2b4c08853eacdb63af41857
cxh <cxh>
2007-04-09 22:59:32 +0000

    -Wall: fix "warning: suggest parentheses around && within ||"

--- ptolemy/codegen/c/actor/lib/Test.c
+++ ptolemy/codegen/c/actor/lib/Test.c
@@ -83,9 +83,9 @@ if ($actorSymbol(numberOfTokensSeen) < $size(correctValues)
 
 /***BooleanBlock($channel)***/
 $actorSymbol(numberOfTokensSeen)++;
-if ($actorSymbol(numberOfTokensSeen) < $size(correctValues)
+if (($actorSymbol(numberOfTokensSeen) < $size(correctValues)
         && (!$ref(correctValues, $actorSymbol(numberOfTokensSeen))
-                && $ref(input#$channel))
+                && $ref(input#$channel)))
         || ($ref(correctValues, $actorSymbol(numberOfTokensSeen))
                 && !$ref(input#$channel)) ) {
     printf("\nTest $actorSymbol($channel) fails in iteration %d.\n Value was a boolean of value: %s. Should have been a boolean of value: %s\n",
@@ -103,9 +103,9 @@ if ($channel == 0) {
 /* $channel of $actorSymbol() */
 $actorSymbol(correctValuesThisFiring_$channel) =
 $ref(correctValues, $actorSymbol(numberOfTokensSeen));
-if ($actorSymbol(numberOfTokensSeen) < $size(correctValues)
+if (($actorSymbol(numberOfTokensSeen) < $size(correctValues)
         && ((!Array_get($actorSymbol(correctValuesThisFiring_$channel), $channel).payload.Boolean
-                    && $ref(input#$channel))
+                    && $ref(input#$channel)))
                 || (Array_get($actorSymbol(correctValuesThisFiring_$channel), $channel).payload.Boolean
                         && !$ref(input#$channel)))) {
     printf("\nTest $actorSymbol($channel) fails in iteration %d.\n Value was a boolean of value: %s. Should have been a boolean of value: %s\n",
