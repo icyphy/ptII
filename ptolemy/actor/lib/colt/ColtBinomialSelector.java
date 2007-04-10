3f11b0fa4d70a43a3e31af891afeadb4b0c46168
cxh <cxh>
2007-04-10 15:02:47 +0000

    Removed commented out code

--- ptolemy/actor/lib/colt/ColtBinomialSelector.java
+++ ptolemy/actor/lib/colt/ColtBinomialSelector.java
@@ -173,29 +173,6 @@ public class ColtBinomialSelector extends ColtRandomSource {
         }
     }
 
-    //     protected void _generateRandomNumber() throws IllegalActionException {
-
-    //         // The following logic protects the binomial generation call
-    //         // because the Colt library will throw an exception (even
-    //         // though they are valid limit cases).  The limits can occur
-    //         // during the course of a simulation, and this actor should
-    //         // produce valid results if they do occur.
-
-    //         int nValue = ((IntToken) n.getToken()).intValue();
-    //         if (nValue == 0) {
-    //             _current = 0;
-    //         } else {
-    //             double pValue = ((DoubleToken) p.getToken()).doubleValue();
-    //             if (pValue == 0.0) {
-    //                 _current = 0;
-    //             } else if (pValue == 1.0) {
-    //                 _current = nValue;
-    //             } else {
-    //                 _current = _generator.nextInt(nValue, pValue);
-    //             }
-    //         }
-    //     }
-
     ///////////////////////////////////////////////////////////////////
     ////                         private variables                 ////
 
