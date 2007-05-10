67f3375dc6616c243c7aadb7d1914db3a1e9d562
cxh <cxh>
2007-05-10 00:17:15 +0000

    Fix up copyright date and @since tag

--- ptolemy/actor/lib/jni/EmbeddedCActor.java
+++ ptolemy/actor/lib/jni/EmbeddedCActor.java
@@ -1,6 +1,6 @@
 /* An actor that executes compiled embedded C code.
 
- Copyright (c) 2003-2006 The Regents of the University of California.
+ Copyright (c) 2007 The Regents of the University of California.
  All rights reserved.
  Permission is hereby granted, without written agreement and without
  license or royalty fees, to use, copy, modify, and distribute this
@@ -47,7 +47,7 @@ import ptolemy.vergil.toolbox.TextEditorConfigureFactory;
 
  @author Gang Zhou
  @version $Id$
- @since Ptolemy II 6.0
+ @since Ptolemy II 6.1
  @Pt.ProposedRating Red (zgang)
  @Pt.AcceptedRating Red (zgang)
  */
