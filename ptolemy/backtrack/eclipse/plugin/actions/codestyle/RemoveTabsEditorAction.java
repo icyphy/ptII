506e2c7e4e6d349a8e3d2d4d9de5bd61d805232a
tfeng <tfeng>
2007-06-06 02:38:00 +0000

    Remove tabs action added to the eclipse plugin; icons are renewed.

---
+++ ptolemy/backtrack/eclipse/plugin/actions/codestyle/RemoveTabsEditorAction.java
@@ -0,0 +1,42 @@
+package ptolemy.backtrack.eclipse.plugin.actions.codestyle;
+
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.IWorkbenchWindowActionDelegate;
+
+import ptolemy.backtrack.eclipse.plugin.editor.MultiPageCompilationUnitEditor;
+
+public class RemoveTabsEditorAction implements IWorkbenchWindowActionDelegate {
+
+    public void dispose() {
+    }
+
+    public void init(IWorkbenchWindow window) {
+        _window = window;
+    }
+
+    /** Sort the source code in the editor.
+     * 
+     *  @param action The action proxy (not used in this method).
+     */
+    public void run(IAction action) {
+        IEditorPart editorPart = _window.getActivePage().getActiveEditor();
+        MultiPageCompilationUnitEditor editor =
+            (MultiPageCompilationUnitEditor) editorPart;
+
+        RemoveTabsUtility.removeTabs(editor);
+    }
+
+    /** Handle the change of selection.
+     * 
+     *  @param action The action proxy (not used in this method).
+     *  @param selection The new selection (not used in this method).
+     */
+    public void selectionChanged(IAction action, ISelection selection) {
+    }
+
+    private IWorkbenchWindow _window;
+
+}
