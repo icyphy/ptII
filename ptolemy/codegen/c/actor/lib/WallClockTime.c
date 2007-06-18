edec371ae80e52dc90ee8f8d7041e9c9bf35eb85
cxh <cxh>
2007-06-18 15:56:26 +0000

    Added fireBlock

--- ptolemy/codegen/c/actor/lib/WallClockTime.c
+++ ptolemy/codegen/c/actor/lib/WallClockTime.c
@@ -6,6 +6,10 @@
     $actorSymbol(startTime) = time((time_t*) NULL);
 /**/
 
+/***fireBlock***/
+    $ref(output) = time((time_t*) NULL) - $actorSymbol(startTime);
+/**/
+
 /***transferBlock($channel)***/
     $ref(output) = time((time_t*) NULL) - $actorSymbol(startTime);
     $ref(passthrough#$channel) = $ref(trigger#$channel);
