d084b577df7adf7985a62085966df08a13917ad9
eal <eal>
2007-07-23 19:57:29 +0000

    Debug message improvement.

--- ptolemy/actor/AtomicActor.java
+++ ptolemy/actor/AtomicActor.java
@@ -460,7 +460,7 @@ public class AtomicActor extends ComponentEntity implements Actor {
      */
     public boolean prefire() throws IllegalActionException {
         if (_debugging) {
-            _debug("Called prefire(), which returns true");
+            _debug("Called prefire().");
         }
 
         return true;
