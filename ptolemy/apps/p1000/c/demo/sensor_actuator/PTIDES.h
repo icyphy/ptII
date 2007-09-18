dfc6b99a9898b4436461ed8b11d01b5802b47062
jiazou <jiazou>
2007-09-18 01:38:41 +0000

    sensor actuator demo for PTIDES

--- ptolemy/apps/p1000/c/demo/sensor_actuator/PTIDES.h
+++ ptolemy/apps/p1000/c/demo/sensor_actuator/PTIDES.h
@@ -4,13 +4,10 @@
 
 //Let's just assume these...
 //HOWEVER IF NSECS IS NON-0, WE HAVE TO CHANGE THE PROCEDURE TO MAKE THE CALCULATION CORRECT!!!!!
-#define MODEL_DELAY_SECS 2
-#define MODEL_DELAY_NSECS 0
-#define BOUNDED_DELAY_SECS 1
-#define BOUNDED_DELAY_NSECS 0
-
-int static_adj_secs = BOUNDED_DELAY_SECS - MODEL_DELAY_SECS;
-int static_adj_nsecs= BOUNDED_DELAY_NSECS- MODEL_DELAY_NSECS;
+#define MODEL_DELAY_SECS 0
+#define MODEL_DELAY_NSECS 7000000
+#define BOUNDED_DELAY_SECS 0
+#define BOUNDED_DELAY_NSECS 5000000
 
 //ALL STRUCTs
 typedef struct act {
@@ -31,38 +28,56 @@ typedef struct act {
     //fire();
     //postfire();
     //wrapup();
-} ACTOR;
+} Actor;
 
 typedef struct{
     int int_value;
-} VALUE;
+} Value;
 
 typedef struct{
     unsigned int secs;
     unsigned int nsecs;
     int microstep;
-} TAG;
+} Tag;
 
 typedef struct{
-    VALUE this_value;
-    TAG this_tag;
-    TAG real_tag;
+    Value this_value;
+    Tag this_tag;
+    Tag real_tag;
 
-    ACTOR* actor_from;
-    ACTOR* actor_to_fire;
+    Actor* actor_from;
+    Actor* actor_to_fire;
     //an event would only have 1 actor_to_fire
     //ACTOR* actor_to_fire2;
-} EVENT;
+    
+    //need to have a bit that tell us whether static timing analysis has been done
+    int sta_done;
+} Event;
 
 //building the linked list
 //where when inserting, we insert by time stamp
 typedef struct el{
-    EVENT this_event;
+    Event this_event;
     struct el *next;
-} EVENT_LINK;
+} Event_Link;
 
-
-
+void actuator_fire(Actor*, Event*);
+void clock_init(Actor*);
+void computation_fire(Actor*, Event*);
+void event_add(Event);
+void event_insert(Event);
+void event_pop();
+void execute_event();
+void firing_actor(Event*);
+void merge_fire(Actor*, Event*);
+void model_delay_fire(Actor*, Event*);
+void processEvents();
+void receive_init(Actor*);
+void sensor_run(Actor*);
+void sensor_init(Actor*);
+void static_timing_analysis(Event*);
+void thread_timed_interrupt();
+void transmit_fire(Actor*, Event*);
 
 //IN EnDecode.c
 void encodeHwNsec( FPGA_TIME*, const unsigned int, const unsigned int);
