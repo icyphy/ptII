9a5ac1ff189106a86a8258780a15f447cf8feab9
cxh <cxh>
2007-10-02 17:13:34 +0000

    FIXME: FindBugs says that InputFrame "defines compareTo(Object) and uses Object.equals()"

--- ptolemy/actor/lib/hoc/RealTimeComposite.java
+++ ptolemy/actor/lib/hoc/RealTimeComposite.java
@@ -325,6 +325,28 @@ public class RealTimeComposite extends MirrorComposite {
      *  @param theTokens The tokens in the input events.
      */
     private class InputFrame implements Delayed {
+        /* FIXME: FindBugs says that InputFrame 
+         * "defines compareTo(Object) and uses Object.equals()"
+         * http://findbugs.sourceforge.net/bugDescriptions.html#EQ_COMPARETO_USE_OBJECT_EQUALS
+         * says:
+         * "This class defines a compareTo(...) method but inherits its
+         * equals() method from java.lang.Object. Generally, the value
+         * of compareTo should return zero if and only if equals
+         * returns true. If this is violated, weird and unpredictable
+         * failures will occur in classes such as PriorityQueue. In
+         * Java 5 the PriorityQueue.remove method uses the compareTo
+         * method, while in Java 6 it uses the equals method.
+         *
+         *  From the JavaDoc for the compareTo method in the
+         *  Comparable interface:
+         *  
+         * It is strongly recommended, but not strictly required that
+         * (x.compareTo(y)==0) == (x.equals(y)). Generally speaking,
+         * any class that implements the Comparable interface and
+         * violates this condition should clearly indicate this
+         * fact. The recommended language is "Note: this class has a
+         * natural ordering that is inconsistent with equals." "
+         */  
         public InputFrame(Time theTime, List<QueuedToken> theTokens) {
             tokens = theTokens;
             time = theTime;
@@ -348,9 +370,9 @@ public class RealTimeComposite extends MirrorComposite {
         }
 
         public int compareTo(Delayed frame) {
-            // NOTE: We assume that only comparisons against instances of Frame will be done.
-            // Is this safe?
-            return time.compareTo(((InputFrame) frame).time);
+            // NOTE: We assume that only comparisons against instances
+            // of Frame will be done.  Is this safe?
+            return time.compareTo(((InputFrame)frame).time);
         }
     }
 
@@ -478,6 +500,9 @@ public class RealTimeComposite extends MirrorComposite {
                     Time responseTime = _responseTimes.peek();
                     if (responseTime != null
                             && responseTime.equals(environmentTime)) {
+
+                        // FIXME: Findbugs says that the next line:
+                        // "ignores return value of java.util.Queue.poll()"
                         _responseTimes.poll();
                         // Time matches.  Compare to real time.
                         long realTimeMillis = System.currentTimeMillis()
