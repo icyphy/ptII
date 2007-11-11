f12c99b4cc8c431bee827ca1a5ad27c5a9293ad4
cxh <cxh>
2007-11-11 22:23:41 +0000

    The IOException differs between Windows and Solaris, so we truncate the message.

--- ptolemy/moml/test/StreamErrorHandler.tcl
+++ ptolemy/moml/test/StreamErrorHandler.tcl
@@ -106,14 +106,14 @@ test StreamErrorHandler-1.2 {Create a stream that we can read} {
 		$output2 {$PTII} output3
 
 
-    # Only take the first few characters
-    list [string range $output3 0 507] {...}
+    # The IOException differs between Windows and Solaris,
+    # so we truncate the message.
+    list [string range $output3 0 368] {...}
 } {{Error encountered in:
 <entity name="b" class="ptolemy.moml.test.notAClass">
 ptolemy.kernel.util.IllegalActionException: Cannot find class: ptolemy.moml.test.notAClass
 Because:
--- $PTII/ptolemy/moml/test/ptolemy/moml/test/notAClass.moml (The system cannot find the path specified)
+-- $PTII/ptolemy/moml/test/ptolemy/moml/test/notAClass.moml (No such file or directory)
 -- XML file not found relative to classpath.
 -- $PTII/ptolemy/moml/test/ptolemy/moml/test/notAClass.moml
-ptolemy/moml/test/notAClass.moml (The system cannot find the path specified)
- in file:$PTII/ptolemy/moml/test/ at line 5 an} ...}
+} ...}
