82621e7ef00085ae28bedece13a903b78a8a78f0
cxh <cxh>
2007-12-18 19:44:22 +0000

    Added ifdefs for other AVR chips such as Arduino/atmega168

--- ptolemy/codegen/c/actor/lib/gui/LEDMatrix.c
+++ ptolemy/codegen/c/actor/lib/gui/LEDMatrix.c
@@ -39,7 +39,9 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
    for 1Mhz speed (chip is shipped at 1Mhz) use:
    #define F_CPU 1000000UL
 */
+#ifndef F_CPU
 #define F_CPU 8000000UL
+#endif /* F_CPU */
 //WinAVR 20040404 does not have util/delay.h, so _delay_ms might not be defined
 //#include <avr/delay.h>
 
@@ -47,6 +49,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #include <util/delay.h>
 
 //MACROS FOR AVR ATmega16
+#if defined(DDRA)
 #define row0_output DDRA|= _BV(PA0)
 #define row1_output DDRA|= _BV(PA1)
 #define row2_output DDRA|= _BV(PA2)
@@ -55,7 +58,14 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #define row5_output DDRA|= _BV(PA5)
 #define row6_output DDRA|= _BV(PA6)
 #define row7_output DDRA|= _BV(PA7)
+#endif /* DDRA */
 
+#ifndef row0_output
+// For the Arduino
+int ledPin =13;
+#endif /* row0_output */
+
+#if defined(PORTA)
 #define row0_high PORTA|= _BV(PA0)
 #define row1_high PORTA|= _BV(PA1)
 #define row2_high PORTA|= _BV(PA2)
@@ -73,6 +83,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #define row5_low PORTA &= ~_BV(PA5)
 #define row6_low PORTA &= ~_BV(PA6)
 #define row7_low PORTA &= ~_BV(PA7)
+#endif /* PORTA */
 
 #define col0_output DDRC|= _BV(PC0)
 #define col1_output DDRC|= _BV(PC1)
@@ -86,29 +97,37 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #define col8_output DDRB|= _BV(PB1)
 #define col9_output DDRB|= _BV(PB0)
 
+#if defined(PORTC)
 #define col0_high PORTC|= _BV(PC0)
 #define col1_high PORTC|= _BV(PC1)
 #define col2_high PORTC|= _BV(PC2)
 #define col3_high PORTC|= _BV(PC3)
 #define col4_high PORTC|= _BV(PC4)
+#endif /* PORTC */
 
+#if defined(PORTB)
 #define col5_high PORTB|= _BV(PB4)
 #define col6_high PORTB|= _BV(PB3)
 #define col7_high PORTB|= _BV(PB2)
 #define col8_high PORTB|= _BV(PB1)
 #define col9_high PORTB|= _BV(PB0)
+#endif /* PORTB */
 
+#if defined(PORTC)
 #define col0_low PORTC &= ~_BV(PC0)
 #define col1_low PORTC &= ~_BV(PC1)
 #define col2_low PORTC &= ~_BV(PC2)
 #define col3_low PORTC &= ~_BV(PC3)
 #define col4_low PORTC &= ~_BV(PC4)
+#endif /* PORTC */
 
+#if defined(PORTB)
 #define col5_low PORTB &= ~_BV(PB4)
 #define col6_low PORTB &= ~_BV(PB3)
 #define col7_low PORTB &= ~_BV(PB2)
 #define col8_low PORTB &= ~_BV(PB1)
 #define col9_low PORTB &= ~_BV(PB0)
+#endif /* PORTB */
 #endif /* __AVR__ */
 
 /* End of LEDMatrix defines */
@@ -118,6 +137,7 @@ void all_on (void)
 {
 #ifdef __AVR__
         //rows low
+#if defined(row0_low)
         row0_low;
         row1_low;
         row2_low;
@@ -138,6 +158,7 @@ void all_on (void)
         col7_high;
         col8_high;
         col9_high;
+#endif /* row0_low */
 #endif /* __AVR__ */
 }
 
@@ -145,6 +166,7 @@ void all_on (void)
 void all_off (void)
 {
 #ifdef __AVR__
+#if defined(row0_high)
         //rows high
         row0_high;
         row1_high;
@@ -166,12 +188,14 @@ void all_off (void)
         col7_low;
         col8_low;
         col9_low;
+#endif /* row0_high */
 #endif /* __AVR__ */
 }
 
 //initializes the pins to be used for the display as outputs
 void initialize_tank_pins_as_output (void) {
 #ifdef __AVR__
+#if defined(row0_output)
     row0_output;
     row1_output;
     row2_output;
@@ -191,7 +215,10 @@ void initialize_tank_pins_as_output (void) {
     col7_output;
     col8_output;
     col9_output;
+#else /* row0_output */
 
+    pinMode(ledPin, OUTPUT);
+#endif /* row0_output */
     all_off();
 #endif
 }
@@ -224,60 +251,100 @@ if ($ref(control)) {
 if ($ref(control)) {
     switch ($ref(row)) {
     case 0:
+#ifdef row0_high
         row0_high;
+#else
+        digitalWrite(ledPin, HIGH);
+#endif
         break;
     case 1:
+#ifdef row1_high
         row1_high;
+#endif
         break;
     case 2:
+#ifdef row2_high
         row2_high;
+#endif
         break;
     case 3:
+#ifdef row3_high
         row3_high;
+#endif
         break;
     case 4:
+#ifdef row4_high
         row4_high;
+#endif
         break;
     case 5:
+#ifdef row5_high
         row5_high;
+#endif
         break;
     case 6:
+#ifdef row6_high
         row6_high;
+#endif
         break;
     case 7:
+#ifdef row7_high
         row7_high;
+#endif
         break;
     }
     switch ($ref(column)) {
     case 0:
+#ifdef col0_low
         col0_low;
+#else
+        digitalWrite(ledPin, LOW);
+#endif
         break;
     case 1:
+#ifdef col1_low
         col1_low;
+#endif
         break;
     case 2:
+#ifdef col2_low
         col2_low;
+#endif
         break;
     case 3:
+#ifdef col3_low
         col3_low;
+#endif
         break;
     case 4:
+#ifdef col4_low
         col4_low;
+#endif
         break;
     case 5:
+#ifdef col5_low
         col5_low;
+#endif
         break;
     case 6:
+#ifdef col6_low
         col6_low;
+#endif
         break;
     case 7:
+#ifdef col7_low
         col7_low;
+#endif
         break;
     case 8:
+#ifdef col8_low
         col8_low;
+#endif
         break;
     case 9:
+#ifdef col9_low
         col9_low;
+#endif
         break;
     }
 
