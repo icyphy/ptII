73e54ed6009c7572cc5556983844c20d79136982
cxh <cxh>
2007-12-18 00:51:11 +0000

    2.1: error message changed slightly

--- ptolemy/actor/lib/test/Publisher.tcl
+++ ptolemy/actor/lib/test/Publisher.tcl
@@ -234,8 +234,8 @@ test Publisher-2.1 {Instantiate twice a class that has a publisher} {
 
     catch {$manager execute} errMsg
     list $errMsg
-} {{ptolemy.kernel.util.IllegalActionException: There is already a publisher using channel "channel1": .PublisherSubscriberInClass.Instance2OfCompositeActor.Publisher
-  in .PublisherSubscriberInClass.Instance1OfCompositeActor.Publisher}}
+} {{ptolemy.kernel.util.IllegalActionException: There is already a publisher using channel "channel1": .PublisherSubscriberInClass.Instance1OfCompositeActor.Publisher
+  in .PublisherSubscriberInClass.Instance2OfCompositeActor.Publisher}}
 
 
 ######################################################################
