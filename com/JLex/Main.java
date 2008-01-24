3c00048a9ceae6c0ffc4622fe85af2dc6a9f5629
cxh <cxh>
2008-01-24 02:39:19 +0000

    Spelling: debuging

--- com/JLex/Main.java
+++ com/JLex/Main.java
@@ -3243,7 +3243,7 @@ public class Main {
             return;
         }
 
-        /* Note: For debuging, it may be helpful to remove the try/catch
+        /* Note: For debugging, it may be helpful to remove the try/catch
          block and permit the Exception to propagate to the top level.
          This gives more information. */
         try {
