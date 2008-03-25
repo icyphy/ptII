77b0afa64094b3b4601cdfdc63cbfde0d33173e9
cxh <cxh>
2008-03-25 22:07:49 +0000

    Fixed copyright date

--- ptolemy/actor/gt/data/FastLinkedList.java
+++ ptolemy/actor/gt/data/FastLinkedList.java
@@ -1,6 +1,6 @@
 /* Linked list that provides efficient add and remove functions.
 
-@Copyright (c) 1997-2008 The Regents of the University of California.
+@Copyright (c) 2007-2008 The Regents of the University of California.
 All rights reserved.
 
 Permission is hereby granted, without written agreement and without
