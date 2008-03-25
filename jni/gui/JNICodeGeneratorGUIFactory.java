2a943fc1366b53316ae1d5db5dab996bb4ebf5d9
cxh <cxh>
2008-03-25 22:33:13 +0000

    Changed copyright to 2008

--- jni/gui/JNICodeGeneratorGUIFactory.java
+++ jni/gui/JNICodeGeneratorGUIFactory.java
@@ -1,7 +1,7 @@
 /* An attribute that creates an editor to that builds support code
  for the Java Native Interface.
 
- Copyright (c) 2006 The Regents of the University of California.
+ Copyright (c) 2006-2008 The Regents of the University of California.
  All rights reserved.
  Permission is hereby granted, without written agreement and without
  license or royalty fees, to use, copy, modify, and distribute this
