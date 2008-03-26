e78e1bc72973630587b902d2b367399379181099
cxh <cxh>
2008-03-26 01:06:25 +0000

    Removed files that are not being shipped

--- ptolemy/apps/p1000/cpp/hwTime.h
+++
@@ -1,9 +0,0 @@
-#ifndef TIME_H
-#define TIME_H
-
-struct Time {
-    unsigned int ms;
-    unsigned int ns;  //FIXME: what are the fields of this struct?
-};
-
-#endif 
--- ptolemy/apps/p1000/m4/README.txt
+++
@@ -1,1 +0,0 @@
-autoreconf requires this directory
