c8abf1187380495b1505f6b5064433cc43818115
jiazou <jiazou>
2008-03-28 22:56:15 +0000

    update

--- ptolemy/apps/jacquard/cg/cg.c
+++ ptolemy/apps/jacquard/cg/cg.c
@@ -58,7 +58,7 @@ printf("T[%d]: localSum = %g\n", MYTHREAD, localSum);
 //printf("T[%d]: should be all the same: globalSum = %d\n", MYTHREAD, *globalSum);
 
     for (i = 0; i < THREADS; ++i) {
-        double* temp = (double*) globalSumEach[i];
+        double* temp = (double*) (&globalSumEach[i]);
         //*globalSum += globalSumEach[i];
         printf("T[%d]: temp globalSumEach[%d] = %g\n", MYTHREAD, i, *temp);
         *globalSum += temp;
