f0de9fb30a0a1aa975e6556cb89667105bf792ed
tfeng <tfeng>
2008-04-10 09:30:30 +0000

    Excessive importation removed.

--- ptolemy/data/expr/ParseTreeEvaluator.java
+++ ptolemy/data/expr/ParseTreeEvaluator.java
@@ -31,7 +31,6 @@ import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
-import java.util.Collection;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
