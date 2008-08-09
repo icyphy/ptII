c27f6a80bfa5dc25277b72f9d92872436d9058ee
cxh <cxh>
2008-08-09 00:38:29 +0000

    setUserLevelDocumentation: reparse the attribute

--- ptolemy/vergil/basic/test/KeplerDocumentationAttribute.tcl
+++ ptolemy/vergil/basic/test/KeplerDocumentationAttribute.tcl
@@ -75,10 +75,23 @@ test KeplerDocumentationAttribute-3.1 {setUserLevelDocumentation} {
     # Test for bogus html
     $kda setUserLevelDocumentation {<p><hello>The StringConstant actor outputs a string specified via the actor's value parameter.</p>}
     $kda exportMoML $stringWriter 2 myKDA
-    set exported "<entity name=\"model3\" class=\"ptolemy.actor.TypedCompositeActor\"><property name=\"myKDA\" class=\"ptolemy.vergil.basic.KeplerDocumentationAttribute\"> \n [$stringWriter toString]\n </property></entity>"
+    set exported "<entity name=\"model3\" class=\"ptolemy.actor.TypedCompositeActor\">\n [$stringWriter toString]\n</entity>"
     set parser [java::new ptolemy.moml.MoMLParser]
     set toplevel [$parser parse $exported]
-} {}
+    list [$toplevel exportMoML myMoML]
+} {{<?xml version="1.0" standalone="no"?>
+<!DOCTYPE entity PUBLIC "-//UC Berkeley//DTD MoML 1//EN"
+    "http://ptolemy.eecs.berkeley.edu/xml/dtd/MoML_1.dtd">
+<entity name="myMoML" class="ptolemy.actor.TypedCompositeActor">
+    <property name="_createdBy" class="ptolemy.kernel.attributes.VersionAttribute" value="7.1.devel">
+    </property>
+<property name="myKDA" class="ptolemy.vergil.basic.KeplerDocumentationAttribute">
+<property name="description" class="ptolemy.kernel.util.ConfigurableAttribute"><configure>null</configure></property>
+<property name="author" class="ptolemy.kernel.util.ConfigurableAttribute"><configure>null</configure></property>
+<property name="version" class="ptolemy.kernel.util.ConfigurableAttribute"><configure>null</configure></property>
+<property name="userLevelDocumentation" class="ptolemy.kernel.util.ConfigurableAttribute"><configure>&lt;p&gt;&lt;hello&gt;The StringConstant actor outputs a string specified via the actor's value parameter.&lt;/p&gt;</configure></property>
+</property></entity>
+}}
 
 test KeplerDocumentatonAttribute-4.1 {addPort} {
     set kda [java::new ptolemy.vergil.basic.KeplerDocumentationAttribute]
