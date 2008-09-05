5b92251a4785dd9c04d4d8a6af0ae9fd782783da
mankit <mankit>
2008-09-05 21:34:14 +0000

    fixed more javadoc warnings.

--- ptolemy/data/properties/token/PropertyCombineFSMHelper.java
+++ ptolemy/data/properties/token/PropertyCombineFSMHelper.java
@@ -52,8 +52,8 @@ public class PropertyCombineFSMHelper extends PropertyCombineCompositeHelper {
      * helper class for any ActomicActor that does not have a
      * specific defined helper class. Default actor constraints
      * are set for this helper. 
+     * @param solver The given solver.
      * @param actor The given ActomicActor.
-     * @param lattice The staticDynamic lattice.
      * @throws IllegalActionException 
      */
     public PropertyCombineFSMHelper(PropertyCombineSolver solver, 
