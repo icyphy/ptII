70022f92a77718931d2643c4ab0c4f1de36a3b92
cxh <cxh>
2008-09-15 16:32:05 +0000

    Organized imports

--- ptolemy/matlab/Expression.java
+++ ptolemy/matlab/Expression.java
@@ -33,8 +33,6 @@ package ptolemy.matlab;
 
 import java.util.HashSet;
 import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.Set;
 import java.util.StringTokenizer;
 
