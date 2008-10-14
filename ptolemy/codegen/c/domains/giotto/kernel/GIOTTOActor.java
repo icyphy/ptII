005c038b3646730aa750aef18ed9a345d6c4d481
sssf <sssf>
2008-10-14 05:20:51 +0000

    initial attempt a giotto codegen. this does not work at the moment. Hopefully the nightly build doesn't know about it and doesn't fail it tonight

---
+++ ptolemy/codegen/c/domains/giotto/kernel/GIOTTOActor.java
@@ -0,0 +1,76 @@
+/*Not sure what should go here yet*/
+
+/* Code generator helper for FSMActor.
+
+ Copyright (c) 2005-2006 The Regents of the University of California.
+ All rights reserved.
+ Permission is hereby granted, without written agreement and without
+ license or royalty fees, to use, copy, modify, and distribute this
+ software and its documentation for any purpose, provided that the above
+ copyright notice and the following two paragraphs appear in all copies
+ of this software.
+
+ IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+ FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+ ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+ THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+ SUCH DAMAGE.
+
+ THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+ INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+ PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+ CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ ENHANCEMENTS, OR MODIFICATIONS.
+
+ PT_COPYRIGHT_VERSION_2
+ COPYRIGHTENDKEY
+
+ */
+package ptolemy.codegen.c.domains.fsm.kernel;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+
+import ptolemy.actor.Actor;
+import ptolemy.actor.Director;
+import ptolemy.actor.IOPort;
+import ptolemy.codegen.c.kernel.CCodeGeneratorHelper;
+import ptolemy.codegen.kernel.ActorCodeGenerator;
+import ptolemy.codegen.kernel.CodeGeneratorHelper;
+import ptolemy.codegen.kernel.ComponentCodeGenerator;
+import ptolemy.codegen.kernel.ParseTreeCodeGenerator;
+import ptolemy.data.BooleanToken;
+import ptolemy.data.ObjectToken;
+import ptolemy.data.Token;
+import ptolemy.data.expr.ASTPtRootNode;
+import ptolemy.data.expr.PtParser;
+import ptolemy.data.expr.Variable;
+import ptolemy.domains.fsm.kernel.AbstractActionsAttribute;
+import ptolemy.domains.fsm.kernel.State;
+import ptolemy.domains.fsm.kernel.Transition;
+import ptolemy.kernel.util.IllegalActionException;
+import ptolemy.kernel.util.NamedObj;
+
+//////////////////////////////////////////////////////////////////////////
+//// GIOTTOActor
+
+/**
+ Code generator helper for GIOTTOActor.
+
+ @author Shanna-Shaye Forbes
+ @version $Id: GIOTTOActor.java 50322 2008-08-07 13:53:48Z cxh $
+ @since Ptolemy II 6.0
+ @Pt.ProposedRating Red (sssf)
+ @Pt.AcceptedRating Red (sssf)
+ */
+public class GIOTTOActor extends CCodeGeneratorHelper {
+    /** Construct the code generator helper associated with the given FSMActor.
+     *  @param component The associated component.
+     */
+    public GIOTTOActor(ptolemy.domains.giotto.kernel.GIOTTOActor component) {
+        super(component);
+    }
