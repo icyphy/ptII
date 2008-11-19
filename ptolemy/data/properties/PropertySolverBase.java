12d92054ca877cc06d58f13f6dbfa368800f1599
cxh <cxh>
2008-11-19 22:54:32 +0000

    javadoc cleanups

--- ptolemy/data/properties/PropertySolverBase.java
+++ ptolemy/data/properties/PropertySolverBase.java
@@ -1,6 +1,6 @@
 /*  The base abstract class for a property solver.
 
- Copyright (c) 1998-2005 The Regents of the University of California.
+ Copyright (c) 2008 The Regents of the University of California.
  All rights reserved.
  Permission is hereby granted, without written agreement and without
  license or royalty fees, to use, copy, modify, and distribute this
@@ -56,13 +56,14 @@ import ptolemy.kernel.util.Settable;
 /**
 The base abstract class for a property solver. 
 
-The base class provides the core functionality for property resolution.
-It provides a method to create PropertyHelper for any given model
-component. The model component can be an object of any Ptolemy class 
-(e.g. ASTPtRootNode, Sink, Entity, and FSMActor). A model component,
-in turn, may have one or multiple property-able objects.
+<p>The base class provides the core functionality for property
+resolution.  It provides a method to create a PropertyHelper for any
+given model component. The model component can be an object of any
+Ptolemy class (e.g. ASTPtRootNode, Sink, Entity, and FSMActor). A
+model component, in turn, may have one or multiple property-able
+objects.
 
-A PropertySolver is associated with strictly one use-case. 
+<p>A PropertySolver is associated with strictly one use-case. 
 PropertySolvers with the same use-case are considered equivalent. 
 Subclasses need to associate the solver with an unique use-case by 
 providing implementation for {@link PropertySolverBase#getUseCaseName()}, 
@@ -71,11 +72,12 @@ An use-case may have dependency with other use-cases. The dependencies
 are declared when the solver is instantiated. The dependencies needs to
 be kept non-circular to avoid non-deterministic resolution result.
 
-Every PropertySolver is linked together by the SharedParameter called
+<p>Every PropertySolver is linked together by the SharedParameter called
 "sharedUtilitiesWrapper", which contains the shared utility object. 
 This allows every PropertySolver to find other solvers in the model. 
 
-Subclasses needs to implement {@link PropertySolverBase#resolveProperties()}
+<p>Subclasses needs to implement
+{@link PropertySolverBase#resolveProperties()}
 to specify exactly how to perform the property resolution. For example,
 one may gather all the constraints from the PropertyHelpers and feed them 
 into a constraint solver. 
@@ -83,24 +85,21 @@ into a constraint solver.
 
 @author Man-Kit Leung
 @version $Id: PropertySolverBase.java 50870 2008-10-22 15:05:00Z cxh $
-@since Ptolemy II 7.0
+@since Ptolemy II 7.1
 @Pt.ProposedRating Red (mankit)
 @Pt.AcceptedRating Red (mankit)
 */
 public abstract class PropertySolverBase extends Attribute {
 
     /*
-     * Construct a PropertySolverBase with the specified container and name. If
-     * this is the first PropertySolver created in the model, the shared utility
-     * object will also be created.
+     * Construct a PropertySolverBase with the specified container and
+     * name. If this is the first PropertySolver created in the model,
+     * the shared utility object will also be created.
      * 
      * @param container The specified container.
-     * 
      * @param name The specified name.
-     * 
-     * @exception IllegalActionException If the PropertySolverBase is not of an
-     * acceptable attribute for the container.
-     * 
+     * @exception IllegalActionException If the PropertySolverBase is
+     * not of an acceptable attribute for the container.
      * @exception NameDuplicationException If the name coincides with an
      * attribute already in the container.
      */
@@ -131,22 +130,24 @@ public abstract class PropertySolverBase extends Attribute {
 
     }
 
-    // /////////////////////////////////////////////////////////////////
-    // // ports and parameters ////
+    ///////////////////////////////////////////////////////////////////
+    ////                    ports and parameters                   ////
 
-    /*
-     * The shared parameter that links together every solver in the same model.
+    /**
+     * The shared parameter that links together every solver in the
+     * same model.
      */
+
     public SharedParameter sharedUtilitiesWrapper;
 
-    // /////////////////////////////////////////////////////////////////
-    // // public methods ////
+    ///////////////////////////////////////////////////////////////////
+    ////                     public methods                        ////
 
-    /*
-     * Add the given unique solver (user-case) identifier to the dependency
-     * list. A dependent solver is one whose analysis result is required for
-     * this solver's resolution. The dependent solvers are run in order before
-     * invoking this solver.
+    /**
+     * Add the given unique solver (user-case) identifier to the
+     * dependency list. A dependent solver is one whose analysis
+     * result is required for this solver's resolution. The dependent
+     * solvers are run in order before invoking this solver.
      * 
      * @param userCaseName The specified user case name.
      */
@@ -154,9 +155,10 @@ public abstract class PropertySolverBase extends Attribute {
         _dependentUseCases.add(userCaseName);
     }
 
-    /*
-     * Clear the resolved property for the specified object. The object is
-     * assumed to be property-able; otherwise, nothing happens.
+    /**
+     * Clear the resolved property for the specified object. The
+     * object is assumed to be property-able; otherwise, nothing
+     * happens.
      * 
      * @param object The specified object.
      */
@@ -164,17 +166,16 @@ public abstract class PropertySolverBase extends Attribute {
         _resolvedProperties.remove(object);
     }
 
-    /*
-     * Find a solver that is associated with the specified label. A solver can
-     * be identified by the use-case name, class name, or its name in the model.
-     * There can be more than one solvers with the label. This method returns
-     * whichever it finds first.
+    /**
+     * Find a solver that is associated with the specified label. A
+     * solver can be identified by the use-case name, class name, or
+     * its name in the model.  There can be more than one solvers with
+     * the label. This method returns whichever it finds first.
      * 
      * @param identifier The specified label.
-     * 
      * @return The property solver associated with the specified label.
-     * 
-     * @throws IllegalActionException Thrown if no matched solver is found.
+     * @exception IllegalActionException Thrown if no matched solver
+     * is found.
      */
     public PropertySolver findSolver(String identifier)
             throws PropertyResolutionException {
@@ -195,8 +196,9 @@ public abstract class PropertySolverBase extends Attribute {
                 + identifier + "\" solver.");
     }
 
-    /*
-     * Return the list of all PropertyHelpers associated with this solver.
+    /**
+     * Return the list of all PropertyHelpers associated with this
+     * solver.
      * 
      * @return The list of PropertyHelpers.
      */
@@ -222,13 +224,12 @@ public abstract class PropertySolverBase extends Attribute {
     }
 
     /*
-     * Return the list of all property-able objects obtained from every
-     * PropertyHelper.
+     * Return the list of all property-able objects obtained from
+     * every PropertyHelper.
      * 
      * @return The list of all property-able objects.
-     * 
-     * @throws IllegalActionException Thrown if {@link #getAllPropertyables()}
-     * throws it.
+     * @exception IllegalActionException Thrown if
+     * {@link#getAllPropertyables()} throws it.
      */
     public List getAllPropertyables() throws IllegalActionException {
         List result = new LinkedList();
@@ -239,13 +240,12 @@ public abstract class PropertySolverBase extends Attribute {
         return result;
     }
 
-    /*
-     * The list of all solvers that are in the same model. They are linked by
-     * the specified SharedParameter.
+    /**
+     * Return the list of all solvers that are in the same model. They
+     * are linked by the specified SharedParameter.
      * 
-     * @param sharedParameter The specified SharedParameter links together the
-     * solvers.
-     * 
+     * @param sharedParameter The specified SharedParameter links
+     * together the solvers.
      * @return A list of PropertySolvers.
      */
     public static List<PropertySolver> getAllSolvers(
@@ -262,17 +262,16 @@ public abstract class PropertySolverBase extends Attribute {
         return solvers;
     }
 
-    /*
-     * Get the attribute that corresponds to the specified ASTPtRootNode. This
-     * assumes that the correspondence is recorded previously through calling
-     * PropertyHelper.putAttribute(ASTPtRootNode, Attribute).
+    /**
+     * Get the attribute that corresponds to the specified
+     * ASTPtRootNode. This assumes that the correspondence is recorded
+     * previously through calling
+     * {@link ptolemy.data.properties.PropertyHelper#putAttribute(ASTPtRootNode, Attribute)).
      * 
      * @param node The specified ASTPtRootNode.
-     * 
      * @return The attribute associated with the specified ASTPtRootNode.
-     * 
-     * @throws AssertionError Thrown if the specified node does not have a
-     * corresponding attribute.
+     * @exception AssertionError Thrown if the specified node does not
+     * have a corresponding attribute.
      */
     public Attribute getAttribute(ASTPtRootNode node) {
         Node root = node;
@@ -294,9 +293,9 @@ public abstract class PropertySolverBase extends Attribute {
         return attributes.get(root);
     }
 
-    /*
-     * Return the list of dependent solvers. The list contains the unique name
-     * of the solvers.
+    /**
+     * Return the list of dependent solvers. The list contains the
+     * unique name of the solvers.
      * 
      * @return The list of dependent solvers.
      */
@@ -304,7 +303,7 @@ public abstract class PropertySolverBase extends Attribute {
         return _dependentUseCases;
     }
 
-    /*
+    /**
      * Return the extended use-case name. The extended use-case name is an
      * unique label for a use-case.
      * 
@@ -312,22 +311,20 @@ public abstract class PropertySolverBase extends Attribute {
      */
     public abstract String getExtendedUseCaseName();
 
-    /*
+    /**
      * Return the property helper for the specified component.
      * 
      * @param component The specified component.
-     * 
      * @return The property helper for the component.
-     * 
-     * @throws IllegalActionException Thrown if the helper cannot be found or
-     * instantiated.
+     * @exception IllegalActionException Thrown if the helper cannot
+     * be found or instantiated.
      */
     public PropertyHelper getHelper(Object object)
             throws IllegalActionException {
         return _getHelper(object);
     }
 
-    /*
+    /**
      * Return the expression parser.
      * 
      * @return The expression parser.
@@ -339,14 +336,14 @@ public abstract class PropertySolverBase extends Attribute {
         return _parser;
     }
 
-    /*
-     * Return the root ASTPtRootNode associated with the specified attribute.
+    /**
+     * Return the root ASTPtRootNode associated with the specified
+     * attribute.
      * 
      * @param attribute The specified attribute.
-     * 
-     * @return The root ASTPtRootNode associated with the specified attribute.
-     * 
-     * @throws IllegalActionException
+     * @return The root ASTPtRootNode associated with the specified
+     * attribute.
+     * @exception IllegalActionException
      */
     public ASTPtRootNode getParseTree(Attribute attribute)
             throws IllegalActionException {
@@ -380,7 +377,7 @@ public abstract class PropertySolverBase extends Attribute {
         return parseTrees.get(attribute);
     }
 
-    /*
+    /**
      * Return the property value associated with the specified object.
      * 
      * @param object The specified object.
@@ -407,9 +404,7 @@ public abstract class PropertySolverBase extends Attribute {
      * Return the resolved property for the specified object.
      * 
      * @param object The specified object
-     * 
      * @param resolve
-     * 
      * @return The resolved property for the specified object.
      */
     public Property getResolvedProperty(Object object, boolean resolve) {
@@ -443,7 +438,7 @@ public abstract class PropertySolverBase extends Attribute {
         return _resolvedProperties.get(object);
     }
 
-    /*
+    /**
      * Return the shared utility object.
      * 
      * @return The shared utility object.
@@ -452,7 +447,7 @@ public abstract class PropertySolverBase extends Attribute {
         return _sharedUtilities;
     }
 
-    /*
+    /**
      * Return the use-case name. The use-case name is not guaranteed to be
      * unique.
      * 
@@ -460,9 +455,9 @@ public abstract class PropertySolverBase extends Attribute {
      */
     public abstract String getUseCaseName();
 
-    /*
-     * Mark the property of the specified object as non-settable. The specified
-     * object has a fixed assigned property.
+    /**
+     * Mark the property of the specified object as non-settable. The
+     * specified object has a fixed assigned property.
      * 
      * @param object The specified object.
      */
@@ -470,7 +465,7 @@ public abstract class PropertySolverBase extends Attribute {
         _nonSettables.add(object);
     }
 
-    /*
+    /**
      * Reset the solver.
      */
     public void reset() {
@@ -479,7 +474,7 @@ public abstract class PropertySolverBase extends Attribute {
         _helperStore = new HashMap<Object, PropertyHelper>();
     }
 
-    /*
+    /**
      * Reset every solver in the model.
      */
     public void resetAll() {
@@ -490,36 +485,33 @@ public abstract class PropertySolverBase extends Attribute {
         getSharedUtilities().resetAll();
     }
 
-    /*
+    /**
      * Perform property resolution.
      * 
-     * @throws KernelException Thrown if sub-class throws it.
+     * @exception KernelException Thrown if sub-class throws it.
      */
     public abstract void resolveProperties() throws KernelException;
 
-    /*
+    /**
      * Set the resolved property of the specified object.
      * 
      * @param object The specified object.
-     * 
      * @param property The specified property.
      */
     public void setResolvedProperty(Object object, Property property) {
         _resolvedProperties.put(object, property);
     }
 
-    // /////////////////////////////////////////////////////////////////
-    // // protected methods ////
+    ///////////////////////////////////////////////////////////////////
+    ////             protected methods                             ////
 
-    /*
+    /**
      * Return the PropertyHelper for the specified component.
      * 
      * @param component The specified component.
-     * 
      * @return The PropertyHelper.
-     * 
-     * @throws IllegalActionException Thrown if the PropertyHelper cannot be
-     * instantiated.
+     * @exception IllegalActionException Thrown if the PropertyHelper
+     * cannot be instantiated.
      */
     protected PropertyHelper _getHelper(Object component)
             throws IllegalActionException {
@@ -596,7 +588,7 @@ public abstract class PropertySolverBase extends Attribute {
         return (PropertyHelper) helperObject;
     }
 
-    /*
+    /**
      * Return the package name that contains the class of this solver.
      * 
      * @return The package name.
@@ -605,32 +597,34 @@ public abstract class PropertySolverBase extends Attribute {
         return getClass().getPackage().getName() + "." + getUseCaseName();
     }
 
-    // /////////////////////////////////////////////////////////////////
-    // // protected variables ////
+    ///////////////////////////////////////////////////////////////////
+    ////             protected variables                           ////
 
-    /*
-     * The list that keeps track of the dependencies on other use-cases.
-     * Circular dependencies are not allowed but it is up to the user to enforce
-     * this requirement. This means that there should not be a case where two
-     * solvers' use-cases exist in each other's dependency list.
+    /**
+     * The list that keeps track of the dependencies on other
+     * use-cases.  Circular dependencies are not allowed but it is up
+     * to the user to enforce this requirement. This means that there
+     * should not be a case where two solvers' use-cases exist in each
+     * other's dependency list.
      */
     protected List<String> _dependentUseCases = new LinkedList<String>();
 
-    /*
+    /**
      * The HashMap that caches components and their PropertyHelper objects.
      */
     protected HashMap<Object, PropertyHelper> _helperStore = new HashMap<Object, PropertyHelper>();
 
-    /*
+    /**
      * The set of property-able objects that have non-settable property. A
      * non-settable property results from setting an object with a fixed
      * property through PropertyHelper.setEquals().
      */
     protected HashSet<Object> _nonSettables = new HashSet<Object>();
 
-    /*
-     * The HashMap that caches property-able objects and their Property values.
-     * Each mapping is a pair of Object and Property.
+    /**
+     * The HashMap that caches property-able objects and their
+     * Property values.  Each mapping is a pair of Object and
+     * Property.
      */
     protected HashMap<Object, Property> _resolvedProperties = new HashMap<Object, Property>();
 
