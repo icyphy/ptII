0c6bf6c4e669367579c254834df477370c751019
sssf <sssf>
2009-01-30 05:10:08 +0000

    I'm adding these files to the luminary target. They may be moved later to the OpenRTOS target. I'll need to talk with Jackie before I do that. Hopefully checking this in doesn't break anything.

---
+++ ptolemy/codegen/c/targets/luminary/domains/giotto/kernel/GiottoDirector.java
@@ -0,0 +1,74 @@
+/* Code generator helper class associated with the GiottoDirector class.
+
+ Copyright (c) 2005-2006 The Regents of the University of California.
+ All rights reserved.
+ Permission is hereby granted, without written agreement and without
+ license or royalty fees, to use, copy, modify, and distribute this
+ software and its documentation for any purpose, provided that the above
+ copyright notice and the following two paragraphs appear in all copies
+ of this software.
+
+ IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+ FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+ ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+ THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+ SUCH DAMAGE.
+
+ THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+ INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+ PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+ CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ ENHANCEMENTS, OR MODIFICATIONS.
+
+ PT_COPYRIGHT_VERSION_2
+ COPYRIGHTENDKEY
+
+ */
+package ptolemy.codegen.c.targets.luminary.domains.giotto.kernel;
+
+import java.util.List;
+
+import ptolemy.actor.Actor;
+import ptolemy.actor.TypedCompositeActor;
+import ptolemy.codegen.kernel.ActorCodeGenerator;
+import ptolemy.data.DoubleToken;
+import ptolemy.data.IntToken;
+import ptolemy.data.expr.Variable;
+import ptolemy.kernel.Entity;
+import ptolemy.kernel.util.Attribute;
+import ptolemy.kernel.util.IllegalActionException;
+
+
+//////////////////////////////////////////////////////////////////
+////GiottoDirector
+
+/**
+ Code generator helper associated with the GiottoDirector class. This class
+ is also associated with a code generator.
+
+ @author Ben Lickly
+ @version $Id$
+ @since Ptolemy II 7.2
+ @Pt.ProposedRating Red (sssf)
+ @Pt.AcceptedRating Red (sssf)
+ */
+
+// at the moment I'm not sure exactly what should go in this specific implementation. It will be filled out as the semester progresses.
+public class GiottoDirector extends ptolemy.codegen.c.domains.giotto.kernel.GiottoDirector {
+
+
+ /** Construct the code generator helper associated with the given
+     *  GiottoDirector.
+     *  @param giottoDirector The associated
+     *  ptolemy.domains.giotto.kernel.GiottoDirector
+     */
+    public GiottoDirector(ptolemy.domains.giotto.kernel.GiottoDirector giottoDirector) {
+        super(giottoDirector);
+    }
+
+
+
+
+
+}
