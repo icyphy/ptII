6dc40cf5db199488de9a5e91f8f89bf616e373da
mankit <mankit>
2009-02-05 18:44:06 +0000

    moved to domains/properties/lib/.

---
+++ ptolemy/domains/properties/lib/ModelAnalyzer.java
@@ -0,0 +1,115 @@
+/*  This actor opens a window to display the specified model and applies its inputs to the model.
+
+ @Copyright (c) 1998-2007 The Regents of the University of California.
+ All rights reserved.
+
+ Permission is hereby granted, without written agreement and without
+ license or royalty fees, to use, copy, modify, and distribute this
+ software and its documentation for any purpose, provided that the
+ above copyright notice and the following two paragraphs appear in all
+ copies of this software.
+
+ IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+ FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+ ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+ THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+ SUCH DAMAGE.
+
+ THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+ INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+ PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+ CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ ENHANCEMENTS, OR MODIFICATIONS.
+
+ PT_COPYRIGHT_VERSION 2
+ COPYRIGHTENDKEY
+ */
+package ptolemy.domains.properties.lib;
+
+import ptolemy.actor.TypedIOPort;
+import ptolemy.actor.lib.Transformer;
+import ptolemy.data.ActorToken;
+import ptolemy.data.StringToken;
+import ptolemy.data.properties.AnalyzerAttribute;
+import ptolemy.data.type.BaseType;
+import ptolemy.kernel.CompositeEntity;
+import ptolemy.kernel.util.Attribute;
+import ptolemy.kernel.util.IllegalActionException;
+import ptolemy.kernel.util.NameDuplicationException;
+import ptolemy.kernel.util.Workspace;
+
+//////////////////////////////////////////////////////////////////////////
+//// ModelGenerator
+
+/**
+This actor opens a window to display the specified model.
+If inputs are provided, they are expected to be MoML strings
+that are to be applied to the model. This can be used, for
+example, to create animations.
+
+@author  Man-Kit Leung
+@version $Id: ModelAnalyzer.java 52327 2009-02-04 22:49:10Z mankit $
+@since Ptolemy II 6.0
+@Pt.ProposedRating Yellow (eal)
+@Pt.AcceptedRating Red (cxh)
+*/
+public class ModelAnalyzer extends Transformer {
+
+    public ModelAnalyzer(CompositeEntity container, String name)
+            throws IllegalActionException, NameDuplicationException {
+        super(container, name);
+
+        setClassName("ptolemy.data.properties.ModelAnalyzer");
+
+        
+        errorMessage = new TypedIOPort(this, "errorMessage", false, true);
+        errorMessage.setTypeEquals(BaseType.STRING);
+        
+        input.setTypeEquals(ActorToken.TYPE);
+        
+        output.setTypeEquals(ActorToken.TYPE);    
+        
+        _analyzerWrapper = new AnalyzerAttribute(this, "_analyzerWrapper");
+    }
+
+    public TypedIOPort errorMessage;
+
+    /** React to a change in an attribute. 
+     *  @param attribute The attribute that changed.
+     *  @exception IllegalActionException If the change is not acceptable
+     *   to this container (not thrown in this class).
+     */
+    public void attributeChanged(Attribute attribute)
+            throws IllegalActionException {
+        
+        super.attributeChanged(attribute);
+    }
+    
+    /** Clone the object into the specified workspace. The new object is
+     *  <i>not</i> added to the directory of that workspace (you must do this
+     *  yourself if you want it there).
+     *  The result is an attribute with no container.
+     *  @param workspace The workspace for the cloned object.
+     *  @exception CloneNotSupportedException Not thrown in this base class
+     *  @return The new Attribute.
+     */
+    public Object clone(Workspace workspace) throws CloneNotSupportedException {
+        ModelAnalyzer newObject = (ModelAnalyzer) super.clone(workspace);
+        newObject._analyzerWrapper = (AnalyzerAttribute)newObject._analyzerWrapper.getAttribute("_analyzerWrapper");
+        return newObject;
+    }
+
+    public void fire() throws IllegalActionException {
+        
+        ActorToken token = (ActorToken) input.get(0);
+        CompositeEntity entity = (CompositeEntity) token.getEntity();
+        
+        String errorString = _analyzerWrapper.analyze(entity);
+        errorMessage.send(0, new StringToken(errorString));
+
+        output.send(0, new ActorToken(entity));
+    }
+
+    private AnalyzerAttribute _analyzerWrapper;
+}
