fd6d41aef924fedeb3fa9c18b6b2f779368fa67e
rodiers <rodiers>
2009-02-26 15:52:22 +0000

    FindBugs: Corrected code.

--- ptolemy/util/OrderedResourceBundle.java
+++ ptolemy/util/OrderedResourceBundle.java
@@ -281,7 +281,12 @@ public class OrderedResourceBundle {
 		while ((index >= 0) && (line.charAt(index--) == '\\')) {
 			slashCount++;
 		}
-		return (slashCount % 2 == 1);
+		// FindBugs: The code uses x % 2 == 1 to check to see if a value is odd,
+		// but this won't work for negative numbers (e.g., (-5) % 2 == -1).
+		// If this code is intending to check for oddness, consider
+		// using x & 1 == 1, or x % 2 != 0. 
+
+		return (slashCount % 2 != 0);
 	}
 
 	private static String getPropsFileNamePlusLocale(String baseName)
