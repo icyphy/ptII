f8be555eff971d58279143ed8187b5904271268f
cxh <cxh>
2009-02-26 22:17:56 +0000

    Added MoMLCommandLineApplication, which takes command line arguments that set parameters

---
+++ ptolemy/moml/test/MoMLCommandLineApplication.tcl
@@ -0,0 +1,78 @@
+# Tests for the MoMLCommandLineApplication class 
+#
+# @Author: Christopher Brooks
+#
+# @Version: $Id: Location.tcl 36425 2005-02-28 20:51:43Z cxh $
+#
+# @Copyright (c) 2009 The Regents of the University of California.
+# All rights reserved.
+#
+# Permission is hereby granted, without written agreement and without
+# license or royalty fees, to use, copy, modify, and distribute this
+# software and its documentation for any purpose, provided that the
+# above copyright notice and the following two paragraphs appear in all
+# copies of this software.
+#
+# IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+# FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+# ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+# THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+#
+# THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+# INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+# PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+# CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+# ENHANCEMENTS, OR MODIFICATIONS.
+#
+# 						PT_COPYRIGHT_VERSION_2
+# 						COPYRIGHTENDKEY
+#######################################################################
+
+# Load the test definitions.
+if {[string compare test [info procs test]] == 1} then {
+    source testDefs.tcl
+} {}
+
+if {[info procs jdkCapture] == "" } then {
+    source [file join $PTII util testsuite jdktools.tcl]
+}
+
+# Uncomment this to get a full report, or set in your Tcl shell window.
+# set VERBOSE 1
+
+######################################################################
+####
+#
+test MoMLCommandLineLocation-1.1 {} {
+    set results [exec  java -classpath $PTII ptolemy.moml.MoMLCommandLineApplication MoMLCommandLineApplicationTest.xml]
+    regsub -all [java::call System getProperty "line.separator"] \
+	        $results "\n" results2
+
+    string range $results2 0 7
+} {A String}
+
+test MoMLCommandLineLocation-1.2 {Set the parameter to 2} {
+    set results [exec  java -classpath $PTII ptolemy.moml.MoMLCommandLineApplication -myParameter 2 MoMLCommandLineApplicationTest.xml]
+    regsub -all [java::call System getProperty "line.separator"] \
+	        $results "\n" results2
+    string range $results2 0 1
+} {2
+}
+
+test MoMLCommandLineLocation-1.3 {Set the parameter to a string} {
+    set results [exec  java -classpath $PTII ptolemy.moml.MoMLCommandLineApplication -myParameter "&quot;Hello, World&quot;" MoMLCommandLineApplicationTest.xml]
+    regsub -all [java::call System getProperty "line.separator"] \
+	        $results "\n" results2
+    string range $results2 0 11
+} {Hello, World}
+
+test MoMLCommandLineLocation-1.4 {Set the director iterations parameter to a 2} {
+    set results [exec  java -classpath $PTII ptolemy.moml.MoMLCommandLineApplication -SDF\ Director.iterations 2 MoMLCommandLineApplicationTest.xml]
+    regsub -all [java::call System getProperty "line.separator"] \
+	        $results "\n" results2
+    string range $results2 0 16
+} {A String
+A String}
+
