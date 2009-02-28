2e6b304d5cb9246f074bdcf400cc6df6f2ef39c4
cxh <cxh>
2009-02-28 01:07:38 +0000

    Call attributeChanged when changing the channelParameter. Added comments

--- ptolemy/actor/lib/test/performance/PublisherCommon.tcl
+++ ptolemy/actor/lib/test/performance/PublisherCommon.tcl
@@ -53,11 +53,11 @@ proc createPublisher {e0 channel nameSuffix {factor 1.1} } {
     set channelParameter [getParameter $publisher channel]
     $channelParameter setExpression $channel
 
-    $publisher attributeChanged $channelParameter
-
     $e0 connect \
 	    [java::field [java::cast ptolemy.actor.lib.Source $ramp] output] \
             [java::field [java::cast ptolemy.actor.lib.Transformer $scale] input]
+
+    $publisher attributeChanged $channelParameter
 
     $e0 connect \
             [java::field [java::cast ptolemy.actor.lib.Transformer $scale] output] \
@@ -248,14 +248,15 @@ proc createHierarchichalPubSubModel {container numberOfPubSubsPerLevel levelNumb
 	    #set channel2 "PubSub_${levelNumber}_${n}_${pubCount}"
 
 	    createPublisher $en $channel "pub_a_[expr {$levelNumber - 1}]_$n" [expr {($n + 0.0)/$numberOfPubSubsPerLevel + 1.0}]
-	    set subscriber [java::new ptolemy.actor.lib.Subscriber $container "sub_${levelNumber}_$n"]
 
+	    # Get the upper subscriber and set the channel 
+	    set subscriber [java::new ptolemy.actor.lib.Subscriber $container "sub_${levelNumber}_$n"]
 	    set channelParameter [getParameter $subscriber channel]
 	    $channelParameter setExpression $channel
 	    $subscriber attributeChanged $channelParameter
 
+	    # Get the upper subscriber and set the channel 
 	    set publisher [java::new ptolemy.actor.lib.Publisher $container "pub_b_${levelNumber}_$n"]
-
 	    set channelParameter [getParameter $publisher channel]
 	    $channelParameter setExpression $channel2
 	    $publisher attributeChanged $channelParameter
@@ -265,36 +266,34 @@ proc createHierarchichalPubSubModel {container numberOfPubSubsPerLevel levelNumb
 		[java::field $publisher input]
         } 
     } else {
-	for {set n 1} { $n <= $numberOfPubSubsPerLevel} {incr n} {
-            set en [java::new ptolemy.actor.TypedCompositeActor $container "en-$n"]
-	    $en allowLevelCrossingConnect true
+ 	for {set n 1} { $n <= $numberOfPubSubsPerLevel} {incr n} {
+             set en [java::new ptolemy.actor.TypedCompositeActor $container "en-$n"]
+ 	    $en allowLevelCrossingConnect true
+# 	    #set channel "PubSub_${pubCount}_[expr {$levelNumber - 1}]_$n"
+ 	    #set channel2 "PubSub_${pubCount}_${levelNumber}_$n"
+# 	    incr pubCount
+ 	    #set channel "PubSub_${pubCount}a"
+ 	    #set channel2 "PubSub_${pubCount}b"
+ 	    #set channel "PubSub_[expr {$levelNumber - 1}]_${n}"
+ 	    #set channel2 "PubSub_${levelNumber}_${n}"
+ 	    set channel "PubSub_[$en getFullName]_[expr {$levelNumber - 1}]_$n"
+ 	    set channel2 "PubSub_[$container getFullName]_${levelNumber}_$n"
 
-	    #set channel "PubSub_${pubCount}_[expr {$levelNumber - 1}]_$n"
-	    #set channel2 "PubSub_${pubCount}_${levelNumber}_$n"
+   	    createHierarchichalPubSubModel $en $numberOfPubSubsPerLevel [expr {$levelNumber - 1}] $returnAll $usePubSub
+ 	    set subscriber [java::new ptolemy.actor.lib.Subscriber $container "sub_c_${levelNumber}_$n"]
+ 	    set channelParameter [getParameter $subscriber channel]
+ 	    $channelParameter setExpression $channel
+ 	    $subscriber attributeChanged $channelParameter
 
-	    incr pubCount
-	    #set channel "PubSub_${pubCount}a"
-	    #set channel2 "PubSub_${pubCount}b"
-	    #set channel "PubSub_[expr {$levelNumber - 1}]_${n}"
-	    #set channel2 "PubSub_${levelNumber}_${n}"
-	    set channel "PubSub_[$en getFullName]_[expr {$levelNumber - 1}]_$n"
-	    set channel2 "PubSub_[$container getFullName]_${levelNumber}_$n"
+ 	    set publisher [java::new ptolemy.actor.lib.Publisher $container "pub_c_${levelNumber}_$n"]
+ 	    set channelParameter [getParameter $publisher channel]
+ 	    $channelParameter setExpression $channel2
+ 	    $publisher attributeChanged $channelParameter
 
-  	    createHierarchichalPubSubModel $en $numberOfPubSubsPerLevel [expr {$levelNumber - 1}] $returnAll $usePubSub
-	    set subscriber [java::new ptolemy.actor.lib.Subscriber $container "sub_c_${levelNumber}_$n"]
-	    set channelParameter [getParameter $subscriber channel]
-	    $channelParameter setExpression $channel
-	    $subscriber attributeChanged $channelParameter
-
-	    set publisher [java::new ptolemy.actor.lib.Publisher $container "pub_c_${levelNumber}_$n"]
-	    set channelParameter [getParameter $publisher channel]
-	    $channelParameter setExpression $channel2
-	    $publisher attributeChanged $channelParameter
-
-	    $container connect \
-		[java::field $subscriber output] \
-		[java::field $publisher input]
-        } 
+ 	    $container connect \
+ 		[java::field $subscriber output] \
+ 		[java::field $publisher input]
+         } 
     }
 }
 
@@ -333,3 +332,129 @@ proc pubSubAggModel {numberOfSubsPerLevel levels} {
     return [list [enumToTokenValues [$recorder getRecord 0]]]
 } 
 
+
+
+proc createHierarchichalModel {container numberOfPubSubsPerLevel levelNumber} {
+    global pubCount
+    if {$levelNumber == 1} {
+	for {set n 1} { $n <= $numberOfPubSubsPerLevel} {incr n} {
+            set en [java::new ptolemy.actor.TypedCompositeActor $container "en-$n"]
+	    #set en $container
+	    incr pubCount
+	    # createPublisher creates a ramp, scale and publisher,
+	    # so we create a ramp, scale and another scale connected to a port
+	    #createPublisher $en $channel "pub_a_[expr {$levelNumber - 1}]_$n" [expr {($n + 0.0)/$numberOfPubSubsPerLevel + 1.0}]
+	    set nameSuffix "[expr {$levelNumber}]_$n"
+
+	    set ramp [java::new ptolemy.actor.lib.Ramp $en "ramp_$nameSuffix"]
+	    set scale [java::new ptolemy.actor.lib.Scale $en "scale_$nameSuffix"]
+
+	    set factor  [expr {($n + 0.0)/$numberOfPubSubsPerLevel + 1.0}]
+	    set factorParameter [getParameter $scale factor]
+	    $factorParameter setExpression $factor
+
+	    $en connect \
+		[java::field [java::cast ptolemy.actor.lib.Source $ramp] output] \
+		[java::field [java::cast ptolemy.actor.lib.Transformer $scale] input]
+
+	    # scale2 is like the publisher that is created in createPublisher for pubsub
+	    set scale2 [java::new ptolemy.actor.lib.Scale $en "scale2_$nameSuffix"]
+	    $en connect \
+		[java::field [java::cast ptolemy.actor.lib.Transformer $scale] output] \
+		[java::field [java::cast ptolemy.actor.lib.Transformer $scale2] input]
+
+	    set port1 [java::new ptolemy.actor.TypedIOPort $en "port1_$nameSuffix" false true]
+	    puts "port1: levelNumber: $levelNumber, n: $n: port1: port1_$nameSuffix"
+	    $en connect \
+		[java::field [java::cast ptolemy.actor.lib.Transformer $scale2] output] \
+		$port1
+
+	    ##set r1 [java::new ptolemy.actor.TypedIORelation $en R1]
+	    ##$port1 link $r1
+	    #set port2 [java::new ptolemy.actor.TypedIOPort $container "port2_$nameSuffix" true false]
+	    #$port2 link $r1
+	    #set upperNameSuffix "${levelNumber}_$n"
+	    #set scale3 [java::new ptolemy.actor.lib.Scale $container "scale3_$upperNameSuffix"]
+	    #$container connect 
+	    #	$port2 
+	    #	[java::field [java::cast ptolemy.actor.lib.Transformer $scale3] input]
+
+	    #set pUpper [java::cast ptolemy.actor.TypedIOPort \
+	    #		    [$container getPort "port2_$upperNameSuffix"]]
+	    #set scale4 [java::new ptolemy.actor.lib.Scale $container "scale4_$upperNameSuffix"]
+	    #$container connect \
+	    #	[java::field [java::cast ptolemy.actor.lib.Transformer $scale4] output] \
+	    #	$pUpper
+
+        } 
+    } else {
+	for {set n 1} { $n <= $numberOfPubSubsPerLevel} {incr n} {
+	    set en [java::new ptolemy.actor.TypedCompositeActor $container "en-$n"]
+   	    createHierarchichalModel $en $numberOfPubSubsPerLevel [expr {$levelNumber - 1}]
+
+	    set addSub [java::new ptolemy.actor.lib.AddSubtract $en addSub]
+	    for {set m 1} { $m <= $numberOfPubSubsPerLevel} {incr m} {
+		set enInner [java::cast ptolemy.actor.TypedCompositeActor [$en getEntity "en-$m"]]
+		set port1Name "port1_[expr {${levelNumber} -1}]_$m"
+		set port1 [$enInner getPort $port1Name]
+		if {[java::isnull $port1]} {
+		    error "levelNumber=$levelNumber, n=$n, m=$m: Could not get port $port1Name from $en. [$en exportMoML]"
+		}
+		$en connect $port1 [java::field $addSub plus] "R_m$m"
+	    }
+	    set upperNameSuffix "${levelNumber}_$n"
+	    set scale3 [java::new ptolemy.actor.lib.Scale $en "scale3_$upperNameSuffix"]
+
+	    $en connect \
+		[java::field $addSub output] \
+		[java::field [java::cast ptolemy.actor.lib.Transformer $scale3] input]
+
+	    set port2 [java::new ptolemy.actor.TypedIOPort $en "port1_$upperNameSuffix" false true]
+	    puts "port2: levelNumber: $levelNumber, n: $n: port1: port1_$upperNameSuffix"
+	    $en connect \
+		[java::field [java::cast ptolemy.actor.lib.Transformer $scale3] output] \
+		$port2
+	}
+    }
+}
+
+proc noPubSubAggModel {numberOfSubsPerLevel levels} {
+    global pubCount 
+    global e0
+    set pubCount 0
+    set e0 [sdfModel 5]
+    createHierarchichalModel $e0 $numberOfSubsPerLevel $levels
+
+    # Connect ports
+    set addSub [java::new ptolemy.actor.lib.AddSubtract $e0 addSub]
+    for {set n 1} { $n <= $numberOfSubsPerLevel} {incr n} {
+        set nameSuffix "${levels}_$n"
+	set en [java::cast ptolemy.actor.TypedCompositeActor [$e0 getEntity "en-$n"]]
+        set port1Name "port1_[expr {${levels}}]_$n"
+	set port1 [$en getPort $port1Name]
+	if {[java::isnull $port1]} {
+	    error "Could not get port $port1Name from $en\n[$en exportMoML]" 
+        }
+	$e0 connect $port1 [java::field $addSub plus] "R_$n"
+        #set port2Name "port2_[expr {${levels}}]_$n"
+	#set port2 [$en getPort $port2Name]
+	#if {[java::isnull $port2]} {
+	#    error "Could not get port $port2Name from $en\n[$en exportMoML]" 
+        #}
+	#$e0 connect $port2 [java::field $addSub plus] "R_$n"
+    }
+    set recorder [java::new ptolemy.actor.lib.Recorder $e0 "recorder"]
+
+    $e0 connect \
+        [java::field $addSub output] \
+	[java::field [java::cast ptolemy.actor.lib.Sink $recorder] input]
+    set filename "hierarchicalModel_${numberOfSubsPerLevel}_${levels}.xml"
+    set fd [open $filename w]
+    puts $fd [$e0 exportMoML]
+    close $fd
+    puts "Created $filename, containing [[$e0 deepOpaqueEntityList] size] actors"
+    
+    [$e0 getManager] execute
+
+    return [list [enumToTokenValues [$recorder getRecord 0]]]
+} 
