0059457eed623655eb33df24306fd62eb050a280
cxh <cxh>
2009-02-28 01:02:30 +0000

    Added tests for ModelScope methods

--- ptolemy/actor/parameters/test/ParameterSet.tcl
+++ ptolemy/actor/parameters/test/ParameterSet.tcl
@@ -82,3 +82,40 @@ test ParameterSet-1.0 {Read in fileReadingAttribute.txt} {
 
     list $r1 $r2 $r3
 } {{5 7 7} {5 42 42} {5 42 42}}
+
+######################################################################
+####
+# 
+test ParameterSet-2.1 {Check out ModelScope methods on e2} {
+    set attribute [java::call ptolemy.data.expr.ModelScope \
+		       {getScopedAttribute ptolemy.kernel.util.Attribute \
+			    ptolemy.kernel.util.NamedObj String} \
+		       [java::null] $e2 p]
+
+    set object  [java::call ptolemy.data.expr.ModelScope \
+		       {getScopedObject \
+			    ptolemy.kernel.util.NamedObj String} \
+		       $e2 p]
+
+    list [$attribute toString] [$object toString]
+} {{ptolemy.data.expr.Parameter {..e2.p} 42} {ptolemy.data.expr.Parameter {..e2.p} 42}} 
+
+######################################################################
+####
+# 
+test ParameterSet-2.2 {Check out the scope} {
+    set const [java::new ptolemy.actor.lib.Const $e1 const]
+    set parameter [getParameter $const value]
+    $parameter setExpression "p"
+    set attribute [java::call ptolemy.data.expr.ModelScope \
+		       {getScopedAttribute ptolemy.kernel.util.Attribute \
+			    ptolemy.kernel.util.NamedObj String} \
+		       [java::null] $e1 p]
+
+    set object  [java::call ptolemy.data.expr.ModelScope \
+		       {getScopedObject \
+			    ptolemy.kernel.util.NamedObj String} \
+		       $e1 p]
+
+    list [$attribute toString] [$object toString]
+} {{ptolemy.data.expr.Parameter {..p} 5} {ptolemy.data.expr.Parameter {..p} 5}} 
