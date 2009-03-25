0976e6ba83e1a3aa44d174a61b811c48ebc9bb3a
jiazou <jiazou>
2009-03-25 18:45:24 +0000

    took out duplicate method setCodeGenerator, which was done in the base class

--- ptolemy/codegen/actor/Director.java
+++ ptolemy/codegen/actor/Director.java
@@ -557,40 +557,6 @@ public class Director extends CodeGeneratorHelper {
         return set;
     }
 
-    /** Set the code generator associated with this helper class.
-     *  @param codeGenerator The code generator associated with this
-     *   helper class.
-     */
-    public void setCodeGenerator(CodeGenerator codeGenerator) {
-        _codeGenerator = codeGenerator;
-
-        String path = getClass().getName().replace('.', '/');  
-
-        String packageName = getClass().getPackage().getName();
-        if (packageName.startsWith("ptolemy.codegen")) {
-            // Strip off the ptolemy.codegen
-            packageName = packageName.substring(16);
-        }
-
-        String extension = "";
-        if (packageName.indexOf('.') != -1) { 
-            // Usually, Directors are in packages like
-            // ptolemy.codegen.c.  Thus "c" is the file extension
-            // for the helper.  This is a bit of a hack.
-            extension = packageName.substring(0,
-                    packageName.indexOf('.'));
-
-	    // See also codegen/kernel/CodeStream.java
-	    if (extension.equals("java")) {
-		// Sigh.  The problem is that for Java codegen, if we
-		// have an actor Foo, then Foo.java defines the Java
-		// interface, so we can't have the stub code in
-		// Foo.java.  So, we use the j extension.
-		extension = "j";
-	    }
-        }
-    }
-
     /////////////////////////////////////////////////////////////////////
     ////                   protected methods                         ////
 
