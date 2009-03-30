2f5ccefe9244dc64177a7ff88c820b7bb89f25d4
jiazou <jiazou>
2009-03-30 07:40:04 +0000

    set output buffer size, reorganize a bit...

--- ptolemy/codegen/c/domains/ptides/kernel/PtidesEmbeddedDirector.c
+++ ptolemy/codegen/c/domains/ptides/kernel/PtidesEmbeddedDirector.c
@@ -4,6 +4,41 @@
 
 /*** initPIBlock ***/
 initializeEvents();
+initPIBlock();
 /**/
 
+processEvents() {
+    while (1) {
+        if (safeToProcess(event)) {
+            // do some analysis, and set these values...
+            // access to these function ptrs some where?
+            event->actor.input1 = true;
+            event->actor.input2 = false;
 
+            fire(event);
+        }
+    }
+}
+
+fire(event) {
+    if (event->actor = A)
+        A_fire();
+}
+
+A_fire() {
+    //generated
+    if (input1 == true) {
+        // sourceOutputPort and input are hard coded.
+        transferDataFunctionZout1_to_Ain1();
+    //    $ref(input) = $ref(sourceOutputPort);
+    }
+    //FireBlock();
+}
+        
+transferDataFunctionZout1_to_Ain1(){
+    Ain1 = Zout1;
+}
+    
+
+// So during codegen, Zout1_to_Ain1 would be generated using:
+// $ref(input), $ref(sourceOutputPort)
