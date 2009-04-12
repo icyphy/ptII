40714a4ab9faaf149d042c40a5e5dab31866f7a1
bldmastr <bldmastr>
2009-04-12 18:11:07 +0000

    javadoc

--- ptolemy/cg/kernel/generic/CodeGeneratorAdapter.java
+++ ptolemy/cg/kernel/generic/CodeGeneratorAdapter.java
@@ -51,7 +51,7 @@ import ptolemy.util.StringUtilities;
 *
 * <p>Subclasses should be sure to properly indent the code by
 * either using the code block functionality in methods like
-* {@link #_generateBlockCode(String)} or by calling
+* _generateBlockCode(String) or by calling
 * {@link ptolemy.codegen.kernel.CodeStream#indent(String)},
 * for example:
 * <pre>
