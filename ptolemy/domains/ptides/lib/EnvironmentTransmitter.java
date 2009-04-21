2ae1a7172e4c76a2370ec60a340aa1c3fc3329e9
jiazou <jiazou>
2009-04-21 22:45:30 +0000

    added EnvironmentReceiver and EnvironmentTransmitter

--- ptolemy/domains/ptides/lib/EnvironmentTransmitter.java
+++ ptolemy/domains/ptides/lib/EnvironmentTransmitter.java
@@ -1,11 +1,12 @@
 package ptolemy.domains.ptides.lib;
 
+import ptolemy.actor.TypedAtomicActor;
 import ptolemy.kernel.CompositeEntity;
 import ptolemy.kernel.util.IllegalActionException;
 import ptolemy.kernel.util.NameDuplicationException;
 
-public class NetworkTransmitter extends EnvironmentTransmitter {
-    public NetworkTransmitter(CompositeEntity container, String name)
+public class EnvironmentTransmitter extends TypedAtomicActor {
+    public EnvironmentTransmitter(CompositeEntity container, String name)
             throws IllegalActionException, NameDuplicationException {
         super(container, name);
     }
--- ptolemy/domains/ptides/lib/NetworkReceiver.java
+++ ptolemy/domains/ptides/lib/NetworkReceiver.java
@@ -1,11 +1,10 @@
 package ptolemy.domains.ptides.lib;
 
-import ptolemy.actor.TypedAtomicActor;
 import ptolemy.kernel.CompositeEntity;
 import ptolemy.kernel.util.IllegalActionException;
 import ptolemy.kernel.util.NameDuplicationException;
 
-public class NetworkReceiver extends TypedAtomicActor {
+public class NetworkReceiver extends EnvironmentReceiver {
     public NetworkReceiver(CompositeEntity container, String name)
             throws IllegalActionException, NameDuplicationException {
         super(container, name);
