a3a68519243e72169d21d00d46dc24c9242ecc5c
jiazou <jiazou>
2009-04-24 03:14:46 +0000

    removed line that didn't compile

--- ptolemy/codegen/c/actor/Receiver.java
+++ ptolemy/codegen/c/actor/Receiver.java
@@ -1,33 +1,17 @@
 package ptolemy.codegen.c.actor;
 
-import ptolemy.codegen.kernel.CodeGeneratorHelper;
+import ptolemy.codegen.c.kernel.CCodeGeneratorHelper;
 import ptolemy.kernel.util.IllegalActionException;
-import ptolemy.kernel.util.NamedObj;
 
 /** 
  * 
  *  @author Jia Zou, Man-Kit Leung, Isaac Liu
  *
  */
-public class Receiver extends CodeGeneratorHelper {
-
-    public Receiver() {
-        // TODO Auto-generated constructor stub
-    }
+public class Receiver extends CCodeGeneratorHelper {
 
     public Receiver(ptolemy.actor.Receiver receiver) {
         super(receiver);
-        // TODO Auto-generated constructor stub
-    }
-
-    public Receiver(NamedObj component, String name) {
-        super(component, name);
-        // TODO Auto-generated constructor stub
-    }
-
-    public Receiver(Object object) {
-        super(object);
-        // TODO Auto-generated constructor stub
     }
     
     public String generateCodeForGet(int channel) throws IllegalActionException {
@@ -38,9 +22,12 @@ public class Receiver extends CodeGeneratorHelper {
         return "1";
     }
     
-    public String generateCodeForPut(ptolemy.codegen.c.actor.IOPort sourcePort, String token) throws IllegalActionException{
+    public String generateCodeForPut(String token) throws IllegalActionException{
         return "";
     }
     
-    protected ptolemy.actor.Receiver _receiver;
+    public ptolemy.actor.Receiver getReceiver() {
+        return (ptolemy.actor.Receiver) getObject();
+    }
+    
 }
