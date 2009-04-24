50a0addaed2ecaa92e916eb26f32f9712603e405
jiazou <jiazou>
2009-04-24 01:24:54 +0000

    removed line that didn't compile

--- ptolemy/codegen/c/domains/ptides/kernel/PtidesActorReceiver.java
+++ ptolemy/codegen/c/domains/ptides/kernel/PtidesActorReceiver.java
@@ -7,53 +7,35 @@ import ptolemy.actor.IOPort;
 import ptolemy.data.type.Type;
 import ptolemy.actor.TypedIOPort;
 import ptolemy.kernel.util.IllegalActionException;
-import ptolemy.kernel.util.NamedObj;
 
 public class PtidesActorReceiver extends ptolemy.codegen.c.actor.Receiver {
-    public PtidesActorReceiver() {
-        // TODO Auto-generated constructor stub
-    }
 
     public PtidesActorReceiver(ptolemy.domains.ptides.kernel.PtidesActorReceiver receiver) {
-        _receiver = receiver;
-        // TODO Auto-generated constructor stub
-    }
-
-    public PtidesActorReceiver(NamedObj component, String name) {
-        super(component, name);
+        super(receiver);
         // TODO Auto-generated constructor stub
     }
 
     public String generateCodeForGet(int channel) throws IllegalActionException {
-        TypedIOPort port = (TypedIOPort)_receiver.getContainer();
+        TypedIOPort port = (TypedIOPort)getReceiver().getContainer();
         return "Event_Head_" + port.getContainer().getName() + "_" + port.getName()
             + "[" + channel + "]->" + port.getType().toString() + "_Value";
     }
 
     public String generateCodeForHasToken(int channel) throws IllegalActionException{
-        IOPort port = _receiver.getContainer();
+        IOPort port = getReceiver().getContainer();
         return "Event_Head_" + port.getContainer().getName() + "_" + port.getName() 
             + "[" + channel + "] != NULL";
     }
 
-    public String generateCodeForPut(ptolemy.codegen.c.actor.IOPort sourcePort, 
-            String token) throws IllegalActionException{
-        Type sourceType = ((TypedIOPort)sourcePort.getComponent()).getType();
-        Type sinkType = ((TypedIOPort)_receiver.getContainer()).getType();
-        
+    public String generateCodeForPut(String token) throws IllegalActionException{
+        Type sinkType = ((TypedIOPort)getReceiver().getContainer()).getType();
         
         // FIXME: not sure whether we should check if we are putting into an input port or
         // output port.
         // Generate a new event.
         List args = new ArrayList();
         args.add(sinkType);
-
-        // The next line does not compile:
-        //args.add(generateTypeConvertCode(sourceType, sinkType, token));
-        if (1 == 1) {
-            throw new RuntimeException("This code does not compile");
-        }
-
+        args.add(token);
         //args.add(token);
         args.add("");//timestamp
         args.add("");//microstep
