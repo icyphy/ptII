4ef7fd5b7961854ae344224cbc153b7d28d73e34
eal <eal>
2009-05-02 04:32:16 +0000

    Added SuperdenseTimeDirector interface.

---
+++ ptolemy/actor/SuperdenseTimeDirector.java
@@ -0,0 +1,48 @@
+/* Interface for directors that can directs timed actors.
+
+ Copyright (c) 2004-2009 The Regents of the University of California.
+ All rights reserved.
+ Permission is hereby granted, without written agreement and without
+ license or royalty fees, to use, copy, modify, and distribute this
+ software and its documentation for any purpose, provided that the above
+ copyright notice and the following two paragraphs appear in all copies
+ of this software.
+
+ IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+ FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+ ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+ THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+ SUCH DAMAGE.
+
+ THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+ INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+ PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+ CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ ENHANCEMENTS, OR MODIFICATIONS.
+
+ PT_COPYRIGHT_VERSION_2
+ COPYRIGHTENDKEY
+
+ */
+package ptolemy.actor;
+
+//////////////////////////////////////////////////////////////////////////
+//// SuperdenseTimeDirector
+
+/**
+ This is an interface for directors that use a superdense model of time.
+ Actors can determine the index of current time by calling geIndex().
+
+ @author Edward A. Lee
+ @version $Id$
+ @since Ptolemy II 8.1
+ @Pt.ProposedRating Yellow (eal)
+ @Pt.AcceptedRating Red (eal)
+ */
+public interface SuperdenseTimeDirector {
+    /** Return a superdense time index for the current time.
+     *  @return A superdense time object.
+     */
+    public int getIndex();
+}
