9e005db899f6d4af0b24036325bface5e91fa4df
leinfeld <leinfeld>
2009-05-05 23:33:05 +0000

    Subclass of RecordToken that uses original order of the record entries (not naturual order)

---
+++ ptolemy/data/OrderedRecordToken.java
@@ -0,0 +1,79 @@
+/* A token that contains a set of label/token pairs - maintaing the original order.
+
+ Copyright (c) 1997-2009 The Regents of the University of California.
+ All rights reserved.
+ Permission is hereby granted, without written agreement and without
+ license or royalty fees, to use, copy, modify, and distribute this
+ software and its documentation for any purpose, provided that the above
+ copyright notice and the following two paragraphs appear in all copies
+ of this software.
+
+ IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+ FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+ ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+ THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+ SUCH DAMAGE.
+
+ THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+ INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+ PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+ CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ ENHANCEMENTS, OR MODIFICATIONS.
+
+ PT_COPYRIGHT_VERSION_2
+ COPYRIGHTENDKEY
+
+
+ */
+package ptolemy.data;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import ptolemy.kernel.util.IllegalActionException;
+
+//////////////////////////////////////////////////////////////////////////
+//// OrderedRecordToken
+
+/**
+ A token that contains a set of label/token pairs. Operations on record
+ tokens result in new record tokens containing only the common fields,
+ where the operation specifies how to combine the data in the common
+ fields.  Thus, for example, if two record tokens
+ are added or subtracted, then common records
+ (those with the same labels) will be added or subtracted,
+ and the disjoint records will not appear in the result.
+ 
+ This implementation maintains the order of the entries as they were added
+
+ @author Ben Leinfelder
+ @version $Id: RecordToken.java 53042 2009-04-10 20:31:21Z cxh $
+ @Pt.ProposedRating Yellow (leinfelder)
+ @Pt.AcceptedRating 
+ */
+public class OrderedRecordToken extends RecordToken {
+	
+	public OrderedRecordToken() {
+		super();
+	}
+	public OrderedRecordToken(Map fieldMap) throws IllegalActionException {
+		super(fieldMap);
+	}
+	public OrderedRecordToken(String init) throws IllegalActionException {
+		super(init);
+	}
+	public OrderedRecordToken(String[] labels, Token[] values) throws IllegalActionException {
+		super(labels, values);
+	}
+	
+    ///////////////////////////////////////////////////////////////////
+    ////                         protected methods                 ////
+	/**
+	 * Using a LinkedHashMap so that the original order of the record is maintained
+	 */
+    protected void _initializeStorage() {
+    	_fields = new LinkedHashMap();
+    }
+
+}
