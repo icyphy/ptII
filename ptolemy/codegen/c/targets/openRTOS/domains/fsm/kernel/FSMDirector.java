743909592f815c79be96eba61aef290c280f592c
sssf <sssf>
2009-05-06 02:23:46 +0000

    changed @since 7.2 to 8.0

--- ptolemy/codegen/c/targets/openRTOS/domains/fsm/kernel/FSMDirector.java
+++ ptolemy/codegen/c/targets/openRTOS/domains/fsm/kernel/FSMDirector.java
@@ -54,7 +54,7 @@ import ptolemy.kernel.util.NamedObj;
 
  @author  Shanna-Shaye Forbes,Ben Lickly
  @version $Id$
- @since Ptolemy II 7.2
+ @since Ptolemy II 8.0
  @Pt.ProposedRating Red (sssf)
  @Pt.AcceptedRating Red (sssf)
  */
