c7055a127b79c917922561bad332accb3315e66a
mankit <mankit>
2009-05-14 18:31:09 +0000

    corrected comments.

--- ptolemy/vergil/properties/LatticeGraphTableau.java
+++ ptolemy/vergil/properties/LatticeGraphTableau.java
@@ -32,7 +32,7 @@ import ptolemy.actor.gui.Effigy;
 import ptolemy.actor.gui.PtolemyEffigy;
 import ptolemy.actor.gui.Tableau;
 import ptolemy.actor.gui.TableauFactory;
-import ptolemy.domains.modal.kernel.FSMActor;
+import ptolemy.domains.properties.PropertyLatticeComposite;
 import ptolemy.kernel.CompositeEntity;
 import ptolemy.kernel.util.IllegalActionException;
 import ptolemy.kernel.util.InternalErrorException;
@@ -43,12 +43,12 @@ import ptolemy.moml.LibraryAttribute;
 
 public class LatticeGraphTableau extends Tableau {
 
-    /** Create a new FSM editor tableau with the specified container
-     *  and name, with no default library.
+    /** Create a new PropertyLattice editor tableau with the 
+     *  specified container and name, with no default library.
      *  @param container The container.
      *  @param name The name.
      *  @exception IllegalActionException If the model associated with
-     *   the container effigy is not an FSMActor.
+     *   the container effigy is not an PropertyLatticeComposite.
      *  @exception NameDuplicationException If the container already
      *   contains an object with the specified name.
      */
@@ -57,13 +57,13 @@ public class LatticeGraphTableau extends Tableau {
         this(container, name, null);
     }
 
-    /** Create a new FSM editor tableau with the specified container,
-     *  name, and default library.
+    /** Create a new PropertyLattice editor tableau with 
+     *  the specified container, name, and default library.
      *  @param container The container.
      *  @param name The name.
      *  @param defaultLibrary The default library, or null to not specify one.
      *  @exception IllegalActionException If the model associated with
-     *   the container effigy is not an FSMActor.
+     *   the container effigy is not an PropertyLatticeComposite.
      *  @exception NameDuplicationException If the container already
      *   contains an object with the specified name.
      */
@@ -74,20 +74,19 @@ public class LatticeGraphTableau extends Tableau {
 
         NamedObj model = container.getModel();
 
-        if (!model.getClassName().equals(
-                "ptolemy.domains.properties.PropertyLatticeComposite")) {
+        if (!(model instanceof PropertyLatticeComposite)) {
             throw new IllegalActionException(this,
-                    "Cannot edit a model that is not an FSMActor.");
+                    "Cannot edit a model that is not an PropertyLatticeComposite.");
         }
 
-        createGraphFrame((FSMActor) model, defaultLibrary);
+        createGraphFrame((CompositeEntity) model, defaultLibrary);
     }
 
     ///////////////////////////////////////////////////////////////////
     ////                         public methods                    ////
 
     /** Create the graph frame that displays the model associated with
-     *  this tableau. This method creates a FSMGraphFrame. If subclass
+     *  this tableau. This method creates a LatticeGraphFrame. If subclass
      *  uses another frame, this method should be overridden to create
      *  that frame.
      *  @param model The Ptolemy II model to display in the graph frame.
@@ -98,7 +97,7 @@ public class LatticeGraphTableau extends Tableau {
 
     /** Create the graph frame that displays the model associated with
      *  this tableau together with the specified library.
-     *  This method creates a FSMGraphFrame. If a subclass
+     *  This method creates a LatticeGraphFrame. If a subclass
      *  uses another frame, this method should be overridden to create
      *  that frame.
      *  @param model The Ptolemy II model to display in the graph frame.
@@ -148,11 +147,11 @@ public class LatticeGraphTableau extends Tableau {
             super(container, name);
         }
 
-        /** Create an instance of FSMGraphTableau for the specified effigy,
-         *  if it is an effigy for an instance of FSMActor.
-         *  @param effigy The effigy for an FSMActor.
-         *  @return A new FSMGraphTableau, if the effigy is a PtolemyEffigy
-         *   that references an FSMActor, or null otherwise.
+        /** Create an instance of LatticeGraphTableau for the specified effigy,
+         *  if it is an effigy for an instance of PropertyLatticeComposite.
+         *  @param effigy The effigy for an PropertyLatticeComposite.
+         *  @return A new LatticeGraphTableau, if the effigy is a PtolemyEffigy
+         *   that references an PropertyLatticeComposite, or null otherwise.
          *  @exception Exception If an exception occurs when creating the
          *   tableau.
          */
@@ -168,8 +167,7 @@ public class LatticeGraphTableau extends Tableau {
 
             NamedObj model = ((PtolemyEffigy) effigy).getModel();
 
-            if (model.getClassName().equals(
-                    "ptolemy.domains.properties.PropertyLatticeComposite")) {
+            if (model instanceof PropertyLatticeComposite) {
 
                 // Check to see whether this factory contains a
                 // default library.
