951f2402a199bc996e1e70e4d37cda6171d6cfe8
bldmastr <bldmastr>
2009-05-18 21:00:41 +0000

    Updates for 8.0

--- adm/gen-8.0/src.ex
+++ adm/gen-8.0/src.ex
@@ -160,7 +160,6 @@ ptolemy/gui/JPanelTest.java
 ptolemy/codegen/c/targets/arduino
 ptolemy/codegen/c/targets/iRobot
 ptolemy/codegen/c/targets/luminary
-ptolemy/codegen/c/targets/posix
 ptolemy/codegen/c/targets/pret
 ptolemy/codegen/c/targets/win32
 ptolemy/codegen/domains/sr
