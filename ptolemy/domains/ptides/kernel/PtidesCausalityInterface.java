4c2002d02d62f2ab0dd2b81bba22e5d39d779064
jiazou <jiazou>
2009-06-02 05:33:32 +0000

    ptides specific causality interface. This interface provides finding finite (non-boolean) dependencies.

--- ptolemy/domains/ptides/kernel/PtidesCausalityInterface.java
+++ ptolemy/domains/ptides/kernel/PtidesCausalityInterface.java
@@ -29,10 +29,15 @@ package ptolemy.domains.ptides.kernel;
 
 import java.util.Collection;
 import java.util.HashSet;
+import java.util.List;
 
 import ptolemy.actor.Actor;
+import ptolemy.actor.AtomicActor;
+import ptolemy.actor.CompositeActor;
 import ptolemy.actor.IOPort;
+import ptolemy.actor.util.CausalityInterface;
 import ptolemy.actor.util.CausalityInterfaceForComposites;
+import ptolemy.actor.util.DefaultCausalityInterface;
 import ptolemy.actor.util.Dependency;
 import ptolemy.kernel.CompositeEntity;
 import ptolemy.kernel.util.IllegalActionException;
@@ -71,7 +76,53 @@ public class PtidesCausalityInterface extends CausalityInterfaceForComposites {
     ///////////////////////////////////////////////////////////////////
     ////                         public methods                    ////
     
-    /** Return a collection of ports the given port is dependent on.
+    /** Get the dependency between the input and output ports. If the 
+     *  ports does not belong to the same actor, an exception is thrown.
+     *  Depending on the actor, the corresponding getDependency() method in
+     *  its super class or super's super's method is called. Also, if the
+     *  output is null, super's getDependency() is called because super's
+     *  method uses this method to update the dependency in the system.
+     *  @param input The input port.
+     *  @param output The output port.
+     *  @return The dependency between the specified input port
+     *   and the specified output port.
+     *  @exception IllegalActionException Not thrown in this base class.
+     */
+    
+    public Dependency getDependency(IOPort input, IOPort output)
+            throws IllegalActionException {
+        Actor actor = (Actor)input.getContainer();
+        if (output != null) {
+            Actor outputActor = (Actor)output.getContainer();
+            if (actor != outputActor) {
+                throw new IllegalActionException(input, output, "Cannot get dependency" +
+                        "from these two ports, becasue they do not belong" +
+                "to the same actor.");
+            }
+        } else {
+            // if the output is null, then the method is simply called to update the
+            // dependency of the graph, which is done in the super method of
+            // getDependency().
+            return super.getDependency(input, output);
+        }
+        CausalityInterface causalityInterface = actor.getCausalityInterface();
+        if (causalityInterface instanceof PtidesCausalityInterface) {
+            return super.getDependency(input, output);
+        } else {
+            return causalityInterface.getDependency(input, output);
+        }
+    }
+    
+    /** Return a collection of ports the given port is dependent on, within
+     *  the same actor.
+     *  This method first look at all declared dependencies, and add all
+     *  dependencies that are not equal to the oPlusIdentity. Then, for
+     *  ports that do not have declared dependencies, we rely on
+     *  dependentPorts() to ensure dependent ports are added to the returned
+     *  Collection.
+     *  Note we use a HashSet to ensure only one copy of each port is
+     *  returned in the Collection.
+     *  @see #dependentPorts(IOPort)
      * 
      *  @param port The given port to find finite dependent ports.
      *  @return Collection of finite dependent ports.
@@ -79,8 +130,36 @@ public class PtidesCausalityInterface extends CausalityInterfaceForComposites {
      */
     public Collection<IOPort> finiteDependentPorts(IOPort port)
             throws IllegalActionException {
-        // FIXME: implement me.
-        HashSet<IOPort> result = new HashSet<IOPort>();
+        // FIXME: This does not support ports that are both input and output.
+        // Should it?
+        Collection<IOPort> result = new HashSet<IOPort>();
+        Actor actor = (Actor)port.getContainer();
+        CausalityInterface actorCausalityInterface;
+        if (actor instanceof AtomicActor) {
+            actorCausalityInterface = (DefaultCausalityInterface)actor.getCausalityInterface();
+        } else if (actor instanceof CompositeActor) {
+            actorCausalityInterface = (CausalityInterfaceForComposites)actor.getCausalityInterface();
+        } else {
+            throw new IllegalActionException(actor, "Actor is not a typed atomic or typed composite" +
+            		"actor, do not know how to deal with it.");
+        }
+        if (port.isInput()) {
+            List<IOPort> outputs = ((Actor)port.getContainer()).outputPortList();
+            for (IOPort output : outputs) {
+                Dependency dependency = actorCausalityInterface.getDependency(port, output);
+                if (dependency != null && dependency.compareTo(dependency.oPlusIdentity())!= 0) {
+                    result.add(output);
+                }
+            }
+        } else { // port is output port.
+            List<IOPort> inputs = ((Actor)port.getContainer()).inputPortList();
+            for (IOPort input : inputs) {
+                Dependency dependency = actorCausalityInterface.getDependency(input, port);
+                if (dependency != null && dependency.compareTo(dependency.oPlusIdentity())!= 0) {
+                    result.add(input);
+                }
+            }
+        }
         return result;
     }
     
@@ -94,8 +173,10 @@ public class PtidesCausalityInterface extends CausalityInterfaceForComposites {
      *  default depenency's oPlusIdentity() on any common port
      *  or on two equivalent ports
      *  or on the state of the associated actor, then they
-     *  are in an equivalence class.
-     *  
+     *  are in a finite equivalence class.
+     *  Note we use a HashSet to ensure only one copy of each port is
+     *  returned in the Collection.
+
      *  If the port is not an input port, an exception
      *  is thrown.
      *  
@@ -103,9 +184,8 @@ public class PtidesCausalityInterface extends CausalityInterfaceForComposites {
      *  @return Collection of finite equivalent ports.
      *  @throws IllegalActionException
      */
-    public Collection<IOPort> FiniteEquivalentPorts(IOPort input)
+    public Collection<IOPort> finiteEquivalentPorts(IOPort input)
             throws IllegalActionException {
-        // FIXME: implement me.
         if (input.getContainer() != _actor || !input.isInput()) {
             throw new IllegalActionException(input, _actor,
                     "equivalentPort() called with argument "
@@ -113,7 +193,14 @@ public class PtidesCausalityInterface extends CausalityInterfaceForComposites {
                     + " which is not an input port of "
                     + _actor.getFullName());
         }
-        HashSet<IOPort> result = new HashSet<IOPort>();
+        Collection<IOPort> result = new HashSet<IOPort>();
+        // first get all outputs that are dependent on this input.
+        Collection<IOPort> outputs = finiteDependentPorts(input);
+        // now for every input that is also dependent on the output, add
+        // it to the list of ports that are returned.
+        for (IOPort output : outputs) {
+            result.addAll(finiteDependentPorts(output));
+        }
         return result;
     }
 }
