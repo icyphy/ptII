f939127a79cb5fcf351a68aaa4b7fc94cd9e4681
cxh <cxh>
2009-06-02 15:18:23 +0000

    Moved auto/bizzare.xml to auto/knownFailedTests.  Marked FSMDirectory 4.1 and 7.1 as known failed.  Edward wrote: 'The model is so utterly strange that I'm inclined to just move it to 'known failed'. You can't build a model like this in Vergil. I haven't been able to figure out what these tests were trying to test for'

--- ptolemy/domains/modal/kernel/test/FSMDirector.tcl
+++ ptolemy/domains/modal/kernel/test/FSMDirector.tcl
@@ -199,8 +199,8 @@ test FSMDirector-4.1 {test action methods} {
     # 	... (omitted)
 
     listToStrings [$rec getHistory 0]
-} {1 3 0 9 1}
-
+} {1 3 0 9 1} {See auto/knownFailedTests/bizarre.xml  "The model is so utterly strange that I'm inclined to just move it to "known failed". You can't build a model like this in Vergil. I haven't been able to figure out what these tests were trying to
+test for."}
 
 
 ######################################################################
@@ -411,4 +411,5 @@ test FSMDirector-7.1 {test clone a modal model} {
     [$e0 getManager] execute
     # In FSM, we get: {1 3 1 6 1 9 1}
     listToStrings [$rec getHistory 0]
-} {1 3 0 9 1}
+} {1 3 0 9 1} {See auto/knownFailedTests/bizarre.xml  "The model is so utterly strange that I'm inclined to just move it to "known failed". You can't build a model like this in Vergil. I haven't been able to figure out what these tests were trying to
+test for."}
