af1380a58d3795fd1bff162e56921ac2910d6922
cxh <cxh>
2009-06-04 02:48:13 +0000

    Cleaned up style of OrderedRecordToken

--- ptolemy/data/OrderedRecordToken.java
+++ ptolemy/data/OrderedRecordToken.java
@@ -54,44 +54,57 @@ import ptolemy.kernel.util.IllegalActionException;
  */
 public class OrderedRecordToken extends RecordToken {
 	
-	/**
-	 * Default constructor
-	 * @see RecordToken
-	 */
-	public OrderedRecordToken() {
-		super();
-	}
-	/**
-	 * @see RecordToken
-	 * @param fieldMap
-	 * @throws IllegalActionException
-	 */
-	public OrderedRecordToken(Map fieldMap) throws IllegalActionException {
-		super(fieldMap);
-	}
-	/**
-	 * @see RecordToken
-	 * @param init
-	 * @throws IllegalActionException
-	 */
-	public OrderedRecordToken(String init) throws IllegalActionException {
-		super(init);
-	}
-	/**
-	 * @see RecordToken
-	 * @param labels
-	 * @param values
-	 * @throws IllegalActionException
-	 */
-	public OrderedRecordToken(String[] labels, Token[] values) throws IllegalActionException {
-		super(labels, values);
-	}
+    /** Construct an OrderedRecordToke with now fields.
+     * @see RecordToken
+     */
+    public OrderedRecordToken() {
+        super();
+    }
+
+    /** Construct an OrderedRecordToken with the labels and values specified
+     *  by a given Map object. The object cannot contain any null keys
+     *  or values.
+     *  @param fieldMap A Map that has keys of type String and
+     *  values of type Token.
+     *  @exception IllegalActionException If the map contains null
+     *  keys or values, or if it contains non-String keys or non-Token
+     *  values.
+     */
+     public OrderedRecordToken(Map fieldMap) throws IllegalActionException {
+     super(fieldMap);
+     }
+
+    /** Construct a RecordToken from the specified string.
+     *  @param init A string expression of a record.
+     *  @exception IllegalActionException If the string does not
+     *  contain a parsable record.
+     */
+    public OrderedRecordToken(String init) throws IllegalActionException {
+        super(init);
+    }
+
+    /** Construct an OrderedRecordToken with the specified labels and values.
+     *  The labels and values arrays must have the same length, and have one
+     *  to one correspondence with each other.  That is, the i'th entry in
+     *  the labels array is the label for the i'th value in the values array.
+     *  If both arrays are empty, this creates an empty record token.
+     *  @param labels An array of labels.
+     *  @param values An array of Tokens.
+     *  @exception IllegalActionException If the labels or the values array
+     *   do not have the same length, or contains null element,
+     *   or the labels array contains duplicate elements.
+     */
+    public OrderedRecordToken(String[] labels, Token[] values) throws IllegalActionException {
+        super(labels, values);
+    }
 	
     ///////////////////////////////////////////////////////////////////
     ////                         protected methods                 ////
-	/**
-	 * Using a LinkedHashMap so that the original order of the record is maintained
-	 */
+
+    /**  Intialize the storage used by this token.  OrderedRecordToken
+     *   uses a LinkedHashMap so that the original order of the record
+     *   is maintained.
+     */
     protected void _initializeStorage() {
     	_fields = new LinkedHashMap();
     }
