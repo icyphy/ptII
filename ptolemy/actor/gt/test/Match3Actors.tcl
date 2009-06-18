458ee86d7a0dcc348726647f07778a310ea951f8
cxh <cxh>
2009-06-18 23:29:24 +0000

    Moved patterns out of test directory and into a separate directory so that we can ship them with gt for use with the HierarchyFlattening demo

--- ptolemy/actor/gt/test/Match3Actors.tcl
+++ ptolemy/actor/gt/test/Match3Actors.tcl
@@ -37,45 +37,48 @@ if {[string compare test [info procs test]] == 1} then {
     source testDefs.tcl
 } {}
 
+# Directory in which the patterns may be found
+set p ../patterns/
+
 ######################################################################
 ####
 #
 test Match3Actors.1 {Test Match3Actors_rule.xml with Match3Actors_succ1.xml} {
-    set matchResult [java::call ptolemy.actor.gt.GraphMatcher match Match3Actors_rule.xml Match3Actors_succ1.xml]
+    set matchResult [java::call ptolemy.actor.gt.GraphMatcher match ${p}Match3Actors_rule.xml ${p}Match3Actors_succ1.xml]
     [$matchResult getMatchResult] toString
 } {{ptolemy.actor.gt.AtomicActorMatcher {.Match3Actors_rule.Pattern.A} = ptolemy.actor.lib.Const {.Match3Actors_succ1.A}, ptolemy.actor.gt.AtomicActorMatcher {.Match3Actors_rule.Pattern.B} = ptolemy.actor.lib.Const {.Match3Actors_succ1.B}, ptolemy.actor.gt.AtomicActorMatcher {.Match3Actors_rule.Pattern.C} = ptolemy.actor.lib.gui.Display {.Match3Actors_succ1.C}, ptolemy.actor.gt.Pattern {.Match3Actors_rule.Pattern} = ptolemy.actor.TypedCompositeActor {.Match3Actors_succ1}, ptolemy.actor.gt.PortMatcher {.Match3Actors_rule.Pattern.A.criterion1} = ptolemy.actor.TypedIOPort {.Match3Actors_succ1.A.output}, ptolemy.actor.gt.PortMatcher {.Match3Actors_rule.Pattern.B.criterion1} = ptolemy.actor.TypedIOPort {.Match3Actors_succ1.B.output}, ptolemy.actor.gt.PortMatcher {.Match3Actors_rule.Pattern.C.criterion1} = ptolemy.actor.TypedIOPort {.Match3Actors_succ1.C.input}}}
 
 test Match3Actors.2 {Test Match3Actors_rule.xml with Match3Actors_fail2.xml} {
-    set matchResult [java::call ptolemy.actor.gt.GraphMatcher match Match3Actors_rule.xml Match3Actors_fail2.xml]
+    set matchResult [java::call ptolemy.actor.gt.GraphMatcher match ${p}Match3Actors_rule.xml ${p}Match3Actors_fail2.xml]
     [$matchResult getMatchResult] toString
 } {{}}
 
 test Match3Actors.3 {Test Match3Actors_rule.xml with Match3Actors_succ3.xml} {
-    set matchResult [java::call ptolemy.actor.gt.GraphMatcher match Match3Actors_rule.xml Match3Actors_succ3.xml]
+    set matchResult [java::call ptolemy.actor.gt.GraphMatcher match ${p}Match3Actors_rule.xml ${p}Match3Actors_succ3.xml]
     [$matchResult getMatchResult] toString
 } {{ptolemy.actor.gt.AtomicActorMatcher {.Match3Actors_rule.Pattern.A} = ptolemy.actor.lib.Const {.Match3Actors_succ3.A}, ptolemy.actor.gt.AtomicActorMatcher {.Match3Actors_rule.Pattern.B} = ptolemy.actor.lib.Const {.Match3Actors_succ3.B}, ptolemy.actor.gt.AtomicActorMatcher {.Match3Actors_rule.Pattern.C} = ptolemy.actor.lib.gui.Display {.Match3Actors_succ3.CompositeActor.C}, ptolemy.actor.gt.Pattern {.Match3Actors_rule.Pattern} = ptolemy.actor.TypedCompositeActor {.Match3Actors_succ3}, ptolemy.actor.gt.PortMatcher {.Match3Actors_rule.Pattern.A.criterion1} = ptolemy.actor.TypedIOPort {.Match3Actors_succ3.A.output}, ptolemy.actor.gt.PortMatcher {.Match3Actors_rule.Pattern.B.criterion1} = ptolemy.actor.TypedIOPort {.Match3Actors_succ3.B.output}, ptolemy.actor.gt.PortMatcher {.Match3Actors_rule.Pattern.C.criterion1} = ptolemy.actor.TypedIOPort {.Match3Actors_succ3.CompositeActor.C.input}}}
 
 test Match3Actors.4 {Test Match3Actors_rule.xml with Match3Actors_succ4.xml} {
-    set matchResult [java::call ptolemy.actor.gt.GraphMatcher match Match3Actors_rule.xml Match3Actors_succ4.xml]
+    set matchResult [java::call ptolemy.actor.gt.GraphMatcher match ${p}Match3Actors_rule.xml ${p}Match3Actors_succ4.xml]
     [$matchResult getMatchResult] toString
 } {{ptolemy.actor.gt.AtomicActorMatcher {.Match3Actors_rule.Pattern.A} = ptolemy.actor.lib.Const {.Match3Actors_succ4.CompositeActor.A}, ptolemy.actor.gt.AtomicActorMatcher {.Match3Actors_rule.Pattern.B} = ptolemy.actor.lib.Const {.Match3Actors_succ4.CompositeActor.B}, ptolemy.actor.gt.AtomicActorMatcher {.Match3Actors_rule.Pattern.C} = ptolemy.actor.lib.gui.Display {.Match3Actors_succ4.C}, ptolemy.actor.gt.Pattern {.Match3Actors_rule.Pattern} = ptolemy.actor.TypedCompositeActor {.Match3Actors_succ4}, ptolemy.actor.gt.PortMatcher {.Match3Actors_rule.Pattern.A.criterion1} = ptolemy.actor.TypedIOPort {.Match3Actors_succ4.CompositeActor.A.output}, ptolemy.actor.gt.PortMatcher {.Match3Actors_rule.Pattern.B.criterion1} = ptolemy.actor.TypedIOPort {.Match3Actors_succ4.CompositeActor.B.output}, ptolemy.actor.gt.PortMatcher {.Match3Actors_rule.Pattern.C.criterion1} = ptolemy.actor.TypedIOPort {.Match3Actors_succ4.C.input}}}
 
 test Match3Actors.5 {Test Match3Actors_rule.xml with Match3Actors_succ5.xml} {
-    set matchResult [java::call ptolemy.actor.gt.GraphMatcher match Match3Actors_rule.xml Match3Actors_succ5.xml]
+    set matchResult [java::call ptolemy.actor.gt.GraphMatcher match ${p}Match3Actors_rule.xml ${p}Match3Actors_succ5.xml]
     [$matchResult getMatchResult] toString
 } {{ptolemy.actor.gt.AtomicActorMatcher {.Match3Actors_rule.Pattern.A} = ptolemy.actor.lib.Const {.Match3Actors_succ5.CompositeActor.A}, ptolemy.actor.gt.AtomicActorMatcher {.Match3Actors_rule.Pattern.B} = ptolemy.actor.lib.Const {.Match3Actors_succ5.CompositeActor.B}, ptolemy.actor.gt.AtomicActorMatcher {.Match3Actors_rule.Pattern.C} = ptolemy.actor.lib.gui.Display {.Match3Actors_succ5.CompositeActor2.C}, ptolemy.actor.gt.Pattern {.Match3Actors_rule.Pattern} = ptolemy.actor.TypedCompositeActor {.Match3Actors_succ5}, ptolemy.actor.gt.PortMatcher {.Match3Actors_rule.Pattern.A.criterion1} = ptolemy.actor.TypedIOPort {.Match3Actors_succ5.CompositeActor.A.output}, ptolemy.actor.gt.PortMatcher {.Match3Actors_rule.Pattern.B.criterion1} = ptolemy.actor.TypedIOPort {.Match3Actors_succ5.CompositeActor.B.output}, ptolemy.actor.gt.PortMatcher {.Match3Actors_rule.Pattern.C.criterion1} = ptolemy.actor.TypedIOPort {.Match3Actors_succ5.CompositeActor2.C.input}}}
 
 test Match3Actors.6 {Test Match3Actors_rule.xml with Match3Actors_fail6.xml} {
-    set matchResult [java::call ptolemy.actor.gt.GraphMatcher match Match3Actors_rule.xml Match3Actors_fail6.xml]
+    set matchResult [java::call ptolemy.actor.gt.GraphMatcher match ${p}Match3Actors_rule.xml ${p}Match3Actors_fail6.xml]
     [$matchResult getMatchResult] toString
 } {{}}
 
 test Match3Actors.7 {Test Match3Actors_rule.xml with Match3Actors_succ7.xml} {
-    set matchResult [java::call ptolemy.actor.gt.GraphMatcher match Match3Actors_rule.xml Match3Actors_succ7.xml]
+    set matchResult [java::call ptolemy.actor.gt.GraphMatcher match ${p}Match3Actors_rule.xml ${p}Match3Actors_succ7.xml]
     [$matchResult getMatchResult] toString
 } {{ptolemy.actor.gt.AtomicActorMatcher {.Match3Actors_rule.Pattern.A} = ptolemy.actor.lib.Const {.Match3Actors_succ7.CompositeActor.A}, ptolemy.actor.gt.AtomicActorMatcher {.Match3Actors_rule.Pattern.B} = ptolemy.actor.lib.Const {.Match3Actors_succ7.CompositeActor.B}, ptolemy.actor.gt.AtomicActorMatcher {.Match3Actors_rule.Pattern.C} = ptolemy.actor.lib.gui.Display {.Match3Actors_succ7.C}, ptolemy.actor.gt.Pattern {.Match3Actors_rule.Pattern} = ptolemy.actor.TypedCompositeActor {.Match3Actors_succ7}, ptolemy.actor.gt.PortMatcher {.Match3Actors_rule.Pattern.A.criterion1} = ptolemy.actor.TypedIOPort {.Match3Actors_succ7.CompositeActor.A.output}, ptolemy.actor.gt.PortMatcher {.Match3Actors_rule.Pattern.B.criterion1} = ptolemy.actor.TypedIOPort {.Match3Actors_succ7.CompositeActor.B.output}, ptolemy.actor.gt.PortMatcher {.Match3Actors_rule.Pattern.C.criterion1} = ptolemy.actor.TypedIOPort {.Match3Actors_succ7.C.input}}}
 
 test Match3Actors.8 {Test Match3Actors_rule.xml with Match3Actors_succ8} {
-    set matchResult [java::call ptolemy.actor.gt.GraphMatcher match Match3Actors_rule.xml Match3Actors_succ8.xml]
+    set matchResult [java::call ptolemy.actor.gt.GraphMatcher match ${p}Match3Actors_rule.xml ${p}Match3Actors_succ8.xml]
     [$matchResult getMatchResult] toString
 } {{ptolemy.actor.gt.AtomicActorMatcher {.Match3Actors_rule.Pattern.A} = ptolemy.actor.lib.Const {.Match3Actors_succ8.CompositeActor.A}, ptolemy.actor.gt.AtomicActorMatcher {.Match3Actors_rule.Pattern.B} = ptolemy.actor.lib.Const {.Match3Actors_succ8.CompositeActor.B}, ptolemy.actor.gt.AtomicActorMatcher {.Match3Actors_rule.Pattern.C} = ptolemy.actor.lib.gui.Display {.Match3Actors_succ8.C}, ptolemy.actor.gt.Pattern {.Match3Actors_rule.Pattern} = ptolemy.actor.TypedCompositeActor {.Match3Actors_succ8}, ptolemy.actor.gt.PortMatcher {.Match3Actors_rule.Pattern.A.criterion1} = ptolemy.actor.TypedIOPort {.Match3Actors_succ8.CompositeActor.A.output}, ptolemy.actor.gt.PortMatcher {.Match3Actors_rule.Pattern.B.criterion1} = ptolemy.actor.TypedIOPort {.Match3Actors_succ8.CompositeActor.B.output}, ptolemy.actor.gt.PortMatcher {.Match3Actors_rule.Pattern.C.criterion1} = ptolemy.actor.TypedIOPort {.Match3Actors_succ8.C.input}}}
