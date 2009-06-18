6a67d214d346fa290bad26da3241889f75f83ea6
mankit <mankit>
2009-06-18 19:55:32 +0000

    added javadoc comments.

--- ptolemy/domains/properties/kernel/ModelAttribute.java
+++ ptolemy/domains/properties/kernel/ModelAttribute.java
@@ -1,30 +1,27 @@
 /*
-
- Copyright (c) 2008-2009 The Regents of the University of California.
- All rights reserved.
- Permission is hereby granted, without written agreement and without
- license or royalty fees, to use, copy, modify, and distribute this
- software and its documentation for any purpose, provided that the above
- copyright notice and the following two paragraphs appear in all copies
- of this software.
-
- IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
- FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
- ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
- THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
- SUCH DAMAGE.
-
- THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
- INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
- MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
- PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
- CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
- ENHANCEMENTS, OR MODIFICATIONS.
-
- PT_COPYRIGHT_VERSION_2
- COPYRIGHTENDKEY
-
-*/
+ * A base class for attributes that contain a refinement model graph.
+ * 
+ * Copyright (c) 2008-2009 The Regents of the University of California. All
+ * rights reserved. Permission is hereby granted, without written agreement and
+ * without license or royalty fees, to use, copy, modify, and distribute this
+ * software and its documentation for any purpose, provided that the above
+ * copyright notice and the following two paragraphs appear in all copies of
+ * this software.
+ * 
+ * IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY FOR
+ * DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES ARISING OUT
+ * OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF THE UNIVERSITY OF
+ * CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * 
+ * THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE PROVIDED HEREUNDER IS ON AN
+ * "AS IS" BASIS, AND THE UNIVERSITY OF CALIFORNIA HAS NO OBLIGATION TO PROVIDE
+ * MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+ * 
+ * PT_COPYRIGHT_VERSION_2 COPYRIGHTENDKEY
+ * 
+ */
 package ptolemy.domains.properties.kernel;
 
 import java.io.IOException;
@@ -33,7 +30,6 @@ import java.io.Writer;
 import java.net.URL;
 
 import ptolemy.actor.Manager;
-import ptolemy.domains.de.kernel.DEDirector;
 import ptolemy.domains.fsm.kernel.Configurer;
 import ptolemy.kernel.CompositeEntity;
 import ptolemy.kernel.attributes.URIAttribute;
@@ -46,24 +42,31 @@ import ptolemy.kernel.util.NameDuplicationException;
 import ptolemy.kernel.util.NamedObj;
 import ptolemy.kernel.util.Workspace;
 import ptolemy.moml.MoMLParser;
-import ptolemy.vergil.gt.TransformationAttributeEditorFactory;
 import ptolemy.vergil.properties.ModelAttributeController;
 
 //////////////////////////////////////////////////////////////////////////
 //// ModelAttribute
 
 /**
-
-
- @author Man-Kit Leung
- @version $Id$
- @since Ptolemy II 7.1
- @Pt.ProposedRating Red (tfeng)
- @Pt.AcceptedRating Red (tfeng)
+ * A base class for attributes that contain a refinement model graph.
+ * 
+ * @author Man-Kit Leung
+ * @version $Id$
+ * @since Ptolemy II 7.1
+ * @Pt.ProposedRating Red (tfeng)
+ * @Pt.AcceptedRating Red (tfeng)
  */
 public class ModelAttribute extends Attribute implements Configurable {
 
-
+    /**
+     * Create a model attribute with the specified container and name.
+     * @param container The specified container.
+     * @param name The specified name.
+     * @exception IllegalActionException If the attribute is not of an
+     * acceptable class for the container, or if the name contains a period.
+     * @exception NameDuplicationException If the name coincides with an
+     * attribute already in the container.
+     */
     public ModelAttribute(NamedObj container, String name)
             throws NameDuplicationException, IllegalActionException {
         super(container, name);
@@ -71,6 +74,12 @@ public class ModelAttribute extends Attribute implements Configurable {
         _init();
     }
 
+    /**
+     * Construct an attribute in the specified workspace with an empty string as
+     * a name. If the workspace argument is null, then use the default
+     * workspace. The object is added to the directory of the workspace.
+     * @param workspace The workspace that will list the attribute.
+     */
     public ModelAttribute(Workspace workspace) {
         super(workspace);
 
@@ -81,6 +90,13 @@ public class ModelAttribute extends Attribute implements Configurable {
         }
     }
 
+    /**
+     * Construct an attribute in the specified workspace with the specified
+     * name. If the workspace argument is null, then use the default workspace.
+     * The object is added to the directory of the workspace.
+     * @param workspace The workspace that will list the attribute.
+     * @param name The specified name.
+     */
     public ModelAttribute(Workspace workspace, String name) {
         super(workspace);
 
@@ -92,25 +108,47 @@ public class ModelAttribute extends Attribute implements Configurable {
         }
     }
 
+    ///////////////////////////////////////////////////////////////////
+    ////                         public methods                    ////
+
+    /**
+     * Return a clone of this model attribute. This also creates a clone for the
+     * contained model.
+     * @return A clone.
+     * @throws CloneNotSupportedException Thrown if an error occurs while
+     * cloning the attribute or the contained model.
+     */
+    @Override
     public Object clone(Workspace workspace) throws CloneNotSupportedException {
-        ModelAttribute newObject =
-            (ModelAttribute) super.clone(workspace);
+        ModelAttribute newObject = (ModelAttribute) super.clone(workspace);
         try {
             newObject._configurer = new Configurer(workspace);
             newObject._configurer.setName("Configurer");
-            new DEDirector(newObject._configurer, "_director");
-            newObject._configurer.setManager(new Manager(workspace,
-                    "_manager"));
+
+            //new DEDirector(newObject._configurer, "_director");
+
+            newObject._configurer
+                    .setManager(new Manager(workspace, "_manager"));
             newObject._configurer.setConfiguredObject(newObject);
-            newObject._model = (CompositeEntity) _model.clone(
-                    workspace);
-            ((CompositeEntity) newObject._model).setContainer(newObject._configurer);
+
+            newObject._model = (CompositeEntity) _model.clone(workspace);
+            newObject._model.setContainer(newObject._configurer);
         } catch (KernelException e) {
             throw new InternalErrorException(e);
         }
         return newObject;
     }
 
+    /**
+     * Construct and configure the contained model with the specified source and
+     * text. It parses the specified MoML text and clear clears the URI
+     * information from the constructed model.
+     * @param base The base URL for relative references, or null if not known.
+     * @param source The URI of the document.
+     * @param text The MoML description.
+     * @exception Exception If the parsing fails or the URI info cannot be
+     * cleared.
+     */
     public void configure(URL base, String source, String text)
             throws Exception {
         _configureSource = source;
@@ -124,40 +162,73 @@ public class ModelAttribute extends Attribute implements Configurable {
         }
     }
 
+    /**
+     * Return the configure source.
+     * @return The configure source.
+     */
     public String getConfigureSource() {
         return _configureSource;
     }
 
+    /**
+     * Return the configure text. In this base class, this returns null.
+     * @return The configure text.
+     */
     public String getConfigureText() {
         return null;
     }
 
+    /**
+     * Return the contained model.
+     * @return The contained model.
+     */
     public CompositeEntity getContainedModel() {
         return _model;
     }
 
-    // FIXME: This class uses classes from vergil, which means the backend 
-    // and the gui are too tighly intertwined.
-    public TransformationAttributeEditorFactory editorFactory;
+    ///////////////////////////////////////////////////////////////////
+    ////                      protected methods                    ////
 
+    /**
+     * Write a MoML description of the contents of this object. This wraps the
+     * MoML description of the contained model within the &lt;configure&gt; tag.
+     * @param output The output stream to write to.
+     * @param depth The depth in the hierarchy, to determine indenting.
+     * @throws IOException If an I/O error occurs.
+     */
+    @Override
     protected void _exportMoMLContents(Writer output, int depth)
-    throws IOException {
+            throws IOException {
         super._exportMoMLContents(output, depth);
 
         String sourceSpec = "";
 
-        if ((_configureSource != null) && !_configureSource.trim().equals("")) {
+        if (_configureSource != null && !_configureSource.trim().equals("")) {
             sourceSpec = " source=\"" + _configureSource + "\"";
         }
 
-        output.write(_getIndentPrefix(depth) + "<configure" + sourceSpec +
-                ">\n");
+        output.write(_getIndentPrefix(depth) + "<configure" + sourceSpec
+                + ">\n");
         _model.exportMoML(output, depth + 1);
         output.write(_getIndentPrefix(depth) + "</configure>\n");
     }
 
+    /**
+     * Return the class name of the contained model. The class name is used to
+     * instantiate the contained model. By default, this returns the string
+     * "ptolemy.actor.CompositeActor". If base classes can override this method
+     * to instantiate a different type of top-level for the contained model.
+     * @return the class name of the contained model.
+     */
+    protected String _getContainedModelClassName() {
+        return "ptolemy.actor.CompositeActor";
+    }
+
+    ///////////////////////////////////////////////////////////////////
+    ////                        private methods                    ////
+
     private static void _clearURI(NamedObj object)
-    throws IllegalActionException, NameDuplicationException {
+            throws IllegalActionException, NameDuplicationException {
         URIAttribute attribute = (URIAttribute) object.getAttribute("_uri",
                 URIAttribute.class);
         if (attribute != null) {
@@ -170,34 +241,34 @@ public class ModelAttribute extends Attribute implements Configurable {
 
         _configurer = new Configurer(workspace());
         _configurer.setName("Configurer");
-        new DEDirector(_configurer, "_director");
+
+        // FIXME: do we need a director for the configurer?
+        //new DEDirector(_configurer, "_director");
+
         _configurer.setManager(new Manager(workspace(), "_manager"));
         _configurer.setConfiguredObject(this);
 
-        String moml = "<entity name=\"Model\" " +
-                "class=\"" + _getContainedModelClassName() + "\"/>";
+        String moml = "<entity name=\"Model\" " + "class=\""
+                + _getContainedModelClassName() + "\"/>";
         MoMLParser parser = new MoMLParser();
         parser.setContext(_configurer);
         try {
             parser.parse(moml);
         } catch (Exception e) {
-            throw new IllegalActionException(this, e, "Unable to populate " +
-                    "the lattice model within \"" + getFullName() +
-                    "\".");
+            throw new IllegalActionException(this, e, "Unable to populate "
+                    + "the model within \"" + getFullName() + "\".");
         }
         _model = (CompositeEntity) _configurer.getEntity("Model");
 
         //new ModelAttributeIcon(this, "_icon");
-	// FIXME: This class uses classes from vergil, which means the backend 
-	// and the gui are too tighly intertwined.
-        new ModelAttributeController.Factory(this,
-                "_controllerFactory");
+        // FIXME: This class uses classes from vergil, which means the backend 
+        // and the gui are too tighly intertwined.
+        new ModelAttributeController.Factory(this, "_controllerFactory");
         //editorFactory = new TransformationAttributeEditorFactory(this, "editorFactory");
     }
 
-    protected String _getContainedModelClassName() {
-        return "ptolemy.actor.CompositeActor";
-    }
+    ///////////////////////////////////////////////////////////////////
+    ////                      private variables                    ////
 
     private String _configureSource;
 
--- ptolemy/domains/properties/kernel/OntologyAttribute.java
+++ ptolemy/domains/properties/kernel/OntologyAttribute.java
@@ -1,29 +1,28 @@
 /*
-Below is the copyright agreement for the Ptolemy II system.
-Version: $Id$
-
-Copyright (c) 2009 The Regents of the University of California.
-All rights reserved.
-
-Permission is hereby granted, without written agreement and without
-license or royalty fees, to use, copy, modify, and distribute this
-software and its documentation for any purpose, provided that the above
-copyright notice and the following two paragraphs appear in all copies
-of this software.
-
-IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
-FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
-ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
-THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
-SUCH DAMAGE.
-
-THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
-INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
-MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
-PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
-CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
-ENHANCEMENTS, OR MODIFICATIONS.
-*/
+ * An attribute that contains a ontology model graph.
+ * 
+ * Below is the copyright agreement for the Ptolemy II system. Version: $Id:
+ * OntologyAttribute.java 53505 2009-05-06 18:18:38Z bldmastr $
+ * 
+ * Copyright (c) 2009 The Regents of the University of California. All rights
+ * reserved.
+ * 
+ * Permission is hereby granted, without written agreement and without license
+ * or royalty fees, to use, copy, modify, and distribute this software and its
+ * documentation for any purpose, provided that the above copyright notice and
+ * the following two paragraphs appear in all copies of this software.
+ * 
+ * IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY FOR
+ * DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES ARISING OUT
+ * OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF THE UNIVERSITY OF
+ * CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * 
+ * THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE PROVIDED HEREUNDER IS ON AN
+ * "AS IS" BASIS, AND THE UNIVERSITY OF CALIFORNIA HAS NO OBLIGATION TO PROVIDE
+ * MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+ */
 package ptolemy.domains.properties.kernel;
 
 import ptolemy.actor.CompositeActor;
@@ -36,8 +35,26 @@ import ptolemy.kernel.util.StringAttribute;
 import ptolemy.kernel.util.Workspace;
 import ptolemy.vergil.toolbox.TextEditorTableauFactory;
 
+/**
+ * An attribute that contains a ontology model graph.
+ * 
+ * @author Man-Kit Leung
+ * @version $Id$
+ * @since Ptolemy II 7.1
+ * @Pt.ProposedRating Red (mankit)
+ * @Pt.AcceptedRating Red (mankit)
+ */
 public class OntologyAttribute extends ModelAttribute {
 
+    /**
+     * Construct an ontology attribute with the specified container and name.
+     * @param container The specified container.
+     * @param name The specified name.
+     * @exception IllegalActionException If the attribute is not of an
+     * acceptable class for the container, or if the name contains a period.
+     * @exception NameDuplicationException If the name coincides with an
+     * attribute already in the container.
+     */
     public OntologyAttribute(CompositeEntity container, String name)
             throws IllegalActionException, NameDuplicationException {
         super(container, name);
@@ -52,36 +69,76 @@ public class OntologyAttribute extends ModelAttribute {
     ///////////////////////////////////////////////////////////////////
     ////                         public methods                    ////
 
-    public void parseSpecificationRules() {
+    //    public void parseSpecificationRules() {
+    //    }
+    //
+    //    public Object executeRules() {
+    //        return null;
+    //    }
 
-    }
-
-    public Object executeRules() {
-        return null;
-    }
-
+    /**
+     * Return the class name of the contained model top-level. A property
+     * lattice graph is contained by a OntologyComposite, so this returns the
+     * string
+     * "ptolemy.domains.properties.kernel.OntologyAttribute$OntologyComposite".
+     * @return the class name of the contained model top-level.
+     */
+    @Override
     protected String _getContainedModelClassName() {
         return getClass().getName() + "$OntologyComposite";
     }
 
+    /**
+     * A composite actor that contains definition of an ontology.
+     */
     public static class OntologyComposite extends CompositeActor {
 
-        public static final String RULES = "_rules";
-
+        /**
+         * Construct an ontology composite with the specified container and
+         * name.
+         * @param container The specified container.
+         * @param name The specified name.
+         * @exception IllegalActionException If the composite is not of an
+         * acceptable class for the container, or if the name contains a period.
+         * @exception NameDuplicationException If the name coincides with an
+         * attribute already in the container.
+         */
         public OntologyComposite(CompositeEntity container, String name)
-        throws IllegalActionException, NameDuplicationException {
+                throws IllegalActionException, NameDuplicationException {
             super(container, name);
         }
 
+        /**
+         * Construct an OntologyComposite in the specified workspace with an
+         * empty string as a name.
+         * @param workspace The specified workspace.
+         */
         public OntologyComposite(Workspace workspace) {
             super(workspace);
         }
 
         ///////////////////////////////////////////////////////////////////
-        ////                         public methods                    ////
+        ////                          public fields                    ////
 
+        /**
+         * The name for the rules attributes.
+         */
+        public static final String RULES = "_rules";
+
+        /**
+         * Add an entity to this container. An entity is a representative of its
+         * type in an ontology composite. Each entity is added an "_rules"
+         * attribute that specifies the property resolution constraints for this
+         * type of entities.
+         * @param entity Entity to contain.
+         * @exception IllegalActionException If the "_rules" attribute or the
+         * entity cannot be added.
+         * @exception NameDuplicationException If the name collides with a name
+         * already on the actor contents list.
+         */
+        @Override
         protected void _addEntity(ComponentEntity entity)
-        throws IllegalActionException, NameDuplicationException {
+                throws IllegalActionException, NameDuplicationException {
 
             if (entity.getAttribute(RULES) == null) {
                 StringAttribute userRules = new StringAttribute(entity, RULES);
@@ -89,16 +146,14 @@ public class OntologyAttribute extends ModelAttribute {
             }
 
             if (entity.getAttribute("_tableauFactory") == null) {
-		// FIXME: This class uses classes from vergil, which means the backend 
-		// and the gui are too tighly intertwined.
-                TextEditorTableauFactory factory =
-                    new TextEditorTableauFactory(entity, "_tableauFactory");
+                // FIXME: This class uses classes from vergil, which means the backend 
+                // and the gui are too tighly intertwined.
+                TextEditorTableauFactory factory = new TextEditorTableauFactory(
+                        entity, "_tableauFactory");
                 factory.attributeName.setExpression(RULES);
 
             }
             super._addEntity(entity);
         }
-
-
     }
 }
