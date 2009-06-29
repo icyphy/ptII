962beeba2de254d7abd98832f74e83dfdb8b3bcf
mankit <mankit>
2009-06-29 22:09:34 +0000

    added javadoc documentations.

--- ptolemy/data/properties/lattice/MonotonicFunction.java
+++ ptolemy/data/properties/lattice/MonotonicFunction.java
@@ -1,24 +1,24 @@
 /*
- * A Base class for monotonic function type constraints.
- *
+ * A base class for monotonic function property constraints.
+ * 
  * Copyright (c) 1998-2009 The Regents of the University of California. All
  * rights reserved. Permission is hereby granted, without written agreement and
  * without license or royalty fees, to use, copy, modify, and distribute this
  * software and its documentation for any purpose, provided that the above
  * copyright notice and the following two paragraphs appear in all copies of
  * this software.
- *
+ * 
  * IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY FOR
  * DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES ARISING OUT
  * OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF THE UNIVERSITY OF
  * CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
+ * 
  * THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
  * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
  * FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE PROVIDED HEREUNDER IS ON AN
  * "AS IS" BASIS, AND THE UNIVERSITY OF CALIFORNIA HAS NO OBLIGATION TO PROVIDE
  * MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
- *
+ * 
  * PT_COPYRIGHT_VERSION_2 COPYRIGHTENDKEY
  */
 package ptolemy.data.properties.lattice;
@@ -32,19 +32,19 @@ import ptolemy.graph.InequalityTerm;
 //// MonotonicFunction
 
 /**
- * Actors often need to implement monotonic functions as part of the declaration
- * of type constraints. This base class makes it easy to do so. In most cases,
- * it is simply necessary to implement the getValue() and getVariables()
- * abstract methods.
- *
- * @author Steve Neuendorffer
+ * Monotonic functions are often used as part of the declaration of property
+ * constraints. This base class makes it easy to do so. It is simply necessary
+ * to implement the _getDependentTerms abstract methods.
+ * 
+ * @author Man-Kit Leung
  * @version $Id$
  * @since Ptolemy II 7.1
- * @Pt.ProposedRating Yellow (neuendor)
- * @Pt.AcceptedRating Red (neuendor)
+ * @Pt.ProposedRating Red (mankit)
+ * @Pt.AcceptedRating Red (mankit)
  */
 public abstract class MonotonicFunction extends
         ptolemy.data.type.MonotonicFunction implements PropertyTerm {
+
     ///////////////////////////////////////////////////////////////////
     ////                         public methods                    ////
 
@@ -54,7 +54,6 @@ public abstract class MonotonicFunction extends
      * function depends on.
      * @return An array of InequalityTerm.
      */
-    @Override
     public final InequalityTerm[] getVariables() {
         List<InequalityTerm> terms = new ArrayList<InequalityTerm>();
 
@@ -77,7 +76,7 @@ public abstract class MonotonicFunction extends
      * a function, return an array containing all the constants in the function.
      * @return An array of InequalityTerms
      */
-    public InequalityTerm[] getConstants() {
+    public final InequalityTerm[] getConstants() {
         List<InequalityTerm> terms = new ArrayList<InequalityTerm>();
 
         for (InequalityTerm term : _getDependentTerms()) {
@@ -92,6 +91,12 @@ public abstract class MonotonicFunction extends
         return array;
     }
 
-    protected abstract InequalityTerm[] _getDependentTerms();
+    ///////////////////////////////////////////////////////////////////
+    ////                      protected methods                    ////
 
+    /**
+     * Return an array of terms depended by this monotonic function.
+     * @return an array of terms depended by this monotonic function.
+     */
+    protected abstract InequalityTerm[] _getDependentTerms();
 }
