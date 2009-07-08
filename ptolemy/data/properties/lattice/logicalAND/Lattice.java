c371ba165fee167ac35c274985d7b702de0ea7f0
mankit <mankit>
2009-07-08 16:26:26 +0000

    added javadoc documentations.

--- ptolemy/data/properties/lattice/logicalAND/Lattice.java
+++ ptolemy/data/properties/lattice/logicalAND/Lattice.java
@@ -1,53 +1,46 @@
 /*
- * An ontology lattice.
- *
- * Copyright (c) 1997-2009 The Regents of the University of California. All
+ * An ontology lattice for the logicalAND use case.
+ * 
+ * Copyright (c) 2007-2009 The Regents of the University of California. All
  * rights reserved. Permission is hereby granted, without written agreement and
  * without license or royalty fees, to use, copy, modify, and distribute this
  * software and its documentation for any purpose, provided that the above
  * copyright notice and the following two paragraphs appear in all copies of
  * this software.
- *
+ * 
  * IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY FOR
  * DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES ARISING OUT
  * OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF THE UNIVERSITY OF
  * CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
+ * 
  * THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
  * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
  * FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE PROVIDED HEREUNDER IS ON AN
  * "AS IS" BASIS, AND THE UNIVERSITY OF CALIFORNIA HAS NO OBLIGATION TO PROVIDE
  * MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
- *
+ * 
  * PT_COPYRIGHT_VERSION_2 COPYRIGHTENDKEY
- *
+ * 
  */
 package ptolemy.data.properties.lattice.logicalAND;
 
 import ptolemy.data.properties.Property;
-import ptolemy.data.properties.lattice.LatticeProperty;
 import ptolemy.data.properties.lattice.PropertyLattice;
-import ptolemy.data.properties.lattice.RecordProperty;
 
 //////////////////////////////////////////////////////////////////////////
 //// Lattice
 
 /**
- * Property hierarchy base class. Note that all public methods are synchronized.
- * There are more than one instances of a property lattice. Although the
- * property lattice is constructed once and then typically does not change
- * during execution, the methods need to be synchronized because there are
- * various data structures used to cache results that are expensive to compute.
- * These data structures do change during execution. Multiple threads may be
- * accessing the property lattice simultaneously and modifying these data
- * structures. To ensure thread safety, the methods need to be synchronized.
- *
- * @author Thomas Mandl, Man-Kit Leung, Edward A. Lee
+ * An ontology lattice for the logicalAND use case. The lattice contains three
+ * elements: TRUE, FALSE and UNKNOWN. UNKNOWN is the bottom, and FALSE is the
+ * top, such that the order of the elements looks like UKNOWN -> TRUE -> FALSE.
+ * 
+ * @author Thomas Mandl, Man-Kit Leung
  * @version $Id$
  * @since Ptolemy II 7.1
  * @Pt.ProposedRating Red (mankit)
  * @Pt.AcceptedRating Red (mankit)
- *
+ * 
  */
 public class Lattice extends PropertyLattice {
 
@@ -59,14 +52,10 @@ public class Lattice extends PropertyLattice {
         addNodeWeight(FALSE);
         addNodeWeight(UNKNOWN);
 
-        // Record Property
-        //FIXME: add an artificial top.
-        addNodeWeight(RECORD);
-        addEdge(UNKNOWN, RECORD);
-        addEdge(RECORD, FALSE);
-
         addEdge(UNKNOWN, TRUE);
         addEdge(TRUE, FALSE);
+
+        addStructuredProperties(RECORD);
 
         if (!isLattice()) {
             throw new AssertionError("This ontology is not a lattice.");
@@ -76,13 +65,19 @@ public class Lattice extends PropertyLattice {
     ///////////////////////////////////////////////////////////////////
     ////                         private variables                 ////
 
+    /**
+     * The lattice element named FALSE.
+     */
     private final Property FALSE = new False(this);
 
-    private final Property RECORD = new RecordProperty(this, new String[0],
-            new LatticeProperty[0]).getRepresentative();
-
+    /**
+     * The lattice element named TRUE.
+     */
     private final Property TRUE = new True(this);
 
+    /**
+     * The lattice element named UNKNOWN.
+     */
     private final Property UNKNOWN = new Unknown(this);
 
 }
