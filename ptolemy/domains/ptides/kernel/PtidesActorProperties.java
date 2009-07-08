614ff1b835baf4c4fcbf6674da8c32be90de8e8a
cxh <cxh>
2009-07-08 19:54:56 +0000

    Fix javadoc warnings and badly formatted copyright.

--- ptolemy/domains/ptides/kernel/PtidesActorProperties.java
+++ ptolemy/domains/ptides/kernel/PtidesActorProperties.java
@@ -1,19 +1,29 @@
-/*
- * @Copyright (c) 2008-2009 The Regents of the University of California. All rights
- * reserved. Permission is hereby granted, without written agreement and without
- * license or royalty fees, to use, copy, modify, and distribute this software
- * and its documentation for any purpose, provided that the above copyright
- * notice and the following two paragraphs appear in all copies of this
- * software. IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY
- * PARTY FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
- * ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF THE
- * UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- * THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
- * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
- * FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE PROVIDED HEREUNDER IS ON AN
- * "AS IS" BASIS, AND THE UNIVERSITY OF CALIFORNIA HAS NO OBLIGATION TO PROVIDE
- * MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
- * PT_COPYRIGHT_VERSION_2 COPYRIGHTENDKEY
+/** A class representing the type of an ArrayToken.
+
+ Copyright (c) 2008-2009 The Regents of the University of California.
+ All rights reserved.
+ Permission is hereby granted, without written agreement and without
+ license or royalty fees, to use, copy, modify, and distribute this
+ software and its documentation for any purpose, provided that the above
+ copyright notice and the following two paragraphs appear in all copies
+ of this software.
+
+ IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+ FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+ ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+ THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+ SUCH DAMAGE.
+
+ THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+ INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+ PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+ CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ ENHANCEMENTS, OR MODIFICATIONS.
+
+ PT_COPYRIGHT_VERSION_2
+ COPYRIGHTENDKEY
+
  */
 package ptolemy.domains.ptides.kernel;
 
@@ -48,8 +58,8 @@ import ptolemy.kernel.util.NamedObj;
  * </ul>.</p>
  *
  * @author Patricia Derler
-@version $Id$
-@since Ptolemy II 7.1
+ * @version $Id$
+ * @since Ptolemy II 7.1
  */
 public class PtidesActorProperties {
 
@@ -151,7 +161,7 @@ public class PtidesActorProperties {
     /** Return the execution time of the port or the worst case
      *  execution time if no execution time is specified, or
      *  null if neither is specified.
-     *  @param actor The actor for which the execution time is
+     *  @param port The port for which the execution time is
      *   requested.
      *  @return The execution time.
      *  @exception IllegalActionException If there is an _executionTime
@@ -217,8 +227,8 @@ public class PtidesActorProperties {
      * Return the worst case execution time of the port or null if no worst case
      * execution time was specified.
      *
-     * @param actor
-     *            The actor for which the worst case execution time is
+     * @param port
+     *            The port for which the worst case execution time is
      *            requested.
      * @return The worst case execution time.
      */
