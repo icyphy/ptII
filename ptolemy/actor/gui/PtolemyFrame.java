32af04b386b5d0305bc078e0faed6390892afaad
crawl <crawl>
2009-07-08 18:29:30 +0000

    Call _saveAs(String) instead of super._saveAs(String) since it may be overridden.

--- ptolemy/actor/gui/PtolemyFrame.java
+++ ptolemy/actor/gui/PtolemyFrame.java
@@ -336,7 +336,7 @@ public abstract class PtolemyFrame extends TableauFrame {
         }
 
         // If the user saves a file without an extension, we force .xml.
-        return super._saveAs(".xml");
+        return _saveAs(".xml");
     }
 
     /** Create and return a file dialog for the "Save As" command.
