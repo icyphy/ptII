cdda3bca6cf302aba292c87785216b9a855eef3c
mwetter <mwetter>
2009-07-15 00:26:39 +0000

    Corrected argument for libisloaded

--- lbnl/lib/matlab/closeIPC.m
+++ lbnl/lib/matlab/closeIPC.m
@@ -7,12 +7,13 @@ function retVal = closeIPC(sockfd)
 
 %  Revision history
 %  ----------------  
+%  2009-07-14 MWetter@lbl.gov: Corrected argument for libisloaded(...)
 %  2009-06-26 MWetter@lbl.gov: First version, based on code of 
 %             Charles Corbin, UC Boulder
 if (sockfd < 0 )
   fprintf('Warning: Cannot close socket in closeIPC(%d) because argument is negative', sockfd);
   retVal = -1;
-elseif ~libisloaded('bcvtb')
+elseif ~libisloaded(getBCVTBLibName())
   fprintf('Warning: Cannot close socket in closeIPC because BCVTB library is not loaded');
   retVal = -2;
 else
