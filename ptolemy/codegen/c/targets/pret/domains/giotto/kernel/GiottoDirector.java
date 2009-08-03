ff9bd7e4a06103d091d590d56a7040c1801f98f9
sssf <sssf>
2009-08-03 22:33:22 +0000

    we commented out a for loop. I think.

--- ptolemy/codegen/c/targets/pret/domains/giotto/kernel/GiottoDirector.java
+++ ptolemy/codegen/c/targets/pret/domains/giotto/kernel/GiottoDirector.java
@@ -306,7 +306,17 @@ public class GiottoDirector extends
             throws IllegalActionException {
         StringBuffer code = new StringBuffer();
         double period = _getPeriod();
-        int milliseconds = (int) (1000 * period / _getFrequency(actor));
+        // FIXME: generate deadline instruction w/ period
+
+        // Note: Currently, the deadline instruction takes the number 
+        // of cycle as argument. In the future, this should be changed
+        // to use time unit, which is platform-independent. 
+        // So for now, we will assume the clock speed to be 250 Mhz. 
+        // Thus, in order to get a delay of t seconds (= period/frequency) 
+        // for each actor, we will need to wait for 
+        // period/frequency * 250,000,000 cycles/second.
+
+        int cycles = (int) (250000000 * period / _getFrequency(actor));
         String driverBound = CodeGeneratorHelper.generateName((NamedObj) actor)
                 + "_OUTPUT_DRIVER_WCET";
         code.append("#ifdef THREAD_" + threadID + _eol + "#ifndef "
@@ -314,15 +324,13 @@ public class GiottoDirector extends
                 + _eol
                 // #warning is non-standard, but useful.
                 + "#warning \"" + driverBound
-                + " was not defined.  Using default value of 1 ms.\"" + _eol
-                + "#define " + driverBound + " MILLISEC" + _eol + "#endif" + _eol);
+                + " was not defined.  Using default value of 1000.\"" + _eol
+                + "#define " + driverBound + " 1000" + _eol + "#endif" + _eol);
         // for
         String index = CodeGeneratorHelper.generateName((NamedObj) actor)
                 + "_frequency";
-        code.append("int " + index + ";" + _eol
-                + "for (" + index + " = 0; " + index + " < "
-                + _getFrequency(actor) + "; ++" + index + ") {" + _eol);
-        code.append("DEADBRANCH0(" + milliseconds + "*MILLISEC - " + driverBound
+       
+                code.append("DEADBRANCH0(" + cycles + "-" + driverBound
                 + "); // period - driver_wcet" + _eol + _getActorName(actor)
                 + "_driver_in();//read inputs from ports deterministically"
                 + _eol);
@@ -335,8 +343,8 @@ public class GiottoDirector extends
         code.append(_getActorName(actor)
                 + "_driver_out(); // output values to ports deterministically"
                 + _eol);
-        code.append("}" + _eol); // end of for loop
-
+        //code.append("}" + _eol); // end of for loop
+code.append(_eol);
         code.append("#endif /* THREAD_" + threadID + "*/\n");
         threadID++;
         if (actor instanceof CompositeActor) {
