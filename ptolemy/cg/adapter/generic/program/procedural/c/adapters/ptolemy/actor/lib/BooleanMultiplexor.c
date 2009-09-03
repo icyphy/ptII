825235e7d3b55942bbdf77de1fe9b457ef1e2dcf
jiazou <jiazou>
2009-09-03 21:25:53 +0000

    removed $hasToken for booleanMultiplexer, added a comment explaining the expected behavior for this actor.

--- ptolemy/cg/adapter/generic/program/procedural/c/adapters/ptolemy/actor/lib/BooleanMultiplexor.c
+++ ptolemy/cg/adapter/generic/program/procedural/c/adapters/ptolemy/actor/lib/BooleanMultiplexor.c
@@ -1,8 +1,8 @@
 /***fireBlock***/
-if ($hasToken(trueInput) && $hasToken(falseInput) && $hasToken(select)){
-	if($get(select))
-		$put(output, ($cgType(output)) $get(trueInput));	//output true token
-	else
-		$put(output, ($cgType(output)) $get(falseInput));	//output false token
-}
+// This fire method assumes there is an input tokens at each one of the three
+// input ports when this method is envoked. 
+if($get(select))
+	$put(output, ($cgType(output)) $get(trueInput));	//output true token
+else
+	$put(output, ($cgType(output)) $get(falseInput));	//output false token
 /**/
