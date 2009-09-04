6bf4276137f1de8d5e5f9acdc59479f1e9207050
Jeff C. Jensen <Jeff C. Jensen>
2009-09-04 22:17:13 +0000

    Added BooleanSwitch Ptides adapter

---
+++ ptolemy/cg/adapter/generic/program/procedural/c/adapters/ptolemy/actor/lib/BooleanSwitch.c
@@ -0,0 +1,6 @@
+/***fireBlock***/
+if($get(control))
+	$put(trueOutput, ($cgType(trueOutput)) $get(input));	//output true token
+else
+	$put(falseOutput, ($cgType(falseOutput)) $get(input));	//output false token
+/**/
