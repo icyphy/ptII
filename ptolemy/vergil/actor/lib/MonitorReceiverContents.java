4906b071025b981499d5483d559cbce3c40ca606
rodiers <rodiers>
2009-09-17 06:28:16 +0000

    Specified that the change request is a structural change, otherwise the buffers in the GUI won't update.

--- ptolemy/vergil/actor/lib/MonitorReceiverContents.java
+++ ptolemy/vergil/actor/lib/MonitorReceiverContents.java
@@ -167,7 +167,8 @@ public class MonitorReceiverContents extends SingletonAttribute {
                     // Request repaint on postfire() and wrapup().
                     public boolean postfire() {
                         ChangeRequest request = new ChangeRequest(this,
-                                "SetVariable change request", false /*Not a structural change, we only want to refresh the GUI*/
+                                "SetVariable change request", true /*Although this not a structural change in my point of view
+                                , we however for some reason need to specify it is, otherwise the GUI won't update.*/
                         ) {
                             protected void _execute()
                                     throws IllegalActionException {
