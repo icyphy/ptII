3c9c35a552402169d25af3e942bd1f1312daf4a5
cxh <cxh>
2009-10-22 17:19:52 +0000

    If compilation of a module fails, throw an error - we need this for testing, though eventually this could be optional

--- ptolemy/cg/lib/ModularCodeGenTypedCompositeActor.java
+++ ptolemy/cg/lib/ModularCodeGenTypedCompositeActor.java
@@ -78,84 +78,7 @@ import ptolemy.kernel.util.Workspace;
 ////ModularCodeGenTypedCompositeActor
 
 /**
-An aggregation of typed actors with lazy evaluation. The contents of
-this actor can be created in the usual way via visual editor by dragging
-in other actors and ports and connecting them. When it exports a MoML
-description of itself, it describes its ports and parameters in the
-usual way, but contained actors, relations, and their interconnections
-are exported within &lt;configure&gt; &lt;/configure&gt; tags.
-When reloading the MoML description, evaluation of the MoML
-within the configure tags is deferred until there is an explicit
-request for the contents. This behavior is useful for large
-complicated models where the time it takes to instantiate the
-entire model is large. It permits opening and browsing the model
-without a long wait. However, the cost comes typically when
-running the model. The instantiation time will be added to
-the time it takes to preinitialize the model.
-<p>
-The lazy contents of this composite are specified via the configure()
-method, which is called by the MoML parser and passed MoML code.
-The MoML is evaluated lazily; i.e. it is not actually evaluated
-until there is a request for its contents, via a call to
-getEntity(), numEntities(), entityList(), relationList(),
-or any related method. You can also force evaluation
-of the MoML by calling populate(). Accessing the attributes
-or ports of this composite does not trigger a populate() call,
-so a visual editor can interact with the actor from the outside
-in the usual way, enabling connections to its ports, editing
-of its parameters, and rendering of its custom icon, if any.
-<p>
-The configure method can be passed a URL or MoML text or both.
-If it is given MoML text, that text will normally be wrapped in a
-processing instruction, as follows:
-<pre>
-&lt;?moml
-<group>
-... <i>MoML elements giving library contents</i> ...
-</group>
-?&gt;
-</pre>
-The processing instruction, which is enclosed in "&lt;?" and "?&gt"
-prevents premature evaluation of the MoML.  The processing instruction
-has a <i>target</i>, "moml", which specifies that it contains MoML code.
-The keyword "moml" in the processing instruction must
-be exactly as above, or the entire processing instruction will
-be ignored.  The populate() method
-strips off the processing instruction and evaluates the MoML elements.
-The group element allows the library contents to be given as a set
-of elements (the MoML parser requires that there always be a single
-top-level element, which in this case will be the group element).
-<p>
-One subtlety in using this class arises because of a problem typical
-of lazy evaluation.  A number of exceptions may be thrown because of
-errors in the MoML code when the MoML code is evaluated.  However,
-since that code is evaluated lazily, it is evaluated in a context
-where these exceptions are not expected.  There is no completely
-clean solution to this problem; our solution is to translate all
-exceptions to runtime exceptions in the populate() method.
-This method, therefore, violates the condition for using runtime
-exceptions in that the condition that causes these exceptions to
-be thrown is not a testable precondition.
-<p>
-A second subtlety involves cloning.  When this class is cloned,
-if the configure MoML text has not yet been evaluated, then the clone
-is created with the same (unevaluated) MoML text, rather than being
-populated with the contents specified by that text.  If the object
-is cloned after being populated, the clone will also be populated.
-Cloning is used in actor-oriented classes to create subclasses
-or instances of a class.  When a LazyTypedCompositeActor contained
-by a subclass or an instance is populated, it delegates to the
-instance in the class definition. When that instance is populated,
-all of the derived instances in subclasses and instances of the
-class will also be populated as a side effect.
-<p>
-A third subtlety is that parameters of this actor cannot refer to
-contained entities or relations, nor to attributes contained by
-those. This is a rather esoteric use of expressions, so
-this limitation may not be onerous. You probably didn't know
-you could do that anyway.  An attempt to make such references
-will simply result in the expression failing to evaluate.
-
+A TypedCompositeActor with Lazy evaluation for Modular code generation. 
 
  @author Bert Rodiers, Dai Bui
  @version $Id$
@@ -205,12 +128,16 @@ public class ModularCodeGenTypedCompositeActor extends LazyTypedCompositeActor {
     ///////////////////////////////////////////////////////////////////
     ////                         parameters                        ////
 
-    /** A boolean parameter to enforce recompilation of this ModularCodeGenTypedCompositeActor 
-     *  and all contained ModularCodeGenTypedCompositeActors.
+    /** A boolean parameter to force recompilation of this
+     *  ModularCodeGenTypedCompositeActor and all contained
+     *  ModularCodeGenTypedCompositeActors.  The default value is
+     *  false, which means that the hierarchy will not be recompiled.
      */
     public Parameter recompileHierarchy;
 
-    /** A boolean parameter to enforce recompilation of this ModularCodeGenTypedCompositeActor. 
+    /** A boolean parameter to enforce recompilation of this
+     *  ModularCodeGenTypedCompositeActor.  The default value is false,
+     *  which means that this level will not be recompiled.
      */
     public Parameter recompileThisLevel;
 
@@ -511,7 +438,7 @@ public class ModularCodeGenTypedCompositeActor extends LazyTypedCompositeActor {
             _generatingCode = true;
             _createCodeGenerator();
             if (_modelChanged()) {
-                    _generateCode();
+                _generateCode();
             }
             String className = NamedProgramCodeGeneratorAdapter.generateName(this);        
             Class<?> classInstance = null;
@@ -564,9 +491,9 @@ public class ModularCodeGenTypedCompositeActor extends LazyTypedCompositeActor {
             recompileThisLevel.setToken(new BooleanToken(false));
             recompileHierarchy.setToken(new BooleanToken(false));
         } catch (Throwable throwable) {
-            System.err.println(throwable);
             _objectWrapper = null;
             _fireMethod = null;
+            throw new IllegalActionException(this, throwable, "Failed to initialize.");
         } finally {
             _generatingCode = false;
         }
@@ -1074,7 +1001,13 @@ public class ModularCodeGenTypedCompositeActor extends LazyTypedCompositeActor {
     private void _generateCode() throws KernelException {
         _createCodeGenerator();
         _codeGenerator.createProfile();
-        _codeGenerator.generateCode();
+        int returnValue = 0; 
+        if ((returnValue = _codeGenerator.generateCode()) != 0) {
+            new Exception("Failed to generate code").printStackTrace();
+            throw new KernelException(this, null, "Failed to generate code, "
+                    + "the return value of the last subprocess was "
+                    + returnValue);
+        }
     }
 
     private Profile _getProfile() {
