36f66d801560f0fc8a17254b0cb6f410deb2387c
acataldo <acataldo>
2009-10-28 03:00:08 +0000

    This code is not in great shape. It's still in prototype-mode. It needs to be documented, and should be considered experimental. Nonetheless, PtalonModel is actually quite an improvement from the PtalonActor it seeks to replace. That said, there are still a few language features missing in PtalonModel that will need to be addressed before it's ready for prime time.

---
+++ ptolemy/actor/ptalon/model/Controls/PtalonEditorFactory.java
@@ -0,0 +1,26 @@
+package ptolemy.actor.ptalon.model.Controls;
+
+import java.awt.Frame;
+
+import ptolemy.actor.gui.EditorFactory;
+import ptolemy.actor.ptalon.model.PtalonModel;
+import ptolemy.kernel.util.IllegalActionException;
+import ptolemy.kernel.util.NameDuplicationException;
+import ptolemy.kernel.util.NamedObj;
+
+public class PtalonEditorFactory extends EditorFactory {
+	public PtalonEditorFactory(NamedObj container, String name,
+			PtalonModel model) throws IllegalActionException,
+			NameDuplicationException {
+		super(container, name);
+		_model = model;
+	}
+
+	private PtalonModel _model;
+
+	@Override
+	public void createEditor(NamedObj object, Frame parent) {
+		new PtalonDialog(parent, "Configure Ptalon Model", _model);
+	}
+
+}
