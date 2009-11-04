8f0238e3d478005a6ed0374f449a88c6e4c412e6
cxh <cxh>
2009-11-04 22:17:37 +0000

    Undo last change, ran test on wrong machine.

--- adm/test/Release.tcl
+++ adm/test/Release.tcl
@@ -59,7 +59,6 @@ test release-1.1 {Check for missing makefiles} {
 ./ptolemy/backtrack/eclipse/plugin/widgets/makefile
 ./ptolemy/backtrack/ui/makefile
 ./ptolemy/backtrack/util/java/util/makefile
-./ptolemy/copernicus/shallow/cg/Butterfly/Butterfly/makefile
 ./ptolemy/plot/servlet/makefile}
 
 test release-2.1 {Check for directories that have java files, but are not in doc/makefile} {
