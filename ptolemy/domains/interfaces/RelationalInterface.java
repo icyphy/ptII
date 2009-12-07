3991652eb3c8f03a336b4d45633d7974f9d6698c
blickly <blickly>
2009-12-07 21:39:26 +0000

    Added some connection types to RelationalInterface

--- ptolemy/domains/interfaces/RelationalInterface.java
+++ ptolemy/domains/interfaces/RelationalInterface.java
@@ -6,6 +6,8 @@ package ptolemy.domains.interfaces;
 import java.util.HashSet;
 import java.util.Set;
 
+import ptolemy.kernel.Relation;
+
 /** Representation of an interface that relates its inputs and outputs,
  *  in the style of the EMSOFT 2009 paper "On Relational Interfaces."
  *  
@@ -22,6 +24,30 @@ public class RelationalInterface {
         _inputPorts = inputs;
         _outputPorts = outputs;
         _contract = contract;
+    }
+
+    public RelationalInterface cascadeComposeWith(RelationalInterface rhs,
+            Set<Relation> connections) {
+        Set<String> newInputs = new HashSet<String>();
+        newInputs.addAll(_inputPorts);
+        newInputs.addAll(rhs._inputPorts);
+        // FIXME: Remove connected ports
+        //newInputs.remove(...);
+        Set<String> newOutputs = new HashSet<String>();
+        newOutputs.addAll(_outputPorts);
+        newOutputs.addAll(rhs._outputPorts);
+        // FIXME: Add connected ports
+        //newOutputs.add(...);
+        String newContract = "(and " + _contract + " " + rhs._contract + ")";
+        // FIXME: Fix up contract
+        // newContract = "(and " + newContract + " " + _connected +")";
+        // Y = "("
+        // for (port : connections U outputs) {
+        //  Y += port.name + "::int "
+        // }
+        // Y += ")"
+        // newContract = "(and " + newContract + " (forall " + _Y + " " + _PHI + ")"; 
+        return new RelationalInterface(newInputs, newOutputs, newContract);
     }
 
     public String getContract() {
@@ -41,6 +67,17 @@ public class RelationalInterface {
         yicesInput.append("(assert " + _contract +")\n");
 
         return yicesInput.toString();
+    }
+    
+    public RelationalInterface parallelComposeWith(RelationalInterface rhs) {
+        Set<String> newInputs = new HashSet<String>();
+        newInputs.addAll(_inputPorts);
+        newInputs.addAll(rhs._inputPorts);
+        Set<String> newOutputs = new HashSet<String>();
+        newOutputs.addAll(_outputPorts);
+        newOutputs.addAll(rhs._outputPorts);
+        String newContract = "(and " + _contract + " " + rhs._contract + ")";
+        return new RelationalInterface(newInputs, newOutputs, newContract);
     }
 
     private Set<String> _inputPorts = new HashSet<String>();
