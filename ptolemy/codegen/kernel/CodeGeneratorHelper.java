954b1df076309316b82550c6177ddc59f6624484
cxh <cxh>
2009-12-13 20:17:06 +0000

    javadoc

--- ptolemy/codegen/kernel/CodeGeneratorHelper.java
+++ ptolemy/codegen/kernel/CodeGeneratorHelper.java
@@ -1740,9 +1740,7 @@ public class CodeGeneratorHelper extends NamedObj implements ActorCodeGenerator 
      *  @param channelAndOffset The given channel and offset.
      *  @param isWrite True if the port is to be written to.
      *  @return The reference to the port.
-     *  @exception IllegalActionException If the port does not
-     *   exist or does not have a value.
-     *  @see #generateReference(IOPort, String[], boolean, boolean)
+     *  @see #getReference(IOPort, String[], boolean, boolean)
      */  
     public static String generatePortReference(IOPort port,
             String[] channelAndOffset, boolean isWrite) {
