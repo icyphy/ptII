b0259574a89afacb6783892f278fac0911911460
blickly <blickly>
2010-02-06 02:07:32 +0000

    Removed unused enums for constraint types.
    (that corresponded to invalid constraints of < and >)

--- ptolemy/data/ontologies/lattice/LatticeOntologySolver.java
+++ ptolemy/data/ontologies/lattice/LatticeOntologySolver.java
@@ -799,17 +799,11 @@ public class LatticeOntologySolver extends OntologySolver {
         /** Represents that the sink must == the least upper bound of all sources. */
         SINK_EQUALS_MEET,
 
-        /** Represents that the sink must be > the source. */
-        SINK_GREATER,
-
         /** Represents that the source must be >= the sink. */
         SRC_EQUALS_GREATER,
 
         /** Represents that the source must == the least upper bound of all sinks. */
-        SRC_EQUALS_MEET,
-
-        /** Represents that the source must be > the sink. */
-        SRC_GREATER
+        SRC_EQUALS_MEET
     }
 
     ///////////////////////////////////////////////////////////////////
