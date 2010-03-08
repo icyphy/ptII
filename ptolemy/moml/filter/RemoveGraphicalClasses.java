afd4f5ca4e58f36485106a119c9e8ad4309ff7f2
bldmastr <bldmastr>
2010-03-08 15:23:00 +0000

    Added FmvStateIcon to the list of classes to be removed.

--- ptolemy/moml/filter/RemoveGraphicalClasses.java
+++ ptolemy/moml/filter/RemoveGraphicalClasses.java
@@ -291,6 +291,8 @@ public class RemoveGraphicalClasses implements MoMLFilter {
         // 11/06 FSM uses StateIcon
         _graphicalClasses.put("ptolemy.vergil.fsm.StateIcon", null);
 
+        _graphicalClasses.put("ptolemy.vergil.fsm.fmv.FmvStateIcon", null);
+
         _graphicalClasses.put("ptolemy.vergil.modal.StateIcon", null);
 
         _graphicalClasses.put(
