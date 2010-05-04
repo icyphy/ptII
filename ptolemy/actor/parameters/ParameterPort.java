34fff3f40f694fba366758aa221b252c6d85fcbd
cxh <cxh>
2010-05-04 01:04:29 +0000

    Mark the notDraggable parameter as non-persistent so that it does not get written out.

--- ptolemy/actor/parameters/ParameterPort.java
+++ ptolemy/actor/parameters/ParameterPort.java
@@ -81,7 +81,8 @@ public class ParameterPort extends TypedIOPort {
         tokenConsumptionRate.setVisibility(Settable.NOT_EDITABLE);
         tokenConsumptionRate.setPersistent(false);
 
-        new Parameter(this,"_notDraggable");
+        Parameter notDraggable = new Parameter(this,"_notDraggable");
+        notDraggable.setPersistent(false);
     }
 
     ///////////////////////////////////////////////////////////////////
