c594ce813674e9a413e50ce1c6df855a183d0630
blickly <blickly>
2010-08-31 16:59:05 +0000

    Commented MonotonicityCounterexamples

--- ptolemy/data/ontologies/lattice/adapters/monotonicityAnalysis/MonotonicityCounterexamples.java
+++ ptolemy/data/ontologies/lattice/adapters/monotonicityAnalysis/MonotonicityCounterexamples.java
@@ -1,4 +1,4 @@
-/* 
+/* Representation of a set of counterexamples to monotonicity.
  * 
  * Copyright (c) 2010 The Regents of the University of California. All
  * rights reserved.
@@ -31,8 +31,11 @@ import ptolemy.data.ontologies.Concept;
 import ptolemy.data.ontologies.util.MultiHashMap;
 import ptolemy.data.ontologies.util.MultiMap;
 
-/**
- *  
+/** Representation of a set of counterexamples to monotonicity.
+ * 
+ *  Each counterexample is of the form of a pair of elements (x1,x2)
+ *  where x1 <= x2, but f(x1) \not <= f(x2), for the non-monotonic function
+ *  f under consideration.
  *
  *  @author Ben Lickly
  *  @version $Id$
@@ -42,20 +45,43 @@ import ptolemy.data.ontologies.util.MultiMap;
  */
 public class MonotonicityCounterexamples {
     
+    /** Construct an empty set of counterexamples.
+     *  We can't really consider this a set of counterexamples
+     *  until we add some counterexamples.
+     */
     public MonotonicityCounterexamples() {
         _counterexamples = new MultiHashMap();
     }
     
+    /** Add a pair of concepts as a counterexample to this set.
+     *  This means that x1 <= x2, but for the (non-monotonic) function in
+     *  question, f(x1) \not <= f(x2).
+     *  
+     *  @param x1 The lesser concept.
+     *  @param x2 The greater concept.
+     */
     public void add(Concept x1, Concept x2) {
         _counterexamples.remove(x1, x2);
         _counterexamples.put(x1, x2);
     }
     
+    /** Determine if this set contains any counterexamples.
+     *
+     *  @return True, if this set contains at least one counterexample.
+     *    False, otherwise.
+     */
     public boolean containsCounterexamples() {
         return !_counterexamples.isEmpty();
     }
     
 
+    /** Return a set of all the counterexamples.
+     *  For each pair in the set returned, (x1, x2),
+     *  we have x1 <= x2, but on the function under consideration
+     *  f(x1) \not <= f(x2)
+     *  
+     *  @return A set of pairs of counterexamples.
+     */
     public Set<ConceptPair> entrySet() {
         Set<ConceptPair> entrySet = new HashSet<ConceptPair>();
         for (Concept lesser : (Set<Concept>)_counterexamples.keySet()) {
@@ -66,6 +92,10 @@ public class MonotonicityCounterexamples {
         return entrySet;
     }
     
+    /** Return the string representation of the counterexample set.
+     *  @return The string representation of this set.
+     *  @see java.lang.Object#toString()
+     */
     public String toString() {
         String result = "{";
         for (Entry<Concept, Concept> pair : entrySet()) {
@@ -74,38 +104,63 @@ public class MonotonicityCounterexamples {
         return result  + "}";
     }
     
+    /** A multimap to keep track of the mapping of lesser concepts
+     *  to greater concepts.  This must be a multimap, because there
+     *  could be multiple counterexamples with the same lesser concept.
+     *
+     *  If our MultiMap supported Java Generics, this would have a type of
+     *    MultiMap<Concept, Concept>
+     */
     private MultiMap _counterexamples;
     
-    /** Encapsulate counterexample pairs. */
+    /** Encapsulate counterexample pairs.
+     *  These are pairs of the form (x1, x2) where x1 <= x2, but
+     *  f(x1) \not <= f(x2) for the function under consideration.
+     *  Thus, these pairs prove as counterexamples to the
+     *  monotonicity of f.
+     */
     public class ConceptPair implements Map.Entry<Concept,Concept> {
+        /** Create a counterexample pair given both of the concepts that  make
+         *  up the counterexample.
+         *
+         *  @param l The lesser concept, i.e. x1
+         *  @param g The greater concept, i.e. x2
+         */
         public ConceptPair(Concept l, Concept g) {
             lesser = l; greater = g;
         }
+        /** The lesser of the concepts (i.e. x1)
+         */
         public Concept lesser;
+        /** The greater of the concepts (i.e. x2)
+         */
         public Concept greater;
         
         
-        /**
+        /** Return the lesser concept of the counterexample.
+         * 
          *  @return The lesser concept.
          *  @see java.util.Map.Entry#getKey()
          */
         public Concept getKey() {
             return lesser;
         }
-        /**
-         *  @return The greater concent
+        /** Return the greater concept of the counterexample.
+         *  
+         *  @return The greater concept
          *  @see java.util.Map.Entry#getValue()
          */
         public Concept getValue() {
             return greater;
         }
-        /**
-         *  @param value
+        /** Do nothing.
+         *  (Counterexamples are immutable)
+         *
+         *  @param value Ignored
          *  @return Always return null.
          *  @see java.util.Map.Entry#setValue(java.lang.Object)
          */
         public Concept setValue(Concept value) {
-            // TODO Auto-generated method stub
             return null;
         }  
     }
