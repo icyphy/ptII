d7723a5b65211b06d8dea23f93e53152f8a6edfb
bldmastr <bldmastr>
2010-09-19 15:19:40 +0000

    javadoc

--- ptolemy/domains/giotto/kernel/GiottoTimingManager.java
+++ ptolemy/domains/giotto/kernel/GiottoTimingManager.java
@@ -100,7 +100,8 @@ import ptolemy.kernel.util.SingletonAttribute;
  * and does not execute the model. Second, during execution the attribute randomly varies 
  * the execution time of the actors. The attribute then compares (b) the sum of the
  * actors execution times to (a) the sum of the actors worst case execution 
- * times. If (a) > (b) then the attribute calls {@link #handleModelError(NamedObj)},
+ * times. If (a) > (b) then the attribute calls
+ * {@link ptolemy.kernel.util.NamedObj#handleModelError(NamedObj, IllegalActionException)},
  *  which throws a model error .
  *  
  * <p> A model error is an exception that is passed up the containment
