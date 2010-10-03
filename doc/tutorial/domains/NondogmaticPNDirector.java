cd2449b5db2cc069f681c8a48246ea26cdb7feeb
cxh <cxh>
2010-10-03 18:57:32 +0000

    Renamed director to domains

--- doc/tutorial/domains/NondogmaticPNDirector.java
+++ doc/tutorial/domains/NondogmaticPNDirector.java
@@ -24,7 +24,7 @@ PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
 CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
 ENHANCEMENTS, OR MODIFICATIONS.
 */
-package doc.tutorial.director;
+package doc.tutorial.domains;
 
 import ptolemy.actor.IOPort;
 import ptolemy.actor.Receiver;
