fa0fe9718a2174b6acf123526f63a6d8b0a45c35
cshelton <cshelton>
2010-10-27 17:47:33 +0000

    Added ontology product lattice demo models in the ptolemy/ontologies/demo/ProductLattices directory.

--- ptolemy/data/ontologies/Ontology.java
+++ ptolemy/data/ontologies/Ontology.java
@@ -48,7 +48,7 @@ import ptolemy.kernel.util.Workspace;
  * between concepts contained by this ontology.
  * 
  * @see ConceptGraph
- * @see FiniteConcept
+ * @see Concept
  * @author Edward A. Lee, Ben Lickly, Dai Bui, Christopher Brooks
  * @version $Id$
  * @since Ptolemy II 8.0
