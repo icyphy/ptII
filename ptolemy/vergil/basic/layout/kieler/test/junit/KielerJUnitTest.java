86d2937fdc47209345142a464d5f92a6cd00b9ce
cxh <cxh>
2011-03-02 16:28:30 +0000

    Added Diff class, based on http://introcs.cs.princeton.edu/96optimization/Diff.java.  Use Diff class with Kieler JUnit test to display differences in MoML.

--- ptolemy/vergil/basic/layout/kieler/test/junit/KielerJUnitTest.java
+++ ptolemy/vergil/basic/layout/kieler/test/junit/KielerJUnitTest.java
@@ -29,6 +29,21 @@
 package ptolemy.vergil.basic.layout.kieler.test.junit;
 
 import static org.junit.Assert.assertArrayEquals;
+import java.io.File;
+import java.util.Iterator;
+import java.util.List;
+import java.util.LinkedList;
+
+import ptolemy.actor.gui.ConfigurationApplication;
+import ptolemy.moml.filter.RemoveGraphicalClasses;
+import ptolemy.moml.MoMLParser;
+import ptolemy.actor.TypedCompositeActor;
+import ptolemy.kernel.util.NamedObj;
+import ptolemy.util.FileUtilities;
+import ptolemy.util.test.Diff;
+import ptolemy.vergil.basic.layout.KielerLayoutAction;
+
+
 
 ///////////////////////////////////////////////////////////////////
 //// KielerJUnitTest
@@ -43,12 +58,86 @@ import static org.junit.Assert.assertArrayEquals;
  * @Pt.AcceptedRating Red (cxh)
  */ 
 public class KielerJUnitTest {
+    /**
+     * Test the layout facility by reading in a model, stripping
+     * out the graphical elements, laying out the model, comparing
+     * the new results with the known good results and then doing
+     * undo and redo.
+     * @param modelFileName The file name of the test model. 
+     * @exception Exception If the file name cannot be read or laid out.
+     */
+    public void layoutTest(String modelFileName) throws Exception {
+        NamedObj model = null;
+        try {
+            // Read in the model, using moml.filter.RemoveGraphicalClasses.
+            List filters = new LinkedList();
+            filters.add(new RemoveGraphicalClasses());
+            MoMLParser.setMoMLFilters(filters);
+
+            ConfigurationApplication application = 
+                new ConfigurationApplication( new String[] {
+                            // Need to display a frame or Kieler fails.
+                            //"ptolemy/actor/gui/test/testConfiguration.xml",
+                            "ptolemy/configs/full/configuration.xml", 
+                            modelFileName});
+            
+            // Find the first TypedCompositeActor, skipping the
+            // Configuration etc.
+            StringBuffer names = new StringBuffer();
+            Iterator models = application.models().iterator();
+            while (models.hasNext()) {
+                model = (NamedObj) models.next();
+                if (names.length() > 0) {
+                    names.append(", ");
+                }
+                names.append(model.getFullName());
+                if (model instanceof TypedCompositeActor) {
+                    break;
+                }
+            }
+            if ( !(model instanceof TypedCompositeActor)) {
+                throw new Exception("Failed to find a TypedComposite.  Models were: " 
+                        + names);
+            }
+
+            // Invoke the Kieler layout mechanism.
+            new KielerLayoutAction().doAction(model);
+            
+            // Export the model and compare it with the original.
+            String laidOutMoML = model.exportMoML();
+            byte [] originalMoMLBytes = FileUtilities.binaryReadURLToByteArray(new File(modelFileName).toURI().toURL());
+
+            System.out.println("Difference between " + modelFileName
+                    + " and the exported MoML after Kieler Layout:");
+            System.out.println(Diff.diff(new String(originalMoMLBytes),
+                            laidOutMoML));
+
+            //System.out.println(new String(laidOutMoML));
+            //System.out.println(new String(originalMoML));
+
+            assertArrayEquals(laidOutMoML.getBytes(), originalMoMLBytes);
+
+            // Invoke the crufty Ptolemy layout mechanism and export.
+            // Invoke the Kieler layout mechanism and export.
+            // Invoke undo and compare against the Ptolemy layout.
+            // Invoke redo and compare against the Kieler layout.
+
+            // Clean up temporary files.
+        } finally {
+            if (model != null) {
+                if (model instanceof TypedCompositeActor) {
+                    ((TypedCompositeActor)model).setContainer(null);
+                }
+            }
+        }        
+    }
+
     /** 
      *  
      */ 
     @org.junit.Test 
     public void run() throws Exception {
-        //assertArrayEquals(example.toString().getBytes(), knownGood.getBytes());
+        layoutTest("ConstDisplay.xml");
     }
 
     public static void main(String args[]) {
