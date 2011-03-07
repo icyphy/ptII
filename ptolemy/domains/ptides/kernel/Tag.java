39e1f35a920b3ff5b6d854fa010b47c03bd4f4e9
bldmastr <bldmastr>
2011-03-07 21:00:43 +0000

    Cleanup, added FIXME about compareTo() and equals().

--- ptolemy/domains/ptides/kernel/Tag.java
+++ ptolemy/domains/ptides/kernel/Tag.java
@@ -31,11 +31,11 @@ package ptolemy.domains.ptides.kernel;
 import ptolemy.actor.util.Time;
 
 /**
- *  Tag is a pair of timestamp and microstep.
+ *  A timestamp and a microstamp that represent a Tag.
  *
  *  @author Jia Zou
  *  @version $Id$
- *  @since Ptolemy II 8.0
+ *  @since Ptolemy II 8.1
  *  @Pt.ProposedRating Yellow (jiazou)
  *  @Pt.AcceptedRating Red (jiazou)
  *
@@ -56,8 +56,20 @@ public class Tag implements Comparable {
     public Tag() {
     }
 
+    ///////////////////////////////////////////////////////////////////
+    ////                         public variables                  ////
+
+    /** The timestamp. */
+    public Time timestamp;
+
+    /** The microstep. */
+    public int microstep;
+
+    ///////////////////////////////////////////////////////////////////
+    ////                         public methods                    ////
+
     /** Compare a tag with another.
-     *  @param other The object comparing to.
+     *  @param other The object to be compared.
      *  @return The result of the comparison, which is a lexicographical order
      *  with the timestamp as the first element and microstep as the second.
      */
@@ -78,14 +90,33 @@ public class Tag implements Comparable {
         }
     }
     
+    // 	FIXME: FindBugs: "Eq: Class defines compareTo(...) and
+    // 	uses Object.equals() (EQ_COMPARETO_USE_OBJECT_EQUALS)"
+
+    // 	"This class defines a compareTo(...) method but inherits its
+    // 	equals() method from java.lang.Object. Generally, the value of
+    // 	compareTo should return zero if and only if equals returns
+    // 	true. If this is violated, weird and unpredictable failures
+    // 	will occur in classes such as PriorityQueue. In Java 5 the
+    // 	PriorityQueue.remove method uses the compareTo method, while
+    // 	in Java 6 it uses the equals method."
+    
+    // 	"From the JavaDoc for the compareTo method in the Comparable interface:"
+
+    // 	    "It is strongly recommended, but not strictly
+    // 	    required that (x.compareTo(y)==0) ==
+    // 	    (x.equals(y)). Generally speaking, any class that
+    // 	    implements the Comparable interface and violates
+    // 	    this condition should clearly indicate this
+    // 	    fact. The recommended language is "Note: this
+    // 	    class has a natural ordering that is inconsistent
+    // 	    with equals.""
+
+    /** Return a string representation of this class.
+     *  @return a string representation of this class.
+     */ 
     public String toString() {
         return "timestamp = " + timestamp.getDoubleValue() +
             ", microstep = " + microstep;
     }
-
-    /** The timestamp. */
-    public Time timestamp;
-
-    /** The microstep. */
-    public int microstep;
 }
