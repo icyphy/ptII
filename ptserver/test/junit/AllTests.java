fd6c8a97e7903131699a4031a839f4078312ffa1
jkillian <jkillian>
2011-06-01 04:31:36 +0000

    Included wrong test class reference - changed to ServerManagerTest

--- ptserver/test/junit/AllTests.java
+++ ptserver/test/junit/AllTests.java
@@ -3,11 +3,9 @@ package ptserver.test.junit;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
-import com.sun.corba.se.spi.activation.ServerManager;
-
 @RunWith(Suite.class)
 @Suite.SuiteClasses({ TokenParserTest.class, ServletTest.class,
-        ServerManager.class })
+        ServerManagerTest.class })
 public class AllTests {
 
 }
