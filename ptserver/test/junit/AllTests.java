2b1c1d0497f715dbc155aa83aa8ef4b0635b79e7
jkillian <jkillian>
2011-06-07 01:23:02 +0000

    Added test for launching the server and client and receiving a set of tokens. Added RemoteModelTest to the test suite.

--- ptserver/test/junit/AllTests.java
+++ ptserver/test/junit/AllTests.java
@@ -4,7 +4,8 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({ TokenParserTest.class, ServletTest.class })
+@Suite.SuiteClasses({ TokenParserTest.class, ServletTest.class,
+        RemoteModelTest.class })
 public class AllTests {
 
 }
