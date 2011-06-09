6f914d381d77684b46a0d584c78dc92e77aa0546
ahuseyno <ahuseyno>
2011-06-09 19:59:04 +0000

    Added PortablePlaceable and corresponding PortableContainer instance along with AWTContainer implementation.
    Updated svn properties of the licenses and build file to include guice jars.

---
+++ ptolemy/actor/gui/AWTContainer.java
@@ -0,0 +1,81 @@
+/*
+ The portable container that wraps java.awt.Container.
+ 
+ Copyright (c) 2011 The Regents of the University of California.
+ All rights reserved.
+ Permission is hereby granted, without written agreement and without
+ license or royalty fees, to use, copy, modify, and distribute this
+ software and its documentation for any purpose, provided that the above
+ copyright notice and the following two paragraphs appear in all copies
+ of this software.
+
+ IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+ FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+ ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+ THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+ SUCH DAMAGE.
+
+ THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+ INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+ PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+ CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ ENHANCEMENTS, OR MODIFICATIONS.
+
+ PT_COPYRIGHT_VERSION_2
+ COPYRIGHTENDKEY
+ */
+
+package ptolemy.actor.gui;
+
+import java.awt.Component;
+import java.awt.Container;
+
+///////////////////////////////////////////////////////////////////
+//// AWTContainer
+
+/**
+ * The portable container that wraps java.awt.Container.
+ * @author Anar Huseynov
+ * @version $Id$ 
+ * @since Ptolemy II 8.0
+ * @Pt.ProposedRating Red (ahuseyno)
+ * @Pt.AcceptedRating Red (ahuseyno)
+ */
+public class AWTContainer implements PortableContainer<Container, Component> {
+
+    /**
+     * Create a new instance of the object by wrapping the provided container.
+     * @param container The container to wrap.
+     */
+    public AWTContainer(Container container) {
+        _container = container;
+    }
+    
+    ///////////////////////////////////////////////////////////////////
+    ////                         public methods                    ////
+    /** 
+     * Add the component to the container.
+     * @param component the component to be added to the container.
+     * @see ptolemy.actor.gui.PortableContainer#add(java.lang.Object)
+     */
+    public void add(Component component) {
+        _container.add(component);
+    }
+
+    /**
+     * Return the AWT container that this instance wraps. 
+     * @see ptolemy.actor.gui.PortableContainer#getPlatformContainer()
+     */
+    public Container getPlatformContainer() {
+        return _container;
+    }
+
+    ///////////////////////////////////////////////////////////////////
+    ////                         private variables                 ////
+    /**
+     * The AWT container that this instance wraps.
+     */
+    private Container _container;
+
+}
---
+++ ptolemy/actor/gui/PortableContainer.java
@@ -0,0 +1,60 @@
+/*
+ Platform independent container for placing UI components into itself.
+ 
+ Copyright (c) 2011 The Regents of the University of California.
+ All rights reserved.
+ Permission is hereby granted, without written agreement and without
+ license or royalty fees, to use, copy, modify, and distribute this
+ software and its documentation for any purpose, provided that the above
+ copyright notice and the following two paragraphs appear in all copies
+ of this software.
+
+ IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+ FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+ ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+ THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+ SUCH DAMAGE.
+
+ THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+ INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+ PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+ CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ ENHANCEMENTS, OR MODIFICATIONS.
+
+ PT_COPYRIGHT_VERSION_2
+ COPYRIGHTENDKEY
+ */
+
+package ptolemy.actor.gui;
+
+///////////////////////////////////////////////////////////////////
+//// PortableContainer
+/**
+ * Platform independent container for placing UI components into itself.
+ * The implementor of this interface would wrap the platform dependent container.
+ * @param <C> The type of the platform dependent container.
+ * @param <T> The type of the components that the container contains.
+ * @author Anar Huseynov
+ * @version $Id$ 
+ * @since Ptolemy II 8.0
+ * @Pt.ProposedRating Red (ahuseyno)
+ * @Pt.AcceptedRating Red (ahuseyno)
+ */
+public interface PortableContainer<C, T> {
+
+    ///////////////////////////////////////////////////////////////////
+    ////                         public methods                    ////
+    /**
+     * Add the specified component into the container.
+     * @param component the component to be added.
+     */
+    public void add(T component);
+
+    /**
+     * Return the platform dependent container that this instance wraps.
+     * @return the platform dependent container 
+     */
+    public C getPlatformContainer();
+
+}
