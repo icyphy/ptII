919eaf68403d92a71475e7c37b7d4439023821d1
pdf <pdf>
2011-07-01 20:53:37 +0000

    Starting to refactor the structure to be used both in the UI designer tool and the Android client. Removed Android dependencies.

---
+++ ptolemy/uidesigner/kernel/PositionableElement.java
@@ -0,0 +1,120 @@
+/* Abstract definition of elements. 
+ 
+ Copyright (c) 2011 The Regents of the University of California.
+ All rights reserved.
+ Permission is hereby granted, without written agreement and without
+ license or royalty fees, to use, copy, modify, and distribute this
+ software and its documentation for any purpose, provided that the above
+ copyright notice and the following two paragraphs appear in all copies
+ of this software.
+
+ IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+ FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+ ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+ THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+ SUCH DAMAGE.
+
+ THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+ INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+ PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+ CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ ENHANCEMENTS, OR MODIFICATIONS.
+
+ PT_COPYRIGHT_VERSION_2
+ COPYRIGHTENDKEY
+ */
+
+package ptolemy.uidesigner.kernel;
+
+import ptolemy.actor.gui.PortableContainer;
+import ptolemy.data.expr.Parameter;
+import ptolemy.kernel.util.IllegalActionException;
+import ptolemy.kernel.util.NamedObj;
+import ptserver.test.helper.HomerLocation;
+
+///////////////////////////////////////////////////////////////////
+//// PositionableElement
+
+/** Abstract definition of elements. 
+ *
+ *  @author Peter Foldes
+ *  @version $Id$
+ *  @since Ptolemy II 8.1
+ *  @Pt.ProposedRating Red (pdf)
+ *  @Pt.AcceptedRating Red (pdf)
+ */
+public abstract class PositionableElement {
+
+    ///////////////////////////////////////////////////////////////////
+    ////                constructor                                ////
+
+    /** Store the element that should have its location defined.
+     * 
+     *  @param element The object with the location defined.
+     */
+    public PositionableElement(NamedObj element) {
+        _element = element;
+    }
+
+    ///////////////////////////////////////////////////////////////////
+    ////                public methods                             ////
+
+    /** Get the location of the element.
+     * 
+     *  @return Return the location object of the element.
+     *  @exception IllegalActionException If the location of the element
+     *  is not defined.
+     */
+    public HomerLocation getLocation() throws IllegalActionException {
+
+        HomerLocation location = (HomerLocation) getElement().getAttribute(
+                HomerConstants.POSITION_NODE);
+        // Check whether the location information is valid.
+        location.validateLocation();
+
+        return location;
+    }
+
+    /** Get the location of the element.
+     *  @return Return the location object of the element.
+     */
+    public String getTab() {
+        Parameter tag = ((Parameter) _element
+                .getAttribute(HomerConstants.TAB_NODE));
+
+        // If the tab is not set, return default
+        if (tag == null) {
+            return HomerConstants.TAG;
+        }
+
+        return tag.getExpression();
+    }
+
+    /** Add the visual representation of the element to the provided
+     *  container.
+     * 
+     *  @param container The container to place the representation in.
+     *  @exception IllegalActionException If the element cannot be added
+     *  to the container. 
+     */
+    public abstract void addToContainer(PortableContainer container) throws IllegalActionException;
+
+    ///////////////////////////////////////////////////////////////////
+    ////                protected methods                          ////
+
+    /** Get the underlying element.
+     * 
+     *  @return The Ptolemy element with Android specific location information.
+     */
+    protected NamedObj getElement() {
+        return _element;
+    }
+
+    ///////////////////////////////////////////////////////////////////
+    ////                private variables                          ////
+
+    /** The Ptolemy element.
+     */
+    private NamedObj _element;
+}
---
+++ ptolemy/uidesigner/kernel/TabDefinition.java
@@ -0,0 +1,117 @@
+/* Define tab properties and content. 
+ 
+ Copyright (c) 2011 The Regents of the University of California.
+ All rights reserved.
+ Permission is hereby granted, without written agreement and without
+ license or royalty fees, to use, copy, modify, and distribute this
+ software and its documentation for any purpose, provided that the above
+ copyright notice and the following two paragraphs appear in all copies
+ of this software.
+
+ IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+ FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+ ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+ THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+ SUCH DAMAGE.
+
+ THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+ INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+ PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+ CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ ENHANCEMENTS, OR MODIFICATIONS.
+
+ PT_COPYRIGHT_VERSION_2
+ COPYRIGHTENDKEY
+ */
+
+package ptolemy.uidesigner.kernel;
+
+import ptolemy.kernel.util.IllegalActionException;
+
+///////////////////////////////////////////////////////////////////
+//// TabDefinition
+
+/** Define tab properties and content. 
+ *
+ *  @author Peter Foldes
+ *  @version $Id$
+ *  @since Ptolemy II 8.1
+ *  @Pt.ProposedRating Red (pdf)
+ *  @Pt.AcceptedRating Red (pdf)
+ */
+public class TabDefinition {
+
+    ///////////////////////////////////////////////////////////////////
+    ////                constructor                                ////
+
+    /** Parse the tab element defined in the Ptolemy attribute.
+     * 
+     *  @param tag The tag used to identify this tab.
+     *  @param name The name of the tab. This will used in the user
+     *  interface. 
+     */
+    public TabDefinition(String tag, String name) {
+        _tag = tag;
+        _name = name;
+    }
+
+    ///////////////////////////////////////////////////////////////////
+    ////                public methods                             ////
+
+    /** Get the tag used to identify the tab.
+     *  @return The tag of the tab.
+     */
+    public String getTag() {
+        return _tag;
+    }
+
+    /** Get the name of the tab.
+     *  @return The name of the tab.
+     */
+    public String getName() {
+        return _name;
+    }
+
+    /** Create and return the underlying layout with all the labels defined.
+     * 
+     * @return The wrapper through which the content of the tab can be
+     * accessed from, or null if it has not been set.
+     */
+    public ContentWrapper getContent() {
+        return _content;
+    }
+
+    /** Add an element to the contents of this tab. The element has to have
+     *  at least the location defined.
+     * 
+     *  @param element Ptolemy element with an Android representation.
+     *  @throws IllegalActionException If the element cannot be added to the
+     *  tab content area, or if the tab content area has not been set.
+     */
+    public void addContent(PositionableElement element)
+            throws IllegalActionException {
+        // Add representation to the tab contents
+        try {
+            _content.add(element);
+        } catch (Exception e) {
+            throw new IllegalActionException(
+                    "Content could not be added to tab " + _name + ".");
+        }
+    }
+
+    ///////////////////////////////////////////////////////////////////
+    ////                private variables                          ////
+
+    /** The tag used to identify the tab.
+     */
+    private String _tag;
+
+    /** The name of the tab.
+     */
+    private String _name;
+
+    /** The complete content of the tab.
+     */
+    private ContentWrapper _content = null;
+}
