2e5d6c2ecc42c07e2df238267702d58d1ea7f180
jkillian <jkillian>
2011-07-07 03:01:01 +0000

    Added listing of remote objects.

--- ptolemy/homer/gui/RemoteObjectList.java
+++ ptolemy/homer/gui/RemoteObjectList.java
@@ -1,9 +1,148 @@
 package ptolemy.homer.gui;
 
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.dnd.DnDConstants;
+import java.awt.dnd.DropTarget;
+import java.awt.dnd.DropTargetAdapter;
+import java.awt.dnd.DropTargetDropEvent;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.List;
+
+import javax.swing.DefaultListCellRenderer;
+import javax.swing.DefaultListModel;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
+import javax.swing.JPopupMenu;
+import javax.swing.JScrollPane;
+import javax.swing.ListSelectionModel;
+
+import org.netbeans.api.visual.model.ObjectScene;
+import org.netbeans.api.visual.widget.LayerWidget;
+
+import ptolemy.kernel.util.NamedObj;
+import ptolemy.util.MessageHandler;
+import ptolemy.vergil.icon.EditorIcon;
+import ptolemy.vergil.icon.XMLIcon;
+import ptolemy.vergil.toolbox.PtolemyTransferable;
 
 public class RemoteObjectList extends JPanel {
+
+    ///////////////////////////////////////////////////////////////////
+    ////                         constructor                       ////
+
     public RemoteObjectList() {
+
+        _scene.addChild(new LayerWidget(_scene));
+        _list.setVisibleRowCount(16);
+        _list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        _list.setLayoutOrientation(JList.VERTICAL_WRAP);
+        _list.setModel(_listModel);
+        _list.setPreferredSize(new Dimension(200, 300));
+        _list.setCellRenderer(new DefaultListCellRenderer() {
+            public Component getListCellRendererComponent(JList list,
+                    Object value, int index, boolean isSelected,
+                    boolean cellHasFocus) {
+
+                JLabel label = (JLabel) super.getListCellRendererComponent(
+                        list, value, index, isSelected, cellHasFocus);
+                label.addMouseListener(new MouseAdapter() {
+                    public void mouseClicked(final MouseEvent mouseEvt) {
+                        if (mouseEvt.getButton() == MouseEvent.BUTTON2) {
+                            JMenuItem delete = new JMenuItem("Delete");
+                            delete.addActionListener(new ActionListener() {
+                                public void actionPerformed(ActionEvent e) {
+                                    _listModel.removeElement(mouseEvt
+                                            .getComponent());
+                                }
+                            });
+
+                            JPopupMenu menu = new JPopupMenu();
+                            menu.add(delete);
+                            menu.show(mouseEvt.getComponent(), mouseEvt.getX(),
+                                    mouseEvt.getY());
+                        }
+                    }
+
+                    public void mouseEntered(MouseEvent e) {
+                        System.out.printf("Mouse entered %s label%n", e
+                                .getComponent().getName());
+                    }
+                });
+
+                try {
+                    NamedObj object = (NamedObj) value;
+                    List iconList = object.attributeList(EditorIcon.class);
+
+                    if (iconList.size() == 0) {
+                        label.setIcon(XMLIcon.getXMLIcon(object, "_icon")
+                                .createIcon());
+                        label.setText(object.getFullName());
+                    } else {
+                        label.setIcon(((EditorIcon) iconList.get(iconList
+                                .size() - 1)).createIcon());
+                        label.setText(object.getFullName());
+                    }
+                } catch (Exception e) {
+                    // Do nothing.
+                }
+
+                return label;
+            }
+        });
+
+        DropTarget target = new DropTarget(this, new DropTargetAdapter() {
+            public void drop(DropTargetDropEvent dropEvent) {
+                if (dropEvent
+                        .isDataFlavorSupported(PtolemyTransferable.namedObjFlavor)) {
+                    try {
+                        List<?> dropItems = (List) dropEvent.getTransferable()
+                                .getTransferData(
+                                        PtolemyTransferable.namedObjFlavor);
+                        if (dropItems.size() > 0) {
+                            _listModel.addElement((NamedObj) dropItems.get(0));
+                        }
+                    } catch (Exception e) {
+                        MessageHandler.error(
+                                "Can't find a supported data flavor for drop in "
+                                        + dropEvent, e);
+                    }
+
+                    dropEvent.acceptDrop(DnDConstants.ACTION_COPY_OR_MOVE);
+                } else {
+                    dropEvent.rejectDrop();
+                }
+            }
+        });
+        add(new JScrollPane(_list));
     }
 
+    ///////////////////////////////////////////////////////////////////
+    ////                         public methods                    ////
+
+    /** Add an item to the list of named objects.
+     *  @param namedObj Object to be appended to the list.
+     */
+    public void addItem(NamedObj namedObj) {
+        _listModel.addElement(namedObj);
+    }
+
+    /** Get the items in the list that have been designated as being remote.
+     *  @return The contents of the list.
+     */
+    public NamedObj[] getItems() {
+        return (NamedObj[]) _listModel.toArray();
+    }
+
+    ///////////////////////////////////////////////////////////////////
+    ////                         private variables                 ////
+
+    private final ObjectScene _scene = new ObjectScene();
+    private final JList _list = new JList();
+    private final DefaultListModel _listModel = new DefaultListModel();
 }
