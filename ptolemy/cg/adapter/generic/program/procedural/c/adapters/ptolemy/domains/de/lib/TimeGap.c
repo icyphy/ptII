cf021a593f2dd6f0bea8f6c0d2c145ea1d1d3061
derler <derler>
2011-08-05 21:07:04 +0000

    fixed bug

--- ptolemy/cg/adapter/generic/program/procedural/c/adapters/ptolemy/domains/de/lib/TimeGap.c
+++ ptolemy/cg/adapter/generic/program/procedural/c/adapters/ptolemy/domains/de/lib/TimeGap.c
@@ -2,6 +2,6 @@
 static Time lastModelTime;
 Time timeGap;
 timeSub(currentModelTime, lastModelTime, &timeGap);
-$put(output, timeGap);
+$put(output, timeGap.secs + timeGap.nsecs/1000000000);
 lastModelTime = currentModelTime;
 /**/
