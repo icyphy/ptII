d89d05f09719e15ead79878091aaebaefe7a88a7
eal <eal>
2011-08-16 22:33:05 +0000

    Changed PeriodicSampler so it now has a microstep parameter.   By default, the value is 0 and the behavior is as before.   But if you set microstep to 1, then PeriodicSampler no   longer introduces a microstep delay. It reads its inputs   in microstep 1 and immediately produces them on the output.   With the microstep parameter set to 1, this actor now   behaves like Sampler with a DiscreteClock driving it.

--- ptolemy/domains/continuous/lib/PeriodicSampler.java
+++ ptolemy/domains/continuous/lib/PeriodicSampler.java
@@ -27,20 +27,19 @@
  */
 package ptolemy.domains.continuous.lib;
 
-import java.util.HashSet;
-import java.util.Set;
-
-import ptolemy.actor.CausalityMarker;
 import ptolemy.actor.Director;
 import ptolemy.actor.SuperdenseTimeDirector;
+import ptolemy.actor.lib.Sampler;
 import ptolemy.actor.lib.Transformer;
+import ptolemy.actor.util.CausalityInterface;
 import ptolemy.actor.util.Time;
 import ptolemy.data.DoubleToken;
+import ptolemy.data.IntToken;
 import ptolemy.data.Token;
 import ptolemy.data.expr.Parameter;
 import ptolemy.data.type.BaseType;
 import ptolemy.kernel.CompositeEntity;
-import ptolemy.kernel.Port;
+import ptolemy.kernel.util.Attribute;
 import ptolemy.kernel.util.IllegalActionException;
 import ptolemy.kernel.util.NameDuplicationException;
 import ptolemy.kernel.util.Workspace;
@@ -54,13 +53,23 @@ import ptolemy.kernel.util.Workspace;
  0.1.  Specifically, if the actor is initialized at time <i>t</i> and the sample
  period is <i>T</i>, then the output will have the value of the input
  at times <i>t</i> + <i>nT</i>, for all natural numbers <i>n</i>.
- This sampler will send to the output the initial value of
- the input (the input value at microstep 0), but will send it one microstep later (at
- microstep 1).
+ By default, this sampler will send to the output the initial value of
+ the input (the input value at microstep 0), but will send it one 
+ microstep later (at microstep 1).
  This ensures that the output at microstep 0 is always absent, thus
  ensuring continuity from the left. That is, the input is absent prior
  to the sample time, so continuity requires that it be absent at
  microstep 0 at the sample time.
+ <p>
+ To get this sampler to record values other than the initial value,
+ set the <i>microstep</i> parameter to a value greater than 0.
+ Setting it to 1, for example, will record the input value after
+ a discontinuity rather than before the discontinuity. Note
+ that {@link Sampler} normally records its inputs at microstep 1
+ (because it is triggered by a discrete signal, which has events
+ at microstep 1), and therefore if you want this PeriodicSampler
+ to behave the same as Sampler, you should set <i>microstep</i>
+ to 1.
  <p>
  This actor has multiport inputs and outputs. Signals in
  each input channel are sampled and produced to corresponding output
@@ -101,11 +110,9 @@ public class PeriodicSampler extends Transformer {
         samplePeriod.setExpression("0.1");
         samplePeriod.setTypeEquals(BaseType.DOUBLE);
 
-        // Empty set of dependent ports, indicating that the output
-        // does not depend on the input.
-        Set<Port> dependentPorts = new HashSet<Port>();
-        CausalityMarker causalityMarker = new CausalityMarker(this, "causalityMarker");
-        causalityMarker.addDependentPortSet(dependentPorts);
+        microstep = new Parameter(this, "microstep");
+        microstep.setExpression("0");
+        microstep.setTypeEquals(BaseType.INT);
 
         _attachText("_iconDescription", "<svg>\n"
                 + "<rect x=\"-30\" y=\"-20\" " + "width=\"60\" height=\"40\" "
@@ -117,6 +124,11 @@ public class PeriodicSampler extends Transformer {
     ///////////////////////////////////////////////////////////////////
     ////                         public variables                  ////
 
+    /** The microstep at which to read the input. This is an
+     *  whose default value is 0.
+     */
+    public Parameter microstep;
+
     /** The parameter for the sampling period. This is a double
      *  whose default value is 0.1.
      */
@@ -124,6 +136,32 @@ public class PeriodicSampler extends Transformer {
 
     ///////////////////////////////////////////////////////////////////
     ////                         public methods                    ////
+
+    /** If the attribute is microstep, adjust the causality interface.
+     *  @throws IllegalActionException If the superclass throws it.
+     */
+    public void attributeChanged(Attribute attribute) throws IllegalActionException {
+        if (attribute == microstep) {
+            int microstepValue = ((IntToken)microstep.getToken()).intValue();
+            if (microstepValue != _microstep) {
+                CausalityInterface causalityInterface = getCausalityInterface();
+                if (microstepValue == 0) {
+                    // Output depends on the input after a microstep delay.
+                    causalityInterface.declareDelayDependency(input, output, 0.0, 1);
+                } else {
+                    // Output depends immediately on the input.
+                    causalityInterface.declareDelayDependency(input, output, 0.0, 0);
+                }
+                _microstep = microstepValue;
+                Director director = getDirector();
+                if (director != null) {
+                    director.invalidateSchedule();
+                }
+            }
+        } else {
+            super.attributeChanged(attribute);
+        }
+    }
 
     /** Clone this actor into the specified workspace. The new actor is
      *  <i>not</i> added to the directory of that workspace (you must do this
@@ -141,14 +179,15 @@ public class PeriodicSampler extends Transformer {
     public Object clone(Workspace workspace) throws CloneNotSupportedException {
         PeriodicSampler newObject = (PeriodicSampler) super.clone(workspace);
         newObject.output.setWidthEquals(newObject.input, true);
-
         return newObject;
     }
 
     /** Generate an output if the current time is one of the sampling
-     *  times and the current microstep is 1.
+     *  times and the microstep matches.
+     *  In addition, if the microstep parameter has value 0,
+     *  produce the output only if the current microstep is 1.
      *  The value of the event is the value of the input signal at the
-     *  current time at microstep 0.
+     *  current time at the microstep specified by the microstep parameter.
      *  @exception IllegalActionException If the transfer of tokens failed.
      */
     public void fire() throws IllegalActionException {
@@ -160,22 +199,52 @@ public class PeriodicSampler extends Transformer {
             _debug("Current time is " + currentTime + " with microstep "
                     + microstep);
         }
-        int width = Math.min(input.getWidth(), output.getWidth());
-        if (currentTime.compareTo(_nextSamplingTime) == 0 && microstep == 1) {
-            for (int i = 0; i < width; i++) {
-                if (_pendingOutputs[i] != null) {
-                    // There is a deferred output for this input channel.
-                    output.send(i, _pendingOutputs[i]);
-                    if (_debugging) {
-                        _debug("Sending output value " + _pendingOutputs[i]
-                                + " on channel " + i);
+        int inputWidth = input.getWidth();
+        int outputWidth = output.getWidth();
+        if (currentTime.compareTo(_nextSamplingTime) == 0) {
+            // Time is right to produce an output. Check the microstep.
+            if (_microstep == 0 && microstep == 1) {
+                // In delay mode. Input should have been read in microstep 0.
+                for (int i = 0; i < outputWidth; i++) {
+                    if (_pendingOutputs[i] != null) {
+                        // There is a deferred output for this input channel.
+                        output.send(i, _pendingOutputs[i]);
+                        if (_debugging) {
+                            _debug("Sending output value " + _pendingOutputs[i]
+                                                                             + " on channel " + i);
+                        }
+                    } else {
+                        output.sendClear(i);
                     }
-                } else {
+                }
+            } else if (_microstep != 0 && _microstep == microstep) {
+                // Microstep matches.
+                // Read the input and produce an output.
+                for (int i = 0; i < inputWidth; i++) {
+                    if (input.isKnown(i) && input.hasToken(i)) {
+                        Token token = input.get(i);
+                        if (i < outputWidth) {
+                            output.send(i, token);
+                            if (_debugging) {
+                                _debug("Read input and sent to output: " + token
+                                        + " on channel " + i);
+                            }
+                        }
+                    }
+                }
+                // If the output is wider than the input, send clear.
+                for (int i = inputWidth; i < outputWidth; i++) {
+                    output.sendClear(i);
+                }
+            } else {
+                // In delay mode, but microstep does not match.
+                for (int i = 0; i < outputWidth; i++) {
                     output.sendClear(i);
                 }
             }
         } else {
-            for (int i = 0; i < width; i++) {
+            // Not time to send an output.
+            for (int i = 0; i < outputWidth; i++) {
                 output.sendClear(i);
             }
         }
@@ -189,7 +258,7 @@ public class PeriodicSampler extends Transformer {
      */
     public void initialize() throws IllegalActionException {
         super.initialize();
-        int width = Math.min(input.getWidth(), output.getWidth());
+        int width = output.getWidth();
         if (_pendingOutputs == null || _pendingOutputs.length != width) {
             _pendingOutputs = new Token[width];
         }
@@ -198,15 +267,21 @@ public class PeriodicSampler extends Transformer {
         for (int i = 0; i < width; i++) {
             _pendingOutputs[i] = null;
         }
+        getDirector().fireAt(this, _nextSamplingTime, _microstep);
     }
 
-    /** Return false. This actor can produce some outputs even the
+    /** Return false if the microstep value
+     *  is zero. In that case, this actor can produce some outputs even the
      *  inputs are unknown. This actor is usable for breaking feedback
      *  loops. It does not read inputs in the fire() method.
      *  @return False.
      */
     public boolean isStrict() {
-        return false;
+        if (_microstep == 0) {
+            return false;
+        } else {
+            return true;
+        }
     }
 
     /** If the current microstep is zero, sample the inputs and request
@@ -220,25 +295,43 @@ public class PeriodicSampler extends Transformer {
         Time currentTime = director.getModelTime();
         int microstep = ((SuperdenseTimeDirector)director).getIndex();
         if (currentTime.compareTo(_nextSamplingTime) == 0) {
-            if (microstep == 0) {
-                int width = Math.min(input.getWidth(), output.getWidth());
-                for (int i = 0; i < width; i++) {
-                    if (input.hasToken(i)) {
-                        _pendingOutputs[i] = input.get(i);
-                    } else {
-                        _pendingOutputs[i] = null;
+            // Current time matches. Check microstep.
+            if (_microstep == 0) {
+                // In delay mode. Read the input if the microstep is 0.
+                if (microstep == 0) {
+                    int inputWidth = input.getWidth();
+                    int outputWidth = output.getWidth();
+                    for (int i = 0; i < outputWidth; i++) {
+                        if (i < inputWidth && input.hasToken(i)) {
+                            _pendingOutputs[i] = input.get(i);
+                        } else {
+                            _pendingOutputs[i] = null;
+                        }
+                        if (_debugging) {
+                            _debug("Read input: " + _pendingOutputs[i]
+                                    + " on channel " + i);
+                        }
                     }
-                    if (_debugging) {
-                        _debug("Read input: " + _pendingOutputs[i]
-                                + " on channel " + i);
+                    for (int i = outputWidth; i < inputWidth; i++) {
+                        // Read and discard the input.
+                        input.get(i);
                     }
+                    director.fireAt(this, currentTime, 1);
+                } else if (microstep == 1) {
+                    double samplePeriodValue = ((DoubleToken) samplePeriod
+                            .getToken()).doubleValue();
+                    _nextSamplingTime = currentTime.add(samplePeriodValue);
+                    director.fireAt(this, _nextSamplingTime, 0);
                 }
-                director.fireAt(this, currentTime, 1);
-            } else if (microstep == 1) {
-                double samplePeriodValue = ((DoubleToken) samplePeriod
-                        .getToken()).doubleValue();
-                _nextSamplingTime = currentTime.add(samplePeriodValue);
-                director.fireAt(this, _nextSamplingTime, 0);
+            } else {
+                // Not in delay mode. If the microstep matches,
+                // request refiring.
+                if (_microstep == microstep) {
+                    double samplePeriodValue = ((DoubleToken) samplePeriod
+                            .getToken()).doubleValue();
+                    _nextSamplingTime = currentTime.add(samplePeriodValue);
+                    director.fireAt(this, _nextSamplingTime, _microstep);
+                }
             }
         }
         return super.postfire();
@@ -259,7 +352,7 @@ public class PeriodicSampler extends Transformer {
 
     ///////////////////////////////////////////////////////////////////
     ////                         private variables                 ////
-
+    
     /** The next sampling time. */
     private Time _nextSamplingTime;
 
@@ -268,4 +361,7 @@ public class PeriodicSampler extends Transformer {
      *  the microstep was zero).
      */
     private Token[] _pendingOutputs;
+    
+    /** Value of the microstep. */
+    private int _microstep = 1;
 }
