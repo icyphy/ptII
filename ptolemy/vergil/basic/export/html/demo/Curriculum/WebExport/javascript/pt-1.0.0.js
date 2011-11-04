931c7005b3e8182041068ca2e614d4c1f0037963
eal <eal>
2011-11-04 18:34:19 +0000

    Demo of web export.

---
+++ ptolemy/vergil/basic/export/html/demo/Curriculum/WebExport/javascript/pt-1.0.0.js
@@ -0,0 +1,24 @@
+/* JavaScript library for Ptolemy Export to Web functionality.
+ *
+ */
+function writeText(text) {
+  document.getElementById("afterImage").innerHTML = text;
+};
+// The following anonymous function will be executed when the document
+// is "ready" (but images are not necessarily loaded).
+// JQuery ensures that the DOM object is fully constructed.
+// The argument to "ready" is a callback function to call
+// after the parent has completed.
+$(document).ready(function(){
+  // The following says to select all elements that are
+  // areas of class iframe (use # instead of . to specify an ID instead
+  // of a class), and apply the function fancybox to them with the specified
+  // arguments.
+  // FIXME: This should be inserted by the classes that use it!
+  // The following requires the jquery fancybox extension.
+  // FIXME: The width and height below are hardwired. Should
+  // be parameterized. Can be parameterized individually
+  // by using an object ID instead of the iframe class.
+  // Many more options can be set, as shown in http://fancybox.net/api.
+  $("area.iframe").fancybox({'width':700,'height':500});
+});
