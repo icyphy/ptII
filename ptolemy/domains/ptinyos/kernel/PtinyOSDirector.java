f43dd46bc68a28d5cb78bf889f93c13c01778f20
cxh <cxh>
2012-01-26 17:03:54 +0000

    Director c'tors now throw exceptions if the Time object cannot be instantiated.

--- ptolemy/domains/ptinyos/kernel/PtinyOSDirector.java
+++ ptolemy/domains/ptinyos/kernel/PtinyOSDirector.java
@@ -197,8 +197,10 @@ public class PtinyOSDirector extends Director {
     /** Construct a director in the default workspace with an empty string
      *  as its name. The director is added to the list of objects in
      *  the workspace. Increment the version number of the workspace.
+     *  @throws NameDuplicationException If construction of Time objects fails.
+     *  @throws IllegalActionException If construction of Time objects fails.
      */
-    public PtinyOSDirector() {
+    public PtinyOSDirector() throws IllegalActionException, NameDuplicationException {
         super();
         _initializeParameters();
     }
@@ -207,8 +209,11 @@ public class PtinyOSDirector extends Director {
      *  The director is added to the list of objects in the workspace.
      *  Increment the version number of the workspace.
      *  @param workspace The workspace of this object.
+     *  @throws NameDuplicationException If construction of Time objects fails.
+     *  @throws IllegalActionException If construction of Time objects fails.
      */
-    public PtinyOSDirector(Workspace workspace) {
+    public PtinyOSDirector(Workspace workspace)
+            throws IllegalActionException, NameDuplicationException {
         super(workspace);
         _initializeParameters();
     }
