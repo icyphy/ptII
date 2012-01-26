e3dcbc2771865054e82e186e85951717ad98abe7
blickly <blickly>
2012-01-26 21:35:40 +0000

    Created actor for generating random integers.

---
+++ ptolemy/actor/lib/RandomInteger.java
@@ -0,0 +1,84 @@
+/* Actor for generating random integers.
+ * 
+ * Copyright (c) 2012 The Regents of the University of California. All
+ * rights reserved.
+ * 
+ * Permission is hereby granted, without written agreement and without license
+ * or royalty fees, to use, copy, modify, and distribute this software and its
+ * documentation for any purpose, provided that the above copyright notice and
+ * the following two paragraphs appear in all copies of this software.
+ * 
+ * IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY FOR
+ * DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES ARISING OUT
+ * OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF THE UNIVERSITY OF
+ * CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * 
+ * THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE PROVIDED HEREUNDER IS ON AN
+ * "AS IS" BASIS, AND THE UNIVERSITY OF CALIFORNIA HAS NO OBLIGATION TO PROVIDE
+ * MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+ */
+package ptolemy.actor.lib;
+
+import ptolemy.data.IntToken;
+import ptolemy.data.type.BaseType;
+import ptolemy.kernel.CompositeEntity;
+import ptolemy.kernel.util.IllegalActionException;
+import ptolemy.kernel.util.NameDuplicationException;
+
+/** Actor for generating random integers.
+ *  Generated integers are uniformly sampled from the range of 32-bit integers.
+ *
+ *  @author Ben Lickly
+ *  @version $Id$
+ *  @since Ptolemy II 9.0
+ *  @Pt.ProposedRating Red (blickly)
+ *  @Pt.AcceptedRating Red (blickly)
+ *
+ */
+public class RandomInteger extends RandomSource {
+
+    /** Construct an actor with the given container and name.
+     *  @param container The container.
+     *  @param name The name of this actor.
+     *  @exception IllegalActionException If the actor cannot be contained
+     *   by the proposed container.
+     *  @exception NameDuplicationException If the container already has an
+     *   actor with this name.
+     */
+    public RandomInteger(CompositeEntity container, String name)
+            throws NameDuplicationException, IllegalActionException {
+        super(container, name);
+        
+        output.setTypeEquals(BaseType.INT);
+    }
+    
+    ///////////////////////////////////////////////////////////////////
+    ////                         public methods                    ////
+
+    /** Send a random integer to the output.
+     *  This number is only changed in the prefire() method, so it will
+     *  remain constant throughout an iteration.
+     *  @exception IllegalActionException If there is no director.
+     */
+    public void fire() throws IllegalActionException {
+        super.fire();
+        output.send(0, new IntToken(_current));
+    }
+
+    ///////////////////////////////////////////////////////////////////
+    ////                         protected methods                 ////
+
+    /** Generate a new random integer.
+     */
+    protected void _generateRandomNumber() {
+        _current = _random.nextInt();
+    }
+
+    ///////////////////////////////////////////////////////////////////
+    ////                         private variables                 ////
+    /** The random integer for the current iteration. */
+    private int _current;
+
+}
