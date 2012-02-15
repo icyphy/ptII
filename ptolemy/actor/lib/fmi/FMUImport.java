e6441df406d35f5a68fd8ff7448a922f2f7cff4e
cxh <cxh>
2012-02-15 21:00:00 +0000

    Unzip the .fmu file

--- ptolemy/actor/lib/fmi/FMUImport.java
+++ ptolemy/actor/lib/fmi/FMUImport.java
@@ -27,10 +27,22 @@
  */
 package ptolemy.actor.lib.fmi;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.zip.ZipInputStream;
+import java.util.zip.ZipEntry;
+
 import ptolemy.actor.TypedAtomicActor;
 import ptolemy.data.expr.FileParameter;
 import ptolemy.data.type.BaseType;
 import ptolemy.kernel.CompositeEntity;
+import ptolemy.kernel.util.Attribute;
 import ptolemy.kernel.util.IllegalActionException;
 import ptolemy.kernel.util.NameDuplicationException;
 import ptolemy.kernel.util.Workspace;
@@ -72,12 +84,102 @@ public class FMUImport extends TypedAtomicActor {
     ///////////////////////////////////////////////////////////////////
     ////                         public methods                    ////
 
-    /** Read exactly one token from the input and output the token
-     *  if it is double or a NaN token.
+    /** If the specified attribute is <i>fmuFile</i>, then unzip
+     *  the file and load in the .xml file, creating and deleting parameters
+     *  as necessary.
+     *  @param attribute The attribute that has changed.
+     *  @exception IllegalActionException If the specified attribute
+     *  is <i>fmuFile</i> and the file cannot be opened or there
+     *  is a problem creating or destroying the parameters
+     *  listed in thile.
+     */
+    public void attributeChanged(Attribute attribute)
+            throws IllegalActionException {
+        if (attribute == fmuFile) {
+            _updateParameters();
+        }
+
+        super.attributeChanged(attribute);
+    }
+    /** 
      *  @exception IllegalActionException If there is no director.
      */
     public void fire() throws IllegalActionException {
         super.fire();
     }
 
+    /** Unzip a file.
+     *  Based on http://java.sun.com/developer/technicalArticles/Programming/compression/
+     *  @param zipFilename  The file to be unzipped.
+     *  @return a list of canonical paths to the files created
+     *  @exception IOException if the file cannot be opened, if there are problems reading
+     *  the zip file or if there are problems creating the files or directories.
+     */
+    private List _unzip(String zipFileName) throws IOException {
+        BufferedOutputStream destination = null;
+        FileInputStream fileInputStream = new FileInputStream(zipFileName);
+        ZipInputStream zipInputStream = new ZipInputStream(new BufferedInputStream(fileInputStream));
+        ZipEntry entry;
+        final int BUFFER = 2048;
+        byte data[] = new byte[BUFFER];
+        // FIXME: maybe put this in the tmp directory?
+        String topDirectory = zipFileName.substring(0, zipFileName.length() - 4);
+        List files = new LinkedList();
+        try {
+            while((entry = zipInputStream.getNextEntry()) != null) {
+                System.out.println("Extracting: " + entry);
+                String entryName = entry.getName();
+                File destinationFile = new File(topDirectory, entryName);
+                File destinationParent = destinationFile.getParentFile();
+                // If the directory does not exist, create it.
+                if (!destinationParent.isDirectory()
+                    && !destinationParent.mkdirs()) {
+                    throw new IOException("Failed to create \""
+                            + destinationParent + "\".");
+                }
+                // If the entry is not a directory, then write the file.
+                if (!entry.isDirectory()) {
+                    // Write the files to the disk.
+                    FileOutputStream fos = new FileOutputStream(destinationFile); 
+                    destination = new BufferedOutputStream(fos, BUFFER);
+                    int count;
+                    while ((count = zipInputStream.read(data, 0, BUFFER)) 
+                            != -1) {
+                        destination.write(data, 0, count);
+                    }
+                    destination.flush();
+                    destination.close();
+                    files.add(destinationFile.getCanonicalPath());
+                }
+            }
+        } finally {
+            if (zipInputStream != null) {
+                zipInputStream.close();
+            }
+        }
+        return files;
+    }
+
+    /** Update the parameters listed in the modelDescription.xml file
+     *  contained in the zipped file named by the <i>fmuFile</i>
+     *  parameter
+     *  @exception IllegalActionException If the file named by the
+     *  <i>fmuFile<i> parameter cannot be unzipped or if there
+     *  is a problem deleting any preexisting parameters or
+     *  creating new parameters.
+     */
+    private void _updateParameters() throws IllegalActionException {
+        // Unzip the fmuFile.  We probably need to do this
+        // because we will need to load the shared library later.
+        String fmuFileName = null;
+        List files = null;
+        try {
+            fmuFileName = fmuFile.asFile().getCanonicalPath();
+            files = _unzip(fmuFileName);
+        } catch (IOException ex) {
+            throw new IllegalActionException(this, ex,
+                    "Failed to unzip \"" + fmuFileName + "\".");
+        }
+        System.out.println("FMUImport: created " + files.size() + " files.");
+    }
 }
