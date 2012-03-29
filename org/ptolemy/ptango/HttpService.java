e4136f8be31aab7042d88d9c737da813576256a7
ltrnc <ltrnc>
2012-03-29 16:25:48 +0000

    New WebServer and HttpService actors plus a demo model.  WebServer runs a Jetty web server when the model is executed.  HttpService registers itself with the WebServer and provides a servlet to handle http requests to the given path.

---
+++ org/ptolemy/ptango/HttpService.java
@@ -0,0 +1,178 @@
+package org.ptolemy.ptango;
+import java.io.IOException;
+import java.util.Iterator;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import ptolemy.actor.CompositeActor;
+import ptolemy.actor.TypedAtomicActor;
+import ptolemy.data.expr.StringParameter;
+import ptolemy.kernel.util.IllegalActionException;
+import ptolemy.kernel.util.NameDuplicationException;
+import ptolemy.kernel.util.NamedObj;
+
+/* An actor that handles an HttpRequest to the given path.
+
+ Copyright (c) 1997-2011 The Regents of the University of California.
+ All rights reserved.
+ Permission is hereby granted, without written agreement and without
+ license or royalty fees, to use, copy, modify, and distribute this
+ software and its documentation for any purpose, provided that the above
+ copyright notice and the following two paragraphs appear in all copies
+ of this software.
+
+ IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+ FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+ ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+ THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+ SUCH DAMAGE.
+
+ THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+ INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+ PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+ CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ ENHANCEMENTS, OR MODIFICATIONS.
+
+ PT_COPYRIGHT_VERSION_2
+ COPYRIGHTENDKEY
+
+ */
+
+/** An actor that handles an HttpRequest to the given path.  This actor creates
+ *  a servlet, registers this servlet with the WebServer during preinitialize(),
+ *  and displays its content at the specified path when a request is received.
+ *  
+ *  @author ltrnc
+ *  @version $Id$
+ *  @since Ptolemy II 9.0
+ *  @Pt.ProposedRating Red (ltrnc)
+ *  @Pt.AcceptedRating Red (ltrnc)
+ *  @see org.ptolemy.ptango.WebServer
+ */
+
+public class HttpService extends TypedAtomicActor {
+    public HttpService(CompositeActor container, String name)
+        throws IllegalActionException, NameDuplicationException {
+        super(container, name);
+
+        message = new StringParameter(this, "message");
+        message.setExpression("Hello World");
+        
+        path = new StringParameter(this, "path");
+        path.setExpression("/*");
+    }
+    
+    ///////////////////////////////////////////////////////////////////
+    ////                     public methods                        ////
+    
+    /** Register this actor's servlet(s) with the WebServer.
+     *
+     *  @exception IllegalActionException If no WebServer actor is found at the
+     *  top level of the model or if this actor is the top level actor.
+     */
+    public void preinitialize() throws IllegalActionException {
+        // Create a new servlet mapped to the given URL that displays the
+        // given message
+        // Note that if the parameters are updated while the model is 
+        // running - e.g. during fire() - the changes will not propagate
+        
+        // Find the WebServer actor in this model.  
+        // Throw an IllegalActionException if none is found
+        NamedObj topLevel = this;
+        topLevel = topLevel.toplevel();
+        
+        if (topLevel.equals(this)) {
+            throw new IllegalActionException(this, "HttpRequestActor is not" +
+            		"allowed to be the top-level actor");
+        }
+        
+        // Find the WebServer actor.  Currently this code requires it to be
+        // immediately contained by the top-level actor.
+        // (is that true or does containedObjectsIterator() traverse hierarchy?)
+        Iterator objects = topLevel.containedObjectsIterator();
+        WebServer webServerActor = null;
+        Object object = null;
+        
+        while(objects.hasNext()) {
+            object = objects.next();
+            if (object instanceof WebServer) {
+                webServerActor = (WebServer) object; 
+            }
+        }
+        
+        if (webServerActor == null){
+            throw new IllegalActionException(this, "No WebServer actor found. "+
+             "Please make sure one is present at the top level of the model.");	
+        }
+        
+        // Register this actor's servlet with the WebServer
+        // TODO:  Check if path is valid (how?).  Check for duplicate paths?
+        // I think that will run OK (last added servlet takes precedence)
+        // but would be confusing to the user.
+        String mappedPath = "/*";
+        if (!path.getExpression().isEmpty()) {
+            mappedPath = path.getExpression().toString();
+        }  
+        
+        String displayMessage = "Hello World";
+        if (message.getExpression() != null) {
+            displayMessage = message.getExpression().toString();
+        }
+        
+        webServerActor
+            .registerServlet(new HelloServlet(displayMessage), mappedPath);
+    }
+    
+    ///////////////////////////////////////////////////////////////////
+    ////                     ports and parameters                  ////
+    
+    /** The message to display when a get request is received.
+     */
+    public StringParameter message;
+    
+    /** The relative URL to map this servlet to. 
+     */
+    public StringParameter path;
+    
+    
+    /** A HelloWorld servlet example from 
+     *  http://wiki.eclipse.org/Jetty/Tutorial/Embedding_Jetty
+     */
+    private class HelloServlet extends HttpServlet
+    {
+        private String greeting="Hello World";
+        
+        /** Construct a servlet with the default greeting.
+         */     
+        public HelloServlet(){}
+        
+        /** Construct a servlet with the specified greeting.
+         * 
+         * @param greeting The message to display on the returned web page
+         */
+        public HelloServlet(String greeting)
+        {
+            this.greeting=greeting;
+        }
+        
+        /** Handle an HTTP get request by creating a web page as the HTTP 
+         *  response.
+         * 
+         * @param request  The HTTP get request
+         * @param response  The HTTP response to write a web page to
+         */
+        protected void doGet(HttpServletRequest request, 
+                HttpServletResponse response) 
+                throws ServletException, IOException
+        {
+            response.setContentType("text/html");
+            response.setStatus(HttpServletResponse.SC_OK);
+            response.getWriter().println("<h1>"+greeting+"</h1>");
+            // response.getWriter().println("session=" + request.getSession(true).getId());
+        }
+    }
+}
---
+++ org/ptolemy/ptango/WebServer.java
@@ -0,0 +1,257 @@
+/* An actor that runs a Jetty web server and routes requests to other actors 
+ * in the model.
+
+ Copyright (c) 1997-2011 The Regents of the University of California.
+ All rights reserved.
+ Permission is hereby granted, without written agreement and without
+ license or royalty fees, to use, copy, modify, and distribute this
+ software and its documentation for any purpose, provided that the above
+ copyright notice and the following two paragraphs appear in all copies
+ of this software.
+
+ IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+ FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+ ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+ THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+ SUCH DAMAGE.
+
+ THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+ INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+ PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+ CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ ENHANCEMENTS, OR MODIFICATIONS.
+
+ PT_COPYRIGHT_VERSION_2
+ COPYRIGHTENDKEY
+
+ */
+
+package org.ptolemy.ptango;
+
+import javax.servlet.http.HttpServlet;
+
+import org.eclipse.jetty.server.Connector;
+import org.eclipse.jetty.server.Handler;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.ContextHandlerCollection;
+import org.eclipse.jetty.server.nio.SelectChannelConnector;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
+import ptolemy.actor.CompositeActor;
+import ptolemy.actor.TypedAtomicActor;
+import ptolemy.kernel.util.IllegalActionException;
+import ptolemy.kernel.util.NameDuplicationException;
+
+///////////////////////////////////////////////////////////////////
+//// WebServer
+
+/** An actor that runs a Jetty web server and routes requests to other actors 
+ *  in the model.  HttpService actors will register their servlets in 
+ *  preinitialize().  Then, the WebServer will route requests to the appropriate
+ *  servlet while the model is running.
+ *  
+
+ *  @author ltrnc
+ *  @version $Id$
+ *  @since Ptolemy II 9.0
+ *  @Pt.ProposedRating Red (ltrnc)
+ *  @Pt.AcceptedRating Red (ltrnc)
+ *  @see org.ptolemy.ptango.HttpService
+ */
+
+public class WebServer extends TypedAtomicActor {
+    public WebServer(CompositeActor container, String name)
+        throws IllegalActionException, NameDuplicationException {
+        super(container, name);
+        
+        // Create an empty collection to store the context handlers and
+        // a new context handler to hold servlets from the actors
+        // This is done here since other actors will register their servlets in
+        // preinitialize
+        _contextHandlers = new ContextHandlerCollection();
+        
+        _actorContextHandler = 
+            new ServletContextHandler(ServletContextHandler.SESSIONS);
+        _actorContextHandler.setContextPath("/");
+        
+        // Create a Jetty server and set its properties
+        _server = new Server();
+        SelectChannelConnector connector = new SelectChannelConnector();
+        connector.setPort(_portNumber);
+        connector.setMaxIdleTime(_maxIdleTime);
+        _server.setConnectors(new Connector[] {connector});
+
+    }
+    
+    ///////////////////////////////////////////////////////////////////
+    ////                         public methods                    ////
+    
+    /** Start the web server in a new thread.
+     *  Reference:  http://wiki.eclipse.org/Jetty/Tutorial/Embedding_Jetty
+     *  http://ptrthomas.wordpress.com/2009/01/24/how-to-start-and-stop-jetty-revisited/
+     *  http://draconianoverlord.com/2009/01/10/war-less-dev-with-jetty.html
+     *  @exception IllegalActionException Not thrown in this base class.
+     */
+    public void initialize() throws IllegalActionException {
+        super.initialize();        
+        
+        // Start the server in a new thread
+        _serverThread = new Thread(new RunnableServer());
+        _serverThread.start();
+
+    }
+    
+    /** Clear the list of context handlers so that an completely new list is 
+     *  generated during the next preinitialize() in case the model changes.
+     *
+     *  @exception IllegalActionException Not thrown in this class.
+     */
+    public void fire() throws IllegalActionException {
+        
+        // Create an empty collection to store the context handlers and
+        // a new context handler to hold servlets from the actors
+        // This is done in fire() in case the model is re-started, since the
+        // collection must be cleared before other actors register their 
+        // servlets in preinitialize()
+        if (_contextHandlers.getHandlers() != null &&
+                _contextHandlers.getHandlers().length > 0) {
+            _contextHandlers = new ContextHandlerCollection();
+        
+            _actorContextHandler = 
+                new ServletContextHandler(ServletContextHandler.SESSIONS);
+            _actorContextHandler.setContextPath("/");
+        }
+    }
+    
+    /** Check if the server is still running.  If so, return true.  If not,
+     *  return false.
+     *  
+     *  @return True if the server is still running, false otherwise
+     */
+    // TODO:  This is not working...
+    /*
+    public boolean prefire() throws IllegalActionException {
+        super.prefire();
+        
+        
+        if (_serverThread.isInterrupted()) {
+            return false;
+        } 
+
+        return true;
+        
+    }
+    */
+
+    
+    /** Register a servlet with this WebServer.  An actor with a servlet should 
+     * call this in its initialize() method to register its servlet. 
+     * 
+     * In the current implementation, servlets must be registered before the
+     * Jetty server starts.  Servlets are not allowed to be added to a running
+     * ContextHandler.  Currently, the Jetty server is started once and runs 
+     * until the model finishes executing.  It would also be possible to pause
+     * the server, add a servlet, and restart the server, which would allow
+     * a model to dynamically add servlets.  This might cause strange behavior
+     * to an outside observer, however, since some HttpRequests could fail 
+     * if a servlet has not been loaded yet.
+     * 
+     * @param servlet  The servlet to register with this web server
+     * @param path  The relative path to map this servlet to
+     */
+    public void registerServlet(HttpServlet servlet, String path) {
+        
+        // FIXME:  How to check that this path is legal?
+        _actorContextHandler.addServlet(new ServletHolder(servlet), path); 
+        
+        _contextHandlers.setHandlers(new Handler[] {_actorContextHandler});
+        _server.setHandler(_contextHandlers);
+    }
+    
+    /** Instruct the thread running the web server to the stop the server and
+     *  terminate itself.
+     */
+     public void wrapup() throws IllegalActionException {
+         super.wrapup();
+         
+         _serverThread.interrupt();
+         _serverThread = null;
+     }
+     
+     /** A Runnable class to run a Jetty web server in a separate thread.
+      * 
+      * @author ltrnc
+      */
+     private class RunnableServer implements Runnable {
+
+        /** Run the Jetty web server.  Stop the server if this thread is 
+         *  interrupted (for example, when the model is finished executing, 
+         *  WebServer's wrapup() will interrupt this thread) or if an 
+         *  exception occurs.
+         */
+        public void run() {
+
+            while(!Thread.interrupted()){
+                     
+            // Start the server.  The .join() method blocks the thread until the 
+            // server terminates.
+             try {
+                _server.start();
+                _server.join();
+             } catch(Exception e){
+                 // Notify thread users and terminate the server and this thread 
+                 // if an exception occurs
+                 try {
+                     _server.stop();
+                 } catch(Exception e2){
+                     Thread.currentThread().interrupt();
+                     return;
+                 }
+                 Thread.currentThread().interrupt();
+                 return;
+             };
+           }
+            try {
+                _server.stop();
+            } catch(Exception e) {
+                // Nothing to do here since the thread will terminate next
+            }
+            return;
+        }
+         
+     }
+     
+    ///////////////////////////////////////////////////////////////////
+    ////                         private variables                 ////
+     
+     /** A context handler which stores all of the servlets registered 
+      *  by other actors.
+      */
+     private ServletContextHandler _actorContextHandler;
+     
+     /** A context handler collection which stores the context handlers.  
+      *  It is possible to have multiple kinds of context handlers. For example, 
+      *  some content in a WebContext handler plus the servlets registered by 
+      *  actors stored in the _actorContextHanlder.
+      */
+     private ContextHandlerCollection _contextHandlers;
+     
+     /** The maximum idle time for a connection.
+      */
+     private int _maxIdleTime = 30000;
+     
+     /** The port number the server receives requests on.
+      */
+     private int _portNumber = 8080;
+     
+     /** The Jetty web server. 
+      */
+     private Server _server;
+     
+     /** The thread that runs the web server.
+      */
+     private Thread _serverThread;   
+        
+}
