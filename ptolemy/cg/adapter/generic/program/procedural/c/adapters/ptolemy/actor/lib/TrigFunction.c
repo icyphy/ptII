702fef597d124f63c362cd512f30ea1426306b9b
cxh <cxh>
2012-03-30 03:07:49 +0000

    Added support for generating C code for the TrigFunction actor in cg.

---
+++ ptolemy/cg/adapter/generic/program/procedural/c/adapters/ptolemy/actor/lib/TrigFunction.c
@@ -0,0 +1,29 @@
+/***sinBlock***/
+$put(output, sin($get(input)));
+/**/
+
+
+/***asinBlock***/
+$put(output, asin($get(input)));
+/**/
+
+
+/***cosBlock***/
+$put(output, cos($get(input)));
+/**/
+
+
+/***acosBlock***/
+$put(output, acos($get(input)));
+/**/
+
+
+/***tanBlock***/
+$put(output, tan($get(input)));
+/**/
+
+
+/***atanBlock***/
+$put(output, atan($get(input)));
+/**/
+
