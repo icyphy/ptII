5705bd434fb46c9d0a1e898c8bd337e85beb5083
cxh <cxh>
2012-04-02 22:40:48 +0000

    Exclude ptolemy/actor/lib/opencv, it needs work:  1) Update to a recent version of OpenCV.  2)Create an OpenCVToken, see /Users/cxh/ptII/doc/ptIIfaq.htm#AddAType for details.  3) Demos and tests for each actor.

--- adm/gen-9.0/src.ex
+++ adm/gen-9.0/src.ex
@@ -145,6 +145,7 @@ ptolemy/actor/lib/javasound/test/pitchshift
 ptolemy/actor/lib/javasound/test/outfile.wav
 ptolemy/actor/lib/jspaces
 ptolemy/actor/lib/net/test_auto
+ptolemy/actor/lib/opencv
 ptolemy/actor/lib/python/demo/HelloWorld
 ptolemy/actor/lib/python/demo/NotifyFailedTest
 ptolemy/actor/lib/security/test/auto/foo.keystore
