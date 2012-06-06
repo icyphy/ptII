07dd8f0a94371cfd2aabe741827206de71cfc950
cxh <cxh>
2012-06-06 21:40:09 +0000

    Added FIXMEs for FindBugs Negating the result of compareTo()/compare().

--- ptolemy/domains/ptera/kernel/Event.java
+++ ptolemy/domains/ptera/kernel/Event.java
@@ -570,11 +570,40 @@ public class Event extends State implements Initializable {
                         String name2 = relation2.destinationState().getName();
                         int eventCompare = name1.compareTo(name2);
                         if (eventCompare != 0) {
+                            // FIXME: FindBugs: "RV: Negating the result of
+                            // compareTo()/compare()
+                            // (RV_NEGATING_RESULT_OF_COMPARETO)
+                            //
+                            // "This code negatives the return value
+                            // of a compareTo or compare method. This
+                            // is a questionable or bad programming
+                            // practice, since if the return value is
+                            // Integer.MIN_VALUE, negating the return
+                            // value won't negate the sign of the
+                            // result. You can achieve the same
+                            // intended result by reversing the order
+                            // of the operands rather than by negating
+                            // the results."
+
                             return lifo ? -eventCompare : eventCompare;
                         } else {
                             name1 = relation1.getName();
                             name2 = relation2.getName();
                             int relationCompare = name1.compareTo(name2);
+                            // FIXME: FindBugs: "RV: Negating the result of
+                            // compareTo()/compare()
+                            // (RV_NEGATING_RESULT_OF_COMPARETO)
+                            //
+                            // "This code negatives the return value
+                            // of a compareTo or compare method. This
+                            // is a questionable or bad programming
+                            // practice, since if the return value is
+                            // Integer.MIN_VALUE, negating the return
+                            // value won't negate the sign of the
+                            // result. You can achieve the same
+                            // intended result by reversing the order
+                            // of the operands rather than by negating
+                            // the results."
                             return lifo ? -relationCompare : relationCompare;
                         }
                     }
--- ptolemy/domains/ptera/kernel/PteraDirector.java
+++ ptolemy/domains/ptera/kernel/PteraDirector.java
@@ -898,6 +898,20 @@ public class PteraDirector extends Director implements ValueListener {
                 public int compare(Event event1, Event event2) {
                     int eventCompare = event1.getName().compareTo(
                             event2.getName());
+                    // FIXME: FindBugs: "RV: Negating the result of
+                    // compareTo()/compare()
+                    // (RV_NEGATING_RESULT_OF_COMPARETO)
+                    //
+                    // "This code negatives the return value
+                    // of a compareTo or compare method. This
+                    // is a questionable or bad programming
+                    // practice, since if the return value is
+                    // Integer.MIN_VALUE, negating the return
+                    // value won't negate the sign of the
+                    // result. You can achieve the same
+                    // intended result by reversing the order
+                    // of the operands rather than by negating
+                    // the results."
                     return lifo ? -eventCompare : eventCompare;
                 }
             });
