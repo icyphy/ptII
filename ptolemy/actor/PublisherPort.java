c8a3bf0883bc02bdfa00a4e65583fe891cb6e71a
eal <eal>
2012-06-20 10:09:02 +0000

    interrim checkin... adding classes not used yet

---
+++ ptolemy/actor/PublisherPort.java
@@ -0,0 +1,328 @@
+/* An output port that publishes its data on a named channel.
+
+ Copyright (c) 1997-2011 The Regents of the University of California.
+ All rights reserved.
+ Permission is hereby granted, without written agreement and without
+ license or royalty fees, to use, copy, modify, and distribute this
+ software and its documentation for any purpose, provided that the above
+ copyright notice and the following two paragraphs appear in all copies
+ of this software.
+
+ IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+ FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+ ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+ THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+ SUCH DAMAGE.
+
+ THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+ INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+ PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+ CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ ENHANCEMENTS, OR MODIFICATIONS.
+
+ PT_COPYRIGHT_VERSION_2
+ COPYRIGHTENDKEY
+
+ Review vectorized methods.
+ Review broadcast/get/send/hasRoom/hasToken.
+ Review setInput/setOutput/setMultiport.
+ Review isKnown/broadcastClear/sendClear.
+ createReceivers creates inside receivers based solely on insideWidth, and
+ outsideReceivers based solely on outside width.
+ connectionsChanged: no longer validates the attributes of this port.  This is
+ now done in Manager.initialize().
+ Review sendInside, getInside, getWidthInside, transferInputs/Outputs, etc.
+ */
+package ptolemy.actor;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import ptolemy.data.BooleanToken;
+import ptolemy.data.expr.Parameter;
+import ptolemy.data.expr.StringParameter;
+import ptolemy.data.type.BaseType;
+import ptolemy.kernel.ComponentEntity;
+import ptolemy.kernel.InstantiableNamedObj;
+import ptolemy.kernel.util.Attribute;
+import ptolemy.kernel.util.IllegalActionException;
+import ptolemy.kernel.util.KernelException;
+import ptolemy.kernel.util.NameDuplicationException;
+import ptolemy.kernel.util.NamedObj;
+
+///////////////////////////////////////////////////////////////////
+//// PublisherPort
+
+/**
+ This is a specialized output port that publishes data sent through it on
+ the specified named channel.
+
+ @author Edward A. Lee
+ @version $Id$
+ @since Ptolemy II 9.0
+ @Pt.ProposedRating Yellow (eal)
+ @Pt.AcceptedRating Red (eal)
+ */
+public class PublisherPort extends TypedIOPort {
+
+    /** Construct an IOPort with a containing actor and a name
+     *  that is neither an input nor an output.  The specified container
+     *  must implement the Actor interface, or an exception will be thrown.
+     *
+     *  @param container The container actor.
+     *  @param name The name of the port.
+     *  @exception IllegalActionException If the port is not of an acceptable
+     *   class for the container, or if the container does not implement the
+     *   Actor interface.
+     *  @exception NameDuplicationException If the name coincides with
+     *   a port already in the container.
+     */
+    public PublisherPort(ComponentEntity container, String name)
+            throws IllegalActionException, NameDuplicationException {
+        super(container, name);
+        channel = new StringParameter(this, "channel");
+        
+        global = new Parameter(this, "global");
+        global.setTypeEquals(BaseType.BOOLEAN);
+        global.setExpression("false");
+        
+        propagateNameChanges = new Parameter(this, "propagateNameChanges");
+        propagateNameChanges.setExpression("false");
+        propagateNameChanges.setTypeEquals(BaseType.BOOLEAN);
+        
+        setOutput(true);
+        setInput(false);
+        // FIXME: Should disable making changes to the above.
+        
+        // FIXME: If the container of this port is set to null, have to handle as in the
+        // Publisher actor.
+        
+        // FIXME: if you also wire something to this port, then the
+        // port will be required to be a multiport, and it will not be
+        // clear which channel goes where!
+    }
+    
+    ///////////////////////////////////////////////////////////////////
+    ////                         parameters                        ////
+
+    /** Specification of whether the published data is global.
+     *  This is ignored if {@link #channel} is empty.
+     *  If this is set to true, then a subscriber anywhere in the model that
+     *  references the same channel by name will see values published by
+     *  this port. If this is set to false (the default), then only
+     *  those subscribers that are fired by the same director will see
+     *  values published on this channel.
+     */
+    public Parameter global;
+
+    /** If set, then this port is used to communicate over a named
+     *  publish and subscribe channel, rather than over manually
+     *  established connections.
+     */
+    public StringParameter channel;
+
+    /** If true, then propagate channel name changes to any
+     *  Subscribers.  The default value is a BooleanToken with the
+     *  value false, indicating that if the channel name is changed,
+     *  then the channel names of the Subscribers are not changed.  If
+     *  the value is true, then if the channel name is changed, the
+     *  channel names of the connected Subscribers are updated.
+     *
+     *  <p>If the value is true, then SubscriptionAggregators that
+     *  have the same regular expression as the channel name of the
+     *  Publisher will be updated.  However, SubscriptionAggregators
+     *  usually have regular expressions as channel names, so usually
+     *  the channel name of the SubscriptionAggregator will <b>not</b>
+     *  be updated.</p>
+     *
+     *  <p>Note that if a Publisher is within an Actor Oriented Class
+     *  definition, then any Subscribers with the same channel name in
+     *  Actor Oriented Class definitions will <b>not</b> be updated.
+     *  This is because there is no connection between the Publisher
+     *  in the Actor Oriented Class definition and the Subscriber.
+     *  However, if the channel name in a Publisher in an instance of
+     *  an Actor Oriented Class is updated, then the
+     *  corresponding Subscribers in instances of Actor Oriented Class
+     *  will be updated.</p>
+     */
+    public Parameter propagateNameChanges;
+
+    ///////////////////////////////////////////////////////////////////
+    ////                         public methods                    ////
+
+    /** If a publish and subscribe channel is set, then set up the connections.
+     *  If a quantity manager is added, removed or modified update the list of
+     *  quantity managers.
+     *  @param attribute The attribute that changed.
+     *  @exception IllegalActionException Thrown if the new color attribute cannot
+     *      be created.
+     */
+    public void attributeChanged(Attribute attribute)
+            throws IllegalActionException {
+        if (attribute == channel || attribute == global) {
+            // We only get the value if we are not in a class definition.
+            // Class definitions need not have connections.
+            // The connections will be made in the instances.
+            // The reason is that some of the Actor Oriented Classes
+            // that use Publishers do not have the parameter defined
+            // in the definition.  See
+            // ptolemy/actor/lib/test/auto/PublisherClassNoParameter.xml
+            NamedObj immediateContainer = getContainer();
+            if (immediateContainer != null) {
+                NamedObj container = immediateContainer.getContainer();
+                if (container instanceof InstantiableNamedObj
+                        && !((InstantiableNamedObj)container).isWithinClassDefinition()) {
+                    String newValue = channel.stringValue();
+                    boolean globalValue = ((BooleanToken) global.getToken())
+                            .booleanValue();
+                    if (!newValue.equals(_channel) || globalValue != _global) {
+                        if (container instanceof CompositeActor) {
+                            // The vergil and config tests were failing because
+                            // moml.EntityLibrary sometimes contains Subscribers.
+                            try {
+                                if (attribute == global) {
+                                    if (_global && !globalValue) {
+                                        // Changing from global to non-global.
+                                        ((CompositeActor) container)
+                                        .unregisterPublisherPort(_channel,
+                                                this, true);
+                                    }
+                                }
+
+                                if (attribute == channel
+                                        && (!(_channel == null || _channel.trim()
+                                        .equals("")))) {
+                                    // Setting 
+                                    if (((BooleanToken) propagateNameChanges
+                                            .getToken()).booleanValue()) {
+                                        try {
+                                            _updateChannelNameOfConnectedSubscribers(
+                                                    _channel, newValue);
+                                        } catch (KernelException ex) {
+                                            throw new IllegalActionException(this, ex,
+                                                    "Failed to set channel to "
+                                                            + newValue);
+                                        }
+                                    }
+                                }
+                                ((CompositeActor) container).registerPublisherPort(
+                                        newValue, this, globalValue);
+
+                                if (attribute == channel
+                                        && (!(_channel == null || _channel.trim()
+                                        .equals("")))) {
+                                    ((CompositeActor) container)
+                                    .unregisterPublisherPort(_channel,
+                                            this, _global);
+                                }
+
+                            } catch (NameDuplicationException e) {
+                                throw new IllegalActionException(this, e,
+                                        "Can't add published port.");
+                            }
+                        }
+                        _channel = newValue;
+                        _global = globalValue;
+                    }
+                }
+            }
+        } else {
+            super.attributeChanged(attribute);
+        }
+    }
+    
+    /** Return a Set of SubscriberPort that are connected to this Publisher.
+     *  @return A Set of Subscribers that are connected to this Publisher
+     *  @exception KernelException If thrown when a Manager is added to
+     *  the top level or if preinitialize() fails.
+     */
+    public Set<SubscriberPort> subscribers()
+            throws KernelException {
+        // preinitialize() creates connections between Publishers and
+        // Subscribers.
+        Manager.preinitializeThenWrapup((Actor)getContainer());
+        return _dependents(this);
+    }
+
+    ///////////////////////////////////////////////////////////////////
+    ////                         protected variables               ////
+
+    /** Cached channel name, for publish and subscribe. */
+    protected String _channel;
+
+    /** Cached variable indicating whether publishing or subscribing is global. */
+    protected boolean _global;
+
+    ///////////////////////////////////////////////////////////////////
+    ////                         private methods                   ////
+
+    /** If the specified port is an instance of
+     *  {@link SubscriberPort}, then return a set containing it;
+     *  otherwise, return a set of SubscriberPort instances that downstream
+     *  of the specified port that subscribe to this publisher.
+     *  This method traverses opaque composites.   
+     *  @param port The port to be checked   
+     *  @param filter The class of dependent actors to be returned.
+     *  @return The Set of all AtomicActors connected to the port.
+     */
+    private Set<SubscriberPort> _dependents(IOPort port) 
+            throws IllegalActionException {
+        //System.out.println("ActorDependencies._dependents: START" + remotePort.getFullName());
+        Set<SubscriberPort> results = new HashSet<SubscriberPort>();
+        if (port instanceof SubscriberPort) {
+            results.add((SubscriberPort)port);
+        } else {
+            Receiver[][] receivers = null;
+            if (port.isOutput() && port.isInput()) {
+                throw new IllegalActionException(port, "Can't handle port that is both input and output.");
+            }
+            if (port.isOutput()) {
+                receivers = port.getRemoteReceivers();
+            } else if (port.isInput()) {
+                receivers = port.deepGetReceivers();
+            } else {
+                throw new IllegalActionException(port, "Can't handle port that is neither input nor output.");
+            }
+            if (receivers != null) {
+                for (int i = 0; i < receivers.length; i++) {
+                    if (receivers[i] != null) {
+                        for (int j = 0; j < receivers[i].length; j++) {
+                            if (receivers[i][j] != null) {
+                                IOPort remotePort2 = receivers[i][j].getContainer();
+                                if (remotePort2 != null) {
+                                    results.addAll(_dependents(remotePort2));
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return results;
+    }
+
+    /** Update the channel name of any connected SubscriberPorts.
+     *  Note that the channel name of connected SubscriptionAggregatorPorts
+     *  are not updated.
+     *  @param previousChannelName The previous name of the channel.
+     *  @param newChannelName The new name of the channel.
+     *  @exception KernelException If thrown when a Manager is added to
+     *  the top level, or when the channel name of a Subscriber is changed.
+     */
+    private void _updateChannelNameOfConnectedSubscribers(
+            String previousChannelName, String newChannelName)
+            throws KernelException {
+
+        // We use subscribers() here so that we get any subscribers in
+        // Opaque TypedCompositeActors.
+        for (SubscriberPort port : subscribers()) {
+            if (port.channel.getExpression().equals(previousChannelName)) {
+                // Avoid updating SubscriptionAggregators that have regular
+                // expressions that are different than the Publisher channel name.
+                port.channel.setExpression(newChannelName);
+                port.attributeChanged(port.channel);
+            }
+        }
+    }
+}
---
+++ ptolemy/actor/SubscriberPort.java
@@ -0,0 +1,261 @@
+/* An output port that publishes its data on a named channel.
+
+ Copyright (c) 1997-2011 The Regents of the University of California.
+ All rights reserved.
+ Permission is hereby granted, without written agreement and without
+ license or royalty fees, to use, copy, modify, and distribute this
+ software and its documentation for any purpose, provided that the above
+ copyright notice and the following two paragraphs appear in all copies
+ of this software.
+
+ IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+ FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+ ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+ THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+ SUCH DAMAGE.
+
+ THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+ INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+ PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+ CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ ENHANCEMENTS, OR MODIFICATIONS.
+
+ PT_COPYRIGHT_VERSION_2
+ COPYRIGHTENDKEY
+
+ Review vectorized methods.
+ Review broadcast/get/send/hasRoom/hasToken.
+ Review setInput/setOutput/setMultiport.
+ Review isKnown/broadcastClear/sendClear.
+ createReceivers creates inside receivers based solely on insideWidth, and
+ outsideReceivers based solely on outside width.
+ connectionsChanged: no longer validates the attributes of this port.  This is
+ now done in Manager.initialize().
+ Review sendInside, getInside, getWidthInside, transferInputs/Outputs, etc.
+ */
+package ptolemy.actor;
+
+import ptolemy.data.BooleanToken;
+import ptolemy.data.expr.Parameter;
+import ptolemy.data.expr.StringParameter;
+import ptolemy.data.type.BaseType;
+import ptolemy.kernel.ComponentEntity;
+import ptolemy.kernel.Entity;
+import ptolemy.kernel.util.Attribute;
+import ptolemy.kernel.util.IllegalActionException;
+import ptolemy.kernel.util.InternalErrorException;
+import ptolemy.kernel.util.NameDuplicationException;
+import ptolemy.kernel.util.NamedObj;
+
+///////////////////////////////////////////////////////////////////
+//// SubscriberPort
+
+/**
+ This is a specialized output port that publishes data sent through it on
+ the specified named channel.
+
+ @author Edward A. Lee
+ @version $Id$
+ @since Ptolemy II 9.0
+ @Pt.ProposedRating Yellow (eal)
+ @Pt.AcceptedRating Red (eal)
+ */
+public class SubscriberPort extends TypedIOPort implements Initializable {
+
+    /** Construct an IOPort with a containing actor and a name
+     *  that is neither an input nor an output.  The specified container
+     *  must implement the Actor interface, or an exception will be thrown.
+     *
+     *  @param container The container actor.
+     *  @param name The name of the port.
+     *  @exception IllegalActionException If the port is not of an acceptable
+     *   class for the container, or if the container does not implement the
+     *   Actor interface.
+     *  @exception NameDuplicationException If the name coincides with
+     *   a port already in the container.
+     */
+    public SubscriberPort(ComponentEntity container, String name)
+            throws IllegalActionException, NameDuplicationException {
+        super(container, name);
+        channel = new StringParameter(this, "channel");
+        
+        global = new Parameter(this, "global");
+        global.setTypeEquals(BaseType.BOOLEAN);
+        global.setExpression("false");
+        
+        setOutput(false);
+        setInput(true);
+        // FIXME: Should disable making changes to the above.
+        
+        // FIXME: If the container of this port is set to null, have to handle as in the
+        // Publisher actor.
+        
+        // FIXME: if you also wire something to this port, then the
+        // port will be required to be a multiport, and it will not be
+        // clear which channel goes where!
+    }
+    
+    ///////////////////////////////////////////////////////////////////
+    ////                         parameters                        ////
+
+    /** Specification of whether the published data is global.
+     *  This is ignored if {@link #channel} is empty.
+     *  If this is set to true, then a subscriber anywhere in the model that
+     *  references the same channel by name will see values published by
+     *  this port. If this is set to false (the default), then only
+     *  those subscribers that are fired by the same director will see
+     *  values published on this channel.
+     */
+    public Parameter global;
+
+    /** If set, then this port is used to communicate over a named
+     *  publish and subscribe channel, rather than over manually
+     *  established connections.
+     */
+    public StringParameter channel;
+
+    ///////////////////////////////////////////////////////////////////
+    ////                         public methods                    ////
+
+    /** Throw an exception. Add initializables to the container. */
+    @Override
+    public void addInitializable(Initializable initializable) {
+        throw new InternalErrorException("Cannot add Initializables to SubscriberPort.");
+    }
+
+    /** If a publish and subscribe channel is set, then set up the connections.
+     *  If a quantity manager is added, removed or modified update the list of
+     *  quantity managers.
+     *  @param attribute The attribute that changed.
+     *  @exception IllegalActionException Thrown if the new color attribute cannot
+     *      be created.
+     */
+    public void attributeChanged(Attribute attribute)
+            throws IllegalActionException {
+        if (attribute == channel) {
+            String newValue = channel.stringValue();
+            if (!newValue.equals(_channel)) {
+                NamedObj immediateContainer = getContainer();
+                if (immediateContainer != null) {
+                    NamedObj container = immediateContainer.getContainer();
+                    if (container instanceof CompositeActor
+                            && !(_channel == null || _channel.trim().equals(""))) {
+                        ((CompositeActor) container).unlinkToPublishedPort(
+                                _channel, this, _global);
+                    }
+                    _channel = newValue;
+                }
+            }
+        } else if (attribute == global) {
+            _global = ((BooleanToken) global.getToken()).booleanValue();
+            NamedObj immediateContainer = getContainer();
+            if (immediateContainer != null) {
+                NamedObj container = immediateContainer.getContainer();
+                if (!_global && container instanceof CompositeActor) {
+                    // The vergil and config tests were failing because
+                    // moml.EntityLibrary sometimes contains Subscribers.
+                    ((CompositeActor) container).unlinkToPublishedPort(
+                            _channel, this, false);
+                }
+            }
+        } else {
+            super.attributeChanged(attribute);
+        }
+    }
+    
+    /** Do nothing. */
+    @Override
+    public void initialize() throws IllegalActionException {
+    }
+
+    /** Override the base class to ensure that there is a publisher.
+     *  @exception IllegalActionException If there is no matching
+     *   publisher.
+     */
+    public void preinitialize() throws IllegalActionException {
+        if (_channel == null) {
+            throw new IllegalActionException(this, "No channel specified.");
+        }
+        _updateLinks();
+    }
+
+    /** Do nothing. */
+    @Override
+    public void removeInitializable(Initializable initializable) {
+    }
+
+    /** Override the base class to register as an Initializable
+     *  so that preinitialize() is invoked.
+     *  @param container The proposed container.
+     *  @exception IllegalActionException If the action would result in a
+     *   recursive containment structure, or if
+     *   this entity and container are not in the same workspace.
+     *  @exception NameDuplicationException If the container already has
+     *   an entity with the name of this entity.
+     */
+    @Override
+    public void setContainer(Entity container)
+            throws IllegalActionException, NameDuplicationException {
+        NamedObj previousContainer = super.getContainer();
+        if (previousContainer != container) {
+            if (previousContainer instanceof Initializable) {
+                ((Initializable)previousContainer).removeInitializable(this);
+            }
+            if (container instanceof Initializable) {
+                ((Initializable)container).addInitializable(this);
+            }
+        }
+        super.setContainer(container);
+    }
+
+    /** Do nothing. */
+    @Override
+    public void wrapup() throws IllegalActionException {
+    }
+
+    ///////////////////////////////////////////////////////////////////
+    ////                         protected methods                 ////
+
+    /** Update the connection to the publisher, if there is one.
+     *  Note that this method is computationally intensive for large
+     *  models as it traverses the model by searching
+     *  up the hierarchy for the nearest opaque container
+     *  or the top level and then traverses the contained entities.
+     *  Thus, avoid calling this method except when the model
+     *  is running.
+     *  @exception IllegalActionException If creating the link
+     *   triggers an exception.
+     */
+    protected void _updateLinks() throws IllegalActionException {
+        // If the channel has not been set, then there is nothing
+        // to do.  This is probably the first setContainer() call,
+        // before the object is fully constructed.
+        if (_channel == null) {
+            return;
+        }
+        NamedObj immediateContainer = getContainer();
+        if (immediateContainer != null) {
+            NamedObj container = immediateContainer.getContainer();
+            if (container instanceof CompositeActor) {
+                try {
+                    ((CompositeActor) container).linkToPublishedPort(_channel,
+                            this, _global);
+                } catch (Exception e) {
+                    throw new IllegalActionException(this, e,
+                            "Can't link SubscriberPort with PublisherPort, channel was \""
+                                    + channel.stringValue() + "\"");
+                }
+            }
+        }
+    }
+
+    ///////////////////////////////////////////////////////////////////
+    ////                         protected variables               ////
+
+    /** Cached channel name, for publish and subscribe. */
+    protected String _channel;
+
+    /** Cached variable indicating whether publishing or subscribing is global. */
+    protected boolean _global;
+}
