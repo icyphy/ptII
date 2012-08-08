514ceb848702b30888f29237488e87b74fa82398
cxh <cxh>
2012-08-08 00:22:48 +0000

    getCompositeModel(): Check for a null argument.

--- ptolemy/vergil/basic/AbstractBasicGraphModel.java
+++ ptolemy/vergil/basic/AbstractBasicGraphModel.java
@@ -180,7 +180,8 @@ public abstract class AbstractBasicGraphModel extends ModularGraphModel
      *   object of this graph model.  Otherwise return null.
      */
     public CompositeModel getCompositeModel(Object composite) {
-        if (composite.equals(_composite)) {
+        if (composite != null 
+                && composite.equals(_composite)) {
             return _compositeModel;
         } else {
             return null;
