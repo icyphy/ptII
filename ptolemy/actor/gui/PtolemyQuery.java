1eb539605688e13a6ae21f8b3849f69346a22137
cxh <cxh>
2012-08-16 14:01:19 +0000

    typo

--- ptolemy/actor/gui/PtolemyQuery.java
+++ ptolemy/actor/gui/PtolemyQuery.java
@@ -469,7 +469,7 @@ public class PtolemyQuery extends Query implements QueryListener,
                             area.setRows(area.getRows() + 1);
                             area.revalidate();
                             Component parent = area.getParent();
-                            while ((parent != null) && !(parent instanceof EditParametersDialog)) {
+                            while ((parent != null) && !(parent instanceof EditParameterDialog)) {
                                 parent = parent.getParent();
                             }
                             if (parent instanceof EditParameterDialog) {
