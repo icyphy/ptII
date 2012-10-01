a344b8d472d7b1a6812d59324ef700d26ae859c5
eal <eal>
2012-10-01 21:32:52 +0000

    Starting point for C code generator for Display actor

---
+++ ptolemy/cg/adapter/generic/program/procedural/c/adapters/ptolemy/actor/lib/gui/Display.c
@@ -0,0 +1,11 @@
+/***IntFireBlock***/
+if ($hasToken(input)) {
+        printf("%d\n", $get(input));
+}
+/**/
+
+/***TokenFireBlock***/
+if ($hasToken(input)) {
+        /* printf("%s\n", tokenToString($get(input))); */
+}
+/**/
---
+++ ptolemy/cg/adapter/generic/program/procedural/c/adapters/ptolemy/actor/lib/gui/Display.java
@@ -0,0 +1,75 @@
+/* An adapter class for ptolemy.actor.lib.gui.Display
+
+ Copyright (c) 2006-2011 The Regents of the University of California.
+ All rights reserved.
+ Permission is hereby granted, without written agreement and without
+ license or royalty fees, to use, copy, modify, and distribute this
+ software and its documentation for any purpose, provided that the above
+ copyright notice and the following two paragraphs appear in all copies
+ of this software.
+
+ IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+ FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+ ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+ THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+ SUCH DAMAGE.
+
+ THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+ INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+ PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+ CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ ENHANCEMENTS, OR MODIFICATIONS.
+
+ PT_COPYRIGHT_VERSION_2
+ COPYRIGHTENDKEY
+
+ */
+package ptolemy.cg.adapter.generic.program.procedural.c.adapters.ptolemy.actor.lib.gui;
+
+import ptolemy.cg.kernel.generic.program.CodeStream;
+import ptolemy.cg.kernel.generic.program.NamedProgramCodeGeneratorAdapter;
+import ptolemy.kernel.util.IllegalActionException;
+
+//////////////////////////////////////////////////////////////////////////
+//// Display
+
+/**
+ An adapter class for ptolemy.actor.lib.gui.Display.
+
+ @author Hokeun Kim and Edward Lee
+ @version $Id$
+ @since Ptolemy II 8.0
+ @Pt.ProposedRating Red (eal)
+ @Pt.AcceptedRating Red (eal)
+ */
+public class Display extends NamedProgramCodeGeneratorAdapter {
+    /**
+     *  Construct the Display adapter.
+     *  @param actor the associated actor
+     */
+    public Display(ptolemy.actor.lib.gui.Display actor) {
+        super(actor);
+    }
+
+    /**
+     * Generate fire code for the Display actor.
+     * @return The generated code.
+     * @exception IllegalActionException If the code stream encounters an
+     *  error in processing the specified code block(s).
+     */
+    protected String _generateFireCode() throws IllegalActionException {
+        super._generateFireCode();
+
+        ptolemy.actor.lib.gui.Display actor = (ptolemy.actor.lib.gui.Display) getComponent();
+
+        String type = getCodeGenerator().codeGenType(actor.input.getType());
+        if (!getCodeGenerator().isPrimitive(type)) {
+            type = "Token";
+        }
+
+        CodeStream codeStream = _templateParser.getCodeStream();
+        codeStream.appendCodeBlock(type + "FireBlock");
+        return processCode(codeStream.toString());
+    }
+}
