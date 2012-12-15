464b0540c84f7998dbb2198945a7891d808122e1
cxh <cxh>
2012-12-15 17:28:20 +0000

    Added comments and before and after results.

--- ptolemy/data/expr/test/Parameter.tcl
+++ ptolemy/data/expr/test/Parameter.tcl
@@ -773,42 +773,80 @@ test Parameter-18.1 {Renaming a parameter} {
 } {p3}
 
 test Parameter-18.2 {Renaming a referenced parameter in quotes} {
+    
+    # See " references to parameter are not always renamed"
+    # http://bugzilla.ecoinformatics.org/show_bug.cgi?id=5723
+    
+    # This tests parameter substituion inside double quotes.
+    # The currently documented behavior is likely that 
+    # parameter substitution does not occur within double quotes.
+
     set e1 [java::new ptolemy.kernel.Entity]
     set p1 [java::new ptolemy.data.expr.Parameter $e1 "p1"]
     set p2 [java::new ptolemy.data.expr.Parameter $e1 "p2"]
     $p1 setExpression {"$p2"}
     $p1 validate
+    set r1 [$p1 getExpression]
     set change "<property name=\"p2\">
 <rename name=\"p3\"/>
 </property>"
     $e1 requestChange [java::new ptolemy.moml.MoMLChangeRequest $e1 $e1 $change]
-    $p1 getExpression
-} {"$p3"}
+    set r2 [$p1 getExpression]
+    list $r1 $r2
+} {{"$p2"} {"$p3"}}
 
 test Parameter-18.3 {Renaming a parameter referenced by a parameter in string mode} {
+
+    # See " references to parameter are not always renamed"
+    # http://bugzilla.ecoinformatics.org/show_bug.cgi?id=5723
+
+    # This tests parameter substitution in string mode
+    # The currently documented behavior of string mode in Variable.java: 
+
+    #  "If the variable is in string mode, then when setting the value
+    # of this variable, the string that you pass to
+    # setExpression(String) is taken to be literally the value of the
+    # instance of StringToken that represents the value of this
+    # parameter. It is not necessary"
+
     set e1 [java::new ptolemy.kernel.Entity]
     set p1 [java::new ptolemy.data.expr.Parameter $e1 "p1"]
     set p2 [java::new ptolemy.data.expr.Parameter $e1 "p2"]
     $p1 setStringMode true
     $p1 setExpression {$p2}
     $p1 validate
+    set r1 [$p1 getExpression]
     set change "<property name=\"p2\">
 <rename name=\"p3\"/>
 </property>"
     $e1 requestChange [java::new ptolemy.moml.MoMLChangeRequest $e1 $e1 $change]
-    $p1 getExpression
-} {$p3}
+    set r2 [$p1 getExpression]
+    list $r1 $r2
+} {{$p2} {$p3}}
 
 test Parameter-18.4 {Renaming a parameter in quotes referenced by a parameter in string mode} {
+    # See " references to parameter are not always renamed"
+    # http://bugzilla.ecoinformatics.org/show_bug.cgi?id=5723
+
+    # This tests parameter substitution in string mode
+    # The currently documented behavior of string mode in Variable.java: 
+
+    #  "If the variable is in string mode, then when setting the value
+    # of this variable, the string that you pass to
+    # setExpression(String) is taken to be literally the value of the
+    # instance of StringToken that represents the value of this
+    # parameter. It is not necessary"
+
     set e1 [java::new ptolemy.kernel.Entity]
     set p1 [java::new ptolemy.data.expr.Parameter $e1 "p1"]
     set p2 [java::new ptolemy.data.expr.Parameter $e1 "p2"]
     $p1 setStringMode true
     $p1 setExpression {"$p2"}
     $p1 validate
+    set r1 [$p1 getExpression]
     set change "<property name=\"p2\">
 <rename name=\"p3\"/>
 </property>"
     $e1 requestChange [java::new ptolemy.moml.MoMLChangeRequest $e1 $e1 $change]
-    $p1 getExpression
-} {"$p3"}
+    list $r1 [$p1 getExpression]
+} {{"$p2"} {"$p3"}}
