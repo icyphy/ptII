51720b785920db8d2735af3a173d80cb1f1d5a2c
cxh <cxh>
2013-03-18 02:47:00 +0000

    Start on porting the fmusdk fmi-1.0 to fmi-2.0

---
+++ ptolemy/actor/lib/fmi/fmus/fmusim2/build_fmu.bat
@@ -0,0 +1,113 @@
+rem @echo off 
+rem ------------------------------------------------------------
+rem This batch builds an FMU of the FMU SDK
+rem Usage: build_fmu  <fmu_dir_name> 
+rem Based on (c) 2011 QTronic GmbH
+
+rem FMU SDK license 
+
+rem Copyright © 2008-2011, QTronic GmbH. All rights reserved.
+rem The FmuSdk is licensed by the copyright holder under the BSD License
+rem (http://www.opensource.org/licenses/bsd-license.html):
+rem Redistribution and use in source and binary forms, with or without
+rem modification, are permitted provided that the following conditions are met:
+rem - Redistributions of source code must retain the above copyright notice,
+rem   this list of conditions and the following disclaimer.
+rem - Redistributions in binary form must reproduce the above copyright notice,
+rem   this list of conditions and the following disclaimer in the documentation
+rem   and/or other materials provided with the distribution.
+
+rem THIS SOFTWARE IS PROVIDED BY QTRONIC GMBH "AS IS" AND ANY EXPRESS OR 
+rem IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES 
+rem OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
+rem IN NO EVENT SHALL QTRONIC GMBH BE LIABLE FOR ANY DIRECT, INDIRECT, 
+rem INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+rem NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, 
+rem DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY 
+rem THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
+rem (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF 
+rem THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+rem This license is also present in org/ptolemy/fmi/driver/fmusdk-license.htm
+rem ------------------------------------------------------------
+
+echo -----------------------------------------------------------
+echo building FMU %1 - FMI for Co-Simulation 1.0
+
+rem save env variable settings
+set PREV_PATH=%PATH%
+if defined INCLUDE set PREV_INCLUDE=%INCLUDE%
+if defined LIB     set PREV_LIB=%LIB%
+if defined LIBPATH set PREV_LIBPATH=%LIBPATH%
+
+rem setup the compiler
+if defined VS110COMNTOOLS (call "%VS110COMNTOOLS%\vsvars32.bat") else ^
+if defined VS100COMNTOOLS (call "%VS100COMNTOOLS%\vsvars32.bat") else ^
+if defined VS90COMNTOOLS (call "%VS90COMNTOOLS%\vsvars32.bat") else ^
+if defined VS80COMNTOOLS (call "%VS80COMNTOOLS%\vsvars32.bat") else ^
+goto noCompiler
+
+rem create the %1.dll in the temp dir
+if not exist temp mkdir temp 
+pushd temp
+if exist *.dll del /Q *.dll
+
+rem /wd4090 disables warnings about different 'const' qualifiers
+
+rem cl /LD /wd4090 /nologo "/DFMIAPI=__declspec(dllexport)" ..\%1.c /I ..\.
+cl /LD /wd4090 /nologo ..\%1.c /I ..\.
+dumpbin /exports %1.dll
+if not exist %1.dll goto compileError
+
+rem create FMU dir structure with root 'fmu'
+set BIN_DIR=fmu\binaries\win32
+set SRC_DIR=fmu\sources
+set DOC_DIR=fmu\documentation
+if not exist %BIN_DIR% mkdir %BIN_DIR%
+if not exist %SRC_DIR% mkdir %SRC_DIR%
+if not exist %DOC_DIR% mkdir %DOC_DIR%
+move /Y %1.dll %BIN_DIR%
+if exist ..\%1\*~ del /Q ..\%1\*~
+type ..\..\modelDescription.xml > fmu\modelDescription.xml
+copy ..\..\model.png fmu
+copy ..\*.c %SRC_DIR%
+copy ..\*.h %SRC_DIR%
+copy ..\build_fmu.bat %SRC_DIR%
+copy ..\build_fmu %SRC_DIR%
+copy ..\makefile %SRC_DIR%
+copy ..\..\documentation\*.html %DOC_DIR%
+copy ..\..\documentation\*.png  %DOC_DIR%
+rem del %DOC_DIR%\model.png 
+
+rem If the 7z.exe binary is found, then
+rem zip the directory tree and move to fmu directory 
+for %%X in (7z.exe) do (set FOUND=%%~$PATH:X)
+if defined FOUND (
+  cd fmu
+  set FMU_FILE=..\..\..\..\%1.fmu
+  if exist %ZIP_FILE% del %FMU_FILE%
+  7z.exe a -tzip -xr!.svn %FMU_FILE% ^
+  modelDescription.xml model.png binaries sources documentation
+  goto cleanup
+) else (
+  echo Warning: Not building the .fmu file because 7z.exe is not found
+  echo 7z.exe is available as part of the fmusdk or %PTII%\ptolemy\actor\lib\fmu\fmus\win32\7z.exe
+)
+
+:noCompiler
+echo No Microsoft Visual C compiler found
+exit
+
+:compileError
+echo build of %1 failed
+
+:cleanup
+popd
+if exist temp rmdir /S /Q temp
+
+rem undo variable settings performed by vsvars32.bat
+set PATH=%PREV_PATH%
+if defined PREV_INCLUDE set INCLUDE=%PREV_INCLUDE%
+if defined PREV_LIB     set LIB=%PREV_LIB%
+if defined PREV_LIBPATH set LIBPATH=%PREV_LIBPATH%
+echo done.
---
+++ ptolemy/actor/lib/fmi/fmus/fmusim2/sim_support.h
@@ -0,0 +1,52 @@
+/* ------------------------------------------------------------------------- 
+ * sim_support.h
+ * Functions used by the FMU simulatios fmusim_me and fmusim_cs.
+ * Copyright 2011 QTronic GmbH. All rights reserved. 
+ * -------------------------------------------------------------------------*/ 
+
+// Used 7z options, version 4.57:
+// -x   Extracts files from an archive with their full paths in the current dir, or in an output dir if specified
+// -aoa Overwrite All existing files without prompt
+// -o   Specifies a destination directory where files are to be extracted
+#define UNZIP_CMD "7z x -aoa -o"
+
+#define XML_FILE  "modelDescription.xml"
+
+#if WINDOWS
+#define DLL_DIR   "binaries\\win32\\"
+#define DLL_SUFFIX ".dll"
+#else
+#if __APPLE__
+#define DLL_DIR   "binaries/darwin64/"
+#define DLL_SUFFIX ".dylib"
+#else /*__APPLE__*/
+// Linux
+#ifdef __x86_64
+#define DLL_DIR   "binaries/linux64/"
+#else
+#define DLL_DIR   "binaries/linux32/"
+#endif /*__x86_64*/
+#define DLL_SUFFIX ".so"
+#endif /*__APPLE__*/
+#endif /*WINDOWS*/
+
+#define RESULT_FILE "result.csv"
+#define BUFSIZE 4096
+
+// return codes of the 7z command line tool
+#define SEVEN_ZIP_NO_ERROR 0 // success
+#define SEVEN_ZIP_WARNING 1  // e.g., one or more files were locked during zip
+#define SEVEN_ZIP_ERROR 2
+#define SEVEN_ZIP_COMMAND_LINE_ERROR 7
+#define SEVEN_ZIP_OUT_OF_MEMORY 8
+#define SEVEN_ZIP_STOPPED_BY_USER 255
+
+void fmuLogger(fmiComponent c, fmiString instanceName, fmiStatus status, fmiString category, fmiString message, ...);
+int unzip(const char *zipPath, const char *outPath);
+void parseArguments(int argc, char *argv[], char** fmuFileName, double* tEnd, double* h, int* loggingOn, char* csv_separator);
+void loadFMU(const char* fmuFileName);
+// Windows wants boolean to be an unsigned char.
+typedef unsigned char boolean; 
+void outputRow(FMU *fmu, fmiComponent c, double time, FILE* file, char separator, boolean header);
+int error(const char* message);
+void printHelp(const char* fmusim);
