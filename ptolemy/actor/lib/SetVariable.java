6c23a9544157cd65019b383897d1a9a14fe9e306
derler <derler>
2013-06-05 20:50:36 +0000

    revert changes that cause type conversion problems

--- ptolemy/actor/lib/SetVariable.java
+++ ptolemy/actor/lib/SetVariable.java
@@ -41,7 +41,6 @@ import ptolemy.data.expr.ModelScope;
 import ptolemy.data.expr.Parameter;
 import ptolemy.data.expr.Variable;
 import ptolemy.data.type.BaseType;
-import ptolemy.data.type.Type;
 import ptolemy.graph.Inequality;
 import ptolemy.kernel.CompositeEntity;
 import ptolemy.kernel.Entity;
@@ -432,13 +431,7 @@ public class SetVariable extends TypedAtomicActor implements ChangeListener,
             Token oldToken = ((Variable) variable).getToken();
 
             if (oldToken == null || !oldToken.equals(value)) {
-                // Convert the token to the type of the variable so
-                // that we don't change the type of the variable (the
-                // token might be a subtype). This convert will
-                // succeed because of the type constraint set in
-                // preinitialize().
-                Type type = ((Variable)variable).getType();
-                ((Variable) variable).setToken(type.convert(value));
+                ((Variable) variable).setToken(value);
 
                 // NOTE: If we don't call validate(), then the
                 // change will not propagate to dependents.
