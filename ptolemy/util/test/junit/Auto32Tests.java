27e88e8fcd7ccdf89402a52c9e62a1e573f52779
cxh <cxh>
2013-06-20 20:52:26 +0000

    32-bit JVM test harness.

---
+++ ptolemy/util/test/junit/Auto32Tests.java
@@ -0,0 +1,141 @@
+/* Run the Ptolemy model tests in the auto32/ directory using JUnit.
+
+   Copyright (c) 2013 The Regents of the University of California.
+   All rights reserved.
+   Permission is hereby granted, without written agreement and without
+   license or royalty fees, to use, copy, modify, and distribute this
+   software and its documentation for any purpose, provided that the above
+   copyright notice and the following two paragraphs appear in all copies
+   of this software.
+
+   IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+   FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+   ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+   THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+   SUCH DAMAGE.
+
+   THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+   INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+   MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+   PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+   CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+   ENHANCEMENTS, OR MODIFICATIONS.
+
+   PT_COPYRIGHT_VERSION_2
+   COPYRIGHTENDKEY
+
+*/
+
+package ptolemy.util.test.junit;
+
+import java.io.IOException;
+
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import ptolemy.util.StringUtilities;
+
+///////////////////////////////////////////////////////////////////
+//// Auto32Tests
+/**
+ * Run the Ptolemy model tests in the auto/ directory using JUnit.
+ *
+ * <p>
+ * This test must be run from the directory that contains the auto/ directory,
+ * for example:
+ * </p>
+ *
+ * <pre>
+ * (cd ~/ptII/ptolemy/actor/lib/io/test; java -classpath ${PTII}:${PTII}/lib/junit-4.8.2.jar:${PTII}/lib/JUnitParams-0.3.0.jar org.junit.runner.JUnitCore ptolemy.util.test.junit.AutoTests)
+ * </pre>
+ *
+ * <p>
+ * This test uses JUnitParams from <a
+ * href="http://code.google.com/p/junitparams/#in_browser"
+ * >http://code.google.com/p/junitparams/</a>, which is released under <a
+ * href="http://www.apache.org/licenses/LICENSE-2.0#in_browser">Apache License
+ * 2.0</a>.
+ * </p>
+ *
+ * @author Christopher Brooks
+ * @version $Id: AutoTests.java 66441 2013-05-30 21:51:37Z cxh $
+ * @since Ptolemy II 8.1
+ * @Pt.ProposedRating Red (cxh)
+ * @Pt.AcceptedRating Red (cxh)
+ */
+@RunWith(JUnitParamsRunner.class)
+public class Auto32Tests extends ModelTests {
+
+    /**
+     * Return a two dimensional array of arrays of strings that name the model
+     * to be executed. If auto/ does not exist, or does not contain files that
+     * end with .xml or .moml, return a list with one element that contains
+     * the value of the THERE_ARE_NO_AUTO_TESTS variable.
+     *
+     * @return The List of model names in auto/
+     * @exception IOException If there is a problem accessing the auto/ directory.
+     */
+    public Object[] modelValues() throws IOException {
+        return modelValues("auto32/", THERE_ARE_NO_AUTO_TESTS);
+    }
+
+    /**
+     * Execute a model.
+     *
+     * @param fullPath
+     *            The full path to the model file to be executed. If the
+     *            fullPath ends with the value of the
+     *            {@link #THERE_ARE_NO_AUTO_TESTS}, then the method returns
+     *            immediately.
+     * @exception Throwable
+     *                If thrown while executing the model.
+     */
+    @Test
+    @Parameters(method = "modelValues")
+    public void RunModel(String fullPath) throws Throwable {
+        if (fullPath.endsWith(THERE_ARE_NO_AUTO_TESTS)) {
+            System.out.println("No auto32/*.xml tests in "
+                    + System.getProperty("user.dir"));
+            return;
+        }
+        if (modelFileIsOK(fullPath)) {
+            System.out.println("----------------- testing " + fullPath);
+            System.out.flush();
+            if (_applicationConstructor == null) {
+                // Delay instantiating MoMLSimpleApplication so that we
+                // can run the kernel tests without requiring moml
+                _applicationClass = Class.forName("ptolemy.moml.MoMLSimpleApplication");
+                _applicationConstructor = _applicationClass
+                    .getConstructor(String.class);
+
+            }
+            _applicationConstructor.newInstance(fullPath);
+        } else {
+            System.err.println("----------------- *** Skipping testing of "
+                    + fullPath);
+            System.err.flush();
+
+        }
+    }
+
+    /** Return true if the model should be run.
+     *  This is a hack to avoid a problem where certain models
+     *  interact badly with the Cobertura code coverage tool.
+     *  @param fullPath The full path of the model to be executed
+     *  @return true if the model should be run.
+     */
+    public boolean modelFileIsOK(String fullPath) {
+        if (fullPath.endsWith("de/test/auto/ThreadedComposite.xml")
+                && !StringUtilities.getProperty(
+                        "net.sourceforge.cobertura.datafile").equals("")) {
+            System.err
+                    .println("ModelTests: Skipping de/test/auto/ThreadedComposite.xml because it interacts badly with Cobertura.");
+            return false;
+        }
+        return true;
+    }
+}
---
+++ ptolemy/util/test/junit/JUnitAuto32Base.java
@@ -0,0 +1,81 @@
+/* Run the 32-bit Java tests in auto/.
+
+ Copyright (c) 2013 The Regents of the University of California.
+ All rights reserved.
+ Permission is hereby granted, without written agreement and without
+ license or royalty fees, to use, copy, modify, and distribute this
+ software and its documentation for any purpose, provided that the above
+ copyright notice and the following two paragraphs appear in all copies
+ of this software.
+
+ IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+ FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+ ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+ THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+ SUCH DAMAGE.
+
+ THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+ INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+ PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+ CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ ENHANCEMENTS, OR MODIFICATIONS.
+
+ PT_COPYRIGHT_VERSION_2
+ COPYRIGHTENDKEY
+
+ */
+
+package ptolemy.util.test.junit;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+///////////////////////////////////////////////////////////////////
+//// JUnitAuto32Base.
+/**
+ * Run the 32-bit Java tests in auto/.
+ * <p>
+ * Derived classes should have a method that calls super.run().
+ *
+ * <p>
+ * If the fileName JVM property is set, then the file named by that property is
+ * sourced. Otherwise, the testDefs.tcl file is sourced and the doallTests Tcl
+ * proc that is defined in $PTII/util/testsuite/testDefs.tcl is invoked and then
+ * any models in the auto/ directory are invoked.
+ * </p>
+ *
+ * <p>
+ * For example, ptolemy.kernel.test.junit.JUnitTclTest extends this class. To
+ * run one test file (Port.tcl):
+ *
+ * <pre>
+ * cd $PTII
+ * java -DfileName=Port.tcl -classpath ${PTII}:${PTII}/bin/ptjacl.jar:${PTII}/lib/junit-4.8.2.jar:${PTII}/lib/JUnitParams-0.3.0.jar org.junit.runner.JUnitCore ptolemy.kernel.test.junit.JUnitAut32Base
+ * </pre>
+ *
+ * </p>
+ *
+ * <p>
+ * To run all the .tcl files in the directory above this directory
+ *
+ * <pre>
+ * cd $PTII
+ * java -classpath ${PTII}:${PTII}/bin/ptjacl.jar:${PTII}/lib/junit-4.8.2.jar:${PTII}/lib/JUnitParams-0.3.0.jar org.junit.runner.JUnitCore ptolemy.kernel.test.junit.JUnitAuto32Base
+ * </pre>
+ *
+ * </p>
+ *
+ * @author Christopher Brooks
+ * @version $Id: JUnitTclTestBase.java 64753 2012-10-02 02:05:53Z cxh $
+ * @since Ptolemy II 8.1
+ * @Pt.ProposedRating Red (cxh)
+ * @Pt.AcceptedRating Red (cxh)
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        ptolemy.util.test.junit.Auto32Tests.class
+
+})
+public class JUnitAuto32Base {
+}
