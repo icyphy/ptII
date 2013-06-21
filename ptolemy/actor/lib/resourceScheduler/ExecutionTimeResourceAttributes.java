38610e92ed57c6f7720f852fc5c366ff57dae778
derler <derler>
2013-06-21 21:28:56 +0000

    comments only

--- ptolemy/actor/lib/resourceScheduler/ExecutionTimeResourceAttributes.java
+++ ptolemy/actor/lib/resourceScheduler/ExecutionTimeResourceAttributes.java
@@ -1,3 +1,33 @@
+/* Container for decorator attributes that are provided to actors by
+ * a resource scheduler.
+
+@Copyright (c) 2008-2013 The Regents of the University of California.
+All rights reserved.
+
+Permission is hereby granted, without written agreement and without
+license or royalty fees, to use, copy, modify, and distribute this
+software and its documentation for any purpose, provided that the
+above copyright notice and the following two paragraphs appear in all
+copies of this software.
+
+IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+SUCH DAMAGE.
+
+THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ENHANCEMENTS, OR MODIFICATIONS.
+
+                                                PT_COPYRIGHT_VERSION_2
+                                                COPYRIGHTENDKEY
+
+
+ */
 package ptolemy.actor.lib.resourceScheduler;
 
 import ptolemy.actor.ResourceAttributes;
@@ -12,6 +42,19 @@ import ptolemy.kernel.util.KernelException;
 import ptolemy.kernel.util.NameDuplicationException;
 import ptolemy.kernel.util.NamedObj;
 
+/**
+Container for decorator attributes that are provided to actors by
+a ResourceScheduler that schedules execution times. 
+The resource scheduler decorates actors
+in a model with the attributes contained by this object.
+
+ @author  Patricia Derler
+ @author  Edward A. Lee
+ @version $Id$
+ @since Ptolemy II 10.0
+ @Pt.ProposedRating Yellow (eal)
+ @Pt.AcceptedRating Red (eal)
+ */
 public class ExecutionTimeResourceAttributes extends ResourceAttributes {
 
     /** Constructor to use when editing a model.
