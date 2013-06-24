ddc36e0aada2d66416ae4fc568362f0f53aa78ce
cxh <cxh>
2013-06-24 21:11:30 +0000

    Added sources for inc Model Exchange FMU.  These sources were copied from fmusdk, which is Copyright (C) 2010 Qtronic GmbH.  See incME/src/documentation/fmusdk_license.txt

--- ptolemy/actor/lib/fmi/fmus/incME/src/sources/fmu.mk
+++ ptolemy/actor/lib/fmi/fmus/incME/src/sources/fmu.mk
@@ -12,8 +12,9 @@ ARCH = darwin64
 
 ARCH_DIR = ../binaries/$(ARCH)/
 
-# Either cs for co-simuluation or me for model exchange.
-INCLUDE = -DFMI_COSIMULATION -I.
+# This is model exchange, so don't define FMI_COSIMULATION
+#INCLUDE = -DFMI_COSIMULATION -I.
+INCLUDE =  -I.
 
 # The suffix for shared libraries.
 # dylib for Mac OS X, so for Linux
@@ -48,23 +49,23 @@ $(FMU_NAME):
 
 darwin64:
 	$(MAKE) ARCH=darwin64 PIC= SHARED_LIBRARY_SUFFIX=dylib $(FMU_NAME).dylib
-	mv $(FMU_NAME).dylib ../binaries/darwin64
+	#mv $(FMU_NAME).dylib ../binaries/darwin64
 
 linux32:
 	$(MAKE) ARCH=linux32 CBITSFLAGS=-m32 PIC=-fPIC SHARED_LIBRARY_SUFFIX=so $(FMU_NAME).so
-	mv $(FMU_NAME).so ../binaries/linux32
+	#mv $(FMU_NAME).so ../binaries/linux32
 
 linux64:
 	$(MAKE) ARCH=linux64 PIC=-fPIC SHARED_LIBRARY_SUFFIX=so $(FMU_NAME).so
-	mv $(FMU_NAME).so ../binaries/linux64
+	#mv $(FMU_NAME).so ../binaries/linux64
 
 win32:
 	$(MAKE) ARCH=win32 PIC= SHARED_LIBRARY_SUFFIX=dll $(FMU_NAME).dll
-	mv $(FMU_NAME).dll ../binaries/win32
+	#mv $(FMU_NAME).dll ../binaries/win32
 
 win64:
 	$(MAKE) ARCH=win64 PIC= SHARED_LIBRARY_SUFFIX=dll $(FMU_NAME).dll
-	mv $(FMU_NAME).dll ../binaries/win64
+	#mv $(FMU_NAME).dll ../binaries/win64
 
 #####
 
@@ -94,9 +95,9 @@ win64:
 		echo "Creating $(ARCH_DIR)"; \
 		mkdir -p $(ARCH_DIR); \
 	fi
-	$(CC) -dynamiclib -g -o $(ARCH_DIR)$@  $< $(OTHER_SRCS) 
+	$(CC) -dynamiclib -g -o $(ARCH_DIR)$@ $<
 
-FMUDIR=../../src
+FMUDIR=..
 
 %.fmu: %.$(SHARED_LIBRARY_SUFFIX)
 	# Remove files that should not be included in the .fmu file.
