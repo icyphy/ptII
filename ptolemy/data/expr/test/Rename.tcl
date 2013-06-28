dfb61941ea533d681d49d8ad1a9d723d1e9709c4
crawl <crawl>
2013-06-28 17:32:08 +0000

    Adding a new test for renaming a variable referenced in a string with multiple characters.
    Reverting some changes in last commit that made 18.2 and 18.4 the
    same test.
    
    Updating the comments.

--- ptolemy/data/expr/test/Rename.tcl
+++ ptolemy/data/expr/test/Rename.tcl
@@ -51,20 +51,24 @@ java::call System setProperty ptolemy.ptII.batchMode true
 #
 # see http://bugzilla.ecoinformatics.org/show_bug.cgi?id=5723
 #
-test Rename-18.1 {Renaming a parameter} {
+test Rename-1.0 {Renaming a parameter} {
     set e1 [java::new ptolemy.kernel.Entity]
     set p1 [java::new ptolemy.data.expr.Parameter $e1 "p1"]
     set p2 [java::new ptolemy.data.expr.Parameter $e1 "p2"]
+    # Set a value for p2 to avoid warnings and stack traces
+    $p2 setExpression 1
     $p1 setExpression {p2}
     $p1 validate
+    set r1 [$p1 getExpression]
     set change "<property name=\"p2\">
 <rename name=\"p3\"/>
 </property>"
     $e1 requestChange [java::new ptolemy.moml.MoMLChangeRequest $e1 $e1 $change]
-    $p1 getExpression
-} {p3}
+    set r2 [$p1 getExpression]
+    list $r1 $r2
+} {p2 p3}
 
-test Rename-18.2 {Renaming a referenced parameter in quotes} {
+test Rename-1.1 {Renaming a referenced parameter in quotes} {
     
     # See " references to parameter are not always renamed"
     # http://bugzilla.ecoinformatics.org/show_bug.cgi?id=5723
@@ -74,8 +78,10 @@ test Rename-18.2 {Renaming a referenced parameter in quotes} {
     # parameter substitution does not occur within double quotes.
 
     set e1 [java::new ptolemy.kernel.Entity]
-    set p1 [java::new ptolemy.data.expr.StringParameter $e1 "p1"]
-    set p2 [java::new ptolemy.data.expr.StringParameter $e1 "p2"]
+    set p1 [java::new ptolemy.data.expr.Parameter $e1 "p1"]
+    set p2 [java::new ptolemy.data.expr.Parameter $e1 "p2"]
+
+    # References inside quotes are not renamed.
     $p1 setExpression {"$p2"}
     $p1 validate
     set r1 [$p1 getExpression]
@@ -85,9 +91,9 @@ test Rename-18.2 {Renaming a referenced parameter in quotes} {
     $e1 requestChange [java::new ptolemy.moml.MoMLChangeRequest $e1 $e1 $change]
     set r2 [$p1 getExpression]
     list $r1 $r2
-} {{"$p2"} {"$p3"}}
+} {{"$p2"} {"$p2"}}
 
-test Rename-18.3 {Renaming a parameter referenced by a parameter in string mode} {
+test Rename-1.2 {Renaming a parameter referenced by a parameter in string mode} {
 
     # See " references to parameter are not always renamed"
     # http://bugzilla.ecoinformatics.org/show_bug.cgi?id=5723
@@ -105,9 +111,9 @@ test Rename-18.3 {Renaming a parameter referenced by a parameter in string mode}
     set p1 [java::new ptolemy.data.expr.StringParameter $e1 "p1"]
     set p2 [java::new ptolemy.data.expr.StringParameter $e1 "p2"]
 
-    # The only difference between this test and the one above is that this
-    # test has no double quotes in the next line.
-
+    # The differences between this test and the one above are that this
+    # test has no double quotes in the next line, and p1 is a StringParameter,
+    # which has string mode on.
 
     $p1 setExpression {$p2}
     $p1 validate
@@ -120,7 +126,7 @@ test Rename-18.3 {Renaming a parameter referenced by a parameter in string mode}
     list $r1 $r2
 } {{$p2} {$p3}}
 
-test Rename-18.4 {Renaming a parameter in quotes referenced by a parameter in string mode} {
+test Rename-1.3 {Renaming a parameter in quotes referenced by a parameter in string mode} {
     # See " references to parameter are not always renamed"
     # http://bugzilla.ecoinformatics.org/show_bug.cgi?id=5723
 
@@ -134,10 +140,9 @@ test Rename-18.4 {Renaming a parameter in quotes referenced by a parameter in st
     # parameter. It is not necessary"
 
     set e1 [java::new ptolemy.kernel.Entity]
-    set p1 [java::new ptolemy.data.expr.Parameter $e1 "p1"]
-    set p2 [java::new ptolemy.data.expr.Parameter $e1 "p2"]
-    $p1 setStringMode true
-    # This is an string that has $p2 inside, which will not be dereferenced (eal-5/16/13)
+    set p1 [java::new ptolemy.data.expr.StringParameter $e1 "p1"]
+    set p2 [java::new ptolemy.data.expr.StringParameter $e1 "p2"]
+    # Variables inside strings are dereferenced if the Variable is in string mode.
     $p1 setExpression {"$p2"}
     $p1 validate
     set r1 [$p1 getExpression]
@@ -146,4 +151,24 @@ test Rename-18.4 {Renaming a parameter in quotes referenced by a parameter in st
 </property>"
     $e1 requestChange [java::new ptolemy.moml.MoMLChangeRequest $e1 $e1 $change]
     list $r1 [$p1 getExpression]
-} {{"$p2"} {"$p2"}}
+} {{"$p2"} {"$p3"}}
+
+test Rename-1.4 {Renaming a parameter referenced by a string parameter containing other characters} {
+    set e1 [java::new ptolemy.kernel.Entity]
+    set p1 [java::new ptolemy.data.expr.StringParameter $e1 "p1"]
+    set p2 [java::new ptolemy.data.expr.StringParameter $e1 "p2"]
+
+    # Set the expression to contain several characters including a reference
+    # to p2. Note: $p2 should be renamed to $p3; p2 should not be changed
+    # since it is not a reference.
+
+    $p1 setExpression {a $p2 b p2 c}
+    $p1 validate
+    set r1 [$p1 getExpression]
+    set change "<property name=\"p2\">
+<rename name=\"p3\"/>
+</property>"
+    $e1 requestChange [java::new ptolemy.moml.MoMLChangeRequest $e1 $e1 $change]
+    set r2 [$p1 getExpression]
+    list $r1 $r2
+} {{a $p2 b p2 c} {a $p3 b p2 c}}
