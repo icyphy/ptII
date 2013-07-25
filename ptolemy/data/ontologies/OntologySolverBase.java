2b3cc15e28dd0d01b236e584f0ad1114f883b5d2
eal <eal>
2013-07-25 06:27:30 +0000

    organized imports

--- ptolemy/data/ontologies/OntologySolverBase.java
+++ ptolemy/data/ontologies/OntologySolverBase.java
@@ -50,7 +50,6 @@ import ptolemy.domains.modal.kernel.Configurer;
 import ptolemy.kernel.CompositeEntity;
 import ptolemy.kernel.util.Attribute;
 import ptolemy.kernel.util.IllegalActionException;
-import ptolemy.kernel.util.KernelException;
 import ptolemy.kernel.util.NameDuplicationException;
 import ptolemy.kernel.util.NamedObj;
 import ptolemy.kernel.util.Settable;
