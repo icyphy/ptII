77ac0705f762b91fda2e1bdf44e318e37b3c0717
hudson <hudson>
2013-09-11 15:35:26 +0000

    Removed coverity test from here.  Instead, we are running it elsewhere.

--- adm/test/NightlyUpdate.tcl
+++ adm/test/NightlyUpdate.tcl
@@ -137,9 +137,5 @@ test nightly-1.7 {update_moog} {
     list $matches $results
 } {{} {}}
 
-test nightly-1.8 {coverity} {
-    set matches [nightlyMake coverity]
-} {}
-
 set VERBOSE 0
 cd $startingDirectory
