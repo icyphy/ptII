12b8440615ba097e0926b94d4a8e2ea532425d35
glp <glp>
2013-11-01 20:26:42 +0000

    Added javadocs for EvenTimeComparator

--- ptolemy/domains/metroII/kernel/EventTimeComparator.java
+++ ptolemy/domains/metroII/kernel/EventTimeComparator.java
@@ -1,3 +1,31 @@
+/* EventTimeComparator is for comparing time tags of MetroII events.
+
+ Copyright (c) 2012-2013 The Regents of the University of California.
+ All rights reserved.
+ Permission is hereby granted, without written agreement and without
+ license or royalty fees, to use, copy, modify, and distribute this
+ software and its documentation for any purpose, provided that the above
+ copyright notice and the following two paragraphs appear in all copies
+ of this software.
+
+ IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+ FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+ ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+ THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+ SUCH DAMAGE.
+
+ THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+ INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+ PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+ CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ ENHANCEMENTS, OR MODIFICATIONS.
+
+ PT_COPYRIGHT_VERSION_2
+ COPYRIGHTENDKEY
+
+ */
+
 package ptolemy.domains.metroII.kernel;
 
 import java.util.Comparator;
@@ -5,11 +33,30 @@ import java.util.Comparator;
 import ptolemy.domains.metroII.kernel.util.ProtoBuf.metroIIcomm.Event;
 import ptolemy.domains.metroII.kernel.util.ProtoBuf.metroIIcomm.Event.Time;
 
+///////////////////////////////////////////////////////////////////
+//// EventTimeComparator
+
+/**
+* EventTimeComparator is for comparing time tags of MetroII events.
+*
+* @author Liangpeng Guo
+* @version $Id$
+* @since Ptolemy II 10.0
+* @Pt.ProposedRating Red (glp)
+* @Pt.AcceptedRating Red (glp)
+*
+*/
 public class EventTimeComparator {
     
+    /**
+     * EventTimeComparator is a singleton. 
+     */
     protected EventTimeComparator() {
         // Exists only to defeat instantiation.
     }
+
+    ///////////////////////////////////////////////////////////////////
+    ////                         public methods                    ////
 
     /**
      * Convert a time value from one resolution to another resolution. Note that
@@ -69,5 +116,11 @@ public class EventTimeComparator {
         return comparator.compare(time1, time2); 
     }
     
+    ///////////////////////////////////////////////////////////////////
+    ////                         private fields                    ////
+
+    /**
+     * Comparator for time tags of MetroII events.
+     */
     private static Comparator<Event.Time> comparator = null; 
 }
