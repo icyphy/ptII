1eff9dbc30bc558ea64bb69ea481050370033fbb
cxh <cxh>
2014-02-07 00:51:36 +0000

    First cut at simple Display actor for the Arduino.

---
+++ ptolemy/cg/adapter/generic/program/procedural/c/arduino/adapters/ptolemy/actor/lib/gui/Display.c
@@ -0,0 +1,34 @@
+/***preinitBlock***/
+/**/
+
+/*** IntPrintBlock($name, $channel) ***/
+if ($hasToken(input#$channel)) {
+        printf("%d\n", $get(input#$channel));
+}
+/**/
+
+/*** DoublePrintBlock($name, $channel) ***/
+if ($hasToken(input#$channel)) {
+        printf("%f\n", $get(input#$channel));
+}
+/**/
+
+/*** StringPrintBlock($name, $channel) ***/
+if ($hasToken(input#$channel)) {
+        printf("%s\n", $get(input#$channel));
+}
+/**/
+
+/*** BooleanPrintBlock($name, $channel) ***/
+// Arduino!
+if ($hasToken(input#$channel)) {
+        printf("%d\n", $get(input#$channel));
+}
+/**/
+
+/*** TokenPrintBlock($name, $channel) ***/
+if ($hasToken(input#$channel)) {
+        printf("%s: ", $tokenFunc($get(input#$channel)::toString()).payload);
+}
+/**/
+
