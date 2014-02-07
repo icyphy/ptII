90bf63e198c361d7122dfda5455f20b104ca860e
cxh <cxh>
2014-02-07 00:39:26 +0000

    Use curly brackets with if statements or spend hours tracking down silly bugs.

--- ptolemy/cg/adapter/generic/program/procedural/c/adapters/ptolemy/actor/lib/gui/Display.c
+++ ptolemy/cg/adapter/generic/program/procedural/c/adapters/ptolemy/actor/lib/gui/Display.c
@@ -2,27 +2,32 @@
 /**/
 
 /*** IntPrintBlock($name, $channel) ***/
-if ($hasToken(input#$channel))
+if ($hasToken(input#$channel)) {
         printf("%d\n", $get(input#$channel));
+}
 /**/
 
 /*** DoublePrintBlock($name, $channel) ***/
-if ($hasToken(input#$channel))
+if ($hasToken(input#$channel)) {
         printf("%f\n", $get(input#$channel));
+}
 /**/
 
 /*** StringPrintBlock($name, $channel) ***/
-if ($hasToken(input#$channel))
+if ($hasToken(input#$channel)) {
         printf("%s\n", $get(input#$channel));
+}
 /**/
 
 /*** BooleanPrintBlock($name, $channel) ***/
-if ($hasToken(input#$channel))
+if ($hasToken(input#$channel)) {
         printf("%d\n", $get(input#$channel));
+}
 /**/
 
 /*** TokenPrintBlock($name, $channel) ***/
-if ($hasToken(input#$channel))
+if ($hasToken(input#$channel)) {
         printf("%s: ", $tokenFunc($get(input#$channel)::toString()).payload);
+}
 /**/
 
