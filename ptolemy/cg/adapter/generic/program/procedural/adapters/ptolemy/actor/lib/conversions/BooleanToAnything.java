40f23e77f3d0675d0a05b7ebe153238b17a1fcdb
cxh <cxh>
2014-02-08 05:49:55 +0000

    Adding BooleanToAnything.

---
+++ ptolemy/cg/adapter/generic/program/procedural/adapters/ptolemy/actor/lib/conversions/BooleanToAnything.java
@@ -0,0 +1,52 @@
+/* A helper class for actor.lib.conversions.BooleanToAnything
+
+ @Copyright (c) 2005-2013 The Regents of the University of California.
+ All rights reserved.
+ Permission is hereby granted, without written agreement and without
+ license or royalty fees, to use, copy, modify, and distribute this
+ software and its documentation for any purpose, provided that the
+ above copyright notice and the following two paragraphs appear in all
+ copies of this software.
+
+ IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+ FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+ ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+ THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+ SUCH DAMAGE.
+
+ THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+ INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+ PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+ CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ ENHANCEMENTS, OR MODIFICATIONS.
+
+ PT_COPYRIGHT_VERSION_2
+ COPYRIGHTENDKEY
+
+
+ */
+package ptolemy.cg.adapter.generic.program.procedural.adapters.ptolemy.actor.lib.conversions;
+
+import ptolemy.cg.kernel.generic.program.CodeStream;
+import ptolemy.cg.kernel.generic.program.NamedProgramCodeGeneratorAdapter;
+import ptolemy.kernel.util.IllegalActionException;
+
+/**
+ * A helper class for ptolemy.actor.lib.conversions.BooleanToAnything.
+ *
+ * @author Man-Kit Leung
+ * @version $Id: BooleanToAnything.java 67784 2013-10-26 16:53:27Z cxh $
+ * @since Ptolemy II 10.0
+ * @Pt.ProposedRating Red (cxh)
+ * @Pt.AcceptedRating Red (cxh)
+ */
+public class BooleanToAnything extends NamedProgramCodeGeneratorAdapter {
+    /**
+     * Constructor the BooleanToAnything helper.
+     * @param actor the associated actor.
+     */
+    public BooleanToAnything(ptolemy.actor.lib.conversions.BooleanToAnything actor) {
+        super(actor);
+    }
+}
