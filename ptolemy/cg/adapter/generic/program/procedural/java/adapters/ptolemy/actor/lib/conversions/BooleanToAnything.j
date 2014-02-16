e073b9bc5155e3b4539c82205b4893dcee9bb846
cxh <cxh>
2014-02-16 02:03:49 +0000

    Java version of BooleanToAnything.

---
+++ ptolemy/cg/adapter/generic/program/procedural/java/adapters/ptolemy/actor/lib/conversions/BooleanToAnything.j
@@ -0,0 +1,7 @@
+/***fireBlock***/
+if ($get(input)) {
+    $put(output#0, $val(($cgType(output)) trueValue));
+} else {
+    $put(output#0, $val(($cgType(output)) falseValue));
+}
+/**/
---
+++ ptolemy/cg/adapter/generic/program/procedural/java/adapters/ptolemy/actor/lib/conversions/BooleanToAnything.java
@@ -0,0 +1,52 @@
+/* A adapter class for ptolemy.actor.lib.conversion.BooleanToAnything
+
+ Copyright (c) 2010-2011 The Regents of the University of California.
+ All rights reserved.
+ Permission is hereby granted, without written agreement and without
+ license or royalty fees, to use, copy, modify, and distribute this
+ software and its documentation for any purpose, provided that the above
+ copyright notice and the following two paragraphs appear in all copies
+ of this software.
+
+ IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+ FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+ ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+ THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+ SUCH DAMAGE.
+
+ THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+ INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+ PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+ CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ ENHANCEMENTS, OR MODIFICATIONS.
+
+ PT_COPYRIGHT_VERSION_2
+ COPYRIGHTENDKEY
+
+ */
+package ptolemy.cg.adapter.generic.program.procedural.java.adapters.ptolemy.actor.lib.conversions;
+
+//////////////////////////////////////////////////////////////////////////
+//// BooleanToAnything
+
+/**
+ A adapter class for ptolemy.actor.lib.conversions.BooleanToAnything.
+
+ @author Christopher Brooks
+ @version $Id: BooleanToAnything.java 67784 2013-10-26 16:53:27Z cxh $
+ @since Ptolemy II 10.0
+ @Pt.ProposedRating Red (rodiers)
+ @Pt.AcceptedRating Red (rodiers)
+ */
+public class BooleanToAnything
+        extends
+        ptolemy.cg.adapter.generic.program.procedural.adapters.ptolemy.actor.lib.conversions.BooleanToAnything {
+    /**
+     *  Construct a BooleanToAnything adapter.
+     *  @param actor The given ptolemy.actor.lib.BooleanToAnything actor.
+     */
+    public BooleanToAnything(ptolemy.actor.lib.conversions.BooleanToAnything actor) {
+        super(actor);
+    }
+}
