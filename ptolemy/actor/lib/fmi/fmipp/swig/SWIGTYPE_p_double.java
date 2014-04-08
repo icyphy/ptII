4e759216416a3d7305c941a576920d354ce741b5
cxh <cxh>
2014-04-08 00:08:10 +0000

    configure.in changed.

--- ptolemy/actor/lib/fmi/fmipp/swig/SWIGTYPE_p_double.java
+++ ptolemy/actor/lib/fmi/fmipp/swig/SWIGTYPE_p_double.java
@@ -8,27 +8,19 @@
 
 package ptolemy.actor.lib.fmi.fmipp.swig;
 
-/**
- * SWIGTYPE_p_double class.
- *
- * @author cxh
- * @version $Id$
- * @since Ptolemy II 10.0
- * @Pt.ProposedRating Red (cxh)
- * @Pt.AcceptedRating Red (cxh)
- */
 public class SWIGTYPE_p_double {
-    private long swigCPtr;
+  private long swigCPtr;
 
-    protected SWIGTYPE_p_double(long cPtr, boolean futureUse) {
-        swigCPtr = cPtr;
-    }
+  protected SWIGTYPE_p_double(long cPtr, boolean futureUse) {
+    swigCPtr = cPtr;
+  }
 
-    protected SWIGTYPE_p_double() {
-        swigCPtr = 0;
-    }
+  protected SWIGTYPE_p_double() {
+    swigCPtr = 0;
+  }
 
-    protected static long getCPtr(SWIGTYPE_p_double obj) {
-        return (obj == null) ? 0 : obj.swigCPtr;
-    }
+  protected static long getCPtr(SWIGTYPE_p_double obj) {
+    return (obj == null) ? 0 : obj.swigCPtr;
+  }
 }
+
--- ptolemy/actor/lib/fmi/fmipp/swig/SWIGTYPE_p_std__string.java
+++ ptolemy/actor/lib/fmi/fmipp/swig/SWIGTYPE_p_std__string.java
@@ -8,27 +8,19 @@
 
 package ptolemy.actor.lib.fmi.fmipp.swig;
 
-/**
- * SWIGTYPE_p_std__string class.
- *
- * @author cxh
- * @version $Id$
- * @since Ptolemy II 10.0
- * @Pt.ProposedRating Red (cxh)
- * @Pt.AcceptedRating Red (cxh)
- */
 public class SWIGTYPE_p_std__string {
-    private long swigCPtr;
+  private long swigCPtr;
 
-    protected SWIGTYPE_p_std__string(long cPtr, boolean futureUse) {
-        swigCPtr = cPtr;
-    }
+  protected SWIGTYPE_p_std__string(long cPtr, boolean futureUse) {
+    swigCPtr = cPtr;
+  }
 
-    protected SWIGTYPE_p_std__string() {
-        swigCPtr = 0;
-    }
+  protected SWIGTYPE_p_std__string() {
+    swigCPtr = 0;
+  }
 
-    protected static long getCPtr(SWIGTYPE_p_std__string obj) {
-        return (obj == null) ? 0 : obj.swigCPtr;
-    }
+  protected static long getCPtr(SWIGTYPE_p_std__string obj) {
+    return (obj == null) ? 0 : obj.swigCPtr;
+  }
 }
+
