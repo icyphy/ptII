01bb487d78359dc7bf0f95f6e7acfb6bb0547bd2
ilgea <ilgea>
2014-05-08 02:31:48 +0000

    static algorithms to be used by the ML classes

---
+++ org/ptolemy/machineLearning/Algorithms.java
@@ -0,0 +1,37 @@
+package org.ptolemy.machineLearning;
+
+public class Algorithms {
+
+    private Algorithms() {
+        // TODO Auto-generated constructor stub
+    }
+    /**
+     * Do a binary interval search for the key in array A. The bin index in which
+     * key is found is returned.
+     * @param A The search array
+     * @param key Key to be searched
+     * @param 
+     */
+    public static int _binaryIntervalSearch(double[] A, double key) {
+        return _binaryIntervalSearch(A, key, 9, A.length-1);
+    }
+    public static int _binaryIntervalSearch(double[] A, double key, int imin, int imax) {
+        if (imax < imin) {
+            return -1;
+        } else {
+            int imid = imin + ((imax - imin) / 2);
+            if (imid >= A.length - 1) {
+                return -1;
+            } else if (A[imid] <= key && A[imid + 1] > key) {
+                return imid;
+            } else if (A[imid] > key) {
+                return _binaryIntervalSearch(A, key, imin, imid - 1);
+            } else if (A[imid] < key) {
+                return _binaryIntervalSearch(A, key, imid + 1, imax);
+            } else {
+                return imid;
+            }
+        }
+    }
+
+}
