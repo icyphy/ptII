6379fcde1392ba4154af84e842141cf04d38ef7b
cxh <cxh>
2014-05-10 15:22:35 +0000

    Simple test of cloning CompositeOptimizer

--- ptolemy/configs/test/cloneTest.tcl
+++ ptolemy/configs/test/cloneTest.tcl
@@ -5,8 +5,12 @@ set workspace [java::new ptolemy.kernel.util.Workspace]
 set compositeEntity [java::new ptolemy.kernel.CompositeEntity $workspace]
 $compositeEntity setName Top
 #set actor [java::new ptolemy.actor.lib.colt.ColtZeta $compositeEntity "Zeta"]
-set actor [java::new ptolemy.actor.lib.Gaussian $compositeEntity "Gaussian"]
-set clone [java::cast ptolemy.actor.TypedAtomicActor [$actor clone $workspace]]
+#set actor [java::new ptolemy.actor.lib.Gaussian $compositeEntity "Gaussian"]
+#set clone [java::cast ptolemy.actor.TypedAtomicActor [$actor clone $workspace]]
+
+set actor [java::new org.ptolemy.optimization.CompositeOptimizer $compositeEntity "CompositeOptimizer"]
+set clone [java::cast ptolemy.actor.TypedCompositeActor [$actor clone $workspace]]
+
 set constraints [$actor typeConstraintList]
 set c [jdkPrintArray [$constraints toArray] "\n" ]
 
