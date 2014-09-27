53eea3105b213e4a900ff62de22559fa8668cd3e
eal <eal>
2014-09-27 23:34:49 +0000

    Add file browser to find local accessors

--- ptolemy/vergil/basic/imprt/accessor/ImportAccessorAction.java
+++ ptolemy/vergil/basic/imprt/accessor/ImportAccessorAction.java
@@ -30,7 +30,6 @@ package ptolemy.vergil.basic.imprt.accessor;
 import java.awt.event.ActionEvent;
 import java.awt.geom.Rectangle2D;
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.StringWriter;
 import java.net.URL;
@@ -45,7 +44,6 @@ import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
 
 import org.json.JSONArray;
-import org.json.JSONException;
 
 import ptolemy.gui.ComponentDialog;
 import ptolemy.gui.Query;
@@ -71,6 +69,12 @@ import diva.graph.GraphController;
 
 /**
    An Action to Import an Internet of Things (IoT) Accessor.
+   This action presents a dialog box that permits specifying a URL or local directory.
+   If the URL or local directory contains a file named index.json, then it presents
+   a list of the options given in that file, which it assumes are all accessors.
+   To manually specify a particular accessor on the local file system, you can
+   browse to its directory, but then you have to manually type in the file name
+   of the accessor.
 
    <p>This package is optional.  To add the "Import Accessor" menu choice
    to the GraphEditor, add the following to the configuration:</p>
@@ -125,122 +129,123 @@ public class ImportAccessorAction extends AbstractAction {
     public void actionPerformed(ActionEvent event) {
     	final Query query = new Query();
     	query.setTextWidth(60);
-    	query.addLine("location", "location", _lastLocation);
+    	query.addFileChooser("location", "location", _lastLocation, null, null, true, true);
+    	// query.addLine("location", "location", _lastLocation);
     	final JComboBox box = query.addChoice("accessor", "accessor",
     			new String[] {}, _lastAccessorName);
     	updateComboBox(box, query);
     	query.addQueryListener(new QueryListener() {
-    		@Override
-    		public void changed(String name) {
-    			if (name.equals("location")) {
-    				updateComboBox(box, query);
-    			}
+    	    @Override
+    	    public void changed(String name) {
+    		if (name.equals("location")) {
+    		    updateComboBox(box, query);
     		}
+    	    }
     	});
     	ComponentDialog dialog = new ComponentDialog(_frame,
-    			"Instantiate Accessor", query);
+    		"Instantiate Accessor", query);
 
     	if (dialog.buttonPressed().equals("OK")) {
-    		// Get the associated Ptolemy model.
-    		GraphController controller = _frame.getJGraph()
-    				.getGraphPane().getGraphController();
-    		AbstractBasicGraphModel model = (AbstractBasicGraphModel) controller
-    				.getGraphModel();
-    		NamedObj context = model.getPtolemyModel();
+    	    // Get the associated Ptolemy model.
+    	    GraphController controller = _frame.getJGraph()
+    		    .getGraphPane().getGraphController();
+    	    AbstractBasicGraphModel model = (AbstractBasicGraphModel) controller
+    		    .getGraphModel();
+    	    NamedObj context = model.getPtolemyModel();
 
-    		// Use the center of the screen as a location.
-    		Rectangle2D bounds = _frame.getVisibleCanvasRectangle();
-    		final double x = bounds.getWidth() / 2.0;
-    		final double y = bounds.getHeight() / 2.0;
+    	    // Use the center of the screen as a location.
+    	    Rectangle2D bounds = _frame.getVisibleCanvasRectangle();
+    	    final double x = bounds.getWidth() / 2.0;
+    	    final double y = bounds.getHeight() / 2.0;
 
-    		URL url;
-    		String input = "";
-    		_lastAccessorName = query.getStringValue("accessor");
-    		final String urlSpec = _lastLocation + _lastAccessorName;
-    		StringBuffer buffer = new StringBuffer();
-    		buffer.append("<group name=\"auto\">\n");
-    		try {
-    			url = new URL(urlSpec);
+    	    URL url;
+    	    String input = "";
+    	    _lastAccessorName = query.getStringValue("accessor");
+    	    final String urlSpec = _lastLocation + _lastAccessorName;
+    	    StringBuffer buffer = new StringBuffer();
+    	    buffer.append("<group name=\"auto\">\n");
+    	    try {
+    		url = FileUtilities.nameToURL(urlSpec, null, null);
 
-    			BufferedReader in = new BufferedReader(
-    					new InputStreamReader(url.openStream()));
-    			StringBuffer contents = new StringBuffer();
-    			while ((input = in.readLine()) != null) {
-    				contents.append(input);
-    			}
-
-    			TransformerFactory factory = TransformerFactory.newInstance();
-    			String xsltLocation = "$CLASSPATH/org/terraswarm/kernel/XMLJStoMOML.xslt";
-    			Source xslt = new StreamSource(FileUtilities.nameToFile(
-    					xsltLocation, null));
-    			Transformer transformer = factory.newTransformer(xslt);
-    			StreamSource source = new StreamSource(
-    					new InputStreamReader(url.openStream()));
-    			StringWriter outWriter = new StringWriter();
-    			StreamResult result = new StreamResult(outWriter);
-    			transformer.transform(source, result);
-    			contents = outWriter.getBuffer();
-
-    			buffer.append(contents);
-    			in.close();
-    		} catch (Exception e1) {
-    			MessageHandler.error("Failed to import accessor.", e1);
-    			return;
+    		BufferedReader in = new BufferedReader(
+    			new InputStreamReader(url.openStream()));
+    		StringBuffer contents = new StringBuffer();
+    		while ((input = in.readLine()) != null) {
+    		    contents.append(input);
     		}
-    		buffer.append("</group>\n");
 
-    		MoMLChangeRequest request = new MoMLChangeRequest(this,
-    				context, buffer.toString()) {
-    			@Override
-    			protected void _postParse(MoMLParser parser) {
-    				List<NamedObj> topObjects = parser.topObjectsCreated();
-    				if (topObjects == null) {
-    					return;
-    				}
-    				for (NamedObj object : topObjects) {
-    					Location location = (Location) object.getAttribute("_location");
-    					// Set the location.
-    					if (location == null) {
-    						try {
-    							location = new Location(object, "_location");
-    						} catch (KernelException e) {
-    							// Ignore.
-    						}
-    					}
-    					if (location != null) {
-    						try {
-    							location.setLocation(new double[] { x, y });
-    						} catch (IllegalActionException e) {
-    							// Ignore.
-    						}
-    					}
-    					// Set the source.
-    					Attribute source = object.getAttribute("accessorSource");
-    					if (source instanceof StringAttribute) {
-    						try {
-    							((StringAttribute) source).setExpression(urlSpec);
-    							// Have to mark persistent or the urlSpec will be assumed to be part
-    							// of the class definition and hence will not be exported to MoML.
-    							((StringAttribute) source).setDerivedLevel(Integer.MAX_VALUE);
-    							((StringAttribute) source).setPersistent(true);
-    						} catch (IllegalActionException e) {
-    							// Should not happen.
-    							throw new InternalErrorException(object, e,
-    									"Failed to set accessorSource");
-    						}
-    					}
-    				}
-    				parser.clearTopObjectsList();
-    				super._postParse(parser);
-    			}
+    		TransformerFactory factory = TransformerFactory.newInstance();
+    		String xsltLocation = "$CLASSPATH/org/terraswarm/kernel/XMLJStoMOML.xslt";
+    		Source xslt = new StreamSource(FileUtilities.nameToFile(
+    			xsltLocation, null));
+    		Transformer transformer = factory.newTransformer(xslt);
+    		StreamSource source = new StreamSource(
+    			new InputStreamReader(url.openStream()));
+    		StringWriter outWriter = new StringWriter();
+    		StreamResult result = new StreamResult(outWriter);
+    		transformer.transform(source, result);
+    		contents = outWriter.getBuffer();
 
-    			@Override
-    			protected void _preParse(MoMLParser parser) {
-    				super._preParse(parser);
-    				parser.clearTopObjectsList();
+    		buffer.append(contents);
+    		in.close();
+    	    } catch (Exception e1) {
+    		MessageHandler.error("Failed to import accessor.", e1);
+    		return;
+    	    }
+    	    buffer.append("</group>\n");
+
+    	    MoMLChangeRequest request = new MoMLChangeRequest(this,
+    		    context, buffer.toString()) {
+    		@Override
+    		protected void _postParse(MoMLParser parser) {
+    		    List<NamedObj> topObjects = parser.topObjectsCreated();
+    		    if (topObjects == null) {
+    			return;
+    		    }
+    		    for (NamedObj object : topObjects) {
+    			Location location = (Location) object.getAttribute("_location");
+    			// Set the location.
+    			if (location == null) {
+    			    try {
+    				location = new Location(object, "_location");
+    			    } catch (KernelException e) {
+    				// Ignore.
+    			    }
     			}
-    		};
-    		context.requestChange(request);
+    			if (location != null) {
+    			    try {
+    				location.setLocation(new double[] { x, y });
+    			    } catch (IllegalActionException e) {
+    				// Ignore.
+    			    }
+    			}
+    			// Set the source.
+    			Attribute source = object.getAttribute("accessorSource");
+    			if (source instanceof StringAttribute) {
+    			    try {
+    				((StringAttribute) source).setExpression(urlSpec);
+    				// Have to mark persistent or the urlSpec will be assumed to be part
+    				// of the class definition and hence will not be exported to MoML.
+    				((StringAttribute) source).setDerivedLevel(Integer.MAX_VALUE);
+    				((StringAttribute) source).setPersistent(true);
+    			    } catch (IllegalActionException e) {
+    				// Should not happen.
+    				throw new InternalErrorException(object, e,
+    					"Failed to set accessorSource");
+    			    }
+    			}
+    		    }
+    		    parser.clearTopObjectsList();
+    		    super._postParse(parser);
+    		}
+
+    		@Override
+    		protected void _preParse(MoMLParser parser) {
+    		    super._preParse(parser);
+    		    parser.clearTopObjectsList();
+    		}
+    	    };
+    	    context.requestChange(request);
     	}
     }
 
@@ -255,7 +260,8 @@ public class ImportAccessorAction extends AbstractAction {
             } else if (!_lastLocation.endsWith("/")) {
                 _lastLocation = _lastLocation + "/";
             }
-            url = new URL(_lastLocation + "index.json");
+            String index = _lastLocation + "index.json";
+            url = FileUtilities.nameToURL(index, null, null);
 
             in = new BufferedReader(new InputStreamReader(url.openStream()));
 
@@ -270,12 +276,9 @@ public class ImportAccessorAction extends AbstractAction {
             for (int i = 0; i < array.length(); i++) {
                 box.addItem(array.get(i));
             }
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        } catch (JSONException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
+        } catch (Exception e) {
+            // Don't provide a list of options, but issue a message.
+            System.err.println("Cannot suggest accessors, because there is no index.json file at " + _lastLocation);
         }
     }
 
