20ea235b81d16a11779922f9384b9370f6414f3f
pd <pd>
2014-11-03 22:56:53 +0000

    try to fix session timeout

--- ptolemy/actor/lib/vertx/demo/PubSub/server.js
+++ ptolemy/actor/lib/vertx/demo/PubSub/server.js
@@ -4,6 +4,6 @@ var httpServer = vertx.createHttpServer();
 
 var sockJSServer = vertx.createSockJSServer(httpServer);
 
-sockJSServer.bridge({prefix : "/eventbus", "ping_interval": 100000, "vertxbus_ping_interval": 100000, "heartbeat_period": 10000}, [{}], [{}] );
+sockJSServer.bridge({"prefix" : "/eventbus", "session_timeout": 10000, "ping_interval": 100000, "vertxbus_ping_interval": 100000, "heartbeat_period": 10000}, [{}], [{}] );
 
 httpServer.listen(8080);
