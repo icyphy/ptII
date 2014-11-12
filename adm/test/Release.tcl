878b0d54d23d049c3432feffea962378e5ec6c74
cxh <cxh>
2014-11-12 15:59:18 +0000

    $PTII/generated-sources is now being created.  Not sure why.

--- adm/test/Release.tcl
+++ adm/test/Release.tcl
@@ -176,6 +176,7 @@ test release-3.1 {Run svn status and look for files that should be checked in.  
 !       ptolemy/vergil/basic/layout/kieler/test/layoutPerformance2.xml} {
 ?       .maven} {
 ?       cobertura.ser} {
+?       generated-sources} {
 ?       ptolemy/actor/lib/jai/test/auto/PtolemyII.bmp} {
 ?       ptolemy/actor/lib/jai/test/auto/PtolemyII.jpg} {
 ?       ptolemy/actor/lib/jai/test/auto/PtolemyII.pgm} {
