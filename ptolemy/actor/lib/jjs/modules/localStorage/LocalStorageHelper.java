a77f232e89eb6fc3521c39ba8cd9b428cb0e2ba4
hokeunkim <hokeunkim>
2014-11-20 09:00:50 +0000

    Implemented JavaScript API for basic persistent local storage. Added a demo for the local storage.

---
+++ ptolemy/actor/lib/jjs/modules/localStorage/LocalStorageHelper.java
@@ -0,0 +1,185 @@
+/* AnExecute a script in JavaScript.
+
+   Copyright (c) 2014 The Regents of the University of California.
+   All rights reserved.
+   Permission is hereby granted, without written agreement and without
+   license or royalty fees, to use, copy, modify, and distribute this
+   software and its documentation for any purpose, provided that the above
+   copyright notice and the following two paragraphs appear in all copies
+   of this software.
+
+   IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+   FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+   ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+   THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+   SUCH DAMAGE.
+
+   THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+   INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+   MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+   PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+   CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+   ENHANCEMENTS, OR MODIFICATIONS.
+
+   PT_COPYRIGHT_VERSION_2
+   COPYRIGHTENDKEY
+
+ */
+package ptolemy.actor.lib.jjs.modules.localStorage;
+
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Enumeration;
+
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttPersistenceException;
+import org.eclipse.paho.client.mqttv3.internal.MqttPersistentData;
+import org.eclipse.paho.client.mqttv3.persist.MqttDefaultFilePersistence;
+
+import ptolemy.actor.TypedAtomicActor;
+
+///////////////////////////////////////////////////////////////////
+//// LocalStorageHelper
+
+/**
+   A helper class for the LocalStorage module in JavaScript.
+   
+   @author Hokeun Kim
+   @version $Id$
+   @since Ptolemy II 11.0
+   @Pt.ProposedRating Yellow (eal)
+   @Pt.AcceptedRating Red (bilung)
+ */
+public class LocalStorageHelper extends TypedAtomicActor {
+    
+    /**
+     * Construct a LocalStorageHelper using the container's name and the actors' display name
+     * for the directory name. Also use current time to make the directory name unique.
+     * 
+     * @param containerActorName Container's name plus the actors' display name
+     * @throws MqttPersistenceException
+     */
+    public LocalStorageHelper(String containerActorName) throws MqttPersistenceException {
+        // FIXME if you can find a better directory
+        _mqttLocalStorage = new MqttDefaultFilePersistence("/tmp");
+        try {
+            _mqttLocalStorage.close();
+        }
+        catch (MqttPersistenceException e) {
+            // Just ignore
+        }
+        
+        DateFormat dateFormat = new SimpleDateFormat("yyyyMMdd-HHmmss");
+        Date date = new Date();
+
+        // FIXME Change the information for the file name here, per mqtt connection.
+        //_mqttLocalStorage.open("clientId", "theConnection");
+        _mqttLocalStorage.open("mqls-" + containerActorName, dateFormat.format(date));
+    }
+    
+    ///////////////////////////////////////////////////////////////////
+    ////                     public methods                        ////
+
+    /**
+     * Take a key and return its value from the local storage if the key exists,
+     * otherwise, return null.
+     * 
+     * @param key The key for the value to be returned.
+     * @return The string value associated with the key.
+     * @throws MqttPersistenceException
+     */
+    public String getItem(String key) throws MqttPersistenceException {
+        if (_mqttLocalStorage.containsKey(key)) {
+            MqttPersistentData message = (MqttPersistentData)_mqttLocalStorage.get(key);
+            return new String(message.getHeaderBytes());
+        }
+        else {
+            return null;
+        }
+    }
+    
+    /**
+     * Take a key-value pair and stores the pair into the local storage.
+     * 
+     * @param key The key to be stored.
+     * @param value The string value associated with the key.
+     * @throws MqttPersistenceException
+     * @throws IOException
+     */
+    public void setItem(String key, String value) throws MqttPersistenceException, IOException {
+        byte[] bytes = value.getBytes();
+        MqttPersistentData message = new MqttPersistentData(value,new byte[0], 0, 0, bytes, 0, bytes.length);
+        _mqttLocalStorage.put(key, message);
+    }
+    
+    /**
+     * Take a key and remove it from the local storage.
+     * 
+     * @param key The key to be removed.
+     * @throws MqttPersistenceException
+     */
+    public void removeItem(String key) throws MqttPersistenceException {
+        if (_mqttLocalStorage.containsKey(key)) {
+            _mqttLocalStorage.remove(key);
+        }
+    }
+    
+    /**
+     * Remove all keys in the local storage.
+     * 
+     * @throws MqttPersistenceException
+     */
+    public void clear() throws MqttPersistenceException {
+        _mqttLocalStorage.clear();
+    }
+    
+    /**
+     * Return a key with index n, or null if it is not present.
+     * 
+     * @param n Index for the key to be returned.
+     * @return The key with index n.
+     * @throws MqttException
+     */
+    public String key(Integer n) throws MqttException {
+        Enumeration keys = _mqttLocalStorage.keys();
+        
+        int cnt = 0;
+        
+        while (keys.hasMoreElements()) {
+            if (cnt == n) {
+                Object obj = keys.nextElement();
+                String message = (String)obj;
+                return message;
+            }
+            cnt++;
+            keys.nextElement();
+        }
+        
+        return null;
+        
+    }
+    
+    /**
+     * Return the number of keys stored in the local storage.
+     * @return The number of keys stored.
+     * @throws MqttPersistenceException
+     */
+    public Integer length() throws MqttPersistenceException {
+        Enumeration keys = _mqttLocalStorage.keys();
+        int cnt = 0;
+        
+        while (keys.hasMoreElements()) {
+            cnt++;
+            keys.nextElement();
+        }
+        
+        return cnt;
+    }
+    ///////////////////////////////////////////////////////////////////
+    ////                     private fields                        ////
+
+    /** Instance of MqttDefaultFilePersistence imported from Paho MQTT */
+    private MqttDefaultFilePersistence _mqttLocalStorage;
+}
---
+++ ptolemy/actor/lib/jjs/modules/localStorage/localStorage.js
@@ -0,0 +1,52 @@
+// Module supporting persistent local key-value storage.
+// Has the same interface as the node-persist (https://github.com/simonlast/node-persist),
+// except for values(callback) and fine-grained control functions (e.g. persist()).
+// Authors: Hokeun Kim
+// Copyright: http://terraswarm.org/accessors/copyright.txt
+//
+var LocalStorageHelper = Java.type('ptolemy.actor.lib.jjs.modules.localStorage.LocalStorageHelper');
+
+////////////////////
+// Create a Java instance of LocalStorageHelper, using the specific name (container + actor)
+// for the directory name.
+var storage = new LocalStorageHelper(actor.getContainer().getName() + '-' + actor.getName());
+
+////////////////////
+// Wrappers of the function in the java helper.
+
+////////////////////
+// Take a key and return its value from the local storage if the key exists,
+// otherwise, return null.
+module.exports.getItem = function(key) {
+    return storage.getItem(key);
+}
+
+////////////////////
+// Take a key-value pair and stores the pair into the local storage.
+module.exports.setItem = function(key, value) {
+    storage.setItem(key, value);
+}
+
+////////////////////
+// Take a key and remove it from the local storage.
+module.exports.removeItem = function(key) {
+    storage.removeItem(key);
+}
+
+////////////////////
+// Remove all keys in the local storage.
+module.exports.clear = function() {
+    storage.clear();
+}
+
+////////////////////
+// Return a key with index n, or null if it is not present.
+module.exports.key = function(n) {
+    return storage.key(n);
+}
+
+////////////////////
+// Return the number of keys stored in the local storage.
+module.exports.length = function() {
+    return storage.length();
+}
