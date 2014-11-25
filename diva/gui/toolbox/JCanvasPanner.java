c3e6bfaf9df892f7604f016f57fed915fe5d08e7
cxh <cxh>
2014-11-25 16:02:44 +0000

    Open a new empty vergil window, and resize it to be small enough that the panner window disappears. An exception appears: "java.awt.geom.NoninvertibleTransformException: Determinant is 0" The fix is that if one or more of the determinants is not greater than 0, act like _target is null.

--- diva/gui/toolbox/JCanvasPanner.java
+++ diva/gui/toolbox/JCanvasPanner.java
@@ -238,23 +238,34 @@ public class JCanvasPanner extends JPanel {
             AffineTransform current = canvas.getCanvasPane()
                     .getTransformContext().getTransform();
 
-            AffineTransform inverse;
+            AffineTransform inverse = null;
 
-            try {
-                // Here's a bug:
-                // Open a new empty vergil window, and resize it to be
-                // small enough that the panner window disappears.
-                // An exception appears: "java.awt.geom.NoninvertibleTransformException: Determinant is 0"
-                forward.concatenate(current.createInverse());
-                inverse = forward.createInverse();
-            } catch (NoninvertibleTransformException e) {
-                throw new RuntimeException("Failed to create an inverse of an AffineTransform.\n viewRect: "
-                        + viewRect
-                        + "\ncurrent: " + current
-                        + "\nforward: " + forward, e);
+            // Here's a bug:
+            // Open a new empty vergil window, and resize it to be
+            // small enough that the panner window disappears.
+            // An exception appears: "java.awt.geom.NoninvertibleTransformException: Determinant is 0"
+            // So, we check to see if the determinants are greater than 0.0.
+            if (current.getDeterminant() > 0.0
+                    && forward.getDeterminant() > 0.0) {
+                try {
+                    forward.concatenate(current.createInverse());
+                    inverse = forward.createInverse();
+                } catch (NoninvertibleTransformException ex) {
+                    throw new RuntimeException("Failed to create an inverse of an AffineTransform.\n viewRect: "
+                            + viewRect
+                            + "\ncurrent: " + current + " current determinant:" + current.getDeterminant()
+                            + "\nforward: " + forward + " forward determinant:" + forward.getDeterminant(),
+                            ex);
+                }
+            } else {
+                // One or more determinates are 0, treat this like _target == null.
+                Rectangle r = _getInsetBounds();
+                g.clearRect(r.x, r.y, r.width, r.height);
+                return;
             }
 
             Graphics2D g2d = (Graphics2D) g;
+                    
             g2d.transform(forward);
             canvas.paint(g);
             g2d.transform(inverse);
