7f73720bdb23e0dc68ad94155829699e5ce181ec
cxh <cxh>
2014-12-18 03:53:49 +0000

    Added import for Locale.

--- org/ptolemy/fmi/FMIModelDescription.java
+++ org/ptolemy/fmi/FMIModelDescription.java
@@ -33,6 +33,7 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 
 import org.ptolemy.fmi.type.FMIRealType;
