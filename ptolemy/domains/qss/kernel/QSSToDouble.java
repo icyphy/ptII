f182c762f348def8b8a4a875754d60e555e263d3
tsnouidui@lbl.gov <tsnouidui@lbl.gov>
2014-12-20 01:33:44 +0000

    Add the converter from QSStoken to a double.

---
+++ ptolemy/domains/qss/kernel/QSSToDouble.java
@@ -0,0 +1,96 @@
+/* An actor that converts a QSS token a double token
+
+ Copyright (c) 1998-2014 The Regents of the University of California.
+ All rights reserved.
+ Permission is hereby granted, without written agreement and without
+ license or royalty fees, to use, copy, modify, and distribute this
+ software and its documentation for any purpose, provided that the above
+ copyright notice and the following two paragraphs appear in all copies
+ of this software.
+
+ IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+ FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+ ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+ THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+ SUCH DAMAGE.
+
+ THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+ INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+ PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+ CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ ENHANCEMENTS, OR MODIFICATIONS.
+
+ PT_COPYRIGHT_VERSION_2
+ COPYRIGHTENDKEY
+
+ */
+package ptolemy.domains.qss.kernel;
+
+import ptolemy.actor.lib.conversions.Converter;
+import ptolemy.data.DoubleToken;
+import ptolemy.data.type.BaseType;
+import ptolemy.kernel.CompositeEntity;
+import ptolemy.kernel.util.IllegalActionException;
+import ptolemy.kernel.util.NameDuplicationException;
+
+///////////////////////////////////////////////////////////////////
+//// QSSToDouble
+
+/**
+ This actor converts a QSSToken to a double.  
+
+ @author Thierry S. Nouidui
+ @version $Id: QSSToDouble.java 69607 2014-07-30 17:07:26Z thn $
+ @Pt.ProposedRating Green (thn)
+ @Pt.AcceptedRating Red (thn)
+ */
+public class QSSToDouble extends Converter {
+    /** Construct an actor with the given container and name.
+     *  @param container The container.
+     *  @param name The name of this actor.
+     *  @exception IllegalActionException If the actor cannot be contained
+     *   by the proposed container.
+     *  @exception NameDuplicationException If the container already has an
+     *   actor with this name.
+     */
+    public QSSToDouble(CompositeEntity container, String name)
+            throws NameDuplicationException, IllegalActionException {
+        super(container, name);
+
+        output.setTypeEquals(BaseType.DOUBLE);
+    }
+
+    ///////////////////////////////////////////////////////////////////
+    ////                         public methods                    ////
+
+    /** Read exactly one token from the input and output the token
+     *  if it is double or a NaN token.
+     *  @exception IllegalActionException If there is no director.
+     */
+    @Override
+    public void fire() throws IllegalActionException {
+        super.fire();
+        QSSToken inputToken = null;
+        try {
+            inputToken = (QSSToken) input.get(0);
+        } catch (ClassCastException e) {
+        	throw new IllegalActionException(
+					" The input token" + inputToken.toString() 
+					+" cannot be cast to a QSSToken");
+        }
+        output.send(0, new DoubleToken(inputToken.doubleValue()));
+    }
+
+    /** Return false if the input port has no token, otherwise return
+     *  what the superclass returns (presumably true).
+     *  @exception IllegalActionException If there is no director.
+     */
+    @Override
+    public boolean prefire() throws IllegalActionException {
+        if (!input.hasToken(0)) {
+            return false;
+        }
+        return super.prefire();
+    }
+}
