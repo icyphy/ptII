6185b8a2a8eacddfdb0b8822e2905113f605045d
tsnouidui@lbl.gov <tsnouidui@lbl.gov>
2014-12-23 22:28:13 +0000

    Replace equals with >.

--- org/ptolemy/qss/solver/LIQSS1.java
+++ org/ptolemy/qss/solver/LIQSS1.java
@@ -305,7 +305,7 @@ public final class LIQSS1
             }
             // Here, tMostRecent + dt < quantEvtTimeMax.
             predQuantEvtTime = tMostRecent.addUnchecked(dt);
-            if( predQuantEvtTime.compareTo(tMostRecent) == 1 ) {
+            if( predQuantEvtTime.compareTo(tMostRecent) > 0 ) {
                 // Here, added {dt} and got a distinct, greater, time.
                 break;
             }
