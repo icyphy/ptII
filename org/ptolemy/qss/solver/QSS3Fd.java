722f44991fb18487b3a36c837f0f6a01296ab2ca
tsnouidui@lbl.gov <tsnouidui@lbl.gov>
2014-12-23 22:32:37 +0000

    Replace equals with >.

--- org/ptolemy/qss/solver/QSS3Fd.java
+++ org/ptolemy/qss/solver/QSS3Fd.java
@@ -297,7 +297,7 @@ public final class QSS3Fd
             }
             // Here, tMostRecent + dt < quantEvtTimeMax.
             predQuantEvtTime = tMostRecent.addUnchecked(dt);
-            if( predQuantEvtTime.compareTo(tMostRecent) == 1 ) {
+            if( predQuantEvtTime.compareTo(tMostRecent) > 0 ) {
                 // Here, added {dt} and got a distinct, greater, time.
                 break;
             }
