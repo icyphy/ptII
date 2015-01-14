cda997bdd1d8cb6666e4d8aefadaca0e7541423b
cxh <cxh>
2015-01-14 00:09:07 +0000

    Added JUnit test for cg.

---
+++ ptolemy/cg/kernel/generic/program/procedural/c/test/junit/JUnitCGCTest.java
@@ -0,0 +1,52 @@
+/* Run the auto/ tests using c cg.
+
+ Copyright (c) 2011-2012 The Regents of the University of California.
+ All rights reserved.
+ Permission is hereby granted, without written agreement and without
+ license or royalty fees, to use, copy, modify, and distribute this
+ software and its documentation for any purpose, provided that the above
+ copyright notice and the following two paragraphs appear in all copies
+ of this software.
+
+ IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+ FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+ ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+ THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+ SUCH DAMAGE.
+
+ THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+ INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+ PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+ CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ ENHANCEMENTS, OR MODIFICATIONS.
+
+ PT_COPYRIGHT_VERSION_2
+ COPYRIGHTENDKEY
+
+ */
+
+package ptolemy.cg.kernel.generic.program.procedural.c.test.junit;
+
+import ptolemy.util.test.junit.JUnitCGCTestBase;
+
+///////////////////////////////////////////////////////////////////
+//// JUnitCGCTest
+/**
+ *  Run the auto/ tests using c cg.
+ * <pre>
+ * (cd $PTII/ptolemy/cg/adapter/generic/program/procedural/c/test/junit; java -classpath ${PTII}:${PTII}/lib/ptjacl.jar:${PTII}/lib/junit-4.8.2.jar:${PTII}/lib/JUnitParams-0.3.0.jar org.junit.runner.JUnitCore package ptolemy.cg.kernel.generic.program.procedural.c.test.junit.JUnitCCGTest)
+ * </pre>
+ *
+ * <p> A copy of this file appears in each test/junit c cg subdirectory
+ * so that it is easy for developers to run tests.</p>
+
+ * @author Christopher Brooks
+ * @version $Id$
+ * @since Ptolemy II 10.0
+ * @Pt.ProposedRating Green (cxh)
+ * @Pt.AcceptedRating Green (cxh)
+ */
+public class JUnitCGCTest extends JUnitCGCTestBase {
+    // This class is empty, all the action occurs in the parent.
+}
