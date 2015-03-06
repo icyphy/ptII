6ea5e7c060a40376a06049a366cc9437ca2499d6
eal <eal>
2015-03-06 00:27:44 +0000

    Further attempts to get a browser accessor working. Still fails silently on attempt to start a server on a busy port.

--- ptolemy/actor/lib/jjs/modules/browser/browser.js
+++ ptolemy/actor/lib/jjs/modules/browser/browser.js
@@ -4,8 +4,8 @@
 // Display the specified HTML text.
 module.exports.display = function(html) {
     // FIXME: Probably should provide an initialize() function to start the server.
-    var helper = Java.type('ptolemy.actor.lib.jjs.modules.browser.VertxHelper');
-    // Use a port selection algorithm here to avoid port conflicts.
+    var helper = Java.type('ptolemy.actor.lib.jjs.modules.browser.VertxBrowserHelper');
+    // FIXME: Use a port selection algorithm here to avoid port conflicts.
     var port = 8080;
     var server = helper.createServer(port);
     server.setResponse(html);
