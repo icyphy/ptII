a9bd122f30300d3e93c7297c35af5c0092974ced
syuuhei_emoto@ihi.co.jp <syuuhei_emoto@ihi.co.jp>
2015-03-08 01:59:18 +0000

    refactoring, changing names of variables, and adding comments

--- org/ptolemy/optimization/ObjectiveFunction.java
+++ org/ptolemy/optimization/ObjectiveFunction.java
@@ -1,9 +1,44 @@
+/* 
+ Copyright (c) 2015 The Regents of the University of California.
+ All rights reserved.
+ Permission is hereby granted, without written agreement and without
+ license or royalty fees, to use, copy, modify, and distribute this
+ software and its documentation for any purpose, provided that the above
+ copyright notice and the following two paragraphs appear in all copies
+ of this software.
+
+ IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+ FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+ ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+ THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+ SUCH DAMAGE.
+
+ THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+ INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+ PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+ CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ ENHANCEMENTS, OR MODIFICATIONS.
+
+ PT_COPYRIGHT_VERSION_2
+ COPYRIGHTENDKEY
+
+ */
+
 package org.ptolemy.optimization;
 
 import ptolemy.math.DoubleMatrixMath;
 
 /**
- * Definition of the objective function and constraint functions.
+ * The class which defines the objective function and constraint functions.
+ * In the Composite Optimizer actor, this class is declared and method "calcFunction" is implemented.
+ * BarrierMethod calls method "calcFuncInternal" during optimization.
+ * 
+@author Shuhei Emoto
+@version $Id$
+@since Ptolemy II 11.0
+@Pt.ProposedRating Red (shuhei)
+@Pt.AcceptedRating
  */
 abstract class ObjectiveFunction {
     /**
@@ -169,7 +204,10 @@ abstract class ObjectiveFunction {
             dx[it] = currentX[it] - _previousX[it];
         }
         boolean is_hessian_calculated = updateHessianByBFGS(f0Hessian, dg0, dx);
-
+        if(!is_hessian_calculated) {
+            //TODO: We should store previous values and update when dx and dg have grown lager.
+        }
+        
         //Hessian of fi
         for(int i=0; i<fiHessians.length; i++) {
             double[] dgi = new double[fiGradients[i].length];
@@ -177,6 +215,9 @@ abstract class ObjectiveFunction {
                 dgi[it] = fiGradients[i][it] - _fiGradientsPrevious[i][it];
             }
             is_hessian_calculated = is_hessian_calculated & updateHessianByBFGS(fiHessians[i], dgi, dx);
+            if(!is_hessian_calculated) {
+                //TODO: We should store previous values and update when dx and dg have grown lager.
+            }
         }
         return is_hessian_calculated; //if at least one of the hessians are not calculated, return false.
     }
@@ -278,6 +319,7 @@ class ObjectiveFunctionForPhaseI extends ObjectiveFunction{
             input_x[i] = x[i];
         }
         _source.calcFuncInternal(input_x);
+        iterationCounter = _source.iterationCounter;
         //objective function: f(x,s) = s
         f0Result = x[x.length-1];
         for(int col=0; col<f0Gradient.length-1; col++) {
@@ -320,93 +362,6 @@ class ObjectiveFunctionForPhaseI extends ObjectiveFunction{
                 _source.currentX[i] = x[i];
             }
         }
-    }
-    
-    /*
-     * Private variables
-     */
-    private ObjectiveFunction _source;
-}
-
-/**
- * The class of approximated objective function.
- * In this class, non-linear multivariate function is approximated 
- * using hessians and gradients.
- * F0(X+dx) = F0(X) + 1/2 * dxT*H0*dx + g0*dx
- * Fi(X+dx) = Fi(X) + gi*dx
- * @author shuhei emoto
- */
-class ApproximatedObjectiveFunction extends ObjectiveFunction{
-    public ApproximatedObjectiveFunction(ObjectiveFunction a_source) {
-        super(a_source.currentX.length, a_source.fiResults.length);
-        _source = a_source;
-        
-        for(int i=0; i<_source.currentX.length; i++) {
-            currentX[i] = _source.currentX[i];
-        }
-
-        // Copy f0 Hessian and fi_gradients
-        for(int row=0; row<f0Hessian.length; row++) {
-            for(int col=0; col<f0Hessian[row].length; col++) {
-                f0Hessian[row][col] = _source.f0Hessian[row][col];
-            }
-        }
-        for(int i=0; i<fiResults.length; i++) {
-            for(int it=0; it<fiGradients[i].length; it++) {
-                fiGradients[i][it] = _source.fiGradients[i][it];
-            }
-            clearMatrix(fiHessians[i]); //fi_hessians are all zero.
-        }
-    }
-    @Override
-    public void addConstraints(int id, double val) {
-        _source.addConstraints(id, val);
-    }
-    @Override
-    public boolean calcFunction(double[] x) {
-        return false;
-    }
-    /**
-     * Objective function called in QPSolver.
-     * @param x : input variables
-     */
-    @Override
-    public void calcFuncInternal(double[] x) {
-        double[] dx = new double[x.length];
-        for(int i=0; i<x.length; i++) {
-            currentX[i] = x[i];
-            dx[i] = x[i]-_source.currentX[i];
-        }
-        
-        /////////////////////////////////////////
-        ///// result of objective function///////
-        // f(x+dx) = f(x)+(1/2)(dxTHdx)+g*dx 
-        double[] Qx = DoubleMatrixMath.multiply(dx, _source.f0Hessian);
-        f0Result = _source.f0Result;
-        for(int i=0; i<Qx.length; i++) {
-            f0Result += (0.5*Qx[i]*dx[i] + _source.f0Gradient[i]*dx[i]);
-        }
-        // df(x+dx) = df(x)+H*dx
-        for(int col=0; col<f0Gradient.length; col++) {
-            f0Gradient[col] = _source.f0Gradient[col]+Qx[col];
-        }
-        // Hessian(x+dx) = Hessian(x) was already copied in the constructor.
-        
-        ///////////////////////////////////////////////////
-        ////// Inequality constraints: 
-        // fi(x+dx) = fi(x)+g*dx
-        for(int i=0; i<fiResults.length; i++) {
-            fiResults[i] = _source.fiResults[i];
-            for(int col=0; col<dx.length; col++) {
-                fiResults[i] += _source.fiGradients[i][col]*dx[col];
-            }
-        }
-        // dfi(x+dx) = dfi(x) were already copied in the constructor.
-        // Hessian_i(x+dx) = 0 were initialized in the constructor.
-    }
-    @Override
-    public void resetConstraints() {
-        _source.resetConstraints();
     }
     
     /*
