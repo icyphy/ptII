d6d9db8615a85f126fa29c17849ab81f2ed18a35
robert.bui@berkeley.edu <robert.bui@berkeley.edu>
2015-03-08 02:36:07 +0000

    Fixed BooleanMultiplexer fireBlock issue. It now can be used with code generator.

--- ptolemy/cg/adapter/generic/program/procedural/c/adapters/ptolemy/actor/lib/BooleanMultiplexor.c
+++ ptolemy/cg/adapter/generic/program/procedural/c/adapters/ptolemy/actor/lib/BooleanMultiplexor.c
@@ -1,4 +1,4 @@
-/***fireBlock***/
+/***fireBlock($type)***/
 // This fire method assumes there is an input tokens at each one of the three
 // input ports when this method is envoked.
 if ($get(select)) {
