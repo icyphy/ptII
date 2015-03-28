42a14b0e8df7309fe2bddd2f309203ee410fa09b
naren.vasanad@berkeley.edu <naren.vasanad@berkeley.edu>
2015-03-28 22:20:36 +0000

    Changes to Display.c to prevent memory leak issues
    The issue seemed to be that input->get was not freeing the token that it was producing and hence memory was leaking. Need to figure out how to do this correctly. Right now the fix is by creating the token and freeing it in the display actor.

--- ptolemy/cg/adapter/generic/program/procedural/c/mbed/adapters/ptolemy/actor/lib/gui/Display.c
+++ ptolemy/cg/adapter/generic/program/procedural/c/mbed/adapters/ptolemy/actor/lib/gui/Display.c
@@ -1,12 +1,16 @@
 /***preinitBlock***/
 #include "mbed.h"
 
-DigitalOut myled(LED1);
+DigitalOut myled(LED2);
 /**/
 
 /*** IntPrintBlock($name, $channel) ***/
 if ($hasToken(input#$channel)) {
-        printf("%d\n", $get(input#$channel));
+        //printf("%d\n", $get(input#$channel));
+        Token *token = (*(input->get))((struct IOPort*) input, 0);
+        //printf("%d\n", (*(input->get))((struct IOPort*) input, 0)->payload.Boolean);
+        printf("%d\n", token->payload.Boolean);
+        free(token);
 }
 /**/
 
@@ -23,14 +27,21 @@ if ($hasToken(input#$channel)) {
 /**/
 
 /*** BooleanPrintBlock($name, $channel) ***/
-// Arduino!
-if ($hasToken(input#$channel)) {
-    wait(0.1);
-    if ($get(input#$channel)) {
+// mbed!!
+//HACK_MAN: Creating the token and freeing it at the end
+//          Should figure out why input->get doesn't free the token
+//          Previous code is in comments
+//if ($hasToken(input#$channel)) {
+if ((*(input->hasToken))((struct IOPort*) input, 0)) {
+    Token *token = (*(input->get))((struct IOPort*) input, 0);
+    wait(0.05);
+    //if ((*(input->get))((struct IOPort*) input, 0)->payload.Boolean) {
+    if (token->payload.Boolean) {
         myled = 1;
     } else {
         myled = 0;
     }
+    free(token);
 }
 /**/
 
