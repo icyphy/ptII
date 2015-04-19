f447eda55b69f6e8fd43ae7d18fa5910536850d2
robert.bui@berkeley.edu <robert.bui@berkeley.edu>
2015-04-19 04:03:50 +0000

    CG Adapter files for new accelerometer actor (for mbed demos).

---
+++ ptolemy/cg/adapter/generic/program/procedural/c/mbed/adapters/ptolemy/actor/lib/io/Accelerometer.c
@@ -0,0 +1,27 @@
+/***preinitBlock***/
+//#include "mbed.h"
+#include "FXOS8700Q.h"
+FXOS8700Q_acc acc( PTE25, PTE24, FXOS8700CQ_SLAVE_ADDR1);
+/**/
+
+/***initBlock***/
+acc.enable();
+/**/
+
+/***fireBlock***/
+// The following is ordinary C code, except for
+// the macro references to the input and output
+// ports.
+float faX, faY, faZ;
+acc.getX(&faX);
+acc.getY(&faY);
+acc.getZ(&faZ);
+$put(accX, faX); 
+$put(accY, faY); 
+$put(accZ, faZ); 
+/**/
+
+/***wrapupBlock***/
+/**/
+
+
