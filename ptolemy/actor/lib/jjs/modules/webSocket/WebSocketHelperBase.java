962688ec89fd267c9debf9933b159b4e3fe84269
Hokeun Kim <hokeunkim@berkeley.edu>
2015-04-29 09:58:26 +0000

    Created a common parent to make sure we create only one vert.x object for both WebSocketHelper and WebSocketServerHelpber.

---
+++ ptolemy/actor/lib/jjs/modules/webSocket/WebSocketHelperBase.java
@@ -0,0 +1,54 @@
+/* AnExecute a script in JavaScript.
+
+   Copyright (c) 2014 The Regents of the University of California.
+   All rights reserved.
+   Permission is hereby granted, without written agreement and without
+   license or royalty fees, to use, copy, modify, and distribute this
+   software and its documentation for any purpose, provided that the above
+   copyright notice and the following two paragraphs appear in all copies
+   of this software.
+
+   IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+   FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+   ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+   THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+   SUCH DAMAGE.
+
+   THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+   INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+   MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+   PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+   CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+   ENHANCEMENTS, OR MODIFICATIONS.
+
+   PT_COPYRIGHT_VERSION_2
+   COPYRIGHTENDKEY
+
+ */
+package ptolemy.actor.lib.jjs.modules.webSocket;
+
+import org.vertx.java.core.Vertx;
+import org.vertx.java.core.VertxFactory;
+
+///////////////////////////////////////////////////////////////////
+//// WebSocketHelper
+
+/**
+   A base class for WebSocketHelper and WebSocketServerHelper.
+   Creates only one Vert.x object and uses it internally.
+   
+   @author Hokeun Kim
+   @version $Id$
+   @since Ptolemy II 11.0
+   @Pt.ProposedRating Yellow (eal)
+   @Pt.AcceptedRating Red (bilung)
+ */
+public class WebSocketHelperBase {
+
+    ///////////////////////////////////////////////////////////////////
+    ////                     protected fields                        ////
+    
+    /** Instance of Vert.x Apparently we need only one. */
+    protected  static Vertx _vertx = VertxFactory.newVertx();
+
+}
