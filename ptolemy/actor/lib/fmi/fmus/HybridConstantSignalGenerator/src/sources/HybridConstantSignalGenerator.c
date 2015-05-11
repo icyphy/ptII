06698512f174248792a4b85a5436b57f9f4ab8c5
f.cremona <f.cremona>
2015-05-11 17:45:55 +0000

    FMI Hybrid CoSimulation, some FMUs from HSCC paper: - Constant Signal Generator - Gain - Adder - Piecewise constant periodic signal generator

---
+++ ptolemy/actor/lib/fmi/fmus/HybridConstantSignalGenerator/src/sources/HybridConstantSignalGenerator.c
@@ -0,0 +1,97 @@
+/* ---------------------------------------------------------------------------*
+ * An FMU that increments its output, but also has an fmi2GetMaxStepSize() method.
+ * Based on the FMUSDK inc fmu Copyright QTronic GmbH. All rights reserved.
+ * ---------------------------------------------------------------------------*/
+
+// Define class name and unique id.
+#define MODEL_IDENTIFIER HybridConstantSignalGenerator
+#define MODEL_GUID "{2aad68a9-f56f-4945-b473-8489d2f49518}"
+
+// Define model size.
+#define NUMBER_OF_REALS 2
+#define NUMBER_OF_INTEGERS 0
+#define NUMBER_OF_BOOLEANS 0
+#define NUMBER_OF_STRINGS 0
+#define NUMBER_OF_STATES 1
+#define NUMBER_OF_EVENT_INDICATORS 0
+
+// Include fmu header files, typedefs and macros.
+#include "fmuTemplate.h"
+
+// Define all model variables and their value references
+// conventions used here:
+// - if x is a variable, then macro x_ is its variable reference
+// - the vr of a variable is its index in array  r, i, b or s
+// - if k is the vr of a real state, then k+1 is the vr of its derivative
+#define output_ 0
+#define value_ 1
+#define STATES { output_ }
+
+#define present_ 0
+#define absent_ 1
+#define unknown_ 2
+
+// Ccalled by fmi2Instantiate.
+// Set values for all variables that define a start value.
+// Settings used unless changed by fmi2SetX before fmi2EnterInitializationMode.
+void setStartValues(ModelInstance *comp) {
+    r(output_) = r(value_);
+    hr(output_) = present_;
+    hr(value_) = present_;
+}
+
+// called by fmi2GetReal, fmi2GetInteger, fmi2GetBoolean, fmi2GetString, fmi2ExitInitialization
+// if setStartValues or environment set new values through fmi2SetXXX.
+// Lazy set values for all variable that are computed from other variables.
+void calculateValues(ModelInstance *comp) {
+    if (comp->state == modelInitializationMode) {
+    }
+}
+
+// called by fmiGetReal, fmiGetContinuousStates and fmiGetDerivatives
+fmi2Real getReal(ModelInstance* comp, fmi2ValueReference vr){
+    switch (vr)
+    {
+        case output_: 
+            hr(output_) = present_;
+            return r(value_);
+        case value_:
+            return r(value_);
+        default:
+            return 0;
+    }
+}
+
+// Used to set the next time event, if any.
+void eventUpdate(ModelInstance* comp, fmi2EventInfo* eventInfo, int timeEvent) {
+
+}
+
+/***************************************************
+Functions for FMI2 for Hybrid Co-Simulation
+****************************************************/
+
+fmi2Status fmi2RequiredTimeResolution (fmi2Component c, fmi2Integer *value) {
+    *value = -6;
+    return fmi2OK;
+}
+
+fmi2Status fmi2SetTimeResolution (fmi2Component c, fmi2Integer value) {
+    return fmi2OK;
+}
+
+fmi2Status fmi2GetMaxStepSize (fmi2Component c, fmi2Real *value) {
+    return fmi2OK;
+}
+
+fmi2Status fmi2HybridGetMaxStepSize (fmi2Component c, fmi2Integer *value) {
+    *value = 1;
+    return fmi2OK;
+}
+
+
+/* END */
+
+// include code that implements the FMI based on the above definitions
+#include "fmuTemplate.c"
+
