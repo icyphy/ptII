619fa6951df73dfd5f3abbf488ee9c0117447518
eal <eal>
2015-05-18 16:40:01 +0000

    Substantial refactoring of WebSocket accessor. Still incomplete. Need to provide server support

--- org/terraswarm/accessor/demo/WebSocketClient/NodeServer.js
+++ org/terraswarm/accessor/demo/WebSocketClient/NodeServer.js
@@ -5,9 +5,9 @@ var replyCnt = 0;
 
 wss.on('connection', function(ws) {
     ws.on('message', function(message) {
-        console.log('received: %s', message);
+        console.log('received: %s', JSON.stringify(message));
         replyCnt ++;
-        ws.send('port: 8080, reply count: ' + replyCnt);
+        ws.send(JSON.stringify({'port':8080, 'reply count':replyCnt}));
     });
 });
 
