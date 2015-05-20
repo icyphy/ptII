8f0860d2428b5e8e3a6c3dba30e14461790d4299
cxh <cxh>
2015-05-20 01:04:55 +0000

    Added test for reloading accessors.  Updated models to refer to accessors installed in the auto/accessors/ directory.

---
+++ org/terraswarm/accessor/test/AccessorAuto.tcl
@@ -0,0 +1,44 @@
+# Run the models and then reload the accessors
+#
+# @Author: Christopher Brooks
+#
+# @Version: $Id: FMUImport.tcl 71772 2015-03-18 22:34:22Z cxh $
+#
+# @Copyright (c) 2015 The Regents of the University of California.
+# All rights reserved.
+#
+# Permission is hereby granted, without written agreement and without
+# license or royalty fees, to use, copy, modify, and distribute this
+# software and its documentation for any purpose, provided that the
+# above copyright notice and the following two paragraphs appear in all
+# copies of this software.
+#
+# IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+# FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+# ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+# THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+#
+# THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+# INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+# PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+# CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+# ENHANCEMENTS, OR MODIFICATIONS.
+#
+# 						PT_COPYRIGHT_VERSION_2
+# 						COPYRIGHTENDKEY
+#######################################################################
+
+# Ptolemy II test bed, see $PTII/doc/coding/testing.html for more information.
+
+# Load up the test definitions.
+if {[string compare test [info procs test]] == 1} then {
+    source testDefs.tcl
+} {}
+
+#set VERBOSE 1
+
+# This file is a separate .tcl file so that when we run the tests using JUnit, we run the Accessor tests.
+# The alternative would be to create separate Java classes to support running reload on the Accessors.
+source $PTII/util/testsuite/AccessorAuto.tcl
