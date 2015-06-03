6dbebf39b68351ffc30b2aee128e3ca98ea37bdd
wasicek <wasicek>
2015-06-03 21:47:06 +0000

    Updated tests for shell module

--- ptolemy/actor/lib/jjs/modules/shell/test/test.py
+++ ptolemy/actor/lib/jjs/modules/shell/test/test.py
@@ -1,26 +1,47 @@
 #!/usr/bin/python
 
 import sys
+import os
+import signal
 from threading import Thread
+from threading import Event
 from time import sleep
 
-def sporadic(arg):
-    for i in range(arg):
+
+def sporadic(runtime, stop_event):
+    while(not stop_event.is_set()):
         print "goodbye"
-	sys.stdout.flush()
-        sleep(1)
+        sys.stdout.flush()
+        stop_event.wait(0.9)
+        
 
-def operate():
-	s = sys.stdin.readline().strip()
-	while s not in ['break', 'quit']:
-	    sys.stdout.write(s.upper() + '\n')
-	    sys.stdout.flush()
-	    s = sys.stdin.readline().strip()
+def operate(stop_event):
+    cnt = 0
+    s = sys.stdin.readline().strip()
+    while ((not stop_event.is_set()) and (cnt<4)) :
+        sys.stdout.write(s.upper() + '\n')
+        sys.stdout.flush()
+        s = sys.stdin.readline().strip()
+        cnt = cnt + 1
 
-thread1 = Thread(target = sporadic, args = (10, ))
-thread2 = Thread(target= operate, args=[])
+t1_stop = Event()
+t2_stop = Event()
+
+thread1 = Thread(target = sporadic, args = (5, t1_stop))
+thread2 = Thread(target= operate, args=[t2_stop])
+
 thread1.start()
 thread2.start()
+
+sleep(4)
+
+t1_stop.set()
+t2_stop.set()
+
 thread1.join()
 thread2.join()
+
+print "Finished."
+sys.stdout.flush()
+exit(0)
 
