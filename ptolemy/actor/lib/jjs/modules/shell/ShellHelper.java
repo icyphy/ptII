d70da75ab03c55a0b907700f7fbf8101ed48ad75
cxh <cxh>
2015-06-04 17:35:41 +0000

    Inserted copyright.

--- ptolemy/actor/lib/jjs/modules/shell/ShellHelper.java
+++ ptolemy/actor/lib/jjs/modules/shell/ShellHelper.java
@@ -1,3 +1,31 @@
+/*
+@Copyright (c) 2015 The Regents of the University of California.
+All rights reserved.
+
+Permission is hereby granted, without written agreement and without
+license or royalty fees, to use, copy, modify, and distribute this
+software and its documentation for any purpose, provided that the
+above copyright notice and the following two paragraphs appear in all
+copies of this software.
+
+IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+SUCH DAMAGE.
+
+THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ENHANCEMENTS, OR MODIFICATIONS.
+
+						PT_COPYRIGHT_VERSION_2
+						COPYRIGHTENDKEY
+
+
+*/
 
 
 package ptolemy.actor.lib.jjs.modules.shell;
@@ -12,17 +40,20 @@ import java.lang.ProcessBuilder.Redirect;
 import jdk.nashorn.api.scripting.ScriptObjectMirror;
 
 ///////////////////////////////////////////////////////////////////
-////ShellHelper
+//// ShellHelper
 
 /**
- A helper class for the shell accessor module. It provides functionality
- to invoke a command and control <i>stdin</i> and <i>stdout</i>. It forks 
- off a process that executes the specified command. A reader thread listens 
- to outputs asynchronously and forward out puts via the EventEmitter subsystem 
- to the <i>shell.js</i> module in Nashorn. 
- 
- @author Armin Wasicek
- @since Ptolemy II 10.0
+ * A helper class for the shell accessor module. It provides functionality
+ * to invoke a command and control <i>stdin</i> and <i>stdout</i>. It forks 
+ * off a process that executes the specified command. A reader thread listens 
+ * to outputs asynchronously and forward out puts via the EventEmitter subsystem 
+ * to the <i>shell.js</i> module in Nashorn. 
+ *
+ * @author Armin Wasicek
+ * @version $Id$
+ * @since Ptolemy II 11.0
+ * @Pt.ProposedRating Yellow (eal)
+ * @Pt.AcceptedRating Red (bilung)
  */
 public class ShellHelper  {
 
@@ -41,7 +72,7 @@ public class ShellHelper  {
      *  @param s The data to be sent to the process.
      */
     public synchronized void write(String s) throws IOException  {
-        if(out!=null && process.isAlive())  {
+        if (out!=null && process.isAlive())  {
             out.write(s);
             out.newLine();  
             out.flush();
@@ -60,18 +91,18 @@ public class ShellHelper  {
     
     /** Kills the process and the reader thread. */
     public void wrapup() throws IOException {
-        if(in!=null)  {
+        if (in!=null)  {
             in.close();
         }
-        if(out!=null) {
+        if (out!=null) {
             out.close();
         }
-        if((readerThread!=null) && (readerThread.isAlive()))  {
+        if ((readerThread!=null) && (readerThread.isAlive()))  {
             _readerThreadRunning=false;
         }
         _readerThreadRunning=false;
         try {
-            if(readerThread!=null)  {
+            if (readerThread!=null)  {
                 readerThread.join(10);
             }
         } catch (InterruptedException e) {
@@ -79,7 +110,7 @@ public class ShellHelper  {
         }
         readerThread=null;
   
-        if((process!=null) && (process.isAlive()))  {
+        if ((process!=null) && (process.isAlive()))  {
             process.destroy();
         }
         process=null;
@@ -135,7 +166,7 @@ public class ShellHelper  {
                         } catch (IOException e) {
                             e.printStackTrace();
                         }
-                        if(line!=null)  {
+                        if (line!=null)  {
                             _currentObj.callMember("emit", "message", line);    
                         }
                         else  {
