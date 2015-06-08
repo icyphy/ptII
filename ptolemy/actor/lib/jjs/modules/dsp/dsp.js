ab89507f4a61bc1684a5600630fea7eb4838383e
ilgea <ilgea>
2015-06-08 22:15:01 +0000

    added demo

--- ptolemy/actor/lib/jjs/modules/dsp/dsp.js
+++ ptolemy/actor/lib/jjs/modules/dsp/dsp.js
@@ -8,13 +8,32 @@
  * @copyright http://terraswarm.org/accessors/copyright.txt
  */
  
-
+/** Construct an instance of a Signal object type. This should be instantiated in your
+ *  JavaScript code as
+ *  <pre>
+ *     var dsp = require("dsp");
+ *     var dsp = require("dsp");  
+ *     var dspEngine = new dsp.Signal(); 
+ *     var player = new SignalProcessing.Signal();
+ *  </pre>
+ *  An instance of this object type implements the following functions: 
+ *  <ul>
+ *  <li> fft(signal): Return an object containing the real and imaginary parts of the Fast Fourier Transform ( FFT) of the input.
+ *  <li> (TODO) ifft(coef): Return an object containing the real and imaginary parts of the inverse FFT of the input sequence.
+ *  </ul>
+ *  @param options A JSON object with fields 'FIXME' and 'FIXME' that give the
+ *   TODO properties of the signal such as sample rate, etc. Provide reasonable
+ *   defaults.
+ */
 var SignalProcessing = Java.type('ptolemy.math.SignalProcessing');
  
 exports.Signal = function(options) {
     // Provide default values for options using the following common JavaScript idiom.
 }
  
+/** Return an FFT.
+ *  @param data An array of numbers. 
+ */
 exports.Signal.prototype.fft = function(data) {  
     var outputReal = [];
     outputReal = Java.type('ptolemy.math.SignalProcessing')["FFTRealOut(double[])"](data);
