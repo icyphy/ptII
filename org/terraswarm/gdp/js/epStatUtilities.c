f0a4d2cbb3a15026159321be044a3dfead3c4a48
cxh <cxh>
2015-06-10 22:22:45 +0000

    Removed the Java/GDP Interface because it is now in the gdp repo.

--- org/terraswarm/gdp/js/epStatUtilities.c
+++
@@ -1,8 +0,0 @@
-#include "ep/ep.h"
-#include "ep/ep_stat.h"
-
-/** Return true if the severity is less than warn. */
-int ep_stat_isok(EP_STAT ep_stat) {
-    fprintf(stderr, "ep_stat_isok: %x\n", ep_stat.code);
-    return EP_STAT_ISOK(ep_stat);
-}
