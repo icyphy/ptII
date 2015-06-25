81c049ab8bd66c4f9d82d6c0eb227f7c2762e526
eal <eal>
2015-06-25 22:00:22 +0000

    Ensure that a base accessor has a proper prototype defining default functions, and add a test

---
+++ org/terraswarm/accessor/test/auto/accessors/Base.js
@@ -0,0 +1,12 @@
+exports.setup = function() {
+   input('in1');
+   console.log('base set up');
+}
+exports.initialize = function() {
+   console.log('base init');
+}
+
+
+exports.wrapupBase = function() {
+   console.log('base wrapped up');
+}
