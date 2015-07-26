90806f14f6bbdfc2f6c027a0736002ed7ceb45e0
cxh <cxh>
2015-07-26 14:55:16 +0000

    Added keywords and eol-style properties

--- org/ptolemy/machineLearning/test/testDefs.tcl
+++ org/ptolemy/machineLearning/test/testDefs.tcl
@@ -2,7 +2,7 @@
 #
 # @Author: Christopher Hylands
 #
-# @Version: $Id: testDefs.tcl 69810 2014-08-21 00:00:29Z cxh $
+# @Version: $Id$
 #
 # @Copyright (c) 1997-2014 The Regents of the University of California.
 # All rights reserved.
