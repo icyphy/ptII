186f7c28cbe3e034bae4b0f65d3cac381e7bc118
eal <eal>
2015-07-28 21:47:27 +0000

    Added module supporting aprilTags detection

---
+++ ptolemy/actor/lib/jjs/modules/aprilTags/aprilTags.js
@@ -0,0 +1,42 @@
+/**
+Module to identify AprilTags in an image.
+To use this module, you have separately install a modified version of the
+AprilTags code by Edwin Olson. This has to be a separate install, because
+(sadly) the code is GPL'd.  See: http://ptolemy.eecs.berkeley.edu/~eal/aprilTags/
+
+@module aprilTags
+@authors Edward A. Lee
+@copyright http://terraswarm.org/accessors/copyright.txt
+ */
+ 
+////////////////////////////////////////////////////////////
+//// Private variables.
+
+var Filter = Java.type('edu.umich.eecs.april.image.AprilTagFilter');
+var filter = new Filter();
+
+////////////////////////////////////////////////////////////
+//// Functions provided in this module.
+
+/** Invoke the AprilTag detector on the specified image with the specified
+ *  options and return the result.  Any unrecognized options are ignored.
+ *  Note that previously applied options for a given filter will still be
+ *  used, even if they are not set in this call.
+ *  @param image The image to filter.
+ *  @param options An object whose fields specify filter options.
+ *  @return The filtered image.
+ */
+exports.filter = function(image, options) {
+    if (options) {
+        for (var optionName in options) {
+            // Look for a setter function for the option.
+            var setter = 'set' + optionName;
+            if (typeof filter[setter] === 'function') {
+                // Invoke the setter function.
+                filter[setter](options[optionName]);
+            }
+        }
+    }
+    // The second (null) argument declines to give a destination image.
+    return filter.filter(image, null);
+}
