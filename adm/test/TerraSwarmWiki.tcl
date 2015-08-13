3901a9cb95dd2f48bf74638568eb6eb023fe8172
cxh <cxh>
2015-08-13 00:14:14 +0000

    Added test that uses wget to download a file from the TerraSwarm wiki.  This is needed by 'ant jsdoc'.

---
+++ adm/test/TerraSwarmWiki.tcl
@@ -0,0 +1,49 @@
+# Tests for the Nightly Build: Download a protected file from the TerraSwarm wiki
+#
+# @Author: Christopher Brooks
+#
+# $Id$
+#
+# @Copyright (c) 2015 The Regents of the University of California.
+# All rights reserved.
+#
+# Permission is hereby granted, without written agreement and without
+# license or royalty fees, to use, copy, modify, and distribute this
+# software and its documentation for any purpose, provided that the
+# above copyright notice and the following two paragraphs appear in all
+# copies of this software.
+#
+# IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+# FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+# ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+# THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+#
+# THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+# INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+# PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+# CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+# ENHANCEMENTS, OR MODIFICATIONS.
+#
+# 						PT_COPYRIGHT_VERSION_2
+# 						COPYRIGHTENDKEY
+#######################################################################
+
+# Ptolemy II test bed, see $PTII/doc/coding/testing.html for more information.
+
+#set VERBOSE 1
+# Get rid of any previous lists of .java files etc.
+exec make clean
+
+# Load up the test definitions.
+if {[string compare test [info procs test]] == 1} then {
+    source testDefs.tcl
+} {}
+
+
+test TerraSwarmWiki-1.0 {Check to make sure that ant jsdoc is downloading from the TerraSwarm wiki} {
+    exec ./terraSwarmWiki
+    list {}
+} {{}}
+
