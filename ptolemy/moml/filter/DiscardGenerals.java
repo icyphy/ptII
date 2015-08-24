dc13f087d91d2b64b7697ccea19969e87922001b
cxh <cxh>
2015-08-24 22:23:26 +0000

    Added DiscardGenerals actor which is used by the RemoveGraphicalClasses filter to replace ImageDisplay actors, which have an input type of General.

---
+++ ptolemy/moml/filter/DiscardGenerals.java
@@ -0,0 +1,72 @@
+/* A simple sink actor that consumes and discards input tokens of type GENERAL.
+
+ Copyright (c) 2015 The Regents of the University of California.
+ All rights reserved.
+ Permission is hereby granted, without written agreement and without
+ license or royalty fees, to use, copy, modify, and distribute this
+ software and its documentation for any purpose, provided that the above
+ copyright notice and the following two paragraphs appear in all copies
+ of this software.
+
+ IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+ FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+ ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+ THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+ SUCH DAMAGE.
+
+ THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+ INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+ PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+ CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ ENHANCEMENTS, OR MODIFICATIONS.
+
+ PT_COPYRIGHT_VERSION_2
+ COPYRIGHTENDKEY
+
+ */
+package ptolemy.moml.filter;
+
+import ptolemy.actor.lib.Discard;
+import ptolemy.data.type.BaseType;
+import ptolemy.kernel.CompositeEntity;
+import ptolemy.kernel.util.IllegalActionException;
+import ptolemy.kernel.util.NameDuplicationException;
+
+///////////////////////////////////////////////////////////////////
+//// DiscardDoubles
+
+/**
+ A simple sink actor that consumes and discards input tokens of type General.
+
+ <p>The primary use of this actor is that it is used when the MoML
+ filter replaces actors that take a General input, like ImageDisplay.
+ A Discard actor is not sufficient because the type of the input is
+ not set. If backward type propagation is in use, a Discard actor that
+ has type General is needed to replace the ImageDIsplay that have inputs of
+ type General.</p>
+
+ @see ptolemy.actor.lib.Discard
+ @see ptolemy.moml.filter.DiscardDoubles
+ @author Christopher Brooks
+ @version $Id$
+ @since Ptolemy II 11.0
+ @Pt.ProposedRating Yellow (cxh)
+ @Pt.AcceptedRating Yellow (cxh)
+ */
+public class DiscardGenerals extends Discard {
+
+    /** Construct an actor with an input multiport with type general.
+     *  @param container The container.
+     *  @param name The name of this actor.
+     *  @exception IllegalActionException If the actor cannot be contained
+     *   by the proposed container.
+     *  @exception NameDuplicationException If the container already has an
+     *   actor with this name.
+     */
+    public DiscardGenerals(CompositeEntity container, String name)
+            throws NameDuplicationException, IllegalActionException {
+        super(container, name);
+        input.setTypeEquals(BaseType.GENERAL);
+    }
+}
