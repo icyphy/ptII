24b2224f3a77c8bf9ce27f3d2860717c97fc6c2b
cxh <cxh>
2015-09-22 23:44:49 +0000

    Started cleanup.

--- ptolemy/actor/lib/jjs/modules/contextAware/contextAware.js
+++ ptolemy/actor/lib/jjs/modules/contextAware/contextAware.js
@@ -2,19 +2,19 @@ var ContextAwareHelper = Java.type('ptolemy.actor.lib.jjs.modules.contextAware.C
 var helper = new ContextAwareHelper();
 
 /** A temporary function that returns existing list of service names
-*/
+ */
 exports.services = function() {
-  return Java.from(helper.availableServices());
-	  }
+    return Java.from(helper.availableServices());
+}
 
 exports.DiscoveryOfRESTService = DiscoveryOfRESTService;
 /** A discovery "class" that gathers details of a particular REST service. Not used now.
  */
 function DiscoveryOfRESTService() {
-   var self = this;
-   var serviceURL;
-   /** Discover IoT services */
-   this.discoverServices = function () {
+    var self = this;
+    var serviceURL;
+    /** Discover IoT services */
+    this.discoverServices = function () {
         var currentService;
         currentService = helper.getSelectedService();
         console.log(currentService);
