fe6ea433c0d62770e88ae530263a8d2e1a3fd4a3
eal <eal>
2015-09-28 16:36:26 +0000

    Added constructor that sets the value in one step

--- ptolemy/data/expr/SingletonParameter.java
+++ ptolemy/data/expr/SingletonParameter.java
@@ -69,6 +69,29 @@ public class SingletonParameter extends Parameter {
         super(container, name);
     }
 
+    /** Construct a Parameter with the given container, name, and Token.
+     *  The container argument must not be null, or a
+     *  NullPointerException will be thrown.  This parameter will use the
+     *  workspace of the container for synchronization and version counts.
+     *  If the name argument is null, then the name is set to the empty string.
+     *  The object is not added to the list of objects in the workspace
+     *  unless the container is null.
+     *  Increment the version of the workspace.
+     *  If the name argument is null, then the name is set to the empty
+     *  string.
+     *  @param container The container.
+     *  @param name The name.
+     *  @param token The Token contained by this Parameter.
+     *  @exception IllegalActionException If the parameter is not of an
+     *   acceptable class for the container.
+     *  @exception NameDuplicationException If the name coincides with
+     *   an parameter already in the container.
+     */
+    public SingletonParameter(NamedObj container, String name, ptolemy.data.Token token)
+            throws IllegalActionException, NameDuplicationException {
+        super(container, name, token);
+    }
+    
     ///////////////////////////////////////////////////////////////////
     ////                         public methods                    ////
 
