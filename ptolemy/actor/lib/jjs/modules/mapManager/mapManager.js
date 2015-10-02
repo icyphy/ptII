451ef6bc2c8d9290454fbbb05e0efb08fbc296bc
matt.weber <matt.weber>
2015-10-02 23:27:51 +0000

    Many modifications to mapManager and tests, because of decision to change Entities to have a list of their own aliases

--- ptolemy/actor/lib/jjs/modules/mapManager/mapManager.js
+++ ptolemy/actor/lib/jjs/modules/mapManager/mapManager.js
@@ -6,20 +6,27 @@
 //******************************************************************************************
 
 
-//Hash mapping "mapName" strings to "map objects"
+//Hash mapping "map._key()" strings to "map objects"
 //This is a simple hash
 var maps = {};
 
-//A set of EntityAlias (source, destination) pairs. The pairs are stored as parameters with the value true.
+//Todo: delete this? I made the decision to maitain each alias's entities with the entity.
+//Hash mapping source entities with their destination alter ego. he pairs are stored as parameters with the value true.
 //This is a keyed hash
-var entityAliases = {};
+//var entityAliases = {};
+
+//Todo: This isn't a structure you could iterate through if you wanted to make sure
+// all transitive coordinate transformation functions could be determined. Make it a
+// two level hash perhaps? Or attach the available transformations to a coordinate system
+// object
 
 //Hash mapping (domainName, codomainName) pairs as keys to functions
 //This is a keyed hash
 var coordinateTransformations= {};
 
-//Todo: create a global set of all entities across all maps?
-//var entities = {};
+//Hash mapping entity names to its entity
+//this is a simple hash
+var entities = {};
 
 
 //******************************************************************************************
@@ -30,37 +37,44 @@ var coordinateTransformations= {};
 //Assumes the key for the hash is an ordinary string
 function _simpleHashToString(hash){
 
-	var text = "{ ";
-	hashNames = Object.keys(hash);
+	//This closure exists to protect the scope of "text" and i and hashNames when there is a recursive call
+	//from the toString method below.
+	return function(){
 
-	for(var i = 0; i < hashNames.length; i++  ){
-		text += hashNames[i] + ": " + hash[hashNames[i]].toString();
-		if(i != hashNames.length -1){
-			text += ", ";
+		var hashNames = Object.keys(hash);
+		var text = "{ ";
+		for(var i = 0; i < hashNames.length; i++  ){
+			text += hashNames[i] +=": " + hash[hashNames[i]].toString();
+			if(i != (hashNames.length -1)){
+				text += ", ";
+			}
 		}
-	}
+		text += " }"
 
-	text += " }"
-
-	return text;
+		return text;
+	}();
 }
 
 //Assumes the key for the hash is a specially formatted string
 function _keyedHashToString(hash){
-	var text = "{ ";
-	hashNames = Object.keys(hash);
 
-	for(var i = 0; i < hashNames.length; i++  ){
-		text += _keyToStrings(hashNames[i]) + ": " + hash[hashNames[i]].toString();
-		if(i != hashNames.length -1){
-			text += ", ";
+
+	//This closure exists to protect the scope of "text" and i and hashNames when there is a recursive call
+	//from the toString method below.
+	return function(){
+		var text = "{ ";
+		hashNames = Object.keys(hash);
+
+		for(var i = 0; i < hashNames.length; i++  ){
+			text += _keyToStrings(hashNames[i]) + ": " + hash[hashNames[i]].toString();
+			if(i != hashNames.length -1){
+				text += ", ";
+			}
 		}
-	}
 
-	text += " }"
-
-	return text;
-
+		text += " }"
+		return text;
+	}();
 }
 
 
@@ -128,15 +142,17 @@ function _keyToStrings( key){
 	return args;
 }
 
-function _entityAliasKey(sourceEntity, destinationEntity ){
- 	if( ! ((sourceEntity instanceof Entity) && (destinationEntity instanceof Entity))  ){
- 		throw "Incorrect arguments to _EntityAliasKey.";
- 	}
+//REMOVED due to change of entity aliases as being stored with the entity
 
- 	var sourceString = sourceEntity.toString();
- 	var destinationString = destinationEntity.toString();
- 	return _stringsToKey([sourceString, destinationString ]);
-}
+// function _entityAliasKey(sourceEntity, destinationEntity ){
+//  	if( ! ((sourceEntity instanceof Entity) && (destinationEntity instanceof Entity))  ){
+//  		throw "Incorrect arguments to _EntityAliasKey.";
+//  	}
+
+//  	var sourceString = sourceEntity.toString();
+//  	var destinationString = destinationEntity.toString();
+//  	return _stringsToKey([sourceString, destinationString ]);
+// }
 
 function __coordinateTransformationKey( domainName, codomainName ){
 	if( ! ( typeof domainName === "string" && typeof codomainName === "string" )){
@@ -173,19 +189,19 @@ exports.coordinateTransformationsToString = function(){
 	return _keyedHashToString(coordinateTransformations);
 }
 
-exports.entityAliasesToString = function(){
-	return _keyedHashToString(entityAliases);
-}
+//Removed due to change of entity aliases as being stored with the entity
 
-//Removed because there is currently no global entities hash
-
-// exports.entitiesToString = function(){
-// 	return _hashToString(entities);
+// exports.entityAliasesToString = function(){
+// 	return _keyedHashToString(entityAliases);
 // }
+
+exports.entitiesToString = function(){
+	return _simpleHashToString(entities);
+}
 
 
 //******************************************************************************************
-//EntityAlias Specific functions
+//Entity Specific functions
 //******************************************************************************************
 
 //Todo what about other ontological metadata?
@@ -196,61 +212,118 @@ function Entity(name){
 
 	this.name = name;
 	this.containingMaps = {};
+	this.aliases = {};
 
 	this._key = function(){
 		return this.name;
 	}
 
 	this.toString = function(){
-		return "{ " + "name: " + this.name + ", " + "containingMaps: " + _simpleHashToString(this.containingMaps) + " }";
+		return "{ " + "name: " + this.name + " }";
+	}
+
+	this.containingMapsToString = function() {
+		return _simpleHashToString(this.containingMaps);
+	}
+
+	this.entityAliasesToString = function() {
+		return _simpleHashToString(this.aliases);
+	}
+
+
+	this.addAlias = function( alias ){
+	 	if( ! ( alias instanceof Entity )){
+	 		throw "Incorrect arguments to addAlias.";
+	 	}
+
+
+	 	//Check to see if this entity has been registered.
+	 	if( ! entities.hasOwnProperty(alias._key()) ){
+	 		throw "This entity is unregistered." + this.toString() + " Cannot give an unregistered entity an alias.";
+	 	} 
+
+
+	 	//Check to see if alias has been registered. If not, throw exception.
+	 	if( ! entities.hasOwnProperty(alias._key()) ){
+	 		throw "Attempt to add an unregistered entity as an alias to " + this.toString();
+	 	} 
+
+	 	if( this.aliases.hasOwnProperty(alias._key())){
+	 		return false;
+	 	} else {
+	 		this.aliases[entity_key()] = alias;
+	 		return true;
+	 	}
 	}
 }
 
 exports.Entity = Entity;
 
-exports.addEntityAlias = function(sourceEntity, destinationEntity ){
- 	if( ! ((sourceEntity instanceof Entity) && (destinationEntity instanceof Entity))  ){
- 		throw "Incorrect arguments to EntityAlias constructor.";
- 	}
+//Note that for now entities cannot be unregistered.
+exports.registerEntity = function(entity){
+	if( ! ( entity instanceof Entity) ){
+		throw "Incorrect arguments to registerEntity.";
+	}
 
-	var key = _entityAliasKey(sourceEntity, destinationEntity)
-
-	if(entityAliases.hasOwnProperty(key)){
+	if(entities.hasOwnProperty(entity._key()) ){
 		return false;
 	} else {
-		entityAliases[key] = true;
+		entities[entity._key()] = entity;
 		return true;
 	}
 }
 
-exports.removeEntityAlias = function(sourceEntity, destinationEntity ){
- 	if( ! ((sourceEntity instanceof Entity) && (destinationEntity instanceof Entity))  ){
- 		throw "Incorrect arguments to EntityAlias constructor.";
- 	}
 
-	var key = _entityAliasKey(sourceEntity, destinationEntity)
+//Todo: is it the responsibility of the caller to remove the entity from all of its maps , and all of the entities that have it as an alias first?
+//Todo: implement this better. The above makes me think this needs to be revised.
 
-	if(entityAliases.hasOwnProperty(key)){
-		delete entityAliases[key];
-		return true;
-	} else {
-		return false;
-	}
-}
+// exports.unregisterEntity = function(entity){
+// 	if( ! ( entity instanceof Entity) ){
+// 		throw "Incorrect arguments to unregisterEntity.";
+// 	}
 
-exports.entityAliasExists = function(sourceEntity, destinationEntity ){
- 	if( ! ((sourceEntity instanceof Entity) && (destinationEntity instanceof Entity))  ){
- 		throw "Incorrect arguments to entityAliasExists.";
- 	}
+// 	if(entities.hasOwnProperty(entity.key()) ){
+// 		delete entities[entity.key()];
+// 		return true;
+// 	} else {
+// 		return false;
+// 	}
+// }
 
- 	var key = _entityAliasKey(sourceEntity, destinationEntity)
 
- 	if(entityAliases.hasOwnProperty(key)){
- 		return true;
- 	} else {
- 		return false;
- 	}
-}
+
+//Removed because EntityAliases no longer exists
+
+// exports.removeEntityAlias = function(sourceEntity, destinationEntity ){
+//  	if( ! ((sourceEntity instanceof Entity) && (destinationEntity instanceof Entity))  ){
+//  		throw "Incorrect arguments to EntityAlias constructor.";
+//  	}
+
+// 	var key = _entityAliasKey(sourceEntity, destinationEntity)
+
+// 	if(entityAliases.hasOwnProperty(key)){
+// 		delete entityAliases[key];
+// 		return true;
+// 	} else {
+// 		return false;
+// 	}
+// }
+
+//Removed because EntityAliases no longer exists
+
+// exports.entityAliasExists = function(sourceEntity, destinationEntity ){
+//  	if( ! ((sourceEntity instanceof Entity) && (destinationEntity instanceof Entity))  ){
+//  		throw "Incorrect arguments to entityAliasExists.";
+//  	}
+
+//  	var key = _entityAliasKey(sourceEntity, destinationEntity)
+
+//  	if(entityAliases.hasOwnProperty(key)){
+//  		return true;
+//  	} else {
+//  		return false;
+//  	}
+// }
 
 //******************************************************************************************
 //CoordinateTransformation Specific functions
@@ -270,8 +343,7 @@ function CoordinateSystem(name ){
 
 exports.CoordinateSystem =  CoordinateSystem;
 
-
-//Function to add a coordinate transform.
+//Todo consider changing the language to "registerCoordainteTransformation" for consistancy.
 //Todo break the assumption that there only has to be one transformation from a particular domain to codomain.
 exports.addCoordinateTransformation = function addCoordinateTransformation( domainName, codomainName, transformation ){
 	if( ! ( typeof domainName === "string" && typeof codomainName === "string" && typeof transformation === "function" )  ){
@@ -299,6 +371,10 @@ exports.getCoordinateTransformation = function getCoordinateTransformation( doma
 	return coordinateTransformations[ key ];
 }
 
+//Todo, think through the possible messy complications of unregistering a coordinate transformation.
+//This might be impossible.
+//exports.unregisterCoordinateTransformation
+
 //******************************************************************************************
 //Maps Specific functions
 //******************************************************************************************
@@ -313,10 +389,15 @@ function Map(mapName, spaceType, coordinateSystem){
 	this.spaceType = spaceType;
 	this.coordinateSystem = coordinateSystem;
 	this.mapName = mapName;
-	this.entities = {}; //set of entities this map contains
+	this.mapEntities = {}; //set of entities this map contains
+
+
+	this.mapEntitiesToString = function(){
+		return _simpleHashToString(this.mapEntities);
+	}
 
 	this.toString = function() {
-		return "{mapName: " + mapName + ", spaceType: " + this.spaceType + ", coordinateSystem: " + this.coordinateSystem + "}";
+		return "{ mapName: " + mapName + ", spaceType: " + this.spaceType + ", coordinateSystem: " + this.coordinateSystem + " }";
 	}
 
 	this._key = function(){
@@ -328,11 +409,29 @@ function Map(mapName, spaceType, coordinateSystem){
 			throw "Incorrect arguments to Map.addEntity."
 		}
 
-		if( entities.hasOwnProperty(entity.toString()) || entity.containingMaps.hasOwnProperty(this.toString()) ){
+		if (! entities.hasOwnProperty(entity._key()) ){
+			throw "Cannot add unregistered entity to map" + this.toString();
+		}
+
+		if( this.mapEntities.hasOwnProperty(entity._key()) || entity.containingMaps.hasOwnProperty(this._key()) ){
 			return false;
 		} else {
-			this.entities[entity._key()] = true;
-			entity.containingMaps[this._key()] = true;
+			this.mapEntities[entity._key()] = entity;
+			entity.containingMaps[this._key()] = this;
+			return true;
+		}
+	}
+
+	this.removeEntity = function(entity) {
+		if ( ! (entity instanceof Entity )  ){
+			throw "Incorrect arguments to Map.addEntity."
+		}
+
+		if( ! (this.mapEntities.hasOwnProperty(entity._key()) && entity.containingMaps.hasOwnProperty(this._key())) ){
+			return false;
+		} else {
+			delete this.mapEntities[entity._key()];
+			delete entity.containingMaps[this._key()];
 			return true;
 		}
 	}
@@ -340,41 +439,44 @@ function Map(mapName, spaceType, coordinateSystem){
 
 exports.Map = Map;
 
-exports.addMap = function(map){
+exports.registerMap = function(map){
 	if( ! (map instanceof Map) ){
-		throw "Incorrect arguments to addMap.";
+		throw "Incorrect arguments to registerMap.";
 	}
 
 	if(map == undefined){
-		throw "undefined argument to addMap."
+		throw "undefined argument to registerMap."
 	}
 
-	if(maps.hasOwnProperty( map.mapName) ){
+	if(maps.hasOwnProperty( map._key()) ){
 		return false;
 	} else {
-		maps[map.mapName] = map;
+		maps[map._key()] = map;
 		return true;
 	}
 }
 
-exports.getMap = function(mapName){
-	if( ! (typeof mapName === "string")  ){
+exports.getMapFromKey = function(mapKey){
+	if( ! (typeof mapKey === "string")  ){
 		throw "Incorrect arguments to getMap";
 	}
 
-	return maps[mapName];
+	return maps[mapKey];
 
 }
 
-exports.removeMap = function(mapName ){
- 	if( typeof mapName !== "string" ){
- 		throw "Incorrect arguments to removeMap";
- 	}
+//Todo think through the implications of unregistering a map.
+//Should this even be allowed?
 
-	if(maps.hasOwnProperty(mapName)){
-		delete maps[mapName];
-		return true;
-	} else {
-		return false;
-	}
-}
+// exports.unregisterMap = function(map){
+//  	if( !( map instanceof Map ) ){
+//  		throw "Incorrect arguments to unregisterMap";
+//  	}
+
+// 	if(maps.hasOwnProperty(map._key())){
+// 		delete maps[map._key()];
+// 		return true;
+// 	} else {
+// 		return false;
+// 	}
+// }
