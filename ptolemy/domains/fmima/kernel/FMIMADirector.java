e9c0990c4e7e997a4b269c07ddacc2e8a7d703ac
f.cremona <f.cremona>
2015-11-05 02:47:41 +0000

    Fixed compilation errors

--- ptolemy/domains/fmima/kernel/FMIMADirector.java
+++ ptolemy/domains/fmima/kernel/FMIMADirector.java
@@ -1,131 +1,116 @@
-/* A director that implements the FMI Master Algorithm Hybrid Co-simulation model of computation. 
-
-   Copyright (c) 2015 The Regents of the University of California.
-   All rights reserved.
-   Permission is hereby granted, without written agreement and without
-   license or royalty fees, to use, copy, modify, and distribute this
-   software and its documentation for any purpose, provided that the above
-   copyright notice and the following two paragraphs appear in all copies
-   of this software.
-
-   IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
-   FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
-   ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
-   THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
-   SUCH DAMAGE.
-
-   THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
-   INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
-   MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
-   PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
-   CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
-   ENHANCEMENTS, OR MODIFICATIONS.
-
-   PT_COPYRIGHT_VERSION_2
-   COPYRIGHTENDKEY
-
- */
 package ptolemy.domains.fmima.kernel;
 
-import ptolemy.domains.de.kernel.DEDirector;
+import java.util.Iterator;
+
+import ptolemy.actor.Actor;
+import ptolemy.actor.CompositeActor;
+import ptolemy.actor.TimeRegulator;
+import ptolemy.actor.util.Time;
+import ptolemy.domains.sr.kernel.SRDirector;
 import ptolemy.kernel.CompositeEntity;
 import ptolemy.kernel.util.IllegalActionException;
 import ptolemy.kernel.util.NameDuplicationException;
+import ptolemy.kernel.util.Nameable;
 import ptolemy.kernel.util.Workspace;
 
-/**
- * A director that implements the FMI Master Algorithm Hybrid Co-simulation model of computation. 
- * @author Fabio Cremona
- * @version $Id: FMUImport.java 73691 2015-10-22 15:12:47Z tsnouidui@lbl.gov $
- * @since Ptolemy II 11.0
- * @Pt.ProposedRating Red (cxh)
- * @Pt.AcceptedRating Red (cxh)
- */
-public class FMIMADirector extends DEDirector {
-    /** Construct a director in the default Workspace.
-     *  @exception IllegalActionException If the name has a period in it, or
-     *   the director is not compatible with the specified container.
-     *  @exception NameDuplicationException If the container is not a
-     *   CompositeActor and the name collides with an entity in the container.
-     */   
+public class FMIMADirector extends SRDirector {
+
+
     public FMIMADirector() throws IllegalActionException, NameDuplicationException {
-        super();
+        // TODO Auto-generated constructor stub
+        _isFirstFire = true;
     }
 
-    /** Construct a director in the given Workspace.
-     *  @param workspace The workspace. Container of this director.
-     *  @exception IllegalActionException If the name has a period in it, or
-     *   the director is not compatible with the specified container.
-     *  @exception NameDuplicationException If the container is not a
-     *   CompositeActor and the name collides with an entity in the container.
-     */
     public FMIMADirector(Workspace workspace) throws IllegalActionException, NameDuplicationException {
         super(workspace);
+        _isFirstFire = true;
+        // TODO Auto-generated constructor stub
     }
 
-    /** Construct a director in the given container with the given name.
-     *  The container argument must not be null, or a
-     *  NullPointerException will be thrown.
-     *  If the name argument is null, then the name is set to the
-     *  empty string. Increment the version number of the workspace.
-     *  @param container Container of this director.
-     *  @param name Name of this director.
-     *  @exception IllegalActionException If the name has a period in it, or
-     *   the director is not compatible with the specified container.
-     *  @exception NameDuplicationException If the container is not a
-     *   CompositeActor and the name collides with an entity in the container.
-     */
     public FMIMADirector(CompositeEntity container, String name)
             throws IllegalActionException, NameDuplicationException {
         super(container, name);
+        _isFirstFire = true;
+        // TODO Auto-generated constructor stub
     }
     
-    /** Fire actors according to events in the event queue. The actual
-     *  selecting which events to process is done in _fire(). _fire()
-     *  will return whether the previous firing was successful. According
-     *  to this information, it is decided whether _fire() should be called
-     *  again in order to keep processing events. After each actor firing,
-     *  book keeping procedures are called, to keep track of the current
-     *  state of the scheduler. The model time of the next events are also
-     *  checked to see if we have produced an event of smaller timestamp.
-     *  @see #_fire
-     *  @exception IllegalActionException If we couldn't process an event
-     *  or if an event of smaller timestamp is found within the event queue.
-     */
     @Override
-    public void fire() throws IllegalActionException {
+    public void fire() throws IllegalActionException {        
         if (_debugging) {
-            _debug("========= " + this.getName() + " director fires at "
-                    + getModelTime() + "  with microstep as " + _microstep);
+            _debug("FMIMADirector: invoking fire().");
         }
-
-        // NOTE: This fire method does not call super.fire()
-        // because this method is very different from that of the super class.
-        // A BIG while loop that handles all events with the same tag.
-        while (true) {
-            int result = _fire();
-            assert result <= 1 && result >= -1;
-            if (result == 1) {
-                continue;
-            } else if (result == -1) {
-                _noActorToFire();
-                return;
-            } // else if 0, keep executing
-
-            // after actor firing, the subclass may wish to perform some book keeping
-            // procedures. However in this class the following method does nothing.
-            _actorFired();
-
-            if (!_checkForNextEvent()) {
-                break;
-            } // else keep executing in the current iteration
-        } // Close the BIG while loop.
-
-        // Since we are now actually stopping the firing, we can set this false.
-        _stopFireRequested = false;
+        
+        if (getModelTime().getDoubleValue() > getStopTime()) {
+            stop();
+        }
+        /**
+         *  Calling FixedPointDirector.fire()
+         *  When super.fire() returns we reached a fixed point:
+         *  all FMUs propagated I/O signals.
+         */
+        super.fire();
 
         if (_debugging) {
-            _debug("DE director fired!");
+            _debug("FMIMADirector: returned super.fire().");
+        }
+        
+        /** 
+         *  We can now compute the step size of the FMU
+         *  Consult all actors that implement TimeRegulator interface.
+         *  FMUs for example, can implement TimeRegulator interface
+         *  to check the acceptance of a step size.
+         */
+        
+        Time proposedFmiTime = getModelTime().add(1E-8);//Time.POSITIVE_INFINITY;
+        // FIXME: move this class var into this class.
+        
+        Nameable container = getContainer();
+        Iterator<?> actors = ((CompositeActor) container).deepEntityList()
+                .iterator();
+        while (actors.hasNext()) {
+            Actor actor = (Actor) actors.next();
+            if (actor instanceof TimeRegulator) {
+                Time modifiedTime = ((TimeRegulator) actor).proposeTime(proposedFmiTime);
+                if (proposedFmiTime.compareTo(modifiedTime) > 0) {
+                    proposedFmiTime = modifiedTime;
+                }
+                if (_debugging) {
+                    _debug("FMU " + actor.getFullName() + " proposed: " + modifiedTime.getLongValue() + " at time: " + getModelTime());
+                }
+            }
+        }
+        if (_debugging) {
+            _debug("Computed future time: " + proposedFmiTime);
         }
     }
+    
+    @Override
+    public boolean postfire() throws IllegalActionException {
+
+        boolean result=  super.postfire();
+
+//        setModelTime(_proposedFmiTime);
+        
+        return result;
+    }
+
+//    @Override
+//    public boolean prefire() throws IllegalActionException {
+//        if (getModelTime().getDoubleValue() >= getStopTime()) {
+//            stop();
+//        }
+////        _proposedFmiTime = getModelTime().add(1.0);
+//        boolean result = super.prefire();
+////        setModelTime(_proposedFmiTime);
+//        return result;
+//    }
+
+    ///////////////////////////////////////////////////////////////////
+    ////                       protected variables                 ////
+    
+    protected boolean _isFirstFire;
+    
+    ///////////////////////////////////////////////////////////////////
+    ////                       private variables                   ////
+
 }
