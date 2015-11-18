6652c3dd14904f6f071432a9f0f21c77ae158dc2
cxh <cxh>
2015-11-18 00:57:13 +0000

    Javadoc and cleanup.

--- ptolemy/actor/lib/jjs/modules/contextAwareTest/ContextAwareHelperTest.java
+++ ptolemy/actor/lib/jjs/modules/contextAwareTest/ContextAwareHelperTest.java
@@ -34,6 +34,7 @@ import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Iterator;
 
+import javax.swing.JButton;
 import javax.swing.JOptionPane;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
@@ -62,22 +63,18 @@ public class ContextAwareHelperTest {
     ///////////////////////////////////////////////////////////////////
     ////                         public methods                    ////
 
-    // FIXME: Alphabetize these methods.
-    
-    /** Return an array of iot REST  service names that are available to the user.
-     *  @return FIXME: returns what?   
+    /** Return an array of iot REST service names that are available
+     *  to the user.
+     *  @return an array of iot REST service names that are available.
      */
     public  String[] availableServices() {
         return iotServiceList;
-           
     }
    
-        
     /** Dialog Box to select a service the accessor mimics. 
-     *  @param sensors FIXME
+     *  @param sensors The names of the sensors.
      */
     public void chooseSensor(String[] sensors) {
-            
         _sensorService = (String)JOptionPane.showInputDialog(null,
                 "Choose sensor to retreive data from:\n",
                 "Sensor Selection",
@@ -86,6 +83,12 @@ public class ContextAwareHelperTest {
                 "1");
     }
         
+    /** Convert an XML data format to JSON data format using the XML
+     * class from org.json.xml.
+     * 
+     * @param response is the input in xml format
+     * @return the json formatted data in an array 
+     */
     public ArrayList  convertXMLtoJSON(String response) {
         try{
             JSONObject xmlJson = XML.toJSONObject(response);
@@ -103,39 +106,74 @@ public class ContextAwareHelperTest {
             return null;
         }
     }
-    /** FIXME: need to implement a discovery process that takes into account user's preferences and locations
-     * currently, just present the set of known services to users and return the selected service
-     * @return _selectedServiceParam 
+
+    /** Return the selected service.
+     * @return The selected service.
      */
     public String discoverServices() {
+         // FIXME: need to implement a discovery process that takes
+         //into account user's preferences and locations  currently,
+         //just present the set of known services to users and return
+         //the selected service
+
         this.setSelectedService(iotServiceList);
         return _selectedServiceParam;
     }
     
+
+    /** Return the list of output choices of GSN service. Currently,
+     * the list of choices is hard coded. Eventually, this list should
+     * come from a discovery process
+     * @return The list of data type to be extracted from the service
+     */
     public String[] getGsnOutput() {
         return gsnOutputPort;
     }
+
+    /** Return the list of output choices of Firebase
+     * service. Currently, the list of choices is hard
+     * coded. Eventually, this list should come from a discovery
+     * process
+     * @return The list of data type to be extracted from Firebase.
+     */
     public String[] getFirebaseOutput(){
-        return firebaseOutputPort;
+        return _firebaseOutputPort;
     }
-    
+
+    /** Return the list of output choices of Paraimpu
+     * service. Currently, the list of choices is hard
+     * coded. Eventually, this list should come from a discovery
+     * process
+     * @return The list of data type to be extracted from Paraimpu.
+     */
     public String[] getParaimpuOutput() {
         return paraimpuOutputPort;
     }
     
+    /** Return the name of the selected service. 
+     * Currently, this method returns the string "GSN".
+     * Eventually, this method will return data from the GUI.
+     * @return The name of the service chosen by the user.
+     * @see #setSelectedService(String[])
+     */
     public String getSelectedService() {
         //return _selectedService;
         return "GSN";
     }
+
     //FIXME:retrieve data entered by a user from a text field in the GUI
   
+    /** Return the parameters associated with the selected
+     *  service. Currently just return a hard wired url for testing.
+     * @param selectedService The name of the service that was selected.
+     * @return An array of service parameters. Not used now
+     */
     public String getSelectedServiceParameter(String selectedService){
         //return GUI.textFields.get(index).getText();
         if (selectedService.equals("GSN")) {
             return "pluto.cs.txstate.edu";}
         else return "";
     }
-        
         
     /** Get the name of a particular sensor in a service.
      * @return The name of the service.   
@@ -144,16 +182,22 @@ public class ContextAwareHelperTest {
         return _sensorService;
     }
         
-       
-    // FIXME: Use a Javadoc comment here.  Complete sentence that ends in a period.  Add @param.
-    //initializes the list of available iot REST services and creates GUI
+
+    /** Initializes the list of available iot REST services and
+     *  creates a GUI for a user to make the selection. Not used
+     *  currently Currently, this method does nothing.
+     *  @param list known list of services
+     *  @see #getSelectedService()
+     */
+
     public void setSelectedService(String[] list) {
         GUI = new ContextAwareGUI(list);
         addListeners();
     }
-        
-    // FIXME: Use a Javadoc comment here.  Complete sentence that ends in a period.  Add @param.
-    //creates list of parameters specific to the middleware chosen
+
+    /** Create a list of parameters specific to the middleware chosen.
+     * @param parameters The names of the parameters.
+     */
     public void setParameters(String[] parameters) {
             
         int length = Array.getLength(parameters);
@@ -172,13 +216,28 @@ public class ContextAwareHelperTest {
         }
     }
  
+    
+    /** The search button. */
+    public JButton searchButton;
+
+    /** The output choices of the FireBase service. */
+    public String[] _firebaseOutputPort = {"microwave", "microwaveStatus", "pastValues"};
+
+    /** The output choices of the GSN service. */
+    public String[] _gsnOutputPort = {"sound", "sensorName"};
+
+    /** The output choices of the Paraimpu service. */
+    public String[] _paraimpuOutputPort = {"payload","producer", "sensorId"};
+
     // FIXME:Need comments
-    public String[] iotServiceList = {"GSN", "Paraimpu", "Firebase"};
-    public String[] gsnOutputPort = {"sound", "sensorName"};
-    public String[] firebaseOutputPort = {"microwave", "microwaveStatus", "pastValues"};
+
     public String[] paraimpuOutputPort = {"payload","producer", "thingId"};
     public String [] defaultParamList =  {"username", "password","ipAddress", "port"};
         
+    public String[] iotServiceList = {"GSN", "Paraimpu", "Firebase"};
+    public String[] gsnOutputPort = {"sound", "sensorName"};
+    public String[] firebaseOutputPort = {"microwave", "microwaveStatus", "pastValues"};
+
     ///////////////////////////////////////////////////////////////////
     ////                         private methods                   ////
 
@@ -187,7 +246,7 @@ public class ContextAwareHelperTest {
     private void addListeners() {
         // FIXME: use a complete sentence below.
         //when button is pressed, call getSensors from accessor
-        GUI.btnSearch.addActionListener(new ActionListener() {
+        GUI.searchButton.addActionListener(new ActionListener() {
                 @Override
                 public void actionPerformed(ActionEvent e) {
                     try {
@@ -201,10 +260,10 @@ public class ContextAwareHelperTest {
             });
         // FIXME: use a complete sentence below.
         //when ever the type of REST service changes, get the parameters required
-        GUI._list.addListSelectionListener(new ListSelectionListener() {
+        GUI.servicesList.addListSelectionListener(new ListSelectionListener() {
                 @Override        
                 public void valueChanged(ListSelectionEvent e) {
-                    _selectedServiceParam = new String((String) GUI._list.getSelectedValue());               
+                    _selectedServiceParam = new String((String) GUI.servicesList.getSelectedValue());               
                     try {
                         System.out.println("getParameters" + _selectedServiceParam);
                         setParameters(defaultParamList);
