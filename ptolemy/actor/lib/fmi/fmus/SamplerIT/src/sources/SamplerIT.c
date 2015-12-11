052724a686e859070f47b65ec1a19fcf67058f90
f.cremona <f.cremona>
2015-12-11 23:52:50 +0000

    SamplerIT sample the signal on the first input port according to events on the second input port. It handles heterogeneous time resolution

---
+++ ptolemy/actor/lib/fmi/fmus/SamplerIT/src/sources/SamplerIT.c
@@ -0,0 +1,129 @@
+/* ---------------------------------------------------------------------------*
+ * An FMU that increments its output, but also has an fmi2GetMaxStepSize() method.
+ * Based on the FMUSDK inc fmu Copyright QTronic GmbH. All rights reserved.
+ * ---------------------------------------------------------------------------*/
+
+// Define class name and unique id.
+#define MODEL_IDENTIFIER SamplerIT
+#define MODEL_GUID "{f634b92b-06b0-4101-917f-f74477d7895f}"
+
+// Define model size.
+#define NUMBER_OF_REALS 3
+#define NUMBER_OF_INTEGERS 0
+#define NUMBER_OF_BOOLEANS 0
+#define NUMBER_OF_STRINGS 0
+#define NUMBER_OF_STATES 0
+#define NUMBER_OF_EVENT_INDICATORS 0
+
+// Include fmu header files, typedefs and macros.
+#include "fmuTemplate.h"
+
+// Define all model variables and their value references
+// conventions used here:
+// - if x is a variable, then macro x_ is its variable reference
+// - the vr of a variable is its index in array  r, i, b or s
+// - if k is the vr of a real state, then k+1 is the vr of its derivative
+
+#define STATES {  }
+
+#define present_ 0
+#define absent_ 1
+#define unknown_ 2
+
+#define RESOLUTION 6
+
+#define output_ 0
+#define input_ 1
+#define sampler_ 2
+
+// Ccalled by fmi2Instantiate.
+// Set values for all variables that define a start value.
+// Settings used unless changed by fmi2SetX before fmi2EnterInitializationMode.
+void setStartValues(ModelInstance *comp) {
+    r(output_) = 0;
+    hr(output_) = absent_;
+    hr(input_) = absent_;
+    hr(sampler_) = absent_;
+    pos(0) = fmi2False;
+}
+
+// called by fmi2GetReal, fmi2GetInteger, fmi2GetBoolean, fmi2GetString, fmi2ExitInitialization
+// if setStartValues or environment set new values through fmi2SetXXX.
+// Lazy set values for all variable that are computed from other variables.
+void calculateValues(ModelInstance *comp) {
+    if (comp->state == modelInitializationMode) {
+        if (hr(sampler_) == present_) {
+            r(output_) = r(input_);
+            hr(output_) = hr(input_);
+        }
+    }
+    else {
+        if (hr(sampler_) == present_ && pos(0) == fmi2False) {
+            r(output_) = r(input_);
+            hr(output_) = hr(input_);
+        }
+        else {
+            hr(output_) = absent_;
+        }
+    }
+}
+
+// called by fmiGetReal, fmiGetContinuousStates and fmiGetDerivatives
+fmi2Real getReal(ModelInstance* comp, fmi2ValueReference vr){
+    switch (vr)
+    {
+        case output_:
+            return r(output_);
+        case input_:
+            return r(input_);
+        case sampler_:
+            return r(sampler_);
+        default:
+            return 0;
+    }
+}
+
+// used to check if there is any state event
+fmi2Real getEventIndicator(ModelInstance* comp, int z) {
+    if (hr(sampler_) == present_ && pos(0) == fmi2False) {
+        pos(0) = fmi2True;
+        return -1;
+    }
+    else {
+        pos(0) = fmi2False;
+    }
+    return 0;
+}
+
+void doStep(ModelInstance* comp, fmi2IntegerTime hLocal, int inBetween) {
+
+    if (inBetween == 0) {
+        comp->time += hLocal;
+        if (hLocal > 0) comp->microstep = 0;
+        else comp->microstep++;
+    } else {
+
+    }
+
+}
+
+// Used to set the next time event, if any.
+void eventUpdate(ModelInstance* comp, fmi2EventInfo* eventInfo, int timeEvent,
+        fmi2Integer localStepSize, int inBetween) {
+
+}
+
+fmi2IntegerTime getMaxStepSize(ModelInstance *comp) {
+    fmi2IntegerTime communicationStepSize;
+    if (pos(0) == fmi2True)
+        communicationStepSize = 0;
+    else
+        communicationStepSize = 1000;
+    return communicationStepSize;
+}
+
+
+/* END */
+
+// include code that implements the FMI based on the above definitions
+#include "fmuTemplate.c"
