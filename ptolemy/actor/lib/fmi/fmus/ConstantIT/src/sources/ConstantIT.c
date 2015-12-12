4bdbec59580e1569dac245e0396c3a3458f1c6c3
f.cremona <f.cremona>
2015-12-12 00:10:29 +0000

    This is an FMU with constant output. It is an untimed component and it handles the new hybrid API

---
+++ ptolemy/actor/lib/fmi/fmus/ConstantIT/src/sources/ConstantIT.c
@@ -0,0 +1,103 @@
+/* ---------------------------------------------------------------------------*
+ * An FMU that increments its output, but also has an fmi2GetMaxStepSize() method.
+ * Based on the FMUSDK inc fmu Copyright QTronic GmbH. All rights reserved.
+ * ---------------------------------------------------------------------------*/
+
+// Define class name and unique id.
+#define MODEL_IDENTIFIER ConstantIT
+#define MODEL_GUID "{ec5e222e-5f4b-4805-97aa-964da19679b0}"
+
+// Define model size.
+#define NUMBER_OF_REALS 2
+#define NUMBER_OF_INTEGERS 0
+#define NUMBER_OF_BOOLEANS 0
+#define NUMBER_OF_STRINGS 0
+#define NUMBER_OF_STATES 0
+#define NUMBER_OF_EVENT_INDICATORS 0
+
+// Include fmu header files, typedefs and macros.
+#include "fmuTemplate.h"
+
+// Define all model variables and their value references
+// conventions used here:
+// - if x is a variable, then macro x_ is its variable reference
+// - the vr of a variable is its index in array  r, i, b or s
+// - if k is the vr of a real state, then k+1 is the vr of its derivative
+
+#define STATES {  }
+
+#define present_ 0
+#define absent_ 1
+#define unknown_ 2
+
+#define RESOLUTION 6
+
+#define output_ 0
+#define value_ 1
+
+// Ccalled by fmi2Instantiate.
+// Set values for all variables that define a start value.
+// Settings used unless changed by fmi2SetX before fmi2EnterInitializationMode.
+void setStartValues(ModelInstance *comp) {
+    r(output_) = r(value_);
+    hr(output_) = present_;
+    hr(value_) = present_;
+}
+
+// called by fmi2GetReal, fmi2GetInteger, fmi2GetBoolean, fmi2GetString, fmi2ExitInitialization
+// if setStartValues or environment set new values through fmi2SetXXX.
+// Lazy set values for all variable that are computed from other variables.
+void calculateValues(ModelInstance *comp) {
+    if (comp->state == modelInitializationMode) {
+        hr(output_) = present_;
+        r(output_) = r(value_);
+    }
+}
+
+// called by fmiGetReal, fmiGetContinuousStates and fmiGetDerivatives
+fmi2Real getReal(ModelInstance* comp, fmi2ValueReference vr){
+    switch (vr)
+    {
+        case output_:
+            return r(output_);
+        case value_:
+            return r(value_);
+        default:
+            return 0;
+    }
+}
+
+// used to check if there is any state event
+fmi2Real getEventIndicator(ModelInstance* comp, int z) {
+    switch (z) {
+        default: return 0;
+    }
+}
+
+void doStep(ModelInstance* comp, fmi2IntegerTime hLocal, int inBetween) {
+
+    if (inBetween == 0) {
+        comp->time += hLocal;
+        if (hLocal > 0) comp->microstep = 0;
+        else comp->microstep++;
+    } else {
+
+    }
+}
+
+// Used to set the next time event, if any.
+void eventUpdate(ModelInstance* comp, fmi2EventInfo* eventInfo, int timeEvent,
+        fmi2Integer localStepSize, int inBetween) {
+
+}
+
+fmi2IntegerTime getMaxStepSize(ModelInstance *comp) {
+    fmi2IntegerTime communicationStepSize = 1000; // default value
+    return communicationStepSize;
+}
+
+
+/* END */
+
+// include code that implements the FMI based on the above definitions
+#include "fmuTemplate.c"
