11520872059281db3c32e06a59b1d019ab5d1a6d
cxh <cxh>
2015-12-27 21:46:16 +0000

    Summary: Updated the classpath in the comment so that the test runs.

--- ptolemy/actor/lib/jjs/modules/xbee/test/XBeeHello.java
+++ ptolemy/actor/lib/jjs/modules/xbee/test/XBeeHello.java
@@ -55,12 +55,12 @@ public class XBeeHello {
      *
      * <p>To list the ports, use:</p>
      * <pre>
-     * java -classpath ${PTII}/lib/lib/nrjavaserial-3.11.0.devel.jar:${PTII}/lib/lib/xbjlib-1.1.0.nrjavaserial.jar:${PTII}/lib/lib/slf4j-api-1.7.12.jar:${PTII}/lib/lib/slf4j-nop-1.7.12.jar ptolemy.actor.lib.jjs.modules.xbee.test.XBeeHello
+     * java -classpath ${PTII}:${PTII}/lib/nrjavaserial-3.11.0.devel.jar:${PTII}/lib/xbjlib-1.1.0.nrjavaserial.jar:${PTII}/lib/slf4j-api-1.7.12.jar:${PTII}/lib/slf4j-nop-1.7.12.jar ptolemy.actor.lib.jjs.modules.xbee.test.XBeeHello
      * </pre>
      *
      * <p>To send data on a port, append the port name:</p>
      * <pre>
-     * java -classpath ${PTII}/lib/lib/nrjavaserial-3.11.0.devel.jar:${PTII}/lib/lib/xbjlib-1.1.0.nrjavaserial.jar:${PTII}/lib/lib/slf4j-api-1.7.12.jar:${PTII}/lib/lib/slf4j-nop-1.7.12.jar ptolemy.actor.lib.jjs.modules.xbee.test.XBeeHello /dev/xxyy
+     * java -classpath ${PTII}:${PTII}/lib/nrjavaserial-3.11.0.devel.jar:${PTII}/lib/xbjlib-1.1.0.nrjavaserial.jar:${PTII}/lib/slf4j-api-1.7.12.jar:${PTII}/lib/slf4j-nop-1.7.12.jar ptolemy.actor.lib.jjs.modules.xbee.test.XBeeHello /dev/xxyy
      * </pre>
      */
     public static void main(String[] args) {
