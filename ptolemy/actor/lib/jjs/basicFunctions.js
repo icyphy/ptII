69a18a6f07738200cc81e9eec7d33354738725e7
eal <eal>
2016-01-30 01:35:32 +0000

    Put in placeholder for stack trace reporting

--- ptolemy/actor/lib/jjs/basicFunctions.js
+++ ptolemy/actor/lib/jjs/basicFunctions.js
@@ -85,6 +85,19 @@ function clearTimeout(handle) {
  *  @param message The message for the exception.
  */
 function error(message) {
+    // Print a stack trace to the console.
+    /*
+    console.error(message);
+    console.error('------------------------- error stack trace:');
+    var e = new Error('dummy');
+    var stack = e.stack.replace(/^[^\(]+?[\n$]/gm, '')
+            .replace(/^\s+at\s+/gm, '')
+            .replace(/^Object.<anonymous>\s*\(/gm, '{anonymous}()@')
+            .split('\n');
+    console.error(stack);
+    console.error('-------------------------');
+    */
+
     actor.error(message);
 }
 
