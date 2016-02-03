eb3a2b903e443aaa769063e58b2675e0082faae6
cxh <cxh>
2016-02-03 01:18:08 +0000

    Added @version.  Added copyright if necessary.

--- org/terraswarm/accessor/test/auto/accessors/DerivedAgainA.js
+++ org/terraswarm/accessor/test/auto/accessors/DerivedAgainA.js
@@ -1,7 +1,31 @@
+// Copyright (c) 2014-2015 The Regents of the University of California.
+// All rights reserved.
+
+// Permission is hereby granted, without written agreement and without
+// license or royalty fees, to use, copy, modify, and distribute this
+// software and its documentation for any purpose, provided that the above
+// copyright notice and the following two paragraphs appear in all copies
+// of this software.
+
+// IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+// FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+// ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+// THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+// SUCH DAMAGE.
+
+// THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+// INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+// PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+// CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+// ENHANCEMENTS, OR MODIFICATIONS.
+
 /** Doubly derived accessor that overrides a function (inputHandler) of the base
  *  two levels up, it should produce an output value of 2 on both outputs.
+ *
  *  @accessor TestDerivedC
  *  @author Edward A. Lee
+ *  @version $$Id$$
  */ 
 exports.setup = function() {
    this.extend('DerivedC');
