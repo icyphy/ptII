1d0db06463c482ee61fa1241443f099a27fd8b13
cxh <cxh>
2016-02-09 23:44:34 +0000

    Ran Eclipse Cleanup

--- ptolemy/gui/ExtensionFilenameFilter.java
+++ ptolemy/gui/ExtensionFilenameFilter.java
@@ -34,17 +34,21 @@ import java.util.Locale;
 import java.util.Set;
 
 /**
- * An implementation of both javax.swing.filechooser.FileFilter and java.io.FilenameFilter
- * that only accepts files that have one of the registered extensions.
+ * An implementation of both javax.swing.filechooser.FileFilter and
+ * java.io.FilenameFilter that only accepts files that have one of the
+ * registered extensions.
  * <p>
- * This class is provided in Ptolemy II to support usage with both java.awt.FileDialog and javax.swing.JFileChooser.
- * As it appears that on MacOSX the FileDialog implementation is better than the JFileChooser, Ptolemy II has some logic in
- * ptolemy.gui.PtGUIUtilities.useFileDialog() to decide which "file-picker"-component to use.
- * Several Ptolemy II classes use this utility while still passing a common filter instance to the selected component.
- * (e.g. ptolemy.gui.Query, ptolemy.gui.Top ...)
+ * This class is provided in Ptolemy II to support usage with both
+ * java.awt.FileDialog and javax.swing.JFileChooser. As it appears that on
+ * MacOSX the FileDialog implementation is better than the JFileChooser, Ptolemy
+ * II has some logic in ptolemy.gui.PtGUIUtilities.useFileDialog() to decide
+ * which "file-picker"-component to use. Several Ptolemy II classes use this
+ * utility while still passing a common filter instance to the selected
+ * component. (e.g. ptolemy.gui.Query, ptolemy.gui.Top ...)
  * </p>
  * <p>
- * The javax.swing.filechooser.FileNameExtensionFilter in the JDK can only be used with JFileChooser.
+ * The javax.swing.filechooser.FileNameExtensionFilter in the JDK can only be
+ * used with JFileChooser.
  * </p>
  *
  * @version $Id$
@@ -53,163 +57,177 @@ import java.util.Set;
  */
 public class ExtensionFilenameFilter extends PtFilenameFilter {
 
-  /**
-   * Construct a file filter that filters out all files that do
-   * not have one of the extensions in the given list.
-   *
-   * @param extensions the file extensions to use
-   */
-  public ExtensionFilenameFilter(List<String> extensions) {
-    for (String ext : extensions) {
-      registerExtension(ext);
-    }
-  }
+	/**
+	 * Construct a file filter that filters out all files that do not have one
+	 * of the extensions in the given list.
+	 *
+	 * @param extensions
+	 *            the file extensions to use
+	 */
+	public ExtensionFilenameFilter(List<String> extensions) {
+		for (String ext : extensions) {
+			registerExtension(ext);
+		}
+	}
 
-  /**
-   * Creates a filter that accepts the given file type,
-   * specified by a number of extensions and a meaningful description of the file types involved.
-   *
-   * @param description a description of the types of files with one of the given extensions
-   * @param extensions the file extensions to use
-   */
-  public ExtensionFilenameFilter(String description, List<String> extensions) {
-    for (String ext : extensions) {
-      registerExtension(ext);
-    }
-    setDescription(description);
-  }
+	/**
+	 * Creates a filter that accepts the given file type, specified by a number
+	 * of extensions and a meaningful description of the file types involved.
+	 *
+	 * @param description
+	 *            a description of the types of files with one of the given
+	 *            extensions
+	 * @param extensions
+	 *            the file extensions to use
+	 */
+	public ExtensionFilenameFilter(String description, List<String> extensions) {
+		for (String ext : extensions) {
+			registerExtension(ext);
+		}
+		setDescription(description);
+	}
 
-  /**
-   * Creates a filter that accepts the given file type,
-   * specified by a number of extensions and a meaningful description of the file types involved.
-   *
-   * @param description a description of the types of files with one of the given extensions
-   * @param extensions the file extensions to use
-   */
-  public ExtensionFilenameFilter(String description, String... extensions) {
-    for (String ext : extensions) {
-      registerExtension(ext);
-    }
-    setDescription(description);
-  }
+	/**
+	 * Creates a filter that accepts the given file type, specified by a number
+	 * of extensions and a meaningful description of the file types involved.
+	 *
+	 * @param description
+	 *            a description of the types of files with one of the given
+	 *            extensions
+	 * @param extensions
+	 *            the file extensions to use
+	 */
+	public ExtensionFilenameFilter(String description, String... extensions) {
+		for (String ext : extensions) {
+			registerExtension(ext);
+		}
+		setDescription(description);
+	}
 
-  /**
-   * Return true if the given file has one of the registered extensions,
-   * or is a directory. Otherwise, or if the file is null, return false.
-   * <p>
-   * Files whose name begins with "." are not accepted.
-   * </p>
-   *
-   * @param file The file to be checked.
-   * @return true if the given file has one of the registered extensions,
-   * or is a directory.
-   */
-  @Override
-  public boolean accept(File file) {
-    if (file == null) {
-      return false;
-    } else if (file.isDirectory()) {
-      return true;
-    } else {
-      String ext = getExtension(file);
-      return (ext != null && _registeredExtensions.contains(ext));
-    }
-  }
+	/**
+	 * Return true if the given file has one of the registered extensions, or is
+	 * a directory. Otherwise, or if the file is null, return false.
+	 * <p>
+	 * Files whose name begins with "." are not accepted.
+	 * </p>
+	 *
+	 * @param file
+	 *            The file to be checked.
+	 * @return true if the given file has one of the registered extensions, or
+	 *         is a directory.
+	 */
+	@Override
+	public boolean accept(File file) {
+		if (file == null) {
+			return false;
+		} else if (file.isDirectory()) {
+			return true;
+		} else {
+			String ext = getExtension(file);
+			return (ext != null && _registeredExtensions.contains(ext));
+		}
+	}
 
-  /**
-   * Return true if the given file name has one of the registered extensions,
-   * or is a directory. Otherwise, or if the directory or name is null, return false.
-   * <p>
-   * Files whose name begins with "." are not accepted.
-   * </p>
-   *
-   * @param directory the parent directory of the file
-   * @param name the name of the file.
-   * @return true if the given file has one of the registered extensions,
-   * or is a directory.
-   */
-  @Override
-  public boolean accept(File directory, String name) {
-    if(name==null || directory==null) {
-      return false;
-    } else {
-      return accept(new File(directory, name));
-    }
-  }
+	/**
+	 * Return true if the given file name has one of the registered extensions,
+	 * or is a directory. Otherwise, or if the directory or name is null, return
+	 * false.
+	 * <p>
+	 * Files whose name begins with "." are not accepted.
+	 * </p>
+	 *
+	 * @param directory
+	 *            the parent directory of the file
+	 * @param name
+	 *            the name of the file.
+	 * @return true if the given file has one of the registered extensions, or
+	 *         is a directory.
+	 */
+	@Override
+	public boolean accept(File directory, String name) {
+		if (name == null || directory == null) {
+			return false;
+		} else {
+			return accept(new File(directory, name));
+		}
+	}
 
-  /**
-   * @return The human readable description of the types of files
-   * accepted by this filter.
-   */
-  @Override
-  public String getDescription() {
-    return _description;
-  }
+	/**
+	 * @return The human readable description of the types of files accepted by
+	 *         this filter.
+	 */
+	@Override
+	public String getDescription() {
+		return _description;
+	}
 
-  /**
-   * Set the human readable description of the types of files
-   * accepted by this filter.
-   *
-   * @param description the human readable description of the types of files
-   * accepted by this filter.
-   */
-  public void setDescription(String description) {
-    _description = description;
-  }
+	/**
+	 * Set the human readable description of the types of files accepted by this
+	 * filter.
+	 *
+	 * @param description
+	 *            the human readable description of the types of files accepted
+	 *            by this filter.
+	 */
+	public void setDescription(String description) {
+		_description = description;
+	}
 
-  /**
-   * @return description + registered extensions
-   */
-  @Override
-  public String toString() {
-    return getDescription() + " : " + _registeredExtensions;
-  }
+	/**
+	 * @return description + registered extensions
+	 */
+	@Override
+	public String toString() {
+		return getDescription() + " : " + _registeredExtensions;
+	}
 
-  ///////////////////////////////////////////////////////////////////
-  ////                         protected methods                 ////
-  /**
-   * Register an additional extension to accept.
-   *
-   * @param extension The extension to be added.
-   */
-  protected void registerExtension(String extension) {
-    if (extension == null || "".equals(extension.trim())) {
-      return;
-    } else {
-      _registeredExtensions.add(extension.trim().toLowerCase(Locale.getDefault()));
-    }
-  }
+	///////////////////////////////////////////////////////////////////
+	////              protected methods                           ////
+	/**
+	 * Register an additional extension to accept.
+	 *
+	 * @param extension
+	 *            The extension to be added.
+	 */
+	protected void registerExtension(String extension) {
+		if (extension == null || "".equals(extension.trim())) {
+			return;
+		} else {
+			_registeredExtensions.add(extension.trim().toLowerCase(Locale.getDefault()));
+		}
+	}
 
-  ///////////////////////////////////////////////////////////////////
-  ////                         private methods                   ////
-  /**
-   * Return the extension of the given file.
-   * <p>
-   * If the file is null, if it's name does not contain a '.',
-   * or if it ends in a '.', this method returns null.
-   * </p>
-   * @param file
-   * @return the extension of the file or null if the file is null, or if it has no extension.
-   */
-  private String getExtension(File file) {
-    if (file == null) {
-      return null;
-    } else {
-      String fileName = file.getName();
-      int i = fileName.lastIndexOf('.');
-      if (i > 0 && i < fileName.length() - 1) {
-        String ext = fileName.substring(i + 1);
-        return ext.toLowerCase(Locale.getDefault());
-      } else {
-        return null;
-      }
-    }
-  }
+	///////////////////////////////////////////////////////////////////
+	////                  private methods                          ////
+	/**
+	 * Return the extension of the given file.
+	 * <p>
+	 * If the file is null, if it's name does not contain a '.', or if it ends
+	 * in a '.', this method returns null.
+	 * </p>
+	 * 
+	 * @param file
+	 * @return the extension of the file or null if the file is null, or if it
+	 *         has no extension.
+	 */
+	private String getExtension(File file) {
+		if (file == null) {
+			return null;
+		} else {
+			String fileName = file.getName();
+			int i = fileName.lastIndexOf('.');
+			if (i > 0 && i < fileName.length() - 1) {
+				String ext = fileName.substring(i + 1);
+				return ext.toLowerCase(Locale.getDefault());
+			} else {
+				return null;
+			}
+		}
+	}
 
-  ///////////////////////////////////////////////////////////////////
-  ////                         private fields                    ////
-  private Set<String> _registeredExtensions = new HashSet<>();
+	///////////////////////////////////////////////////////////////////
+	////               private fields                              ////
+	private Set<String> _registeredExtensions = new HashSet<>();
 
-  private String _description = null;
+	private String _description = null;
 }
-
