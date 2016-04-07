856bcac3ceb08d5e105418e67493d266ec2e11c8
cxh <cxh>
2016-04-07 17:43:01 +0000

    Adding support of the JavaScript actor to the Accessor code generator.

---
+++ ptolemy/cg/adapter/generic/accessor/adapters/ptolemy/actor/lib/jjs/JavaScript.java
@@ -0,0 +1,53 @@
+/* An adapter class for ptolemy.actor.lib.jjs.JavaScript
+
+ Copyright (c) 2016 The Regents of the University of California.
+ All rights reserved.
+ Permission is hereby granted, without written agreement and without
+ license or royalty fees, to use, copy, modify, and distribute this
+ software and its documentation for any purpose, provided that the above
+ copyright notice and the following two paragraphs appear in all copies
+ of this software.
+
+ IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+ FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+ ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+ THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+ SUCH DAMAGE.
+
+ THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+ INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+ PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+ CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ ENHANCEMENTS, OR MODIFICATIONS.
+
+ PT_COPYRIGHT_VERSION_2
+ COPYRIGHTENDKEY
+
+ */
+package ptolemy.cg.adapter.generic.accessor.adapters.ptolemy.actor.lib.jjs;
+
+//////////////////////////////////////////////////////////////////////////
+//// JavaScript
+
+/**
+ An adapter class for ptolemy.actor.lib.jjs.JavaScript.
+
+ @author Christopher Brooks
+ @version $Id$
+ @since Ptolemy II 11.0
+ @Pt.ProposedRating Red (eal)
+ @Pt.AcceptedRating Red (eal)
+ */
+public class JavaScript
+extends
+ptolemy.cg.adapter.generic.program.procedural.adapters.ptolemy.actor.lib.jjs.JavaScript {
+
+    /**
+     *  Construct the JavaScript adapter.
+     *  @param actor the associated actor
+     */
+    public JavaScript(ptolemy.actor.lib.jjs.JavaScript actor) {
+        super(actor);
+    }
+}
