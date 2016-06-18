b121e6ad30d0d3ad71e25b69c1026b40f1c17ef6
cxh <cxh>
2016-06-18 15:45:20 +0000

    gdp-0.5-0.jar now gets updated.

--- adm/test/Release.tcl
+++ adm/test/Release.tcl
@@ -194,7 +194,8 @@ test release-3.1 {Run svn status and look for files that should be checked in.  
 ?       vendors/jogl} {
 ?       vendors/misc/javamail/javax.mail.jar} {
 ?       vendors/universalJavaApplicationStub} {
-M       lib/diva.jar}}
+M       lib/diva.jar} {
+M       lib/gdp-0.5-0.jar}}
         if { $result == $result2 } {
 	    puts "Result was:\n$result\nWhich is ok"
             set resultMessage {}
