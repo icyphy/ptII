8df0c03bdb3cb99db313e32dd3e29cd0c6822a14
cxh <cxh>
2016-11-28 17:34:35 +0000

    Updated test/makefile to be a standard test makefile.  Reindented auto/testNestedSuites.js.  Removed test/auto/makefile because auto directories do not usually have make run in them.  Updated svn:ignore.

--- ptolemy/actor/lib/jjs/modules/testing/test/auto/testNestedSuites.js
+++ ptolemy/actor/lib/jjs/modules/testing/test/auto/testNestedSuites.js
@@ -11,7 +11,39 @@ var should = chai.should();
 
 
 describe('Outer suite', function () {
+    before(function() {
+        // Read the accessor source code.
+        code = getAccessorCode('test/TestAccessor');
+        
+        instance = new commonHost.Accessor('TestAccessor', code);
+        
+        // Invoke the initialize function.
+        instance.initialize();
+        
+    });
+    
+    describe('Inner suite 1', function () {
         before(function() {
+            // Read the accessor source code.
+            code = getAccessorCode('test/TestAccessor');
+            
+            instance = new commonHost.Accessor('TestAccessor', code);
+            
+            // Invoke the initialize function.
+            instance.initialize();
+            
+        });
+        
+        it('Passing inner 1', function(){
+            instance.getParameter('p').should.equal(42);
+        });
+        
+        it('Failing inner 1', function(){
+            instance.get('numeric').should.equal(1);
+        });        
+        
+        describe('Inner suite 1.1', function () {
+            before(function() {
                 // Read the accessor source code.
                 code = getAccessorCode('test/TestAccessor');
                 
@@ -19,126 +51,94 @@ describe('Outer suite', function () {
                 
                 // Invoke the initialize function.
                 instance.initialize();
-        
-        });
-        
-        describe('Inner suite 1', function () {
-                before(function() {
-                        // Read the accessor source code.
-                        code = getAccessorCode('test/TestAccessor');
-                        
-                        instance = new commonHost.Accessor('TestAccessor', code);
-                        
-                        // Invoke the initialize function.
-                        instance.initialize();
                 
-                });
-                
-                it('Passing inner 1', function(){
-                        instance.getParameter('p').should.equal(42);
-                });
-                
-                it('Failing inner 1', function(){
-                        instance.get('numeric').should.equal(1);
-                });        
-                
-                describe('Inner suite 1.1', function () {
-                        before(function() {
-                                // Read the accessor source code.
-                                code = getAccessorCode('test/TestAccessor');
-                                
-                                instance = new commonHost.Accessor('TestAccessor', code);
-                                
-                                // Invoke the initialize function.
-                                instance.initialize();
-                        
-                        });
-                        
-                        it('Passing inner 1.1', function(){
-                                instance.getParameter('p').should.equal(42);
-                        });
-                        
-                        it('Failing inner 1.1', function(){
-                                instance.get('numeric').should.equal(1);
-                        });                
-                });
-                
-                describe('Inner suite 1.2', function () {
-                        before(function() {
-                                // Read the accessor source code.
-                                code = getAccessorCode('test/TestAccessor');
-                                
-                                instance = new commonHost.Accessor('TestAccessor', code);
-                                
-                                // Invoke the initialize function.
-                                instance.initialize();
-                        
-                        });
-                        
-                        describe('Inner suite 1.2.1', function () {
-                                before(function() {
-                                        // Read the accessor source code.
-                                        code = getAccessorCode('test/TestAccessor');
-                                        
-                                        instance = new commonHost.Accessor('TestAccessor', code);
-                                        
-                                        // Invoke the initialize function.
-                                        instance.initialize();
-                                
-                                });
-                                
-                                it('Passing inner 1.2.1', function(){
-                                        instance.getParameter('p').should.equal(42);
-                                });
-                                
-                                it('Failing inner 1.2.1', function(){
-                                        instance.get('numeric').should.equal(1);
-                                });                
-                        });
-                        
-                        it('Passing inner 1.2', function(){
-                                instance.getParameter('p').should.equal(42);
-                        });
-                        
-                        it('Failing inner 1.2', function(){
-                                instance.get('numeric').should.equal(1);
-                        });                
-                });
-        });
-        
-        describe('Inner suite 2', function () {
-                before(function() {
-                        // Read the accessor source code.
-                        code = getAccessorCode('test/TestAccessor');
-                        
-                        instance = new commonHost.Accessor('TestAccessor', code);
-                        
-                        // Invoke the initialize function.
-                        instance.initialize();
-                
-                });
-                
-                it('Passing inner 2', function(){
-                        instance.getParameter('p').should.equal(42);
-                });
-                
-                it('Failing inner 2', function(){
-                        instance.get('numeric').should.equal(1);
-                });                
-        });
-        
-        
-        describe('Inner suite 3 no tests', function () {
-                
-        });
-        
-        
-        it('Passing outer', function(){
+            });
+            
+            it('Passing inner 1.1', function(){
                 instance.getParameter('p').should.equal(42);
+            });
+            
+            it('Failing inner 1.1', function(){
+                instance.get('numeric').should.equal(1);
+            });                
         });
         
-        it('Failing outer', function(){
+        describe('Inner suite 1.2', function () {
+            before(function() {
+                // Read the accessor source code.
+                code = getAccessorCode('test/TestAccessor');
+                
+                instance = new commonHost.Accessor('TestAccessor', code);
+                
+                // Invoke the initialize function.
+                instance.initialize();
+                
+            });
+            
+            describe('Inner suite 1.2.1', function () {
+                before(function() {
+                    // Read the accessor source code.
+                    code = getAccessorCode('test/TestAccessor');
+                    
+                    instance = new commonHost.Accessor('TestAccessor', code);
+                    
+                    // Invoke the initialize function.
+                    instance.initialize();
+                    
+                });
+                
+                it('Passing inner 1.2.1', function(){
+                    instance.getParameter('p').should.equal(42);
+                });
+                
+                it('Failing inner 1.2.1', function(){
+                    instance.get('numeric').should.equal(1);
+                });                
+            });
+            
+            it('Passing inner 1.2', function(){
+                instance.getParameter('p').should.equal(42);
+            });
+            
+            it('Failing inner 1.2', function(){
                 instance.get('numeric').should.equal(1);
-        });        
+            });                
+        });
+    });
+    
+    describe('Inner suite 2', function () {
+        before(function() {
+            // Read the accessor source code.
+            code = getAccessorCode('test/TestAccessor');
+            
+            instance = new commonHost.Accessor('TestAccessor', code);
+            
+            // Invoke the initialize function.
+            instance.initialize();
+            
+        });
+        
+        it('Passing inner 2', function(){
+            instance.getParameter('p').should.equal(42);
+        });
+        
+        it('Failing inner 2', function(){
+            instance.get('numeric').should.equal(1);
+        });                
+    });
+    
+    
+    describe('Inner suite 3 no tests', function () {
+        
+    });
+    
+    
+    it('Passing outer', function(){
+        instance.getParameter('p').should.equal(42);
+    });
+    
+    it('Failing outer', function(){
+        instance.get('numeric').should.equal(1);
+    });        
 });
 
