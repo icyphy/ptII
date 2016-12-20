e702f4f80307d86f0785a4811a7ce8ff89bb5ffe
cxh <cxh>
2016-12-20 15:42:11 +0000

    Set PTRTOOL_JNLP_JARS for jnlp.mk

--- mk/jnlp.mk
+++ mk/jnlp.mk
@@ -687,7 +687,8 @@ FULL_11_0_JARS = \
 	ptolemy/domains/qss/qss.jar \
 	ptolemy/domains/qss/demo/demo.jar \
 	$(PTRSYNTAXTEXTAREA_JAR) \
-	$(PTRTOOL_JARS)
+	$(PTRTOOL_JARS) \
+	$(PTRTOOL_JNLP_JARS)
 
 #       $(PTPAHO_MQTT_JAR)
 #	$(PTXBEE_JARS)
