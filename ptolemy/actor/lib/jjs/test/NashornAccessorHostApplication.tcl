2e02b6253901c9e74242be0b18093f41618f0c67
cxh <cxh>
2017-01-05 03:06:49 +0000

    Unfortunately, the Nashorn Host spawns a separate thread, so there is no way to asyncronously wait to get the results back. This makes it difficult to test the command line argument processing.

--- ptolemy/actor/lib/jjs/test/NashornAccessorHostApplication.tcl
+++ ptolemy/actor/lib/jjs/test/NashornAccessorHostApplication.tcl
@@ -45,70 +45,76 @@ if {[info procs jdkCapture] == "" } then {
 # set VERBOSE 1
 
 
-######################################################################
-####
-#
-test NashornAccessorHostApplication-0.1 {test with no arguments} {
-    jdkCaptureOutAndErr {
-	set commandArguments [java::new {java.lang.String[]} 0]
-	java::call ptolemy.actor.lib.jjs.NashornAccessorHostApplication evaluate $commandArguments
-    } out err
-    list $out $err
-} {{ERROR: Usage: [-help] [-echo] [-js filename] [-timeout milliseconds] [-version] [accessorClassName] [accessorClassName ...]
-} 3}
+# Unfortunately, the Nashorn Host spawns a separate thread, so there
+# is no way to asyncronously wait to get the results back.
+# This makes it difficult to test the command line argument processing.
 
-######################################################################
-####
-#
-test NashornAccessorHostApplication-1.1 {test -e -h} {
-    jdkCaptureOutAndErr {
-	set commandArguments [java::new {java.lang.String[]} 2 \
-				  {{-e} {-h}}]
-	java::call ptolemy.actor.lib.jjs.NashornAccessorHostApplication evaluate $commandArguments
-    } out err
-    list $out $err
-} {{[ '-e', '-h' ]
-Usage: [-help] [-echo] [-js filename] [-timeout milliseconds] [-version] [accessorClassName] [accessorClassName ...]
-} 0}
 
-######################################################################
-####
-#
-test NashornAccessorHostApplication-2.1 {test -h} {
-    jdkCaptureOutAndErr {
-	set commandArguments [java::new {java.lang.String[]} 1 \
-			 {{-h}}]
-	java::call ptolemy.actor.lib.jjs.NashornAccessorHostApplication evaluate $commandArguments
-    } out err
-    list $out $err
-} {{Usage: [-help] [-echo] [-js filename] [-timeout milliseconds] [-version] [accessorClassName] [accessorClassName ...]
-} 0}
+# ######################################################################
+# ####
+# #
+# test NashornAccessorHostApplication-0.1 {test with no arguments} {
+#     jdkCapture {
+# 	set commandArguments [java::new {java.lang.String[]} 0]
+# 	set result [java::call ptolemy.actor.lib.jjs.NashornAccessorHostApplication evaluate $commandArguments]
+# 	puts "result: $result"
+#     } out
+#     list $out 
+# } {{ERROR: Usage: [-help] [-echo] [-js filename] [-timeout milliseconds] [-version] [accessorClassName] [accessorClassName ...]
+# } 3}
 
-######################################################################
-####
-#
-test NashornAccessorHostApplication-3.1 {test -v} {
-    jdkCaptureOutAndErr {
-	set commandArguments [java::new {java.lang.String[]} 1 \
-			 {{-v}}]
-	java::call ptolemy.actor.lib.jjs.NashornAccessorHostApplication evaluate $commandArguments
-    } out err
+# ######################################################################
+# ####
+# #
+# test NashornAccessorHostApplication-1.1 {test -e -h} {
+#     jdkCaptureOutAndErr {
+# 	set commandArguments [java::new {java.lang.String[]} 2 \
+# 				  {{-e} {-h}}]
+# 	java::call ptolemy.actor.lib.jjs.NashornAccessorHostApplication evaluate $commandArguments
+#     } out err
+#     list $out $err
+# } {{[ '-e', '-h' ]
+# Usage: [-help] [-echo] [-js filename] [-timeout milliseconds] [-version] [accessorClassName] [accessorClassName ...]
+# } 0}
 
-    list [string range $out 0 8] $err
-} {Accessors 0}
+# ######################################################################
+# ####
+# #
+# test NashornAccessorHostApplication-2.1 {test -h} {
+#     jdkCaptureOutAndErr {
+# 	set commandArguments [java::new {java.lang.String[]} 1 \
+# 			 {{-h}}]
+# 	java::call ptolemy.actor.lib.jjs.NashornAccessorHostApplication evaluate $commandArguments
+#     } out err
+#     list $out $err
+# } {{Usage: [-help] [-echo] [-js filename] [-timeout milliseconds] [-version] [accessorClassName] [accessorClassName ...]
+# } 0}
 
-######################################################################
-####
-#
-test NashornAccessorHostApplication-4.1 {test -accessor and -timeout} {
-    # Unfortunately, -timeout invokes process.exit(), so we can't easily test this right now.
-    #jdkCaptureOutAndErr {
-	# set commandArguments [java::new {java.lang.String[]} 4  {{-accessor} {-timeout} {2000} {hosts/nashorn/test/testNashornHost.js}}]
-	# puts $commandArguments
-	# catch {java::call ptolemy.actor.lib.jjs.NashornAccessorHostApplication evaluate $commandArguments} errMsg
-	# puts $errMsg
-    #} out err
+# ######################################################################
+# ####
+# #
+# test NashornAccessorHostApplication-3.1 {test -v} {
+#     jdkCaptureOutAndErr {
+# 	set commandArguments [java::new {java.lang.String[]} 1 \
+# 			 {{-v}}]
+# 	java::call ptolemy.actor.lib.jjs.NashornAccessorHostApplication evaluate $commandArguments
+#     } out err
 
-    #list $out $err
-} {}
+#     list [string range $out 0 8] $err
+# } {Accessors 0}
+
+# ######################################################################
+# ####
+# #
+# test NashornAccessorHostApplication-4.1 {test -accessor and -timeout} {
+#     # Unfortunately, -timeout invokes process.exit(), so we can't easily test this right now.
+#     #jdkCaptureOutAndErr {
+# 	# set commandArguments [java::new {java.lang.String[]} 4  {{-accessor} {-timeout} {2000} {hosts/nashorn/test/testNashornHost.js}}]
+# 	# puts $commandArguments
+# 	# catch {java::call ptolemy.actor.lib.jjs.NashornAccessorHostApplication evaluate $commandArguments} errMsg
+# 	# puts $errMsg
+#     #} out err
+
+#     #list $out $err
+# } {}
 
