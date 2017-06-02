6ad4418cb40638d6febce06de1fbc210464f4c7f
cxh <cxh>
2017-06-02 15:32:23 +0000

    Updated commonHost.js and added deterministicTemporalSemantics.js.

--- ptolemy/actor/lib/jjs/commonHost.js
+++ ptolemy/actor/lib/jjs/commonHost.js
@@ -234,63 +234,65 @@ var allAccessors = [];
 // will have a binding for the getTopLevelAccessors() function.
 var trustedAccessorsAllowed = false;
 
-// Determine which accessor host is in use.
-// See https://www.icyphy.org/accessors/wiki/Main/ResourcesForHostAuthors#Differentiating
-// See https://stijndewitt.com/2014/01/26/enums-in-javascript/
-var platformEnum = {
-    BROWSER: 1,
-    CAPECODE: 2,
-    DEFAULT: 3,
-    DUKTAPE: 4,
-    NODE: 5,
-    NASHORN: 6,
-    CORDOVA:7
-};
+// // Determine which accessor host is in use.
+// // See https://www.icyphy.org/accessors/wiki/Main/ResourcesForHostAuthors#Differentiating
+// // See https://stijndewitt.com/2014/01/26/enums-in-javascript/
+// var platformEnum = {
+//     BROWSER: 1,
+//     CAPECODE: 2,
+//     DEFAULT: 3,
+//     DUKTAPE: 4,
+//     NODE: 5,
+//     NASHORN: 6,
+//     CORDOVA:7
+// };
 
-exports.platformEnum = platformEnum;
+// exports.platformEnum = platformEnum;
 
-/** 
- * Determine the platform in use.
- */
-exports.getPlatform = function() { // FIXME: This should not be needed!!! Remove it. 
-    if (typeof window !== 'undefined') {
-        if (typeof cordova !== 'undefined') {
-            return platformEnum.CORDOVA;
-        } else if (window.hasOwnProperty('browserJSLoaded')) {
-            return platformEnum.BROWSER;
-        }
-    } else if (typeof actor !== 'undefined' && typeof Packages !== 'undefined' && typeof Packages.java.util.Vector === 'function') {
-        return platformEnum.CAPECODE;
-    } else if (typeof Duktape === 'object') {
-        return platformEnum.DUKTAPE;
-    } else if (typeof Packages !== 'undefined' && typeof Packages.java.util.Vector === 'function') {
-        return platformEnum.NASHORN;
-    } else if (typeof process !== 'undefined' && typeof process.version === 'string') {
-        return platformEnum.NODE;
-    }
-}
+// /** 
+//  * Determine the platform in use.
+//  */
+// exports.getPlatform = function() { // FIXME: This should not be needed!!! Remove it. 
+//     if (typeof window !== 'undefined') {
+//         if (typeof cordova !== 'undefined') {
+//             return platformEnum.CORDOVA;
+//         } else if (window.hasOwnProperty('browserJSLoaded')) {
+//             return platformEnum.BROWSER;
+//         }
+//     } else if (typeof actor !== 'undefined' && typeof Packages !== 'undefined' && typeof Packages.java.util.Vector === 'function') {
+//         return platformEnum.CAPECODE;
+//     } else if (typeof Duktape === 'object') {
+//         return platformEnum.DUKTAPE;
+//     } else if (typeof Packages !== 'undefined' && typeof Packages.java.util.Vector === 'function') {
+//         return platformEnum.NASHORN;
+//     } else if (typeof process !== 'undefined' && typeof process.version === 'string') {
+//         return platformEnum.NODE;
+//     }
+// }
 
-// The current platform in use.
-var platform = exports.getPlatform();
+// // The current platform in use.
+// var platform = exports.getPlatform();
 
-// Be sure to export these variables before any require()s of the util module because util.js
-// require()s commonHost.  Do not put these at the end of the file.
-// To test, run "ant tests.duk"
-exports.platform = platform;
+// // Be sure to export these variables before any require()s of the util module because util.js
+// // require()s commonHost.  Do not put these at the end of the file.
+// // To test, run "ant tests.duk"
+// exports.platform = platform;
 
 var util = require('util');
 var EventEmitter = require('events').EventEmitter;
 
+var deterministicTemporalSemantics = require('deterministicTemporalSemantics');
+
 // FIXME: Remove this platform dependency
-if (platform === platformEnum.CAPECODE || platform === platformEnum.NASHORN || platform === platformEnum.DUKTAPE) {
-    ; // Then no deterministic temporal semantics
-} else if (platform === platformEnum.BROWSER) {
-    var deterministicTemporalSemantics = require('/accessors/node_modules/@accessors-hosts/common/modules/deterministicTemporalSemantics');
-} else if (platform === platformEnum.CORDOVA) {
-    var deterministicTemporalSemantics = require('common/modules/deterministicTemporalSemantics.js');
-} else {    
-    var deterministicTemporalSemantics = require('../common/modules/deterministicTemporalSemantics.js');
-}
+// if (platform === platformEnum.CAPECODE || platform === platformEnum.NASHORN || platform === platformEnum.DUKTAPE) {
+//     ; // Then no deterministic temporal semantics
+// } else if (platform === platformEnum.BROWSER) {
+//     var deterministicTemporalSemantics = require('/accessors/node_modules/@accessors-hosts/common/modules/deterministicTemporalSemantics');
+// } else if (platform === platformEnum.CORDOVA) {
+//     var deterministicTemporalSemantics = require('common/modules/deterministicTemporalSemantics.js');
+// } else {    
+//     var deterministicTemporalSemantics = require('../common/modules/deterministicTemporalSemantics.js');
+// }
 
 ///////////////////////////////////////////////////////////////////
 //// Accessor class and its functions.
@@ -1663,19 +1665,21 @@ Accessor.prototype.react = function (name) {
                         // If the exception was thrown because of
                         // Java, we should get the Java stacktrace.
                         var stacktrace = exception.stack;
-                        if (typeof stacktrace === 'undefined' && platform === platformEnum.CAPECODE || platform === platformEnum.NASHORN) {
-                            try {
-                                // This code is Cape Code Host-specific because it uses Java.
-                                var StringWriter = java.io.StringWriter,
-                                    PrintWriter = java.io.PrintWriter;
-                                var stringWriter = new StringWriter();
-                                var printWriter = new PrintWriter(stringWriter);
-                                exception.printStackTrace(printWriter);
-                                stacktrace = "\n" + stringWriter.toString();
-                            } catch (exception2) {
-                                stacktrace = "undefined and the exception was not a Java exception so exception.printStackTrace() failed with: " + exception2;
-                            }
-                        }
+                        // FIXME: Now that commonHost has no platform-dependent code,
+                        // we can no longer get Java-specific exceptions under the Java-based hosts.
+                        // if (typeof stacktrace === 'undefined' && platform === platformEnum.CAPECODE || platform === platformEnum.NASHORN) {
+                        //     try {
+                        //         // This code is Cape Code Host-specific because it uses Java.
+                        //         var StringWriter = java.io.StringWriter,
+                        //             PrintWriter = java.io.PrintWriter;
+                        //         var stringWriter = new StringWriter();
+                        //         var printWriter = new PrintWriter(stringWriter);
+                        //         exception.printStackTrace(printWriter);
+                        //         stacktrace = "\n" + stringWriter.toString();
+                        //     } catch (exception2) {
+                        //         stacktrace = "undefined and the exception was not a Java exception so exception.printStackTrace() failed with: " + exception2;
+                        //     }
+                        // }
 
                         throw new Error('commonHost.js, react(), invoking a specific handler for \"' +
                             name + '\": Exception occurred in input handler,' +
