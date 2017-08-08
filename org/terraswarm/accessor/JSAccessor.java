31c8b04f083266b54af0c90035c39d4bb5fca8bc
cxh <cxh>
2017-08-08 19:57:33 +0000

    No need to run jsdoc on the Mutable accessor, it contains no documentation.

--- org/terraswarm/accessor/JSAccessor.java
+++ org/terraswarm/accessor/JSAccessor.java
@@ -947,7 +947,10 @@ public class JSAccessor extends JavaScript {
 
                 // Get the DocAttribute by looking for an adjacent *PtDoc.xml file.
                 try {
-                    if (urlSpec.indexOf("test/auto") != -1) {
+                    if (urlSpec.endsWith("utilities/Mutable.js")) {
+                        System.out.println("The Mutable accessor has no documentation, so we are not "
+                                           + "running jsdoc on " + urlSpec);
+                    } else if (urlSpec.indexOf("test/auto") != -1) {
                         System.out.println("Accessors in test/auto do not typically have documentation, so we are not "
                                            + "running jsdoc on " + urlSpec);
                     } else {
