a0e358a72617a83f5d1fbef433094b323361ab26
moez@berkeley.edu <moez@berkeley.edu>
2017-08-18 19:50:11 +0000

    scala check in

--- org/ptolemy/scala/actor/lib/hoc/MultiInstanceRelation.scala
+++ org/ptolemy/scala/actor/lib/hoc/MultiInstanceRelation.scala
@@ -33,7 +33,6 @@ import org.ptolemy.scala.actor.ComponentEntity
 import org.ptolemy.scala.actor.TypedCompositeActor
 import org.ptolemy.scala.actor.TypedIOPort
 import org.ptolemy.scala.actor.TypedIORelation
-import org.ptolemy.scala.implicits.ImplicitComponentEntity
 
 import ptolemy.kernel.CompositeEntity
 
@@ -52,120 +51,20 @@ import ptolemy.kernel.CompositeEntity
  */
 case class MultiInstanceRelation (name: String, N: Integer)(implicit var container: CompositeEntity) {
 
-  /**
-	 * A ListBuffer to store the references of the instantiated relations
-	 */
-	val relations  = scala.collection.mutable.ListBuffer.empty[TypedIORelation]  
-		// instantiate the relations and store their references in the ListBuffer
-			for( a <- 0 until N){
-				relations +=  TypedIORelation( name+a)
-			}
+    /**
+     * A ListBuffer to store the references of the instantiated relations
+     */
+    val relations  = scala.collection.mutable.ListBuffer.empty[TypedIORelation]  
+            // instantiate the relations and store their references in the ListBuffer
+            for( a <- 0 until N){
+                relations +=  TypedIORelation( name+a)
+            }
 
 
-		/** Returns a List of relation references.
-			 * @return List of relation references.	
-			 */  
-	def getRelations() = relations
-	  	/**
-     --width-->
-		 */     
-		def --/(width: Int):  MultiInstanceRelation = {
-				connectionWidth = width      
-						this 
-		}       
+    /** Returns a List of relation references.
+     * @return List of relation references.	
+     */  
+    def getRelations() = relations
 
-		/**
-   Connects HoC actor output to  an OPort
-
-		 */    
-		def -->(port: ptolemy.actor.IOPort)(implicit container: CompositeEntity):Unit ={ 
-				if (connectionWidth > 0 ) {
-					val width = connectionWidth
-							this.relations.foreach{relation => 
-							port.link(relation.typedIORelation)
-							relation.typedIORelation.width.setExpression(width.toString())		
-					}
-					connectionWidth = -1
-				}else
-					this.relations.foreach(relation  => port.link(relation.typedIORelation))				
-		}
-
-
-		/**
-   Connects HoC actor output to  a TypedIOPort. 
-
-		 */    
-		def -->(port: TypedIOPort)(implicit container: CompositeEntity):Unit ={ 
-
-				if (connectionWidth > 0 ) {
-					val width = connectionWidth
-							this.relations.foreach{relation => 
-							port.getActor().link(relation.typedIORelation)
-							relation.typedIORelation.width.setExpression(width.toString())		
-					}
-					connectionWidth = -1
-				}else
-					this.relations.foreach(relation  => port.getActor().link(relation.typedIORelation))
-		}
-
-
-		/**
-   Connects each relation to an actor  input. 
-
-		 */     
-		def -->[B <: ComponentEntity](actor: B)(implicit container: CompositeEntity): B = {
-				if (connectionWidth > 0 ) {
-					val width = connectionWidth
-							this.relations.foreach{relation => 
-							actor.inputPortList().get(0).link(relation.typedIORelation)
-							relation.typedIORelation.width.setExpression(width.toString())		
-					}
-					connectionWidth = -1
-				}else
-					this.relations.foreach(relation  => actor.inputPortList().get(0).link(relation.typedIORelation))
-
-					actor
-		}   
-
-	
-		/**
-   Connects MultiInstanceRelation to a HoC actor. 
-
-		 */    
-		def -->[B <: ComponentEntity](hocActor : MultiInstance [B])(implicit container: CompositeEntity):MultiInstance [B] ={ 
-				if (connectionWidth > 0 ) {
-					val width = connectionWidth
-							hocActor.actors.zip(this.relations).map(x => x match{case (actor:ComponentEntity, relation:TypedIORelation) => actor --/width--> relation}  )    				
-							connectionWidth = -1
-				}else
-					hocActor.actors.zip(this.relations).map(x => x match{case (actor:ComponentEntity, relation:TypedIORelation) => actor --> relation}  )
-					hocActor
-		}
-	
-		/**
-		 * Stores the width of a relation.
-		 * Modified by the method --/(width: Int).
-		 * Used by the method --/(n: Integer) 
-		 */	
-		private var connectionWidth= 0	
-
-				/**
-				 * used by <t(n:Integer) and last(n:Integer) methods.
-				 *  first == true, means iterating only the first numberOfElements.
-				 */
-				private var first = false
-
-				/**
-				 * used by >(n:Integer) and last(n:Integer) methods.
-				 *  last == true, means iterating only the last numberOfElements.
-				 */
-				private var last = false
-
-				/**
-				 * used by first(n:Integer) and last(n:Integer) methods.   
-				 *  numberOfElements is used to know on how many elements to iterate.
-				 *   in the case of High Order Components.
-				 */
-				private var numberOfElements= 0
 
 }
