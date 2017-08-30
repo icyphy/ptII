762aa4d8dc5edea41fe08a3b26d3618aca9f1fa0
cxh <cxh>
2017-08-30 00:40:26 +0000

    How to create a pdf of the tags.

--- edu/umich/eecs/april/image/AprilTagFilter.java
+++ edu/umich/eecs/april/image/AprilTagFilter.java
@@ -91,8 +91,12 @@ import ptolemy.kernel.util.IllegalActionException;
  *  <a href="https://april.eecs.umich.edu/software/apriltag.html#in_browser">https://april.eecs.umich.edu/software/apriltag.html</a>
  *  contains a set of pregenerated tags as png and PostScript files.
  *  However, these are of low resolution.  To scale them, use linear
- *  interpolation to avoid blurring.  Or, search the web for "tag
- *  36H11".
+ *  interpolation to avoid blurring.  
+ *  For example, with ImageMagik, use:
+ *
+ *  mogrify -scale 1000x1000 *.png; convert *.png tag36h11.pdf
+ *
+ *  Or, search the web for "tag 36H11".
  *
  *  In the Ptolemy tree, a sample file may be found at
  *  $PTII/ptolemy/actor/lib/jjs/modules/aprilTags//demo/AprilTags/tag36_11_00586.pdf
