64c8d9773f8ce4c7d9f3ca533a97db234ac94a54
eal <eal>
2017-09-14 22:47:56 +0000

    try-catch for util.inspect so it doesn't throw exceptions

--- ptolemy/actor/lib/jjs/node/util.js
+++ ptolemy/actor/lib/jjs/node/util.js
@@ -104,28 +104,35 @@ exports.debuglog = function (set) {
  */
 /* legacy: obj, showHidden, depth, colors*/
 function inspect(obj, opts) {
-    // default options
-    var ctx = {
-        seen: [],
-        stylize: stylizeNoColor
-    };
-    // legacy...
-    if (arguments.length >= 3) ctx.depth = arguments[2];
-    if (arguments.length >= 4) ctx.colors = arguments[3];
-    if (isBoolean(opts)) {
+    try {
+        // default options
+        var ctx = {
+                seen: [],
+                stylize: stylizeNoColor
+        };
         // legacy...
-        ctx.showHidden = opts;
-    } else if (opts) {
-        // got an "options" object
-        exports._extend(ctx, opts);
+        if (arguments.length >= 3) ctx.depth = arguments[2];
+        if (arguments.length >= 4) ctx.colors = arguments[3];
+        if (isBoolean(opts)) {
+            // legacy...
+            ctx.showHidden = opts;
+        } else if (opts) {
+            // got an "options" object
+            exports._extend(ctx, opts);
+        }
+        // set default options
+        if (isUndefined(ctx.showHidden)) ctx.showHidden = false;
+        if (isUndefined(ctx.depth)) ctx.depth = 2;
+        if (isUndefined(ctx.colors)) ctx.colors = false;
+        if (isUndefined(ctx.customInspect)) ctx.customInspect = true;
+        if (ctx.colors) ctx.stylize = stylizeWithColor;
+        return formatValue(ctx, obj, ctx.depth);
+    } catch (e) {
+        return "$PTII/ptolemy/actor/lib/jjs/node/util.js: util.inspect("
+                + obj.toString()
+                + " fails with "
+                + e;
     }
-    // set default options
-    if (isUndefined(ctx.showHidden)) ctx.showHidden = false;
-    if (isUndefined(ctx.depth)) ctx.depth = 2;
-    if (isUndefined(ctx.colors)) ctx.colors = false;
-    if (isUndefined(ctx.customInspect)) ctx.customInspect = true;
-    if (ctx.colors) ctx.stylize = stylizeWithColor;
-    return formatValue(ctx, obj, ctx.depth);
 }
 exports.inspect = inspect;
 
