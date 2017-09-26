3a5e31d54060eeb4a3aaa07f042a1c5af8b7d0b2
cxh <cxh>
2017-09-26 18:14:30 +0000

    Test for JavaFX, which is used by the ClipPlayer accessor under CapeCode.

---
+++ config/JavaFXTest.java
@@ -0,0 +1,50 @@
+/* Simple Java Test Program for JavaFX, used by the ClipPlayer CapeCode accessor.
+
+Copyright (c) 2002-2017 The Regents of the University of California.
+All rights reserved.
+Permission is hereby granted, without written agreement and without
+license or royalty fees, to use, copy, modify, and distribute this
+software and its documentation for any purpose, provided that the above
+copyright notice and the following two paragraphs appear in all copies
+of this software.
+
+IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+SUCH DAMAGE.
+
+THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ENHANCEMENTS, OR MODIFICATIONS.
+
+PT_COPYRIGHT_VERSION_2
+COPYRIGHTENDKEY
+
+*/
+import javafx.embed.swing.JFXPanel;
+import javafx.scene.media.Media;
+
+
+/** Simple Java Test Program for JavaFX, used by the ClipPlayer CapeCode accessor.
+    is present.
+    
+    If this file will not compile because the import statement fails,
+    then the problem is likely that you are using OpenJDK instead of 
+    an Oracle JDK.
+
+    @author Christopher Brooks.
+    @version $Id: JAITest.java 61458 2011-07-11 19:54:07Z cxh $
+    @since Ptolemy II 2.1
+    @Pt.ProposedRating Green (cxh)
+    @Pt.AcceptedRating Red
+*/
+public class JAITest {
+    public static void main(String[] args) {
+        System.out.print(System.getProperty("java.version"));
+    }
+    public static JFXPanel fxPanel;
+}
