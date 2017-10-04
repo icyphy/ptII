0b56803c28f9e742364093bde16230945ef84a4f
cxh <cxh>
2017-10-04 02:23:49 +0000

    Added GenerateMQTTAccessors, which reads in mqtts.json and adds MQTT accessors to the key-value store.

---
+++ org/terraswarm/accessor/demo/AugmentedRealityVideoSOHO/MQTTAccessorTemplate.js
@@ -0,0 +1,38 @@
+// Stop extra messages from jslint.  Note that there should be no
+// space between the / and the * and global.
+/*globals console, error, exports, require */
+/*jshint globalstrict: true*/
+"use strict";
+
+exports.setup = function() {	
+    this.output('data', {'type' : 'JSON'});
+    
+    var MQTTSubscriber = this.instantiate('MQTTSubscriber', 'net/MQTTSubscriber'); 
+    MQTTSubscriber.input('subscribe', {
+	// 'value':'#'
+    // The GenerateBleeAccessors.xml model substitutes in the topic here.
+        'value': '@topic@'
+    });
+    
+    // The GenerateBleeAccessors.xml model substitutes in the BrokerHost here.
+    var brokerHost = '@brokerHost@';
+
+    // Hint: Use the lab11 Summon app to find local SwarmBoxes with BLE
+    MQTTSubscriber.setParameter('brokerHost', brokerHost);
+
+    var code = "\
+	exports.setup = function() {\n\
+	    this.input('in');\n\
+	    this.output('out');\n\
+    }\n\
+    exports.initialize = function() {\n\
+	    var thiz = this;\n\
+	    this.addInputHandler('in', function() {\n\
+		    thiz.send('out', JSON.parse(thiz.get('in')));\n\
+	    });\n\
+    }";
+    var JavaScript = this.instantiateFromCode('JavaScript', code, false);
+
+    this.connect(JavaScript, 'out', 'data');
+    this.connect(MQTTSubscriber, 'received', JavaScript, 'in');
+};
