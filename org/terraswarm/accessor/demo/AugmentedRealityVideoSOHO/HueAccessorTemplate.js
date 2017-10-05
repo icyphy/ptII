9df7290f44ed6f094e52863d98a1416267797f35
cxh <cxh>
2017-10-05 01:25:48 +0000

    We are now successfully generating Hue Accessors and adding them to the KV store.  Next up: Add a UI and get data from the Hue.

--- org/terraswarm/accessor/demo/AugmentedRealityVideoSOHO/HueAccessorTemplate.js
+++ org/terraswarm/accessor/demo/AugmentedRealityVideoSOHO/HueAccessorTemplate.js
@@ -1,18 +1,16 @@
 "use strict";
 
 exports.setup = function() {       
+    console.log('HueAccessorTemplate: setup(): start');
     this.output('data', {'type' : 'JSON'});
     
     var Hue = this.instantiate('Hue', 'devices/Hue'); 
-    Hue.input('bridgeIP', {
-        /* The GenerateHueAccessors.xml model substitutes in the userName here.*/
-        'value': '@bridgeIP@'
-    });
-    
-    /* The GenerateHueAccessors.xml model substitutes in the userName here.*/
-    var userName = '@userName@';
 
-    Hue.setParameter('userName', userName);
+    /* The GenerateHueAccessors.xml model substitutes in the bridgeIP here.*/
+    Hue.setParameter('bridgeIP','@bridgeIP@');
+
+    /* The GenerateHueAccessors.xml model substitutes in the userName here.*/
+    Hue.setParameter('userName', '@userName@');
 
     var code = "\
        exports.setup = function() {\n\
@@ -22,12 +20,16 @@ exports.setup = function() {
     exports.initialize = function() {\n\
            var thiz = this;\n\
            this.addInputHandler('in', function() {\n\
-               var inValue = thiz.get('in');\n\
+               /* var inValue = thiz.get('in'); */\n\
+               /* FIXME: Need to get real data from the Hue. */\n\
+               var inValue = '{ \"bridgeIP\": \"@bridgeIP@\", \"id\": \"@id@\", \"userName\": \"@userName@\" }';\n\
+               console.log('HueAccessorTemplate: inputHandler()');\n\
                thiz.send('out', JSON.parse(inValue));\n\
            });\n\
     }";
     var JavaScript = this.instantiateFromCode('JavaScript', code, false);
 
     this.connect(JavaScript, 'out', 'data');
-    this.connect(Hue, 'received', JavaScript, 'in');
+    this.connect(Hue, 'lights', JavaScript, 'in');
+    console.log('HueAccessorTemplate: setup(): end');
 };
