4816473c75d26d5f09d419407297bb64067d65ec
cxh <cxh>
2017-10-27 17:30:18 +0000

    Add a few tests for getResource().  We should add similar tests to all the hosts.

--- org/terraswarm/accessor/test/JSAccessor.tcl
+++ org/terraswarm/accessor/test/JSAccessor.tcl
@@ -65,7 +65,7 @@ test JSAccessor-1.1 {Test out importing of accessors} {
 
     java::call org.terraswarm.accessor.JSAccessor handleAccessorMoMLChangeRequest $e1 $urlSpec $e1 $changeRequestText 100 100
 
-    set accessor [$e1 getEntity {TestAccessor}]
+    set accessor [java::cast org.terraswarm.accessor.JSAccessor [$e1 getEntity {TestAccessor}]]
     set moml [$accessor exportMoML]
     
     regsub {value=".*/org/terraswarm/accessor/accessors/web/test/TestAccessor.js"} $moml {value="$CLASSPATH/org/terraswarm/accessor/accessors/test/TestAccessor.js"} moml2
@@ -74,12 +74,12 @@ test JSAccessor-1.1 {Test out importing of accessors} {
     regsub {value=".*\\org\\terraswarm\\accessor\\accessors\\test\\TestAccessor.js"} $moml2 {value="$CLASSPATH/org/terraswarm/accessor/accessors/test/TestAccessor.js"} moml3
 
     # Substitute the svn version control strings so that the test continues to pass.
-    regsub {<property name="version" class="ptolemy.kernel.util.StringAttribute" value=".*">} $moml3 {<property name="version" class="ptolemy.kernel.util.StringAttribute" value="VERSION_REMOVED_BY_TEST">} moml4
-    regsub {@version.*$} $moml4 {@version VERSION_REMOVED_BY_TEST} moml5
+    regsub {<property name="version" class="ptolemy.kernel.util.StringAttribute" value="[^>]*">} $moml3 {<property name="version" class="ptolemy.kernel.util.StringAttribute" value="VERSION_REMOVED_BY_TEST">} moml4
+    regsub {@version [^&]*} $moml4 {@version VERSION_REMOVED_BY_TEST2} moml5
 
     list $moml5
 } {{<entity name="TestAccessor" class="org.terraswarm.accessor.JSAccessor">
-    <property name="script" class="ptolemy.actor.parameters.PortParameter" value="// Test accessor with various input and output types and handlers.&#10;//&#10;// Copyright (c) 2015-2016 The Regents of the University of California.&#10;// All rights reserved.&#10;//&#10;// Permission is hereby granted, without written agreement and without&#10;// license or royalty fees, to use, copy, modify, and distribute this&#10;// software and its documentation for any purpose, provided that the above&#10;// copyright notice and the following two paragraphs appear in all copies&#10;// of this software.&#10;//&#10;// IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY&#10;// FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES&#10;// ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF&#10;// THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF&#10;// SUCH DAMAGE.&#10;//&#10;// THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,&#10;// INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF&#10;// MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE&#10;// PROVIDED HEREUNDER IS ON AN &quot;AS IS&quot; BASIS, AND THE UNIVERSITY OF&#10;// CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,&#10;// ENHANCEMENTS, OR MODIFICATIONS.&#10;//&#10;&#10;/** Test accessor with various input and output types and handlers.&#10; *  This accessor is designed to be instantiable on any host, including&#10; *  the common host, which does not implement the require() function&#10; *  nor provide any mechanism for loading accessors.&#10; *&#10; *  @accessor test/TestAccessor&#10; *  @parameter p A parameter with default value 42.&#10; *  @input untyped An untyped input that will accept any JavaScript object.&#10; *  @input numeric A numeric input.&#10; *  @input boolean A boolean input.&#10; *  @output typeOfUntyped Produces the type (a string) of the input named 'untyped'.&#10; *  @output jsonOfUntyped Produces a JSON representation of the input named 'untyped',&#10; *   created using JSON.toString().&#10; *  @output numericPlusP Produces the value of the 'numeric' input plus 'p'.&#10; *  @output negation Produces the negation of the 'boolean' input.&#10; *  @author Edward A. Lee&#10; *  @version VERSION_REMOVED_BY_TEST
+    <property name="script" class="ptolemy.actor.parameters.PortParameter" value="// Test accessor with various input and output types and handlers.&#10;//&#10;// Copyright (c) 2015-2016 The Regents of the University of California.&#10;// All rights reserved.&#10;//&#10;// Permission is hereby granted, without written agreement and without&#10;// license or royalty fees, to use, copy, modify, and distribute this&#10;// software and its documentation for any purpose, provided that the above&#10;// copyright notice and the following two paragraphs appear in all copies&#10;// of this software.&#10;//&#10;// IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY&#10;// FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES&#10;// ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF&#10;// THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF&#10;// SUCH DAMAGE.&#10;//&#10;// THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,&#10;// INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF&#10;// MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE&#10;// PROVIDED HEREUNDER IS ON AN &quot;AS IS&quot; BASIS, AND THE UNIVERSITY OF&#10;// CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,&#10;// ENHANCEMENTS, OR MODIFICATIONS.&#10;//&#10;&#10;/** Test accessor with various input and output types and handlers.&#10; *  This accessor is designed to be instantiable on any host, including&#10; *  the common host, which does not implement the require() function&#10; *  nor provide any mechanism for loading accessors.&#10; *&#10; *  @accessor test/TestAccessor&#10; *  @parameter p A parameter with default value 42.&#10; *  @input untyped An untyped input that will accept any JavaScript object.&#10; *  @input numeric A numeric input.&#10; *  @input boolean A boolean input.&#10; *  @output typeOfUntyped Produces the type (a string) of the input named 'untyped'.&#10; *  @output jsonOfUntyped Produces a JSON representation of the input named 'untyped',&#10; *   created using JSON.toString().&#10; *  @output numericPlusP Produces the value of the 'numeric' input plus 'p'.&#10; *  @output negation Produces the negation of the 'boolean' input.&#10; *  @author Edward A. Lee&#10; *  @version VERSION_REMOVED_BY_TEST2&#10; */&#10;&#10;// Stop extra messages from jslint.  Note that there should be no&#10;// space between the / and the * and global.&#10;/*globals console, error, exports, require */&#10;/*jshint globalstrict: true*/&#10;&quot;use strict&quot;;&#10;&#10;exports.setup = function () {&#10;    this.input('untyped'); // Untyped input.&#10;    this.input('numeric', {&#10;        'type': 'number',&#10;        'value': 0&#10;    }); // Numeric input.&#10;    this.input('boolean', {&#10;        'type': 'boolean'&#10;    }); // Boolean input.&#10;    this.output('typeOfUntyped', {&#10;        'type': 'string'&#10;    }); // Type of untyped input.&#10;    this.output('jsonOfUntyped', {&#10;        'type': 'string'&#10;    }); // JSON of untyped input.&#10;    this.output('numericPlusP', {&#10;        'type': 'number'&#10;    }); // Numeric input plus p.&#10;    this.output('negation', {&#10;        'type': 'boolean'&#10;    }); // Negation of boolean input.&#10;    this.parameter('p', {&#10;        'value': 42&#10;    }); // Untyped, with numeric value.&#10;};&#10;&#10;// Base class variable that is visible to subclasses through inheritance.&#10;exports.variable = 'hello';&#10;&#10;exports.initialize = function () {&#10;    // Respond to any input by updating them all.&#10;    this.addInputHandler('untyped', function () {&#10;        this.send('typeOfUntyped', typeof this.get('untyped'));&#10;        // Refer to the function using 'this.exports' rather than 'exports'&#10;        // to allow an override. Note that we choose here to invoke formatOutput&#10;        // with 'this' bound to 'this.exports'.&#10;        this.send('jsonOfUntyped', this.exports.formatOutput(this.get('untyped')));&#10;    });&#10;    this.addInputHandler('numeric', function () {&#10;        this.send('numericPlusP', this.get('numeric') + this.getParameter('p'));&#10;    });&#10;    this.addInputHandler('boolean', function () {&#10;        this.send('negation', !this.get('boolean'));&#10;    });&#10;};&#10;&#10;/** Define a function that can be overridden in subclasses. */&#10;exports.formatOutput = function (value) {&#10;    return 'JSON for untyped input: ' + JSON.stringify(value);&#10;};&#10;&#10;exports.fire = function () {&#10;    console.log('TestAccessor.fire() invoked.');&#10;};&#10;">
         <property name="style" class="ptolemy.actor.gui.style.NoteStyle">
             <property name="note" class="ptolemy.kernel.util.StringAttribute" value="NOTE: To see the script, invoke Open Actor">
             </property>
@@ -213,4 +213,51 @@ proc importAccessors {accessorDirectory} {
 }
 
 importAccessors $PTII/org/terraswarm/accessor/test/auto/accessors
+
+######################################################################
+####
+#
+
+# We should have more complete tests of getResource for each of the
+# accessor Hosts, but these will help in the near term.
+
+test JSAccessor-2.1 {getResource(): simple} {
+    set makefile [$accessor getResource makefile [java::new {Object[]} 0]]
+    list [expr {[string length $makefile] > 0}]
+} {1}
+
+test JSAccessor-2.2 {getResource(): Get something from the CLASSPATH} {
+    set copyright [$accessor getResource {$CLASSPATH/copyright.txt} [java::new {Object[]} 0]]
+    list [expr {[string length $copyright] > 0}]
+} {1}
+
+test JSAccessor-2.3 {getResource(): Should not be able to get /etc/passwd because _restricted is set} {
+    catch { set password [$accessor getResource /etc/password [java::new {Object[]} 0]] } errMsg
+    list $errMsg
+} {{ptolemy.kernel.util.IllegalActionException: Absolute file names not permitted: /etc/password
+  in .top.TestAccessor}}
+
+test JSAccessor-2.4 {getResource(): Should not be able to ..} {
+    catch { set makefile [$accessor getResource ../makefile [java::new {Object[]} 0]] } errMsg
+    list $errMsg
+} {{ptolemy.kernel.util.IllegalActionException: Only file names at or within the model location are permitted: ../makefile
+  in .top.TestAccessor}}
+
+test JSAccessor-2.5 {getResource(): Should not be able to get file:///} {
+    catch { set password [$accessor getResource file:///etc/passwd [java::new {Object[]} 0]] } errMsg
+    list $errMsg
+} {{ptolemy.kernel.util.IllegalActionException: Protocol not permitted: file in URI file:///etc/passwd
+  in .top.TestAccessor}}
+
+test JSAccessor-2.6 {getResource(): get https://accessors.org} {
+    set accessors [$accessor getResource https://accessors.org [java::new {Object[]} 0]]
+    list [expr {[string length $accessors] > 0}]
+} {1}
+
+
+    
+    
+
+
+
 
