d8a4e8febbbc6311245c9a70c18e39ce53925d05
eal <eal>
2018-01-31 23:10:07 +0000

    Added Lights shell script

---
+++ ptolemy/demo/Office/OfficeServices/Lights.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+# Usage:
+# pm2 start Lights.sh
+
+# Disable auto-update of accessors repo
+export PT_NO_NET=true
+
+# Sleep so that the KV store has a chance of running
+echo "$0: Sleeping for 13 seconds: `date`"
+sleep 13
+
+model=$PTII/ptolemy/demo/Office/OfficeServices/Lights.xml
+echo "$0: about to start $model: `date`"
+echo "$0: about to start $model: `date`" 1>&2
+$PTII/bin/ptinvoke ptolemy.moml.MoMLSimpleApplication $model
+echo "$0: $model exited: `date`"
+echo "$0: $model exited: `date`" 1>&2
