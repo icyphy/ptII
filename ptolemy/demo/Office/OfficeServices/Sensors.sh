01de7bacbbc7c440e6557cb73876e405cf9ca6c0
eal <eal>
2018-02-16 18:58:56 +0000

    First working version of Sensors service

---
+++ ptolemy/demo/Office/OfficeServices/Sensors.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# Usage:
+# pm2 start Sensors.sh
+# pm2 save
+
+# Disable auto-update of accessors repo
+export PT_NO_NET=true
+
+# Sleep so that the KV store has a chance of running
+echo "$0: Sleeping for 13 seconds: `date`"
+sleep 13
+
+model=$PTII/ptolemy/demo/Office/OfficeServices/Sensors.xml
+echo "$0: about to start $model: `date`"
+echo "$0: about to start $model: `date`" 1>&2
+$PTII/bin/ptinvoke ptolemy.moml.MoMLSimpleApplication $model
+echo "$0: $model exited: `date`"
+echo "$0: $model exited: `date`" 1>&2
