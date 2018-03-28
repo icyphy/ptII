2b3c575d4f5aad452f04cfd353d2a021f7a53c23
eal <eal@eecs.berkeley.edu>
2018-03-27 17:49:47 -0700

    Added ChoiceStringParameter

---
+++ ptolemy/data/expr/ChoiceStringParameter.java
@@ -0,0 +1,104 @@
+/* A parameter that is in string mode and has a list of choices.
+
+ Copyright (c) 2003-2009 The Regents of the University of California.
+ All rights reserved.
+ Permission is hereby granted, without written agreement and without
+ license or royalty fees, to use, copy, modify, and distribute this
+ software and its documentation for any purpose, provided that the above
+ copyright notice and the following two paragraphs appear in all copies
+ of this software.
+
+ IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+ FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+ ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+ THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+ SUCH DAMAGE.
+
+ THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+ INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+ PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+ CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ ENHANCEMENTS, OR MODIFICATIONS.
+
+ PT_COPYRIGHT_VERSION_2
+ COPYRIGHTENDKEY
+
+
+ */
+package ptolemy.data.expr;
+
+import ptolemy.kernel.util.Attribute;
+import ptolemy.kernel.util.IllegalActionException;
+import ptolemy.kernel.util.NameDuplicationException;
+import ptolemy.kernel.util.NamedObj;
+
+//////////////////////////////////////////////////////////////////////////
+//// ChoiceStringParameter
+
+/**
+ This parameter contains a string value and has a list of acceptable choices.
+ The acceptable choices are contained in the parameter _choices_, a string
+ containing a comma-separated list of choices.
+
+ @author Edward A. Lee
+ @version $Id$
+ @since Ptolemy II 11.0
+ @Pt.ProposedRating Red (tfeng)
+ @Pt.AcceptedRating Red (tfeng)
+ */
+public class ChoiceStringParameter extends StringParameter {
+
+    /** Construct a parameter with the given name contained by the specified
+     *  entity. The container argument must not be null, or a
+     *  NullPointerException will be thrown.  This parameter will use the
+     *  workspace of the container for synchronization and version counts.
+     *  If the name argument is null, then the name is set to the empty string.
+     *  The object is not added to the list of objects in the workspace
+     *  unless the container is null.
+     *  Increment the version of the workspace.
+     *  @param container The container.
+     *  @param name The name of the parameter.
+     *  @param enumClass The enum class that contains the choices.
+     *  @exception IllegalActionException If the parameter is not of an
+     *   acceptable class for the container.
+     *  @exception NameDuplicationException If the name coincides with
+     *   a parameter already in the container.
+     */
+    public ChoiceStringParameter(NamedObj container, String name)
+            throws IllegalActionException, NameDuplicationException {
+        super(container, name);
+        choices = new StringParameter(this, "choices");
+    }
+
+    ///////////////////////////////////////////////////////////////////
+    ////                         public variables                  ////
+    
+    /** A comma-separated list of choices. */
+    public StringParameter choices;
+
+    ///////////////////////////////////////////////////////////////////
+    ////                         public methods                    ////
+    
+    /** React to a change in an attribute.  This method is called by
+     *  a contained attribute when its value changes.  This overrides
+     *  the base class to check whether the attribute is choices, and if
+     *  so, it removes all previously specified choices and adds the ones
+     *  given in choices.
+     *  @param attribute The attribute that changed.
+     *  @exception IllegalActionException If the change is not acceptable
+     *   to this container.
+     */
+    @Override
+    public void attributeChanged(Attribute attribute) throws IllegalActionException {
+        super.attributeChanged(attribute);
+        if (attribute == choices) {
+            removeAllChoices();
+            String spec = ((StringParameter)choices).stringValue();
+            String[] choices = spec.split(",");
+            for (int i = 0; i < choices.length; i++) {
+                addChoice(choices[i].trim());
+            }
+        }
+    }
+}
