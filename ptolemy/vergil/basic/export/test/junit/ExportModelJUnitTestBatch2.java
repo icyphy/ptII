7b897cf06e1aa5f1945e56945fba62495cef6697
travis-ci <travis@travis-ci.org>
2018-05-11 09:05:32 -0700

    Batch1: 100, Batch2: 200, Batch3: 70

--- ptolemy/vergil/basic/export/test/junit/ExportModelJUnitTestBatch2.java
+++ ptolemy/vergil/basic/export/test/junit/ExportModelJUnitTestBatch2.java
@@ -84,8 +84,8 @@ import ptolemy.vergil.basic.export.ExportModel;
 @RunWith(JUnitParamsRunner.class)
 public class ExportModelJUnitTestBatch2 extends ExportModelJUnitTestBatch {
 
-    /** Run demos 86 through 170. */
+    /** Run demos 101 through 200. */
     public Object[] demos() throws IOException {
-        return super.demos(86, 170);
+        return super.demos(101, 20);
     }
 }
