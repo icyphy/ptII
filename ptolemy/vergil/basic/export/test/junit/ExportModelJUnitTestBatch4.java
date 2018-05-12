46774fc232bd6273e8d0911a579c033f81aab81b
travis-ci <travis@travis-ci.org>
2018-05-12 06:42:57 -0700

    Adjusted model numbers in export batches.

--- ptolemy/vergil/basic/export/test/junit/ExportModelJUnitTestBatch4.java
+++ ptolemy/vergil/basic/export/test/junit/ExportModelJUnitTestBatch4.java
@@ -1,4 +1,4 @@
-/* JUnit test that exports the second batch of demos.
+/* JUnit test that exports the fourth batch of demos.
 
    Copyright (c) 2018 The Regents of the University of California.
    All rights reserved.
@@ -52,7 +52,7 @@ import ptolemy.util.StringUtilities;
 import ptolemy.vergil.basic.export.ExportModel;
 
 ///////////////////////////////////////////////////////////////////
-//// ExportModelJUnitTestBatch2
+//// ExportModelJUnitTestBatch4
 /**
  * JUnit test that exports the demos between two indices.
  *
@@ -60,18 +60,17 @@ import ptolemy.vergil.basic.export.ExportModel;
  * <pre>
  * cd $PTII
  * ./configure
- * ant test.single -Dtest.name=ptolemy.vergil.basic.export.test.junit.ExportModelJUnitTestBatch2 -Djunit.formatter=plain
+ * ant test.single -Dtest.name=ptolemy.vergil.basic.export.test.junit.ExportModelJUnitTestBatch4 -Djunit.formatter=plain
  * </pre>
  * or
  * <pre>
  * cd $PTII/ptolemy/vergil/basic/export/test/junit/;
  * export CLASSPATH=${PTII}:${PTII}/lib/junit-4.8.2.jar:${PTII}/lib/JUnitParams-0.3.0.jar${PTII}:${PTII}/lib/junit-4.8.2.jar:${PTII}/lib/JUnitParams-0.3.0.jar;
  * export JAVAFLAGS="-Dptolemy.ptII.exportHTML.linkToJNLP=true -Dptolemy.ptII.exportHTML.usePtWebsite=true"
- * $PTII/bin/ptinvoke org.junit.runner.JUnitCore ptolemy.vergil.basic.export.test.junit.ExportModelJUnitTestBatch2
+ * $PTII/bin/ptinvoke org.junit.runner.JUnitCore ptolemy.vergil.basic.export.test.junit.ExportModelJUnitTestBatch4
  * </pre>
  * <p>
- * This test uses JUnitParams from <a
- * href="http://code.google.com/p/junitparams/#in_browser"http://code.google.com/p/junitparams/</a>,
+ * This test uses JUnitParams from <a href="http://code.google.com/p/junitparams/#in_browser"http://code.google.com/p/junitparams/</a>,
  *  which is released under <a href="http://www.apache.org/licenses/LICENSE-2.0#in_browser">Apache License 2.0</a>.
  * </p>
  *
@@ -82,10 +81,10 @@ import ptolemy.vergil.basic.export.ExportModel;
  * @Pt.AcceptedRating Red (cxh)
  */
 @RunWith(JUnitParamsRunner.class)
-public class ExportModelJUnitTestBatch2 extends ExportModelJUnitTestBatch {
+public class ExportModelJUnitTestBatch4 extends ExportModelJUnitTestBatch {
 
-    /** Run demos 151 through 220. */
+    /** Run demos 291 through 360. */
     public Object[] demos() throws IOException {
-        return super.demos(151, 220);
+        return super.demos(290, 360);
     }
 }
