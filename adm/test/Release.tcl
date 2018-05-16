06e80f7462952cccac2f1d7c60db532d6755a714
travis-ci <travis@travis-ci.org>
2018-05-16 07:56:08 -0700

    Reorganizing tests in adm/test.  Move remaining installer tests to adm/installer2/test so that they can be run after adm/installer/test.  Fixed up tests to run under git.

--- adm/test/Release.tcl
+++ adm/test/Release.tcl
@@ -45,8 +45,6 @@ test release-1.1 {Check for missing makefiles} {
 } {./config/makefile
 ./doc/coding/templates/makefile
 ./ptolemy/actor/lib/fmi/fmipp/swig/makefile
-./ptolemy/actor/ptalon/demo/ptinyos/SendAndReceiveCnt/output/makefile
-./ptolemy/actor/ptalon/demo/ptinyos/SenseToLeds/output/makefile
 ./ptolemy/backtrack/automatic/ptolemy/actor/lib/makefile
 ./ptolemy/backtrack/automatic/ptolemy/domains/sdf/lib/makefile
 ./ptolemy/backtrack/automatic/ptolemy/math/makefile
@@ -66,8 +64,7 @@ test release-1.1 {Check for missing makefiles} {
 
 test release-2.1 {Check for directories that have java files, but are not in doc/makefile} {
     exec make --no-print-directory --silent missingDocPackages
-} {.
-config
+} {config
 contrib.actor.lib.example
 diva.util.java2d.svg
 doc.coding.templates
@@ -82,11 +79,6 @@ ptolemy.actor.lib.javasound.test.pitchshift
 ptolemy.actor.lib.reactable
 ptolemy.actor.lib.tutorial
 ptolemy.backtrack.automatic.ptolemy.math
-ptolemy.backtrack.eclipse.plugin.actions.codestyle
-ptolemy.backtrack.eclipse.plugin.compatibility
-ptolemy.backtrack.eclipse.plugin.console
-ptolemy.backtrack.eclipse.plugin.dialogs
-ptolemy.backtrack.eclipse.plugin.widgets
 ptolemy.backtrack.test.array1
 ptolemy.backtrack.test.random1
 ptolemy.backtrack.test.test1
@@ -99,13 +91,17 @@ ptolemy.component.domains.ptinyos
 ptolemy.copernicus.interpreted
 ptolemy.copernicus.kernel.fragment
 ptolemy.domains.gr.lib.experimental
+ptolemy.domains.openmodelica.kernel
+ptolemy.domains.openmodelica.lib
+ptolemy.domains.openmodelica.lib.omc
+ptolemy.domains.openmodelica.lib.omc.corba
 ptolemy.domains.wireless.lib.network
 ptolemy.domains.wireless.lib.network.mac
 ptolemy.domains.wireless.lib.tinyOS
 ptolemy.plot.servlet}
 
 set currentDirectory [pwd]
-test release-3.1 {Run svn status and look for files that should be checked in.  See ptII/adm/bin/svnignoreupdate for a script to fix this} {
+test release-3.1 {Run git status and look for files that should be checked in.  See ptII/adm/bin/svnignoreupdate for a script to fix this} {
 
     exec make --no-print-directory --silent rmClassFiles
     cd "$PTII"
@@ -114,7 +110,7 @@ test release-3.1 {Run svn status and look for files that should be checked in.  
     #} 
     puts "Removing doc/books/systems because building jnlp files creates files and directories."
     exec rm -rf doc/books/systems papers
-    exec svn update doc/books/systems
+    exec git checkout doc/books/systems
     # Other places to update the book location: $PTII/adm/gen-X.Y/{makefile,ptIIX_Y_devel_setup_windows.xml,shortcutSpec.xml}
     set bookURL http://ptolemy.eecs.berkeley.edu/books/Systems/PtolemyII_DigitalV1_02.pdf
     puts "Getting the Ptolemy book $bookURL"
@@ -132,7 +128,7 @@ test release-3.1 {Run svn status and look for files that should be checked in.  
     }
 
     set result {}
-    set status [exec svn status]
+    set status [exec git status]
     set data [split $status "\n"]
     foreach line $data {
 	# Skip directories in /demo/ because they were created by exporting MoML
@@ -152,70 +148,72 @@ test release-3.1 {Run svn status and look for files that should be checked in.  
 	}
     }
 
-    set result [lsort $result]
-    set result1 \
-{{
-?       .maven} {
-?       capeCodeNonGUI} {
-?       cobertura.ser} {
-?       ptolemy/actor/lib/jai/test/auto/PtolemyII.bmp} {
-?       ptolemy/actor/lib/jai/test/auto/PtolemyII.jpg} {
-?       ptolemy/actor/lib/jai/test/auto/PtolemyII.pgm} {
-?       ptolemy/actor/lib/jai/test/auto/PtolemyII.tif} {
-?       ptolemy/configs/doc/ClassesIllustrated} {
-?       ptolemy/matlab/META-INF} {
-?       ptolemy/matlab/matlabLinux.jar} {
-?       reports}}
-    if { $result == $result1 } {
-	puts "Result was:\n$result\nWhich is ok"
-        set resultMessage {}
-    } else {
-	set result2 \
-{{
-!       ptolemy/vergil/basic/layout/kieler/test/layoutPerformance.xml} {
-!       ptolemy/vergil/basic/layout/kieler/test/layoutPerformance2.xml} {
-?       .maven} {
-?       adm/gen-11.0/opencv_java320.dll} {
-?       capeCodeNonGUI} {
-?       cobertura.ser} {
-?       generated-sources} {
-?       org/terraswarm/accessor/accessors} {
-?       ptolemy/actor/lib/jai/test/auto/PtolemyII.bmp} {
-?       ptolemy/actor/lib/jai/test/auto/PtolemyII.jpg} {
-?       ptolemy/actor/lib/jai/test/auto/PtolemyII.pgm} {
-?       ptolemy/actor/lib/jai/test/auto/PtolemyII.tif} {
-?       ptolemy/actor/lib/jai/test/auto/file.png} {
-?       ptolemy/configs/doc/ClassesIllustrated} {
-?       ptolemy/domains/space/test/auto/DOP.csv} {
-?       ptolemy/domains/space/test/auto/Placard.tex} {
-?       ptolemy/vergil/basic/export/html/test/Butterfly.gif} {
-?       ptolemy/vergil/basic/export/test/outfile.wav} {
-?       ptserver/test/PtolemyServer.log} {
-?       reports} {
-?       vendors/jogl} {
-?       vendors/universalJavaApplicationStub} {
-M       lib/diva.jar} {
-M       lib/ptliblicenses.jar}}
-        if { $result == $result2 } {
-	    puts "Result was:\n$result\nWhich is ok"
-            set resultMessage {}
-        } else {
-	    puts "This test is annoying, it runs svn status on the build directory to
-                look for files that are not checked in.  There are two states:
-                1) Running \"cd \$PTII/adm/test; \$PTII/bin/ptjacl Release.tcl\" on a regular, non-nightly build machine
-	        2) Running the same command by hand on the nightly build machine.
-	        The result of (cd \$PTII; svn status) was:\n $result \
-                \nWhich is not the same as the result on a regular, non-nightly build machine.
-                $result1 \nHere's the diff:\n [diffText $result $result1] \
-                or the command on the nightly build machine\n $result2 Here's the diff:\n [diffText $result $result2]
-                The fix is to run (cd \$PTII; svn status)
-                and for each modified file, either remove it, check it in or add it to svn:ignore by running
-                \$PTII/adm/bin/svnignoreupdate in the directory above the file to be ignored or by using
-                \"svn propertyset svn:ignore filename; svn commit -m 'Added filename to svn:ignore.'\""
-	    set resultMessage $result
-        }
-    }
-    list $resultMessage
+
+#     set result [lsort $result]
+#     set result1 \
+# {{
+# ?       .maven} {
+# ?       capeCodeNonGUI} {
+# ?       cobertura.ser} {
+# ?       ptolemy/actor/lib/jai/test/auto/PtolemyII.bmp} {
+# ?       ptolemy/actor/lib/jai/test/auto/PtolemyII.jpg} {
+# ?       ptolemy/actor/lib/jai/test/auto/PtolemyII.pgm} {
+# ?       ptolemy/actor/lib/jai/test/auto/PtolemyII.tif} {
+# ?       ptolemy/configs/doc/ClassesIllustrated} {
+# ?       ptolemy/matlab/META-INF} {
+# ?       ptolemy/matlab/matlabLinux.jar} {
+# ?       reports}}
+#     if { $result == $result1 } {
+# 	puts "Result was:\n$result\nWhich is ok"
+#         set resultMessage {}
+#     } else {
+# 	set result2 \
+# {{
+# !       ptolemy/vergil/basic/layout/kieler/test/layoutPerformance.xml} {
+# !       ptolemy/vergil/basic/layout/kieler/test/layoutPerformance2.xml} {
+# ?       .maven} {
+# ?       adm/gen-11.0/opencv_java320.dll} {
+# ?       capeCodeNonGUI} {
+# ?       cobertura.ser} {
+# ?       generated-sources} {
+# ?       org/terraswarm/accessor/accessors} {
+# ?       ptolemy/actor/lib/jai/test/auto/PtolemyII.bmp} {
+# ?       ptolemy/actor/lib/jai/test/auto/PtolemyII.jpg} {
+# ?       ptolemy/actor/lib/jai/test/auto/PtolemyII.pgm} {
+# ?       ptolemy/actor/lib/jai/test/auto/PtolemyII.tif} {
+# ?       ptolemy/actor/lib/jai/test/auto/file.png} {
+# ?       ptolemy/configs/doc/ClassesIllustrated} {
+# ?       ptolemy/domains/space/test/auto/DOP.csv} {
+# ?       ptolemy/domains/space/test/auto/Placard.tex} {
+# ?       ptolemy/vergil/basic/export/html/test/Butterfly.gif} {
+# ?       ptolemy/vergil/basic/export/test/outfile.wav} {
+# ?       ptserver/test/PtolemyServer.log} {
+# ?       reports} {
+# ?       vendors/jogl} {
+# ?       vendors/universalJavaApplicationStub} {
+# M       lib/diva.jar} {
+# M       lib/ptliblicenses.jar}}
+#         if { $result == $result2 } {
+# 	    puts "Result was:\n$result\nWhich is ok"
+#             set resultMessage {}
+#         } else {
+	    # puts "This test is annoying, it runs git status on the build directory to
+            #     look for files that are not checked in.  There are two states:
+            #     1) Running \"cd \$PTII/adm/test; \$PTII/bin/ptjacl Release.tcl\" on a regular, non-nightly build machine
+	    #     2) Running the same command by hand on the nightly build machine.
+	    #     The result of (cd \$PTII; git status) was:\n $result \
+            #     \nWhich is not the same as the result on a regular, non-nightly build machine.
+            #     $result1 \nHere's the diff:\n [diffText $result $result1] \
+            #     or the command on the nightly build machine\n $result2 Here's the diff:\n [diffText $result $result2]
+            #     The fix is to run (cd \$PTII; git status)
+            #     and for each modified file, either remove it, check it in or add it to $PTII/.gitignore."
+	    # set resultMessage $result
+#        }
+#    }
+#    list $resultMessage
+
+    list $result
+
 } {{}}
 cd "$currentDirectory"
 
