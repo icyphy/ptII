5d11eb1d141f626999f2cff526e6bdf63ec24372
travis-ci <travis@travis-ci.org>
2018-10-19 10:32:00 -0700

    Update location of ptolemy wiki, which is different than the ptexternal wiki

--- ptolemy/apps/eclipse/awt/Snippet135.java
+++ ptolemy/apps/eclipse/awt/Snippet135.java
@@ -1,5 +1,5 @@
 // From http://dev.eclipse.org/viewcvs/index.cgi/org.eclipse.swt.snippets/src/org/eclipse/swt/snippets/Snippet135.java?view=co
-// See also http://chess.eecs.berkeley.edu/ptolemy/wiki/Ptolemy/EclipseAndAWT
+// See also https://wiki.eecs.berkeley.edu/ptolemy/Ptolemy/EclipseAndAWT
 
 /*******************************************************************************
  * Copyright (c) 2000, 2004 IBM Corporation and others.
