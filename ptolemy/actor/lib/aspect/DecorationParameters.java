7249f0c5052598d9a05e0d03390fb0da6be9e695
eal <eal@eecs.berkeley.edu>
2018-12-03 17:17:34 -0800

    Very raw start to adding decoration parameters to composite communication aspects

---
+++ ptolemy/actor/lib/aspect/DecorationParameters.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package ptolemy.actor.lib.aspect;
+
+import ptolemy.kernel.util.Attribute;
+import ptolemy.kernel.util.IllegalActionException;
+import ptolemy.kernel.util.NameDuplicationException;
+import ptolemy.kernel.util.NamedObj;
+import ptolemy.kernel.util.Workspace;
+
+/** FIXME
+ *  @author FIXME
+ *
+ */
+public class DecorationParameters extends Attribute {
+
+    /**
+     * @param container
+     * @param name
+     * @throws IllegalActionException
+     * @throws NameDuplicationException
+     */
+    public DecorationParameters(NamedObj container, String name)
+            throws IllegalActionException, NameDuplicationException {
+        super(container, name);
+        // TODO Auto-generated constructor stub
+    }
+}
