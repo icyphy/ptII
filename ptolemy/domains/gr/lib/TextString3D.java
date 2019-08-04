c775d1f8b27ab5923e8abda39a0b66193f5a9bc8
Christopher Brooks <cxbrooks@gmail.com>
2019-08-04 15:30:24 -0700

    Converted tabs to spaces.

--- ptolemy/domains/gr/lib/TextString3D.java
+++ ptolemy/domains/gr/lib/TextString3D.java
@@ -244,28 +244,28 @@ public class TextString3D extends GRShadedShape {
         Font3D font3D = new Font3D(new Font(fontFamilyValue, Font.PLAIN, 1),
                 extrusion);
 
-	try {
-	    _textGeometry = new Text3D(font3D, textValue);
-	    _textGeometry.setCapability(Text3D.ALLOW_STRING_WRITE);
-	} catch (IllegalArgumentException ex) {
-	    // 7/19: Recent version of Java3D is throwing: TriangleArray: illegal vertexCount
-	    // Try with a default font.
-	    Font3D previousFont3D = font3D;
+        try {
+            _textGeometry = new Text3D(font3D, textValue);
+            _textGeometry.setCapability(Text3D.ALLOW_STRING_WRITE);
+        } catch (IllegalArgumentException ex) {
+            // 7/19: Recent version of Java3D is throwing: TriangleArray: illegal vertexCount
+            // Try with a default font.
+            Font3D previousFont3D = font3D;
 
-	    try {
-		System.err.println(getFullName() + ": Failed to create the Text3D."
-				   + "  Trying with the default font.");
-		font3D = new Font3D(new Font(null, Font.PLAIN, 1), extrusion);
-		_textGeometry = new Text3D(font3D, textValue);
-		_textGeometry.setCapability(Text3D.ALLOW_STRING_WRITE);
-	    } catch (Throwable ex2) {
-		throw new IllegalActionException(this, ex,
-					     "Failed to create Text3D for font \""
-					     + previousFont3D + "\" "
-					     + previousFont3D.getFont() + " and \""
-					     + textValue + "\".");
-	    }
-	}
+            try {
+                System.err.println(getFullName() + ": Failed to create the Text3D."
+                                   + "  Trying with the default font.");
+                font3D = new Font3D(new Font(null, Font.PLAIN, 1), extrusion);
+                _textGeometry = new Text3D(font3D, textValue);
+                _textGeometry.setCapability(Text3D.ALLOW_STRING_WRITE);
+            } catch (Throwable ex2) {
+                throw new IllegalActionException(this, ex,
+                                             "Failed to create Text3D for font \""
+                                             + previousFont3D + "\" "
+                                             + previousFont3D.getFont() + " and \""
+                                             + textValue + "\".");
+            }
+        }
 
         String alignmentValue = alignment.stringValue();
         int align = Text3D.ALIGN_CENTER;
