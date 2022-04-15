a53a10aa0933405e53705aee8a251937012b9739
Christopher Brooks <cxbrooks@gmail.com>
2022-04-14 18:14:45 -0700

    Revert "Print out the value of CERTI_FOM_PATH."
    This reverts commit 35ae824e83b35b1b5778e687d5a95221f6cb4fe9.

--- org/hlacerti/lib/CertiRtig.java
+++ org/hlacerti/lib/CertiRtig.java
@@ -247,7 +247,6 @@ public class CertiRtig extends NamedObj {
         } else {
             certiFomPath = "CERTI_FOM_PATH=" + certiHome + "/share/federations/";
         }
-        System.out.println("CertiRtig: " + certiFomPath);
         _environmentArray[1] = certiFomPath;
 
         _directoryAsFile = new File(fedFileName.getParent());
